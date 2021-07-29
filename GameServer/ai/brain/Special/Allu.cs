dccecad845992bf459e598f5fecf9b6fdaaf69dd
Meeks Meeks <blurubberduck87@gmail.com>
2021-07-28 17:00:13 -0700

    broke out into independant classes. Clean commit for naming convention change on brains

---
+++ GameServer/ai/brain/Special/Allu.cs
@@ -0,0 +1,25 @@
+﻿using DOL.AI.Brain;
+using DOL.GS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOL.AI.Brain
+{
+	public class AlluBrain : StandardMobBrain
+	{
+		public override void Think()
+		{
+			Alluvian mob = Body as Alluvian;
+
+			if (Alluvian.GlobuleNumber < 12)
+			{
+				mob.SpawnGlobule();
+			}
+			base.Think();
+
+		}
+	}
+}
--- GameServer/ai/brain/Special/Globule.cs
+++ GameServer/ai/brain/Special/Globule.cs
@@ -1,85 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using DOL.Events;
+﻿using System.Collections.Generic;
 using DOL.GS;
 using DOL.GS.Scheduler;
-
-
-namespace DOL.AI.Brain
-{
-    public class AlluvianMob : GameNPC
-	{
-		/// <summary>
-		/// Blank for spawned mobs, which dont REspawn!
-		/// </summary>
-		public override void StartRespawn()
-		{
-		}
-
-		public override void SaveIntoDatabase()
-		{
-		}
-	}
-
-	public class Alluvian : GameNPC
-	{
-		public Alluvian() : base()
-		{
-			SetOwnBrain(new AlluBrain());
-		}
-
-		private static int m_globuleCount;
-
-		//Holds the current number of globules, be sure to GlobuleNumber--; when a globule dies.
-		public static int GlobuleNumber
-		{
-			get { return m_globuleCount; }
-			set { m_globuleCount = value; }
-		}
-
-		public int SpawnGlobule()
-		{
-			AlluvianMob globulespawn = new AlluvianMob();
-			globulespawn.Model = 928;
-			globulespawn.Size = 40;
-			globulespawn.Level = (byte)Util.Random(3, 4);
-			globulespawn.Name = "alluvian globule";
-			globulespawn.CurrentRegionID = 51;
-			globulespawn.Heading = Heading;
-			globulespawn.Realm = 0;
-			globulespawn.CurrentSpeed = 0;
-			globulespawn.MaxSpeedBase = 191;
-			globulespawn.GuildName = "";
-			globulespawn.X = X;
-			globulespawn.Y = Y;
-			globulespawn.Z = 3083;
-			globulespawn.RespawnInterval = -1;
-			globulespawn.BodyType = 4;
-			globulespawn.Flags ^= eFlags.FLYING;
-
-			GlobuleBrain brain = new GlobuleBrain();
-			brain.AggroLevel = 70;
-			brain.AggroRange = 500;
-			globulespawn.SetOwnBrain(brain);
-			globulespawn.AutoSetStats();
-			globulespawn.AddToWorld();
-			GlobuleNumber++;
-			brain.WalkFromSpawn();
-			//Tell me when you die so I can GlobuleNumber--;
-			GameEventMgr.AddHandler(globulespawn, GameNPCEvent.Dying, new DOLEventHandler(GlobuleHasDied));
-
-			return 0;
-		}
-
-		public static void GlobuleHasDied(DOLEvent e, object sender, EventArgs args)
-		{
-			GlobuleNumber--;
-			//Remove the handler so they don't pile up.
-			GameEventMgr.RemoveHandler(sender, GameNPCEvent.Dying, new DOLEventHandler(GlobuleHasDied));
-			return;
-		}
-	}
-}
 
 namespace DOL.AI.Brain
 {
@@ -226,7 +147,7 @@ namespace DOL.AI.Brain
 		public bool CheckStorm()
 		{
 			var currentStorm = GameServer.Instance.WorldManager.WeatherManager[Body.CurrentRegionID];
-			var Glob = Body as AlluvianMob;
+			var Glob = Body as Alluvian;
 			if (currentStorm != null)
 			{
 				var weatherCurrentPosition = currentStorm.CurrentPosition(SimpleScheduler.Ticks);
@@ -254,21 +175,6 @@ namespace DOL.AI.Brain
 			Body.Level = (byte)Util.Random(10, 11);
 			Body.AutoSetStats();
 			hasGrown = true;
-		}
-	}
-
-	public class AlluBrain : StandardMobBrain
-	{
-		public override void Think()
-		{
-			Alluvian mob = Body as Alluvian;
-
-			if (Alluvian.GlobuleNumber < 12)
-			{
-				mob.SpawnGlobule();
-			}
-			base.Think();
-
 		}
 	}
 }
