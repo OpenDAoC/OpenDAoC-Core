07b29a98f8a564340638a50555398a2650d90b73
Anthony Knight <midgetlord123@hotmail.com>
2021-08-02 19:26:57 -0700

    Damage, Buff, and Debuff Components

---
+++ GameServer/ECS-Components/SpellEffects/DamageEffectComponent.cs
@@ -0,0 +1,20 @@
+﻿namespace DOL.GS.SpellEffects
+{
+    public struct DamageEffectComponent : IEffectComponent
+    {
+        public int Value;
+        public GameLiving Target;
+        public GameLiving Caster;
+        public eSpellEffect Type { get; set; }
+        public ushort SpellEffectId { get; set; }
+
+        public DamageEffectComponent(int value, GameLiving target, GameLiving caster, ushort spellEffectId)
+        {
+            this.Value = value;
+            this.Target = target;
+            this.Caster = caster;
+            this.Type = eSpellEffect.Damage;
+            this.SpellEffectId = spellEffectId;
+        }
+    }
+}
