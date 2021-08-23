2027140000aeecf945e9fd563cdf7989eb744bdc
Ian Applegate <ian@jedi.sh>
2021-08-23 13:12:35 -0400

    Apply registry to AttackSystem

--- GameServer/ECS-Services/AttackService.cs
+++ GameServer/ECS-Services/AttackService.cs
@@ -20,25 +20,15 @@ namespace DOL.GS
         {
             Diagnostics.StartPerfCounter(ServiceName);
 
-            foreach (var p in EntityManager.GetAllPlayers())
+            foreach (var living in EntityManager.GetLivingByComponent(typeof(AttackComponent)))
             {
-                if (p == null)
+                if (living == null)
                     continue;
 
-                if (p.attackComponent is null)
+                if (living.attackComponent is null)
                     continue;
 
-                p.attackComponent.Tick(tick);
-            }
-            foreach (var npc in EntityManager.GetAllNpcs())
-            {
-                if (npc == null)
-                    continue;
-
-                if (npc.attackComponent is null)
-                    continue;
-
-                npc.attackComponent.Tick(tick);
+                living.attackComponent.Tick(tick);
             }
 
             Diagnostics.StopPerfCounter(ServiceName);
