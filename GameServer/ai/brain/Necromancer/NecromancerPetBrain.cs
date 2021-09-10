c599f81cf3b0d69fbcd482853fde2352e58d7b97
kbvandergriffjr <kbvandergriffjr@gmail.com>
2021-09-10 16:34:28 -0400

    Necro pet fix

--- GameServer/ai/brain/Necromancer/NecromancerPetBrain.cs
+++ GameServer/ai/brain/Necromancer/NecromancerPetBrain.cs
@@ -72,7 +72,14 @@ namespace DOL.AI.Brain
 			{
 				GamePlayer playerowner = GetPlayerOwner();
 
-				if (playerowner != null && (GameTimer.GetTickCount() - playerowner.Client.GameObjectUpdateArray[new Tuple<ushort, ushort>(Body.CurrentRegionID, (ushort)Body.ObjectID)]) > ThinkInterval)
+                long lastUpdate = 0;
+                if (!playerowner.Client.GameObjectUpdateArray.TryGetValue(new Tuple<ushort, ushort>(Body.CurrentRegionID, (ushort)Body.ObjectID), out lastUpdate))
+                {
+                    playerowner.Client.GameObjectUpdateArray.Add(new Tuple<ushort, ushort>(Body.CurrentRegionID, (ushort)Body.ObjectID), lastUpdate);
+                }
+
+
+                if (playerowner != null && (GameTimer.GetTickCount() - playerowner.Client.GameObjectUpdateArray[new Tuple<ushort, ushort>(Body.CurrentRegionID, (ushort)Body.ObjectID)]) > ThinkInterval)
 				{
 					playerowner.Out.SendObjectUpdate(Body);
 				}
