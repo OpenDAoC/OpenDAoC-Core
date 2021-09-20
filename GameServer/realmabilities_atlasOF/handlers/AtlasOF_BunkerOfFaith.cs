c6b8dadaab65b0b3f5dd8bf0d8076abb3d20c42d
jeromeparent <jerome.parent@gmail.com>
2021-09-20 07:59:09 -0400

    Fixed Bunker of Faith reuse timer that I accidentally left to 5s instead of 30min...

--- GameServer/realmabilities_atlasOF/handlers/AtlasOF_BunkerOfFaith.cs
+++ GameServer/realmabilities_atlasOF/handlers/AtlasOF_BunkerOfFaith.cs
@@ -13,7 +13,7 @@ namespace DOL.GS.RealmAbilities
 
         public override int MaxLevel { get { return 1; } }
         public override int CostForUpgrade(int level) { return 10; }
-        public override int GetReUseDelay(int level) { return 5; } // 30 mins
+        public override int GetReUseDelay(int level) { return 1800; } // 30 mins
         protected override int GetAbsorbAmount() { return 50; }
     }
 }
