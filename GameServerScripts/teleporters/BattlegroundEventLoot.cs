1f5c355cfc66df1fa97be0d717228bd7326ccf43
Anthony Knight <midgetlord123@hotmail.com>
2021-10-06 11:25:46 -0700

    Midgard 2h event loot fix

--- GameServerScripts/teleporters/BattlegroundEventLoot.cs
+++ GameServerScripts/teleporters/BattlegroundEventLoot.cs
@@ -220,6 +220,9 @@ namespace DOL.GS.Scripts
 					GenerateWeapon(player, charClass, eObjectType.Hammer, eInventorySlot.RightHandWeapon);
 					GenerateWeapon(player, charClass, eObjectType.Sword, eInventorySlot.RightHandWeapon);
 					GenerateWeapon(player, charClass, eObjectType.Shield, eInventorySlot.LeftHandWeapon);
+					GenerateWeapon(player, charClass, eObjectType.Axe, eInventorySlot.TwoHandWeapon);
+					GenerateWeapon(player, charClass, eObjectType.Hammer, eInventorySlot.TwoHandWeapon);
+					GenerateWeapon(player, charClass, eObjectType.Sword, eInventorySlot.TwoHandWeapon);
 					break;
 
 				case eCharacterClass.Skald:
