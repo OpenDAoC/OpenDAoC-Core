4a235d10cb02cbda2b97613b7dc1d34608839f79
clait <stefanomus@fastmail.com>
2021-10-14 18:56:54 +0100

    Additional testing phase title.

--- GameServerScripts/titles/AtlasTesterTitle.cs
+++ GameServerScripts/titles/AtlasTesterTitle.cs
@@ -19,6 +19,16 @@ namespace GameServerScripts.Titles
         protected override Tuple<string, string> DescriptionValue { get { return new Tuple<string, string>("Titles.Atlas.Bugged", "Titles.Atlas.Bugged"); }}
         protected override Func<DOL.GS.GamePlayer, bool> SuitableMethod { get { return player => player.CreationDate < DateTime.Now; }} // fake check TODO change with launch date
     }
+    
+    /// <summary>
+    /// "I'm bugged" title granted to all account that have played the beta
+    /// </summary>
+    public class AtlasTestTitleLD : TranslatedNoGenderGenericEventPlayerTitle
+    {
+        public override DOLEvent Event { get { return GamePlayerEvent.GameEntered; }}
+        protected override Tuple<string, string> DescriptionValue { get { return new Tuple<string, string>("Titles.Atlas.LD", "Titles.Atlas.LD"); }}
+        protected override Func<DOL.GS.GamePlayer, bool> SuitableMethod { get { return player => player.CreationDate < DateTime.Now; }} // fake check TODO change with launch date
+    }
 	
 }
 
