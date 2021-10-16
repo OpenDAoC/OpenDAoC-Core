b41cd74d5d7cbdca5fc05964c602a7fc12f954fd
jeromeparent <jerome.parent@gmail.com>
2021-10-16 16:41:53 -0400

    Crash fix for keep doors

--- GameServer/keeps/Gameobjects/GameKeepDoor.cs
+++ GameServer/keeps/Gameobjects/GameKeepDoor.cs
@@ -171,14 +171,14 @@ namespace DOL.GS.Keeps
 		public override bool IsAttackableDoor
 		{
 			get
-			{
-				if (Component.Keep.Region == 252)
-					return true;
-				
+			{				
 				if (Component == null || Component.Keep == null)
 					return false;
 
-				if (Component.Keep is GameKeepTower)
+                if (Component.Keep.Region == 252)
+                    return true;
+
+                if (Component.Keep is GameKeepTower)
 				{
 					if (DoorIndex == 1)
 						return true;
