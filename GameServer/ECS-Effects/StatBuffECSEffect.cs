dca96e68a03e29a4a5be81f9ad5f6deb68d1d111
kbvandergriffjr <kbvandergriffjr@gmail.com>
2021-10-26 15:54:43 -0400

    Paladin self AF buffs should now properly apply their AF.

--- GameServer/ECS-Effects/StatBuffECSEffect.cs
+++ GameServer/ECS-Effects/StatBuffECSEffect.cs
@@ -27,6 +27,10 @@ namespace DOL.GS
             {
                 ApplyBonus(Owner, (SpellHandler as ArmorFactorBuff).BonusCategory1, eProperty.ArmorFactor, SpellHandler.Spell.Value, Effectiveness, false);
             }
+            else if (SpellHandler.Spell.SpellType == (byte)eSpellType.PaladinArmorFactorBuff)
+            {
+                ApplyBonus(Owner, (SpellHandler as PaladinArmorFactorBuff).BonusCategory1, eProperty.ArmorFactor, SpellHandler.Spell.Value, Effectiveness, false);
+            }
             else
             {
                 foreach (var prop in EffectService.GetPropertiesFromEffect(EffectType))
@@ -70,6 +74,10 @@ namespace DOL.GS
             else if (SpellHandler.Spell.SpellType == (byte)eSpellType.ArmorFactorBuff)
             {
                 ApplyBonus(Owner, (SpellHandler as ArmorFactorBuff).BonusCategory1, eProperty.ArmorFactor, SpellHandler.Spell.Value, Effectiveness, true);
+            }
+            else if (SpellHandler.Spell.SpellType == (byte)eSpellType.PaladinArmorFactorBuff)
+            {
+                ApplyBonus(Owner, (SpellHandler as PaladinArmorFactorBuff).BonusCategory1, eProperty.ArmorFactor, SpellHandler.Spell.Value, Effectiveness, true);
             }
             else
             {
