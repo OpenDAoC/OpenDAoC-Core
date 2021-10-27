26af05a2c2360d7dce8ca9a5456ca57f2681ac91
Anthony Knight <midgetlord123@hotmail.com>
2021-10-26 23:23:19 -0700

    Hunters no longer receive axes or hammers :catlook:

--- GameServerScripts/AtlasEvents/BattlegroundEventLoot.cs
+++ GameServerScripts/AtlasEvents/BattlegroundEventLoot.cs
@@ -397,11 +397,7 @@ namespace DOL.GS.Scripts
 					break;
 
 				case eCharacterClass.Hunter:
-					GenerateWeapon(player, charClass, eObjectType.Axe, eInventorySlot.RightHandWeapon);
-					GenerateWeapon(player, charClass, eObjectType.Hammer, eInventorySlot.RightHandWeapon);
 					GenerateWeapon(player, charClass, eObjectType.Sword, eInventorySlot.RightHandWeapon);
-					GenerateWeapon(player, charClass, eObjectType.Axe, eInventorySlot.TwoHandWeapon);
-					GenerateWeapon(player, charClass, eObjectType.Hammer, eInventorySlot.TwoHandWeapon);
 					GenerateWeapon(player, charClass, eObjectType.Sword, eInventorySlot.TwoHandWeapon);
 					GenerateWeapon(player, charClass, eObjectType.CompositeBow, eInventorySlot.DistanceWeapon);
 					GenerateWeapon(player, charClass, eObjectType.Spear, eInventorySlot.TwoHandWeapon);
