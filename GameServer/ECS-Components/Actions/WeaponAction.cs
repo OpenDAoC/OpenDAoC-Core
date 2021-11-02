ec1d8c5d6e7d09e1c962ed0577f9e7523d785505
kbvandergriffjr <kbvandergriffjr@gmail.com>
2021-11-02 17:45:19 -0400

    Style bonuses will only be applied on a successful style.

--- GameServer/ECS-Components/Actions/WeaponAction.cs
+++ GameServer/ECS-Components/Actions/WeaponAction.cs
@@ -215,6 +215,8 @@ namespace DOL.GS
                     //                         }
                 }
             }
+            else
+                owner.TempProperties.removeProperty(LAST_ATTACK_DATA);
 
             //CMH
             // 1.89:
