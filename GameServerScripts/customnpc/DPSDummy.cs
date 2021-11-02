d6e70ebe5ae6a14589364acc8bc9e2cd3cdb6121
Anthony Knight <midgetlord123@hotmail.com>
2021-11-02 00:54:32 -0700

    Players can modify the [resistances] of DPS Dummies

--- GameServerScripts/customnpc/DPSDummy.cs
+++ GameServerScripts/customnpc/DPSDummy.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DOL.GS.PacketHandler;
+using System;
 
 namespace DOL.GS
 {
@@ -16,10 +17,141 @@ namespace DOL.GS
                 return false;
             }
 
+            
+
             Damage = 0;
             StartCheck = true;
             Name = "Total: 0 DPS: 0";
+            ResetArmorAndResists();
+
+            SendReply(player, "Hello, you can change my [armor] and [resistances] with ease, you need but ask. Right click me to reset them back to 0.");
             return true;
+        }
+
+
+        public override bool WhisperReceive(GameLiving source, string text)
+        {
+            if (!base.WhisperReceive(source, text)) return false;
+            if (!(source is GamePlayer player)) return false;
+
+            string[] splitText = text.Split(' ');
+            if(splitText.Length > 1)
+            {
+                if(!double.TryParse(splitText[1], out _) || double.Parse(splitText[1]) < 0 || double.Parse(splitText[1]) > 70) { 
+                    SendReply(player, "Number must be between 0 and 70");
+                    return false;
+                }
+                switch (splitText[0].ToLower())
+                {
+                    case "slash":
+                        ApplyBonus(this, eBuffBonusCategory.BaseBuff, (eProperty)eResist.Slash, double.Parse(splitText[1]), 1, false);
+                        Console.WriteLine($"Slash resistance increased by {double.Parse(splitText[1])}");
+                        break;
+                    case "thrust":
+                        ApplyBonus(this, eBuffBonusCategory.BaseBuff, (eProperty)eResist.Thrust, double.Parse(splitText[1]), 1, false);
+                        Console.WriteLine($"Thrust resistance increased by {double.Parse(splitText[1])}");
+                        break;
+                    case "crush":
+                        ApplyBonus(this, eBuffBonusCategory.BaseBuff, (eProperty)eResist.Crush, double.Parse(splitText[1]), 1, false);
+                        Console.WriteLine($"Crush resistance increased by {double.Parse(splitText[1])}");
+                        break;
+                    case "body":
+                        ApplyBonus(this, eBuffBonusCategory.BaseBuff, (eProperty)eResist.Body, double.Parse(splitText[1]), 1, false);
+                        Console.WriteLine($"Body resistance increased by {double.Parse(splitText[1])}");
+                        break;
+                    case "cold":
+                        ApplyBonus(this, eBuffBonusCategory.BaseBuff, (eProperty)eResist.Cold, double.Parse(splitText[1]), 1, false);
+                        Console.WriteLine($"Cold resistance increased by {double.Parse(splitText[1])}");
+                        break;
+                    case "energy":
+                        ApplyBonus(this, eBuffBonusCategory.BaseBuff, (eProperty)eResist.Energy, double.Parse(splitText[1]), 1, false);
+                        Console.WriteLine($"Energy resistance increased by {double.Parse(splitText[1])}");
+                        break;
+                    case "heat":
+                        ApplyBonus(this, eBuffBonusCategory.BaseBuff, (eProperty)eResist.Heat, double.Parse(splitText[1]), 1, false);
+                        Console.WriteLine($"Heat resistance increased by {double.Parse(splitText[1])}");
+                        break;
+                    case "matter":
+                        ApplyBonus(this, eBuffBonusCategory.BaseBuff, (eProperty)eResist.Matter, double.Parse(splitText[1]), 1, false);
+                        Console.WriteLine($"Matter resistance increased by {double.Parse(splitText[1])}");
+                        break;
+                    case "spirit":
+                        ApplyBonus(this, eBuffBonusCategory.BaseBuff, (eProperty)eResist.Spirit, double.Parse(splitText[1]), 1, false);
+                        Console.WriteLine($"Spirit resistance increased by {double.Parse(splitText[1])}");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"inside else bracket {splitText[0]}");
+                switch (splitText[0].ToLower())
+                {
+                    case "resistances":
+                        SendReply(player, "Whisper me the resist type and value you'd like. Example: '/whisper Body 10' will give me +10% Body resist");
+                        break;
+                    default:
+                        ResetArmorAndResists();
+                        break;
+                }
+            }
+            
+
+            return true;
+        }
+
+        private void ResetResists()
+        {
+            if (GetResist(eDamageType.Slash) > 0)
+            {
+                ApplyBonus(this, eBuffBonusCategory.BaseBuff, (eProperty)eResist.Slash, GetResist(eDamageType.Slash), 1, true);
+            }
+            if (GetResist(eDamageType.Crush) > 0)
+            {
+                ApplyBonus(this, eBuffBonusCategory.BaseBuff, (eProperty)eResist.Crush, GetResist(eDamageType.Crush), 1, true);
+            }
+            if (GetResist(eDamageType.Thrust) > 0)
+            {
+                ApplyBonus(this, eBuffBonusCategory.BaseBuff, (eProperty)eResist.Thrust, GetResist(eDamageType.Thrust), 1, true);
+            }
+            if (GetResist(eDamageType.Natural) > 0)
+            {
+                ApplyBonus(this, eBuffBonusCategory.BaseBuff, (eProperty)eResist.Natural, GetResist(eDamageType.Natural), 1, true);
+            }
+            if (GetResist(eDamageType.Body) > 0)
+            {
+                ApplyBonus(this, eBuffBonusCategory.BaseBuff, (eProperty)eResist.Body, GetResist(eDamageType.Body), 1, true);
+            }
+            if (GetResist(eDamageType.Cold) > 0)
+            {
+                ApplyBonus(this, eBuffBonusCategory.BaseBuff, (eProperty)eResist.Cold, GetResist(eDamageType.Cold), 1, true);
+            }
+            if (GetResist(eDamageType.Energy) > 0)
+            {
+                ApplyBonus(this, eBuffBonusCategory.BaseBuff, (eProperty)eResist.Energy, GetResist(eDamageType.Energy), 1, true);
+            }
+            if (GetResist(eDamageType.Heat) > 0)
+            {
+                ApplyBonus(this, eBuffBonusCategory.BaseBuff, (eProperty)eResist.Heat, GetResist(eDamageType.Heat), 1, true);
+            }
+            if (GetResist(eDamageType.Matter) > 0)
+            {
+                ApplyBonus(this, eBuffBonusCategory.BaseBuff, (eProperty)eResist.Matter, GetResist(eDamageType.Matter), 1, true);
+            }
+            if (GetResist(eDamageType.Spirit) > 0)
+            {
+                ApplyBonus(this, eBuffBonusCategory.BaseBuff, (eProperty)eResist.Spirit, GetResist(eDamageType.Spirit), 1, true);
+            }
+        }
+
+        private void ResetArmor()
+        {
+            //we will figure out armor next 
+        }
+
+        private void ResetArmorAndResists()
+        {
+            ResetResists();
+            ResetArmor();
         }
 
         public override void OnAttackedByEnemy(AttackData ad)
@@ -41,6 +173,11 @@ namespace DOL.GS
             GuildName = "DPS Dummy";
             Model = 34;
             return base.AddToWorld(); // Finish up and add him to the world.
+        }
+
+        public void SendReply(GamePlayer player, string msg)
+        {
+            player.Out.SendMessage(msg, eChatType.CT_Merchant, eChatLoc.CL_PopupWindow);
         }
 
     }
