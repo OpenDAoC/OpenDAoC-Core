b7f768157131d5ea132b569427391bcd1a2334f3
clait <stefanomus@fastmail.com>
2021-11-20 15:26:15 +0000

    Additional check for enemies in range when porting - TODO for solo players.

--- GameServerScripts/AtlasEvents/AtlasEventTeleporter.cs
+++ GameServerScripts/AtlasEvents/AtlasEventTeleporter.cs
@@ -35,7 +35,7 @@ namespace DOL.GS.Scripts
 			int inArea = 0;
 			foreach (GamePlayer NearbyPlayers in t.GetPlayersInRadius(radius))
 			{
-				if (GameServer.ServerRules.IsAllowedToAttack(this, NearbyPlayers, true));
+				if (GameServer.ServerRules.IsAllowedToAttack(t, NearbyPlayers, true));
                 {
                     inArea++;
                 }
@@ -91,10 +91,11 @@ namespace DOL.GS.Scripts
 						log.Info("Player in group");
 						foreach (GamePlayer groupMember in t.Group.GetPlayersInTheGroup())
 						{
+							int enemiesInRange = GetEnemyCountInArea(groupMember, 2750);
 							// checking if any group member is already in the zone and safe
-							if (GetDistanceTo(groupMember) > 5000 && !groupMember.InCombat)
+							if (GetDistanceTo(groupMember) > 5000 && enemiesInRange == 0)
 							{
-								log.Info("Enemies in range: " + GetEnemyCountInArea(groupMember, 2750));
+								log.Info("Enemies in range: " + enemiesInRange);
 								log.Info("Distance > 5k");
 								t.StartInvulnerabilityTimer(ServerProperties.Properties.TIMER_PVP_TELEPORT*1000,null);
 								t.MoveTo(groupMember.CurrentRegionID, groupMember.X, groupMember.Y, groupMember.Z, groupMember.Heading);
