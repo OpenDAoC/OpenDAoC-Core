634eeaf596ed2059c055604a73e1f09b330faa5d
Anthony Knight <midgetlord123@hotmail.com>
2021-11-23 23:29:45 -0800

    ListCasters now gain benefits of MotArcane for self/pet buffs

--- GameServer/spells/SingleStatBuff.cs
+++ GameServer/spells/SingleStatBuff.cs
@@ -51,14 +51,17 @@ namespace DOL.GS.Spells
         {
             int specLevel = Caster.GetModifiedSpecLevel(m_spellLine.Spec);
 			
-			if (Caster is GamePlayer && ((GamePlayer)Caster).CharacterClass.ClassType != eClassType.ListCaster && Spell.Level > 0 && ((GamePlayer)Caster).CharacterClass.ID != (int)eCharacterClass.Savage)
+			if (Caster is GamePlayer && Spell.Level > 0 && ((GamePlayer)Caster).CharacterClass.ID != (int)eCharacterClass.Savage)
             {
-                effectiveness = 0.75; // This section is for self bulfs, cleric buffs etc.
                 if (Spell.Level > 0)
                 {
-                    effectiveness += (specLevel - 1.0) * 0.5 / Spell.Level;
-                    effectiveness = Math.Max(0.75, effectiveness);
-                    effectiveness = Math.Min(1.25, effectiveness);
+                    if (((GamePlayer)Caster).CharacterClass.ClassType != eClassType.ListCaster)
+                    {
+                        effectiveness = 0.75; // This section is for self bulfs, cleric buffs etc.
+                        effectiveness += (specLevel - 1.0) * 0.5 / Spell.Level;
+                        effectiveness = Math.Max(0.75, effectiveness);
+                        effectiveness = Math.Min(1.25, effectiveness);
+                    }
                     effectiveness *= (1.0 + m_caster.GetModified(eProperty.BuffEffectiveness) * 0.01);
                 }
             }
