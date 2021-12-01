abb3392e8930e87b97b2e2ccfde949913d5a7463
Kelteen <dean.dudlitz@live.de>
2021-12-01 14:55:31 +0100

    Revert "[Line 866-926] Guard Database Loading Fix (no Crash at Restarts) + you can use /keepguard command now"
    This reverts commit ce96c8b8102aad2c0f6165cbec8ca5d79681a4f4.

--- GameServer/keeps/Gameobjects/Guards/GameKeepGuard.cs
+++ GameServer/keeps/Gameobjects/Guards/GameKeepGuard.cs
@@ -864,7 +864,6 @@ namespace DOL.GS.Keeps
 			return s;
 		}
 
-		/*
 		#region Database
 
 		string m_dataObjectID = "";
@@ -886,7 +885,7 @@ namespace DOL.GS.Keeps
 					Component.Keep = keepArea.Keep;
 					m_dataObjectID = mobobject.ObjectId;
 					// mob reload command might be reloading guard, so check to make sure it isn't already added
-					if (Component.Keep.Guards.ContainsKey(sKey) == false)
+					if (Component.Keep?.Guards?.ContainsKey(sKey) == false)
 						Component.Keep.Guards.Add(sKey, this);
 					// break; This is a bad idea.  If there are multiple KeepAreas, we should put a guard on each
 				}
@@ -894,35 +893,6 @@ namespace DOL.GS.Keeps
 
 			RefreshTemplate();
 		}
-		*/
-		string m_dataObjectID = "";
-
-        #region Database
-        /// <summary>
-        /// Load the guard from the database
-        /// </summary>
-        /// <param name="mobobject">The database mobobject</param>
-        public override void LoadFromDatabase(DataObject mobobject)
-		{
-			base.LoadFromDatabase(mobobject);
-			foreach (AbstractArea area in this.CurrentAreas)
-			{
-				if (area is KeepArea)
-				{
-					AbstractGameKeep keep = (area as KeepArea).Keep;
-					Component = new GameKeepComponent();
-					Component.Keep = keep;
-					m_dataObjectID = mobobject.ObjectId;
-					// mob reload command might be reloading guard, so check to make sure it isn't already added
-					if (Component.Keep.Guards.ContainsKey(m_dataObjectID) == false)
-					{
-						Component.Keep.Guards.Add(m_dataObjectID, this);
-					}
-					break;
-				}
-			}
-				RefreshTemplate();			
-		}		
 
 		public void DeleteObject()
 		{
