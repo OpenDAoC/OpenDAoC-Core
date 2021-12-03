e142ac93c3d7505d70d7390209abe32cdcd8082e
kbvandergriffjr <kbvandergriffjr@gmail.com>
2021-12-03 16:59:32 -0500

    GTAoE spells no longer require LoS to target.

--- GameServer/spells/SpellHandler.cs
+++ GameServer/spells/SpellHandler.cs
@@ -844,11 +844,11 @@ namespace DOL.GS.Spells
 					if (!quiet) MessageToCaster("Your area target is out of range.  Select a closer target.", eChatType.CT_SpellResisted);
 					return false;
 				}
-                if (!Caster.GroundTargetInView)
-                {
-                    MessageToCaster("Your ground target is not in view!", eChatType.CT_SpellResisted);
-                    return false;
-                }
+                //if (!Caster.GroundTargetInView)
+                //{
+                //    MessageToCaster("Your ground target is not in view!", eChatType.CT_SpellResisted);
+                //    return false;
+                //}
             }
 			else if (targetType != "self" && targetType != "group" && targetType != "pet"
 			         && targetType != "controlled" && targetType != "cone" && m_spell.Range > 0)
@@ -1498,11 +1498,11 @@ namespace DOL.GS.Spells
 					if (!quiet) MessageToCaster("Your area target is out of range.  Select a closer target.", eChatType.CT_SpellResisted);
 					return false;
 				}
-                if (!Caster.GroundTargetInView)
-                {
-                    MessageToCaster("Your ground target is not in view!", eChatType.CT_SpellResisted);
-                    return false;
-                }
+                //if (!Caster.GroundTargetInView)
+                //{
+                //    MessageToCaster("Your ground target is not in view!", eChatType.CT_SpellResisted);
+                //    return false;
+                //}
             }
 			else if (m_spell.Target.ToLower() != "self" && m_spell.Target.ToLower() != "group" && m_spell.Target.ToLower() != "cone" && m_spell.Range > 0)
 			{
