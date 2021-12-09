6de2d903dca4f199317b919512ccff12f5afd4c6
kbvandergriffjr <kbvandergriffjr@gmail.com>
2021-12-09 17:31:16 -0500

    Bomber fix attempt.

--- GameServer/spells/Animist/BomberSpellHandler.cs
+++ GameServer/spells/Animist/BomberSpellHandler.cs
@@ -68,7 +68,7 @@ namespace DOL.GS.Spells
             }
 
             base.ApplyEffectOnTarget(target, effectiveness);
-            m_pet.Level = m_pet.Owner.Level; // No bomber class to override SetPetLevel() in, so set level here
+            m_pet.Level = m_pet.Owner is null ? (byte)1 : m_pet.Owner.Level; // No bomber class to override SetPetLevel() in, so set level here
             m_pet.TempProperties.setProperty(BOMBERTARGET, target);
             m_pet.Name = Spell.Name;
 			m_pet.Flags ^= GameNPC.eFlags.DONTSHOWNAME;
