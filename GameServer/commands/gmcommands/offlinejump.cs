b19b839bda34cf0adfea6c5f84f3578ea47942c5
clait <stefanomus@fastmail.com>
2021-12-18 15:41:45 +0000

    /offlinejump <player> to capital

--- GameServer/commands/gmcommands/offlinejump.cs
+++ GameServer/commands/gmcommands/offlinejump.cs
@@ -37,7 +37,52 @@ namespace DOL.GS.Commands
 					BindCharacter(character);
 					GameServer.Database.SaveObject(character);
 					client.Out.SendMessage("Character " + args[1].ToUpperInvariant() + " has been moved to Jail", eChatType.CT_Staff, eChatLoc.CL_SystemWindow);
-				}
+				} 
+				else if (args.Length == 4 && args[2] == "to" && args[3] == "capital")
+				{
+					DOLCharacters character;
+					character = DOLDB<DOLCharacters>.SelectObject(DB.Column("Name").IsEqualTo(args[1]));
+					if (character == null)
+					{
+						client.Out.SendMessage("Character " + args[1] + " not found", eChatType.CT_Staff, eChatLoc.CL_SystemWindow);
+						return;
+					}
+					
+					switch (character.Realm)
+					{
+						case 1:
+							character.Xpos = 36209;
+							character.Ypos = 29843;
+							character.Zpos = 7971;
+							character.Region = 10;
+							character.Direction = 18;
+							break;
+						case 2:
+							character.Xpos = 31619;
+							character.Ypos = 28768;
+							character.Zpos = 8800;
+							character.Region = 101;
+							character.Direction = 2201;
+							break;
+						case 3:
+							character.Xpos = 30011;
+							character.Ypos = 33138;
+							character.Zpos = 7916;
+							character.Region = 201;
+							character.Direction = 3079;
+							break;
+						default:
+							character.Xpos = 47411;
+							character.Ypos= 48694;
+							character.Zpos = 25000;
+							character.Region = 249;
+							character.Direction = 5;
+							break;
+					}
+					BindCharacter(character);
+					GameServer.Database.SaveObject(character);
+					client.Out.SendMessage("Character " + args[1].ToUpperInvariant() + " has been moved to their Realm's Capital City", eChatType.CT_Staff, eChatLoc.CL_SystemWindow);
+				} 
 				else
 				{
 					DisplaySyntax(client);
