5c4b4e273436e831330d07be4efb09efa2af37e9
clait <stefanomus@fastmail.com>
2021-12-18 15:14:56 +0000

    /sameip verbosity

--- GameServer/scripts/commands/SameIP.cs
+++ GameServer/scripts/commands/SameIP.cs
@@ -1,9 +1,6 @@
-using System;
-using DOL.GS;
-using DOL.Database;
-using DOL.GS.PacketHandler;
 using System.Net;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace DOL.GS.Commands
 {
@@ -16,7 +13,8 @@ namespace DOL.GS.Commands
 	{
 		public void OnCommand(GameClient client, string[] args)
 		{
-			int i = 0;
+			int i = 1;
+			IList<string> output = new List<string>();
 			Hashtable ip = new Hashtable();
 			string accip;
 			foreach (GameClient cl in WorldMgr.GetAllClients())
@@ -33,14 +31,21 @@ namespace DOL.GS.Commands
 					string ip1 = ((IPEndPoint)cl.Socket.RemoteEndPoint).Address.ToString();
 					string name2 = cls.Player != null ? cls.Player.Name : "Entering game...";
 					string ip2 = ((IPEndPoint)cls.Socket.RemoteEndPoint).Address.ToString();
-
-					DisplayMessage(client, "IP: {0} AccountName: {1} (Player: {2}) ", ip1, cl.Account.Name, name1);
-					DisplayMessage(client, "IP: {0} AccountName: {1} (Player: {2}) ", ip2, cls.Account.Name, name2);
+					
+					output.Add("Same IP violation #" + i);
+					output.Add("IP: " + ip1);
+					output.Add("Account 1: " + cl.Account.Name);
+					output.Add("Player 1: " + name1 + " (L" + cl.Player.Level + " " + cl.Player?.CharacterClass.Name + " in " + cl.Player.CurrentZone.Description + ")");
+					output.Add("X: " + cl.Player.X + " Y: " + cl.Player.Y + " Z: " + cl.Player.Z + " Region: " + cl.Player.CurrentRegionID);
+					output.Add("Account 2: " + cls.Account.Name);
+					output.Add("Player 2: " + name2 + " (L" + cls.Player.Level + " " + cls.Player?.CharacterClass.Name + " in " + cls.Player.CurrentZone.Description + ")");
+					output.Add("X: " + cls.Player.X + " Y: " + cls.Player.Y + " Z: " + cls.Player.Z + " Region: " + cls.Player.CurrentRegionID);
+					output.Add("\n");
 					i++;
 				}
 			}
-            //DisplayMessage.Out.SendMessage(client, "{0} double IP found.", i, eChatType.CT_Say, eChatLoc.CL_PopupWindow);
-			DisplayMessage(client, "{0} double IP found.", i);			
+			DisplayMessage(client, "{0} double IP found.", i-1);	
+			client.Out.SendCustomTextWindow(i-1 + " double IP found", output);
 		}
 	}
 }
