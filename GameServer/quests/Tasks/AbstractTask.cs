df822295dbcb90c3315270732ca620da981adb06
clait <stefanomus@fastmail.com>
2021-12-21 15:57:51 +0000

    Tasks now run off the gameloop

--- GameServer/quests/Tasks/AbstractTask.cs
+++ GameServer/quests/Tasks/AbstractTask.cs
@@ -531,7 +531,7 @@ namespace DOL.GS.Quests
 				player.Out.SendMessage("You cannot do more than "+MaxTasksDone(player.Level).ToString()+" tasks at your level!",eChatType.CT_System,eChatLoc.CL_SystemWindow);
 				return false;
 			}
-			else if (player.TempProperties.getProperty<int>(CHECK_TASK_TICK) > Environment.TickCount)
+			else if (player.TempProperties.getProperty<int>(CHECK_TASK_TICK) > GameLoop.GameLoopTime)
 			{
 				player.Out.SendMessage("I have no tasks for you at the moment. Come back sometime later, perhaps then you can help me with something.",eChatType.CT_Say,eChatLoc.CL_PopupWindow);
 				return false;
@@ -544,7 +544,7 @@ namespace DOL.GS.Quests
 			{
 				player.Out.SendMessage("I have no tasks for you at the moment. Come back sometime later, perhaps then you can help me with something.",eChatType.CT_Say,eChatLoc.CL_PopupWindow);
 				// stored time of try to disable task for defined time.
-				player.TempProperties.setProperty(CHECK_TASK_TICK, Environment.TickCount + CHECK_TASK_DELAY);
+				player.TempProperties.setProperty(CHECK_TASK_TICK, GameLoop.GameLoopTime + CHECK_TASK_DELAY);
 				return false;
 			}
 		}
