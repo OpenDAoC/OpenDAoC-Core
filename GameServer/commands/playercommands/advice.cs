7c0f9087e178d7041a06694f7b462e067be98f1b
clait <stefanomus@fastmail.com>
2022-01-08 18:19:54 +0000

    this should fix /adv

--- GameServer/commands/playercommands/advice.cs
+++ GameServer/commands/playercommands/advice.cs
@@ -71,10 +71,9 @@ namespace DOL.GS.Commands
 			}
 			foreach (GameClient playerClient in WorldMgr.GetAllClients())
 			{
-				if (playerClient.Player.IsIgnoring(client.Player)) continue;
 				if (playerClient.Player == null) continue;
-				if ((playerClient.Player.Realm == client.Player.Realm) ||
-					playerClient.Account.PrivLevel > 1)
+				if ((playerClient.Player.Realm == client.Player.Realm ||
+					playerClient.Account.PrivLevel > 1) && !playerClient.Player.IsIgnoring(client.Player))
 					playerClient.Out.SendMessage(LanguageMgr.GetTranslation(client.Account.Language, "Scripts.Players.Advice.Advice", getRealmString(client.Player.Realm), client.Player.Name, msg), eChatType.CT_Advise, eChatLoc.CL_ChatWindow);
 
 			}
