b3d25b6d9daa39604c5895e2e653eef59d21df7a
clait <stefanomus@fastmail.com>
2022-01-20 12:02:58 +0000

    `/spammob create [amount] [radius]` + `/spammob clear [radius]`

--- GameServer/commands/gmcommands/SpamMob.cs
+++ GameServer/commands/gmcommands/SpamMob.cs
@@ -14,17 +14,140 @@ using DOL.GS.PacketHandler;
 namespace DOL.GS.Commands
 {
     [Cmd("&spammob", //command to handle
-         ePrivLevel.GM, //minimum privelege level
-         "Mob creation and modification commands.", //command description
-         "/spammob create [amount]"
-        )]
+        ePrivLevel.GM, //minimum privelege level
+        "Mob creation and modification commands.", //command description
+        "/spammob create [amount] - creates [amount] mobs at the player's location",
+        "/spammob create [amount] [radius] - creates [amount] mobs in a [radius] around the player",
+        "/spammob clear [radius] - deletes all spam mobs in a [radius] around the player"
+    )]
     public class SpamMobCommandHandler : AbstractCommandHandler, ICommandHandler
     {
         public void OnCommand(GameClient client, string[] args)
         {
-            if (args.Length == 2)
+            if (args.Length <= 2)
             {
-                //create one mob
+                DisplaySyntax(client);
+                return;
+            }
+
+            // if (args.Length == 2)
+            // {
+            //     //create one mob
+            //     string theType = "DOL.GS.SpamMob.SpamMobNPC";
+            //     byte realm = 0;
+            //
+            //     //Create a new mob
+            //     GameNPC mob = null;
+            //
+            //     foreach (Assembly script in ScriptMgr.GameServerScripts)
+            //     {
+            //         try
+            //         {
+            //             client.Out.SendDebugMessage(script.FullName);
+            //             mob = (GameNPC)script.CreateInstance(theType, false);
+            //
+            //             if (mob != null)
+            //                 break;
+            //         }
+            //         catch (Exception e)
+            //         {
+            //             client.Out.SendMessage(e.ToString(), eChatType.CT_System, eChatLoc.CL_PopupWindow);
+            //         }
+            //     }
+            //
+            //     if (mob == null)
+            //     {
+            //         client.Out.SendMessage("There was an error creating an instance of " + theType + "!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+            //         return;
+            //     }
+            //
+            //     //Fill the object variables
+            //     mob.X = client.Player.X;
+            //     mob.Y = client.Player.Y;
+            //     mob.Z = client.Player.Z;
+            //     mob.CurrentRegion = client.Player.CurrentRegion;
+            //     mob.Heading = client.Player.Heading;
+            //     mob.Level = 50;
+            //     mob.Realm = (eRealm)realm;
+            //     mob.Name = "Spam Mob";
+            //     mob.Model = 34;
+            //
+            //     //Fill the living variables
+            //     mob.CurrentSpeed = 0;
+            //     mob.MaxSpeedBase = 200;
+            //     mob.GuildName = "Burn Baby Burn";
+            //     mob.Size = 50;
+            //     mob.Flags |= GameNPC.eFlags.PEACE;
+            //     mob.Flags ^= GameNPC.eFlags.PEACE;
+            //     mob.AddToWorld();
+            //     //mob.LoadedFromScript = false; // allow saving
+            //     //mob.SaveIntoDatabase();
+            //     //client.Out.SendMessage("Mob created: OID=" + mob.ObjectID, eChatType.CT_System, eChatLoc.CL_SystemWindow);
+            //     //client.Out.SendMessage("The mob has been created with the peace flag, so it can't be attacked, to remove type /mob peace", eChatType.CT_Important, eChatLoc.CL_SystemWindow);
+            // }
+
+            if (args.Length == 3)
+            {
+                if (args[1].Equals("clear"))
+                {
+                    ushort radius;
+                    if (ushort.TryParse(args[2], out radius))
+                    {
+                        if (radius < 0) radius = 0;
+                        if (radius > 5000) radius = 5000;
+
+                        foreach (GameNPC npc in client.Player.GetNPCsInRadius(radius))
+                            if (npc.Realm == eRealm.None && (npc is SpamMob.SpamMobNPC))
+                                remove(npc);
+                    }
+                    else
+                        DisplayMessage(client.Player, "Radius not valid");
+                }
+                
+                if (args[1].Equals("create"))
+                {
+                    int temp = 0;
+                    int.TryParse(args[2], out temp);
+                    if (temp == 0)
+                    {
+                        DisplayMessage(client.Player, "Amount not valid, using default value of 1");
+                        temp = 1;
+                    }
+                    SpawnSpamMob(client, temp);
+                }
+                //create multiple mobs
+            }
+            
+            if (args.Length == 4)
+            {
+                if (args[1].Equals("create"))
+                {
+                    int temp = 0;
+                    int.TryParse(args[2], out temp);
+                    if (temp == 0)
+                    {
+                        temp = 1;
+                    }
+                    
+                    int radius;
+                    if (int.TryParse(args[3], out radius))
+                    {
+                        SpawnSpamMob(client, temp, radius);
+                    }
+                    else
+                    {
+                        DisplayMessage(client.Player, "Radius not valid");
+                    }
+                    
+                }
+                //create multiple mobs
+            }
+        }
+
+        public void SpawnSpamMob(GameClient client, int number, int radius = 0)
+        {
+            for (int i = 0; i < number; i++)
+            {
                 string theType = "DOL.GS.SpamMob.SpamMobNPC";
                 byte realm = 0;
 
@@ -36,7 +159,7 @@ namespace DOL.GS.Commands
                     try
                     {
                         client.Out.SendDebugMessage(script.FullName);
-                        mob = (GameNPC)script.CreateInstance(theType, false);
+                        mob = (GameNPC) script.CreateInstance(theType, false);
 
                         if (mob != null)
                             break;
@@ -49,18 +172,19 @@ namespace DOL.GS.Commands
 
                 if (mob == null)
                 {
-                    client.Out.SendMessage("There was an error creating an instance of " + theType + "!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+                    client.Out.SendMessage("There was an error creating an instance of " + theType + "!",
+                        eChatType.CT_System, eChatLoc.CL_SystemWindow);
                     return;
                 }
 
                 //Fill the object variables
-                mob.X = client.Player.X;
-                mob.Y = client.Player.Y;
+                mob.X = client.Player.X + Util.Random(radius);
+                mob.Y = client.Player.Y + Util.Random(radius);
                 mob.Z = client.Player.Z;
                 mob.CurrentRegion = client.Player.CurrentRegion;
                 mob.Heading = client.Player.Heading;
                 mob.Level = 50;
-                mob.Realm = (eRealm)realm;
+                mob.Realm = (eRealm) realm;
                 mob.Name = "Spam Mob";
                 mob.Model = 34;
 
@@ -77,75 +201,18 @@ namespace DOL.GS.Commands
                 //client.Out.SendMessage("Mob created: OID=" + mob.ObjectID, eChatType.CT_System, eChatLoc.CL_SystemWindow);
                 //client.Out.SendMessage("The mob has been created with the peace flag, so it can't be attacked, to remove type /mob peace", eChatType.CT_Important, eChatLoc.CL_SystemWindow);
             }
-            else if (args.Length == 3)
-            {
-                if (args[1].Equals("create"))
-                {
-                    int temp = 0;
-                    int.TryParse(args[2],out temp);
-                    for (int i = 0; i < temp; i++)
-                    {
-                        string theType = "DOL.GS.SpamMob.SpamMobNPC";
-                        byte realm = 0;
-
-                        //Create a new mob
-                        GameNPC mob = null;
-
-                        foreach (Assembly script in ScriptMgr.GameServerScripts)
-                        {
-                            try
-                            {
-                                client.Out.SendDebugMessage(script.FullName);
-                                mob = (GameNPC)script.CreateInstance(theType, false);
-
-                                if (mob != null)
-                                    break;
-                            }
-                            catch (Exception e)
-                            {
-                                client.Out.SendMessage(e.ToString(), eChatType.CT_System, eChatLoc.CL_PopupWindow);
-                            }
-                        }
-
-                        if (mob == null)
-                        {
-                            client.Out.SendMessage("There was an error creating an instance of " + theType + "!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                            return;
-                        }
-
-                        //Fill the object variables
-                        mob.X = client.Player.X;
-                        mob.Y = client.Player.Y;
-                        mob.Z = client.Player.Z;
-                        mob.CurrentRegion = client.Player.CurrentRegion;
-                        mob.Heading = client.Player.Heading;
-                        mob.Level = 50;
-                        mob.Realm = (eRealm)realm;
-                        mob.Name = "Spam Mob";
-                        mob.Model = 34;
-
-                        //Fill the living variables
-                        mob.CurrentSpeed = 0;
-                        mob.MaxSpeedBase = 200;
-                        mob.GuildName = "Burn Baby Burn";
-                        mob.Size = 50;
-                        mob.Flags |= GameNPC.eFlags.PEACE;
-                        mob.Flags ^= GameNPC.eFlags.PEACE;
-                        mob.AddToWorld();
-                        //mob.LoadedFromScript = false; // allow saving
-                        //mob.SaveIntoDatabase();
-                        //client.Out.SendMessage("Mob created: OID=" + mob.ObjectID, eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                        //client.Out.SendMessage("The mob has been created with the peace flag, so it can't be attacked, to remove type /mob peace", eChatType.CT_Important, eChatLoc.CL_SystemWindow);
-                    }
-                    //create multiple mobs
-                }
-            }
-
-
+        }
+        
+        private void remove(GameNPC targetMob)
+        {
+            targetMob.StopAttack();
+            targetMob.StopCurrentSpellcast();
+            targetMob.DeleteFromDatabase();
+            targetMob.Delete();
         }
     }
-    
 }
+
 namespace DOL.GS.SpamMob
 {
     public class SpamMobBrain : StandardMobBrain
@@ -158,6 +225,7 @@ namespace DOL.GS.SpamMob
         {
             base.Think();
         }
+
         public override bool CheckSpells(eCheckSpellType type)
         {
             if (Body.IsCasting)
@@ -175,7 +243,8 @@ namespace DOL.GS.SpamMob
                     foreach (Spell spell in Body.Spells)
                     {
                         if (Body.GetSkillDisabledDuration(spell) > 0) continue;
-                        if (spell.Target.ToLower() == "enemy" || spell.Target.ToLower() == "area" || spell.Target.ToLower() == "cone") continue;
+                        if (spell.Target.ToLower() == "enemy" || spell.Target.ToLower() == "area" ||
+                            spell.Target.ToLower() == "cone") continue;
 
                         if (spell.Uninterruptible && CheckDefensiveSpells(spell))
                             casted = true;
@@ -187,31 +256,32 @@ namespace DOL.GS.SpamMob
                 {
                     foreach (Spell spell in Body.Spells)
                     {
-
                         if (Body.GetSkillDisabledDuration(spell) == 0)
                         {
                             if (spell.CastTime > 0)
                             {
-                                if (spell.Target.ToLower() == "enemy" || spell.Target.ToLower() == "area" || spell.Target.ToLower() == "cone")
+                                if (spell.Target.ToLower() == "enemy" || spell.Target.ToLower() == "area" ||
+                                    spell.Target.ToLower() == "cone")
                                     spell_rec.Add(spell);
                             }
                         }
                     }
+
                     if (spell_rec.Count > 0)
                     {
-                        spellToCast = (Spell)spell_rec[Util.Random((spell_rec.Count - 1))];
+                        spellToCast = (Spell) spell_rec[Util.Random((spell_rec.Count - 1))];
 
 
                         if (spellToCast.Uninterruptible && CheckOffensiveSpells(spellToCast))
                             casted = true;
-                        else
-                            if (!Body.IsBeingInterrupted && CheckOffensiveSpells(spellToCast))
+                        else if (!Body.IsBeingInterrupted && CheckOffensiveSpells(spellToCast))
                             casted = true;
                     }
                 }
 
                 return casted;
             }
+
             return casted;
         }
     }
@@ -236,7 +306,8 @@ namespace DOL.GS.SpamMob
                 spell.Duration = 5;
                 spell.Value = 20; //Effective buff 58
                 spell.Name = "Armor of the Realm";
-                spell.Description = "Adds to the recipient's Armor Factor (AF) resulting in better protection againts some forms of attack. It acts in addition to any armor the target is wearing.";
+                spell.Description =
+                    "Adds to the recipient's Armor Factor (AF) resulting in better protection againts some forms of attack. It acts in addition to any armor the target is wearing.";
                 spell.Range = WorldMgr.VISIBILITY_DISTANCE;
                 spell.SpellID = 88001;
                 spell.Target = "Self";
@@ -246,6 +317,7 @@ namespace DOL.GS.SpamMob
 
                 af = new Spell(spell, 0);
             }
+
             if (str == null)
             {
                 DBSpell spell = new DBSpell();
@@ -267,6 +339,7 @@ namespace DOL.GS.SpamMob
 
                 str = new Spell(spell, 0);
             }
+
             if (con == null)
             {
                 DBSpell spell = new DBSpell();
@@ -288,6 +361,7 @@ namespace DOL.GS.SpamMob
 
                 con = new Spell(spell, 0);
             }
+
             if (dex == null)
             {
                 DBSpell spell = new DBSpell();
@@ -320,14 +394,15 @@ namespace DOL.GS.SpamMob
                 dex
             };
         }
+
         public override bool CastSpell(Spell spell, SpellLine line)
         {
             return castingComponent.StartCastSpell(spell, line);
         }
+
         public override bool CastSpell(Spell spell, SpellLine line, bool checkLOS)
         {
-            return castingComponent.StartCastSpell(spell, line); 
+            return castingComponent.StartCastSpell(spell, line);
         }
-
     }
 }
