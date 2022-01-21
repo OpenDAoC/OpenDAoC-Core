8be8c68235668bd2f9a235f775f40514bd202c1b
Anthony Knight <midgetlord123@hotmail.com>
2022-01-21 08:47:22 -0800

    Lower tether range for necromancer pet

--- GameServer/ai/brain/Necromancer/NecromancerPetBrain.cs
+++ GameServer/ai/brain/Necromancer/NecromancerPetBrain.cs
@@ -609,8 +609,8 @@ namespace DOL.AI.Brain
 
         #region Tether
 
-        private const int m_softTether = 2000;    // TODO: Check on Pendragon
-        private const int m_hardTether = 2500;
+        private const int m_softTether = 750;    // TODO: Check on Pendragon
+        private const int m_hardTether = 1500;
         private TetherTimer m_tetherTimer = null;
 
         private void CheckTether()
