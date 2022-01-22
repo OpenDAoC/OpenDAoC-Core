c4b4dd739fd809a1fa4a1152bb677a3e86d25d94
Anthony Knight <midgetlord123@hotmail.com>
2022-01-22 12:47:25 -0800

    Use deck to determine spell resists

--- GameServer/spells/SpellHandler.cs
+++ GameServer/spells/SpellHandler.cs
@@ -2887,7 +2887,15 @@ namespace DOL.GS.Spells
 					((Caster as GameNPC).Brain as IOldAggressiveBrain).AddToAggroList(t, 1);
 
 				int spellResistChance = CalculateSpellResistChance(t);
-				int randNum = Util.CryptoNextInt(100);
+				int randNum = 0;
+				if (Caster is GamePlayer caster)
+				{
+					randNum = caster.RandomNumberDeck.GetInt();
+				}
+				else
+				{
+					randNum = Util.CryptoNextInt(100);
+				}
 
 				if (this.Caster is GamePlayer spellCaster && spellCaster.UseDetailedCombatLog && spellResistChance > 0)
 				{
