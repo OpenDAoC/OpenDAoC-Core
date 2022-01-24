c34231ec602df5a18bb4edf272ed3946cacc0f81
Anthony Knight <midgetlord123@hotmail.com>
2022-01-24 09:16:53 -0800

    More research shows Savage == DT 20, not 19

--- GameServer/playerclasses/midgard/ClassSavage.cs
+++ GameServer/playerclasses/midgard/ClassSavage.cs
@@ -33,7 +33,7 @@ namespace DOL.GS.PlayerClass
 			m_primaryStat = eStat.DEX;
 			m_secondaryStat = eStat.QUI;
 			m_tertiaryStat = eStat.STR;
-			m_wsbase = 380;
+			m_wsbase = 400;
 		}
 
 		public override bool CanUseLefthandedWeapon
