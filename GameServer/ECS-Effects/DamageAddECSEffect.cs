d43dbf7c6bb0ee9e9c92acf29785c40c353de1aa
kbvandergriffjr <kbvandergriffjr@gmail.com>
2022-01-29 16:36:02 -0500

    Added DamageAddECSEffect and DamageShieldECSEffect.

---
+++ GameServer/ECS-Effects/DamageAddECSEffect.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOL.GS
+{
+    public class DamageAddECSEffect : ECSGameSpellEffect
+    {
+        public DamageAddECSEffect(ECSGameEffectInitParams initParams)
+            : base(initParams) { }
+
+        public override void OnStartEffect()
+        {
+        }
+
+        public override void OnStopEffect()
+        {
+        }
+    }
+}
---
+++ GameServer/ECS-Effects/DamageShieldECSEffect.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOL.GS
+{
+    public class DamageShieldECSEffect : ECSGameSpellEffect
+    {
+        public DamageShieldECSEffect(ECSGameEffectInitParams initParams)
+            : base(initParams) { }
+
+        public override void OnStartEffect()
+        {
+        }
+
+        public override void OnStopEffect()
+        {
+        }
+    }
+}
