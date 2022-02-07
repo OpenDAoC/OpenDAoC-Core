f43212440a13e4b1dccad6b85e51b9b58a64bafb
leftygamer <dashes_chump0h@icloud.com>
2022-02-07 13:05:21 -0700

    Added missing messages, standardized message calls, fixed message phrasing

--- GameServer/gameobjects/CustomNPC/Blacksmith.cs
+++ GameServer/gameobjects/CustomNPC/Blacksmith.cs
@@ -41,47 +41,61 @@ namespace DOL.GS
         }
 
 
-        #region Examine/Interact Message
-
+        #region Examine Messages
         /// <summary>
-        /// Adds messages to ArrayList which are sent when object is targeted
+        /// Adds messages to array, which are all sent when the NPC is examined (i.e., clicked on) by a GamePlayer.
         /// </summary>
-        /// <param name="player">GamePlayer that is examining this object</param>
-        /// <returns>list with string messages</returns>
+        /// <param name="player">The GamePlayer examining the NPC.</param>
+        /// <returns>Returns the messages in the array and sends them all to the player.</returns>
         public override IList GetExamineMessages(GamePlayer player)
         {
             IList list = new ArrayList();
-            list.Add(LanguageMgr.GetTranslation(player.Client.Account.Language, "Scripts.Blacksmith.YouTarget",
+            // Message: You target [{0}].
+            list.Add(LanguageMgr.GetTranslation(player.Client.Account.Language, "GameNPC.Blacksmith.YouTarget",
                 GetName(0, false, player.Client.Account.Language, this)));
-            list.Add(LanguageMgr.GetTranslation(player.Client.Account.Language, "Scripts.Blacksmith.YouExamine",
+            // Message: You examine {0}. {1} is {2} and is a smith.
+            list.Add(LanguageMgr.GetTranslation(player.Client.Account.Language, "GameNPC.Blacksmith.YouExamine",
                 GetName(0, false, player.Client.Account.Language, this),
-                GetPronoun(0, true, player.Client.Account.Language), GetAggroLevelString(player, false)));
-            list.Add(LanguageMgr.GetTranslation(player.Client.Account.Language, "Scripts.Blacksmith.GiveObject",
-                GetPronoun(0, true, player.Client.Account.Language)));
+                GetPronoun(0, true, player.Client.Account.Language), 
+                GetAggroLevelString(player, false)));
+            // Message: [Give {0} an item to be repaired]
+            list.Add(LanguageMgr.GetTranslation(player.Client.Account.Language, "GameNPC.Blacksmith.GiveObject",
+                GetPronoun(0, false, player.Client.Account.Language)));
             return list;
         }
+        #endregion Examine Messages
 
+        #region Interact Message
         /// <summary>
-        /// This function is called from the ObjectInteractRequestHandler
+        /// Behaviors that occur when a GamePlayer interacts with the NPC (e.g., facing player, send interact message, etc.).
         /// </summary>
-        /// <param name="player">GamePlayer that interacts with this object</param>
-        /// <returns>false if interaction is prevented</returns>
+        /// <param name="player">The GamePlayer interacting with the NPC.</param>
+        /// <returns>If the interaction is prevented by the base class on GamePlayer.cs, then return 'false'.</returns>
         public override bool Interact(GamePlayer player)
         {
+            // If the interaction is prevented by the base class on GamePlayer.cs, then return 'false'
             if (!base.Interact(player))
                 return false;
 
-            TurnTo(player, 1000);
+            // NPC faces the player that just interacted with them for #### milliseconds
+            TurnTo(player, 5000);
 
-            SayTo(player, eChatLoc.CL_PopupWindow,
-                LanguageMgr.GetTranslation(player.Client.Account.Language, "Scripts.Blacksmith.Say"));
+            // Check for ambient trigger messages for the NPC in the 'MobXAmbientBehaviour' table
+            var triggers = GameServer.Instance.NpcManager.AmbientBehaviour[base.Name];
+            // If the NPC has no ambient trigger message assigned, then return this message
+            if (triggers == null || triggers.Length == 0)
+                // Message: {0} says, "I can repair weapons or armor for you. Just hand me the item you want repaired and I'll see what I can do, for a small fee."
+                ChatUtil.SendDialogMessage(player, "GameNPC.Blacksmith.Say", GetName(0, true));
             return true;
         }
+        #endregion Interact Messages
 
-        #endregion Examine/Interact Message
-
-        #region Receive item
-
+        #region Receive Responses
+        /// <summary>
+        /// Behaviors that occur when an NPC is given an item (primarily to repair, but also for XP item turn-in).
+        /// </summary>
+        /// <param name="source">The entity giving the item to the NPC (e.g., 'GamePlayer player').</param>
+        /// /// <param name="item">The specific item being given to the NPC.</param>
         public override bool ReceiveItem(GameLiving source, InventoryItem item)
         {
             GamePlayer player = source as GamePlayer;
@@ -98,9 +112,8 @@ namespace DOL.GS
 
             if (item.Count != 1)
             {
-                player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language,
-                        "Scripts.Blacksmith.StackedObjets", GetName(0, false)),
-                    eChatType.CT_System, eChatLoc.CL_SystemWindow);
+                // Message: {0} doesn't want stacked items.
+                ChatUtil.SendErrorMessage(player, "GameNPC.Blacksmith.StackedObjets", GetName(0, true));
 
                 return false;
             }
@@ -111,8 +124,8 @@ namespace DOL.GS
                 case (int) eObjectType.Magical:
                 case (int) eObjectType.Instrument:
                 case (int) eObjectType.Poison:
-                    SayTo(player, LanguageMgr.GetTranslation(player.Client.Account.Language,
-                        "Scripts.Blacksmith.CantRepairThat"));
+                    // Message: {0} says, "I can't repair that."
+                    ChatUtil.SendSayMessage(player, "GameNPC.Blacksmith.CantRepairThat", GetName(0, true));
 
                     return false;
             }
@@ -121,32 +134,37 @@ namespace DOL.GS
             {
                 if (item.Durability <= 0)
                 {
-                    player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language,
-                            "Scripts.Blacksmith.ObjectCantRepaired"), eChatType.CT_System,
-                        eChatLoc.CL_SystemWindow);
+                    // Message: {0} says, "I can't repair that."
+                    ChatUtil.SendSayMessage(player, "GameNPC.Blacksmith.CantRepairThat", GetName(0, true));
 
                     return false;
                 }
                 else
                 {
                     player.TempProperties.setProperty(REPAIR_ITEM_WEAK, new WeakRef(item));
-                    player.Client.Out.SendCustomDialog(LanguageMgr.GetTranslation(player.Client.Account.Language,
-                            "Scripts.Blacksmith.RepairCostAccept",
-                            Money.GetString(item.RepairCost), item.Name),
-                        new CustomDialogResponse(BlacksmithDialogResponse));
+                    // Pop-up dialog prompting repair with ACCEPT/DECLINE options
+                    // Message: It will cost {0} to repair {1}. Do you accept?
+                    player.Client.Out.SendCustomDialog(LanguageMgr.GetTranslation(player.Client.Account.Language, "GameNPC.Blacksmith.RepairCostAccept", 
+                        Money.GetString(item.RepairCost), item.GetName(0, false)), new CustomDialogResponse(BlacksmithDialogResponse));
                 }
             }
             else
             {
-                player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language,
-                        "Scripts.Blacksmith.NoNeedRepair"), eChatType.CT_System,
-                    eChatLoc.CL_SystemWindow);
+                // Message: Your {0} is already in perfect condition.
+                ChatUtil.SendErrorMessage(player, "GameNPC.Blacksmith.NoNeedRepair", item.Name);
             }
 
 
             return false;
         }
+        #endregion Receive Responses
 
+        #region Repair Responses
+        /// <summary>
+        /// Responses that occur when an NPC attempts to repair an item given to them.
+        /// </summary>
+        /// <param name="player">The entity that originally gave the item to the NPC.</param>
+        /// /// <param name="response">The player's response when prompted to initiate the repair (ACCEPT/DECLINE).</param>
         protected void BlacksmithDialogResponse(GamePlayer player, byte response)
         {
             WeakReference itemWeak =
@@ -159,9 +177,8 @@ namespace DOL.GS
 
             if (response != 0x01)
             {
-                player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language,
-                        "Scripts.Blacksmith.AbortRepair", item.Name), eChatType.CT_System,
-                    eChatLoc.CL_SystemWindow);
+                // Message: You decline to have your {0} repaired.
+                ChatUtil.SendSystemMessage(player, "GameNPC.Blacksmith.AbortRepair", item.Name);
 
                 return;
             }
@@ -170,9 +187,8 @@ namespace DOL.GS
             if (item == null || item.SlotPosition == (int) eInventorySlot.Ground
                              || item.OwnerID == null || item.OwnerID != player.InternalID)
             {
-                player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language,
-                        "Scripts.Blacksmith.InvalidItem"), eChatType.CT_System,
-                    eChatLoc.CL_SystemWindow);
+                // Message: {0} says, "I can't repair that."
+                ChatUtil.SendSayMessage(player, "GameNPC.Blacksmith.CantRepairThat", GetName(0, true));
 
                 return;
             }
@@ -182,25 +198,25 @@ namespace DOL.GS
             if (!player.RemoveMoney(item.RepairCost))
             {
                 InventoryLogging.LogInventoryAction(player, this, eInventoryActionType.Merchant, item.RepairCost);
-                player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language,
-                        "Scripts.Blacksmith.NotEnoughMoney"), eChatType.CT_System,
-                    eChatLoc.CL_SystemWindow);
+                // Message: {0} says, "It costs {1} to repair {2}. You don't have that much."
+                ChatUtil.SendSayMessage(player, "GameNPC.Blacksmith.NotEnoughMoney", 
+                    GetName(0, true), 
+                    Money.GetString(item.RepairCost), 
+                    item.GetName(0, false));
 
                 return;
             }
 
-            player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language,
-                    "Scripts.Blacksmith.YouPay",
-                    GetName(0, false), Money.GetString(item.RepairCost)), eChatType.CT_System,
-                eChatLoc.CL_SystemWindow);
+            // Message: You pay {0} {1}.
+            ChatUtil.SendSystemMessage(player, "GameNPC.Blacksmith.YouPay", GetName(0, false), Money.GetString(item.RepairCost));
 
             // Items with IsNotLosingDur are not....losing DUR.
             if (ToRecoverCond + 1 >= item.Durability)
             {
                 item.Condition = item.Condition + item.Durability;
                 item.Durability = 0;
-                SayTo(player, LanguageMgr.GetTranslation(player.Client.Account.Language,
-                    "Scripts.Blacksmith.ObjectRatherOld", item.Name));
+                // Message: {0} says, "Uhh, {1} is rather old. I won't be able to repair it again, so be careful!"
+                ChatUtil.SendSayMessage(player, "GameNPC.Blacksmith.ObjectRatherOld", GetName(0, true), item.GetName(0, false));
             }
             else
             {
@@ -210,13 +226,11 @@ namespace DOL.GS
 
 
             player.Out.SendInventoryItemsUpdate(new InventoryItem[] {item});
-            player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language,
-                    "Scripts.Blacksmith.ItsDone", item.Name), eChatType.CT_System,
-                eChatLoc.CL_SystemWindow);
+            // Message: {0} says, "There, {1} is ready for combat."
+            ChatUtil.SendSayMessage(player, "GameNPC.Blacksmith.ItsDone", GetName(0, true), item.GetName(0, false));
 
             return;
         }
-
-        #endregion
+        #endregion  Repair Responses
     }
 }
--- GameServer/gameobjects/CustomNPC/Enchanter.cs
+++ GameServer/gameobjects/CustomNPC/Enchanter.cs
@@ -37,50 +37,80 @@ namespace DOL.GS
 		private const string ENCHANT_ITEM_WEAK = "enchanting item";
 		private int[] BONUS_TABLE = new int[] {5, 5, 10, 15, 20, 25, 30, 30};
 
-        /// <summary>
-        /// Adds messages to ArrayList which are sent when object is targeted
-        /// </summary>
-        /// <param name="player">GamePlayer that is examining this object</param>
-        /// <returns>list with string messages</returns>
+		#region Examine Messages
+		/// <summary>
+		/// Adds messages to array, which are all sent when the NPC is examined (i.e., clicked on) by a GamePlayer.
+		/// </summary>
+		/// <param name="player">The GamePlayer examining the NPC.</param>
+		/// <returns>Returns the messages in the array and sends them all to the player.</returns>
 		public override IList GetExamineMessages(GamePlayer player)
 		{
             IList list = new ArrayList();
-            list.Add(LanguageMgr.GetTranslation(player.Client.Account.Language, "Enchanter.GetExamineMessages.Text1", 
-                                                GetName(0, false, player.Client.Account.Language, this)));
-            list.Add(LanguageMgr.GetTranslation(player.Client.Account.Language, "Enchanter.GetExamineMessages.Text2",
-                                                GetName(0, false, player.Client.Account.Language, this), GetPronoun(0, true, player.Client.Account.Language),
-                                                GetAggroLevelString(player, false)));
+            // Message: You target [{0}].
+            list.Add(LanguageMgr.GetTranslation(player.Client.Account.Language, "GameNPC.Enchanter.GetExamineMessages.YouTarget", 
+				GetName(0, false, player.Client.Account.Language, this)));
+            // Message: You examine {0}. {1} is {2} and is an enchanter.
+            list.Add(LanguageMgr.GetTranslation(player.Client.Account.Language, "GameNPC.Enchanter.GetExamineMessages.YouExamine",
+				GetName(0, false, player.Client.Account.Language, this), GetPronoun(0, true, player.Client.Account.Language),
+				GetAggroLevelString(player, false)));
+            // Message: [Give {0} an item to be magically enhanced]
+            list.Add(LanguageMgr.GetTranslation(player.Client.Account.Language, "GameNPC.Enchanter.GetExamineMessages.GiveItem",
+	            GetPronoun(0, false, player.Client.Account.Language)));
             return list;
 		}
+		#endregion Examine Messages
 
+		#region Interact Message
+		/// <summary>
+		/// Behaviors that occur when a GamePlayer interacts with the NPC (e.g., facing player, send interact message, etc.).
+		/// </summary>
+		/// <param name="player">The GamePlayer interacting with the NPC.</param>
+		/// <returns>If the interaction is prevented by the base class on GamePlayer.cs, then return 'false'.</returns>
 		public override bool Interact(GamePlayer player)
 		{
+			// If the interaction is not prevented by the base class on GamePlayer.cs, then return 'true'
 			if (base.Interact(player))
 			{
-				TurnTo(player, 25000);
-				string Material;
-				if (player.Realm == eRealm.Hibernia)
-                    Material = LanguageMgr.GetTranslation(ServerProperties.Properties.DB_LANGUAGE, "Enchanter.Interact.Text1");
-				else
-                    Material = LanguageMgr.GetTranslation(ServerProperties.Properties.DB_LANGUAGE, "Enchanter.Interact.Text2");
+				TurnTo(player, 5000);
+				
+				//string Material;
+				// Specify a different material requirement based on realm
+				//if (player.Realm == eRealm.Hibernia)
+					// Message: quartz
+                    //Material = LanguageMgr.GetTranslation(ServerProperties.Properties.DB_LANGUAGE, "GameNPC.Enchanter.Interact.Quartz");
+				//else
+					// Message: steel
+                    //Material = LanguageMgr.GetTranslation(ServerProperties.Properties.DB_LANGUAGE, "GameNPC.Enchanter.Interact.Steel");
 
-                SayTo(player, eChatLoc.CL_ChatWindow, LanguageMgr.GetTranslation(player.Client.Account.Language, "Enchanter.Interact.Text3", Material));
+				// Check for ambient trigger messages for the NPC in the 'MobXAmbientBehaviour' table
+				var triggers = GameServer.Instance.NpcManager.AmbientBehaviour[base.Name];
+				// If the NPC has no ambient trigger message assigned, then return this message
+				if (triggers == null || triggers.Length == 0)
+					// Message: {0} says, "For a small fee, I can infuse your weapons or armor with an enchantment that will improve their quality. Just hand me the item. It must be of a material I can work with."
+					ChatUtil.SendSayMessage(player, "GameNPC.Enchanter.Interact.Message", GetName(0, true));
                 return true;
 			}
 			return false;
 		}
+		#endregion Interact Messages
 
+		#region Receive Responses
+		/// <summary>
+		/// Behaviors that occur when an NPC is given an item (primarily to repair, but also for XP item turn-in).
+		/// </summary>
+		/// <param name="source">The entity giving the item to the NPC (e.g., 'GamePlayer player').</param>
+		/// /// <param name="item">The specific item being given to the NPC.</param>
 		public override bool ReceiveItem(GameLiving source, InventoryItem item)
 		{
-			GamePlayer t = source as GamePlayer;
-			if (t == null || item == null)
+			GamePlayer player = source as GamePlayer;
+			if (player == null || item == null)
 				return false;
 
 			if (this.DataQuestList.Count > 0)
 			{
 				foreach (DataQuest quest in DataQuestList)
 				{
-					quest.Notify(GameLivingEvent.ReceiveItem, this, new ReceiveItemEventArgs(t, this, item));
+					quest.Notify(GameLivingEvent.ReceiveItem, this, new ReceiveItemEventArgs(player, this, item));
 				}
 			}
 			
@@ -90,21 +120,35 @@ namespace DOL.GS
 				{
 					if (item.Bonus == 0)
 					{
-						t.TempProperties.setProperty(ENCHANT_ITEM_WEAK, new WeakRef(item));
-                        t.Client.Out.SendCustomDialog(LanguageMgr.GetTranslation(t.Client, "Enchanter.ReceiveItem.Text1", Money.GetString(CalculEnchantPrice(item))), new CustomDialogResponse(EnchanterDialogResponse));
+						player.TempProperties.setProperty(ENCHANT_ITEM_WEAK, new WeakRef(item));
+						// Message: It will cost {0} to enchant that. Do you accept?
+                        player.Client.Out.SendCustomDialog(LanguageMgr.GetTranslation(player.Client, "GameNPC.Enchanter.ReceiveItem.Cost", Money.GetString(CalculEnchantPrice(item))), new CustomDialogResponse(EnchanterDialogResponse));
                     }
 					else
-                        SayTo(t, eChatLoc.CL_SystemWindow, LanguageMgr.GetTranslation(t.Client, "Enchanter.ReceiveItem.Text2"));
+						// Message: {0} says, "That is already enchanted."
+						ChatUtil.SendSayMessage(player, "GameNPC.Enchanter.ReceiveItem.AlreadyEnchanted", GetName(0, true));
+                        //SayTo(player, eChatLoc.CL_SystemWindow, LanguageMgr.GetTranslation(player.Client, "GameNPC.Enchanter.ReceiveItem.AlreadyEnchanted"));
                 }
 				else
-                    SayTo(t, eChatLoc.CL_SystemWindow, LanguageMgr.GetTranslation(t.Client, "Enchanter.ReceiveItem.Text3"));
+					// Message: {0} says, "That item can't be enchanted."
+					ChatUtil.SendSayMessage(player, "GameNPC.Enchanter.ReceiveItem.CantBeEnchanted", GetName(0, true));
+                    //SayTo(player, eChatLoc.CL_SystemWindow, LanguageMgr.GetTranslation(player.Client, "GameNPC.Enchanter.ReceiveItem.CantBeEnchanted"));
             }
 			else
-                SayTo(t, eChatLoc.CL_SystemWindow, LanguageMgr.GetTranslation(t.Client, "Enchanter.ReceiveItem.Text4"));
+				// Message: {0} says, "I can't enchant that material."
+				ChatUtil.SendSayMessage(player, "GameNPC.Enchanter.ReceiveItem.CantEnchantMaterial", GetName(0, true));
+                //SayTo(player, eChatLoc.CL_SystemWindow, LanguageMgr.GetTranslation(player.Client, "GameNPC.Enchanter.ReceiveItem.CantEnchantMaterial"));
 
 			return false;
 		}
+		#endregion Receive Responses
 
+		#region Repair Responses
+		/// <summary>
+		/// Responses that occur when an NPC attempts to enchant an item given to them.
+		/// </summary>
+		/// <param name="player">The entity that originally gave the item to the NPC.</param>
+		/// /// <param name="response">The player's response when prompted to initiate the enchant (ACCEPT/DECLINE).</param>
 		protected void EnchanterDialogResponse(GamePlayer player, byte response)
 		{
 			WeakReference itemWeak =
@@ -114,15 +158,22 @@ namespace DOL.GS
 					);
 			player.TempProperties.removeProperty(ENCHANT_ITEM_WEAK);
 
+			InventoryItem item = (InventoryItem) itemWeak.Target;
 
 			if (response != 0x01 || !this.IsWithinRadius(player, WorldMgr.INTERACT_DISTANCE))
-				return;
-
-			InventoryItem item = (InventoryItem) itemWeak.Target;
-			if (item == null || item.SlotPosition == (int) eInventorySlot.Ground
-				|| item.OwnerID == null || item.OwnerID != player.InternalID)
 			{
-                player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language, "Enchanter.EnchanterDialogResponse.Text1"), eChatType.CT_System, eChatLoc.CL_SystemWindow);
+				// Message: You decline to have your {0} repaired.
+				ChatUtil.SendSystemMessage(player, "GameNPC.Enchanter.Response.Decline", item.Name);
+				
+				return;
+			}
+
+			if (item == null || item.SlotPosition == (int) eInventorySlot.Ground
+			                 || item.OwnerID == null || item.OwnerID != player.InternalID)
+			{
+				// Message: {0} says, "I can't enchant that."
+				ChatUtil.SendSayMessage(player, "GameNPC.Enchanter.Response.CantEnchant", GetName(0, true));
+                //player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language, "GameNPC.Enchanter.Response.CantEnchant"), eChatType.CT_System, eChatLoc.CL_SystemWindow);
                 return;
 			}
 
@@ -130,7 +181,12 @@ namespace DOL.GS
 
 			if (player.GetCurrentMoney() < Fee)
 			{
-                SayTo(player, eChatLoc.CL_SystemWindow, LanguageMgr.GetTranslation(player.Client.Account.Language, "Enchanter.EnchanterDialogResponse.Text2", Money.GetString(Fee)));
+				// Message: {0} says, "It costs {1} to enchant {2}. You don't have that much."
+				ChatUtil.SendSayMessage(player, "GameNPC.Enchanter.Response.NotEnoughMoney", 
+					GetName(0, true), 
+					Money.GetString(Fee), 
+					item.GetName(0, false));
+                //SayTo(player, eChatLoc.CL_SystemWindow, LanguageMgr.GetTranslation(player.Client.Account.Language, "GameNPC.Enchanter.Response.NotEnoughMoney", Money.GetString(Fee)));
                 return;
 			}
 			if (item.Level < 50)
@@ -138,15 +194,25 @@ namespace DOL.GS
 			else
 				item.Bonus = 35;
 
-            item.Name = LanguageMgr.GetTranslation(player.Client.Account.Language, "Enchanter.EnchanterDialogResponse.Text3") + " " + item.Name;
+			// bright
+            item.Name = LanguageMgr.GetTranslation(player.Client.Account.Language, "GameNPC.Enchanter.ItemLevel.Bright") + " " + item.Name;
             player.Out.SendInventoryItemsUpdate(new InventoryItem[] { item });
-            player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language, "Enchanter.EnchanterDialogResponse.Text4", 
-                                    GetName(0, false, player.Client.Account.Language, this), Money.GetString(Fee)), eChatType.CT_System, eChatLoc.CL_SystemWindow);
+            // Message: You give {0} {1}.
+            ChatUtil.SendSayMessage(player, "GameNPC.Enchanter.Response.YouGive",
+	            GetName(0, true),
+	            Money.GetString(Fee));
+            //player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language, "GameNPC.Enchanter.Response.YouGive", 
+                                    //GetName(0, false, player.Client.Account.Language, this), Money.GetString(Fee)), eChatType.CT_System, eChatLoc.CL_SystemWindow);
             player.RemoveMoney(Fee, null);
             InventoryLogging.LogInventoryAction(player, this, eInventoryActionType.Merchant, Fee);
-            SayTo(player, eChatLoc.CL_SystemWindow, LanguageMgr.GetTranslation(player.Client.Account.Language, "Enchanter.EnchanterDialogResponse.Text5", item.GetName(1, false)));
+            // Message: {0} says, "There, it is now {1}!"
+            ChatUtil.SendSayMessage(player, "GameNPC.Enchanter.Response.NowEnchanted", 
+	            GetName(0, true),
+	            item.GetName(1, false));
+            //SayTo(player, eChatLoc.CL_SystemWindow, LanguageMgr.GetTranslation(player.Client.Account.Language, "GameNPC.Enchanter.Response.NowEnchanted", item.GetName(1, false)));
             return;
 		}
+		#endregion Repair Responses
 
 		public long CalculEnchantPrice(InventoryItem item)
 		{
