9677ecad43d3b5ebcc9bc1efc5cf4595589311e0
clait <stefanomus@fastmail.com>
2022-02-09 12:53:04 +0000

    Skeletal Sacristan (`DOL.GS.Scripts.SkeletalSacristan`)

---
+++ GameServer/scripts/namedmobs/CaerSidi/SkeletalSacristan.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using DOL.AI.Brain;
+using DOL.Events;
+using DOL.GS;
+using DOL.GS.PacketHandler;
+
+namespace DOL.GS.Scripts
+{
+    public class SkeletalSacristan : GameNPC
+    {
+
+		public SkeletalSacristan() : base() { }
+		public static GameNPC SkeletalSacristanMob = new GameNPC();
+		public override bool AddToWorld()
+		{
+			Model = 916;
+			Name = "Skeletal Sacristan";
+			Size = 80;
+			Level = 77;
+			Gender = eGender.Neutral;
+			BodyType = 11; // undead
+			MaxDistance = 1500;
+			Flags -= eFlags.PEACE;
+			TetherRange = 2000;
+			RoamingRange = 0;
+			SkeletalSacristanBrain sBrain = new SkeletalSacristanBrain();
+			SetOwnBrain(sBrain);
+			sBrain.AggroLevel = 100;
+			sBrain.AggroRange = 500;
+			base.AddToWorld();
+			return true;
+		}
+		
+		[ScriptLoadedEvent]
+		public static void ScriptLoaded(DOLEvent e, object sender, EventArgs args)
+		{
+			if (log.IsInfoEnabled)
+				log.Info("Skeletal Sacristan NPC Initializing...");
+		}
+		
+	}
+    
+}
+namespace DOL.AI.Brain
+{
+	public class SkeletalSacristanBrain : StandardMobBrain
+	{
+		public SkeletalSacristanBrain() : base() { }
+
+		public override void OnAttackedByEnemy(AttackData ad)
+		{
+			Body.WalkTo(Body.X + Util.Random(1000), Body.Y + Util.Random(1000), Body.Z, 460);
+
+			base.OnAttackedByEnemy(ad);
+		}
+		
+		public override void AttackMostWanted()
+		{
+
+		}
+		
+		public override void Think()
+		{
+			if (Body.InCombat == true && Body.IsAlive && HasAggro)
+			{
+				Body.WalkTo(Body.X + Util.Random(1000), Body.Y + Util.Random(1000), Body.Z, 460);
+			}
+			if (Body.InCombatInLast(15 * 1000) == false && this.Body.InCombatInLast(15 * 1000))
+			{
+				Body.WalkTo(Body.X + Util.Random(1000), Body.Y + Util.Random(1000), Body.Z, 460);
+			}
+			base.Think();
+		}
+		
+	}
+}
