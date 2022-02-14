b8dc0d7ce51086fedff91725b2c5a570d1a8250e
Kelteen <dean.dudlitz@live.de>
2022-02-14 11:31:37 +0100

    Dragons Broadcast Messages are in Chatwindow now

--- GameServer/gameobjects/Dragons/GameDragon.cs
+++ GameServer/gameobjects/Dragons/GameDragon.cs
@@ -331,7 +331,7 @@ namespace DOL.GS
 		{
 			foreach (GamePlayer player in GetPlayersInRadius(WorldMgr.OBJ_UPDATE_DISTANCE))
 			{
-				player.Out.SendMessage(message, eChatType.CT_Broadcast, eChatLoc.CL_SystemWindow);
+				player.Out.SendMessage(message, eChatType.CT_Broadcast, eChatLoc.CL_ChatWindow);
 			}
 		}
 
