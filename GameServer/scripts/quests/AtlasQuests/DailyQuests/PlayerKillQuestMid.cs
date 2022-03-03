abaa8173f092f382bdc0aa6942ead942278f6a5a
Anthony Knight <midgetlord123@hotmail.com>
2022-03-02 16:02:34 -0800

    Player kill dailies reward half a level and 1000 orbs

--- GameServer/scripts/quests/AtlasQuests/DailyQuests/PlayerKillQuestMid.cs
+++ GameServer/scripts/quests/AtlasQuests/DailyQuests/PlayerKillQuestMid.cs
@@ -338,8 +338,9 @@ namespace DOL.GS.DailyQuest.Hibernia
 
 		public override void FinishQuest()
 		{
-			m_questPlayer.GainExperience(eXPSource.Quest, 200, true);
+			m_questPlayer.GainExperience(eXPSource.Quest, (m_questPlayer.ExperienceForNextLevel - m_questPlayer.ExperienceForCurrentLevel)/2, true);
 			m_questPlayer.AddMoney(Money.GetMoney(0,0,1,32,Util.Random(50)), "You receive {0} as a reward.");
+			AtlasROGManager.GenerateOrbAmount(m_questPlayer, 1000);
 			PlayersKilled = 0;
 			base.FinishQuest(); //Defined in Quest, changes the state, stores in DB etc ...
 			
