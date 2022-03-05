a528004d6969f95941c2d1b8135b06f29928a826
Kelteen <dean.dudlitz@live.de>
2022-03-05 15:16:41 +0100

    Adding Weekly DF Kill Quest for Hib

---
+++ GameServer/scripts/quests/AtlasQuests/DailyQuests/DFWeeklyKillQuestHib.cs
@@ -0,0 +1,354 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using DOL.Database;
+using DOL.Events;
+using DOL.GS;
+using DOL.GS.API;
+using DOL.GS.PacketHandler;
+using DOL.GS.PlayerTitles;
+using DOL.GS.Quests;
+using log4net;
+
+namespace DOL.GS.DailyQuest.Hibernia
+{
+	public class DFWeeklyKillQuestHib : Quests.WeeklyQuest
+	{
+		/// <summary>
+		/// Defines a logger for this class.
+		/// </summary>
+		private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+		protected const string questTitle = "[Weekly] Vacuum Darkness Falls";
+		protected const int minimumLevel = 45;
+		protected const int maximumLevel = 50;
+		
+		// prevent grey killing
+		protected byte MIN_PLAYER_LVL = 35;
+		// Kill Goal
+		protected int MAX_KILLED = 50;
+
+		private static GameNPC Dean = null; // Start NPC
+
+		private static int EnemiesKilled = 0;
+
+		// Constructors
+		public DFWeeklyKillQuestHib() : base()
+		{
+		}
+
+		public DFWeeklyKillQuestHib(GamePlayer questingPlayer) : base(questingPlayer)
+		{
+		}
+
+		public DFWeeklyKillQuestHib(GamePlayer questingPlayer, int step) : base(questingPlayer, step)
+		{
+		}
+
+		public DFWeeklyKillQuestHib(GamePlayer questingPlayer, DBQuest dbQuest) : base(questingPlayer, dbQuest)
+		{
+		}
+
+
+		[ScriptLoadedEvent]
+		public static void ScriptLoaded(DOLEvent e, object sender, EventArgs args)
+		{
+			if (!ServerProperties.Properties.LOAD_QUESTS)
+				return;
+			
+
+			#region defineNPCs
+
+			GameNPC[] npcs = WorldMgr.GetNPCsByName("Dean", eRealm.Hibernia);
+
+			if (npcs.Length > 0)
+				foreach (GameNPC npc in npcs)
+					if (npc.CurrentRegionID == 200 && npc.X == 334962 && npc.Y == 420687)
+					{
+						Dean = npc;
+						break;
+					}
+
+			if (Dean == null)
+			{
+				if (log.IsWarnEnabled)
+					log.Warn("Could not find Dean , creating it ...");
+				Dean = new GameNPC();
+				Dean.Model = 355;
+				Dean.Name = "Dean";
+				Dean.GuildName = "Atlas Quest";
+				Dean.Realm = eRealm.Hibernia;
+				//Druim Ligen Location
+				Dean.CurrentRegionID = 200;
+				Dean.Size = 50;
+				Dean.Level = 59;
+				Dean.X = 334962;
+				Dean.Y = 420687;
+				Dean.Z = 5184;
+				Dean.Heading = 1571;
+				Dean.AddToWorld();
+				if (SAVE_INTO_DATABASE)
+				{
+					Dean.SaveIntoDatabase();
+				}
+			}
+
+			#endregion
+
+			#region defineItems
+			#endregion
+
+			#region defineObject
+			#endregion
+
+			GameEventMgr.AddHandler(GamePlayerEvent.AcceptQuest, new DOLEventHandler(SubscribeQuest));
+			GameEventMgr.AddHandler(GamePlayerEvent.DeclineQuest, new DOLEventHandler(SubscribeQuest));
+
+			GameEventMgr.AddHandler(Dean, GameObjectEvent.Interact, new DOLEventHandler(TalkToDean));
+			GameEventMgr.AddHandler(Dean, GameLivingEvent.WhisperReceive, new DOLEventHandler(TalkToDean));
+
+			/* Now we bring to Dean the possibility to give this quest to players */
+			Dean.AddQuestToGive(typeof (DFWeeklyKillQuestHib));
+
+			if (log.IsInfoEnabled)
+				log.Info("Quest \"" + questTitle + "\" initialized");
+		}
+
+		[ScriptUnloadedEvent]
+		public static void ScriptUnloaded(DOLEvent e, object sender, EventArgs args)
+		{
+			//if not loaded, don't worry
+			if (Dean == null)
+				return;
+			// remove handlers
+			GameEventMgr.RemoveHandler(GamePlayerEvent.AcceptQuest, new DOLEventHandler(SubscribeQuest));
+			GameEventMgr.RemoveHandler(GamePlayerEvent.DeclineQuest, new DOLEventHandler(SubscribeQuest));
+
+			GameEventMgr.RemoveHandler(Dean, GameObjectEvent.Interact, new DOLEventHandler(TalkToDean));
+			GameEventMgr.RemoveHandler(Dean, GameLivingEvent.WhisperReceive, new DOLEventHandler(TalkToDean));
+
+			/* Now we remove to Dean the possibility to give this quest to players */
+			Dean.RemoveQuestToGive(typeof (DFWeeklyKillQuestHib));
+		}
+
+		protected static void TalkToDean(DOLEvent e, object sender, EventArgs args)
+		{
+			//We get the player from the event arguments and check if he qualifies		
+			GamePlayer player = ((SourceEventArgs) args).Source as GamePlayer;
+			if (player == null)
+				return;
+
+			if(Dean.CanGiveQuest(typeof (DFWeeklyKillQuestHib), player)  <= 0)
+				return;
+
+			//We also check if the player is already doing the quest
+			DFWeeklyKillQuestHib quest = player.IsDoingQuest(typeof (DFWeeklyKillQuestHib)) as DFWeeklyKillQuestHib;
+
+			if (e == GameObjectEvent.Interact)
+			{
+				if (quest != null)
+				{
+					switch (quest.Step)
+					{
+						case 1:
+							Dean.SayTo(player, "Please enter Darkness Falls and defend Hibernia from enemies!");
+							break;
+						case 2:
+							Dean.SayTo(player, "Hello " + player.Name + ", did you [defend Darkness Falls] for your reward?");
+							break;
+					}
+				}
+				else
+				{
+					Dean.SayTo(player, "Hello "+ player.Name +", I am Dean, do you need a task? "+
+					                       "I heard you are strong enough to help me with Weekly Missions of Hibernia. \n\n"+
+					                       "\nCan you [support Hibernia]?");
+				}
+			}
+				// The player whispered to the NPC
+			else if (e == GameLivingEvent.WhisperReceive)
+			{
+				WhisperReceiveEventArgs wArgs = (WhisperReceiveEventArgs) args;
+				if (quest == null)
+				{
+					switch (wArgs.Text)
+					{
+						case "support Hibernia":
+							player.Out.SendQuestSubscribeCommand(Dean, QuestMgr.GetIDForQuestType(typeof(DFWeeklyKillQuestHib)), "Will you help Dean [Daily] Atlas for the Win?");
+							break;
+					}
+				}
+				else
+				{
+					switch (wArgs.Text)
+					{
+						case "defend Darkness Falls":
+							if (quest.Step == 2)
+							{
+								player.Out.SendMessage("Thank you for your contribution!", eChatType.CT_Chat, eChatLoc.CL_PopupWindow);
+								quest.FinishQuest();
+							}
+							break;
+						case "abort":
+							player.Out.SendCustomDialog("Do you really want to abort this quest, \nall items gained during quest will be lost?", new CustomDialogResponse(CheckPlayerAbortQuest));
+							break;
+					}
+				}
+			}
+		}
+		
+		public override bool CheckQuestQualification(GamePlayer player)
+		{
+			// if the player is already doing the quest his level is no longer of relevance
+			if (player.IsDoingQuest(typeof (DFWeeklyKillQuestHib)) != null)
+				return true;
+
+			// This checks below are only performed is player isn't doing quest already
+
+			//if (player.HasFinishedQuest(typeof(Academy_47)) == 0) return false;
+
+			//if (!CheckPartAccessible(player,typeof(CityOfCamelot)))
+			//	return false;
+
+			if (player.Level < minimumLevel || player.Level > maximumLevel)
+				return false;
+
+			return true;
+		}
+
+		private static void CheckPlayerAbortQuest(GamePlayer player, byte response)
+		{
+			DFWeeklyKillQuestHib quest = player.IsDoingQuest(typeof (DFWeeklyKillQuestHib)) as DFWeeklyKillQuestHib;
+
+			if (quest == null)
+				return;
+
+			if (response == 0x00)
+			{
+				SendSystemMessage(player, "Good, now go out there and finish your work!");
+			}
+			else
+			{
+				SendSystemMessage(player, "Aborting Quest " + questTitle + ". You can start over again if you want.");
+				quest.AbortQuest();
+			}
+		}
+
+		protected static void SubscribeQuest(DOLEvent e, object sender, EventArgs args)
+		{
+			QuestEventArgs qargs = args as QuestEventArgs;
+			if (qargs == null)
+				return;
+
+			if (qargs.QuestID != QuestMgr.GetIDForQuestType(typeof(DFWeeklyKillQuestHib)))
+				return;
+
+			if (e == GamePlayerEvent.AcceptQuest)
+				CheckPlayerAcceptQuest(qargs.Player, 0x01);
+			else if (e == GamePlayerEvent.DeclineQuest)
+				CheckPlayerAcceptQuest(qargs.Player, 0x00);
+		}
+
+		private static void CheckPlayerAcceptQuest(GamePlayer player, byte response)
+		{
+			if(Dean.CanGiveQuest(typeof (DFWeeklyKillQuestHib), player)  <= 0)
+				return;
+
+			if (player.IsDoingQuest(typeof (DFWeeklyKillQuestHib)) != null)
+				return;
+
+			if (response == 0x00)
+			{
+				player.Out.SendMessage("Thank you for helping Atlas.", eChatType.CT_Say, eChatLoc.CL_PopupWindow);
+			}
+			else
+			{
+				//Check if we can add the quest!
+				if (!Dean.GiveQuest(typeof (DFWeeklyKillQuestHib), player, 1))
+					return;
+				EnemiesKilled = 0;
+
+				Dean.SayTo(player, "Defend your realm in Darkness Falls and kill them for your reward.");
+
+			}
+		}
+
+		//Set quest name
+		public override string Name
+		{
+			get { return questTitle; }
+		}
+
+		// Define Steps
+		public override string Description
+		{
+			get
+			{
+				switch (Step)
+				{
+					case 1:
+						return "Defend Hibernia in Darkness Falls. \nKilled: Enemies ("+ EnemiesKilled +" | 50)";
+					case 2:
+						return "Return to Dean for your Reward.";
+				}
+				return base.Description;
+			}
+		}
+
+		public override void Notify(DOLEvent e, object sender, EventArgs args)
+		{
+			GamePlayer player = sender as GamePlayer;
+
+			if (player == null || player.IsDoingQuest(typeof(DFWeeklyKillQuestHib)) == null)
+				return;
+
+			if (Step == 1 && e == GameLivingEvent.Interact)
+			{
+				InteractEventArgs gArgs = (InteractEventArgs) args;
+				if (gArgs.Source.Name == Dean.Name)
+				{
+					Dean.SayTo(player, "Did you know that Fen is awesome? Now you know!");
+					return;
+				}
+			}
+
+			
+			if (Step == 1 && e == GameLivingEvent.EnemyKilled)
+			{
+				EnemyKilledEventArgs gArgs = (EnemyKilledEventArgs) args;
+				//prevent grey killing
+				
+				
+				if (gArgs.Target.Realm != 0 && gArgs.Target.Realm != player.Realm && gArgs.Target is GamePlayer && gArgs.Target.Level >= MIN_PLAYER_LVL && gArgs.Target.CurrentRegionID == 249) 
+				{
+					EnemiesKilled++;
+					player.Out.SendMessage("[Weekly] Enemy Killed: ("+EnemiesKilled+" | "+MAX_KILLED+")", eChatType.CT_ScreenCenter, eChatLoc.CL_SystemWindow);
+					player.Out.SendQuestUpdate(this);
+					
+					if (EnemiesKilled >= MAX_KILLED)
+					{
+						// FinishQuest or go back to Dean
+						Step = 2;
+					}
+				}
+			}
+			
+		}
+
+		public override void AbortQuest()
+		{
+			base.AbortQuest(); //Defined in Quest, changes the state, stores in DB etc ...
+		}
+
+		public override void FinishQuest()
+		{
+			m_questPlayer.GainExperience(eXPSource.Quest, (m_questPlayer.ExperienceForNextLevel - m_questPlayer.ExperienceForCurrentLevel)/10, true);
+			m_questPlayer.AddMoney(Money.GetMoney(0,0,1,32,Util.Random(50)), "You receive {0} as a reward.");
+			AtlasROGManager.GenerateOrbAmount(m_questPlayer, 5000);
+			EnemiesKilled = 0;
+			base.FinishQuest(); //Defined in Quest, changes the state, stores in DB etc ...
+			
+		}
+	}
+}
