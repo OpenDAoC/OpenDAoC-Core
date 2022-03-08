be1d5be2f985d704b6cb652cf36db31be1a2ba39
Kelteen <dean.dudlitz@live.de>
2022-03-08 18:15:30 +0100

    Added Heal Spell, rdy for test

--- GameServer/scripts/namedmobs/TuscarianGlacier/Hord.cs
+++ GameServer/scripts/namedmobs/TuscarianGlacier/Hord.cs
@@ -9,7 +9,7 @@ using DOL.GS.PacketHandler;
 using DOL.GS.Styles;
 using DOL.GS.Effects;
 
-namespace DOL.GS
+namespace DOL.GS.Scripts
 {
     public class Hord : GameNPC
     {
@@ -38,6 +38,8 @@ namespace DOL.GS
             Level = 77;
             // Giant
             BodyType = 5;
+            ScalingFactor = 45;
+            
             HordBrain sBrain = new HordBrain();
             SetOwnBrain(sBrain);
             base.AddToWorld();
@@ -64,7 +66,7 @@ namespace DOL.GS
         }
         public override bool HasAbility(string keyName)
         {
-            if (this.IsAlive && keyName == DOL.GS.Abilities.CCImmunity)
+            if (this.IsAlive && keyName == GS.Abilities.CCImmunity)
                 return true;
 
             return base.HasAbility(keyName);
@@ -97,6 +99,23 @@ namespace DOL.GS
             base.Die(killer);
         }
         
+        #region Damage & Heal Events
+
+        /// <summary>
+        /// Take some amount of damage inflicted by another GameObject.
+        /// </summary>
+        /// <param name="source">The object inflicting the damage.</param>
+        /// <param name="damageType">The type of damage.</param>
+        /// <param name="damageAmount">The amount of damage inflicted.</param>
+        /// <param name="criticalAmount">The critical amount of damage inflicted</param>
+        public override void TakeDamage(GameObject source, eDamageType damageType, int damageAmount, int criticalAmount)
+        {
+            base.TakeDamage(source, damageType, damageAmount, criticalAmount);
+            Brain.Notify(GameObjectEvent.TakeDamage, this,
+                new TakeDamageEventArgs(source, damageType, damageAmount, criticalAmount));
+        }
+        #endregion
+        
     }
 }
 namespace DOL.AI.Brain
@@ -104,23 +123,98 @@ namespace DOL.AI.Brain
     public class HordBrain : StandardMobBrain
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        protected String m_HealAnnounce;
+        
         public HordBrain()
             : base()
         {
+            m_HealAnnounce = "{0} heals his wounds.";
             AggroLevel = 200;
             AggroRange = 1500; //so players cant just pass him without aggroing
         }
+        
         public void BroadcastMessage(String message)
         {
             foreach (GamePlayer player in Body.GetPlayersInRadius(WorldMgr.OBJ_UPDATE_DISTANCE))
             {
-                player.Out.SendMessage(message, eChatType.CT_Broadcast, eChatLoc.CL_SystemWindow);
+                player.Out.SendMessage(message, eChatType.CT_Broadcast, eChatLoc.CL_ChatWindow);
             }
         }
-       
+        
         public override void Think()
         {
+            /*if (Body.TargetObject != null && Body.InCombat && Body.Health != Body.MaxHealth && HasAggro)
+            {
+                if (Util.Chance(1))
+                {
+                    new RegionTimer(Body, new RegionTimerCallback(CastHeal), 1000);
+                }
+            }*/
             base.Think();
+        }
+
+        public override void Notify(DOLEvent e, object sender, EventArgs args)
+        {
+            base.Notify(e, sender, args);
+
+            if (e == GameNPCEvent.TakeDamage)
+            {
+                if (Body.TargetObject != null && Body.InCombat && Body.Health != Body.MaxHealth && HasAggro)
+                {
+                    if (Util.Chance(3))
+                    {
+                        new RegionTimer(Body, new RegionTimerCallback(CastHeal), 1000);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Cast Heal on itself
+        /// </summary>
+        /// <param name="timer">The timer that started this cast.</param>
+        /// <returns></returns>
+        private int CastHeal(RegionTimer timer)
+        {
+            BroadcastMessage(String.Format(m_HealAnnounce, Body.Name));
+            Body.CastSpell(Heal, SkillBase.GetSpellLine(GlobalSpellsLines.Mob_Spells));
+            return 0;
+        }
+        
+        protected Spell m_healSpell;
+        /// <summary>
+        /// The Heal spell.
+        /// </summary>
+        protected Spell Heal
+        {
+            get
+            {
+                if (m_healSpell == null)
+                {
+                    DBSpell spell = new DBSpell();
+                    spell.AllowAdd = false;
+                    spell.CastTime = 0;
+                    spell.RecastDelay = 0;
+                    spell.Power = 0;
+                    spell.ClientEffect = 3011;
+                    spell.Icon = 3011;
+                    spell.TooltipId = 3011;
+                    spell.Damage = 0;
+                    spell.Name = "Minor Emendation";
+                    spell.Range = 0;
+                    spell.SpellID = 3011;
+                    spell.Duration = 0;
+                    spell.Value = 500;
+                    spell.SpellGroup = 130;
+                    spell.Target = "Self";
+                    spell.Type = "Heal";
+                    spell.Uninterruptible = true;
+                    spell.MoveCast = true;
+                    m_healSpell = new Spell(spell, 70);
+                    SkillBase.AddScriptedSpell(GlobalSpellsLines.Mob_Spells, m_healSpell);
+                }
+                return m_healSpell;
+            }
         }
     }
 }
