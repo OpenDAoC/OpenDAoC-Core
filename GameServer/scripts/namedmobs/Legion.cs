e4ca36c38fd6743e80238febcf022ebe6718f5a7
Kelteen <dean.dudlitz@live.de>
2022-03-12 20:22:18 +0100

    Added Legion with Spawn mechanic (WIP)

---
+++ GameServer/scripts/namedmobs/Legion.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DOL.AI.Brain;
+using DOL.Events;
+using DOL.Database;
+using DOL.GS;
+using DOL.GS.PacketHandler;
+using DOL.GS.Styles;
+using DOL.GS.Effects;
+using DOL.GS.Scripts;
+
+namespace DOL.GS.Scripts
+{
+    public class Legion : GameNPC
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public Legion()
+            : base()
+        {
+        }
+
+        public virtual int LegionDifficulty
+        {
+            get { return ServerProperties.Properties.SET_DIFFICULTY_ON_EPIC_ENCOUNTERS; }
+        }
+
+        public override double AttackDamage(InventoryItem weapon)
+        {
+            return base.AttackDamage(weapon) * Strength / 100;
+        }
+
+        public override int MaxHealth
+        {
+            get
+            {
+                return 20000 * LegionDifficulty;
+            }
+        }
+
+        public override int AttackRange
+        {
+            get
+            {
+                return 450;
+            }
+            set
+            {
+            }
+        }
+        public override bool HasAbility(string keyName)
+        {
+            if (IsAlive && keyName == GS.Abilities.CCImmunity)
+                return true;
+
+            return base.HasAbility(keyName);
+        }
+        public override double GetArmorAF(eArmorSlot slot)
+        {
+            return 1000 * LegionDifficulty;
+        }
+
+        public override double GetArmorAbsorb(eArmorSlot slot)
+        {
+            // 85% ABS is cap.
+            return 0.85 * LegionDifficulty;
+        }
+        
+        public override bool AddToWorld()
+        {
+            INpcTemplate npcTemplate = NpcTemplateMgr.GetTemplate(13333);
+            LoadTemplate(npcTemplate);
+            
+            Strength = npcTemplate.Strength;
+            Constitution = npcTemplate.Constitution;
+            Dexterity = npcTemplate.Dexterity;
+            Quickness = npcTemplate.Quickness;
+            Empathy = npcTemplate.Empathy;
+            Piety = npcTemplate.Piety;
+            Intelligence = npcTemplate.Intelligence;
+
+            // demon
+            BodyType = 2;
+            
+            LegionBrain sBrain = new LegionBrain();
+            SetOwnBrain(sBrain);
+            base.AddToWorld();
+            return true;
+        }
+        public override void Die(GameObject killer)
+        {
+            foreach (GameNPC npc in GetNPCsInRadius(5000))
+            {
+                if (npc.Brain is LegionAddBrain)
+                {
+                    npc.RemoveFromWorld();
+                }
+            }
+            
+            // debug
+            log.Debug($"{Name} killed by {killer.Name}");
+            
+            GamePlayer playerKiller = killer as GamePlayer;
+
+            if (playerKiller?.Group != null)
+            {
+                foreach (GamePlayer groupPlayer in playerKiller.Group.GetPlayersInTheGroup())
+                {
+                    AtlasROGManager.GenerateOrbAmount(groupPlayer,5000);
+                }
+            }
+            DropLoot(killer);
+            base.Die(killer);
+        }
+    }
+}
+
+namespace DOL.AI.Brain
+{
+    public class LegionBrain : StandardMobBrain
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public LegionBrain()
+            : base()
+        {
+            AggroLevel = 100;
+            AggroRange = 500;
+        }
+
+        public void BroadcastMessage(String message)
+        {
+            foreach (GamePlayer player in Body.GetPlayersInRadius(WorldMgr.OBJ_UPDATE_DISTANCE))
+            {
+                player.Out.SendMessage(message, eChatType.CT_Broadcast, eChatLoc.CL_SystemWindow);
+            }
+        }
+
+        public override void Think()
+        {
+            if (Body.InCombatInLast(60 * 1000) == false && Body.InCombatInLast(65 * 1000))
+            {
+                Body.Health = Body.MaxHealth;
+                foreach (GameNPC npc in Body.GetNPCsInRadius(5000))
+                {
+                    if (npc.Brain is LegionAddBrain)
+                    {
+                        npc.RemoveFromWorld();
+                    }
+                }
+
+                if (Body.TargetObject != null && HasAggro && Body.InCombat)
+                {
+                    
+                    // 5% chance to spawn 15-20 zombies
+                    if (Util.Chance(5))
+                    {
+                        SpawnAdds();
+                    }
+                }
+            }
+            base.Think();
+        }
+
+        public override void Notify(DOLEvent e, object sender, EventArgs args)
+        {
+            base.Notify(e, sender, args);
+        }
+
+        public void SpawnAdds()
+        {
+            for (int i = 0; i < Util.Random(15, 20); i++)
+            {
+                LegionAdd add = new LegionAdd();
+                add.X = Body.X;
+                add.Y = Body.Y;
+                add.Z = Body.Z;
+                add.CurrentRegion = Body.CurrentRegion;
+                add.Heading = Body.Heading;
+                add.AddToWorld();
+                break;
+            }
+            
+        }
+    }
+}
+
+namespace DOL.GS
+{
+    public class LegionAdd : GameNPC
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public LegionAdd()
+            : base()
+        {
+        }
+
+        public override double AttackDamage(InventoryItem weapon)
+        {
+            return base.AttackDamage(weapon) * Strength / 100;
+        }
+
+        public override int MaxHealth
+        {
+            get
+            {
+                return 7000;
+            }
+        }
+
+        public override int AttackRange
+        {
+            get
+            {
+                return 450;
+            }
+            set
+            {
+            }
+        }
+        public override double GetArmorAF(eArmorSlot slot)
+        {
+            return 1000;
+        }
+
+        public override double GetArmorAbsorb(eArmorSlot slot)
+        {
+            // 85% ABS is cap.
+            return 0.85;
+        }
+        public override bool AddToWorld()
+        {
+            Legion legion = new Legion();
+            Model = 660;
+            Name = "graspering soul";
+            Size = 50;
+            Level = (byte)Util.Random(50,58);
+            Realm = 0;
+            CurrentRegionID = legion.CurrentRegionID;
+
+            Strength = 60;
+            Intelligence = 60;
+            Piety = 60;
+            Dexterity = 60;
+            Constitution = 60;
+            Quickness = 60;
+            RespawnInterval = -1;
+
+            Gender = eGender.Neutral;
+            MeleeDamageType = eDamageType.Slash;
+
+            BodyType = 2;
+            LegionAddBrain sBrain = new LegionAddBrain();
+            SetOwnBrain(sBrain);
+            sBrain.AggroLevel = 100;
+            sBrain.AggroRange = 800;
+            base.AddToWorld();
+            return true;
+        }
+    }
+}
+
+namespace DOL.AI.Brain
+{
+    public class LegionAddBrain : StandardMobBrain
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public LegionAddBrain()
+            : base()
+        {
+            AggroLevel = 100;
+            AggroRange = 800;
+        }
+        public override void Think()
+        {
+            base.Think();
+        }
+    }
+}
