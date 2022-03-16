462b7c863c6d2c415049b8dc4d0f415e97d1d529
Glimmer Warshade <glimmer@europe.com>
2022-03-16 14:09:51 +0100

    Fornfrusonen double drop loot fix.

--- GameServer/scripts/namedmobs/TuscarianGlacier/Fornfrusonen.cs
+++ GameServer/scripts/namedmobs/TuscarianGlacier/Fornfrusonen.cs
@@ -72,7 +72,6 @@ namespace DOL.GS
                     }
                 }
             }
-            DropLoot(killer);
             base.Die(killer);
         }
         public override bool AddToWorld()
