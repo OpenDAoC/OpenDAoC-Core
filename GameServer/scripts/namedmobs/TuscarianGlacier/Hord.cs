5e930aeed5e3fe572f6311aa77eda5d5ac9eb219
Glimmer Warshade <glimmer@europe.com>
2022-03-16 14:10:02 +0100

    Hord double drop loot fix.

--- GameServer/scripts/namedmobs/TuscarianGlacier/Hord.cs
+++ GameServer/scripts/namedmobs/TuscarianGlacier/Hord.cs
@@ -95,7 +95,6 @@ namespace DOL.GS.Scripts
                     AtlasROGManager.GenerateOrbAmount(groupPlayer,5000);
                 }
             }
-            DropLoot(killer);
             base.Die(killer);
         }
         
