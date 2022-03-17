0e407f420ed9721a9012a2391fa498162baa28b2
Glimmer Warshade <glimmer@europe.com>
2022-03-17 18:12:09 +0100

    Icelord Kvasir script

---
+++ GameServer/scripts/namedmobs/TuscarianGlacier/IcelordKvasir.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DOL.AI.Brain;
+using DOL.Events;
+using DOL.Database;
+using DOL.GS;
+using DOL.GS.ServerRules;
+using DOL.GS.PacketHandler;
+using DOL.GS.Styles;
+using DOL.GS.Effects;
+using Timer = System.Timers.Timer;
+using System.Timers;
+
+namespace DOL.GS
+{
+    public class Kvasir : GameEpicBoss
+    {
+        public Kvasir() : base() { }
+        public override int GetResist(eDamageType damageType)
+        {
+            switch (damageType)
+            {
+                case eDamageType.Slash: return 95;// dmg reduction for melee dmg
+                case eDamageType.Crush: return 95;// dmg reduction for melee dmg
+                case eDamageType.Thrust: return 95;// dmg reduction for melee dmg
+                default: return 80;// dmg reduction for rest resists
+            }
+        }
+        public override void TakeDamage(GameObject source, eDamageType damageType, int damageAmount, int criticalAmount)
+        {
+            if (source is GamePlayer || source is GamePet)
+            {
+                if (damageType == eDamageType.Cold)//take no damage
+                {
+                    this.Health += this.MaxHealth/5;//heal himself if damage is cold
+                    BroadcastMessage(String.Format("Icelord Kvasir says, 'aahhhh thank you "+source.Name+" for healing me !'"));
+                    base.TakeDamage(source, damageType, 0, 0);
+                    return;
+                }
+                else//take dmg
+                {
+                    base.TakeDamage(source, damageType, damageAmount, criticalAmount);
+                }
+            }
+        }
+        public override double AttackDamage(InventoryItem weapon)
+        {
+            return base.AttackDamage(weapon) * Strength / 100;
+        }
+        public override int AttackRange
+        {
+            get { return 350; }
+            set { }
+        }
+        public override bool HasAbility(string keyName)
+        {
+            if (this.IsAlive && keyName == DOL.GS.Abilities.CCImmunity)
+                return true;
+
+            return base.HasAbility(keyName);
+        }
+        public override double GetArmorAF(eArmorSlot slot)
+        {
+            return 1000;
+        }
+        public override double GetArmorAbsorb(eArmorSlot slot)
+        {
+            // 85% ABS is cap.
+            return 0.85;
+        }
+        public override int MaxHealth
+        {
+            get { return 20000; }
+        }
+        public void BroadcastMessage(String message)
+        {
+            foreach (GamePlayer player in this.GetPlayersInRadius(WorldMgr.OBJ_UPDATE_DISTANCE))
+            {
+                player.Out.SendMessage(message, eChatType.CT_Broadcast, eChatLoc.CL_SystemWindow);
+            }
+        }
+        public override void Die(GameObject killer)//on kill generate orbs
+        {
+            // debug
+            log.Debug($"{Name} killed by {killer.Name}");
+
+            GamePlayer playerKiller = killer as GamePlayer;
+
+            if (playerKiller?.Group != null)
+            {
+                foreach (GamePlayer groupPlayer in playerKiller.Group.GetPlayersInTheGroup())
+                {
+                    AtlasROGManager.GenerateOrbAmount(groupPlayer, 5000);//5k orbs for every player in group
+                }
+            }
+            base.Die(killer);
+        }
+        public override bool AddToWorld()
+        {
+            INpcTemplate npcTemplate = NpcTemplateMgr.GetTemplate(60162348);
+            LoadTemplate(npcTemplate);
+            Strength = npcTemplate.Strength;
+            Dexterity = npcTemplate.Dexterity;
+            Constitution = npcTemplate.Constitution;
+            Quickness = npcTemplate.Quickness;
+            Piety = npcTemplate.Piety;
+            Intelligence = npcTemplate.Intelligence;
+            Empathy = npcTemplate.Empathy;
+            Faction = FactionMgr.GetFactionByID(140);
+            Faction.AddFriendFaction(FactionMgr.GetFactionByID(140));
+            RespawnInterval = ServerProperties.Properties.SET_SI_EPIC_ENCOUNTER_RESPAWNINTERVAL * 60000;//1min is 60000 miliseconds
+
+            KvasirBrain sbrain = new KvasirBrain();
+            SetOwnBrain(sbrain);
+            LoadedFromScript = false;//load from database
+            SaveIntoDatabase();
+            base.AddToWorld();
+            return true;
+        }
+        [ScriptLoadedEvent]
+        public static void ScriptLoaded(DOLEvent e, object sender, EventArgs args)
+        {
+            GameNPC[] npcs;
+            npcs = WorldMgr.GetNPCsByNameFromRegion("Icelord Kvasir", 160, (eRealm)0);
+            if (npcs.Length == 0)
+            {
+                log.Warn("Icelord Kvasir not found, creating it...");
+
+                log.Warn("Initializing Icelord Kvasir...");
+                Kvasir TG = new Kvasir();
+                TG.Name = "Icelord Kvasir";
+                TG.PackageID = "Icelord Kvasir";
+                TG.Model = 918;
+                TG.Realm = 0;
+                TG.Level = 78;
+                TG.Size = 70;
+                TG.CurrentRegionID = 160;//tuscaran glacier
+                TG.MeleeDamageType = eDamageType.Crush;
+                TG.RespawnInterval = ServerProperties.Properties.SET_SI_EPIC_ENCOUNTER_RESPAWNINTERVAL * 60000;//1min is 60000 miliseconds
+                TG.Faction = FactionMgr.GetFactionByID(140);
+                TG.Faction.AddFriendFaction(FactionMgr.GetFactionByID(140));
+
+                TG.X = 20107;
+                TG.Y = 52112;
+                TG.Z = 10916;
+                TG.Heading = 1025;
+                KvasirBrain ubrain = new KvasirBrain();
+                TG.SetOwnBrain(ubrain);
+                TG.AddToWorld();
+                TG.SaveIntoDatabase();
+                TG.Brain.Start();
+            }
+            else
+                log.Warn("Icelord Kvasir exist ingame, remove it and restart server if you want to add by script code.");
+        }
+    }
+}
+namespace DOL.AI.Brain
+{
+    public class KvasirBrain : StandardMobBrain
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public KvasirBrain()
+            : base()
+        {
+            AggroLevel = 100;
+            AggroRange = 600;
+            ThinkInterval = 2000;
+        }
+        public static bool IsPulled = false;
+        public override void OnAttackedByEnemy(AttackData ad)
+        {
+            if (IsPulled == false)
+            {
+                foreach (GameNPC npc in WorldMgr.GetNPCsFromRegion(Body.CurrentRegionID))
+                {
+                    if (npc != null)
+                    {
+                        if (npc.IsAlive && npc.PackageID == "KvasirBaf")
+                        {
+                            AddAggroListTo(npc.Brain as StandardMobBrain);
+                            IsPulled = true;
+                        }
+                    }
+                }               
+            }
+            base.OnAttackedByEnemy(ad);
+        }
+        public override void Think()
+        {
+            if (!HasAggressionTable())
+            {
+                //set state to RETURN TO SPAWN
+                FSM.SetCurrentState(eFSMStateType.RETURN_TO_SPAWN);
+                this.Body.Health = this.Body.MaxHealth;
+                IsPulled = false;
+            }
+            if (Body.IsOutOfTetherRange)
+            {
+                this.Body.Health = this.Body.MaxHealth;
+                ClearAggroList();
+            }
+            else if (Body.InCombatInLast(30 * 1000) == false && this.Body.InCombatInLast(35 * 1000))
+            {
+                this.Body.Health = this.Body.MaxHealth;
+            }
+            if (Body.InCombat || HasAggro || Body.AttackState == true)
+            {
+                if(Util.Chance(10))
+                {
+                    Body.CastSpell(Mezz, SkillBase.GetSpellLine(GlobalSpellsLines.Mob_Spells));
+                }
+            }
+            base.Think();
+        }
+        protected Spell m_mezSpell;
+
+        protected Spell Mezz
+        {
+            get
+            {
+                if (m_mezSpell == null)
+                {
+                    DBSpell spell = new DBSpell();
+                    spell.AllowAdd = false;
+                    spell.CastTime = 0;
+                    spell.RecastDelay = Util.Random(60,120);
+                    spell.ClientEffect = 3371;
+                    spell.Icon = 3371;
+                    spell.Damage = 0;
+                    spell.Name = "Mesmerize";
+                    spell.Range = 1500;
+                    spell.Radius = 500;
+                    spell.SpellID = 11746;
+                    spell.Duration = 60;
+                    spell.Target = eSpellTarget.Enemy.ToString();
+                    spell.Type = "Mesmerize";
+                    spell.Uninterruptible = true;
+                    spell.MoveCast = true;
+                    spell.DamageType = (int)eDamageType.Spirit; //Spirit DMG Type
+                    m_mezSpell = new Spell(spell, 70);
+                    SkillBase.AddScriptedSpell(GlobalSpellsLines.Mob_Spells, m_mezSpell);
+                }
+                return m_mezSpell;
+            }
+        }
+    }
+}
