15467990a11d9b34e6cc36ef3203aafb338f9dfe
Kelteen <dean.dudlitz@live.de>
2022-03-18 20:22:21 +0100

    Quest is for Hibernia

--- GameServer/scripts/quests/AtlasQuests/DailyQuests/Daily Hib/GallaMobQuestHib.cs
+++ GameServer/scripts/quests/AtlasQuests/DailyQuests/Daily Hib/GallaMobQuestHib.cs
@@ -12,7 +12,7 @@ using DOL.GS.PlayerTitles;
 using DOL.GS.Quests;
 using log4net;
 
-namespace DOL.GS.DailyQuest.Albion
+namespace DOL.GS.DailyQuest.Hibernia
 {
     public class GallaMobQuestHib : Quests.DailyQuest
     {
