5cd7d725842c2569d1cfd2c7073ddb8aa2c1a652
Anthony Knight <midgetlord123@hotmail.com>
2022-03-21 12:16:50 -0700

    Add Rain of Fire/Ice/Annihilation RAs

---
+++ GameServer/realmabilities_atlasOF/effects/AtlasOF_RainOfAnnihilationECSEffect.cs
@@ -0,0 +1,16 @@
+using DOL.GS.PacketHandler;
+
+namespace DOL.GS.Effects
+{
+    public class AtlasOF_RainOfAnnihilationECSEffect : DamageAddECSEffect
+    {
+        public AtlasOF_RainOfAnnihilationECSEffect(ECSGameEffectInitParams initParams)
+            : base(initParams)
+        {
+        }
+        
+        public override ushort Icon { get { return 7023; } }
+        public override string Name { get { return "Rain Of Annihilation"; } }
+        public override bool HasPositiveEffect { get { return true; } }
+    }
+}
