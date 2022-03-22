9464110300b39e57a0baced1f572ed8f9a821260
Anthony Knight <midgetlord123@hotmail.com>
2022-03-21 17:27:30 -0700

    `Whirling Dervish` and `Hail Of Blows` have correct pre-reqs

--- GameServer/realmabilities_atlasOF/handlers/AtlasOF_HailOfBlowsAbility.cs
+++ GameServer/realmabilities_atlasOF/handlers/AtlasOF_HailOfBlowsAbility.cs
@@ -17,6 +17,8 @@ namespace DOL.GS.RealmAbilities
         public const int duration = 60000; // 60 seconds
         public override int MaxLevel { get { return 3; } }
         public override int GetReUseDelay(int level) { return 900; } // 15 mins
+        public override bool CheckRequirement(GamePlayer player) { return AtlasRAHelpers.HasAugDexLevel(player, 3); }
+        
         public override int CostForUpgrade(int level) {
             switch (level)
             {
