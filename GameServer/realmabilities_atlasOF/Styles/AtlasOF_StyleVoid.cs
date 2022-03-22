b217e8702f8731f5c8675141ec8a8b501baad664
Anthony Knight <midgetlord123@hotmail.com>
2022-03-22 13:51:40 -0700

    Added `Void` Style RA

---
+++ GameServer/realmabilities_atlasOF/Styles/AtlasOF_StyleVoid.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using DOL.GS;
+using DOL.GS.PacketHandler;
+using DOL.GS.Effects;
+using DOL.Database;
+using DOL.GS.Spells;
+using DOL.GS.Styles;
+
+namespace DOL.GS.RealmAbilities
+{
+    public class AtlasOF_StyleVoid : StyleRealmAbility
+    {
+        
+        public AtlasOF_StyleVoid(DBAbility ability, int level) : base(ability, level)
+        {
+        }
+
+        protected override Style CreateStyle()
+        {
+            DBStyle tmpStyle = new DBStyle();
+            tmpStyle.Name = "Void";
+            tmpStyle.GrowthRate = 1.4;
+            tmpStyle.EnduranceCost = 0;
+            tmpStyle.BonusToHit = 15;
+            tmpStyle.BonusToDefense = 10;
+            tmpStyle.WeaponTypeRequirement = 1001; //any weapon type
+            tmpStyle.OpeningRequirementType = 0;
+            tmpStyle.OpeningRequirementValue = 0;
+            tmpStyle.AttackResultRequirement = 0;
+            tmpStyle.Icon = 1695; 
+            tmpStyle.SpecKeyName = GlobalSpellsLines.Realm_Spells;
+            return new Style(tmpStyle);
+        }
+    }
+
+}
