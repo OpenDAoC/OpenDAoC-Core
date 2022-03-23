212819f91af4347f53a60eb380bef013bd5ab88d
Anthony Knight <midgetlord123@hotmail.com>
2022-03-23 08:23:36 -0700

    Tracking fix for `A Bit of Bravery`

--- GameServer/scripts/quests/AtlasQuests/DailyQuests/Daily Hib/KillNPCInFrontiersHib.cs
+++ GameServer/scripts/quests/AtlasQuests/DailyQuests/Daily Hib/KillNPCInFrontiersHib.cs
@@ -325,7 +325,7 @@ namespace DOL.GS.DailyQuest.Hibernia
 			if (e == GameLivingEvent.EnemyKilled && Step == 1)
 			{
 				EnemyKilledEventArgs gArgs = (EnemyKilledEventArgs) args;
-				if (player.GetConLevel(gArgs.Target) >= 0 
+				if (player.GetConLevel(gArgs.Target) > -1
 				    && gArgs.Target.CurrentZone.IsRvR && player.CurrentZone.IsRvR) 
 				{
 					FrontierMobsKilled++;
