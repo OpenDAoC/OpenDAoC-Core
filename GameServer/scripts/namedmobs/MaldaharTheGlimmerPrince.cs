6a270b4e716f547fa1b6db56f708579bec03ba05
clait <stefanomus@fastmail.com>
2022-03-23 12:24:27 +0000

    MaldaharTheGlimmerPrince double drop fix

--- GameServer/scripts/namedmobs/MaldaharTheGlimmerPrince.cs
+++ GameServer/scripts/namedmobs/MaldaharTheGlimmerPrince.cs
@@ -95,8 +95,6 @@ namespace DOL.GS
                     AtlasROGManager.GenerateOrbAmount(groupPlayer, 5000);
                 }
             }
-
-            DropLoot(killer);
             base.Die(killer);
         }
     }
