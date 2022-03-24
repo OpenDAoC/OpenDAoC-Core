3e26d873df0878d9015bf62be670f32fe5908def
clait <stefanomus@fastmail.com>
2022-03-24 17:35:21 +0000

    OrbsReward added as a property to GameNPC, GameEpicNPC and GameBossNPC (last 2 linked to server property)

--- GameServer/gameobjects/GameEpicBoss.cs
+++ GameServer/gameobjects/GameEpicBoss.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DOL.GS.ServerProperties;
 
 namespace DOL.GS {
     public class GameEpicBoss : GameEpicNPC {
         public GameEpicBoss() : base()
         {
             ScalingFactor = 80;
+            OrbsReward = Properties.EPICBOSS_ORBS;
         }
+        
     }
 }
--- GameServer/gameobjects/GameEpicNPC.cs
+++ GameServer/gameobjects/GameEpicNPC.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DOL.Database;
+using DOL.GS.ServerProperties;
 
 namespace DOL.GS {
     public class GameEpicNPC : GameNPC {
         public GameEpicNPC() : base()
         {
             ScalingFactor = 60;
+            OrbsReward = Properties.EPICNPC_ORBS;
         }
     }
 }
