eae70d99456dc36bfa9b53a4110929a4a297f7dd
clait <stefanomus@fastmail.com>
2022-03-24 10:39:01 +0000

    Night Spawn fix

--- GameServer/scripts/mobs/NightSpawn.cs
+++ GameServer/scripts/mobs/NightSpawn.cs
@@ -1,72 +1,71 @@
 ﻿using System;
 using DOL.AI.Brain;
-using DOL.Database;
 using DOL.Events;
 using DOL.GS;
-
 
 namespace DOL.GS
 {
     public class NightSpawn : GameNPC
     {
-	    
-	    public override bool AddToWorld()
-		{
-			NightSpawnBrain sBrain = new NightSpawnBrain();
-			SetOwnBrain(sBrain);
-			base.AddToWorld();
-			return true;
-		}        
-	    
-		[ScriptLoadedEvent]
-		public static void ScriptLoaded(DOLEvent e, object sender, EventArgs args)
-		{
-			if (log.IsInfoEnabled)
-				log.Info("Night mobs initialising...");
-		}
+        public override bool AddToWorld()
+        {
+            NightSpawnBrain sBrain = new NightSpawnBrain();
+            SetOwnBrain(sBrain);
+            base.AddToWorld();
+            return true;
+        }
+
+        [ScriptLoadedEvent]
+        public static void ScriptLoaded(DOLEvent e, object sender, EventArgs args)
+        {
+            if (log.IsInfoEnabled)
+                log.Info("Night mobs initialising...");
+        }
     }
-    
 }
 
 namespace DOL.AI.Brain
 {
-	public class NightSpawnBrain : StandardMobBrain
-	{
-		private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    public class NightSpawnBrain : StandardMobBrain
+    {
+        private static readonly log4net.ILog log =
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-		public static ushort oldModel = 0;
-		public static GameNPC.eFlags oldFlags = 0;
+        public static ushort oldModel = 0;
+        public static GameNPC.eFlags oldFlags = 0;
+        private bool changed;
 
-		public override void Think()
-		{
+        public override void Think()
+        {
+            if (!Body.CurrentRegion.IsNightTime)
+            {
+                if (!changed)
+                {
+                    oldFlags = Body.Flags;
+                    Body.Flags ^= GameNPC.eFlags.CANTTARGET;
+                    Body.Flags ^= GameNPC.eFlags.DONTSHOWNAME;
+                    Body.Flags ^= GameNPC.eFlags.PEACE;
+                    
+                    if (oldModel == 0)
+                    {
+                        oldModel = Body.Model;
+                    }
+                    Body.Model = 1;
 
-			if (!Body.CurrentRegion.IsNightTime)
-			{
-				if (oldModel == 0)
-				{
-					oldModel = Body.Model;
-				}
-				
-				if (Body.Flags != GameNPC.eFlags.CANTTARGET)
-					Body.Flags ^= GameNPC.eFlags.CANTTARGET;
-				if (Body.Flags != GameNPC.eFlags.DONTSHOWNAME)
-					Body.Flags ^= GameNPC.eFlags.DONTSHOWNAME;
-				
-				Body.Model = 1;
-			}
-			else
-			{
-				if (oldModel != 0)
-				{
-					Body.Model = oldModel;
-					oldModel = 0;
-				}
-				Body.Flags = oldFlags;
-			}
+                    changed = true;
+                }
+            }
+            else
+            {
+                if (changed)
+                {
+                    Body.Model = oldModel;
+                    Body.Flags = oldFlags;
+                    changed = false;
+                }
+            }
 
-			base.Think();
-		}
-		
-		
-	}
+            base.Think();
+        }
+    }
 }
