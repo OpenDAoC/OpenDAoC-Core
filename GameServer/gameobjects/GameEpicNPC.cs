1ca89450171ae8bc814da45407c0098fadb8d8d5
clait <stefanomus@fastmail.com>
2022-03-24 19:56:01 +0000

    `GameEpicNPC` has a 80% (+20% loyalty) chance to drop orbs based on the mob level

--- GameServer/gameobjects/GameEpicNPC.cs
+++ GameServer/gameobjects/GameEpicNPC.cs
@@ -1,11 +1,10 @@
-﻿using DOL.GS.ServerProperties;
+﻿using System;
 
 namespace DOL.GS {
     public class GameEpicNPC : GameNPC {
         public GameEpicNPC() : base()
         {
             ScalingFactor = 60;
-            OrbsReward = Properties.EPICNPC_ORBS;
         }
         public override void Die(GameObject killer)
         {
@@ -13,19 +12,42 @@ namespace DOL.GS {
             log.Debug($"{Name} killed by {killer.Name}");
             
             GamePlayer playerKiller = killer as GamePlayer;
-
+            
+            var amount = Util.Random(Level / 10, Level * 2 / 10);
+            var baseChance = 80;
+            var realmLoyalty = 0;
+            
+            
             if (playerKiller?.Group != null)
             {
                 foreach (GamePlayer groupPlayer in playerKiller.Group.GetPlayersInTheGroup())
                 {
-                    AtlasROGManager.GenerateOrbAmount(groupPlayer,OrbsReward);
+                    int numCurrentLoyalDays = groupPlayer.TempProperties.getProperty<int>("current_loyalty_days");
+                    if (numCurrentLoyalDays >= 1)
+                    {
+                        realmLoyalty = (int)Math.Round(20 / (numCurrentLoyalDays / 30.0) );
+                    }
+                    if(Util.Chance(baseChance+realmLoyalty))
+                    {
+                        AtlasROGManager.GenerateOrbAmount(groupPlayer,amount);
+                    }
                 }
             }
             else
             {
-                AtlasROGManager.GenerateOrbAmount(playerKiller,OrbsReward);
+                int numCurrentLoyalDays = playerKiller.TempProperties.getProperty<int>("current_loyalty_days");
+                if (numCurrentLoyalDays >= 1)
+                {
+                    realmLoyalty = (int)Math.Round(20 / (numCurrentLoyalDays / 30.0) );
+                }
+                if(Util.Chance(baseChance+realmLoyalty))
+                {
+                    AtlasROGManager.GenerateOrbAmount(playerKiller,amount);
+                }
+                {
+                    AtlasROGManager.GenerateOrbAmount(playerKiller,amount);
+                }
             }
-            
             base.Die(killer);
         }
     }
