95468ec9522820fc95e0cd2314e9658b459a4ff7
Glimmer Warshade <glimmer@europe.com>
2022-03-27 19:35:22 +0200

    Captain Heathyr script

---
+++ GameServer/scripts/namedmobs/HallOfTheCorrupt/CaptainHeathyr.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using DOL.AI.Brain;
+using DOL.Events;
+using DOL.Database;
+using DOL.GS;
+using DOL.GS.PacketHandler;
+using DOL.GS.Styles;
+namespace DOL.GS
+{
+    public class CaptainHeathyr : GameEpicBoss
+    {
+        public CaptainHeathyr() : base()
+        {
+        }
+        public static int AfterBlockID = 137;
+        public static int AfterBlockClassID = 2;
+        public static Style AfterBlock = SkillBase.GetStyleByID(AfterBlockID, AfterBlockClassID);
+
+        public static int TauntID = 134;
+        public static int TauntClassID = 2;
+        public static Style Taunt = SkillBase.GetStyleByID(TauntID, TauntClassID);
+        public override void OnAttackedByEnemy(AttackData ad) // on Boss actions
+        {
+            base.OnAttackedByEnemy(ad);
+        }
+        public override void OnAttackEnemy(AttackData ad) //on enemy actions
+        {
+            if (Util.Chance(35))
+            {
+                if (ad != null && (ad.AttackResult == eAttackResult.HitUnstyled || ad.AttackResult == eAttackResult.HitStyle))
+                {
+                    this.CastSpell(Bleed, SkillBase.GetSpellLine(GlobalSpellsLines.Mob_Spells));
+                }
+            }
+            base.OnAttackEnemy(ad);
+        }
+        public override int GetResist(eDamageType damageType)
+        {
+            switch (damageType)
+            {
+                case eDamageType.Slash: return 35; // dmg reduction for melee dmg
+                case eDamageType.Crush: return 35; // dmg reduction for melee dmg
+                case eDamageType.Thrust: return 35; // dmg reduction for melee dmg
+                default: return 25; // dmg reduction for rest resists
+            }
+        }
+
+        public override void TakeDamage(GameObject source, eDamageType damageType, int damageAmount, int criticalAmount)
+        {
+            if (source is GamePlayer || source is GamePet)
+            {
+                if (this.IsOutOfTetherRange)
+                {
+                    if (damageType == eDamageType.Body || damageType == eDamageType.Cold ||
+                        damageType == eDamageType.Energy || damageType == eDamageType.Heat
+                        || damageType == eDamageType.Matter || damageType == eDamageType.Spirit ||
+                        damageType == eDamageType.Crush || damageType == eDamageType.Thrust
+                        || damageType == eDamageType.Slash)
+                    {
+                        GamePlayer truc;
+                        if (source is GamePlayer)
+                            truc = (source as GamePlayer);
+                        else
+                            truc = ((source as GamePet).Owner as GamePlayer);
+                        if (truc != null)
+                            truc.Out.SendMessage(this.Name + " is immune to any damage!", eChatType.CT_System,
+                                eChatLoc.CL_ChatWindow);
+                        base.TakeDamage(source, damageType, 0, 0);
+                        return;
+                    }
+                }
+                else
+                {
+                    base.TakeDamage(source, damageType, damageAmount, criticalAmount);
+                }
+            }
+        }
+        public override double AttackDamage(InventoryItem weapon)
+        {
+            return base.AttackDamage(weapon) * Strength / 100;
+        }
+        public override int AttackRange
+        {
+            get { return 350; }
+            set { }
+        }
+        public override bool HasAbility(string keyName)
+        {
+            if (this.IsAlive && keyName == DOL.GS.Abilities.CCImmunity)
+                return true;
+
+            return base.HasAbility(keyName);
+        }
+        public override double GetArmorAF(eArmorSlot slot)
+        {
+            return 800;
+        }
+        public override double GetArmorAbsorb(eArmorSlot slot)
+        {
+            // 85% ABS is cap.
+            return 0.65;
+        }
+        public override int MaxHealth
+        {
+            get { return 15000; }
+        }
+        public override bool AddToWorld()
+        {
+            INpcTemplate npcTemplate = NpcTemplateMgr.GetTemplate(7717);
+            LoadTemplate(npcTemplate);
+            Strength = npcTemplate.Strength;
+            Dexterity = npcTemplate.Dexterity;
+            Constitution = npcTemplate.Constitution;
+            Quickness = npcTemplate.Quickness;
+            Piety = npcTemplate.Piety;
+            Intelligence = npcTemplate.Intelligence;
+            Empathy = npcTemplate.Empathy;
+            Faction = FactionMgr.GetFactionByID(187);
+            Faction.AddFriendFaction(FactionMgr.GetFactionByID(187));
+            RespawnInterval = ServerProperties.Properties.SET_SI_EPIC_ENCOUNTER_RESPAWNINTERVAL * 60000; //1min is 60000 miliseconds
+            BodyType = (ushort)NpcTemplateMgr.eBodyType.Humanoid;
+
+            GameNpcInventoryTemplate template = new GameNpcInventoryTemplate();
+            template.AddNPCEquipment(eInventorySlot.TorsoArmor, 46, 0, 0, 0);//modelID,color,effect,extension
+            template.AddNPCEquipment(eInventorySlot.ArmsArmor, 48, 0);
+            template.AddNPCEquipment(eInventorySlot.LegsArmor, 47, 0);
+            template.AddNPCEquipment(eInventorySlot.HandsArmor, 49, 0, 0, 0);
+            template.AddNPCEquipment(eInventorySlot.FeetArmor, 50, 0, 0, 0);
+            template.AddNPCEquipment(eInventorySlot.Cloak, 91, 0, 0, 0);
+            template.AddNPCEquipment(eInventorySlot.RightHandWeapon, 653, 0, 0);
+            template.AddNPCEquipment(eInventorySlot.LeftHandWeapon, 1077, 0, 0);
+            Inventory = template.CloseTemplate();
+            SwitchWeapon(eActiveWeaponSlot.Standard);
+            if (!this.Styles.Contains(AfterBlock))
+            {
+                Styles.Add(AfterBlock);
+            }
+            if (!this.Styles.Contains(Taunt))
+            {
+                Styles.Add(Taunt);
+            }
+            VisibleActiveWeaponSlots = 16;
+            CaptainHeathyrBrain sbrain = new CaptainHeathyrBrain();
+            SetOwnBrain(sbrain);
+            LoadedFromScript = false; //load from database
+            SaveIntoDatabase();
+            base.AddToWorld();
+            return true;
+        }
+
+        [ScriptLoadedEvent]
+        public static void ScriptLoaded(DOLEvent e, object sender, EventArgs args)
+        {
+            GameNPC[] npcs;
+            npcs = WorldMgr.GetNPCsByNameFromRegion("Captain Heathyr", 277, (eRealm)0);
+            if (npcs.Length == 0)
+            {
+                log.Warn("Captain Heathyr found, creating it...");
+
+                log.Warn("Initializing Captain Heathyr...");
+                CaptainHeathyr HOC = new CaptainHeathyr();
+                HOC.Name = "Captain Heathyr";
+                HOC.Model = 5;
+                HOC.Realm = 0;
+                HOC.Level = 65;
+                HOC.Size = 50;
+                HOC.CurrentRegionID = 277; //hall of the corrupt
+                HOC.RespawnInterval = ServerProperties.Properties.SET_SI_EPIC_ENCOUNTER_RESPAWNINTERVAL * 60000; //1min is 60000 miliseconds
+                HOC.Faction = FactionMgr.GetFactionByID(187);
+                HOC.Faction.AddFriendFaction(FactionMgr.GetFactionByID(187));
+
+                HOC.X = 33732;
+                HOC.Y = 35839;
+                HOC.Z = 14646;
+                HOC.Heading = 3089;
+                CaptainHeathyrBrain ubrain = new CaptainHeathyrBrain();
+                HOC.SetOwnBrain(ubrain);
+                HOC.AddToWorld();
+                HOC.SaveIntoDatabase();
+                HOC.Brain.Start();
+            }
+            else
+                log.Warn("Captain Heathyr exist ingame, remove it and restart server if you want to add by script code.");
+        }
+        private Spell m_Bleed;
+
+        private Spell Bleed
+        {
+            get
+            {
+                if (m_Bleed == null)
+                {
+                    DBSpell spell = new DBSpell();
+                    spell.AllowAdd = false;
+                    spell.CastTime = 0;
+                    spell.RecastDelay = 2;
+                    spell.ClientEffect = 2130;
+                    spell.Icon = 3411;
+                    spell.TooltipId = 3411;
+                    spell.Damage = 65;
+                    spell.Name = "Bleed";
+                    spell.Description = "Does 65 damage to a target every 3 seconds for 30 seconds.";
+                    spell.Message1 = "You are bleeding! ";
+                    spell.Message2 = "{0} is bleeding! ";
+                    spell.Duration = 30;
+                    spell.Frequency = 30;
+                    spell.Range = 350;
+                    spell.SpellID = 11779;
+                    spell.Target = eSpellTarget.Enemy.ToString();
+                    spell.Type = eSpellType.StyleBleeding.ToString();
+                    spell.Uninterruptible = true;
+                    spell.MoveCast = true;
+                    spell.DamageType = (int)eDamageType.Body;
+                    m_Bleed = new Spell(spell, 70);
+                    SkillBase.AddScriptedSpell(GlobalSpellsLines.Mob_Spells, m_Bleed);
+                }
+
+                return m_Bleed;
+            }
+        }
+    }
+}
+
+namespace DOL.AI.Brain
+{
+    public class CaptainHeathyrBrain : StandardMobBrain
+    {
+        private static readonly log4net.ILog log =
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public CaptainHeathyrBrain()
+            : base()
+        {
+            AggroLevel = 100;
+            AggroRange = 500;
+            ThinkInterval = 1500;
+        }
+        public static bool reset_darra = false;
+        public override void Think()
+        {
+            if (!HasAggressionTable())
+            {
+                //set state to RETURN TO SPAWN
+                FSM.SetCurrentState(eFSMStateType.RETURN_TO_SPAWN);
+                this.Body.Health = this.Body.MaxHealth;
+            }
+            if (Body.IsOutOfTetherRange)
+            {
+                this.Body.Health = this.Body.MaxHealth;
+            }
+            else if (Body.InCombatInLast(30 * 1000) == false && this.Body.InCombatInLast(35 * 1000))
+            {
+                this.Body.Health = this.Body.MaxHealth;
+            }
+            if (Body.InCombat && HasAggro)
+            {
+                if (Body.TargetObject != null)
+                {
+                    Body.styleComponent.NextCombatBackupStyle = CaptainHeathyr.Taunt;
+                    Body.styleComponent.NextCombatStyle = CaptainHeathyr.AfterBlock;
+                }
+            }
+            base.Think();
+        }
+    }
+}
+
