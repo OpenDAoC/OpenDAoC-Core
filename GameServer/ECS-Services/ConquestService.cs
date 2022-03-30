49ae175eefc9fc9c74fd6227963426e9104bf08d
Anthony Knight <midgetlord123@hotmail.com>
2022-03-29 18:23:42 -0700

    Creation of ConquestService

---
+++ GameServer/ECS-Services/ConquestService.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DOL.Database;
+using DOL.GS.Quests;
+using ECS.Debug;
+
+namespace DOL.GS;
+
+public class ConquestService
+{
+    private const string ServiceName = "Conquest Service";
+
+    public static ConquestManager ConquestManager;
+
+
+    static ConquestService()
+    {
+        EntityManager.AddService(typeof(ConquestService));
+        ConquestManager = new ConquestManager();
+    }
+
+    public static void Tick(long tick)
+    {
+        Diagnostics.StartPerfCounter(ServiceName);
+        
+        if(ConquestManager.LastTaskRolloverTick + ServerProperties.Properties.MAX_CONQUEST_TASK_DURATION * 10000 < GameLoop.GameLoopTime) //multiply by 60k ms to accomodate minute input
+        {
+            ConquestManager.RotateKeeps();
+        }
+        //Console.WriteLine($"conquest heartbeat {GameLoop.GameLoopTime} countdown {GameLoop.GameLoopTime - (ConquestManager.LastTaskRolloverTick + ServerProperties.Properties.MAX_CONQUEST_TASK_DURATION * 10000)}");
+        
+        Diagnostics.StopPerfCounter(ServiceName);
+    }
+}
