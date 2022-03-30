0b048967993b84fc123d7d7c07bb6c965763d57b
Anthony Knight <midgetlord123@hotmail.com>
2022-03-30 11:32:29 -0700

    Capturing successive keeps will build a streak. Bonus RPs based on streak

--- GameServer/managers/ConquestManager/ConquestManager.cs
+++ GameServer/managers/ConquestManager/ConquestManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text.RegularExpressions;
 using DOL.Database;
@@ -18,7 +19,7 @@ public class ConquestManager
     private int[] albionKeepIDs = new[] {50, 51, 52, 53, 54, 55, 56};
     private int[] midgardKeepIDs = new[] {75, 76, 77, 78, 79, 80, 81};
     private int[] hiberniaKeepIDs = new[] {100, 101, 102, 103, 104, 105, 106};
-    
+
     private Dictionary<ConquestObjective, int> _albionObjectives;
     private Dictionary<ConquestObjective, int> _hiberniaObjectives;
     private Dictionary<ConquestObjective, int> _midgardObjectives;
@@ -27,15 +28,23 @@ public class ConquestManager
     public ConquestObjective ActiveHiberniaObjective;
     public ConquestObjective ActiveMidgardObjective;
 
+    private int HibStreak;
+    private int AlbStreak;
+    private int MidStreak;
+
     public long LastTaskRolloverTick;
 
     private List<GamePlayer> ContributedPlayers = new List<GamePlayer>();
 
-    public int SumOfContributions { get { return AlbionContribution + HiberniaContribution + MidgardContribution; }}
+    public int SumOfContributions
+    {
+        get { return AlbionContribution + HiberniaContribution + MidgardContribution; }
+    }
+
     int HiberniaContribution = 0;
     int AlbionContribution = 0;
     int MidgardContribution = 0;
-    
+
     public List<ConquestObjective> GetActiveObjectives
     {
         get
@@ -48,14 +57,14 @@ public class ConquestManager
         }
         set { }
     }
-    
+
     public ConquestManager()
     {
         ResetKeeps();
         ResetObjectives();
         RotateKeeps();
     }
-    
+
     private void ResetKeeps()
     {
         if (_albionKeeps == null) _albionKeeps = new List<AbstractGameKeep>();
@@ -66,11 +75,11 @@ public class ConquestManager
         _midgardKeeps.Clear();
         foreach (var keep in GameServer.KeepManager.GetAllKeeps())
         {
-            if(albionKeepIDs.Contains(keep.KeepID))
+            if (albionKeepIDs.Contains(keep.KeepID))
                 _albionKeeps.Add(keep);
-            if(hiberniaKeepIDs.Contains(keep.KeepID))
+            if (hiberniaKeepIDs.Contains(keep.KeepID))
                 _hiberniaKeeps.Add(keep);
-            if(midgardKeepIDs.Contains(keep.KeepID))
+            if (midgardKeepIDs.Contains(keep.KeepID))
                 _midgardKeeps.Add(keep);
         }
     }
@@ -89,12 +98,12 @@ public class ConquestManager
         {
             _albionObjectives.Add(new ConquestObjective(keep), GetConquestValue(keep));
         }
-        
+
         foreach (var keep in _hiberniaKeeps)
         {
             _hiberniaObjectives.Add(new ConquestObjective(keep), GetConquestValue(keep));
         }
-        
+
         foreach (var keep in _midgardKeeps)
         {
             _midgardObjectives.Add(new ConquestObjective(keep), GetConquestValue(keep));
@@ -147,18 +156,37 @@ public class ConquestManager
 
     public void ConquestCapture(AbstractGameKeep CapturedKeep)
     {
-        BroadcastConquestMessageToRvRPlayers($"{GetStringFromRealm(CapturedKeep.Realm)} has captured a conquest objective!");
-        
+        BroadcastConquestMessageToRvRPlayers(
+            $"{GetStringFromRealm(CapturedKeep.Realm)} has captured a conquest objective!");
+
         foreach (var activeObjective in GetActiveObjectives)
         {
             AddSubtotalToOverallFrom(activeObjective);
             activeObjective.ConquestCapture();
         }
-        
+
+        CheckStreak(CapturedKeep);
+
         AwardContributorsForRealm(CapturedKeep.Realm);
         RotateKeepsOnCapture(CapturedKeep);
     }
-    
+
+    private void CheckStreak(AbstractGameKeep capturedKeep)
+    {
+        switch (capturedKeep.Realm)
+        {
+            case eRealm.Albion:
+                AlbStreak++;
+                break;
+            case eRealm.Hibernia:
+                HibStreak++;
+                break;
+            case eRealm.Midgard:
+                MidStreak++;
+                break;
+        }
+    }
+
     private void AwardContributorsForRealm(eRealm realmToAward)
     {
         foreach (var conquestObjective in GetActiveObjectives)
@@ -173,14 +201,21 @@ public class ConquestManager
                         realmContribution = HiberniaContribution;
                     if (realmToAward == eRealm.Albion)
                         realmContribution = AlbionContribution;
-                    if(realmToAward == eRealm.Midgard)
+                    if (realmToAward == eRealm.Midgard)
                         realmContribution = MidgardContribution;
 
                     int calculatedReward = (SumOfContributions * (realmContribution / SumOfContributions));
-                    
+
                     if (calculatedReward > ServerProperties.Properties.MAX_KEEP_CONQUEST_RP_REWARD)
                         calculatedReward = ServerProperties.Properties.MAX_KEEP_CONQUEST_RP_REWARD;
-                    
+
+                    if (contributingPlayer.Realm == eRealm.Hibernia && HibStreak > 0)
+                        calculatedReward += calculatedReward * (HibStreak * 10)/100;
+                    if (contributingPlayer.Realm == eRealm.Albion && AlbStreak > 0)
+                        calculatedReward += calculatedReward * (AlbStreak * 10)/100;
+                    if (contributingPlayer.Realm == eRealm.Midgard && MidStreak > 0)
+                        calculatedReward += calculatedReward * (MidStreak * 10)/100;
+
                     contributingPlayer.GainRealmPoints(calculatedReward, false, true);
                 }
             }
@@ -206,22 +241,22 @@ public class ConquestManager
     {
         for (int i = 1; i < 4; i++)
         {
-            if ((eRealm) i == capturedKeep.Realm)
+            if ((eRealm) i == capturedKeep.OriginalRealm)
             {
                 SetKeepForCapturedRealm(capturedKeep);
             }
             else
             {
-                SetKeepForRealm((eRealm)i);
+                SetDefensiveKeepForRealm((eRealm) i);
             }
         }
     }
 
     public void RotateKeeps()
     {
-        SetKeepForRealm(eRealm.Albion);
-        SetKeepForRealm(eRealm.Hibernia);
-        SetKeepForRealm(eRealm.Midgard);
+        SetDefensiveKeepForRealm(eRealm.Albion);
+        SetDefensiveKeepForRealm(eRealm.Hibernia);
+        SetDefensiveKeepForRealm(eRealm.Midgard);
         ResetContribution();
         LastTaskRolloverTick = GameLoop.GameLoopTime;
         BroadcastConquestMessageToRvRPlayers($"Conquest targets have changed.");
@@ -239,38 +274,116 @@ public class ConquestManager
         //notify everyone an objective was captured
         foreach (var client in WorldMgr.GetAllPlayingClients())
         {
-            if(client.Player.CurrentZone.IsRvR && !client.Player.CurrentZone.IsBG)
-                client.Player.Out.SendMessage(message, eChatType.CT_ScreenCenterSmaller_And_CT_System, eChatLoc.CL_SystemWindow);
+            if (client.Player.CurrentZone.IsRvR && !client.Player.CurrentZone.IsBG)
+                client.Player.Out.SendMessage(message, eChatType.CT_ScreenCenterSmaller_And_CT_System,
+                    eChatLoc.CL_SystemWindow);
         }
     }
 
     private void SetKeepForCapturedRealm(AbstractGameKeep keep)
     {
-        
-        if(keep.Realm != keep.OriginalRealm && GetConquestValue(keep) == 1)
+        if (keep.Realm != keep.OriginalRealm)
         {
-            SetKeepForRealm(keep.Realm, 2);
+            Dictionary<ConquestObjective, int> keepDict = new Dictionary<ConquestObjective, int>();
+            switch (keep.OriginalRealm)
+            {
+                case eRealm.Albion:
+                    keepDict = _albionObjectives;
+                    break;
+                case eRealm.Hibernia:
+                    keepDict = _hiberniaObjectives;
+                    break;
+                case eRealm.Midgard:
+                    keepDict = _midgardObjectives;
+                    break;
+            }
+
+            //check if all keeps of the captured tier are captured
+            //e.g. if a tier2 keep is captured, check for other tier 2 keeps
+            bool allKeepsOfTierAreCaptured = true;
+            foreach (var conquestVal in keepDict.Values.ToImmutableSortedSet())
+            {
+                if (conquestVal == GetConquestValue(keep))
+                {
+                    foreach (var conq in keepDict.Keys.Where(x => keepDict[x] == GetConquestValue(keep)))
+                    {
+                        Console.WriteLine($"{keep.Name} of same value {GetConquestValue(keep)}");
+                        if (conq.Keep.Realm == conq.Keep.OriginalRealm)
+                            allKeepsOfTierAreCaptured = false;
+                    }
+                }
+                
+            }
+
+            Console.WriteLine($"All captured? {allKeepsOfTierAreCaptured}");
+            int objectiveWeight = GetConquestValue(keep);
+            //pick an assault target in next tier if all are captured
+            if (allKeepsOfTierAreCaptured) objectiveWeight++;
+
+            switch (keep.OriginalRealm)
+            {
+                case eRealm.Albion:
+                    Console.WriteLine($"alb weight {objectiveWeight}");
+                    List<ConquestObjective> albKeepsSort =
+                        new List<ConquestObjective>(keepDict.Keys.Where(x =>
+                            keepDict[x] == objectiveWeight)); //get a list of all keeps with the current weight
+                    foreach (var tmp in albKeepsSort)
+                    {
+                        Console.WriteLine(tmp.Keep.Name);
+                    }
+                    ActiveAlbionObjective =
+                        albKeepsSort[Util.Random(albKeepsSort.Count() - 1)]; //pick one at random
+                    break;
+                case eRealm.Hibernia:
+                    Console.WriteLine($"hib weight {objectiveWeight}");
+                    List<ConquestObjective> hibKeepsSort = new List<ConquestObjective>(keepDict.Keys.Where(x =>
+                        keepDict[x] == objectiveWeight)); //get a list of all keeps with the current weight
+                    foreach (var tmp in hibKeepsSort)
+                    {
+                        Console.WriteLine(tmp.Keep.Name);
+                    }
+                    ActiveHiberniaObjective =
+                        hibKeepsSort[Util.Random(hibKeepsSort.Count() - 1)]; //pick one at random
+                    break;
+                case eRealm.Midgard:
+                    Console.WriteLine($"mid weight {objectiveWeight}");
+                    List<ConquestObjective> midKeepsSort = new List<ConquestObjective>(keepDict.Keys.Where(x =>
+                        keepDict[x] == objectiveWeight)); //get a list of all keeps with the current weight
+                    foreach (var tmp in midKeepsSort)
+                    {
+                        Console.WriteLine(tmp.Keep.Name);
+                    }
+                    ActiveMidgardObjective =
+                        midKeepsSort[Util.Random(midKeepsSort.Count() - 1)]; //pick one at random
+                    break;
+            }
+            
+            Console.WriteLine($"H Conq Targ {ActiveHiberniaObjective?.Keep?.Name}");
+            Console.WriteLine($"A Conq Targ {ActiveAlbionObjective?.Keep?.Name}");
+            Console.WriteLine($"M Conq Targ {ActiveMidgardObjective?.Keep?.Name}");
         }
         else
         {
-            SetKeepForRealm(keep.Realm);
+            SetDefensiveKeepForRealm(keep.Realm);
         }
-        
     }
 
-    private void SetKeepForRealm(eRealm realm, int minimumValue)
+    private void SetDefensiveKeepForRealm(eRealm realm, int minimumValue)
     {
         Dictionary<ConquestObjective, int> keepDict = new Dictionary<ConquestObjective, int>();
         switch (realm)
         {
             case eRealm.Albion:
                 keepDict = _albionObjectives;
+                AlbStreak = 0;
                 break;
             case eRealm.Hibernia:
                 keepDict = _hiberniaObjectives;
+                HibStreak = 0;
                 break;
             case eRealm.Midgard:
                 keepDict = _midgardObjectives;
+                MidStreak = 0;
                 break;
         }
 
@@ -293,10 +406,12 @@ public class ConquestManager
                 }
                 else
                 {
-                    List<ConquestObjective> albKeepsSort = new List<ConquestObjective>(keepDict.Keys.Where(x => keepDict[x] == objectiveWeight && x.Keep.OriginalRealm != x.Keep.Realm)); //get a list of all keeps with the current weight
-                    ActiveAlbionObjective = albKeepsSort[Util.Random(albKeepsSort.Count()-1)]; //pick one at random
+                    List<ConquestObjective> albKeepsSort = new List<ConquestObjective>(keepDict.Keys.Where(x =>
+                        keepDict[x] == objectiveWeight &&
+                        x.Keep.OriginalRealm != x.Keep.Realm)); //get a list of all keeps with the current weight
+                    ActiveAlbionObjective = albKeepsSort[Util.Random(albKeepsSort.Count() - 1)]; //pick one at random
                 }
-                
+
                 break;
             case eRealm.Hibernia:
                 if (objectiveWeight == 1)
@@ -327,12 +442,13 @@ public class ConquestManager
 
                 break;
         }
+        Console.WriteLine($"H Def Targ {ActiveHiberniaObjective?.Keep?.Name}");
+        Console.WriteLine($"A Def Targ {ActiveAlbionObjective?.Keep?.Name}");
+        Console.WriteLine($"M Def Targ {ActiveMidgardObjective?.Keep?.Name}");
     }
 
-    private void SetKeepForRealm(eRealm realm)
+    private void SetDefensiveKeepForRealm(eRealm realm)
     {
-        SetKeepForRealm(realm, 1);
+        SetDefensiveKeepForRealm(realm, 1);
     }
-
-
 }
