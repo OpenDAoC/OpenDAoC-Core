29d7f8295d834273bdbafcc9f36c281c884e2de7
clait <stefanomus@fastmail.com>
2022-03-31 10:26:11 +0100

    Agmundr fix

--- GameServer/scripts/namedmobs/TuscarianGlacier/IcelordAgmundr.cs
+++ GameServer/scripts/namedmobs/TuscarianGlacier/IcelordAgmundr.cs
@@ -8,26 +8,32 @@ namespace DOL.GS
 {
     public class Agmundr : GameEpicBoss
     {
-        public Agmundr() : base() { }
+        public Agmundr() : base()
+        {
+        }
+
         public override int GetResist(eDamageType damageType)
         {
             switch (damageType)
             {
-                case eDamageType.Slash: return 85;// dmg reduction for melee dmg
-                case eDamageType.Crush: return 85;// dmg reduction for melee dmg
-                case eDamageType.Thrust: return 85;// dmg reduction for melee dmg
-                default: return 80;// dmg reduction for rest resists
+                case eDamageType.Slash: return 85; // dmg reduction for melee dmg
+                case eDamageType.Crush: return 85; // dmg reduction for melee dmg
+                case eDamageType.Thrust: return 85; // dmg reduction for melee dmg
+                default: return 80; // dmg reduction for rest resists
             }
         }
+
         public override double AttackDamage(InventoryItem weapon)
         {
             return base.AttackDamage(weapon) * Strength / 100;
         }
+
         public override int AttackRange
         {
             get { return 350; }
             set { }
         }
+
         public override bool HasAbility(string keyName)
         {
             if (this.IsAlive && keyName == DOL.GS.Abilities.CCImmunity)
@@ -35,19 +41,23 @@ namespace DOL.GS
 
             return base.HasAbility(keyName);
         }
+
         public override double GetArmorAF(eArmorSlot slot)
         {
             return 1000;
         }
+
         public override double GetArmorAbsorb(eArmorSlot slot)
         {
             // 85% ABS is cap.
             return 0.85;
         }
+
         public override int MaxHealth
         {
             get { return 20000; }
         }
+
         public override bool AddToWorld()
         {
             INpcTemplate npcTemplate = NpcTemplateMgr.GetTemplate(60162346);
@@ -61,7 +71,8 @@ namespace DOL.GS
             Empathy = npcTemplate.Empathy;
             Faction = FactionMgr.GetFactionByID(140);
             Faction.AddFriendFaction(FactionMgr.GetFactionByID(140));
-            RespawnInterval = ServerProperties.Properties.SET_SI_EPIC_ENCOUNTER_RESPAWNINTERVAL * 60000;//1min is 60000 miliseconds
+            RespawnInterval =
+                ServerProperties.Properties.SET_SI_EPIC_ENCOUNTER_RESPAWNINTERVAL * 60000; //1min is 60000 miliseconds
 
             GameNpcInventoryTemplate template = new GameNpcInventoryTemplate();
             template.AddNPCEquipment(eInventorySlot.TwoHandWeapon, 19, 0);
@@ -73,16 +84,17 @@ namespace DOL.GS
 
             AgmundrBrain sbrain = new AgmundrBrain();
             SetOwnBrain(sbrain);
-            LoadedFromScript = false;//load from database
+            LoadedFromScript = false; //load from database
             SaveIntoDatabase();
             base.AddToWorld();
             return true;
         }
+
         [ScriptLoadedEvent]
         public static void ScriptLoaded(DOLEvent e, object sender, EventArgs args)
         {
             GameNPC[] npcs;
-            npcs = WorldMgr.GetNPCsByNameFromRegion("Icelord Agmundr", 160, (eRealm)0);
+            npcs = WorldMgr.GetNPCsByNameFromRegion("Icelord Agmundr", 160, (eRealm) 0);
             if (npcs.Length == 0)
             {
                 log.Warn("Icelord Agmundr not found, creating it...");
@@ -94,9 +106,11 @@ namespace DOL.GS
                 TG.Realm = 0;
                 TG.Level = 78;
                 TG.Size = 70;
-                TG.CurrentRegionID = 160;//tuscaran glacier
+                TG.CurrentRegionID = 160; //tuscaran glacier
                 TG.MeleeDamageType = eDamageType.Crush;
-                TG.RespawnInterval = ServerProperties.Properties.SET_SI_EPIC_ENCOUNTER_RESPAWNINTERVAL * 60000;//1min is 60000 miliseconds
+                TG.RespawnInterval =
+                    ServerProperties.Properties.SET_SI_EPIC_ENCOUNTER_RESPAWNINTERVAL *
+                    60000; //1min is 60000 miliseconds
                 TG.Faction = FactionMgr.GetFactionByID(140);
                 TG.Faction.AddFriendFaction(FactionMgr.GetFactionByID(140));
 
@@ -119,15 +133,19 @@ namespace DOL.GS
                 TG.Brain.Start();
             }
             else
-                log.Warn("Icelord Agmundr exist ingame, remove it and restart server if you want to add by script code.");
+                log.Warn(
+                    "Icelord Agmundr exist ingame, remove it and restart server if you want to add by script code.");
         }
     }
 }
+
 namespace DOL.AI.Brain
 {
     public class AgmundrBrain : StandardMobBrain
     {
-        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly log4net.ILog log =
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public AgmundrBrain()
             : base()
         {
@@ -135,7 +153,9 @@ namespace DOL.AI.Brain
             AggroRange = 600;
             ThinkInterval = 2000;
         }
-        public static bool IsPulled = false;
+
+        private static bool IsPulled = false;
+
         public override void OnAttackedByEnemy(AttackData ad)
         {
             if (IsPulled == false)
@@ -146,88 +166,90 @@ namespace DOL.AI.Brain
                     {
                         if (npc.IsAlive && npc.PackageID == "AgmundrBaf")
                         {
-                            AddAggroListTo(npc.Brain as StandardMobBrain);// add to aggro mobs with CryptLordBaf PackageID
+                            AddAggroListTo(
+                                npc.Brain as StandardMobBrain); // add to aggro mobs with CryptLordBaf PackageID
                             IsPulled = true;
                         }
                     }
                 }
             }
+
             base.OnAttackedByEnemy(ad);
         }
+
         public override void Think()
         {
             if (!HasAggressionTable())
             {
                 //set state to RETURN TO SPAWN
                 FSM.SetCurrentState(eFSMStateType.RETURN_TO_SPAWN);
-                this.Body.Health = this.Body.MaxHealth;
+                Body.Health = Body.MaxHealth;
                 IsPulled = false;
+                LoadBAFTemplate();
             }
+
             if (Body.IsOutOfTetherRange)
             {
-                this.Body.Health = this.Body.MaxHealth;
+                Body.Health = Body.MaxHealth;
                 ClearAggroList();
             }
-            else if (Body.InCombatInLast(30 * 1000) == false && this.Body.InCombatInLast(35 * 1000))
+            else if (Body.InCombatInLast(30 * 1000) == false && Body.InCombatInLast(35 * 1000))
             {
-                this.Body.Health = this.Body.MaxHealth;
+                Body.Health = Body.MaxHealth;
             }
-            if (Body.InCombat || HasAggro || Body.AttackState == true)
+
+            if (Body.InCombat || HasAggro || Body.AttackState)
             {
-                if(Util.Chance(15))
+                if (Util.Chance(15))
                 {
                     Body.CastSpell(AgmundrDD, SkillBase.GetSpellLine(GlobalSpellsLines.Mob_Spells));
                 }
+
+                SetMobstats(); // //setting mob distance+tether+speed
             }
-            // SetMobstats();//setting mob distance+tether+speed
+
             base.Think();
         }
-        public void SetMobstats()
+
+        private void SetMobstats()
         {
-            if (Body.TargetObject != null && (Body.InCombat || HasAggro || Body.AttackState == true))//if in combat
+            foreach (GameNPC npc in WorldMgr.GetNPCsFromRegion(Body.CurrentRegionID))
             {
-                foreach (GameNPC npc in WorldMgr.GetNPCsFromRegion(Body.CurrentRegionID))
+                if (npc == null) continue;
+                if (!npc.IsAlive || npc.PackageID != "AgmundrBaf") continue;
+                if (!IsPulled || npc.TargetObject != Body.TargetObject) continue;
+                npc.MaxDistance = 10000; //set mob distance to make it reach target
+                npc.TetherRange = 10000; //set tether to not return to home
+                if (!npc.IsWithinRadius(Body.TargetObject, 100))
                 {
-                    if (npc != null)
-                    {
-                        if (npc.IsAlive && npc.PackageID == "AgmundrBaf")
-                        {
-                            if (IsPulled == true && npc.TargetObject == Body.TargetObject)
-                            {
-                                npc.MaxDistance = 10000;//set mob distance to make it reach target
-                                npc.TetherRange = 10000;//set tether to not return to home
-                                if (!npc.IsWithinRadius(Body.TargetObject, 100))
-                                {
-                                    npc.MaxSpeedBase = 300;//speed is is not near to reach target faster
-                                }
-                                else
-                                    npc.MaxSpeedBase = npc.NPCTemplate.MaxSpeed;//return speed to normal
-                            }
-                        }
-                    }
+                    npc.MaxSpeedBase = 300; //speed is is not near to reach target faster
                 }
+                else
+                    npc.MaxSpeedBase = npc.NPCTemplate.MaxSpeed; //return speed to normal
             }
-            else//if not in combat
+        }
+
+        private void LoadBAFTemplate()
+        {
             {
                 foreach (GameNPC npc in WorldMgr.GetNPCsFromRegion(Body.CurrentRegionID))
                 {
-                    if (npc != null)
+                    if (npc == null) continue;
+                    INpcTemplate npcTemplate = NpcTemplateMgr.GetTemplate(npc.NPCTemplate.TemplateId);
+                    if (npcTemplate == null)
+                        return;
+                    if (!npc.IsAlive || npc.PackageID != "AgmundrBaf") continue;
+                    if (IsPulled == false)
                     {
-                        if (npc.IsAlive && npc.PackageID == "AgmundrBaf")
-                        {
-                            if (IsPulled == false)
-                            {
-                                npc.MaxDistance = npc.NPCTemplate.MaxDistance;//return distance to normal
-                                npc.TetherRange = npc.NPCTemplate.TetherRange;//return tether to normal
-                                npc.MaxSpeedBase = npc.NPCTemplate.MaxSpeed;//return speed to normal
-                            }
-                        }
+                        npc.LoadTemplate(npcTemplate);
                     }
                 }
             }
         }
-        public Spell m_AgmundrDD;
-        public Spell AgmundrDD
+
+        private Spell m_AgmundrDD;
+
+        private Spell AgmundrDD
         {
             get
             {
@@ -248,10 +270,11 @@ namespace DOL.AI.Brain
                     spell.Type = "DirectDamageNoVariance";
                     spell.Uninterruptible = true;
                     spell.MoveCast = true;
-                    spell.DamageType = (int)eDamageType.Cold;
+                    spell.DamageType = (int) eDamageType.Cold;
                     m_AgmundrDD = new Spell(spell, 70);
                     SkillBase.AddScriptedSpell(GlobalSpellsLines.Mob_Spells, m_AgmundrDD);
                 }
+
                 return m_AgmundrDD;
             }
         }
