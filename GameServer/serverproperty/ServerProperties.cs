efb57e0aef6fcd399f0f103494930506681269a3
Anthony Knight <midgetlord123@hotmail.com>
2022-03-31 14:39:18 -0700

    Add supporting server properties for duration of conquest/overall window

--- GameServer/serverproperty/ServerProperties.cs
+++ GameServer/serverproperty/ServerProperties.cs
@@ -2627,19 +2627,31 @@ namespace DOL.GS.ServerProperties
 		/// <summary>
 		/// Max duration of a Conquest Task in minutes
 		/// </summary>
-		[ServerProperty("conquest", "max_conquest_task_duration", "Max duration of a Conquest Task in minutes", 45)] //2700000 == 45 minutes
+		[ServerProperty("conquest", "max_conquest_task_duration", "Max duration of a Conquest Task in minutes", 45)]
 		public static int MAX_CONQUEST_TASK_DURATION;
+		
+		/// <summary>
+		/// Time (in minutes) of the overall conquest window and cooldown
+		/// </summary>
+		[ServerProperty("conquest", "conquest_cycle_timer", "Time (in minutes) of the overall conquest window and cooldown", 90)]
+		public static int CONQUEST_CYCLE_TIMER;
+		
+		/// <summary>
+		/// Time (in seconds) of the duration between conquest objective point tallies
+		/// </summary>
+		[ServerProperty("conquest", "conquest_tally_interval", "Time (in seconds) of the duration between conquest objective point tallies", 300)]
+		public static int CONQUEST_TALLY_INTERVAL;
 		
 		/// <summary>
 		/// Max range to contribute to a conquest target
 		/// </summary>
-		[ServerProperty("conquest", "max_conquest_range", "Max range to contribute to a conquest target", 10000)]
+		[ServerProperty("conquest", "max_conquest_range", "Max range to contribute to a conquest target", 15000)]
 		public static int MAX_CONQUEST_RANGE;
 		
 		/// <summary>
 		/// Max reward (in RP value) for any given subtask interval
 		/// </summary>
-		[ServerProperty("conquest", "max_subtask_rp_reward", "Max reward (in RP value) for any given subtask interval", 1000)]
+		[ServerProperty("conquest", "max_subtask_rp_reward", "Max reward (in RP value) for any given subtask interval", 5000)]
 		public static int MAX_SUBTASK_RP_REWARD;
 		
 		/// <summary>
@@ -2647,6 +2659,8 @@ namespace DOL.GS.ServerProperties
 		/// </summary>
 		[ServerProperty("conquest", "max_keep_conquest_rp_reward", "Max reward (in RP value) for a keep capture", 25000)]
 		public static int MAX_KEEP_CONQUEST_RP_REWARD;
+		
+		
 
 		#endregion
 		public static IDictionary<string, object> AllCurrentProperties
