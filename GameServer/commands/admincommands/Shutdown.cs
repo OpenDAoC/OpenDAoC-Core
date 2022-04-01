3ec7bb5bf190ed42df097deca4245dabe58a75b4
clait <stefanomus@fastmail.com>
2022-04-01 17:30:14 +0100

    119 secs

--- GameServer/commands/admincommands/Shutdown.cs
+++ GameServer/commands/admincommands/Shutdown.cs
@@ -311,10 +311,10 @@ namespace DOL.GS.Commands
 						ChatUtil.SendDebugMessage(client, "AdminCommands.Account.Msg.ServerClosed", secs);
 						ChatUtil.SendDebugMessage(client, "AdminCommands.Account.Msg.ServerClosed", secs);
 					}
-					
-					if (Properties.DISCORD_ACTIVE && (!string.IsNullOrEmpty(Properties.DISCORD_WEBHOOK_ID)))
-					{
-
+				}
+				
+				if (secs == 119 && GameServer.Instance.ServerStatus != eGameServerStatus.GSS_Closed && Properties.DISCORD_ACTIVE && (!string.IsNullOrEmpty(Properties.DISCORD_WEBHOOK_ID))) // 2 mins remaining
+				{
 						var discordClient = new DiscordWebhookClient(Properties.DISCORD_WEBHOOK_ID);
 						// var discordClient = new DiscordWebhookClient("https://discord.com/api/webhooks/928723074898075708/cyZbVefc0gc__9c2wq3DwVxOBFIT45VyK-1-z7tT_uXDd--WcHrY1lw1y9H6wPg6SEyM");
 					
@@ -327,15 +327,13 @@ namespace DOL.GS.Commands
 							{
 								new DiscordMessageEmbed(
 									color: 15158332,
-									description: $"The server will reboot in **2 minutes** and is temporarily not accepting new incoming connections!\n Stay tuned for the patch notes.",
+									description: "The server will reboot in **2 minutes** and is temporarily not accepting new incoming connections!\n Stay tuned for the patch notes.",
 									thumbnail: new DiscordMessageEmbedThumbnail("https://cdn.discordapp.com/emojis/893545614942564412.webp")
 								)
 							}
 						);
 
 						discordClient.SendToDiscord(message);
-					}
-					
 				}
 			}
 		}
