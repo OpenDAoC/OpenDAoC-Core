01aad79226a6aed425167f682c0c1f62060fc89c
Glimmer Warshade <glimmer@europe.com>
2022-04-03 21:05:53 +0200

    Weno'iak the Enlightened script

---
+++ GameServer/scripts/namedmobs/AvalonCity/WenoiakEnlightened.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using DOL.AI.Brain;
+using DOL.Database;
+using DOL.GS.PacketHandler;
+using DOL.Events;
+using DOL.GS;
+
+namespace DOL.GS
+{
+	public class WenoiakEnlightened : GameEpicBoss
+	{
+		public WenoiakEnlightened() : base() { }
+
+		[ScriptLoadedEvent]
+		public static void ScriptLoaded(DOLEvent e, object sender, EventArgs args)
+		{
+			if (log.IsInfoEnabled)
+				log.Info("Weno'iak the Enlightened Initializing...");
+		}
+		public override int GetResist(eDamageType damageType)
+		{
+			switch (damageType)
+			{
+				case eDamageType.Slash: return 60;// dmg reduction for melee dmg
+				case eDamageType.Crush: return 60;// dmg reduction for melee dmg
+				case eDamageType.Thrust: return 60;// dmg reduction for melee dmg
+				default: return 40;// dmg reduction for rest resists
+			}
+		}
+		public override double AttackDamage(InventoryItem weapon)
+		{
+			return base.AttackDamage(weapon) * Strength / 100;
+		}
+		public override int AttackRange
+		{
+			get { return 350; }
+			set { }
+		}
+		public override bool HasAbility(string keyName)
+		{
+			if (IsAlive && keyName == DOL.GS.Abilities.CCImmunity)
+				return true;
+
+			return base.HasAbility(keyName);
+		}
+		public override double GetArmorAF(eArmorSlot slot)
+		{
+			return 1000;
+		}
+		public override double GetArmorAbsorb(eArmorSlot slot)
+		{
+			// 85% ABS is cap.
+			return 0.85;
+		}
+		public override int MaxHealth
+		{
+			get { return 15000; }
+		}
+		public override bool AddToWorld()
+		{
+			INpcTemplate npcTemplate = NpcTemplateMgr.GetTemplate(60167846);
+			LoadTemplate(npcTemplate);
+			Strength = npcTemplate.Strength;
+			Dexterity = npcTemplate.Dexterity;
+			Constitution = npcTemplate.Constitution;
+			Quickness = npcTemplate.Quickness;
+			Piety = npcTemplate.Piety;
+			Intelligence = npcTemplate.Intelligence;
+			Empathy = npcTemplate.Empathy;
+			RespawnInterval = ServerProperties.Properties.SET_SI_EPIC_ENCOUNTER_RESPAWNINTERVAL * 60000;//1min is 60000 miliseconds
+
+			Faction = FactionMgr.GetFactionByID(13);
+			Faction.AddFriendFaction(FactionMgr.GetFactionByID(13));
+
+			WenoiakEnlightenedBrain sbrain = new WenoiakEnlightenedBrain();
+			SetOwnBrain(sbrain);
+			LoadedFromScript = false;//load from database
+			SaveIntoDatabase();
+			base.AddToWorld();
+			return true;
+		}
+	}
+}
+namespace DOL.AI.Brain
+{
+	public class WenoiakEnlightenedBrain : StandardMobBrain
+	{
+		private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		public WenoiakEnlightenedBrain() : base()
+		{
+			AggroLevel = 100;
+			AggroRange = 600;
+			ThinkInterval = 1500;
+		}
+		public static bool IsPulled = false;
+		public override void Think()
+		{
+			if (!HasAggressionTable())
+			{
+				//set state to RETURN TO SPAWN
+				FSM.SetCurrentState(eFSMStateType.RETURN_TO_SPAWN);
+				Body.Health = Body.MaxHealth;
+				IsPulled = false;
+			}
+			if (Body.InCombat && Body.IsAlive && HasAggro)
+			{
+				if (IsPulled == false)
+				{
+					foreach (GameNPC npc in WorldMgr.GetNPCsFromRegion(Body.CurrentRegionID))
+					{
+						if (npc != null)
+						{
+							if (npc.IsAlive && npc.PackageID == "WenoiakBaf")
+							{
+								AddAggroListTo(npc.Brain as StandardMobBrain); // add to aggro mobs with IssordenBaf PackageID
+							}
+						}
+					}
+					IsPulled = true;
+				}
+				if (Util.Chance(25))
+					Body.CastSpell(Light_dd, SkillBase.GetSpellLine(GlobalSpellsLines.Mob_Spells));
+				if (Util.Chance(25))
+					Body.CastSpell(Light_pbaoe, SkillBase.GetSpellLine(GlobalSpellsLines.Mob_Spells));
+			}
+			base.Think();
+		}
+		private Spell m_Light_dd;
+		private Spell Light_dd
+		{
+			get
+			{
+				if (m_Light_dd == null)
+				{
+					DBSpell spell = new DBSpell();
+					spell.AllowAdd = false;
+					spell.CastTime = 3;
+					spell.RecastDelay = Util.Random(10,15);
+					spell.ClientEffect = 1678;
+					spell.Icon = 1678;
+					spell.TooltipId = 1678;
+					spell.Damage = 180;
+					spell.Name = "Weno'iak Lighs";
+					spell.Range = 1500;
+					spell.SpellID = 11797;
+					spell.Target = "Enemy";
+					spell.Type = eSpellType.DirectDamageNoVariance.ToString();
+					spell.Uninterruptible = true;
+					spell.DamageType = (int)eDamageType.Spirit;
+					m_Light_dd = new Spell(spell, 70);
+					SkillBase.AddScriptedSpell(GlobalSpellsLines.Mob_Spells, m_Light_dd);
+				}
+				return m_Light_dd;
+			}
+		}
+
+		private Spell m_Light_pbaoe;
+		private Spell Light_pbaoe
+		{
+			get
+			{
+				if (m_Light_pbaoe == null)
+				{
+					DBSpell spell = new DBSpell();
+					spell.AllowAdd = false;
+					spell.CastTime = 0;
+					spell.RecastDelay = Util.Random(20, 25);
+					spell.ClientEffect = 1666;
+					spell.Icon = 1666;
+					spell.TooltipId = 1666;
+					spell.Damage = 220;
+					spell.Name = "Weno'iak's Annihilate";
+					spell.Range = 0;
+					spell.Radius = 400;
+					spell.SpellID = 11798;
+					spell.Target = "Enemy";
+					spell.Type = eSpellType.DirectDamageNoVariance.ToString();
+					spell.Uninterruptible = true;
+					spell.DamageType = (int)eDamageType.Spirit;
+					m_Light_pbaoe = new Spell(spell, 70);
+					SkillBase.AddScriptedSpell(GlobalSpellsLines.Mob_Spells, m_Light_pbaoe);
+				}
+				return m_Light_pbaoe;
+			}
+		}
+	}
+}
+
+
+
