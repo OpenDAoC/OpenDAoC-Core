e8043f5b777b2a7e9e53cedb9a25b885f0c328b8
Glimmer Warshade <glimmer@europe.com>
2022-04-03 19:23:58 +0200

    Vera'erius the Brave script

---
+++ GameServer/scripts/namedmobs/AvalonCity/VeraeriusBrave.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using DOL.AI.Brain;
+using DOL.Database;
+using DOL.GS.PacketHandler;
+using DOL.Events;
+using DOL.GS;
+
+namespace DOL.GS
+{
+	public class VeraeriusBrave : GameEpicBoss
+	{
+		public VeraeriusBrave() : base() { }
+
+		[ScriptLoadedEvent]
+		public static void ScriptLoaded(DOLEvent e, object sender, EventArgs args)
+		{
+			if (log.IsInfoEnabled)
+				log.Info("Vera'erius the Brave Initializing...");
+		}
+		public override int GetResist(eDamageType damageType)
+		{
+			switch (damageType)
+			{
+				case eDamageType.Slash: return 60;// dmg reduction for melee dmg
+				case eDamageType.Crush: return 60;// dmg reduction for melee dmg
+				case eDamageType.Thrust: return 60;// dmg reduction for melee dmg
+				default: return 40;// dmg reduction for rest resists
+			}
+		}
+
+		public override void TakeDamage(GameObject source, eDamageType damageType, int damageAmount, int criticalAmount)
+		{
+			if (source is GamePlayer || source is GamePet)
+			{
+				if (damageType == eDamageType.Spirit || damageType == eDamageType.Body)
+				{
+					GamePlayer truc;
+					if (source is GamePlayer)
+						truc = (source as GamePlayer);
+					else
+						truc = ((source as GamePet).Owner as GamePlayer);
+					if (truc != null)
+						truc.Out.SendMessage("Your damage is absorbed and used to heal " + Name, eChatType.CT_System, eChatLoc.CL_ChatWindow);
+					Health += damageAmount + criticalAmount;
+					base.TakeDamage(source, damageType, 0, 0);
+					return;
+
+				}
+				else//take dmg
+				{
+					base.TakeDamage(source, damageType, damageAmount, criticalAmount);
+				}
+			}
+		}
+		public override double AttackDamage(InventoryItem weapon)
+		{
+			return base.AttackDamage(weapon) * Strength / 100;
+		}
+		public override int AttackRange
+		{
+			get { return 350; }
+			set { }
+		}
+		public override bool HasAbility(string keyName)
+		{
+			if (IsAlive && keyName == DOL.GS.Abilities.CCImmunity)
+				return true;
+
+			return base.HasAbility(keyName);
+		}
+		public override double GetArmorAF(eArmorSlot slot)
+		{
+			return 1000;
+		}
+		public override double GetArmorAbsorb(eArmorSlot slot)
+		{
+			// 85% ABS is cap.
+			return 0.85;
+		}
+		public override int MaxHealth
+		{
+			get { return 15000; }
+		}
+		public override bool AddToWorld()
+		{
+			INpcTemplate npcTemplate = NpcTemplateMgr.GetTemplate(60167614);
+			LoadTemplate(npcTemplate);
+			Strength = npcTemplate.Strength;
+			Dexterity = npcTemplate.Dexterity;
+			Constitution = npcTemplate.Constitution;
+			Quickness = npcTemplate.Quickness;
+			Piety = npcTemplate.Piety;
+			Intelligence = npcTemplate.Intelligence;
+			Empathy = npcTemplate.Empathy;
+			RespawnInterval = ServerProperties.Properties.SET_SI_EPIC_ENCOUNTER_RESPAWNINTERVAL * 60000;//1min is 60000 miliseconds
+
+			Faction = FactionMgr.GetFactionByID(12);
+			Faction.AddFriendFaction(FactionMgr.GetFactionByID(12));
+
+			VeraeriusBraveBrain sbrain = new VeraeriusBraveBrain();
+			SetOwnBrain(sbrain);
+			LoadedFromScript = false;//load from database
+			SaveIntoDatabase();
+			base.AddToWorld();
+			return true;
+		}
+	}
+}
+namespace DOL.AI.Brain
+{
+	public class VeraeriusBraveBrain : StandardMobBrain
+	{
+		private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		public VeraeriusBraveBrain() : base()
+		{
+			AggroLevel = 100;
+			AggroRange = 600;
+			ThinkInterval = 1500;
+		}
+		public static bool IsPulled = false;
+		public override void Think()
+		{
+			if (!HasAggressionTable())
+			{
+				//set state to RETURN TO SPAWN
+				FSM.SetCurrentState(eFSMStateType.RETURN_TO_SPAWN);
+				Body.Health = Body.MaxHealth;
+				IsPulled = false;
+			}
+			if (Body.InCombat && Body.IsAlive && HasAggro)
+			{
+				if (IsPulled == false)
+				{
+					foreach (GameNPC npc in WorldMgr.GetNPCsFromRegion(Body.CurrentRegionID))
+					{
+						if (npc != null)
+						{
+							if (npc.IsAlive && npc.PackageID == "VeraeriusBaf")
+							{
+								AddAggroListTo(npc.Brain as StandardMobBrain); // add to aggro mobs with IssordenBaf PackageID
+							}
+						}
+					}
+					IsPulled = true;
+				}
+				if(Body.HealthPercent <= 70)
+                {
+					Body.CastSpell(VeraeriusHeal, SkillBase.GetSpellLine(GlobalSpellsLines.Mob_Spells));
+				}
+			}
+			base.Think();
+		}
+		private Spell m_VeraeriusHeal;
+		private Spell VeraeriusHeal
+		{
+			get
+			{
+				if (m_VeraeriusHeal == null)
+				{
+					DBSpell spell = new DBSpell();
+					spell.AllowAdd = false;
+					spell.CastTime = 3;
+					spell.RecastDelay = 8;
+					spell.ClientEffect = 1340;
+					spell.Icon = 1340;
+					spell.TooltipId = 1340;
+					spell.Value = 300;
+					spell.Name = "Vera'erius Heal";
+					spell.Range = 1500;
+					spell.SpellID = 11796;
+					spell.Target = "Self";
+					spell.Type = eSpellType.Heal.ToString();
+					spell.Uninterruptible = true;
+					m_VeraeriusHeal = new Spell(spell, 70);
+					SkillBase.AddScriptedSpell(GlobalSpellsLines.Mob_Spells, m_VeraeriusHeal);
+				}
+				return m_VeraeriusHeal;
+			}
+		}
+	}
+}
+
+
