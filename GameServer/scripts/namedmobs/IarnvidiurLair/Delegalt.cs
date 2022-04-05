a57fa5c1fd2f193bd655821962453df674976767
Glimmer Warshade <glimmer@europe.com>
2022-04-05 13:31:02 +0200

    Delegalt script

---
+++ GameServer/scripts/namedmobs/IarnvidiurLair/Delegalt.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using DOL.AI.Brain;
+using DOL.Database;
+using DOL.GS;
+using DOL.GS.PacketHandler;
+using DOL.Events;
+
+namespace DOL.GS
+{
+	public class Delegalt : GameEpicBoss
+	{
+		public Delegalt() : base() { }
+
+		[ScriptLoadedEvent]
+		public static void ScriptLoaded(DOLEvent e, object sender, EventArgs args)
+		{
+			if (log.IsInfoEnabled)
+				log.Info("Delegalt Initializing...");
+		}
+		public override int GetResist(eDamageType damageType)
+		{
+			switch (damageType)
+			{
+				case eDamageType.Slash: return 60;// dmg reduction for melee dmg
+				case eDamageType.Crush: return 60;// dmg reduction for melee dmg
+				case eDamageType.Thrust: return 60;// dmg reduction for melee dmg
+				default: return 50;// dmg reduction for rest resists
+			}
+		}
+		public override void OnAttackEnemy(AttackData ad) //on enemy actions
+		{
+			if (Util.Chance(35))
+			{
+				if (ad != null && (ad.AttackResult == eAttackResult.HitUnstyled || ad.AttackResult == eAttackResult.HitStyle))
+				{
+					this.CastSpell(DelegaltDisease, SkillBase.GetSpellLine(GlobalSpellsLines.Mob_Spells));
+				}
+			}
+			base.OnAttackEnemy(ad);
+		}
+		public override double AttackDamage(InventoryItem weapon)
+		{
+			return base.AttackDamage(weapon) * Strength / 100;
+		}
+		public override int AttackRange
+		{
+			get { return 350; }
+			set { }
+		}
+		public override bool HasAbility(string keyName)
+		{
+			if (IsAlive && keyName == DOL.GS.Abilities.CCImmunity)
+				return true;
+
+			return base.HasAbility(keyName);
+		}
+		public override double GetArmorAF(eArmorSlot slot)
+		{
+			return 1000;
+		}
+		public override double GetArmorAbsorb(eArmorSlot slot)
+		{
+			// 85% ABS is cap.
+			return 0.85;
+		}
+		public override int MaxHealth
+		{
+			get { return 15000; }
+		}
+		public override bool AddToWorld()
+		{
+			INpcTemplate npcTemplate = NpcTemplateMgr.GetTemplate(83019);
+			LoadTemplate(npcTemplate);
+			Strength = npcTemplate.Strength;
+			Dexterity = npcTemplate.Dexterity;
+			Constitution = npcTemplate.Constitution;
+			Quickness = npcTemplate.Quickness;
+			Piety = npcTemplate.Piety;
+			Intelligence = npcTemplate.Intelligence;
+			Empathy = npcTemplate.Empathy;
+			RespawnInterval = ServerProperties.Properties.SET_SI_EPIC_ENCOUNTER_RESPAWNINTERVAL * 60000;//1min is 60000 miliseconds
+
+			Faction = FactionMgr.GetFactionByID(159);
+			Faction.AddFriendFaction(FactionMgr.GetFactionByID(159));
+
+			DelegaltBrain sbrain = new DelegaltBrain();
+			SetOwnBrain(sbrain);
+			LoadedFromScript = false;
+			SaveIntoDatabase();
+			base.AddToWorld();
+			return true;
+		}
+		private Spell m_DelegaltDisease;
+		private Spell DelegaltDisease
+		{
+			get
+			{
+				if (m_DelegaltDisease == null)
+				{
+					DBSpell spell = new DBSpell();
+					spell.AllowAdd = false;
+					spell.CastTime = 0;
+					spell.RecastDelay = 2;
+					spell.ClientEffect = 4375;
+					spell.Icon = 4375;
+					spell.Name = "Black Plague";
+					spell.Message1 = "You are diseased!";
+					spell.Message2 = "{0} is diseased!";
+					spell.Message3 = "You look healthy.";
+					spell.Message4 = "{0} looks healthy again.";
+					spell.TooltipId = 4375;
+					spell.Range = 350;
+					spell.Duration = 650;
+					spell.SpellID = 11803;
+					spell.Target = "Enemy";
+					spell.Type = "Disease";
+					spell.Uninterruptible = true;
+					spell.MoveCast = true;
+					spell.DamageType = (int)eDamageType.Energy; //Energy DMG Type
+					m_DelegaltDisease = new Spell(spell, 70);
+					SkillBase.AddScriptedSpell(GlobalSpellsLines.Mob_Spells, m_DelegaltDisease);
+				}
+				return m_DelegaltDisease;
+			}
+		}
+	}
+}
+namespace DOL.AI.Brain
+{
+	public class DelegaltBrain : StandardMobBrain
+	{
+		private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		public DelegaltBrain() : base()
+		{
+			AggroLevel = 100;
+			AggroRange = 600;
+			ThinkInterval = 1500;
+		}
+		public static bool IsPulled = false;
+		public override void Think()
+		{
+			if (!HasAggressionTable())
+			{
+				//set state to RETURN TO SPAWN
+				FSM.SetCurrentState(eFSMStateType.RETURN_TO_SPAWN);
+				Body.Health = Body.MaxHealth;
+				IsPulled = false;
+			}
+			if (Body.InCombat && HasAggro)
+			{
+				if (IsPulled == false)
+				{
+					foreach (GameNPC npc in WorldMgr.GetNPCsFromRegion(Body.CurrentRegionID))
+					{
+						if (npc != null)
+						{
+							if (npc.IsAlive && npc.PackageID == "DelegaltBaf")
+							{
+								AddAggroListTo(npc.Brain as StandardMobBrain);
+							}
+						}
+					}
+					IsPulled = true;
+				}
+			}
+			base.Think();
+		}
+	}
+}
