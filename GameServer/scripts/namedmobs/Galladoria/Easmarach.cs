5b8f9938f5fac139ad8db0d927e05217186d4e75
Glimmer Warshade <glimmer@europe.com>
2022-04-08 21:44:03 +0200

    Easmarach AF+Abs fix

--- GameServer/scripts/namedmobs/Galladoria/Easmarach.cs
+++ GameServer/scripts/namedmobs/Galladoria/Easmarach.cs
@@ -61,13 +61,13 @@ namespace DOL.GS
 
         public override double GetArmorAF(eArmorSlot slot)
         {
-            return 1000;
+            return 850;
         }
 
         public override double GetArmorAbsorb(eArmorSlot slot)
         {
             // 85% ABS is cap.
-            return 0.85;
+            return 0.55;
         }
 
         public override bool AddToWorld()
