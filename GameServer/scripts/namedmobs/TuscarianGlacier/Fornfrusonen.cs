980c759c728ef54b597c7767a17c58642436bb8a
Glimmer Warshade <glimmer@europe.com>
2022-04-08 22:22:18 +0200

    Fornfrusonen AF+Abs fix

--- GameServer/scripts/namedmobs/TuscarianGlacier/Fornfrusonen.cs
+++ GameServer/scripts/namedmobs/TuscarianGlacier/Fornfrusonen.cs
@@ -34,13 +34,13 @@ namespace DOL.GS
 
         public override double GetArmorAF(eArmorSlot slot)
         {
-            return 1000;
+            return 800;
         }
 
         public override double GetArmorAbsorb(eArmorSlot slot)
         {
             // 85% ABS is cap.
-            return 0.85;
+            return 0.55;
         }
 
         public override int MaxHealth
@@ -306,13 +306,13 @@ namespace DOL.GS
 
         public override double GetArmorAF(eArmorSlot slot)
         {
-            return 800;
+            return 500;
         }
 
         public override double GetArmorAbsorb(eArmorSlot slot)
         {
             // 85% ABS is cap.
-            return 0.65;
+            return 0.35;
         }
 
         public override int MaxHealth
