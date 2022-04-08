f6a995a4dd2f84966d8f4735f1d0d78523d63042
Glimmer Warshade <glimmer@europe.com>
2022-04-08 21:40:40 +0200

    Jarl Ormarr AF+Abs fix

--- GameServer/scripts/namedmobs/Dodens/JarlOrmarr.cs
+++ GameServer/scripts/namedmobs/Dodens/JarlOrmarr.cs
@@ -87,13 +87,13 @@ namespace DOL.GS.Scripts
 		}
 		public override double GetArmorAF(eArmorSlot slot)
 		{
-			return 1000;
+			return 800;
 		}
 
 		public override double GetArmorAbsorb(eArmorSlot slot)
 		{
 			// 85% ABS is cap.
-			return 0.85;
+			return 0.55;
 		}
 		
 		/// <summary>
