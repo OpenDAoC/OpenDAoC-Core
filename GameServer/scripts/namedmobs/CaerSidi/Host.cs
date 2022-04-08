72cd82cc486d5893cdb5cfb41affec103925430b
Glimmer Warshade <glimmer@europe.com>
2022-04-08 21:20:13 +0200

    Host AF + Abs fix

--- GameServer/scripts/namedmobs/CaerSidi/Host.cs
+++ GameServer/scripts/namedmobs/CaerSidi/Host.cs
@@ -254,12 +254,12 @@ namespace DOL.GS
         }
         public override double GetArmorAF(eArmorSlot slot)
         {
-            return 1000;
+            return 750;
         }
         public override double GetArmorAbsorb(eArmorSlot slot)
         {
             // 85% ABS is cap.
-            return 0.75;
+            return 0.55;
         }
 
         public override void Die(GameObject killer)
