803996ec2422ffb758591665a94af6de710e87f6
clait <stefanomus@fastmail.com>
2022-04-24 16:18:30 +0100

    Turns out it's more similar to a theurgist pet than a FnF - still quite not working alright though

--- GameServer/ai/brain/Animist/ForestheartAmbusherBrain.cs
+++ GameServer/ai/brain/Animist/ForestheartAmbusherBrain.cs
@@ -1,105 +1,73 @@
-using System.Collections;
-using System.Collections.Generic;
 using DOL.GS;
-using DOL.GS.Spells;
+using log4net;
+using System.Reflection;
+
 
 namespace DOL.AI.Brain
 {
-	public class ForestheartAmbusherBrain : TurretFNFBrain
+	public class ForestheartAmbusherBrain : TheurgistPetBrain, IControlledBrain
 	{
-		private static log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
+		private GameLiving m_owner;
+		private GameLiving m_target;
+		private bool m_melee = true;
+		private bool m_active = true;
+
+		private ushort m_range = 2000;
+		public bool Melee { get { return m_melee; } set { m_melee = value; } }
 		public ForestheartAmbusherBrain(GameLiving owner) : base(owner)
 		{
+			if (owner != null)
+			{
+				m_owner = owner;
+			}
+			AggroLevel = 100;
+			AggroRange = 2000;
+			IsMainPet = false;
 		}
 
-		/// <summary>
-		/// Get a random target from aggro table
-		/// </summary>
-		/// <returns></returns>
-		protected override GameLiving CalculateNextAttackTarget()
+		public virtual GameNPC GetNPCOwner()
 		{
-			List<GameLiving> newTargets = new List<GameLiving>();
-			List<GameLiving> oldTargets = new List<GameLiving>();
-			base.CalculateNextAttackTarget();
-			lock((m_aggroTable as ICollection).SyncRoot)
-			{
-				foreach(GameLiving living in m_aggroTable.Keys)
-				{
-					if(!living.IsAlive || living.CurrentRegion != Body.CurrentRegion || living.ObjectState != GameObject.eObjectState.Active)
-						continue;
-
-					if (living.IsMezzed || living.IsStealthed)
-						continue;
-
-					if (!Body.IsWithinRadius(living, MAX_AGGRO_DISTANCE, true))
-						continue;
-
-					newTargets.Add(living);
-				}
-			}
-
-			foreach (GamePlayer living in Body.GetPlayersInRadius((ushort)((TurretPet)Body).TurretSpell.Range, Body.CurrentRegion.IsDungeon ? false : true))
-            {
-                if (!GameServer.ServerRules.IsAllowedToAttack(Body, living, true))
-                    continue;
-
-                if (living.IsInvulnerableToAttack)
-                    continue;
-
-                if (!living.IsAlive || living.CurrentRegion != Body.CurrentRegion || living.ObjectState != GameObject.eObjectState.Active)
-                    continue;
-
-                if (living.IsMezzed || living.IsStealthed)
-                    continue;
-                
-	            newTargets.Add(living);
-				
-            }
-
-			foreach (GameNPC living in Body.GetNPCsInRadius((ushort)((TurretPet)Body).TurretSpell.Range, Body.CurrentRegion.IsDungeon ? false : true))
-            {
-                if (!GameServer.ServerRules.IsAllowedToAttack(Body, living, true))
-                    continue;
-
-                if (!living.IsAlive || living.CurrentRegion != Body.CurrentRegion || living.ObjectState != GameObject.eObjectState.Active)
-                    continue;
-
-                if (living.IsMezzed || living.IsStealthed)
-                    continue;
-
-                if (Body.GetConLevel(living) <= -3)
-	                continue;
-
-				newTargets.Add(living);
-				
-			}
-
-			// always favor previous targets and new targets that have not been attacked first, then re-attack old targets
-
-            if (newTargets.Count > 0)
-			{
-				return newTargets[Util.Random(newTargets.Count - 1)];
-			}
-			else if (oldTargets.Count > 0)
-			{
-				return oldTargets[Util.Random(oldTargets.Count - 1)];
-			}
-
-			m_aggroTable.Clear();
-			return null;
+		    return null;
 		}
-
-		public override void OnAttackedByEnemy(AttackData ad)
+		public virtual GameLiving GetLivingOwner()
 		{
-			AddToAggroList(ad.Attacker, (ad.Attacker.Level + 1) << 1);
+		    GamePlayer player = GetPlayerOwner();
+		    if (player != null)
+				return player;
+
+		    GameNPC npc = GetNPCOwner();
+		    if (npc != null)
+				return npc;
+
+		    return null;
 		}
 
-		/// <summary>
-    /// Updates the pet window
-    /// </summary>
-    public override void UpdatePetWindow()
-    {
-    }
-  }
+		public override int ThinkInterval { get { return 1500; } }
+
+		public override void Think() { AttackMostWanted(); }
+
+		public void SetAggressionState(eAggressionState state) { }
+		
+		public override bool CheckSpells(eCheckSpellType type)
+		{
+			return false;
+		}
+
+		#region IControlledBrain Members
+		public eWalkState WalkState { get { return WalkState; } }
+		public eAggressionState AggressionState { get { return eAggressionState.Aggressive; } set { } }
+		public GameLiving Owner { get { return m_owner; } }
+		public void Attack(GameObject target) { }
+		public void Follow(GameObject target) { }
+		public void FollowOwner() { }
+		public void Stay() { }
+		public void ComeHere() { }
+		public void Goto(GameObject target) { }
+		public void UpdatePetWindow() { }
+		public GamePlayer GetPlayerOwner() { return m_owner as GamePlayer; }
+		public bool IsMainPet { get { return false; } set { } }
+		#endregion
+	}
 }
--- GameServer/spells/Animist/SummonAnimistAmbusher.cs
+++ GameServer/spells/Animist/SummonAnimistAmbusher.cs
@@ -1,29 +1,9 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
 using System;
 using DOL.Events;
 using DOL.GS.PacketHandler;
 using DOL.AI.Brain;
 using DOL.GS.Effects;
-using DOL.GS.ServerProperties;
 using DOL.Language;
-using System.Linq;
 
 namespace DOL.GS.Spells
 {
@@ -31,7 +11,7 @@ namespace DOL.GS.Spells
 	/// Summon a fnf animist pet.
 	/// </summary>
 	[SpellHandler("SummonAnimistAmbusher")]
-	public class SummonAnimistAmbusher : SummonAnimistFnF
+	public class SummonAnimistAmbusher : SummonTheurgistPet
 	{
 		public SummonAnimistAmbusher(GameLiving caster, Spell spell, SpellLine line) : base(caster, spell, line) { }
 
@@ -39,35 +19,37 @@ namespace DOL.GS.Spells
 		{
 			Region rgn = WorldMgr.GetRegion(Caster.CurrentRegion.ID);
 
-			if (rgn == null || rgn.GetZone(Caster.GroundTarget.X, Caster.GroundTarget.Y) == null)
-			{
-                if (Caster is GamePlayer)
-                    MessageToCaster(LanguageMgr.GetTranslation((Caster as GamePlayer).Client, "SummonAnimistFnF.CheckBeginCast.NoGroundTarget"), eChatType.CT_SpellResisted);
-                return false;
-			}
+			if (rgn?.GetZone(Caster.GroundTarget.X, Caster.GroundTarget.Y) != null)
+				return base.CheckBeginCast(selectedTarget);
+			if (Caster is GamePlayer)
+				MessageToCaster(LanguageMgr.GetTranslation((Caster as GamePlayer).Client, "SummonAnimistFnF.CheckBeginCast.NoGroundTarget"), eChatType.CT_SpellResisted);
+			return false;
 
-			return base.CheckBeginCast(selectedTarget);
-		}
-
-		public override void ApplyEffectOnTarget(GameLiving target, double effectiveness)
-		{
-			base.ApplyEffectOnTarget(target, effectiveness);
-
-			if (Spell.SubSpellID > 0 && m_pet.Spells != null && SkillBase.GetSpellByID(Spell.SubSpellID) != null)
-			{
-				m_pet.Spells.Add(SkillBase.GetSpellByID(Spell.SubSpellID));
-			}
-			
-			(m_pet.Brain as TurretBrain).IsMainPet = false;
-
-			(m_pet.Brain as IOldAggressiveBrain).AddToAggroList(target, 1);
-			(m_pet.Brain as TurretBrain).Think();
-			
-			Caster.PetCount++;
 		}
 
 		protected override void SetBrainToOwner(IControlledBrain brain)
 		{
+		}
+		
+		public override void ApplyEffectOnTarget(GameLiving target, double effectiveness)
+		{
+			base.ApplyEffectOnTarget(target, effectiveness);
+
+			m_pet.TempProperties.setProperty("target", target);
+			(m_pet.Brain as IOldAggressiveBrain).AddToAggroList(target, 1);
+			(m_pet.Brain as ForestheartAmbusherBrain).Think();
+
+			Caster.PetCount++;
+		}
+		
+		protected override void GetPetLocation(out int x, out int y, out int z, out ushort heading, out Region region)
+		{
+			x = Caster.GroundTarget.X;
+			y = Caster.GroundTarget.Y;
+			z = Caster.GroundTarget.Z;
+			region = Caster.CurrentRegion;
+			
+			heading = Caster.Heading;
 		}
 
 		/// <summary>
