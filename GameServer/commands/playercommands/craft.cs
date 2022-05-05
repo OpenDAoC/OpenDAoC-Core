fb0d7e988f15cedb21c636ae238137abcbad2fb7
clait <stefanomus@fastmail.com>
2022-05-05 15:11:11 +0100

    `/craft buyto #` & cleanup

--- GameServer/commands/playercommands/craft.cs
+++ GameServer/commands/playercommands/craft.cs
@@ -1,115 +1,195 @@
-﻿using System;
-using DOL.Database;
-using DOL.GS.PacketHandler;
+﻿using DOL.Database;
 
 namespace DOL.GS.Commands
 {
-	[CmdAttribute(
-		"&craft",
-		ePrivLevel.Player,
-		"Crafting macros and utilities",
-		"'/craft set <#>' to set how many items you want to craft",
-		"'/craft buy' to buy the necessary materials to craft one item",
-		"'/craft buy <#>' to buy the necessary materials to craft <#> item",
-		"'/craft clear' to reset to crafting once",
-		"'/craft show' to show the current craft settings")]
-	public class CraftMacroCommandHandler : AbstractCommandHandler, ICommandHandler
-	{
-		public const string CraftQueueLength = "CraftQueueLength";
-		public const string RecipeToCraft = "RecipeToCraft";
-		public void OnCommand(GameClient client, string[] args)
-		{
-			if (args.Length >= 2)
-			{
-				if (args[1] == "set")
-				{
-					if (args.Length >= 3)
-					{
-						int.TryParse(args[2], out int count);
-						if (count == 0)
-						{
-							client.Out.SendMessage("Use: /craft set <#>", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-							return;
-						}
-						client.Player.TempProperties.setProperty(CraftQueueLength, count);
-						client.Out.SendMessage($"Crafting queue set to {count} items", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-					}
-					else
-					{
-						client.Out.SendMessage("Use: /craft set <#>", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-					}
-				}
+    [CmdAttribute(
+        "&craft",
+        ePrivLevel.Player,
+        "Crafting macros and utilities",
+        "'/craft set <#>' to set how many items you want to craft",
+        "'/craft clear' to reset to crafting once",
+        "'/craft show' to show the current craft settings",
+        "'/craft buy' to buy the necessary materials to craft one item",
+        "'/craft buy <#>' to buy the necessary materials to craft <#> items",
+        "'/craft buyto <#>' to buy only the missing materials to craft <#> items")]
+    public class CraftMacroCommandHandler : AbstractCommandHandler, ICommandHandler
+    {
+        public const string CraftQueueLength = "CraftQueueLength";
+        public const string RecipeToCraft = "RecipeToCraft";
 
-				if (args[1].Contains("clear"))
-				{
-					if (client.Player.TempProperties.getProperty<int>(CraftQueueLength) != 0)
-					{
-						client.Player.TempProperties.removeProperty(CraftQueueLength);
-						client.Out.SendMessage("Crafting queue reset to 1", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-					}
-					else
-						client.Out.SendMessage("The crafting queue is already set to 1", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-					var recipe = client.Player.TempProperties.getProperty<Recipe>(RecipeToCraft);
-					if (recipe != null)
-					{
-						client.Player.TempProperties.removeProperty(RecipeToCraft);
-						client.Out.SendMessage("Crafting buying macro item cleared", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-					}
-				}
-				
-				if (args[1].Contains("show"))
-				{
-					if (client.Player.TempProperties.getProperty<int>(CraftQueueLength) != 0)
-						client.Out.SendMessage($"Crafting queue set to {client.Player.TempProperties.getProperty<int>(CraftQueueLength)}", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-					else
-						client.Out.SendMessage("Crafting queue set to 1", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-				}
+        public void OnCommand(GameClient client, string[] args)
+        {
+            if (args.Length >= 2)
+            {
+                #region set
 
-				if (args[1].Contains("buy"))
-				{
-					int amount = 1;
-					if (args.Length >= 3)
-					{
-						int.TryParse(args[2], out amount);
-						if (amount == 0)
-						{
-							amount = 1;
-						}
-					}
-					
-					var recipe = client.Player.TempProperties.getProperty<Recipe>("RecipeToCraft");
-					if (recipe != null)
-					{
-						if (client.Player.TargetObject is GameMerchant merchant)
-						{
-							client.Out.SendMessage($"Buying items to craft {amount}x {recipe.Product.Name}", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+                if (args[1] == "set")
+                {
+                    if (args.Length >= 3)
+                    {
+                        int.TryParse(args[2], out int count);
+                        if (count == 0)
+                        {
+                            DisplayMessage(client, "Use: /craft set <#>");
+                            return;
+                        }
 
-							var merchantitems = DOLDB<MerchantItem>.SelectObjects(DB.Column("ItemListID").IsEqualTo(merchant.TradeItems.ItemsListID));
+                        client.Player.TempProperties.setProperty(CraftQueueLength, count);
+                        DisplayMessage(client, $"Crafting queue set to {count} items");
+                    }
+                    else
+                    {
+                        DisplayMessage(client, "Use: /craft set <#>");
+                    }
+                }
 
-							foreach (var ingredient in recipe.Ingredients)
-							{
-								foreach (var items in merchantitems)
-								{
-									ItemTemplate item = GameServer.Database.FindObjectByKey<ItemTemplate>(items.ItemTemplateID);
-									if (item != ingredient.Material) continue;
-									merchant.OnPlayerBuy(client.Player, items.SlotPosition, items.PageNumber, ingredient.Count * amount);
-								}
-							}
-							client.Out.SendMessage($"Bought items to craft {amount}x {recipe.Product.Name}", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-							return;
-						}
-						
-						client.Out.SendMessage("You must target a merchant", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-						return;
-						
-					}
-					client.Out.SendMessage("No recipe selected. Start crafting an item first.", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-				}
-			}
-			else
-			{
-				client.Out.SendMessage("Use: `/craft set <#>', `/craft clear', `/craft show'", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-			}
-		}
-	}
+                #endregion
+
+                #region clear
+
+                if (args[1] == "clear")
+                {
+                    if (client.Player.TempProperties.getProperty<int>(CraftQueueLength) != 0)
+                    {
+                        client.Player.TempProperties.removeProperty(CraftQueueLength);
+                    }
+
+                    var recipe = client.Player.TempProperties.getProperty<Recipe>(RecipeToCraft);
+                    if (recipe != null)
+                    {
+                        client.Player.TempProperties.removeProperty(RecipeToCraft);
+                    }
+
+                    DisplayMessage(client, "Crafting queue reset to 1 and item cleared");
+                }
+
+                #endregion
+
+                #region show
+
+                if (args[1] == "show")
+                {
+                    if (client.Player.TempProperties.getProperty<int>(CraftQueueLength) != 0)
+                        DisplayMessage(client,
+                            $"Crafting queue set to {client.Player.TempProperties.getProperty<int>(CraftQueueLength)}");
+                    else
+                        DisplayMessage(client, "Crafting queue set to 1");
+                }
+
+                #endregion
+
+                #region buy
+
+                if (args[1] == "buy")
+                {
+                    int amount = 1;
+                    if (args.Length >= 3)
+                    {
+                        int.TryParse(args[2], out amount);
+                        if (amount == 0)
+                        {
+                            amount = 1;
+                        }
+                    }
+
+                    var recipe = client.Player.TempProperties.getProperty<Recipe>("RecipeToCraft");
+                    if (recipe != null)
+                    {
+                        if (client.Player.TargetObject is GameMerchant merchant)
+                        {
+                            var merchantitems = DOLDB<MerchantItem>.SelectObjects(DB.Column("ItemListID")
+                                .IsEqualTo(merchant.TradeItems.ItemsListID));
+
+                            foreach (var ingredient in recipe.Ingredients)
+                            {
+                                foreach (var items in merchantitems)
+                                {
+                                    ItemTemplate item =
+                                        GameServer.Database.FindObjectByKey<ItemTemplate>(items.ItemTemplateID);
+                                    if (item != ingredient.Material) continue;
+                                    merchant.OnPlayerBuy(client.Player, items.SlotPosition, items.PageNumber,
+                                        ingredient.Count * amount);
+                                }
+                            }
+
+                            return;
+                        }
+
+                        DisplayMessage(client, "You must target a merchant");
+                        return;
+                    }
+
+                    DisplayMessage(client, "No recipe selected, start crafting an item first");
+                }
+
+                #endregion
+
+                #region buyto
+
+                if (args[1] == "buyto")
+                {
+                    if (args.Length < 3)
+                    {
+                        DisplayMessage(client, "Use: /craft buyto <#>");
+                        return;
+                    }
+
+                    if (int.TryParse(args[2], out int amount))
+                    {
+                        if (amount == 0)
+                        {
+                            amount = 1;
+                        }
+                    }
+                    else
+                    {
+                        DisplayMessage(client, "Use: /craft buyto <#>");
+                        return;
+                    }
+
+                    var recipe = client.Player.TempProperties.getProperty<Recipe>("RecipeToCraft");
+                    if (recipe != null)
+                    {
+                        if (client.Player.TargetObject is GameMerchant merchant)
+                        {
+                            var merchantitems = DOLDB<MerchantItem>.SelectObjects(DB.Column("ItemListID")
+                                .IsEqualTo(merchant.TradeItems.ItemsListID));
+
+                            foreach (var ingredient in recipe.Ingredients)
+                            {
+                                foreach (var items in merchantitems)
+                                {
+                                    ItemTemplate item =
+                                        GameServer.Database.FindObjectByKey<ItemTemplate>(items.ItemTemplateID);
+                                    if (item != ingredient.Material) continue;
+                                    int playerAmount;
+                                    var playeritems = client.Player.Inventory.GetFirstItemByName(
+                                        ingredient.Material.Name, eInventorySlot.FirstBackpack,
+                                        eInventorySlot.LastBackpack);
+                                    playerAmount = playeritems?.Count ?? 0;
+
+                                    merchant.OnPlayerBuy(client.Player, items.SlotPosition, items.PageNumber,
+                                        (ingredient.Count * amount) - playerAmount);
+                                }
+                            }
+
+                            DisplayMessage(client, $"Bought items to craft {amount}x {recipe.Product.Name}");
+                            return;
+                        }
+
+                        DisplayMessage(client, "You must target a merchant");
+                        return;
+                    }
+
+                    DisplayMessage(client, "No recipe selected, start crafting an item first");
+                }
+
+                #endregion
+            }
+            else
+            {
+                DisplaySyntax(client);
+            }
+        }
+    }
 }
