705f97954bcfd47f07c5907a98b9948f90a1c4cb
Anthony Knight <midgetlord123@hotmail.com>
2022-05-13 02:49:41 -0700

    Viper displays messages at effect application and falloff

--- GameServer/realmabilities_atlasOF/effects/AtlasOF_ViperECSEffect.cs
+++ GameServer/realmabilities_atlasOF/effects/AtlasOF_ViperECSEffect.cs
@@ -18,5 +18,17 @@ namespace DOL.GS.Effects
         public override ushort Icon { get { return 4283; } }
         public override string Name { get { return "Viper"; } }
         public override bool HasPositiveEffect { get { return true; } }
+
+        public override void OnStartEffect()
+        {
+            base.OnStartEffect();
+            if(OwnerPlayer != null) OwnerPlayer.Out.SendMessage("The blood of the viper surges in your veins.", eChatType.CT_Spell, eChatLoc.CL_SystemWindow);
+        }
+
+        public override void OnStopEffect()
+        {
+            if(OwnerPlayer != null) OwnerPlayer.Out.SendMessage("The blood of the viper fades from within.", eChatType.CT_Spell, eChatLoc.CL_SystemWindow);
+            base.OnStopEffect();
+        }
     }
 }
