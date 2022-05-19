a931d043ceb0c9be4decdb032e2e40d47e016e3f
Anthony Knight <midgetlord123@hotmail.com>
2022-05-19 11:26:48 -0700

    Slow weapon bonus now applies to all attacks, not just ranged

--- GameServer/ECS-Components/AttackComponent.cs
+++ GameServer/ECS-Components/AttackComponent.cs
@@ -509,6 +509,10 @@ namespace DOL.GS
 
                 double effectiveness = 1.00;
                 double damage = p.WeaponDamage(weapon) * weapon.SPD_ABS * 0.1;
+                
+                //slow weapon bonus as found here: https://www2.uthgard.net/tracker/issue/2753/@/Bow_damage_variance_issue_(taking_item_/_spec_???)
+                //EDPS * (your WS/target AF) * (1-absorb) * slow weap bonus * SPD * 2h weapon bonus * Arrow Bonus 
+                damage *= 1 + ((weapon.SPD_ABS - 20) * 0.03) * .1;
 
                 if (weapon.Hand == 1) // two-hand
                 {
@@ -536,10 +540,6 @@ namespace DOL.GS
                                 break; //Broadhead (X-heavy) +25%
                         }
                     }
-
-                    //slow weapon bonus as found here: https://www2.uthgard.net/tracker/issue/2753/@/Bow_damage_variance_issue_(taking_item_/_spec_???)
-                    //EDPS * (your WS/target AF) * (1-absorb) * slow weap bonus * SPD * 2h weapon bonus * Arrow Bonus 
-                    damage *= 1 + ((weapon.SPD_ABS - 20) * 0.03) * .1;
 
                     //Ranged damage buff,debuff,Relic,RA
                     effectiveness += p.GetModified(eProperty.RangedDamage) * 0.01;
@@ -3382,6 +3382,7 @@ namespace DOL.GS
                         DPS = cap;
 
                     double result = DPS * weapon.SPD_ABS * 0.03 * (0.94 + 0.003 * weapon.SPD_ABS);
+                    Console.WriteLine($"Slow weapon bonus {1 + ((weapon.SPD_ABS - 20) * 0.03) * .1}");
 
                     if (weapon.Hand == 1) //2h
                     {
