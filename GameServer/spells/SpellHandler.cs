875c1c374d7fff3d8a755380d66436b46fcd9fe0
Anthony Knight <midgetlord123@hotmail.com>
2022-05-27 10:42:52 -0700

    Pulse spells no longer drain endurance on cast

--- GameServer/spells/SpellHandler.cs
+++ GameServer/spells/SpellHandler.cs
@@ -2384,7 +2384,8 @@ namespace DOL.GS.Spells
             {
 				(Caster as GamePlayer).Out.SendObjectUpdate(target);
             }*/
-			m_caster.ChangeEndurance(m_caster, eEnduranceChangeType.Spell, -5);
+			if(!this.Spell.IsPulsingEffect && !this.Spell.IsPulsing)
+				m_caster.ChangeEndurance(m_caster, eEnduranceChangeType.Spell, -5);
 
 			GameEventMgr.Notify(GameLivingEvent.CastFinished, m_caster, new CastingEventArgs(this, target, m_lastAttackData));
 		}
