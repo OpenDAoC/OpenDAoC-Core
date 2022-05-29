e690c1893a0f2f150bf667f0b581752e6c02a9d4
Glimmer Warshade <glimmer@europe.com>
2022-05-29 14:29:02 +0200

    Midgard Classic Zones nameds

---
+++ GameServer/scripts/namedmobs/Midgard Named Mobs/Classic Zones/Mokkurvalve.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using DOL.AI.Brain;
+using DOL.Database;
+using DOL.Events;
+using DOL.GS;
+using DOL.GS.PacketHandler;
+
+namespace DOL.GS
+{
+	public class Mokkurvalve : GameEpicBoss
+	{
+		public Mokkurvalve() : base() { }
+
+		[ScriptLoadedEvent]
+		public static void ScriptLoaded(DOLEvent e, object sender, EventArgs args)
+		{
+			if (log.IsInfoEnabled)
+				log.Info("Mokkurvalve Initializing...");
+		}
+		public void BroadcastMessage(String message)
+		{
+			foreach (GamePlayer player in GetPlayersInRadius(WorldMgr.OBJ_UPDATE_DISTANCE))
+			{
+				player.Out.SendMessage(message, eChatType.CT_Broadcast, eChatLoc.CL_ChatWindow);
+			}
+		}
+		public override int GetResist(eDamageType damageType)
+		{
+			switch (damageType)
+			{
+				case eDamageType.Slash: return 40;// dmg reduction for melee dmg
+				case eDamageType.Crush: return 40;// dmg reduction for melee dmg
+				case eDamageType.Thrust: return 40;// dmg reduction for melee dmg
+				default: return 70;// dmg reduction for rest resists
+			}
+		}
+		public override double AttackDamage(InventoryItem weapon)
+		{
+			return base.AttackDamage(weapon) * Strength / 100;
+		}
+		public override int AttackRange
+		{
+			get { return 350; }
+			set { }
+		}
+		public override bool HasAbility(string keyName)
+		{
+			if (IsAlive && keyName == GS.Abilities.CCImmunity)
+				return true;
+
+			return base.HasAbility(keyName);
+		}
+		public override double GetArmorAF(eArmorSlot slot)
+		{
+			return 350;
+		}
+		public override double GetArmorAbsorb(eArmorSlot slot)
+		{
+			// 85% ABS is cap.
+			return 0.20;
+		}
+		public override int MaxHealth
+		{
+			get { return 30000; }
+		}
+		public override bool AddToWorld()
+		{
+			INpcTemplate npcTemplate = NpcTemplateMgr.GetTemplate(60164144);
+			LoadTemplate(npcTemplate);
+			Strength = npcTemplate.Strength;
+			Dexterity = npcTemplate.Dexterity;
+			Constitution = npcTemplate.Constitution;
+			Quickness = npcTemplate.Quickness;
+			Piety = npcTemplate.Piety;
+			Intelligence = npcTemplate.Intelligence;
+			Empathy = npcTemplate.Empathy;
+			RespawnInterval = ServerProperties.Properties.SET_SI_EPIC_ENCOUNTER_RESPAWNINTERVAL * 60000;//1min is 60000 miliseconds
+
+			MokkurvalveBrain sbrain = new MokkurvalveBrain();
+			SetOwnBrain(sbrain);
+			LoadedFromScript = false;//load from database
+			SaveIntoDatabase();
+			base.AddToWorld();
+			return true;
+		}
+        public override void Die(GameObject killer)
+        {
+			BroadcastMessage("Part of " + Name + "'s body falls to the ground.");
+			SpawnShardsAfterDeath();
+            base.Die(killer);
+        }
+		private void SpawnShardsAfterDeath()
+        {
+			for (int i = 0; i < 20; i++)
+			{
+				MokkurvalveAdds add = new MokkurvalveAdds();
+				add.X = X + Util.Random(-200, 200);
+				add.Y = Y + Util.Random(-200, 200);
+				add.Z = Z;
+				add.Heading = Heading;
+				add.CurrentRegion = CurrentRegion;
+				add.AddToWorld();
+			}
+		}
+    }
+}
+namespace DOL.AI.Brain
+{
+	public class MokkurvalveBrain : StandardMobBrain
+	{
+		private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		public MokkurvalveBrain() : base()
+		{
+			AggroLevel = 100;
+			AggroRange = 600;
+			ThinkInterval = 1500;
+		}
+		public void BroadcastMessage(String message)
+		{
+			foreach (GamePlayer player in Body.GetPlayersInRadius(WorldMgr.OBJ_UPDATE_DISTANCE))
+			{
+				player.Out.SendMessage(message, eChatType.CT_Broadcast, eChatLoc.CL_ChatWindow);
+			}
+		}
+		private bool CanSpawnShard = false;
+		public override void Think()
+		{
+			if (!HasAggressionTable())
+			{
+				//set state to RETURN TO SPAWN
+				FSM.SetCurrentState(eFSMStateType.RETURN_TO_SPAWN);
+				Body.Health = Body.MaxHealth;
+				CanSpawnShard = false;
+				foreach(GameNPC  npc in Body.GetNPCsInRadius(8000))
+                {
+					if (npc != null && npc.IsAlive && npc.Brain is MokkurvalveAddsBrain)
+						npc.Die(Body);
+                }
+			}
+			if (HasAggro)
+			{
+				if(!CanSpawnShard)
+                {
+					new ECSGameTimer(Body, new ECSGameTimer.ECSTimerCallback(SpawnShards), Util.Random(15000, 35000));
+					CanSpawnShard = true;
+                }
+			}
+			base.Think();
+		}
+		private int SpawnShards(ECSGameTimer timer)
+        {
+			if (HasAggro)
+			{
+				BroadcastMessage("Part of " + Body.Name + "'s body falls to the ground.");
+				MokkurvalveAdds add = new MokkurvalveAdds();
+				add.X = Body.X + Util.Random(-200, 200);
+				add.Y = Body.Y + Util.Random(-200, 200);
+				add.Z = Body.Z;
+				add.Heading = Body.Heading;
+				add.CurrentRegion = Body.CurrentRegion;
+				add.AddToWorld();
+			}
+			CanSpawnShard = false;
+			return 0;
+        }
+	}
+}
+////////////////////////////////////////////////////////////adds//////////////////////////////////////////
+namespace DOL.GS
+{
+	public class MokkurvalveAdds : GameNPC
+	{
+		public MokkurvalveAdds() : base() { }
+		public override int GetResist(eDamageType damageType)
+		{
+			switch (damageType)
+			{
+				case eDamageType.Slash: return 20;// dmg reduction for melee dmg
+				case eDamageType.Crush: return 20;// dmg reduction for melee dmg
+				case eDamageType.Thrust: return 20;// dmg reduction for melee dmg
+				default: return 20;// dmg reduction for rest resists
+			}
+		}
+		public override double AttackDamage(InventoryItem weapon)
+		{
+			return base.AttackDamage(weapon) * Strength / 100;
+		}
+		public override double GetArmorAF(eArmorSlot slot)
+		{
+			return 200;
+		}
+		public override double GetArmorAbsorb(eArmorSlot slot)
+		{
+			// 85% ABS is cap.
+			return 0.10;
+		}
+		public override int MaxHealth
+		{
+			get { return 1500; }
+		}
+		public override short Quickness { get => base.Quickness; set => base.Quickness = 80; }
+		public override short Strength { get => base.Strength; set => base.Strength = 150; }
+		public override bool AddToWorld()
+		{
+			Model = 1770;
+			Size = (byte)Util.Random(25, 35);
+			Name = "Mokkurvalve's shard";
+			RespawnInterval = -1;
+			Level = (byte)Util.Random(42, 44);
+			MaxSpeedBase = 225;
+
+			MokkurvalveAddsBrain sbrain = new MokkurvalveAddsBrain();
+			SetOwnBrain(sbrain);
+			LoadedFromScript = true;
+			base.AddToWorld();
+			return true;
+		}
+		public override void DropLoot(GameObject killer) //no loot
+		{
+		}
+		public override long ExperienceValue => 0;
+	}
+}
+namespace DOL.AI.Brain
+{
+	public class MokkurvalveAddsBrain : StandardMobBrain
+	{
+		private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		public MokkurvalveAddsBrain() : base()
+		{
+			AggroLevel = 100;
+			AggroRange = 600;
+			ThinkInterval = 1500;
+		}
+		public override void Think()
+		{
+			base.Think();
+		}
+	}
+}
