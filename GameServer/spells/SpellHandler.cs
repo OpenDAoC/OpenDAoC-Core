e9dd9fd48eb58e78d18c60c2581ad928415fe7bf
Anthony Knight <midgetlord123@hotmail.com>
2022-06-02 13:32:23 -0700

    Savage spells no longer use endurance on cast

--- GameServer/spells/SpellHandler.cs
+++ GameServer/spells/SpellHandler.cs
@@ -27,6 +27,7 @@ using DOL.Database;
 using DOL.Events;
 using DOL.GS.Effects;
 using DOL.GS.PacketHandler;
+using DOL.GS.PlayerClass;
 using DOL.GS.ServerProperties;
 using DOL.GS.RealmAbilities;
 using DOL.GS.SkillHandler;
@@ -2384,7 +2385,7 @@ namespace DOL.GS.Spells
             {
 				(Caster as GamePlayer).Out.SendObjectUpdate(target);
             }*/
-			if(!this.Spell.IsPulsingEffect && !this.Spell.IsPulsing)
+			if(!this.Spell.IsPulsingEffect && !this.Spell.IsPulsing && Caster is GamePlayer {CharacterClass: not ClassSavage})
 				m_caster.ChangeEndurance(m_caster, eEnduranceChangeType.Spell, -5);
 
 			GameEventMgr.Notify(GameLivingEvent.CastFinished, m_caster, new CastingEventArgs(this, target, m_lastAttackData));
