ec5473bf7f97dad73023a4493089420b85bbb79c
Fyr <jparrott7@gmail.com>
2022-06-11 03:29:34 +0000

    fix for spell pulses showing in buff bar but not applying the effect

--- GameServer/ECS-Components/EffectList/EffectListComponent.cs
+++ GameServer/ECS-Components/EffectList/EffectListComponent.cs
@@ -361,9 +361,14 @@ namespace DOL.GS
                     {
                         if (effect.CancelEffect)
                         {
-                            Effects[effect.EffectType].Remove(effect);
-                            EffectIdToEffect.Remove(effect.Icon);
+                            List<ECSGameEffect> existingEffects = Effects[effect.EffectType];
+                            
+                            //Get the effectToRemove from the Effects list. Had issues trying to remove the effect directly from the list if it wasn't the same object.
+                            ECSGameEffect effectToRemove = existingEffects.FirstOrDefault(e => e.Name == effect.Name);
+                            Effects[effect.EffectType].Remove(effectToRemove);
 
+                            EffectIdToEffect.Remove(effect.Icon);
+                            
                             if (Effects[effect.EffectType].Count == 0)
                             {
                                 EffectIdToEffect.Remove(effect.Icon);
