a55765b3426ef9200f713afccdcaea2aee314002
clait <stefanomus@fastmail.com>
2022-06-20 18:47:29 +0100

    Removed the debug no reuse timer (fixes no timer on charges)

--- GameServer/gameobjects/GamePlayer.cs
+++ GameServer/gameobjects/GamePlayer.cs
@@ -10029,18 +10029,18 @@ namespace DOL.GS
                                 {
                                     Out.SendMessage("In your state you can't discharge any object.", eChatType.CT_System, eChatLoc.CL_SystemWindow);
                                 }
-                                // else if (Client.Account.PrivLevel == 1 && (changeTime < delay || (CurrentRegion.Time - itemdelay) < itemreuse)) //2 minutes reuse timer
-                                // {
-                                //     if ((CurrentRegion.Time - itemdelay) < itemreuse)
-                                //     {
-                                //         Out.SendMessage("You must wait " + (itemreuse - (CurrentRegion.Time - itemdelay)) / 1000 + " more second before discharge " + useItem.Name + "!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                                //     }
-                                //     else
-                                //     {
-                                //         Out.SendMessage("You must wait " + (delay - changeTime) / 1000 + " more second before discharge another object!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                                //     }
-                                //     return;
-                                // }
+                                else if (Client.Account.PrivLevel == 1 && (changeTime < delay || (CurrentRegion.Time - itemdelay) < itemreuse)) //2 minutes reuse timer
+                                {
+                                    if ((CurrentRegion.Time - itemdelay) < itemreuse)
+                                    {
+                                        Out.SendMessage("You must wait " + (itemreuse - (CurrentRegion.Time - itemdelay)) / 1000 + " more second before discharge " + useItem.Name + "!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+                                    }
+                                    else
+                                    {
+                                        Out.SendMessage("You must wait " + (delay - changeTime) / 1000 + " more second before discharge another object!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+                                    }
+                                    return;
+                                }
                                 else
                                 {
                                     if (type == 1) //use1
