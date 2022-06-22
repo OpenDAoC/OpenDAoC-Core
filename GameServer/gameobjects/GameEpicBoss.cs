95ed36378cb11779d7cddd5a94d417dcf700a7e5
clait <stefanomus@fastmail.com>
2022-06-22 01:05:13 +0100

    Automatic kill achievement upon killing Epic NPC/Boss

--- GameServer/gameobjects/GameEpicBoss.cs
+++ GameServer/gameobjects/GameEpicBoss.cs
@@ -1,4 +1,5 @@
-﻿using DOL.GS.ServerProperties;
+﻿using System.Text.RegularExpressions;
+using DOL.GS.ServerProperties;
 
 namespace DOL.GS {
     public class GameEpicBoss : GameNPC {
@@ -17,33 +18,38 @@ namespace DOL.GS {
             // debug
             log.Debug($"{Name} killed by {killer.Name}");
             
-            GamePlayer playerKiller = killer as GamePlayer;
+            var playerKiller = killer as GamePlayer;
             
-            BattleGroup killerBG = (BattleGroup)playerKiller?.TempProperties.getProperty<object>(BattleGroup.BATTLEGROUP_PROPERTY, null);
+            var achievementMob = Regex.Replace(Name, @"\s+", "");
+
+            var killerBG = (BattleGroup)playerKiller?.TempProperties.getProperty<object>(BattleGroup.BATTLEGROUP_PROPERTY, null);
             
             if (killerBG != null && (killerBG.Members.Contains(playerKiller) || (bool)killerBG.Members[playerKiller]!))
             {
-                foreach (GamePlayer bgPlayer in killerBG.GetPlayersInTheBattleGroup())
+                foreach (var bgPlayer in killerBG.GetPlayersInTheBattleGroup())
                 {
                     if (bgPlayer.IsWithinRadius(this, WorldMgr.MAX_EXPFORKILL_DISTANCE))
                     {
                         AtlasROGManager.GenerateOrbAmount(bgPlayer,OrbsReward);
+                        bgPlayer.Achieve($"{achievementMob}-Credit");
                     }
                 }
             }
             else if (playerKiller?.Group != null)
             {
-                foreach (GamePlayer groupPlayer in playerKiller.Group.GetPlayersInTheGroup())
+                foreach (var groupPlayer in playerKiller.Group.GetPlayersInTheGroup())
                 {
                     if (groupPlayer.IsWithinRadius(this, WorldMgr.MAX_EXPFORKILL_DISTANCE))
                     {
                         AtlasROGManager.GenerateOrbAmount(groupPlayer,OrbsReward);
+                        groupPlayer.Achieve($"{achievementMob}-Credit");
                     }
                 }
             }
             else
             {
                 AtlasROGManager.GenerateOrbAmount(playerKiller,OrbsReward);
+                playerKiller.Achieve($"{achievementMob}-Credit");
             }
 
             base.Die(killer);
