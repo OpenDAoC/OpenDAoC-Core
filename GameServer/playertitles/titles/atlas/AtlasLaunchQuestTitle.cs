f25192347db1e48de11f310853e4eac4dc1c21a2
clait <stefanomus@fastmail.com>
2022-06-24 12:44:28 +0100

    LaunchQuest title fix

--- GameServer/playertitles/titles/atlas/AtlasLaunchQuestTitle.cs
+++ GameServer/playertitles/titles/atlas/AtlasLaunchQuestTitle.cs
@@ -1,4 +1,5 @@
-﻿using DOL.GS.PacketHandler;
+﻿using System;
+using DOL.Events;
 
 namespace DOL.GS.PlayerTitles
 {
@@ -6,27 +7,63 @@ namespace DOL.GS.PlayerTitles
     /// Example...
     /// </summary>
     ///
-    public class LaunchQuestTitle : SimplePlayerTitle
+    public class LaunchQuestTitle : EventPlayerTitle
     {
-
+        /// <summary>
+        /// The title description, shown in "Titles" window.
+        /// </summary>
+        /// <param name="player">The title owner.</param>
+        /// <returns>The title description.</returns>
         public override string GetDescription(GamePlayer player)
         {
-            return "Congrats I guess?";
-        }
-        
-        public override string GetValue(GamePlayer source, GamePlayer player)
-        {
-            return "Congrats I guess?";
-        }
-        
-        public override void OnTitleGained(GamePlayer player)
-        {
-            player.Out.SendMessage("You have gained the Launch Quest title!", eChatType.CT_Important, eChatLoc.CL_SystemWindow);
+            return "Congrats, I guess?";
         }
 
+        /// <summary>
+        /// The title value, shown over player's head.
+        /// </summary>
+        /// <param name="source">The player looking.</param>
+        /// <param name="player">The title owner.</param>
+        /// <returns>The title value.</returns>
+        public override string GetValue(GamePlayer source, GamePlayer player)
+        {
+            return "Congrats, I guess?";
+        }
+		
+        /// <summary>
+        /// The event to hook.
+        /// </summary>
+        public override DOLEvent Event
+        {
+            get { return GamePlayerEvent.GameEntered; }
+        }
+		
+        /// <summary>
+        /// Verify whether the player is suitable for this title.
+        /// </summary>
+        /// <param name="player">The player to check.</param>
+        /// <returns>true if the player is suitable for this title.</returns>
         public override bool IsSuitable(GamePlayer player)
         {
-            return AchievementUtils.CheckAccountCredit("LaunchQuest-Credit", player);;
+            var hasCredit = AchievementUtils.CheckAccountCredit("LaunchQuest", player);
+            return hasCredit;
+        }
+		
+        /// <summary>
+        /// The event callback.
+        /// </summary>
+        /// <param name="e">The event fired.</param>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="arguments">The event arguments.</param>
+        protected override void EventCallback(DOLEvent e, object sender, EventArgs arguments)
+        {
+            if (sender is GamePlayer p && p.Titles.Contains(this) && p.CurrentTitle == null)
+            {
+                p.UpdateCurrentTitle();
+                return;
+            }
+            base.EventCallback(e, sender, arguments);
         }
     }
+   
 }
