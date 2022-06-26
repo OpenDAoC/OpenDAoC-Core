37640a5f604558a62e8d7b1e0e39b5a33effe249
clait <1231586-claitz@users.noreply.gitlab.com>
2022-06-26 15:32:24 +0000

    Revert "Merge branch 'timed-db-enhancements' into 'master'"
    This reverts merge request !247

--- GameServer/GameServer.cs
+++ GameServer/GameServer.cs
@@ -1526,7 +1526,6 @@ namespace DOL.GS
 				if (log.IsDebugEnabled)
 					log.Debug("Save ThreadId=" + Thread.CurrentThread.ManagedThreadId);
 				int saveCount = 0;
-				int craftingSaveCount = 0;
 				if (m_database != null)
 				{
 					ThreadPriority oldprio = Thread.CurrentThread.Priority;
@@ -1543,7 +1542,6 @@ namespace DOL.GS
 					BoatMgr.SaveAllBoats();
 
 					FactionMgr.SaveAllAggroToFaction();
-					craftingSaveCount = CraftingProgressMgr.Save();
 
 					// 2008-01-29 Kakuri - Obsolete
 					//m_database.WriteDatabaseTables();
@@ -1552,9 +1550,8 @@ namespace DOL.GS
 				if (log.IsInfoEnabled)
 					log.Info("Saving database complete!");
 				startTick = Environment.TickCount - startTick;
-				if (log.IsInfoEnabled) 
-					log.Info("Saved all databases and " + saveCount + " players in " + startTick + "ms" + Environment.NewLine 
-						+ "Crafting Progress Saved for: " + craftingSaveCount);
+				if (log.IsInfoEnabled)
+					log.Info("Saved all databases and " + saveCount + " players in " + startTick + "ms");
 			}
 			catch (Exception e1)
 			{
