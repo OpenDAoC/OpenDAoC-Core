2ba0bf135f88745dc65d6836fd065f75e97db131
Anthony Knight <midgetlord123@hotmail.com>
2022-07-05 12:42:51 -0700

    Increase pet health

--- GameServer/scripts/titles/PrivilegedTitle.cs
+++ GameServer/scripts/titles/PrivilegedTitle.cs
@@ -42,4 +42,11 @@ namespace GameServerScripts.Titles
 		protected override Tuple<string, string> DescriptionValue { get { return new Tuple<string, string>("Titles.PrivLevel.Gamemaster", "Titles.PrivLevel.Gamemaster"); }}
 		protected override Func<DOL.GS.GamePlayer, bool> SuitableMethod { get { return player => player.Client.Account.PrivLevel == (uint)ePrivLevel.GM; }}
 	}
+	
+	public class Friend : NoGenderGenericEventPlayerTitle
+	{
+		public override DOLEvent Event { get { return GamePlayerEvent.GameEntered; }}
+		protected override Tuple<string, string> DescriptionValue { get { return new Tuple<string, string>("My Uncle Works At Nintendo", "My Uncle Works At Nintendo"); }}
+		protected override Func<DOL.GS.GamePlayer, bool> SuitableMethod { get { return player => player.GetAchievementProgress("NintendoDad") > 0; }}
+	}
 }
