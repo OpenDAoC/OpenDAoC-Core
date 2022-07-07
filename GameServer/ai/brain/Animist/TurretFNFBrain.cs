9134dbcb11dbfae0df29949dfcc0d3a0386d8eb8
Anthony Knight <midgetlord123@hotmail.com>
2022-07-06 19:24:38 -0700

    Fix for animist turrets not picking new targets after first one dies

--- GameServer/ai/brain/Animist/TurretFNFBrain.cs
+++ GameServer/ai/brain/Animist/TurretFNFBrain.cs
@@ -39,6 +39,9 @@ namespace DOL.AI.Brain
 			List<GameLiving> newTargets = new List<GameLiving>();
 			List<GameLiving> oldTargets = new List<GameLiving>();
 			base.CalculateNextAttackTarget();
+			TurretPet turretBody = Body as TurretPet;
+			if (turretBody == null) return null;
+			
 			lock((m_aggroTable as ICollection).SyncRoot)
 			{
 				foreach(GameLiving living in m_aggroTable.Keys)
@@ -52,22 +55,21 @@ namespace DOL.AI.Brain
 					if (!Body.IsWithinRadius(living, MAX_AGGRO_DISTANCE, true))
 						continue;
 
-					if (!Body.IsWithinRadius(living, ((TurretPet)Body).TurretSpell.Range, true))
+					if (!Body.IsWithinRadius(living, turretBody.TurretSpell.Range, true))
 						continue;
 
 					//if (((TurretPet)Body).TurretSpell.SpellType != (byte)eSpellType.SpeedDecrease && SpellHandler.FindEffectOnTarget(living, "SpeedDecrease") != null)
-					if (((TurretPet)Body).TurretSpell.SpellType != (byte)eSpellType.SpeedDecrease && EffectListService.GetEffectOnTarget(living, eEffect.MovementSpeedDebuff) != null)
+					if (turretBody.TurretSpell.SpellType != (byte)eSpellType.SpeedDecrease && EffectListService.GetEffectOnTarget(living, eEffect.MovementSpeedDebuff) != null)
 						continue;
 
-					if (((TurretPet)Body).TurretSpell.SpellType == (byte)eSpellType.SpeedDecrease && living.HasAbility(Abilities.RootImmunity))
+					if (turretBody.TurretSpell.SpellType == (byte)eSpellType.SpeedDecrease && living.HasAbility(Abilities.RootImmunity))
 						continue;
 
 					newTargets.Add(living);
 				}
 			}
-
-			foreach (GameLiving living in Body.GetPlayersInRadius((ushort) ((TurretPet) Body).TurretSpell.Range,
-				         Body.CurrentRegion.IsDungeon ? false : true))
+			
+			foreach (GameLiving living in Body.GetPlayersInRadius((ushort) turretBody.TurretSpell.Range, !Body.CurrentRegion.IsDungeon))
 			{
 				// if (!GameServer.ServerRules.IsAllowedToAttack(Body, living, true))
 				// 	continue;
@@ -87,21 +89,21 @@ namespace DOL.AI.Brain
 						continue;
 
 					//if (((TurretPet)Body).TurretSpell.SpellType != (byte)eSpellType.SpeedDecrease && SpellHandler.FindEffectOnTarget(living, "SpeedDecrease") != null)
-					if (((TurretPet)Body).TurretSpell.SpellType != (byte)eSpellType.SpeedDecrease && EffectListService.GetEffectOnTarget(living, eEffect.MovementSpeedDebuff) != null && living.CurrentSpeed <= (living.MaxSpeed / 10)) //turrets will only not attack enemies that are snared, only rooted
+					if (turretBody.TurretSpell.SpellType != (byte)eSpellType.SpeedDecrease && EffectListService.GetEffectOnTarget(living, eEffect.MovementSpeedDebuff) != null && living.CurrentSpeed <= (living.MaxSpeed / 10)) //turrets will only not attack enemies that are snared, only rooted
 						continue;
 
-					if (((TurretPet)Body).TurretSpell.SpellType == (byte)eSpellType.SpeedDecrease && (living.HasAbility(Abilities.RootImmunity) || living.HasAbility(Abilities.DamageImmunity)))
+					if (turretBody.TurretSpell.SpellType == (byte)eSpellType.SpeedDecrease && (living.HasAbility(Abilities.RootImmunity) || living.HasAbility(Abilities.DamageImmunity)))
 						continue;
 				} else if (living is GamePlayer gamelivingPl)
 				{
 					if (gamelivingPl.IsInvulnerableToAttack)
 						continue;
 					//if (((TurretPet)Body).TurretSpell.SpellType != (byte)eSpellType.SpeedDecrease && SpellHandler.FindEffectOnTarget(living, "SpeedDecrease") != null)
-					if (((TurretPet)Body).TurretSpell.SpellType != (byte)eSpellType.SpeedDecrease && EffectListService.GetEffectOnTarget(living, eEffect.MovementSpeedDebuff) != null)
+					if (turretBody.TurretSpell.SpellType != (byte)eSpellType.SpeedDecrease && EffectListService.GetEffectOnTarget(living, eEffect.MovementSpeedDebuff) != null)
 						continue;
 				}
 				
-				if (LivingHasEffect(living, ((TurretPet)Body).TurretSpell))
+				if (LivingHasEffect(living, turretBody.TurretSpell))
 				{
 					oldTargets.Add(living);
 				}
@@ -110,6 +112,27 @@ namespace DOL.AI.Brain
 					newTargets.Add(living);
 				}
 								
+			}
+			
+			foreach (GameNPC living in Body.GetNPCsInRadius((ushort) turretBody.TurretSpell.Range, !Body.CurrentRegion.IsDungeon))
+			{
+				if (!GameServer.ServerRules.IsAllowedToAttack(Body, living, true))
+					continue;
+
+				if (!living.IsAlive || living.CurrentRegion != Body.CurrentRegion || living.ObjectState != GameObject.eObjectState.Active)
+					continue;
+
+				if (living.IsMezzed || living.IsStealthed)
+					continue;
+
+				if (LivingHasEffect(living, turretBody.TurretSpell))
+				{
+					oldTargets.Add(living);
+				}
+				else
+				{
+					newTargets.Add(living as GameLiving);
+				}
 			}
 			
 
@@ -124,7 +147,10 @@ namespace DOL.AI.Brain
 				return oldTargets[Util.Random(oldTargets.Count - 1)];
 			}
 
-			m_aggroTable.Clear();
+			lock ((m_aggroTable as ICollection).SyncRoot)
+			{
+				m_aggroTable.Clear();
+			}
 			return null;
 		}
 
