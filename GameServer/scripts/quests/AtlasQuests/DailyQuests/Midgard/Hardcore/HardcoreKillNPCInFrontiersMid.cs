5974ba36c192d58e5729c2e8bd1b52ed27e051bc
Anthony Knight <midgetlord123@hotmail.com>
2022-07-15 13:17:10 -0700

    Fix for mid hardcore quests

--- GameServer/scripts/quests/AtlasQuests/DailyQuests/Midgard/Hardcore/HardcoreKillNPCInFrontiersMid.cs
+++ GameServer/scripts/quests/AtlasQuests/DailyQuests/Midgard/Hardcore/HardcoreKillNPCInFrontiersMid.cs
@@ -309,9 +309,13 @@ namespace DOL.GS.DailyQuest
 
 			if (player?.IsDoingQuest(typeof(HardcoreKillNPCInFrontiersMid)) == null)
 				return;
-			
-			if(player.Group != null && Step == 1)
+
+			if (player.Group != null && Step == 1)
+			{
 				FailQuest();
+				return;
+			}
+				
 
 			if (sender != m_questPlayer)
 				return;
@@ -319,6 +323,7 @@ namespace DOL.GS.DailyQuest
 			if (e == GameLivingEvent.Dying && Step == 1)
 			{
 				FailQuest();
+				return;
 			}
 
 			if (e != GameLivingEvent.EnemyKilled || Step != 1) return;
@@ -374,7 +379,7 @@ namespace DOL.GS.DailyQuest
 			Step = -2;
 			// move quest from active list to finished list...
 			m_questPlayer.QuestList.Remove(this);
-			DeleteFromDatabase();
+			m_questPlayer.QuestListFinished.Add(this);
 			
 			m_questPlayer.Out.SendQuestListUpdate();
 		}
