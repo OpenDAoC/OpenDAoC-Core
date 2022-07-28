c659be3f461544ea064ee4890b27aa9017e044c0
Glimmer Warshade <glimmer@europe.com>
2022-07-28 17:20:07 +0200

    Trollheim

--- GameServer/scripts/namedmobs/Trollheim/Dagar.cs
+++ GameServer/scripts/namedmobs/Trollheim/Dagar.cs
@@ -20,10 +20,10 @@ namespace DOL.GS
 		{
 			switch (damageType)
 			{
-				case eDamageType.Slash: return 40;// dmg reduction for melee dmg
-				case eDamageType.Crush: return 40;// dmg reduction for melee dmg
-				case eDamageType.Thrust: return 40;// dmg reduction for melee dmg
-				default: return 50;// dmg reduction for rest resists
+				case eDamageType.Slash: return 20;// dmg reduction for melee dmg
+				case eDamageType.Crush: return 20;// dmg reduction for melee dmg
+				case eDamageType.Thrust: return 20;// dmg reduction for melee dmg
+				default: return 30;// dmg reduction for rest resists
 			}
 		}
 		public override double AttackDamage(InventoryItem weapon)
@@ -64,7 +64,7 @@ namespace DOL.GS
 			template.AddNPCEquipment(eInventorySlot.TwoHandWeapon, 956, 0);
 			Inventory = template.CloseTemplate();
 			SwitchWeapon(eActiveWeaponSlot.TwoHanded);
-			RespawnInterval = ServerProperties.Properties.SET_SI_EPIC_ENCOUNTER_RESPAWNINTERVAL * 60000;//1min is 60000 miliseconds
+			RespawnInterval = ServerProperties.Properties.SET_EPIC_GAME_ENCOUNTER_RESPAWNINTERVAL * 60000;//1min is 60000 miliseconds
 
 			VisibleActiveWeaponSlots = 34;
 			MeleeDamageType = eDamageType.Crush;
