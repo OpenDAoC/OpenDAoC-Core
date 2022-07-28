4263b0e144f913f57668aa3898fe9f4088f75685
Glimmer Warshade <glimmer@europe.com>
2022-07-28 17:20:47 +0200

    Rest Named mobs  boss mobs resists decreased to mage it more caster friendly.

--- GameServer/scripts/namedmobs/Yar.cs
+++ GameServer/scripts/namedmobs/Yar.cs
@@ -24,10 +24,10 @@ namespace DOL.GS
         {
             switch (damageType)
             {
-                case eDamageType.Slash: return 40; // dmg reduction for melee dmg
-                case eDamageType.Crush: return 40; // dmg reduction for melee dmg
-                case eDamageType.Thrust: return 40; // dmg reduction for melee dmg
-                default: return 70; // dmg reduction for rest resists
+                case eDamageType.Slash: return 20; // dmg reduction for melee dmg
+                case eDamageType.Crush: return 20; // dmg reduction for melee dmg
+                case eDamageType.Thrust: return 20; // dmg reduction for melee dmg
+                default: return 30; // dmg reduction for rest resists
             }
         }
         public override double GetArmorAF(eArmorSlot slot)
@@ -80,7 +80,7 @@ namespace DOL.GS
             
             Faction = FactionMgr.GetFactionByID(154);
             Faction.AddFriendFaction(FactionMgr.GetFactionByID(154));
-            RespawnInterval = ServerProperties.Properties.SET_SI_EPIC_ENCOUNTER_RESPAWNINTERVAL * 60000;//1min is 60000 miliseconds
+            RespawnInterval = ServerProperties.Properties.SET_EPIC_GAME_ENCOUNTER_RESPAWNINTERVAL * 60000;//1min is 60000 miliseconds
 
             YarBrain sBrain = new YarBrain();
             SetOwnBrain(sBrain);
