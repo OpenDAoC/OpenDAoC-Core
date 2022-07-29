d9ad32a9584ffd6ac2b36fe615e66b9ed7d27ed4
Kelteen <dean.dudlitz@live.de>
2022-07-29 20:49:28 +0200

    Stefano in Darkness Falls gives "Femurs from Darkness Falls"-Quest now

--- GameServer/scripts/quests/AtlasQuests/DailyQuests/Hibernia/RvR/Weekly/DFWeeklyKillQuestHib.cs
+++ GameServer/scripts/quests/AtlasQuests/DailyQuests/Hibernia/RvR/Weekly/DFWeeklyKillQuestHib.cs
@@ -29,7 +29,7 @@ namespace DOL.GS.DailyQuest.Hibernia
 		// Kill Goal
 		private const int MAX_KILLED = 50;
 
-		private static GameNPC ReyHib = null; // Start NPC
+		private static GameNPC Stefano = null; // Start NPC
 
 		private int EnemiesKilled = 0;
 
@@ -68,40 +68,37 @@ namespace DOL.GS.DailyQuest.Hibernia
 
 			#region defineNPCs
 
-			GameNPC[] npcs = WorldMgr.GetNPCsByName("Rey", eRealm.Hibernia);
+			GameNPC[] npcs = WorldMgr.GetNPCsByName("Stefano", eRealm.Hibernia);
 
 			if (npcs.Length > 0)
 				foreach (GameNPC npc in npcs)
-				{
-					if (npc.CurrentRegionID == 200 && npc.X == 334866 && npc.Y == 420749)
+					if (npc.CurrentRegionID == 249 && npc.X == 46083 && npc.Y == 39681)
 					{
-						ReyHib = npc;
+						Stefano = npc;
 						break;
 					}
-				}
 
-			if (ReyHib == null)
+			if (Stefano == null)
 			{
 				if (log.IsWarnEnabled)
-					log.Warn("Could not find Rey , creating it ...");
-				ReyHib = new GameNPC();
-				ReyHib.Model = 26;
-				ReyHib.Name = "Rey";
-				ReyHib.GuildName = "Bone Collector";
-				ReyHib.Realm = eRealm.Hibernia;
-				//Druim Ligen Location
-				ReyHib.CurrentRegionID = 200;
-				ReyHib.Size = 60;
-				ReyHib.Level = 59;
-				ReyHib.X = 334866;
-				ReyHib.Y = 420749;
-				ReyHib.Z = 5184;
-				ReyHib.Heading = 1640;
-				ReyHib.Flags |= GameNPC.eFlags.PEACE;
-				ReyHib.AddToWorld();
+					log.Warn("Could not find Stefano , creating it ...");
+				Stefano = new GameNPC();
+				Stefano.Model = 306;
+				Stefano.Name = "Stefano";
+				Stefano.GuildName = "Realm Logistics";
+				Stefano.Realm = eRealm.Hibernia;
+				//Darkness Falls Hib Entrance Location
+				Stefano.CurrentRegionID = 249;
+				Stefano.Size = 50;
+				Stefano.Level = 59;
+				Stefano.X = 46083;
+				Stefano.Y = 39681;
+				Stefano.Z = 21357;
+				Stefano.Heading = 3066;
+				Stefano.AddToWorld();
 				if (SAVE_INTO_DATABASE)
 				{
-					ReyHib.SaveIntoDatabase();
+					Stefano.SaveIntoDatabase();
 				}
 			}
 
@@ -116,11 +113,10 @@ namespace DOL.GS.DailyQuest.Hibernia
 			GameEventMgr.AddHandler(GamePlayerEvent.AcceptQuest, new DOLEventHandler(SubscribeQuest));
 			GameEventMgr.AddHandler(GamePlayerEvent.DeclineQuest, new DOLEventHandler(SubscribeQuest));
 
-			GameEventMgr.AddHandler(ReyHib, GameObjectEvent.Interact, new DOLEventHandler(TalkToDean));
-			GameEventMgr.AddHandler(ReyHib, GameLivingEvent.WhisperReceive, new DOLEventHandler(TalkToDean));
+			GameEventMgr.AddHandler(Stefano, GameObjectEvent.Interact, new DOLEventHandler(TalkToStefano));
+			GameEventMgr.AddHandler(Stefano, GameLivingEvent.WhisperReceive, new DOLEventHandler(TalkToStefano));
 
-			/* Now we bring to Dean the possibility to give this quest to players */
-			ReyHib.AddQuestToGive(typeof (DFWeeklyKillQuestHib));
+			Stefano.AddQuestToGive(typeof (DFWeeklyKillQuestHib));
 
 			if (log.IsInfoEnabled)
 				log.Info("Quest \"" + questTitle + "\" initialized");
@@ -130,27 +126,26 @@ namespace DOL.GS.DailyQuest.Hibernia
 		public static void ScriptUnloaded(DOLEvent e, object sender, EventArgs args)
 		{
 			//if not loaded, don't worry
-			if (ReyHib == null)
+			if (Stefano == null)
 				return;
 			// remove handlers
 			GameEventMgr.RemoveHandler(GamePlayerEvent.AcceptQuest, new DOLEventHandler(SubscribeQuest));
 			GameEventMgr.RemoveHandler(GamePlayerEvent.DeclineQuest, new DOLEventHandler(SubscribeQuest));
 
-			GameEventMgr.RemoveHandler(ReyHib, GameObjectEvent.Interact, new DOLEventHandler(TalkToDean));
-			GameEventMgr.RemoveHandler(ReyHib, GameLivingEvent.WhisperReceive, new DOLEventHandler(TalkToDean));
+			GameEventMgr.RemoveHandler(Stefano, GameObjectEvent.Interact, new DOLEventHandler(TalkToStefano));
+			GameEventMgr.RemoveHandler(Stefano, GameLivingEvent.WhisperReceive, new DOLEventHandler(TalkToStefano));
 
-			/* Now we remove to Dean the possibility to give this quest to players */
-			ReyHib.RemoveQuestToGive(typeof (DFWeeklyKillQuestHib));
+			Stefano.RemoveQuestToGive(typeof (DFWeeklyKillQuestHib));
 		}
 
-		private static void TalkToDean(DOLEvent e, object sender, EventArgs args)
+		private static void TalkToStefano(DOLEvent e, object sender, EventArgs args)
 		{
 			//We get the player from the event arguments and check if he qualifies		
 			GamePlayer player = ((SourceEventArgs) args).Source as GamePlayer;
 			if (player == null)
 				return;
 
-			if(ReyHib.CanGiveQuest(typeof (DFWeeklyKillQuestHib), player)  <= 0)
+			if(Stefano.CanGiveQuest(typeof (DFWeeklyKillQuestHib), player)  <= 0)
 				return;
 
 			//We also check if the player is already doing the quest
@@ -163,16 +158,16 @@ namespace DOL.GS.DailyQuest.Hibernia
 					switch (quest.Step)
 					{
 						case 1:
-							ReyHib.SayTo(player, "Please enter Darkness Falls and harvest parts from Hibernia's enemies!");
+							Stefano.SayTo(player, "Please head into Darkness Falls and harvest parts from Hibernia's enemies!");
 							break;
 						case 2:
-							ReyHib.SayTo(player, "Hello " + player.Name + ", did you [find the bones] we needed?");
+							Stefano.SayTo(player, "Hello " + player.Name + ", did you [find the bones] we needed?");
 							break;
 					}
 				}
 				else
 				{
-					ReyHib.SayTo(player, "Oh, "+ player.Name +", glad you finally returned. Boss has a new recipe that requires bones that have been steeped in a [demonic aura]. \n"+
+					Stefano.SayTo(player, "Oh, "+ player.Name +", glad you finally returned. Boss has a new recipe that requires bones that have been steeped in a [demonic aura]. \n"+
 					                   "Sure hope you know what that means, because I sure don't. My best guess is to try looking in Darkness Falls.");
 				}
 			}
@@ -185,7 +180,7 @@ namespace DOL.GS.DailyQuest.Hibernia
 					switch (wArgs.Text)
 					{
 						case "demonic aura":
-							player.Out.SendQuestSubscribeCommand(ReyHib, QuestMgr.GetIDForQuestType(typeof(DFWeeklyKillQuestHib)), "Will you help Rey with "+questTitle+"?");
+							player.Out.SendQuestSubscribeCommand(Stefano, QuestMgr.GetIDForQuestType(typeof(DFWeeklyKillQuestHib)), "Will you help Stefano with "+questTitle+"?");
 							break;
 					}
 				}
@@ -262,7 +257,7 @@ namespace DOL.GS.DailyQuest.Hibernia
 
 		private static void CheckPlayerAcceptQuest(GamePlayer player, byte response)
 		{
-			if(ReyHib.CanGiveQuest(typeof (DFWeeklyKillQuestHib), player)  <= 0)
+			if(Stefano.CanGiveQuest(typeof (DFWeeklyKillQuestHib), player)  <= 0)
 				return;
 
 			if (player.IsDoingQuest(typeof (DFWeeklyKillQuestHib)) != null)
@@ -275,10 +270,10 @@ namespace DOL.GS.DailyQuest.Hibernia
 			else
 			{
 				//Check if we can add the quest!
-				if (!ReyHib.GiveQuest(typeof (DFWeeklyKillQuestHib), player, 1))
+				if (!Stefano.GiveQuest(typeof (DFWeeklyKillQuestHib), player, 1))
 					return;
 
-				ReyHib.SayTo(player, "Find your realm's enemies in Darkness Falls and kill them for your reward.");
+				Stefano.SayTo(player, "Find your realm's enemies in Darkness Falls and kill them for your reward.");
 
 			}
 		}
@@ -299,7 +294,7 @@ namespace DOL.GS.DailyQuest.Hibernia
 					case 1:
 						return "Kill Hibernia's enemies in Darkness Falls. \nKilled: Enemies ("+ EnemiesKilled +" | "+MAX_KILLED+")";
 					case 2:
-						return "Return to Rey for your Reward.";
+						return "Return to Stefano in Darkness Falls for your Reward.";
 				}
 				return base.Description;
 			}
@@ -327,7 +322,6 @@ namespace DOL.GS.DailyQuest.Hibernia
 					
 			if (EnemiesKilled >= MAX_KILLED)
 			{
-				// FinishQuest or go back to Dean
 				Step = 2;
 			}
 
