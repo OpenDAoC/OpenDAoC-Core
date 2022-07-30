c1ba0874194b044c1c463c7816d43a926f57ea20
Glimmer Warshade <glimmer@europe.com>
2022-07-30 12:51:35 +0200

    Fate Chosen for caer sidi script added.

---
+++ GameServer/scripts/namedmobs/CaerSidi/FateChosen.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using DOL.AI.Brain;
+using DOL.Database;
+using DOL.Events;
+using DOL.GS;
+
+namespace DOL.GS
+{
+	public class FateChosen : GameEpicBoss
+	{
+		public FateChosen() : base() 
+		{
+		}
+
+		[ScriptLoadedEvent]
+		public static void ScriptLoaded(DOLEvent e, object sender, EventArgs args)
+		{
+			if (log.IsInfoEnabled)
+				log.Info("Fate's Chosen Initializing...");
+		}
+		public override int GetResist(eDamageType damageType)
+		{
+			switch (damageType)
+			{
+				case eDamageType.Slash: return 40;// dmg reduction for melee dmg
+				case eDamageType.Crush: return 40;// dmg reduction for melee dmg
+				case eDamageType.Thrust: return 40;// dmg reduction for melee dmg
+				default: return 70;// dmg reduction for rest resists
+			}
+		}
+		public override double AttackDamage(InventoryItem weapon)
+		{
+			return base.AttackDamage(weapon) * Strength / 100;
+		}
+		public override int AttackRange
+		{
+			get { return 350; }
+			set { }
+		}
+		public override double GetArmorAF(eArmorSlot slot)
+		{
+			return 350;
+		}
+		public override double GetArmorAbsorb(eArmorSlot slot)
+		{
+			// 85% ABS is cap.
+			return 0.20;
+		}
+		public override int MaxHealth
+		{
+			get { return 40000; }
+		}
+		public override bool AddToWorld()
+		{
+			GameNpcInventoryTemplate template = new GameNpcInventoryTemplate();
+			template.AddNPCEquipment(eInventorySlot.TwoHandWeapon, 567, 0);
+			Inventory = template.CloseTemplate();
+			SwitchWeapon(eActiveWeaponSlot.TwoHanded);
+
+			INpcTemplate npcTemplate = NpcTemplateMgr.GetTemplate(50040);
+			LoadTemplate(npcTemplate);
+			Strength = npcTemplate.Strength;
+			Dexterity = npcTemplate.Dexterity;
+			Constitution = npcTemplate.Constitution;
+			Quickness = npcTemplate.Quickness;
+			Piety = npcTemplate.Piety;
+			Intelligence = npcTemplate.Intelligence;
+			Empathy = npcTemplate.Empathy;
+
+			RespawnInterval = ServerProperties.Properties.SET_SI_EPIC_ENCOUNTER_RESPAWNINTERVAL * 60000;//1min is 60000 miliseconds
+			FateChosenBrain sbrain = new FateChosenBrain();
+			SetOwnBrain(sbrain);
+			base.AddToWorld();
+			return true;
+		}
+	}
+}
+namespace DOL.AI.Brain
+{
+	public class FateChosenBrain : StandardMobBrain
+	{
+		private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		public FateChosenBrain() : base()
+		{
+			AggroLevel = 100;
+			AggroRange = 600;
+			ThinkInterval = 1500;
+		}
+
+		public override void Think()
+		{
+			if (!HasAggressionTable())
+			{
+				//set state to RETURN TO SPAWN
+				FSM.SetCurrentState(eFSMStateType.RETURN_TO_SPAWN);
+				Body.Health = Body.MaxHealth;
+			}
+			base.Think();
+		}
+	}
+}
+
