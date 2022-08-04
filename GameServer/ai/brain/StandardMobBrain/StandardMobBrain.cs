4d8c84293ff5058d6ce042321b941a97b036c8b3
flame <jparrott7@gmail.com>
2022-08-03 22:15:43 -0500

    set merchants/trainers/hasteners to 5000 think interval. add random offset for think time for mobs

--- GameServer/ai/brain/StandardMobBrain/StandardMobBrain.cs
+++ GameServer/ai/brain/StandardMobBrain/StandardMobBrain.cs
@@ -491,7 +491,12 @@ namespace DOL.AI.Brain
         /// 10 seconds for 0 aggro mobs
         /// </summary>
         public override int ThinkInterval {
-            get { return Math.Max(500, 1500 - (AggroLevel/10) * 100); }
+            get { 
+                
+                if(Body is GameMerchant || Body is GameTrainer || Body is GameHastener)
+                    return 5000; //Merchants and other special NPCs don't need to think that often
+
+                return Math.Max(500, 1500 - (AggroLevel/10) * 100); }
         }
 
         /// <summary>
