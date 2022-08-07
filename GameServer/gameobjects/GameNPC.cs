c5b8d989f0dd96e8f9ddc0e154e86771fa1b98e0
Navelator <>
2022-08-07 04:46:20 +0200

    - Standard Mobs die properly now since the reaper is called when pets die far away from their spawnpoint (killer == null)

--- GameServer/gameobjects/GameNPC.cs
+++ GameServer/gameobjects/GameNPC.cs
@@ -60,7 +60,7 @@ namespace DOL.GS
 		/// This helps to reduce the turning of an npc while fighting or returning to a spawn
 		/// Tested - min distance for mob sticking within combat range to player is 25
 		/// </remarks>
-		public const int CONST_WALKTOTOLERANCE = 25;
+		public const int CONST_WALKTOTOLERANCE = 10;
 
 		private int m_databaseLevel;
 		
@@ -4401,10 +4401,10 @@ namespace DOL.GS
 				Diagnostics.StartPerfCounter("ReaperService-NPC-ProcessDeath-OnNPCKIlled-NPC("+this.GetHashCode()+")");
 				GameServer.ServerRules.OnNPCKilled(this, killer);
 				Diagnostics.StopPerfCounter("ReaperService-NPC-ProcessDeath-OnNPCKIlled-NPC("+this.GetHashCode()+")");
-
-				base.ProcessDeath(killer);
 			}
-			
+
+			base.ProcessDeath(killer);
+
 			lock (this.XPGainers.SyncRoot)
 				this.XPGainers.Clear();
 			
