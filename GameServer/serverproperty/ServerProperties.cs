eb59a53278f15c9459f84e0d3b260095f16fd970
Anthony Knight <midgetlord123@hotmail.com>
2022-08-14 13:26:36 -0700

    Add FLAG_CAPTURE_TIME server prop

--- GameServer/serverproperty/ServerProperties.cs
+++ GameServer/serverproperty/ServerProperties.cs
@@ -1607,6 +1607,9 @@ namespace DOL.GS.ServerProperties
 		[ServerProperty("conquest", "flag_capture_radius", "How far away can players capture an objective?", 750)]
 		public static ushort FLAG_CAPTURE_RADIUS;
 		
+		[ServerProperty("conquest", "flag_capture_time", "How long does it take to capture a flag?", 20)]
+		public static ushort FLAG_CAPTURE_TIME;
+		
 		[ServerProperty("conquest", "subtick_rp_award", "How many RPs awarded for a participation tick?", 200)]
 		public static int SUBTICK_RP_AWARD;
 		
