f2c48263f72e13e3003f4e2c6e3b86ff3a4a2fb9
Anthony Knight <midgetlord123@hotmail.com>
2022-08-26 14:51:44 -0700

    Daily player kill quests award rog minimum 55-65

--- GameServer/scripts/quests/AtlasQuests/DailyQuests/Hibernia/RvR/Daily/EveryLittleBitHelpsQuestHib.cs
+++ GameServer/scripts/quests/AtlasQuests/DailyQuests/Hibernia/RvR/Daily/EveryLittleBitHelpsQuestHib.cs
@@ -367,7 +367,7 @@ namespace DOL.GS.DailyQuest.Hibernia
 			m_questPlayer.ForceGainExperience((m_questPlayer.ExperienceForNextLevel - m_questPlayer.ExperienceForCurrentLevel)/5);
 			m_questPlayer.AddMoney(Money.GetMoney(0,0,m_questPlayer.Level,32,Util.Random(50)), "You receive {0} as a reward.");
 			AtlasROGManager.GenerateOrbAmount(m_questPlayer, 250);
-			AtlasROGManager.GenerateJewel(m_questPlayer, 65);
+			AtlasROGManager.GenerateJewel(m_questPlayer, (byte)(m_questPlayer.Level + 1), m_questPlayer.Level + Util.Random(5, 15));
 			_playersKilledAlb = 0;
 			_playersKilledMid = 0;
 			
--- GameServer/scripts/quests/AtlasQuests/DailyQuests/Midgard/RvR/Daily/EveryLittleBitHelpsQuestMid.cs
+++ GameServer/scripts/quests/AtlasQuests/DailyQuests/Midgard/RvR/Daily/EveryLittleBitHelpsQuestMid.cs
@@ -365,7 +365,7 @@ namespace DOL.GS.DailyQuest.Midgard
 			m_questPlayer.ForceGainExperience((m_questPlayer.ExperienceForNextLevel - m_questPlayer.ExperienceForCurrentLevel)/5);
 			m_questPlayer.AddMoney(Money.GetMoney(0,0,m_questPlayer.Level,32,Util.Random(50)), "You receive {0} as a reward.");
 			AtlasROGManager.GenerateOrbAmount(m_questPlayer, 250);
-			AtlasROGManager.GenerateJewel(m_questPlayer, 65);
+			AtlasROGManager.GenerateJewel(m_questPlayer, (byte)(m_questPlayer.Level + 1), m_questPlayer.Level + Util.Random(5, 15));
 			_playersKilledHib = 0;
 			_playersKilledAlb = 0;
 			
