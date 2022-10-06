f9470e31952c7595ac4d178d72ca0a6ba609d476
Baptiste Marie <bm01@users.noreply.github.com>
2022-10-06 05:02:32 +0200

    Fix /friend inconsistencies

--- GameServer/commands/playercommands/friend.cs
+++ GameServer/commands/playercommands/friend.cs
@@ -16,8 +16,8 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
+using System;
 using System.Linq;
-
 using DOL.GS.Friends;
 
 namespace DOL.GS.Commands
@@ -44,57 +44,89 @@ namespace DOL.GS.Commands
 			
 			string name = string.Join(" ", args, 1, args.Length - 1);
 
-			int result = 0;
-			GameClient fclient = WorldMgr.GuessClientByPlayerNameAndRealm(name, 0, false, out result);
-			if (fclient != null && !GameServer.ServerRules.IsSameRealm(fclient.Player, client.Player, true))
-			{
-				fclient = null;
-			}
+			// attempt to remove from friends list now to avoid being unable to do so because of a guessed name from an online player
+			if (RemoveFriend(name, client))
+				return;
 
-			if (fclient == null)
+			int result;
+			GameClient fclient = WorldMgr.GuessClientByPlayerNameAndRealm(name, 0, false, out result);
+
+			// abort if the returned player is from a hostile realm
+			if (fclient != null)
 			{
-				name = args[1];
-				if (client.Player.GetFriends().Contains(name) && client.Player.RemoveFriend(name))
-				{
-					DisplayMessage(client, name + " was removed from your friend list!");
-					return;
-				}
-				else
-				{
-					// nothing found
-					DisplayMessage(client, "No players online with that name.");
-					return;
-				}
+				name = fclient.Player.Name;
+
+				if (!GameServer.ServerRules.IsSameRealm(fclient.Player, client.Player, true))
+					result = 1;
 			}
 
 			switch (result)
 			{
-				case 2:
+				case 1: // not found
 					{
-						// name not unique
+						DisplayMessage(client, "No players with that name, or you cannot add this player.");
+						return;
+					}
+				case 2: // not unique
+					{
 						DisplayMessage(client, "Character name is not unique.");
-						break;
+						return;
 					}
 				case 3: // exact match
-				case 4: // guessed name
 					{
-						if (fclient == client)
+						if (IsAddingSelf(fclient, client))
+							return;
+
+						AddFriend(name, client);
+						return;
+					}
+				case 4: // guessed
+					{
+						if (IsAddingSelf(fclient, client))
+							return;
+
+						if (IsNameInFriendsList(name, client))
 						{
-							DisplayMessage(client, "You can't add yourself!");
+							DisplayMessage(client, "Type the full name to remove " + name + " from your list.");
 							return;
 						}
 
-						name = fclient.Player.Name;
-						if (client.Player.GetFriends().Contains(name) && client.Player.RemoveFriend(name))
-						{
-							DisplayMessage(client, name + " was removed from your friend list!");
-						}
-						else if (client.Player.AddFriend(name))
-						{
-							DisplayMessage(client, name + " was added to your friend list!");
-						}
-						break;
+						AddFriend(name, client);
+						return;
 					}
+			}
+		}
+
+		private bool IsAddingSelf(GameClient fclient, GameClient client)
+		{
+			if (fclient == client)
+			{
+				DisplayMessage(client, "You can't add yourself!");
+				return true;
+			}
+			return false;
+		}
+
+		private bool IsNameInFriendsList(string name, GameClient client, StringComparer comparer = null)
+		{
+			return client.Player.GetFriends().Contains(name, comparer);
+		}
+
+		private bool RemoveFriend(string name, GameClient client)
+		{
+			if (IsNameInFriendsList(name, client, StringComparer.OrdinalIgnoreCase) && client.Player.RemoveFriend(name))
+			{
+				DisplayMessage(client, name + " was removed from your friend list!");
+				return true;
+			}
+			return false;
+		}
+
+		private void AddFriend(string name, GameClient client)
+		{
+			if (client.Player.AddFriend(name))
+			{
+				DisplayMessage(client, name + " was added to your friend list!");
 			}
 		}
 	}
