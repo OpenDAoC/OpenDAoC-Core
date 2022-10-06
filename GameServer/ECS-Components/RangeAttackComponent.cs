d3709caa3b180bb42db11150383c8433392e304b
Baptiste Marie <bm01@users.noreply.github.com>
2022-10-06 23:46:04 +0200

    Fix Arrow Salvaging working backwards (#546)

--- GameServer/ECS-Components/RangeAttackComponent.cs
+++ GameServer/ECS-Components/RangeAttackComponent.cs
@@ -382,14 +382,10 @@ namespace DOL.GS
                 case eAttackResult.HitStyle:
                 case eAttackResult.Fumbled:
                     // remove an arrow and endurance
-                    InventoryItem ammo = RangeAttackAmmo;
-                    if (owner.GetModified(eProperty.ArrowRecovery) > 0 &&
-                        Util.Chance(100 - owner.GetModified(eProperty.ArrowRecovery)))
-                    {
-                        //do not remove an arrow
-                    }
-                    else
-                        owner.Inventory.RemoveCountFromStack(ammo, 1); //remove arrow
+                    int arrowRecoveryChance = owner.GetModified(eProperty.ArrowRecovery);
+
+                    if (arrowRecoveryChance == 0 || Util.Chance(100 - arrowRecoveryChance))
+                        owner.Inventory.RemoveCountFromStack(RangeAttackAmmo, 1);
 
                     if (RangedAttackType == eRangedAttackType.Critical)
                         owner.Endurance -= CRITICAL_SHOT_ENDURANCE;
