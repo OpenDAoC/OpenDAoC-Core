OpenDAoC/OpenDAoC-Core

commit 2a8542576ca9e1609b3d31ab102d77a4f879e216

Allow beneficial effects to be applied in a disabled state when weaker than an existing one Also made it so that we skip disabled effects, and break as soon as we want to add an effect. I believe the previous implementation was faulty and could allow effects to be removed without having any added

GameServer/ECS-Components/EffectList/EffectListComponent.cs
@@ -130,13 +130,14 @@ namespace DOL.GS
                         else
                         {
                             bool addEffect = false;
+                            bool addEffectInDisabledState = false;
                             // foundIsOverwriteableEffect is a bool for if we find an overwriteable effect when looping over existing effects. Will be used to later to add effects that are not in same effect group.
                             bool foundIsOverwriteableEffect = false;
                             // Check to see if we can add new Effect
                             for (int i = 0; i < existingEffects.Count; i++)
                             {
                                 ECSGameSpellEffect existingEffect = existingEffects[i];
-                                ISpellHandler existingSpellHandler = existingEffects[i].SpellHandler;
+                                ISpellHandler existingSpellHandler = existingEffect.SpellHandler;
                                 Spell existingSpell = existingSpellHandler.Spell;
 
                                 // Check if existingEffect is overwritable by new effect
@@ -145,21 +146,33 @@ namespace DOL.GS
                                     foundIsOverwriteableEffect = true;
                                     if (effect.EffectType != eEffect.Bladeturn)
                                     {
+                                        if (existingEffect.IsDisabled)
+                                            continue;
+
                                         // New Effect is better than the current enabled effect so disable the current Effect and add the new effect.
-                                        if ((newSpell.Value > existingSpell.Value || newSpell.Damage > existingSpell.Damage)
-                                            && !existingEffects[i].IsDisabled)
+                                        if (newSpell.Value > existingSpell.Value || newSpell.Damage > existingSpell.Damage)
+                                        {
+                                            if (newSpell.IsHelpful&& (newSpellHandler.Caster != existingSpellHandler.Caster
+                                                || newSpellHandler.SpellLine.KeyName == GlobalSpellsLines.Potions_Effects
+                                                || existingSpellHandler.SpellLine.KeyName == GlobalSpellsLines.Potions_Effects))
+                                                EffectService.RequestDisableEffect(existingEffect);
+                                            else
+                                                EffectService.RequestCancelEffect(existingEffect);
+                                            addEffect = true;
+                                            break;
+                                        }
+                                        // New Effect is not as good as current effect, but it can be added in a disabled state.
+                                        else
                                         {
                                             if (newSpell.IsHelpful && (newSpellHandler.Caster != existingSpellHandler.Caster
                                                 || newSpellHandler.SpellLine.KeyName == GlobalSpellsLines.Potions_Effects
                                                 || existingSpellHandler.SpellLine.KeyName == GlobalSpellsLines.Potions_Effects))
-                                                EffectService.RequestDisableEffect(existingEffects[i]);
-                                            else
-                                                EffectService.RequestCancelEffect(existingEffects[i]);
-                                            addEffect = true;
+                                            {
+                                                addEffect = true;
+                                                addEffectInDisabledState = true;
+                                                break;
+                                            }
                                         }
-                                        // New Effect is not as good as current effect.
-                                        else if (newSpell.Value < existingSpell.Value || newSpell.Damage < existingSpell.Damage)
-                                            addEffect = false;
                                     }
                                     else
                                     {
@@ -185,6 +198,10 @@ namespace DOL.GS
                             {
                                 Effects[newSpellEffect.EffectType].Add(newSpellEffect);
                                 EffectIdToEffect.TryAdd(newSpellEffect.Icon, newSpellEffect);
+
+                                if (addEffectInDisabledState)
+                                    EffectService.RequestDisableEffect(newSpellEffect);
+
                                 return true;
                             }
                         }

commit 1dc4e50e57a4df1ba970733b0ff5716dbca1bd5d

Fix cases of stat (de)buffs persisting until the player logs out - EffectListComponent wasn't updating the EffectIdToEffect collection with the new effect, this caused a mismatch between what's stored in both collection and led to stats being added or removed when they shouldn't (for example buffing oneself twice with the same buff, then disabling that buff with a stronger conc-buff, and canceling the disabled icon would make the player lose stats). - Caused by 2a854257, EffectService would call OnStartEffect even if the applied buff was marked as Disabled. This would make the effects stack and also lead to a permanent stat buff after cancellation.

GameServer/ECS-Components/EffectList/EffectListComponent.cs
@@ -61,7 +61,7 @@ namespace DOL.GS
                             for (int i = 0; i < existingEffects.Count; i++)
                             {
                                 ECSGameSpellEffect existingEffect = existingEffects[i];
-                                ISpellHandler existingSpellHandler = existingEffects[i].SpellHandler;
+                                ISpellHandler existingSpellHandler = existingEffect.SpellHandler;
                                 Spell existingSpell = existingSpellHandler.Spell;
 
                                 // Console.WriteLine($"Effect found! Name: {existingEffect.Name}, Effectiveness: {existingEffect.Effectiveness}, Poison: {existingSpell.IsPoisonEffect}, ID: {existingSpell.ID}, EffectGroup: {existingSpell.EffectGroup}");
@@ -91,6 +91,7 @@ namespace DOL.GS
                                         newSpellEffect.IsBuffActive = existingEffect.IsBuffActive;
                                         newSpellEffect.PreviousPosition = GetAllEffects().IndexOf(existingEffect);
                                         Effects[newSpellEffect.EffectType][i] = newSpellEffect;
+                                        EffectIdToEffect[newSpellEffect.Icon] = newSpellEffect;
                                     }
                                     return true;
                                 }

GameServer/ECS-Services/EffectService.cs
@@ -126,7 +126,7 @@ namespace DOL.GS
                     if (e is StatDebuffECSEffect && spellEffect.SpellHandler.Spell.CastTime == 0)
                         StatDebuffECSEffect.TryDebuffInterrupt(spellEffect.SpellHandler.Spell, e.OwnerPlayer, spellEffect.SpellHandler.Caster);
 
-                    if ((spellEffect.SpellHandler.Spell.IsConcentration && !spellEffect.SpellHandler.Spell.IsPulsing) || (!spellEffect.IsBuffActive && !spellEffect.IsDisabled)
+                    if ((!spellEffect.IsBuffActive && !spellEffect.IsDisabled)
                         || spellEffect is SavageBuffECSGameEffect)
                     {
                         //if (spellEffect.EffectType == eEffect.EnduranceRegenBuff)
@@ -191,34 +191,15 @@ namespace DOL.GS
 
         private static void HandleCancelEffect(ECSGameEffect e)
         {
-            //Console.WriteLine($"Handling Cancel Effect {e.SpellHandler.ToString()}");
             EntityManager.RemoveEffect(e);
             if (!e.Owner.effectListComponent.RemoveEffect(e))
-            {
-                //Console.WriteLine("Unable to remove effect!");
                 return;
-            }
 
             ECSGameSpellEffect spellEffect = e as ECSGameSpellEffect;
             if (spellEffect != null)
             {
-                if (!spellEffect.IsBuffActive)
-                {
-                    //Console.WriteLine("Buff not active! {0} on {1}", e.SpellHandler.Spell.Name, e.Owner.Name);
-                }
-                else if (spellEffect.EffectType != eEffect.Pulse)
-                {
-                    if (!(spellEffect is ECSImmunityEffect))
-                    {
-                        //if (spellEffect.EffectType == eEffect.EnduranceRegenBuff)
-                        //{
-                        //    //Console.WriteLine("Removing EnduranceRegenBuff");
-                        //    var handler = spellEffect.SpellHandler as EnduranceRegenSpellHandler;
-                        //    ApplyBonus(spellEffect.Owner, handler.BonusCategory1, handler.Property1, spellEffect.SpellHandler.Spell.Value, spellEffect.Effectiveness, true);
-                        //}
-                        e.OnStopEffect();
-                    }
-                }
+                if (spellEffect.IsBuffActive && spellEffect.EffectType != eEffect.Pulse && !(spellEffect is ECSImmunityEffect))
+                    e.OnStopEffect();
 
                 e.IsBuffActive = false;
                 // Update the Concentration List if Conc Buff/Song/Chant.