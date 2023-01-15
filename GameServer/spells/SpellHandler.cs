OpenDAoC/OpenDAoC-Core

commit ff5b975ae8419ee741cb8a7f6e8d1113ec18c024

Improve 'AttackWeapon' caching Caching it in 'StartAttack' was error prone since 'AttackComponent' is a permanent object and doesn't just reflect the current attack. It's also no longer necesarry for delayed attacks to work properly since the weapon that was used for the attack is now passed to relevant methods directly. Since 'AttackWeapon' became again a copy of 'EquippedMainWeapon', only the latter was kept, and renamed. It's called multiple times per attack, and since it calls 'GameInventory.GetItem' which locks the inventory , it may be quite expensive. For this reason 'ActiveWeapon' will cache the weapon for the current game loop tick.

GameServer/spells/SpellHandler.cs
@@ -329,7 +329,7 @@ namespace DOL.GS.Spells
 		/// <returns>true if right instrument</returns>
 		protected bool CheckInstrument()
 		{
-			InventoryItem instrument = Caster.EquippedMainWeapon;
+			InventoryItem instrument = Caster.ActiveWeapon;
 			// From patch 1.97:  Flutes, Lutes, and Drums will now be able to play any song type, and will no longer be limited to specific songs.
 			if (instrument == null || instrument.Object_Type != (int)eObjectType.Instrument ) // || (instrument.DPS_AF != 4 && instrument.DPS_AF != m_spell.InstrumentRequirement))
 			{
@@ -2206,7 +2206,7 @@ namespace DOL.GS.Spells
 		public virtual void FinishSpellCast(GameLiving target)
 		{
 			GamePlayer playerCaster = Caster as GamePlayer;
-			GameInventoryItem playerWeapon = playerCaster?.EquippedMainWeapon as GameInventoryItem;
+			GameInventoryItem playerWeapon = playerCaster.ActiveWeapon as GameInventoryItem;
 
 			if (playerCaster != null)
 			{
@@ -3112,7 +3112,7 @@ namespace DOL.GS.Spells
 			duration *= (1.0 + m_caster.GetModified(eProperty.SpellDuration) * 0.01);
 			if (Spell.InstrumentRequirement != 0)
 			{
-				InventoryItem instrument = Caster.EquippedMainWeapon;
+				InventoryItem instrument = Caster.ActiveWeapon;
 				if (instrument != null)
 				{
 					duration *= 1.0 + Math.Min(1.0, instrument.Level / (double)Caster.Level); // up to 200% duration for songs
@@ -4253,9 +4253,9 @@ namespace DOL.GS.Spells
 				}
 				else if (SpellLine.KeyName == GlobalSpellsLines.Combat_Styles_Effect)
 				{
-					double weaponskillScalar = (3 + .02 * player.GetWeaponStat(player.EquippedMainWeapon)) /
-					                           (1 + .005 * player.GetWeaponStat(player.EquippedMainWeapon));
-					spellDamage *= (player.GetWeaponSkill(player.EquippedMainWeapon) * weaponskillScalar /3 + 200) / 200;
+					double weaponskillScalar = (3 + .02 * player.GetWeaponStat(player.ActiveWeapon)) /
+					                           (1 + .005 * player.GetWeaponStat(player.ActiveWeapon));
+					spellDamage *= (player.GetWeaponSkill(player.ActiveWeapon) * weaponskillScalar /3 + 200) / 200;
 				}
 				else if (player.CharacterClass.ManaStat != eStat.UNDEFINED
 				    && SpellLine.KeyName != GlobalSpellsLines.Combat_Styles_Effect

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -68,7 +68,7 @@ namespace DOL.GS
             }
 
             _attackData = _owner.TempProperties.getProperty<object>(LAST_ATTACK_DATA, null) as AttackData;
-            _weapon = _attackComponent.AttackWeapon;
+            _weapon = _owner.ActiveWeapon;
             _leftWeapon = _owner.Inventory?.GetItem(eInventorySlot.LeftHandWeapon);
             _effectiveness = _owner.Effectiveness;
 
@@ -195,10 +195,11 @@ namespace DOL.GS
             }
 
             _interval = _attackComponent.AttackSpeed(_weapon);
+            _interruptDuration = _interval;
             _ticksToTarget = _owner.GetDistanceTo(_target) * 1000 / 1800; // 1800 units per second. Live value is unknown, but DoL had 1500.
             int model = _weapon == null ? 0 : _weapon.Model;
             byte flightDuration = (byte)(_ticksToTarget > 350 ? 1 + (_ticksToTarget - 350) / 75 : 1);
-            bool cancelPrepareAnimation = _owner.attackComponent.AttackWeapon.Object_Type == (int)eObjectType.Thrown;
+            bool cancelPrepareAnimation = _owner.ActiveWeapon.Object_Type == (int)eObjectType.Thrown;
 
             Parallel.ForEach(_owner.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE).OfType<GamePlayer>(), player =>
             {
@@ -215,9 +216,7 @@ namespace DOL.GS
                 // 1 means roughly 350ms (the lowest time possible), then each increment adds about 75ms (needs testing).
                 // Using ticksToTarget, we can make the arrow take more time to reach its target the farther it is.
                 player.Out.SendCombatAnimation(_owner, _target, (ushort)model, 0x00, player.Out.BowShoot, flightDuration, 0x00, ((GameLiving)_target).HealthPercent);
-            });
-
-            _interruptDuration = _attackComponent.AttackSpeed(_weapon);
+            });            
 
             switch (_owner.rangeAttackComponent.RangedAttackType)
             {

GameServer/ECS-Components/Actions/NpcAttackAction.cs
@@ -30,7 +30,7 @@ namespace DOL.GS
                 !_npcOwner.IsWithinRadius(_target, 500))
             {
                 _npcOwner.SwitchToRanged(_target);
-                _interval = _attackComponent.AttackSpeed(_attackComponent.AttackWeapon);
+                _interval = _attackComponent.AttackSpeed(_weapon);
                 return false;
             }
 

GameServer/ECS-Components/Actions/PlayerAttackAction.cs
@@ -32,7 +32,7 @@ namespace DOL.GS
             string attackTypeMsg;
             GameObject attacker = _playerOwner.attackComponent.Attackers.Last();
 
-            if (_playerOwner.attackComponent.AttackWeapon != null && _playerOwner.attackComponent.AttackWeapon.Object_Type == (int)eObjectType.Thrown)
+            if (_playerOwner.ActiveWeapon != null && _playerOwner.ActiveWeapon.Object_Type == (int)eObjectType.Thrown)
                 attackTypeMsg = LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.Attack.Type.Throw");
             else
                 attackTypeMsg = LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.Attack.Type.Shot");

GameServer/ECS-Components/Actions/WeaponAction.cs
@@ -174,7 +174,7 @@ namespace DOL.GS
                     //    RealmAbilities.L3RAPropertyEnhancer ra = living.GetAbility<RealmAbilities.ReflexAttackAbility>();
                     //    if (ra != null && Util.Chance(ra.Amount))
                     //    {
-                    //        AttackData ReflexAttackAD = living.attackComponent.LivingMakeAttack(owner, living.attackComponent.AttackWeapon, null, 1, m_interruptDuration, false, true);
+                    //        AttackData ReflexAttackAD = living.attackComponent.LivingMakeAttack(owner, living.ActiveWeapon, null, 1, m_interruptDuration, false, true);
                     //        living.DealDamage(ReflexAttackAD);
                     //        living.SendAttackingCombatMessages(ReflexAttackAD);
                     //    }
@@ -371,7 +371,7 @@ namespace DOL.GS
                 case eAttackResult.Blocked:
                 case eAttackResult.Evaded:
                 case eAttackResult.Parried:
-                    AttackData ReflexAttackAD = target.attackComponent.LivingMakeAttack(attacker, target.attackComponent.AttackWeapon, null, 1, interruptDuration, false, true);
+                    AttackData ReflexAttackAD = target.attackComponent.LivingMakeAttack(attacker, target.ActiveWeapon, null, 1, interruptDuration, false, true);
                     target.DealDamage(ReflexAttackAD);
 
                     // If we get hit by Reflex Attack (it can miss), send a "you were hit" message to the attacker manually

GameServer/ECS-Components/AttackComponent.cs
@@ -93,10 +93,7 @@ namespace DOL.GS
             }
 
             if (weaponAction is null && attackAction is null && !owner.InCombat)
-            {
                 EntityManager.RemoveComponent(typeof(AttackComponent), owner);
-                AttackWeapon = null;
-            }
         }
 
 
@@ -293,10 +290,11 @@ namespace DOL.GS
             {
                 if (owner is GamePlayer)
                 {
-                    if (AttackWeapon == null)
+                    InventoryItem weapon = owner.ActiveWeapon;
+
+                    if (weapon == null)
                         return 0;
 
-                    InventoryItem weapon = AttackWeapon;
                     var player = owner as GamePlayer;
                     GameLiving target = player.TargetObject as GameLiving;
 
@@ -648,11 +646,8 @@ namespace DOL.GS
         public void StartAttack(GameObject attackTarget)
         {
             EntityManager.AddComponent(typeof(AttackComponent), owner);
-            
-            AttackWeapon = owner.EquippedMainWeapon;
-            GamePlayer player = owner as GamePlayer;
 
-            if (player != null)
+            if (owner is GamePlayer player)
             {
                 if (player.CharacterClass.StartAttack(attackTarget) == false)
                 {
@@ -736,7 +731,9 @@ namespace DOL.GS
                     player.Sit(false);
                 }
 
-                if (AttackWeapon == null)
+                InventoryItem attackWeapon = owner.ActiveWeapon;
+
+                if (attackWeapon == null)
                 {
                     player.Out.SendMessage(
                         LanguageMgr.GetTranslation(player.Client.Account.Language,
@@ -745,7 +742,7 @@ namespace DOL.GS
                     return;
                 }
 
-                if (AttackWeapon.Object_Type == (int) eObjectType.Instrument)
+                if (attackWeapon.Object_Type == (int) eObjectType.Instrument)
                 {
                     player.Out.SendMessage(
                         LanguageMgr.GetTranslation(player.Client.Account.Language, "GamePlayer.StartAttack.CannotMelee"),
@@ -767,7 +764,7 @@ namespace DOL.GS
                     }
 
                     // Check arrows for ranged attack
-                    if (player.rangeAttackComponent.UpdateAmmo(AttackWeapon) == null)
+                    if (player.rangeAttackComponent.UpdateAmmo(attackWeapon) == null)
                     {
                         player.Out.SendMessage(
                             LanguageMgr.GetTranslation(player.Client.Account.Language,
@@ -805,7 +802,7 @@ namespace DOL.GS
                     {
                         player.Out.SendMessage(
                             LanguageMgr.GetTranslation(player.Client.Account.Language, "GamePlayer.StartAttack.TiredUse",
-                                AttackWeapon.Name), eChatType.CT_YouHit, eChatLoc.CL_SystemWindow);
+                                attackWeapon.Name), eChatType.CT_YouHit, eChatLoc.CL_SystemWindow);
                         return;
                     }
 
@@ -881,7 +878,7 @@ namespace DOL.GS
                     player.TempProperties.setProperty(RangeAttackComponent.RANGED_ATTACK_START, GameLoop.GameLoopTime);
 
                     string typeMsg = "shot";
-                    if (AttackWeapon.Object_Type == (int) eObjectType.Thrown)
+                    if (attackWeapon.Object_Type == (int) eObjectType.Thrown)
                         typeMsg = "throw";
 
                     string targetMsg = "";
@@ -895,7 +892,7 @@ namespace DOL.GS
                                 "GamePlayer.StartAttack.TargetOutOfRange");
                     }
 
-                    int speed = AttackSpeed(AttackWeapon) / 100;
+                    int speed = AttackSpeed(attackWeapon) / 100;
                     if (player.rangeAttackComponent.RangedAttackType == eRangedAttackType.RapidFire)
                         speed = Math.Max(15, speed / 2);
 
@@ -929,8 +926,9 @@ namespace DOL.GS
                 owner.CancelEngageEffect();
 
             AttackState = true;
+            InventoryItem attackWeapon = owner.ActiveWeapon;
 
-            int speed = AttackSpeed(AttackWeapon);
+            int speed = AttackSpeed(attackWeapon);
 
             if (speed <= 0)
                 return;
@@ -958,7 +956,7 @@ namespace DOL.GS
                     {
                         // The 'stance' parameter appears to be used to tell whether or not the animation should be held, and doesn't seem to be related to the weapon speed.
                         foreach (GamePlayer player in owner.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
-                            player.Out.SendCombatAnimation(owner, null, (ushort)(AttackWeapon != null ? AttackWeapon.Model : 0), 0, player.Out.BowPrepare, 0x1A, 0x00, 0x00);
+                            player.Out.SendCombatAnimation(owner, null, (ushort)(attackWeapon != null ? attackWeapon.Model : 0), 0, player.Out.BowPrepare, 0x1A, 0x00, 0x00);
                     }
 
                     attackAction.StartTime = owner.rangeAttackComponent?.RangedAttackType == eRangedAttackType.RapidFire ? Math.Max(1500, speed / 2) : speed;
@@ -1241,7 +1239,7 @@ namespace DOL.GS
                             int random;
                             IList extraTargets = new ArrayList();
                             IList listAvailableTargets = new ArrayList();
-                            InventoryItem attackWeapon = AttackWeapon;
+                            InventoryItem attackWeapon = owner.ActiveWeapon;
                             InventoryItem leftWeapon = (p.Inventory == null)
                                 ? null
                                 : p.Inventory.GetItem(eInventorySlot.LeftHandWeapon);
@@ -2518,7 +2516,7 @@ namespace DOL.GS
                 {
                     // check player is wearing shield and NO two handed weapon
                     InventoryItem leftHand = guard.GuardSource.Inventory.GetItem(eInventorySlot.LeftHandWeapon);
-                    InventoryItem rightHand = guard.GuardSource.attackComponent.AttackWeapon;
+                    InventoryItem rightHand = guard.GuardSource.ActiveWeapon;
                     if (((rightHand == null || rightHand.Hand != 1) && leftHand != null &&
                          leftHand.Object_Type == (int) eObjectType.Shield) || guard.GuardSource is GameNPC)
                     {
@@ -2617,7 +2615,7 @@ namespace DOL.GS
                 {
                     // check player is wearing shield and NO two handed weapon
                     InventoryItem leftHand = dashing.GuardSource.Inventory.GetItem(eInventorySlot.LeftHandWeapon);
-                    InventoryItem rightHand = dashing.GuardSource.attackComponent.AttackWeapon;
+                    InventoryItem rightHand = dashing.GuardSource.ActiveWeapon;
                     InventoryItem twoHand = dashing.GuardSource.Inventory.GetItem(eInventorySlot.TwoHandWeapon);
                     if ((rightHand == null || rightHand.Hand != 1) && leftHand != null &&
                         leftHand.Object_Type == (int) eObjectType.Shield)
@@ -2989,9 +2987,9 @@ namespace DOL.GS
 
                     case eAttackResult.Parried:
                         resultByte = 1;
-                        if (target != null && target.attackComponent.AttackWeapon != null)
+                        if (target != null && target.ActiveWeapon != null)
                         {
-                            defendersWeapon = target.attackComponent.AttackWeapon.Model;
+                            defendersWeapon = target.ActiveWeapon.Model;
                         }
 
                         break;
@@ -3549,11 +3547,6 @@ namespace DOL.GS
         }
 
         /// <summary>
-        /// Returns the weapon for this attack, null=natural
-        /// </summary>
-        public virtual InventoryItem AttackWeapon { get; private set; }
-
-        /// <summary>
         /// Whether the living is actually attacking something.
         /// </summary>
         public virtual bool IsAttacking
@@ -3628,7 +3621,7 @@ namespace DOL.GS
 
                 // HtH chance
                 specLevel = owner.GetModifiedSpecLevel(Specs.HandToHand);
-                InventoryItem attackWeapon = AttackWeapon;
+                InventoryItem attackWeapon = owner.ActiveWeapon;
                 InventoryItem leftWeapon = (owner.Inventory == null)
                     ? null
                     : owner.Inventory.GetItem(eInventorySlot.LeftHandWeapon);

GameServer/ECS-Components/RangeAttackComponent.cs
@@ -130,7 +130,7 @@ namespace DOL.GS
                     playerOwner.TempProperties.setProperty(RANGED_ATTACK_START, attackStart);
                 }
 
-                if ((GameLoop.GameLoopTime - attackStart) > 15000 && playerOwner.attackComponent.AttackWeapon.Object_Type != (int)eObjectType.Crossbow)
+                if ((GameLoop.GameLoopTime - attackStart) > 15000 && playerOwner.ActiveWeapon.Object_Type != (int)eObjectType.Crossbow)
                 {
                     playerOwner.Out.SendMessage(LanguageMgr.GetTranslation(playerOwner.Client.Account.Language, "GamePlayer.Attack.TooTired"), eChatType.CT_System, eChatLoc.CL_SystemWindow);
                     return eCheckRangeAttackStateResult.Stop;
@@ -156,7 +156,7 @@ namespace DOL.GS
                         playerOwner.Out.SendMessage(LanguageMgr.GetTranslation(playerOwner.Client.Account.Language, "GamePlayer.Attack.CantSeeTarget"), eChatType.CT_System, eChatLoc.CL_SystemWindow);
                     else if (!playerOwner.IsObjectInFront(target, 90))
                         playerOwner.Out.SendMessage(LanguageMgr.GetTranslation(playerOwner.Client.Account.Language, "GamePlayer.Attack.NotInView", target.GetName(0, true)), eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                    else if (UpdateAmmo(playerOwner.attackComponent.AttackWeapon) == null)
+                    else if (UpdateAmmo(playerOwner.ActiveWeapon) == null)
                         // Another check for ammo just before firing.
                         playerOwner.Out.SendMessage(LanguageMgr.GetTranslation(playerOwner.Client.Account.Language, "GamePlayer.Attack.MustSelectQuiver"), eChatType.CT_YouHit, eChatLoc.CL_SystemWindow);
                     else if (!m_isAmmoCompatible)

GameServer/ECS-Components/StyleComponent.cs
@@ -113,7 +113,7 @@ namespace DOL.GS
             if (NextCombatStyle == null) return null;
             if (NextCombatStyle.WeaponTypeRequirement == (int)eObjectType.Shield)
                 weapon = owner.Inventory.GetItem(eInventorySlot.LeftHandWeapon);
-            else weapon = owner.attackComponent.AttackWeapon;
+            else weapon = owner.ActiveWeapon;
 
             if (StyleProcessor.CanUseStyle(owner, NextCombatStyle, weapon))
                 return NextCombatStyle;
@@ -139,12 +139,12 @@ namespace DOL.GS
             // 0.4% of the time.
             if (p.StylesChain != null && p.StylesChain.Count > 0)
                 foreach (Style s in p.StylesChain)
-                    if (StyleProcessor.CanUseStyle(p, s, p.attackComponent.AttackWeapon))
+                    if (StyleProcessor.CanUseStyle(p, s, p.ActiveWeapon))
                         return s;
 
             if (p.StylesDefensive != null && p.StylesDefensive.Count > 0)
                 foreach (Style s in p.StylesDefensive)
-                    if (StyleProcessor.CanUseStyle(p, s, p.attackComponent.AttackWeapon)
+                    if (StyleProcessor.CanUseStyle(p, s, p.ActiveWeapon)
                         && p.CheckStyleStun(s)) // Make sure we don't spam stun styles like Brutalize
                         return s;
 
@@ -161,21 +161,21 @@ namespace DOL.GS
                 if (p.StylesBack != null && p.StylesBack.Count > 0)
                 {
                     Style s = p.StylesBack[Util.Random(0, p.StylesBack.Count - 1)];
-                    if (StyleProcessor.CanUseStyle(p, s, p.attackComponent.AttackWeapon))
+                    if (StyleProcessor.CanUseStyle(p, s, p.ActiveWeapon))
                         return s;
                 }
 
                 if (p.StylesSide != null && p.StylesSide.Count > 0)
                 {
                     Style s = p.StylesSide[Util.Random(0, p.StylesSide.Count - 1)];
-                    if (StyleProcessor.CanUseStyle(p, s, p.attackComponent.AttackWeapon))
+                    if (StyleProcessor.CanUseStyle(p, s, p.ActiveWeapon))
                         return s;
                 }
 
                 if (p.StylesFront != null && p.StylesFront.Count > 0)
                 {
                     Style s = p.StylesFront[Util.Random(0, p.StylesFront.Count - 1)];
-                    if (StyleProcessor.CanUseStyle(p, s, p.attackComponent.AttackWeapon))
+                    if (StyleProcessor.CanUseStyle(p, s, p.ActiveWeapon))
                         return s;
                 }
 

commit 7843ce5519c215ff1b5e94d116a5510719cbd049

Fix NPE caused by ff5b975

GameServer/spells/SpellHandler.cs
@@ -2206,17 +2206,18 @@ namespace DOL.GS.Spells
 		public virtual void FinishSpellCast(GameLiving target)
 		{
 			GamePlayer playerCaster = Caster as GamePlayer;
-			GameInventoryItem playerWeapon = playerCaster.ActiveWeapon as GameInventoryItem;
+			InventoryItem playerWeapon = null;
 
 			if (playerCaster != null)
 			{
+				playerWeapon = playerCaster.ActiveWeapon;
+
 				if (!HasPositiveEffect)
 				{
 					if (playerCaster.IsOnHorse)
 						playerCaster.IsOnHorse = false;
 
-					if (playerWeapon != null)
-						playerWeapon.OnSpellCast(playerCaster, target, Spell);
+					(playerWeapon as GameInventoryItem)?.OnSpellCast(playerCaster, target, Spell);
 				}
 
 				if (UnstealthCasterOnFinish)