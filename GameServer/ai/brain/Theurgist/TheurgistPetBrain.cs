OpenDAoC/OpenDAoC-Core

commit 615da92bbaa9b50d3f634a534ccaff2bdf8caf8f

Fix Theurgist pets logic inconsistencies - As far as I know we intend the Theurgist pets to behave like any other mobs, except they can't change targets, die when their target dies, randomly cast their instant stun. For this purpose, 'TheurgistPetBrain' now inherits 'ControlledNpcBrain' and all copy pasted code is removed. - Their aggro will now be (again?) correctly split with their owner (which relies on the brain to be a 'ControlledNpcBrain'). - They now get interrupted like other pets and mobs, using the same probabilities and interrupt duration logic, instead of being allowed to cast immediately after getting interrupted. Ice pets will now correctly attack in melee while being interrupted, and resume spellcasting when possible. - Ice pets are now able to queue their spells, and they will no longer move forward between casts. - Air pets had a 25% chance to cast their instant spell on each tick, but that means the faster the brain thinks, the more likely it is to cast. This was replaced by disabling the skill with a random duration after each cast and when the pet is created. This may require some adjustments (they will be less likely to immediately cast their stun). - There could be other positive or negative consequences I missed.

GameServer/ai/brain/Theurgist/TheurgistPetBrain.cs
@@ -16,206 +16,48 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
-using System;
-using DOL.Events;
 using DOL.GS;
 
 namespace DOL.AI.Brain
 {
-	public class TheurgistPetBrain : StandardMobBrain, IControlledBrain
+	public class TheurgistPetBrain : ControlledNpcBrain
 	{
-		private GameLiving m_owner;
-		private GameLiving m_target;
-		private bool m_active = true;
-		public bool Melee { get; set; } = false;
-		public static readonly short MIN_ENEMY_FOLLOW_DIST = 90;
-		public static readonly short MAX_ENEMY_FOLLOW_DIST = 5000;
+		private GameObject m_target;
 
-		public TheurgistPetBrain(GameLiving owner)
+		public TheurgistPetBrain(GameLiving owner) : base(owner)
 		{
-			if (owner != null)
-				m_owner = owner;
-
-			AggroLevel = 100;
 			IsMainPet = false;
 		}
 
-		public virtual GameNPC GetNPCOwner()
-		{
-		    return null;
-		}
-
-		public virtual GameLiving GetLivingOwner()
-		{
-		    GamePlayer player = GetPlayerOwner();
-		    if (player != null)
-				return player;
-
-		    GameNPC npc = GetNPCOwner();
-		    if (npc != null)
-				return npc;
-
-		    return null;
-		}
-
-		public override int ThinkInterval => 1500;
-
 		public override void Think()
 		{
-			AttackMostWanted();
-		}
+			m_target = Body.TargetObject;
 
-		public void SetAggressionState(eAggressionState state) { }
-
-		public override void Notify(DOLEvent e, object sender, EventArgs args)
-		{
-			if (!IsActive || Melee || !m_active)
-				return;
-
-			if (args as AttackFinishedEventArgs != null)
+			if (m_target == null || m_target.HealthPercent <= 0)
 			{
-				Melee = true;
-
-				GameLiving target = m_target;
-				if (target != null)
-					Body.StartAttack(target);
-
+				Body.Die(null);
 				return;
 			}
-			if (e == GameLivingEvent.CastFailed)
-			{
-				GameLiving target = m_target;
-				if (target != null)
-					Body.StartAttack(target);
 
-				return;
+			if (Body.CurrentFollowTarget != m_target)
+			{
+				Body.StopFollowing();
+				Body.Follow(m_target, MIN_ENEMY_FOLLOW_DIST, MAX_ENEMY_FOLLOW_DIST);
 			}
+
+			if (!CheckSpells(eCheckSpellType.Offensive))
+				Body.StartAttack(m_target);
 		}
 
-		public override void AttackMostWanted()
-		{
-			if (!IsActive || !m_active)
-				return;
-
-			if (Body.attackComponent == null)
-				Body.attackComponent = new AttackComponent(Body);
-
-			EntityManager.AddComponent(typeof(AttackComponent), Body);
-
-			if (Body.castingComponent == null)
-				Body.castingComponent = new CastingComponent(Body);
-
-			EntityManager.AddComponent(typeof(CastingComponent), Body);
-
-			if (m_target == null)
-				m_target = (GameLiving)Body.TempProperties.getProperty<object>("target", null);
-			
-			if (m_target == null || !m_target.IsAlive)
-				Body.Die(Body);
-			else
-			{
-				GameLiving target = m_target;
-				Body.TargetObject = target;
-				if (Body.IsWithinRadius(target, Body.AttackRange) || Melee)
-				{
-					Body.StartAttack(target);
-					if (Body.Name.Contains("air"))
-						CheckSpells(eCheckSpellType.Offensive);
-				}
-				else if (!CheckSpells(eCheckSpellType.Offensive))
-				{
-					if (Body.IsWithinRadius(target,Body.attackComponent.AttackRange))
-						Body.StartAttack(target);
-					//Get closer to the target
-					else
-					{
-						if(Body.CurrentFollowTarget!=target)
-						{
-							Body.StopFollowing();
-							Body.Follow(target, MIN_ENEMY_FOLLOW_DIST, MAX_ENEMY_FOLLOW_DIST);
-						}
-					}
-				}
-			}
-		}
-
-		public override bool CheckSpells(eCheckSpellType type)
-		{
-			if (Body == null || Body.Spells == null || Body.Spells.Count < 1 || Melee)
-				return false;
-
-			if (Body.IsCasting)
-				return true;
-
-			bool casted = false;
-
-			if (type == eCheckSpellType.Defensive)
-			{
-				foreach (Spell spell in Body.Spells)
-				{
-					if (!Body.IsBeingInterrupted && Body.GetSkillDisabledDuration(spell) == 0 && CheckDefensiveSpells(spell))
-					{
-						casted = true;
-						break;
-					}
-				}
-			}
-			else
-			{
-				//Check Offensive Instant Casts
-				if (Body.CanCastInstantHarmfulSpells)
-				{
-					foreach (Spell spell in Body.InstantHarmfulSpells)
-					{
-						if (Body.GetSkillDisabledDuration(spell) == 0)
-						{
-							if (Body.Name.Contains("air"))
-							{
-								if (Util.Chance(25))
-								{
-									if (CheckInstantSpells(spell))
-										break;
-								}
-							}
-							else
-							{
-								if (CheckInstantSpells(spell))
-									break;
-							}
-						}
-					}
-				}
-				//Check Offensive Casts
-				if (Body.CanCastHarmfulSpells)
-				{
-					foreach (Spell spell in Body.HarmfulSpells)
-					{
-						if (!Body.IsBeingInterrupted && CheckOffensiveSpells(spell))
-						{
-							casted = true;
-							break;
-						}
-					}
-				}
-			}
-
-			return casted || Body.IsCasting;
-		}
-
-		#region IControlledBrain Members
-		public eWalkState WalkState => eWalkState.Stay;
-		public eAggressionState AggressionState { get => eAggressionState.Aggressive; set { } }
-		public GameLiving Owner => m_owner;
-		public void Attack(GameObject target) { }
-		public void Disengage() { }
-		public void Follow(GameObject target) { }
-		public void FollowOwner() { }
-		public void Stay() { }
-		public void ComeHere() { }
-		public void Goto(GameObject target) { }
-		public void UpdatePetWindow() { }
-		public GamePlayer GetPlayerOwner() { return m_owner as GamePlayer; }
-		public bool IsMainPet { get => false; set { } }
-		#endregion
+		public override eWalkState WalkState { get => eWalkState.Stay; set { } }
+		public override eAggressionState AggressionState { get => eAggressionState.Aggressive; set { } }
+		public override void Attack(GameObject target) { }
+		public override void Disengage() { }
+		public override void Follow(GameObject target) { }
+		public override void FollowOwner() { }
+		public override void Stay() { }
+		public override void ComeHere() { }
+		public override void Goto(GameObject target) { }
+		public override void UpdatePetWindow() { }
 	}
 }

GameServer/ai/brain/ControlledNPCs/ControlledNpcBrain.cs
@@ -488,41 +488,62 @@ namespace DOL.AI.Brain
 			{
 				// Check instant spells, but only cast one of each type to prevent spamming
 				if (Body.CanCastInstantHealSpells)
+				{
 					foreach (Spell spell in Body.InstantHealSpells)
+					{
 						if (CheckDefensiveSpells(spell))
 							break;
+					}
+				}
 
 				if (Body.CanCastInstantMiscSpells)
+				{
 					foreach (Spell spell in Body.InstantMiscSpells)
+					{
 						if (CheckDefensiveSpells(spell))
 							break;
+					}
+				}
 
 				// Check spell lists, prioritizing healing
 				if (Body.CanCastHealSpells)
+				{
 					foreach (Spell spell in Body.HealSpells)
+					{
 						if (CheckDefensiveSpells(spell))
 						{
 							casted = true;
 							break;
 						}
+					}
+				}
 
 				if (!casted && Body.CanCastMiscSpells)
+				{
 					foreach (Spell spell in Body.MiscSpells)
+					{
 						if (CheckDefensiveSpells(spell))
 						{
 							casted = true;
 							break;
 						}
+					}
+				}
 			}
 			else if (Body.TargetObject is GameLiving living && living.IsAlive)
 			{
 				// Check instant spells, but only cast one to prevent spamming
 				if (Body.CanCastInstantHarmfulSpells)
+				{
 					foreach (Spell spell in Body.InstantHarmfulSpells)
+					{
 						if (CheckOffensiveSpells(spell))
 							break;
+					}
+				}
 
 				if (Body.CanCastHarmfulSpells)
+				{
 					foreach (Spell spell in Body.HarmfulSpells)
 					{
 						if (CheckOffensiveSpells(spell))
@@ -531,6 +552,7 @@ namespace DOL.AI.Brain
 							break;
 						}
 					}
+				}
 			}
 
 			return casted || Body.IsCasting;

GameServer/gameobjects/Theurgist/TheurgistPet.cs
@@ -1,9 +1,5 @@
-using DOL.AI.Brain;
-using DOL.GS.ServerProperties;
-using DOL.GS.Spells;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using DOL.GS.ServerProperties;
 
 namespace DOL.GS
 {
@@ -11,48 +7,46 @@ namespace DOL.GS
 	{
 		public TheurgistPet(INpcTemplate npcTemplate) : base(npcTemplate)
 		{
-			
 			if (npcTemplate.Name.ToLower().Contains("earth"))
-			{
 				ScalingFactor = 17;
-			}
-
-			if (npcTemplate.Name.ToLower().Contains("air"))
+			else if (npcTemplate.Name.ToLower().Contains("air"))
 			{
 				ScalingFactor = 11;
-			}
-		}
 
-		public override void OnAttackedByEnemy(AttackData ad) 
-		{
-			if (ad != null && (ad.CausesCombat || ad.IsSpellResisted))
-			{
-				if (castingComponent != null && castingComponent.IsCasting && castingComponent.spellHandler.CastStartTick + (CurrentSpellHandler as SpellHandler).CalculateCastingTime() / 2 < GameLoop.GameLoopTime)
+				// Make air pet's instant stun a bit more random, see 'DisableSkill'.
+				// Should ideally be in its own class.
+				foreach (Spell spell in Spells)
 				{
-					InterruptTime = 0;
+					if (spell.IsInstantCast)
+						DisableSkill(spell, 0);
 				}
-				else
-					(Brain as TheurgistPetBrain).Melee = true;
 			}
 		}
-		
 
-		//public override int MaxHealth => Constitution * 10;
+		public override void DisableSkill(Skill skill, int duration)
+		{
+			// Make air pet's instant stun a bit more random.
+			// Should ideally be in its own class.
+			if (skill is Spell spell && spell.IsInstantCast)
+				duration += Util.Random((int)(spell.RecastDelay / 2.5));
 
-        /// <summary>
-        /// not each summoned pet 'll fire ambiant sentences
-        /// let's say 10%
-        /// </summary>
-        protected override void BuildAmbientTexts()
+			base.DisableSkill(skill, duration);
+		}
+
+		protected override void BuildAmbientTexts()
 		{
 			base.BuildAmbientTexts();
-			if (ambientTexts.Count>0)
-				foreach (var at in ambientTexts)
-					at.Chance /= 10;
+
+			// Not each summoned pet will fire ambient sentences.
+			if (ambientTexts.Count > 0)
+			{
+				foreach (MobXAmbientBehaviour ambientText in ambientTexts)
+					ambientText.Chance /= 10;
+			}
 		}
 
-        public override void AutoSetStats()
-        {
+		public override void AutoSetStats()
+		{
 			Strength = Properties.PET_AUTOSET_STR_BASE;
 			if (Strength < 1)
 				Strength = 1;
@@ -77,7 +71,6 @@ namespace DOL.GS
 			Piety = 30;
 			Charisma = 30;
 
-			
 			if (Level > 1)
 			{
 				// Now add stats for levelling
@@ -90,7 +83,6 @@ namespace DOL.GS
 				Piety += (short)(Level - 1);
 				Charisma += (short)(Level - 1);
 			}
-			//base.AutoSetStats();
-        }
-    }
+		}
+	}
 }

GameServer/spells/SpellHandler.cs
@@ -685,7 +685,7 @@ namespace DOL.GS.Spells
 		{
 			// [StephenxPimentel] Check if the necro has MoC effect before interrupting.
 			if (Caster is NecromancerPet necroPet && necroPet.Owner is GamePlayer necroOwner)
-            {
+			{
 				if (necroOwner.effectListComponent.ContainsEffectForEffectType(eEffect.MasteryOfConcentration))
 					return false;
 			}
@@ -711,7 +711,7 @@ namespace DOL.GS.Spells
 					playerCaster.LastInterruptMessage = $"{attacker.GetName(0, true)} attacks you and your spell is interrupted!";
 					MessageToLiving(playerCaster, playerCaster.LastInterruptMessage, eChatType.CT_SpellResisted);
 				}
-												
+
 				InterruptCasting(); // Always interrupt at the moment.
 				return true;
 			}
@@ -1924,7 +1924,6 @@ namespace DOL.GS.Spells
 		/// </summary>
 		public virtual void InterruptCasting()
 		{
-			//castState = eCastState.Interrupted;
 			if (m_interrupted || !IsCasting)
 				return;
 
@@ -1938,22 +1937,21 @@ namespace DOL.GS.Spells
 				});
 			}
 			
-			if(m_caster is GamePlayer p && p.castingComponent != null)
-            {
+			if (m_caster is GamePlayer p && p.castingComponent != null)
+			{
 				p.castingComponent.spellHandler = null;
 				p.castingComponent.queuedSpellHandler = null;
-            }
+			}
 
 			if (m_castTimer != null)
 			{
 				m_castTimer.Stop();
 				m_castTimer = null;
 
-				if (m_caster is GamePlayer)
-				{
-					((GamePlayer)m_caster).ClearSpellQueue();
-				}
+				if (m_caster is GamePlayer playerCaster)
+					playerCaster.ClearSpellQueue();
 			}
+
 			CastState = eCastState.Interrupted;
 			m_startReuseTimer = false;
 			OnAfterSpellCastSequence();

GameServer/spells/Theurgist/SummonTheurgistPet.cs
@@ -33,8 +33,6 @@ namespace DOL.GS.Spells
 		/// <summary>
 		/// Check whether it's possible to summon a pet.
 		/// </summary>
-		/// <param name="selectedTarget"></param>
-		/// <returns></returns>
 		public override bool CheckBeginCast(GameLiving selectedTarget)
 		{
 			if (Caster.PetCount >= ServerProperties.Properties.THEURGIST_PET_CAP)
@@ -49,24 +47,20 @@ namespace DOL.GS.Spells
 		/// <summary>
 		/// Summon the pet.
 		/// </summary>
-		/// <param name="target"></param>
-		/// <param name="effectiveness"></param>
 		public override void ApplyEffectOnTarget(GameLiving target, double effectiveness)
 		{
 			base.ApplyEffectOnTarget(target, effectiveness);
 
-            m_pet.TempProperties.setProperty("target", target);
-            (m_pet.Brain as IOldAggressiveBrain).AddToAggroList(target, 1);
+			m_pet.TargetObject = target;
+			(m_pet.Brain as IOldAggressiveBrain).AddToAggroList(target, 1);
 			m_pet.Brain.Think();
 
 			Caster.PetCount++;
 		}
 
 		/// <summary>
-		/// Despawn pet.
+		/// Despawn the pet.
 		/// </summary>
-		/// <param name="effect"></param>
-		/// <param name="noMessages"></param>
 		/// <returns>Immunity timer (in milliseconds).</returns>
 		public override int OnEffectExpires(GameSpellEffect effect, bool noMessages)
 		{

commit cbb77cce8d5eb4036e5296dc2c7a06e746e63496

Fix Theurgist pets dying when their target reaches 0% HP but is still alive Caused by 615da92

GameServer/ai/brain/Theurgist/TheurgistPetBrain.cs
@@ -33,7 +33,7 @@ namespace DOL.AI.Brain
 		{
 			m_target = Body.TargetObject;
 
-			if (m_target == null || m_target.HealthPercent <= 0)
+			if (m_target == null || m_target.Health <= 0)
 			{
 				Body.Die(null);
 				return;