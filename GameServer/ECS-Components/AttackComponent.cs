OpenDAoC/OpenDAoC-Core

commit 707d46db9e636b651081bc5ba933f42d3a5ef140

Fix spell effect animations being cancelled by subsequent spells when the user is holding a ranged weapon

GameServer/ECS-Components/AttackComponent.cs
@@ -931,7 +931,7 @@ namespace DOL.GS
             if (owner.ActiveWeaponSlot == eActiveWeaponSlot.Distance)
             {
                 // Only cancel the animation if the ranged ammo isn't released already.
-                if (weaponAction?.AttackFinished != true)
+                if (weaponAction != null && weaponAction.AttackFinished != true)
                 {
                     foreach (GamePlayer player in owner.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
                         player.Out.SendInterruptAnimation(owner);

GameServer/spells/SpellHandler.cs
@@ -672,7 +672,7 @@ namespace DOL.GS.Spells
 			}
 
 			// Stop our melee attack. NPC brains will resume it automatically.
-			if (!Spell.IsInstantCast && !m_caster.CanCastWhileAttacking())
+			if (!Spell.IsInstantCast && m_caster.attackComponent.AttackState && !m_caster.CanCastWhileAttacking())
 				m_caster.attackComponent.StopAttack();
 
 			// Check interrupt timer.

commit 350ad903e7aca9e3441591e49e5e370e31e36c9d

Fix ranged attack animations not canceling properly Caused by 707d46d. Instead, we will cancel the animation only if `AttackState` is true (and before setting it to false), and unless `AttackFinished` is true (which would mean the arrow is already released). Spell effect animations will still be canceled if after casting a spell the player readies its ranged weapon then interrupt it by moving, but I'm not sure if we can prevent that since calling `SendInterruptAnimation` always cancels spell effect animations anyway.

GameServer/ECS-Components/AttackComponent.cs
@@ -923,15 +923,10 @@ namespace DOL.GS
 
         public void StopAttack()
         {
-            AttackState = false;
-            owner.CancelEngageEffect();
-            owner.styleComponent.NextCombatStyle = null;
-            owner.styleComponent.NextCombatBackupStyle = null;
-
             if (owner.ActiveWeaponSlot == eActiveWeaponSlot.Distance)
             {
                 // Only cancel the animation if the ranged ammo isn't released already.
-                if (weaponAction != null && weaponAction.AttackFinished != true)
+                if (AttackState && weaponAction?.AttackFinished != true)
                 {
                     foreach (GamePlayer player in owner.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
                         player.Out.SendInterruptAnimation(owner);
@@ -940,6 +935,11 @@ namespace DOL.GS
                 owner.rangeAttackComponent.RangedAttackState = eRangedAttackState.None;
                 owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
             }
+
+            AttackState = false;
+            owner.CancelEngageEffect();
+            owner.styleComponent.NextCombatStyle = null;
+            owner.styleComponent.NextCombatBackupStyle = null;
 
             if (owner is GamePlayer playerOwner && playerOwner.IsAlive)
                 playerOwner.Out.SendAttackMode(AttackState);