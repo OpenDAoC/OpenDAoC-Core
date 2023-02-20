ba705ed825c50e76c57cce94bbd115b7990a2482
Baptiste Marie <bm01@users.noreply.github.com>
2023-02-20 22:09:07 +0100

    Move 'GamePlayer' instantiation from 'CharacterSelectRequestHandler' to 'WorldInitRequestHandler' Tested with client versions 1124, 1125d, 1127. 0x10 packets are received every time the realm selection screen is opened, or the character selection screen is reached, or the player clicks on Play. The value of the packet is always 0 unless the player clicks on "Play", in which case it returns the slot of the selected character. But 0 is a valid value and represent the first Albion slot. And while the client state check would prevent the first packet from instantiating 'GamePlayer', this isn't enough if the first screen is the realm selection screen, or if the player changes realm, or returns from the character creation screen. For this reason, the code was moved to 'WorldInitRequestHandler'.

--- GameServer/packets/Client/168/CharacterSelectRequestHandler.cs
+++ GameServer/packets/Client/168/CharacterSelectRequestHandler.cs
@@ -20,21 +20,17 @@ using DOL.Events;
 
 namespace DOL.GS.PacketHandler.Client.v168
 {
-    [PacketHandler(PacketHandlerType.TCP, eClientPackets.CharacterSelectRequest, "Handles setting SessionID and the active character", eClientStatus.LoggedIn)]
+    [PacketHandler(PacketHandlerType.TCP, eClientPackets.CharacterSelectRequest, "Handles setting SessionID", eClientStatus.LoggedIn)]
     public class CharacterSelectRequestHandler : IPacketHandler
     {
         public void HandlePacket(GameClient client, GSPacketIn packet)
         {
-            if (client.Version >= GameClient.eClientVersion.Version1125 && client.MinorRev == "d") // 1125d support TODO this needs to be changed when a version greater than 1125d comes out
+            // 'GamePlayer' instantiation was moved to 'WorldInitRequestHandler' for game versions superior to 1124, because it didn't prevent multiple instantation correctly.
+            // Prior versions are still handled here because they're untested.
+            if (client.Version >= GameClient.eClientVersion.Version1124)
             {
-                var charSelectRequest = new CharacterSelectRequestHandler1125d();
-                charSelectRequest.HandlePacket(client, packet);
-                return;
-            }
-            else if (client.Version >= GameClient.eClientVersion.Version1126)
-            {
-                var charSelectRequest = new CharacterSelectRequestHandler1126();
-                charSelectRequest.HandlePacket(client, packet);
+                client.Out.SendLoginGranted();
+                client.Out.SendSessionID();
                 return;
             }
 
@@ -46,6 +42,7 @@ namespace DOL.GS.PacketHandler.Client.v168
             // TODO Character handling
             if (charName.Equals("noname"))
             {
+                client.Out.SendLoginGranted();
                 client.Out.SendSessionID();
             }
             else
@@ -82,109 +79,9 @@ namespace DOL.GS.PacketHandler.Client.v168
                     }
                 }
 
-                client.Out.SendSessionID();
-            }
-        }
-    }
-
-    /// <summary>
-    /// 1125d + support
-    /// </summary>
-    public class CharacterSelectRequestHandler1125d : IPacketHandler // 1125d support TODO duplicate code from above, can be reused
-    {        
-        public void HandlePacket(GameClient client, GSPacketIn packet)
-        {
-            
-            byte type = (byte)packet.ReadByte(); // changed from ushort low end
-                        
-            packet.Skip(1); // unknown
-
-            string charName = packet.ReadString(24); // down from 28, both need checking
-                        
-            //TODO Character handling 
-            if (charName.Equals("noname"))
-            {                
                 client.Out.SendLoginGranted();
                 client.Out.SendSessionID();
             }
-            else
-            {
-                // SH: Also load the player if client player is NOT null but their charnames differ!!!
-                // only load player when on charscreen and player is not loaded yet
-                // packet is sent on every region change (and twice after "play" was pressed)
-                if (((client.Player == null && client.Account.Characters != null) || (client.Player != null && client.Player.Name.ToLower() != charName.ToLower())) 
-                    && client.ClientState == GameClient.eClientState.CharScreen)
-                {
-                    bool charFound = false;
-                    for (int i = 0; i < client.Account.Characters.Length; i++)
-                    {
-                        if (client.Account.Characters[i] != null && client.Account.Characters[i].Name == charName)
-                        {
-                            charFound = true;
-                            client.LoadPlayer(i);
-                            break;
-                        }
-                    }
-                    if (!charFound)
-                    {
-                        client.Player = null;
-                        client.ActiveCharIndex = -1;
-                    }
-                    else
-                    {
-                        // Log character play
-                        AuditMgr.AddAuditEntry(client, AuditType.Character, AuditSubtype.CharacterLogin, "", charName);
-                    }
-                }
-                
-                // live actually sends the login granted packet, which sets the button activity states
-                client.Out.SendLoginGranted();
-                client.Out.SendSessionID();
-            }
-        }
-    }
-
-    /// <summary>
-    /// 1126 support. Packet changed again from version 1125d
-    /// Alot of test code i slapped together to get it to work, likely can be changed
-    /// </summary>
-    public class CharacterSelectRequestHandler1126 : IPacketHandler
-    {        
-        public void HandlePacket(GameClient client, GSPacketIn packet)
-        {
-            byte charIndex = (byte)packet.ReadByte(); // character account location
-
-            // some funkyness going on below here. Could use some safeguards to ensure a character is loaded correctly
-            if (client.Player == null && client.Account.Characters != null && client.ClientState == GameClient.eClientState.CharScreen)
-            {                
-                bool charFound = false;
-                string selectedChar = "";
-                int realmOffset = charIndex - (client.Account.Realm * 10 - 10);
-                int charSlot = (client.Account.Realm * 100) + realmOffset;
-                for (int i = 0; i < client.Account.Characters.Length; i++)
-                {
-                    if (client.Account.Characters[i] != null && client.Account.Characters[i].AccountSlot == charSlot)
-                    {
-                        charFound = true;
-                        selectedChar = client.Account.Characters[i].Name;
-                        client.LoadPlayer(i);
-                        break;
-                    }
-                }
-                
-                if (!charFound)
-                {
-                    client.Player = null;
-                    client.ActiveCharIndex = -1;
-                }
-                else
-                {
-                    // Log character play
-                    AuditMgr.AddAuditEntry(client, AuditType.Character, AuditSubtype.CharacterLogin, "", selectedChar);
-                }
-            }
-
-            client.Out.SendSessionID();
         }
     }
 }
--- GameServer/packets/Client/168/WorldInitRequestHandler.cs
+++ GameServer/packets/Client/168/WorldInitRequestHandler.cs
@@ -16,183 +16,216 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Reflection;
 using DOL.Database;
 using log4net;
 
 namespace DOL.GS.PacketHandler.Client.v168
 {
-	[PacketHandlerAttribute(PacketHandlerType.TCP, eClientPackets.WorldInitRequest, "Handles world init replies", eClientStatus.PlayerInGame)]
-	public class WorldInitRequestHandler : IPacketHandler
-	{
-		/// <summary>
-		/// Defines a logger for this class.
-		/// </summary>
-		private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+    [PacketHandlerAttribute(PacketHandlerType.TCP, eClientPackets.WorldInitRequest, "Handles world init replies", eClientStatus.LoggedIn)]
+    public class WorldInitRequestHandler : IPacketHandler
+    {
+        /// <summary>
+        /// Defines a logger for this class.
+        /// </summary>
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
-		public void HandlePacket(GameClient client, GSPacketIn packet)
-		{
-			if (client?.Player == null)
-				return;
+        public void HandlePacket(GameClient client, GSPacketIn packet)
+        {
+            // Instantiate 'GamePlayer'. Previous versions are handled in 'CharacterSelectRequestHandler'.
+            if (client.Version >= GameClient.eClientVersion.Version1124)
+                HandlePacket1124(client, packet);
 
-			client.UdpConfirm = false;
+            if (client.Player == null)
+                return;
 
-			new WorldInitAction(client.Player).Start(1);
-		}
+            client.UdpConfirm = false;
+            new WorldInitAction(client.Player).Start(1);
+        }
 
-		/// <summary>
-		/// Handles player world init requests
-		/// </summary>
-		protected class WorldInitAction : AuxRegionECSAction
-		{
-			/// <summary>
-			/// Constructs a new WorldInitAction
-			/// </summary>
-			/// <param name="actionSource">The action source</param>
-			public WorldInitAction(GamePlayer actionSource)
-				: base(actionSource)
-			{
-			}
+        private static void HandlePacket1124(GameClient client, GSPacketIn packet)
+        {
+            byte charIndex = (byte)packet.ReadByte(); // character account location
 
-			/// <summary>
-			/// Called on every timer tick
-			/// </summary>
-			protected override int OnTick(AuxECSGameTimer timer)
-			{
-				GamePlayer player = (GamePlayer)m_actionSource;
-				if (player == null) return 0;
-				//check emblems at world load before any updates
-				if (player.Inventory != null) 
-				{
-					lock (player.Inventory)
-					{
-						Guild playerGuild = player.Guild;
-						foreach (InventoryItem myitem in player.Inventory.AllItems)
-						{
-							if (myitem != null && myitem.Emblem != 0)
-							{
-								if (playerGuild == null || myitem.Emblem != playerGuild.Emblem)
-								{
-									myitem.Emblem = 0;
-								}
-								if (player.Level < 20)
-								{
-									if (player.CraftingPrimarySkill == eCraftingSkill.NoCrafting)
-									{
-										myitem.Emblem = 0;
-									}
-									else
-									{
-										if (player.GetCraftingSkillValue(player.CraftingPrimarySkill) < 400)
-										{
-											myitem.Emblem = 0;
-										}
-									}
-								}
-							}
-						}
-					}
-				}
+            // some funkyness going on below here. Could use some safeguards to ensure a character is loaded correctly
+            if (client.Player == null && client.Account.Characters != null && client.ClientState == GameClient.eClientState.CharScreen)
+            {
+                bool charFound = false;
+                string selectedChar = "";
+                int realmOffset = charIndex - (client.Account.Realm * 10 - 10);
+                int charSlot = client.Account.Realm * 100 + realmOffset;
 
-				player.Client.ClientState = GameClient.eClientState.WorldEnter;
-				// 0x88 - Position
-				// 0x6D - FriendList
-				// 0x15 - Encumberance update
-				// 0x1E - Speed update
-				// 0xDD - Shared Buffs update
-				// 0x1E - Speed update
-				// 0x05 - Health, Sit update
-				// 0xAA - Inventory Update
-				// 0xAA - Inventory Update /Vault ?
-				// 0xBE - 01 various tabs update (skills/spells...)
-				// 0xBE - 08 various tabs update (skills/spells...)
-				// 0xBE - 02 spells
-				// 0xBE - 03 various tabs update (skills/spells...)
-				// 0x52 - Money update
-				// 0x53 - stats update
-				// 0xD7 - Self Buffs update
-				// 0xBE - 05, various tabs ...
-				// 0x2B - Quest list
-				// 0x05 - health again...?
-				// 0x39 - XP update?
-				// 0x15 - Encumberance update
-				// 0xBE - 06, group
-				// 0xE4 - ??  (0 0 5 0 0 0 0 0)
-				// 0xBE - 0 1 0 0
-				// 0x89 - Debug mode
-				// 0x1E - Speed again!?
-				// 0x25 - model change
+                for (int i = 0; i < client.Account.Characters.Length; i++)
+                {
+                    if (client.Account.Characters[i] != null && client.Account.Characters[i].AccountSlot == charSlot)
+                    {
+                        charFound = true;
+                        selectedChar = client.Account.Characters[i].Name;
+                        client.LoadPlayer(i);
+                        break;
+                    }
+                }
 
-				//Get the objectID for this player
-				//IMPORTANT ... this is needed BEFORE
-				//sending Packet 0x88!!!
+                if (!charFound)
+                {
+                    client.Player = null;
+                    client.ActiveCharIndex = -1;
+                }
+                else
+                    AuditMgr.AddAuditEntry(client, AuditType.Character, AuditSubtype.CharacterLogin, "", selectedChar);
+            }
+        }
 
-				if (!player.AddToWorld())
-				{
-					log.ErrorFormat("Failed to add player to the region! {0}", player.ToString());
-					player.Client?.Out.SendPlayerQuit(true);
-					player.Client?.Player.SaveIntoDatabase();
-					player.Client?.Player.Quit(true);
-					player.Client?.Disconnect();
+        /// <summary>
+        /// Handles player world init requests
+        /// </summary>
+        protected class WorldInitAction : AuxRegionECSAction
+        {
+            /// <summary>
+            /// Constructs a new WorldInitAction
+            /// </summary>
+            /// <param name="actionSource">The action source</param>
+            public WorldInitAction(GamePlayer actionSource)
+                : base(actionSource)
+            {
+            }
 
-					return 0;
-				}
+            /// <summary>
+            /// Called on every timer tick
+            /// </summary>
+            protected override int OnTick(AuxECSGameTimer timer)
+            {
+                GamePlayer player = (GamePlayer)m_actionSource;
+                if (player == null) return 0;
+                //check emblems at world load before any updates
+                if (player.Inventory != null) 
+                {
+                    lock (player.Inventory)
+                    {
+                        Guild playerGuild = player.Guild;
+                        foreach (InventoryItem myitem in player.Inventory.AllItems)
+                        {
+                            if (myitem != null && myitem.Emblem != 0)
+                            {
+                                if (playerGuild == null || myitem.Emblem != playerGuild.Emblem)
+                                {
+                                    myitem.Emblem = 0;
+                                }
+                                if (player.Level < 20)
+                                {
+                                    if (player.CraftingPrimarySkill == eCraftingSkill.NoCrafting)
+                                    {
+                                        myitem.Emblem = 0;
+                                    }
+                                    else
+                                    {
+                                        if (player.GetCraftingSkillValue(player.CraftingPrimarySkill) < 400)
+                                        {
+                                            myitem.Emblem = 0;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
 
-				// this is bind stuff
-				// make sure that players doesnt start dead when coming in
-				// thats important since if client moves the player it requests player creation
-				if (player.Health <= 0)
-				{
-					player.Health = player.MaxHealth;
-				}
+                player.Client.ClientState = GameClient.eClientState.WorldEnter;
+                // 0x88 - Position
+                // 0x6D - FriendList
+                // 0x15 - Encumberance update
+                // 0x1E - Speed update
+                // 0xDD - Shared Buffs update
+                // 0x1E - Speed update
+                // 0x05 - Health, Sit update
+                // 0xAA - Inventory Update
+                // 0xAA - Inventory Update /Vault ?
+                // 0xBE - 01 various tabs update (skills/spells...)
+                // 0xBE - 08 various tabs update (skills/spells...)
+                // 0xBE - 02 spells
+                // 0xBE - 03 various tabs update (skills/spells...)
+                // 0x52 - Money update
+                // 0x53 - stats update
+                // 0xD7 - Self Buffs update
+                // 0xBE - 05, various tabs ...
+                // 0x2B - Quest list
+                // 0x05 - health again...?
+                // 0x39 - XP update?
+                // 0x15 - Encumberance update
+                // 0xBE - 06, group
+                // 0xE4 - ??  (0 0 5 0 0 0 0 0)
+                // 0xBE - 0 1 0 0
+                // 0x89 - Debug mode
+                // 0x1E - Speed again!?
+                // 0x25 - model change
 
-				player.Out.SendPlayerPositionAndObjectID();
-				player.Out.SendUpdateMaxSpeed();
-				//TODO 0xDD - Conc Buffs // 0 0 0 0
-				//Now find the friends that are online
-				player.Out.SendUpdateMaxSpeed(); // Speed after conc buffs
-				player.Out.SendStatusUpdate();
-				player.Out.SendInventoryItemsUpdate(eInventoryWindowType.Equipment, player.Inventory.EquippedItems);
+                //Get the objectID for this player
+                //IMPORTANT ... this is needed BEFORE
+                //sending Packet 0x88!!!
+
+                if (!player.AddToWorld())
+                {
+                    log.ErrorFormat("Failed to add player to the region! {0}", player.ToString());
+                    player.Client?.Out.SendPlayerQuit(true);
+                    player.Client?.Player.SaveIntoDatabase();
+                    player.Client?.Player.Quit(true);
+                    player.Client?.Disconnect();
+
+                    return 0;
+                }
+
+                // this is bind stuff
+                // make sure that players doesnt start dead when coming in
+                // thats important since if client moves the player it requests player creation
+                if (player.Health <= 0)
+                {
+                    player.Health = player.MaxHealth;
+                }
+
+                player.Out.SendPlayerPositionAndObjectID();
+                player.Out.SendUpdateMaxSpeed();
+                //TODO 0xDD - Conc Buffs // 0 0 0 0
+                //Now find the friends that are online
+                player.Out.SendUpdateMaxSpeed(); // Speed after conc buffs
+                player.Out.SendStatusUpdate();
+                player.Out.SendInventoryItemsUpdate(eInventoryWindowType.Equipment, player.Inventory.EquippedItems);
                 player.Out.SendInventoryItemsUpdate(eInventoryWindowType.Inventory, player.Inventory.GetItemRange(eInventorySlot.FirstBackpack, eInventorySlot.LastBagHorse));
-				player.Out.SendUpdatePlayerSkills();   //TODO Insert 0xBE - 08 Various in SendUpdatePlayerSkills() before send spells
-				player.Out.SendUpdateCraftingSkills(); // ^
-				player.Out.SendUpdatePlayer();
-				player.Out.SendUpdateMoney();
-				player.Out.SendCharStatsUpdate();
-				player.Out.SendCharResistsUpdate();
-				int effectsCount = 0;
-				player.Out.SendUpdateIcons(null, ref effectsCount);
-				player.Out.SendUpdateWeaponAndArmorStats();
-				player.Out.SendQuestListUpdate();
-				player.Out.SendStatusUpdate();
-				player.Out.SendUpdatePoints();
-				player.Out.SendConcentrationList();
-				// Visual 0x4C - Color Name style (0 0 5 0 0 0 0 0) for RvR or (0 0 5 1 0 0 0 0) for PvP
-				// 0xBE - 0 1 0 0
-				//used only on PvP, sets THIS players ID for nearest friend/enemy buttons and "friendly" name colors
-				//if (GameServer.ServerRules.GetColorHandling(player.Client) == 1) // PvP
-				player.Out.SendObjectGuildID(player, player.Guild);
-				player.Out.SendDebugMode(player.TempProperties.getProperty<object>(GamePlayer.DEBUG_MODE_PROPERTY, null) != null);
-				player.Out.SendUpdateMaxSpeed(); // Speed in debug mode ?
-												 //WARNING: This would change problems if a scripter changed the values for plvl
-												 //GSMessages.SendDebugMode(client,client.Account.PrivLevel>1);
-				player.UpdateEncumberance(); // Update encumberance on init.
+                player.Out.SendUpdatePlayerSkills();   //TODO Insert 0xBE - 08 Various in SendUpdatePlayerSkills() before send spells
+                player.Out.SendUpdateCraftingSkills(); // ^
+                player.Out.SendUpdatePlayer();
+                player.Out.SendUpdateMoney();
+                player.Out.SendCharStatsUpdate();
+                player.Out.SendCharResistsUpdate();
+                int effectsCount = 0;
+                player.Out.SendUpdateIcons(null, ref effectsCount);
+                player.Out.SendUpdateWeaponAndArmorStats();
+                player.Out.SendQuestListUpdate();
+                player.Out.SendStatusUpdate();
+                player.Out.SendUpdatePoints();
+                player.Out.SendConcentrationList();
+                // Visual 0x4C - Color Name style (0 0 5 0 0 0 0 0) for RvR or (0 0 5 1 0 0 0 0) for PvP
+                // 0xBE - 0 1 0 0
+                //used only on PvP, sets THIS players ID for nearest friend/enemy buttons and "friendly" name colors
+                //if (GameServer.ServerRules.GetColorHandling(player.Client) == 1) // PvP
+                player.Out.SendObjectGuildID(player, player.Guild);
+                player.Out.SendDebugMode(player.TempProperties.getProperty<object>(GamePlayer.DEBUG_MODE_PROPERTY, null) != null);
+                player.Out.SendUpdateMaxSpeed(); // Speed in debug mode ?
+                                                 //WARNING: This would change problems if a scripter changed the values for plvl
+                                                 //GSMessages.SendDebugMode(client,client.Account.PrivLevel>1);
+                player.UpdateEncumberance(); // Update encumberance on init.
 
-				// Don't unstealth GMs.
-				if (player.Client.Account.PrivLevel > 1)
-					player.GMStealthed = player.IsStealthed;
-				else
-					player.Stealth(false);
+                // Don't unstealth GMs.
+                if (player.Client.Account.PrivLevel > 1)
+                    player.GMStealthed = player.IsStealthed;
+                else
+                    player.Stealth(false);
 
-				player.Out.SendSetControlledHorse(player);
-				//check item at world load
-				if (log.IsDebugEnabled)
-					log.DebugFormat("Client {0}({1} PID:{2} OID:{3}) entering Region {4}(ID:{5})", player.Client.Account.Name, player.Name, player.Client.SessionID, player.ObjectID, player.CurrentRegion.Description, player.CurrentRegionID);
-				return 0;
-			}
-		}
-	}
+                player.Out.SendSetControlledHorse(player);
+                //check item at world load
+                if (log.IsDebugEnabled)
+                    log.DebugFormat("Client {0}({1} PID:{2} OID:{3}) entering Region {4}(ID:{5})", player.Client.Account.Name, player.Name, player.Client.SessionID, player.ObjectID, player.CurrentRegion.Description, player.CurrentRegionID);
+                return 0;
+            }
+        }
+    }
 }
