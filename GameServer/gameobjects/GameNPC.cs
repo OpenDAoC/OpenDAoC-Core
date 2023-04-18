OpenDAoC/OpenDAoC-Core

commit ceaa90e7cb5f48166423c11c846ccf4b59eaff9b

Make inactive NPCs be removed from the 'EntityManager' 'EntityManager' now holds brains instead of NPCs. Brains are added when 'Start' is called, either when added to the world or when 'GameNPC.NPCUpdatedCallback' is called. They're then removed when 'GaneNPC.IsVisibleToPlayers' returns false by 'NPCThinkService'. This reduces the amount of iterations in 'NPCThinkService' and makes the tick rate go from ~7ms to ~0.7 ms with one player online.

GameServer/gameobjects/GameNPC.cs
@@ -56,6 +56,7 @@ namespace DOL.GS
 		/// Tested - min distance for mob sticking within combat range to player is 25 (Edit Navelator, 25 stops them too early, 20 keeps them in range)
 		/// </remarks>
 		public const int CONST_WALKTOTOLERANCE = 20;
+		private const int VISIBLE_TO_PLAYER_SPAN = 60000;
 
 		private int m_databaseLevel;
 
@@ -717,13 +718,9 @@ namespace DOL.GS
 		}
 
 		/// <summary>
-		/// The last time this NPC sent the 0x09 update packet
-		/// </summary>
-		protected volatile uint m_lastUpdateTickCount = uint.MinValue;
-		/// <summary>
 		/// The last time this NPC was actually updated to at least one player
-		/// </summary>
-		protected volatile uint m_lastVisibleToPlayerTick = uint.MinValue;
+		/// </summary> 
+		protected long m_lastVisibleToPlayerTick = -VISIBLE_TO_PLAYER_SPAN; // Prevents 'IsVisibleToPlayers' from returning true during the first server tick.
 
 		/// <summary>
 		/// Gets or Sets the flags of this npc
@@ -764,7 +761,7 @@ namespace DOL.GS
 		/// </summary>
 		public virtual bool IsVisibleToPlayers
 		{
-			get { return (uint)GameLoop.GameLoopTime - m_lastVisibleToPlayerTick < 60000; }
+			get { return GameLoop.GameLoopTime - m_lastVisibleToPlayerTick < VISIBLE_TO_PLAYER_SPAN; }
 		}
 
 		/// <summary>
@@ -1303,14 +1300,6 @@ namespace DOL.GS
 
 				return 0;
 			}
-		}
-
-		/// <summary>
-		/// Gets the last time this mob was updated
-		/// </summary>
-		public uint LastUpdateTickCount
-		{
-			get { return m_lastUpdateTickCount; }
 		}
 
 		/// <summary>
@@ -3038,64 +3027,58 @@ namespace DOL.GS
 		#region Add/Remove/Create/Remove/Update
 
 		/// <summary>
-		/// Broadcasts the NPC Update to all players around
-		/// </summary>
-		public override void BroadcastUpdate()
-		{
-			base.BroadcastUpdate();
-
-			m_lastUpdateTickCount = (uint)GameLoop.GameLoopTime;
-		}
-
-		/// <summary>
 		/// callback that npc was updated to the world
 		/// so it must be visible to at least one player
 		/// </summary>
 		public void NPCUpdatedCallback()
 		{
-			m_lastVisibleToPlayerTick = (uint)GameLoop.GameLoopTime;
+			m_lastVisibleToPlayerTick = GameLoop.GameLoopTime;
+
 			lock (BrainSync)
 			{
-				ABrain brain = Brain;
-				if (brain != null)
-					brain.Start();
+				Brain?.Start();
 			}
 		}
+
 		/// <summary>
 		/// Adds the npc to the world
 		/// </summary>
 		/// <returns>true if the npc has been successfully added</returns>
 		public override bool AddToWorld()
 		{
-			if (!base.AddToWorld()) return false;
+			if (!base.AddToWorld())
+				return false;
 
 			if (MAX_PASSENGERS > 0)
 				Riders = new GamePlayer[MAX_PASSENGERS];
 
 			bool anyPlayer = false;
+
 			foreach (GamePlayer player in GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
 			{
-				if (player == null) continue;
+				if (player == null)
+					continue;
+
 				player.Out.SendNPCCreate(this);
+
 				if (m_inventory != null)
 					player.Out.SendLivingEquipmentUpdate(this);
 
-				// If any player was initialized, update last visible tick to enable brain
+				// If any player was initialized, update last visible tick to enable brain.
 				anyPlayer = true;
 			}
 
 			if (anyPlayer)
-				m_lastVisibleToPlayerTick = (uint)GameLoop.GameLoopTime;
+				m_lastVisibleToPlayerTick = GameLoop.GameLoopTime;
 
 			m_spawnPoint.X = X;
 			m_spawnPoint.Y = Y;
 			m_spawnPoint.Z = Z;
 			m_spawnHeading = Heading;
+
 			lock (BrainSync)
 			{
-				ABrain brain = Brain;
-				if (brain != null)
-					brain.Start();
+				Brain?.Start();
 			}
 
 			if (Mana <= 0 && MaxMana > 0)
@@ -3161,6 +3144,7 @@ namespace DOL.GS
 
 			if (Flags.HasFlag(eFlags.STEALTH))
 				m_wasStealthed = true;
+
 			return true;
 		}
 
@@ -3182,12 +3166,11 @@ namespace DOL.GS
 		{
 			if (IsMovingOnPath)
 				StopMovingOnPath();
+
 			if (MAX_PASSENGERS > 0)
 			{
 				foreach (GamePlayer player in CurrentRiders)
-				{
 					player.DismountSteed(true);
-				}
 			}
 
 			if (ObjectState == eObjectState.Active)
@@ -3195,13 +3178,15 @@ namespace DOL.GS
 				foreach (GamePlayer player in GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
 					player.Out.SendObjectRemove(this);
 			}
-			if (!base.RemoveFromWorld()) return false;
+
+			if (!base.RemoveFromWorld())
+				return false;
 
 			lock (BrainSync)
 			{
-				ABrain brain = Brain;
-				brain.Stop();
+				Brain.Stop();
 			}
+
 			EffectList.CancelAll();
 
 			if (ShowTeleporterIndicator && m_teleporterIndicator != null)
@@ -3209,9 +3194,6 @@ namespace DOL.GS
 				m_teleporterIndicator.RemoveFromWorld();
 				m_teleporterIndicator = null;
 			}
-
-			if (m_respawnInterval < 0)
-				EntityManagerId = EntityManager.Remove(EntityManager.EntityType.Npc, EntityManagerId);
 
 			return true;
 		}
@@ -3318,11 +3300,12 @@ namespace DOL.GS
 					m_respawnTimer = null;
 				}
 			}
+
 			lock (BrainSync)
 			{
-				ABrain brain = Brain;
-				brain.Stop();
+				Brain.Stop();
 			}
+
 			StopFollowing();
 			TempProperties.removeProperty(CHARMED_TICK_PROP);
 			base.Delete();
@@ -5775,8 +5758,6 @@ namespace DOL.GS
 				m_ownBrain = defaultBrain;
 				m_ownBrain.Body = this;
 			}
-
-			EntityManagerId = EntityManager.Add(EntityManager.EntityType.Npc, this);
 		}
 
 		/// <summary>

GameServer/ECS-Components/AttackComponent.cs
@@ -579,7 +579,7 @@ namespace DOL.GS
                 m_startAttackTarget = attackTarget;
                 StartAttackRequested = true;
 
-                if (EntityManagerId == -1)
+                if (EntityManagerId == EntityManager.UNSET_ID)
                     EntityManagerId = EntityManager.Add(EntityManager.EntityType.AttackComponent, this);
             }
         }

GameServer/ECS-Components/CastingComponent.cs
@@ -80,7 +80,7 @@ namespace DOL.GS
 
             _startCastSpellRequests.Enqueue(new StartCastSpellRequest(spell, spellLine, spellCastingAbilityHandler, target));
 
-            if (EntityManagerId == -1)
+            if (EntityManagerId == EntityManager.UNSET_ID)
                 EntityManagerId = EntityManager.Add(EntityManager.EntityType.CastingComponent, this);
 
             return true;

GameServer/ECS-Components/CraftComponent.cs
@@ -53,7 +53,7 @@ namespace DOL.GS
             {
                 CraftAction = new CraftAction(_owner, craftingTime, recipe, skill);
 
-                if (EntityManagerId == -1)
+                if (EntityManagerId == EntityManager.UNSET_ID)
                     EntityManagerId = EntityManager.Add(EntityManager.EntityType.CraftComponent, this);
             }
         }

GameServer/ECS-Components/EffectList/EffectListComponent.cs
@@ -34,7 +34,7 @@ namespace DOL.GS
                     if (!Owner.IsAlive || Owner.ObjectState != GameObject.eObjectState.Active)
                         return false;
 
-                    if (EntityManagerId == -1)
+                    if (EntityManagerId == EntityManager.UNSET_ID)
                         EntityManagerId = EntityManager.Add(EntityManager.EntityType.EffectListComponent, this);
 
                     // Check to prevent crash from holding sprint button down.

GameServer/ECS-Services/AuxTimerService.cs
@@ -129,10 +129,7 @@ namespace DOL.GS
 
                 log.Debug("---------------------------------------------------------------------------");
 
-                if (DebugTickCount > 1)
-                    DebugTickCount--;
-                else
-                    DebugTickCount = 0;
+                DebugTickCount--;
             }
 
             // Diagnostics.StopPerfCounter(SERVICE_NAME);

GameServer/ECS-Services/EffectService.cs
@@ -280,7 +280,7 @@ namespace DOL.GS
             effect.CancelEffect = true;
             effect.ExpireTick = GameLoop.GameLoopTime - 1;
 
-            if (effect.EntityManagerId == -1)
+            if (effect.EntityManagerId == EntityManager.UNSET_ID)
                 effect.EntityManagerId = EntityManager.Add(EntityManager.EntityType.Effect, effect);
         }
 

GameServer/ECS-Services/NPCThinkService.cs
@@ -17,7 +17,6 @@ namespace DOL.GS
 
         // Will print active brain count/array size info for debug purposes if superior to 0.
         public static int DebugTickCount;
-        private static int _debugRemainingTicks;
         private static int _npcCount;
         private static int _nullNpcCount;
 
@@ -30,23 +29,22 @@ namespace DOL.GS
 
             if (Debug)
             {
-                _debugRemainingTicks = 0;
                 _npcCount = 0;
                 _nullNpcCount = 0;
             }
 
-            List<GameNPC> list = EntityManager.GetAll<GameNPC>(EntityManager.EntityType.Npc);
+            List<ABrain> list = EntityManager.GetAll<ABrain>(EntityManager.EntityType.Brain);
 
-            Parallel.For(0, EntityManager.GetLastNonNullIndex(EntityManager.EntityType.Npc) + 1, i =>
+            Parallel.For(0, EntityManager.GetLastNonNullIndex(EntityManager.EntityType.Brain) + 1, i =>
             {
-                GameNPC npc = list[i];
+                ABrain brain = list[i];
 
                 try
                 {
-                    if (npc == null)
+                    if (brain == null)
                     {
                         if (Debug)
-                            Interlocked.Increment(ref _nullNpcCount);;
+                            Interlocked.Increment(ref _nullNpcCount);
 
                         return;
                     }
@@ -54,33 +52,29 @@ namespace DOL.GS
                     if (Debug)
                         Interlocked.Increment(ref _npcCount);
 
-                    ABrain brain = npc.Brain;
-
-                    if (brain != null)
+                    if (!brain.IsActive)
+                        brain.Stop();
+                    else if (brain.LastThinkTick + brain.ThinkInterval < tick)
                     {
-                        if (brain.IsActive && brain.LastThinkTick + brain.ThinkInterval < tick)
-                        {
-                            if (Debug)
-                                Interlocked.Increment(ref _debugRemainingTicks);
+                        long startTick = GameTimer.GetTickCount();
+                        brain.Think();
+                        long stopTick = GameTimer.GetTickCount();
 
-                            long startTick = GameTimer.GetTickCount();
-                            brain.Think();
-                            long stopTick = GameTimer.GetTickCount();
-                            if((stopTick - startTick)  > 25 && brain != null)
-                                log.Warn($"Long NPCThink for {brain.Body?.Name}({brain.Body?.ObjectID}) Interval: {brain.ThinkInterval} BrainType: {brain.GetType()} Time: {stopTick - startTick}ms");
+                        if ((stopTick - startTick) > 25 && brain != null)
+                            log.Warn($"Long NPCThink for {brain.Body?.Name}({brain.Body?.ObjectID}) Interval: {brain.ThinkInterval} BrainType: {brain.GetType()} Time: {stopTick - startTick}ms");
 
-                            // Set the LastThinkTick. Offset the LastThinkTick interval for non-controlled mobs so NPC Think ticks are not all "grouped" in one tick.
-                            if(brain is ControlledNpcBrain)
-                                brain.LastThinkTick = tick; // We wamt controlled pets to keep their normal interval.
-                            else
-                                brain.LastThinkTick = tick + Util.Random(10) * 50; // Offsets the LastThinkTick by 0-500ms.
-
-                        }
-
-                        if (brain.Body is not {NeedsBroadcastUpdate: true}) return;
-                        brain.Body.BroadcastUpdate();
-                        brain.Body.NeedsBroadcastUpdate = false;
+                        // Set the LastThinkTick. Offset the LastThinkTick interval for non-controlled mobs so NPC Think ticks are not all "grouped" in one tick.
+                        if(brain is ControlledNpcBrain)
+                            brain.LastThinkTick = tick; // We wamt controlled pets to keep their normal interval.
+                        else
+                            brain.LastThinkTick = tick + Util.Random(10) * 50; // Offsets the LastThinkTick by 0-500ms.
                     }
+
+                    if (brain.Body is not {NeedsBroadcastUpdate: true})
+                        return;
+
+                    brain.Body.BroadcastUpdate();
+                    brain.Body.NeedsBroadcastUpdate = false;
                 }
                 catch (Exception e)
                 {
@@ -89,16 +83,11 @@ namespace DOL.GS
             });
 
             // Output debug info.
-            if (Debug && _debugRemainingTicks > 0)
+            if (Debug)
             {
-                log.Debug($"==== NPCThink Debug - Total ActiveThinkTimers: {_debugRemainingTicks} ====");
-                log.Debug($"==== Non-Null NPCs in EntityManager Array: {_npcCount} | Null NPCs: {_nullNpcCount} |  Total Size: {list.Count}====");
+                log.Debug($"==== Non-Null NPCs in EntityManager Array: {_npcCount} | Null NPCs: {_nullNpcCount} | Total Size: {list.Count}====");
                 log.Debug("---------------------------------------------------------------------------");
-
-                if (_debugRemainingTicks > 1)
-                    _debugRemainingTicks --;
-                else
-                    _debugRemainingTicks = 0;
+                DebugTickCount--;
             }
 
             Diagnostics.StopPerfCounter(SERVICE_NAME);

commit 3f0d6c945986cadcc62b7a6a40984cf55d75d9c4

Fix pets' only able to follow their owner (caused by ceaa90e7)

GameServer/gameobjects/GameNPC.cs
@@ -3036,7 +3036,8 @@ namespace DOL.GS
 
 			lock (BrainSync)
 			{
-				Brain?.Start();
+				if (Brain?.EntityManagerId == EntityManager.UNSET_ID)
+					Brain.Start();
 			}
 		}
 