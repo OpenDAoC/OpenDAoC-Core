7df8db4258b882065b877b2c885c513f49ed60a7
Deftness <115406842+deftness02@users.noreply.github.com>
2023-05-10 21:58:03 -0600

    Updated how command syntax and descriptions are listed for gradual implementation

--- GameServer/commands/AbstractCommandHandler.cs
+++ GameServer/commands/AbstractCommandHandler.cs
@@ -27,13 +27,12 @@ namespace DOL.GS.Commands
 	public abstract class AbstractCommandHandler
 	{
 		private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-
-
+		
 		/// <summary>
 		/// Is this player spamming this command
 		/// </summary>
-		/// <param name="player"></param>
-		/// <param name="commandName"></param>
+		/// <param name="player">The player performing the command</param>
+		/// <param name="commandName">The command being performed</param>
 		/// <returns></returns>
 		public bool IsSpammingCommand(GamePlayer player, string commandName)
 		{
@@ -43,8 +42,8 @@ namespace DOL.GS.Commands
 		/// <summary>
 		/// Is this player spamming this command
 		/// </summary>
-		/// <param name="player"></param>
-		/// <param name="commandName"></param>
+		/// <param name="player">The player performing the command</param>
+		/// <param name="commandName">The command being performed</param>
 		/// <param name="delay">How long is the spam delay in milliseconds</param>
 		/// <returns>true if less than spam protection interval</returns>
 		public bool IsSpammingCommand(GamePlayer player, string commandName, int delay)
@@ -83,48 +82,164 @@ namespace DOL.GS.Commands
 			if (client == null || !client.IsPlaying)
 				return;
 
-			ChatUtil.SendSystemMessage(client, string.Format(message, objs));
+			ChatUtil.SendTypeMessage((int)eMsg.System, client, string.Format(message, objs));
 			return;
 		}
 
+		/// <summary>
+		/// Displays all attribute messages for a command
+		/// </summary>
+		/// <param name="client">The recipient of the messages (e.g., client)</param>
+		/// <returns>Messages about a command's syntax and usage</returns>
+		/// <example>DisplaySyntax(client);</example>
 		public virtual void DisplaySyntax(GameClient client)
 		{
 			if (client == null || !client.IsPlaying)
 				return;
 
 			var attrib = (CmdAttribute[]) GetType().GetCustomAttributes(typeof (CmdAttribute), false);
+			
 			if (attrib.Length == 0)
 				return;
 
-			// If a value isn't found for a command type header/divider, ignore it
-			if (string.IsNullOrEmpty(attrib[0].Header))
-			{
-				// Include command description at head of list upon typing '/example'
-				ChatUtil.SendCommMessage(client, attrib[0].Description, null);
-			}
-			else
+			var plvlReq = "(plvl " + (int)attrib[0].Level + ") ";
+			var command = attrib[0].Cmd.Remove(0, 1);
+
+			// Only include this on GM/Admin commands until Player commands are "ready"
+			if (client.Account.PrivLevel > 1)
 			{
 				// Include header/divider at head of return upon typing the main command identifier or alias (e.g., '/command')
-				ChatUtil.SendHeaderMessage(client, attrib[0].Header, null);
-
-				// Include main command type description below separator
-				ChatUtil.SendCommMessage(client, attrib[0].Description, null);
+				if (attrib[0].Header != "")
+					// Message: 
+					ChatUtil.SendTypeMessage((int)eMsg.CmdHeader, client, attrib[0].Header, command, plvlReq);
+				else
+					// Message: <----- '/{0}' Command ----->
+					ChatUtil.SendTypeMessage((int)eMsg.CmdHeader, client, "AllCommands.Header.Basic.Commands", command);
 			}
 			
-			// Run for each value found under "params usage" until the whole command list is displayed
+			// Include main command type description below the header
+			// Example: Creates new, manages existing, and controls character assignment for HavenDAoC accounts.
+			ChatUtil.SendTypeMessage((int)eMsg.CmdDesc, client, attrib[0].Description, null);
+			// Line break
+			ChatUtil.SendTypeMessage((int)eMsg.Command, client, " ");
+			// Message: Use the following syntax for this command:
+			ChatUtil.SendTypeMessage((int)eMsg.Command, client, "AllCommands.Text.General.UseSyntax", null);
+
+			// Run for each value found under params until the whole list of subcommands is displayed
 			foreach (var sentence in attrib[0].Usage)
 			{
 				// To contrast the appearance of command syntax against their descriptions, include ".Syntax." in the translation ID (e.g., "AdminCommands.Account.Syntax.AccountName")
 				if (sentence.Contains(".Syntax."))
 				{
-					ChatUtil.SendSyntaxMessage(client, sentence, null);
+					// Example: /account changepassword <accountName> <newPassword>
+					ChatUtil.SendTypeMessage((int)eMsg.CmdSyntax, client, sentence, null);
 				}
 				// All other values display as command descriptions (i.e., CT_System)
 				else
 				{
-					ChatUtil.SendCommMessage(client, sentence, null);	
+					// Example: Changes the password associated with an existing account. If a player requests a password reset, verify ownership of the account.
+					ChatUtil.SendTypeMessage((int)eMsg.CmdUsage, client, sentence, null);
 				}
 			}
+		}
+		
+		/// <summary>
+		/// Displays messages that describe the syntax and behaviors of a command or subcommand
+		/// </summary>
+		/// <param name="client">The recipient of the messages (e.g., client)</param>
+		/// <param name="command">The full base command (e.g., "plvl")</param>
+		/// <param name="subcommand">The full subcommand (e.g., "remove")--leave blank ("") for base command</param>
+		/// <param name="plvl">The privilege level required to use the command (e.g., 3)</param>
+		/// <param name="database">Specifies whether this command may be accomplished using the Atlas Admin tool (e.g., true)</param>
+		/// <param name="syntaxID">The translation ID associated with the subcommand's syntax (e.g., "AdminCommands.Account.Syntax.Comm")</param>
+		/// <param name="usageID">The translation ID associated with the subcommand's description (e.g., "AdminCommands.Account.Usage.Comm")</param>
+		/// <returns>Messages about a command's syntax and usage</returns>
+		/// <example>DisplayHeadSyntax(client, "plvl", "remove", 3, false, "AdminCommands.Account.Syntax.Comm", "AdminCommands.Account.Usage.Comm");</example>
+		public void DisplayHeadSyntax(GameClient client, string command, string subcommand, string subcommand2, int plvl, bool database, string syntaxID, string usageID)
+		{
+			if (client == null || !client.IsPlaying)
+				return;
+			
+			var plvlReq = "(plvl " + plvl + ") "; // Attach plvl to header
+			var subVar = " " + subcommand; // Displays for {1} on subcommand message
+			var subVar2 = " " + subcommand2; // Displays for {1} on subcommand message
+			
+			// Don't show plvl requirements to Players (plvl shouldn't exist to them)
+			if (plvl == 1 && client.Account.PrivLevel == 1)
+				plvlReq = "";
+
+			if (subcommand != "")
+			{
+				if (subcommand2 != "")
+					// Message: <----- '/{0}{1}{2}' Subcommand {3}----->
+					ChatUtil.SendTypeMessage((int)eMsg.CmdHeader, client, "AllCommands.Header.General.2Subcommand", command, subVar, subVar2, plvlReq);
+				else
+					// Message: <----- '/{0}{1}' Subcommand {2}----->
+					ChatUtil.SendTypeMessage((int)eMsg.CmdHeader, client, "AllCommands.Header.General.Subcommand", command, subVar, plvlReq);
+			}
+			
+			if (subcommand == "")
+				// Message: <----- '/{0}' Command {2}----->
+				ChatUtil.SendTypeMessage((int)eMsg.CmdHeader, client, "AllCommands.Header.General.Commands", command, plvlReq);
+			
+			if (database == false || client.Account.PrivLevel == 1)
+				// Message: Use the following syntax for this command:
+				ChatUtil.SendTypeMessage((int)eMsg.Command, client, "AllCommands.Text.General.UseSyntax", null);
+				
+			if (database && client.Account.PrivLevel > 1)
+				// Message: It is recommended that you perform actions associated with this command with the Atlas Admin (https://admin.atlasfreeshard.com). Otherwise, use the following syntax:
+				ChatUtil.SendTypeMessage((int)eMsg.Command, client, "AllCommands.Text.General.SyntaxDB", null);
+			
+			// Example: /account command
+			ChatUtil.SendTypeMessage((int)eMsg.CmdSyntax, client, syntaxID, null);
+			// Example: Provides additional information regarding the '/account' command type.
+			ChatUtil.SendTypeMessage((int)eMsg.CmdUsage, client, usageID, null);
+		}
+		
+		/// <summary>
+		/// Displays messages that describe the syntax and behaviors of a subcommand, without the header strings
+		/// </summary>
+		/// <param name="client">The recipient of the messages (e.g., client)</param>
+		/// <param name="syntaxID">The translation ID associated with the subcommand's syntax (e.g., "AdminCommands.Account.Syntax.Comm")</param>
+		/// <param name="usageID">The translation ID associated with the subcommand's description (e.g., "AdminCommands.Account.Usage.Comm")</param>
+		/// <returns>Messages about a subcommand's syntax</returns>
+		/// <example>DisplaySubSyntax(client, "AdminCommands.Account.Syntax.Comm", "AdminCommands.Account.Usage.Comm");</example>
+		public void DisplaySubSyntax(GameClient client, string syntaxID, string usageID)
+		{
+			if (client == null || !client.IsPlaying)
+				return;
+			
+			// Example: /account command
+			ChatUtil.SendTypeMessage((int)eMsg.CmdSyntax, client, syntaxID, null);
+			// Example: Provides additional information regarding the '/account' command type.
+			ChatUtil.SendTypeMessage((int)eMsg.CmdUsage, client, usageID, null);
+		}
+		
+		public void DisplayHeaderSyntax(GameClient client, string headerID)
+		{
+			if (client == null || !client.IsPlaying)
+				return;
+			
+			// Example: /account command
+			ChatUtil.SendTypeMessage((int)eMsg.CmdHeader, client, headerID, null);
+		}
+		
+		public void DisplaySyntaxSyntax(GameClient client, string syntaxID)
+		{
+			if (client == null || !client.IsPlaying)
+				return;
+			
+			// Example: /account command
+			ChatUtil.SendTypeMessage((int)eMsg.CmdSyntax, client, syntaxID, null);
+		}
+		
+		public void DisplayUsageSyntax(GameClient client, string usageID)
+		{
+			if (client == null || !client.IsPlaying)
+				return;
+			
+			// Example: Provides additional information regarding the '/account' command type. 
+			ChatUtil.SendTypeMessage((int)eMsg.CmdUsage, client, usageID, null);
 		}
 
 		public virtual void DisplaySyntax(GameClient client, string subcommand)
