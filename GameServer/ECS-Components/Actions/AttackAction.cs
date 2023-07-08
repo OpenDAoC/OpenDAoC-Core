OpenDAoC/OpenDAoC-Core

commit df8a2ee9a9be2acaea71dfba118c6bfb33161aa3

Fix odd ranged attack targetting behavior

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -83,8 +83,7 @@ namespace DOL.GS
             }
             else
             {
-                // Must be done here because 'RangeAttackTarget' is changed in 'CheckRangeAttackState'.
-                _target = _owner.rangeAttackComponent.Target;
+                _target = _owner.rangeAttackComponent.Target ?? _owner.TargetObject;
 
                 if (PrepareRangedAttack())
                 {
@@ -95,7 +94,7 @@ namespace DOL.GS
 
             StartTime = _interval;
         }
-        
+
         public virtual bool CheckInterruptTimer()
         {
             return false;
@@ -129,13 +128,10 @@ namespace DOL.GS
 
         protected virtual bool CanPerformAction()
         {
-            if (_owner.IsMezzed || _owner.IsStunned)
+            if (_owner.IsMezzed || _owner.IsStunned || _owner.IsEngaging)
                 return false;
 
             if (_owner.IsCasting && !_owner.CurrentSpellHandler?.Spell.Uninterruptible != true)
-                return false;
-
-            if (_owner.IsEngaging || _owner.TargetObject == null)
                 return false;
 
             return true;

GameServer/ECS-Components/RangeAttackComponent.cs
@@ -2,7 +2,6 @@
 using DOL.Database;
 using DOL.GS.PacketHandler;
 using DOL.Language;
-using static DOL.GS.GameObject;
 
 namespace DOL.GS
 {
@@ -21,34 +20,12 @@ namespace DOL.GS
         public const int VOLLEY_ENDURANCE_COST = 15;
         public const int PROJECTILE_FLIGHT_SPEED = 1800; // 1800 units per second. Live value is unknown, but DoL had 1500. Also affects throwing weapons.
         public const int MAX_DRAW_DURATION = 15000;
-        protected eRangedAttackState m_rangedAttackState;
-        protected eRangedAttackType m_rangedAttackType;
-        protected eActiveQuiverSlot m_activeQuiverSlot;
-        protected WeakReference m_rangeAttackAmmo = new WeakRef(null);
-        protected WeakReference m_rangeAttackTarget = new WeakRef(null);
-        protected InventoryItem m_ammo;
-        protected bool m_isAmmoCompatible;
-
-        public eRangedAttackState RangedAttackState
-        {
-            get => m_rangedAttackState;
-            set => m_rangedAttackState = value;
-        }
-
-        public eRangedAttackType RangedAttackType
-        {
-            get => m_rangedAttackType;
-            set => m_rangedAttackType = value;
-        }
-
-        public eActiveQuiverSlot ActiveQuiverSlot
-        {
-            get => m_activeQuiverSlot;
-            set => m_activeQuiverSlot = value;
-        }
-
-        public InventoryItem Ammo => m_ammo;
-        public bool IsAmmoCompatible => m_isAmmoCompatible;
+        public GameObject Target { get; set; }
+        public eRangedAttackState RangedAttackState { get; set; }
+        public eRangedAttackType RangedAttackType { get; set; }
+        public eActiveQuiverSlot ActiveQuiverSlot { get; set; }
+        public InventoryItem Ammo { get; private set; }
+        public bool IsAmmoCompatible { get; private set; }
 
         private InventoryItem GetAmmoFromInventory(eObjectType ammoType)
         {
@@ -71,8 +48,8 @@ namespace DOL.GS
 
         public InventoryItem UpdateAmmo(InventoryItem weapon)
         {
-            m_ammo = null;
-            m_isAmmoCompatible = true;
+            Ammo = null;
+            IsAmmoCompatible = true;
 
             if (m_owner is not GamePlayer || weapon == null)
                 return null;
@@ -80,41 +57,22 @@ namespace DOL.GS
             switch (weapon.Object_Type)
             {
                 case (int)eObjectType.Thrown:
-                    m_ammo = m_owner.Inventory.GetItem(eInventorySlot.DistanceWeapon);
+                    Ammo = m_owner.Inventory.GetItem(eInventorySlot.DistanceWeapon);
                     break;
                 case (int)eObjectType.Crossbow:
-                    m_ammo = GetAmmoFromInventory(eObjectType.Bolt);
-                    m_isAmmoCompatible = m_ammo?.Object_Type == (int)eObjectType.Bolt;
+                    Ammo = GetAmmoFromInventory(eObjectType.Bolt);
+                    IsAmmoCompatible = Ammo?.Object_Type == (int)eObjectType.Bolt;
                     break;
                 case (int)eObjectType.Longbow:
                 case (int)eObjectType.CompositeBow:
                 case (int)eObjectType.RecurvedBow:
                 case (int)eObjectType.Fired:
-                    m_ammo = GetAmmoFromInventory(eObjectType.Arrow);
-                    m_isAmmoCompatible = m_ammo?.Object_Type == (int)eObjectType.Arrow;
+                    Ammo = GetAmmoFromInventory(eObjectType.Arrow);
+                    IsAmmoCompatible = Ammo?.Object_Type == (int)eObjectType.Arrow;
                     break;
             }
 
-            return m_ammo;
-        }
-
-        public GameObject Target
-        {
-            get
-            {
-                if (m_owner is GamePlayer)
-                {
-                    GameObject target = (GameObject)m_rangeAttackTarget.Target;
-
-                    if (target == null || target.ObjectState != eObjectState.Active)
-                        target = m_owner.TargetObject;
-
-                    return target;
-                }
-                else
-                    return m_owner.TargetObject;
-            }
-            set => m_rangeAttackTarget.Target = value;
+            return Ammo;
         }
 
         /// <summary>
@@ -162,7 +120,7 @@ namespace DOL.GS
                     else if (UpdateAmmo(playerOwner.ActiveWeapon) == null)
                         // Another check for ammo just before firing.
                         playerOwner.Out.SendMessage(LanguageMgr.GetTranslation(playerOwner.Client.Account.Language, "GamePlayer.Attack.MustSelectQuiver"), eChatType.CT_YouHit, eChatLoc.CL_SystemWindow);
-                    else if (!m_isAmmoCompatible)
+                    else if (!IsAmmoCompatible)
                         playerOwner.Out.SendMessage(LanguageMgr.GetTranslation(playerOwner.Client.Account.Language, "GamePlayer.Attack.CantUseQuiver"), eChatType.CT_YouHit, eChatLoc.CL_SystemWindow);
                     else if (GameServer.ServerRules.IsAllowedToAttack(playerOwner, (GameLiving)target, false))
                     {
@@ -233,7 +191,7 @@ namespace DOL.GS
             int arrowRecoveryChance = m_owner.GetModified(eProperty.ArrowRecovery);
 
             if (arrowRecoveryChance == 0 || Util.Chance(100 - arrowRecoveryChance))
-                m_owner.Inventory.RemoveCountFromStack(m_ammo, 1);
+                m_owner.Inventory.RemoveCountFromStack(Ammo, 1);
 
             if (RangedAttackType == eRangedAttackType.Critical)
                 m_owner.Endurance -= CRITICAL_SHOT_ENDURANCE_COST;

commit 10d619892757c7d97d3cf30c0689350c24cdc765

Fix ranged NPCs not shooting (caused by df8a2ee)

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -83,7 +83,7 @@ namespace DOL.GS
             }
             else
             {
-                _target = _owner.rangeAttackComponent.Target ?? _owner.TargetObject;
+                _target = _owner.rangeAttackComponent.AutoFireTarget ?? _owner.TargetObject;
 
                 if (PrepareRangedAttack())
                 {

GameServer/ECS-Components/AttackComponent.cs
@@ -854,10 +854,10 @@ namespace DOL.GS
             if (speed <= 0)
                 return false;
 
-            // Npcs aren't allowed to prepare their ranged attack while moving or out of range.
+            // NPCs aren't allowed to prepare their ranged attack while moving or out of range.
             if (owner is not GamePlayer && owner.ActiveWeaponSlot == eActiveWeaponSlot.Distance)
             {
-                if (owner.IsMoving || !owner.IsWithinRadius(owner.rangeAttackComponent.Target, owner.attackComponent.AttackRange))
+                if (owner.IsMoving || !owner.IsWithinRadius(owner.TargetObject, owner.attackComponent.AttackRange))
                     return false;
             }
 

GameServer/ECS-Components/RangeAttackComponent.cs
@@ -20,7 +20,7 @@ namespace DOL.GS
         public const int VOLLEY_ENDURANCE_COST = 15;
         public const int PROJECTILE_FLIGHT_SPEED = 1800; // 1800 units per second. Live value is unknown, but DoL had 1500. Also affects throwing weapons.
         public const int MAX_DRAW_DURATION = 15000;
-        public GameObject Target { get; set; }
+        public GameObject AutoFireTarget { get; set; } // Used to shoot at a different target than the one currently selected. Always null for NPCs.
         public eRangedAttackState RangedAttackState { get; set; }
         public eRangedAttackType RangedAttackType { get; set; }
         public eActiveQuiverSlot ActiveQuiverSlot { get; set; }
@@ -101,7 +101,7 @@ namespace DOL.GS
                 if (RangedAttackState is eRangedAttackState.Fire or eRangedAttackState.AimFire or eRangedAttackState.AimFireReload)
                 {
                     // Clean the RangeAttackTarget at the first shot try even if failed.
-                    Target = null;
+                    AutoFireTarget = null;
 
                     if (target is null or not GameLiving)
                     {

GameServer/gameobjects/GameLiving.cs
@@ -2545,7 +2545,7 @@ namespace DOL.GS
 			//ConcentrationEffects.CancelAll();
 
             // clear all of our targets
-            rangeAttackComponent.Target = null;
+            rangeAttackComponent.AutoFireTarget = null;
 			TargetObject = null;
 
 			// cancel all left effects

GameServer/gameobjects/GamePlayer.cs
@@ -8004,10 +8004,10 @@ namespace DOL.GS
                                 }
                                 else
                                 {
-                                    if (rangeAttackComponent.Target == null)
+                                    if (rangeAttackComponent.AutoFireTarget == null)
                                     {
                                         //set new target only if there was no target before
-                                        rangeAttackComponent.Target = TargetObject;
+                                        rangeAttackComponent.AutoFireTarget = TargetObject;
                                     }
 
                                     rangeAttackComponent.RangedAttackState = eRangedAttackState.AimFire;

GameServer/spells/AmnesiaSpellHandler.cs
@@ -77,7 +77,7 @@ namespace DOL.GS.Spells
 			if (target.CurrentSpellHandler?.Spell.InstrumentRequirement == 0)
 				target.castingComponent.ClearUpSpellHandlers(); //stop even if MoC or QC
 
-			target.rangeAttackComponent.Target = null;
+			target.rangeAttackComponent.AutoFireTarget = null;
 			//if(target is GamePlayer)
 				//target.TargetObject = null;
 