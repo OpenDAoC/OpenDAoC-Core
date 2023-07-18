501199ae0bfcaa28629b06312bf471a795ab4678
Baptiste Marie <bm01@users.noreply.github.com>
2023-07-18 04:18:46 +0200

    Remove bonus decks (RNG) I think we should leave it to server owners to decide if they want to "lie" to players. Most of them are probably even unaware of it. Bonus decks effectively screw in-game RNG tests and affect game balance to some extent. They're also unintuitive to use since they only add high numbers, so depending on the usage it may have the opposite of the desired effect.

--- GameServer/gameutils/DeckOfCardsRandomizer/Deck.cs
+++ GameServer/gameutils/DeckOfCardsRandomizer/Deck.cs
@@ -3,134 +3,123 @@ using System.Collections.Concurrent;
 using System.Linq;
 using Newtonsoft.Json;
 
-namespace DOL.GS.Utils;
-
-public class PlayerDeck
+namespace DOL.GS.Utils
 {
-    private const int NUM_BONUS_DECKS = 1;
-    private const int NUM_NORMAL_DECKS = 1;
-    private const int BONUS_DECK_SIZE = 25;
-    private const int PLAYER_DECK_SIZE = NUM_NORMAL_DECKS * 100 + NUM_BONUS_DECKS * BONUS_DECK_SIZE;
-
-    private ConcurrentStack<int> _cards;
-
-    public PlayerDeck()
+    public class PlayerDeck
     {
-        _cards = new ConcurrentStack<int>();
-        ResetDeck();
-    }
+        private const int DECKS_COUNT = 1;
 
-    private void InitializeDeck()
-    {
-        lock (_cardLock)
+        private ConcurrentStack<int> _cards;
+
+        public PlayerDeck()
         {
-            _cards.Clear();
+            _cards = new ConcurrentStack<int>();
+            ResetDeck();
+        }
 
-            for (int i = 0; i < NUM_NORMAL_DECKS; i++)
+        private void InitializeDeck()
+        {
+            lock (_cardLock)
             {
-                for (int j = 0; j < 100; j++)
-                {
-                    _cards.Push(j+1); //offset by 1 to only generate 'cards' with values 1-100
-                }
-            }
+                _cards.Clear();
 
-            for (int i = 0; i < NUM_BONUS_DECKS; i++)
-            {
-                for (int j = (100-BONUS_DECK_SIZE); j < 100; j++)
+                for (int i = 0; i < DECKS_COUNT; i++)
                 {
-                    //add a "bonus deck" of high numbers X-99
-                    _cards.Push(j);
+                    for (int j = 1; j < 101; j++) // Offset by 1 to only generate 'cards' with values 1-100
+                    {
+                        _cards.Push(j);
+                    }
                 }
             }
         }
-    }
 
-    private void ResetDeck()
-    {
-        InitializeDeck();
-        //shuffle thrice for good luck?
-        Shuffle();
-        Shuffle();
-        Shuffle();
-        //Console.WriteLine($"deck reset");
-    }
-
-    private object _cardLock = new object();
-    
-    private void Shuffle()
-    {
-        int[] preshuffle = null;
-        lock (_cardLock)
+        private void ResetDeck()
         {
-            preshuffle = _cards.ToArray();
+            InitializeDeck();
+            //shuffle thrice for good luck?
+            Shuffle();
+            Shuffle();
+            Shuffle();
+            //Console.WriteLine($"deck reset");
         }
 
-        //Fisher-Yates shuffle algorithm
-        // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-        int cardsLength = _cards.Count - 1;
-        while (cardsLength > 1)
+        private object _cardLock = new object();
+
+        private void Shuffle()
         {
-            cardsLength--;
-            int k = Util.CryptoNextInt(cardsLength + 1);
-            (preshuffle[k], preshuffle[cardsLength]) = (preshuffle[cardsLength], preshuffle[k]);
-        }
-        /*
-        //randomly order the contents of the array, then reassign the array
-        int[] shuffled = _cards.ToArray().OrderBy(x => Util.CryptoNextInt(cardsLength-1)).ToArray();
-        */
-        lock (_cardLock)
-        {
-            _cards.Clear();
-            foreach (var i in preshuffle)
+            int[] preshuffle = null;
+            lock (_cardLock)
             {
-                _cards.Push(i);
+                preshuffle = _cards.ToArray();
+            }
+
+            //Fisher-Yates shuffle algorithm
+            // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+            int cardsLength = _cards.Count - 1;
+            while (cardsLength > 1)
+            {
+                cardsLength--;
+                int k = Util.CryptoNextInt(cardsLength + 1);
+                (preshuffle[k], preshuffle[cardsLength]) = (preshuffle[cardsLength], preshuffle[k]);
+            }
+            /*
+            //randomly order the contents of the array, then reassign the array
+            int[] shuffled = _cards.ToArray().OrderBy(x => Util.CryptoNextInt(cardsLength-1)).ToArray();
+            */
+            lock (_cardLock)
+            {
+                _cards.Clear();
+                foreach (var i in preshuffle)
+                {
+                    _cards.Push(i);
+                }
             }
         }
-    }
 
-    public int GetInt()
-    {
-        if (_cards.Count < 2)
-            ResetDeck();
-
-        _cards.TryPop(out int output);
-        return output;
-    }
-
-    public double GetPseudoDouble()
-    {
-        if (_cards.Count < 2)
+        public int GetInt()
         {
-            ResetDeck();
-            Shuffle(); //shuffle it for fun
+            if (_cards.Count < 2)
+                ResetDeck();
+
+            _cards.TryPop(out int output);
+            return output;
         }
 
-        //we append two ints together to simulate more accuracy on the double
-        //subtract 1 to only generate values 0-99
-        //useful to get outputs of 0-9999 instead of 11-100100
+        public double GetPseudoDouble()
+        {
+            if (_cards.Count < 2)
+            {
+                ResetDeck();
+                Shuffle(); //shuffle it for fun
+            }
 
-        _cards.TryPop(out int first);
-        first--;
-        int second = Util.CryptoNextInt(100); //just use a simple random for the .XX values
+            //we append two ints together to simulate more accuracy on the double
+            //subtract 1 to only generate values 0-99
+            //useful to get outputs of 0-9999 instead of 11-100100
 
-        //append our ints together
-        //if we are unable to parse numbers for any reason, use a 0
-        if (!int.TryParse(first.ToString() + second.ToString("D2"), out int append))
-            append = 0;
+            _cards.TryPop(out int first);
+            first--;
+            int second = Util.CryptoNextInt(100); //just use a simple random for the .XX values
 
-        //divide by max possible value to simulate 0-1 output of doubles
-        double pseudoDouble = append / 9999.0;
-        return pseudoDouble;
-    }
+            //append our ints together
+            //if we are unable to parse numbers for any reason, use a 0
+            if (!int.TryParse(first.ToString() + second.ToString("D2"), out int append))
+                append = 0;
 
-    public string SaveDeckToJSON()
-    {
-        string json = JsonConvert.SerializeObject(_cards.Reverse());
-        return json;
-    }
+            //divide by max possible value to simulate 0-1 output of doubles
+            double pseudoDouble = append / 9999.0;
+            return pseudoDouble;
+        }
 
-    public void LoadDeckFromJSON(string json)
-    {
-        _cards = JsonConvert.DeserializeObject<ConcurrentStack<int>>(json);
+        public string SaveDeckToJSON()
+        {
+            string json = JsonConvert.SerializeObject(_cards.Reverse());
+            return json;
+        }
+
+        public void LoadDeckFromJSON(string json)
+        {
+            _cards = JsonConvert.DeserializeObject<ConcurrentStack<int>>(json);
+        }
     }
 }
