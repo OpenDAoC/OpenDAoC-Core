OpenDAoC/OpenDAoC-Core

commit 10d619892757c7d97d3cf30c0689350c24cdc765

Fix ranged NPCs not shooting (caused by df8a2ee)

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -83,7 +83,7 @@ namespace DOL.GS
             }
             else
             {
-                _target = _owner.rangeAttackComponent.Target ?? _owner.TargetObject;
+                _target = _owner.rangeAttackComponent.AutoFireTarget ?? _owner.TargetObject;
 
                 if (PrepareRangedAttack())
                 {

GameServer/ECS-Components/AttackComponent.cs
@@ -854,10 +854,10 @@ namespace DOL.GS
             if (speed <= 0)
                 return false;
 
-            // Npcs aren't allowed to prepare their ranged attack while moving or out of range.
+            // NPCs aren't allowed to prepare their ranged attack while moving or out of range.
             if (owner is not GamePlayer && owner.ActiveWeaponSlot == eActiveWeaponSlot.Distance)
             {
-                if (owner.IsMoving || !owner.IsWithinRadius(owner.rangeAttackComponent.Target, owner.attackComponent.AttackRange))
+                if (owner.IsMoving || !owner.IsWithinRadius(owner.TargetObject, owner.attackComponent.AttackRange))
                     return false;
             }
 

GameServer/ECS-Components/RangeAttackComponent.cs
@@ -20,7 +20,7 @@ namespace DOL.GS
         public const int VOLLEY_ENDURANCE_COST = 15;
         public const int PROJECTILE_FLIGHT_SPEED = 1800; // 1800 units per second. Live value is unknown, but DoL had 1500. Also affects throwing weapons.
         public const int MAX_DRAW_DURATION = 15000;
-        public GameObject Target { get; set; }
+        public GameObject AutoFireTarget { get; set; } // Used to shoot at a different target than the one currently selected. Always null for NPCs.
         public eRangedAttackState RangedAttackState { get; set; }
         public eRangedAttackType RangedAttackType { get; set; }
         public eActiveQuiverSlot ActiveQuiverSlot { get; set; }
@@ -101,7 +101,7 @@ namespace DOL.GS
                 if (RangedAttackState is eRangedAttackState.Fire or eRangedAttackState.AimFire or eRangedAttackState.AimFireReload)
                 {
                     // Clean the RangeAttackTarget at the first shot try even if failed.
-                    Target = null;
+                    AutoFireTarget = null;
 
                     if (target is null or not GameLiving)
                     {

GameServer/gameobjects/GameLiving.cs
@@ -2545,7 +2545,7 @@ namespace DOL.GS
 			//ConcentrationEffects.CancelAll();
 
             // clear all of our targets
-            rangeAttackComponent.Target = null;
+            rangeAttackComponent.AutoFireTarget = null;
 			TargetObject = null;
 
 			// cancel all left effects

GameServer/gameobjects/GamePlayer.cs
@@ -8004,10 +8004,10 @@ namespace DOL.GS
                                 }
                                 else
                                 {
-                                    if (rangeAttackComponent.Target == null)
+                                    if (rangeAttackComponent.AutoFireTarget == null)
                                     {
                                         //set new target only if there was no target before
-                                        rangeAttackComponent.Target = TargetObject;
+                                        rangeAttackComponent.AutoFireTarget = TargetObject;
                                     }
 
                                     rangeAttackComponent.RangedAttackState = eRangedAttackState.AimFire;

GameServer/spells/AmnesiaSpellHandler.cs
@@ -77,7 +77,7 @@ namespace DOL.GS.Spells
 			if (target.CurrentSpellHandler?.Spell.InstrumentRequirement == 0)
 				target.castingComponent.ClearUpSpellHandlers(); //stop even if MoC or QC
 
-			target.rangeAttackComponent.Target = null;
+			target.rangeAttackComponent.AutoFireTarget = null;
 			//if(target is GamePlayer)
 				//target.TargetObject = null;
 

commit 55bb1236a79a8dfd92f1c0f7c8416c46fae65c25

Fix `AttackAction` ticking at attack speed interval even with no target Could delay the first attack. Caused by 10d6198.

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -146,7 +146,6 @@ namespace DOL.GS
                 _styleComponent.NextCombatBackupStyle = null;
                 _attackData.AttackResult = eAttackResult.Missed;
                 _interval = _attackComponent.AttackSpeed(_weapon) * 2;
-                StartTime = _interval;
                 return false;
             }
 
@@ -167,7 +166,14 @@ namespace DOL.GS
                 _styleComponent.NextCombatBackupStyle = null;
             }
 
-            // Damage is doubled on sitting players, but only with melee weapons; arrows and magic does normal damage.
+            // Styles must be checked before the target.
+            if (_target == null)
+            {
+                _interval = TICK_INTERVAL_FOR_NON_ATTACK;
+                return false;
+            }
+
+            // Damage is doubled on sitting players, but only with melee weapons; arrows and magic do normal damage.
             if (_target is GamePlayer playerTarget && playerTarget.IsSitting)
                 _effectiveness *= 2;
 