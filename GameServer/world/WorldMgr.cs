OpenDAoC/OpenDAoC-Core

commit d1ba7e9e86edcfc27ceb1cde194fcb8332253134

Replace `ReaderWriterDictionary` with `ConcurrentDictionary` Using simulated clients, a lot of CPU time was apparently spent in locks, mostly because in `WorldMgr.GetRegion`. `ConcurrentDictionary` is lock free on read. Substantially reduces the amount of "lag" message at low and medium CPU usage. `WorldMgr.CreateInstance` and `FriendsManager` had to be rewritten a bit need to be tested.

GameServer/world/WorldMgr.cs
@@ -18,6 +18,7 @@
  */
 
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -170,7 +171,7 @@ namespace DOL.GS
 		/// <summary>
 		/// This hashtable holds all regions in the world
 		/// </summary>
-		private static readonly ReaderWriterDictionary<ushort, Region> m_regions = new ReaderWriterDictionary<ushort, Region>();
+		private static readonly ConcurrentDictionary<ushort, Region> m_regions = new();
 
 		public static IDictionary<ushort, Region> Regions
 		{
@@ -180,7 +181,7 @@ namespace DOL.GS
 		/// <summary>
 		/// This hashtable holds all zones in the world, for easy access
 		/// </summary>
-		private static readonly ReaderWriterDictionary<ushort, Zone> m_zones = new ReaderWriterDictionary<ushort, Zone>();
+		private static readonly ConcurrentDictionary<ushort, Zone> m_zones = new();
 
 		public static IDictionary<ushort, Zone> Zones
 		{
@@ -386,10 +387,6 @@ namespace DOL.GS
 			regions.Sort();
 
 			log.DebugFormat("{0}MB - Region Data Loaded", GC.GetTotalMemory(true) / 1024 / 1024);
-
-			int cpuCount = GameServer.Instance.Configuration.CPUUse;
-			if (cpuCount < 1)
-				cpuCount = 1;
 
 			for (int i = 0; i < regions.Count; i++)
 			{
@@ -728,8 +725,7 @@ namespace DOL.GS
 		public static Region RegisterRegion(RegionData data)
 		{
 			Region region =  Region.Create(data);
-			m_regions.Add(data.Id, region);
-			return region;
+			return m_regions.TryAdd(data.Id, region) ? region : null;
 		}
 
 		/// <summary>
@@ -815,23 +811,18 @@ namespace DOL.GS
                     height * 8192);*/
 
 			region.Zones.Add(zone);
-			
-			m_zones.AddOrReplace(zoneID, zone);
-
+			m_zones[zoneID] = zone;
 			log.InfoFormat("Added a zone, {0}, to region {1}", zoneData.Description, region.Name);
 		}
 
 		/// <summary>
 		/// Starts all RegionMgrs inside the Regions
 		/// </summary>
-		/// <returns>true</returns>
 		public static bool StartRegionMgrs()
 		{
-			m_regions.FreezeWhile(dict => {
-			                      	foreach (Region reg in dict.Values)
-			                      		reg.StartRegionMgr();
+			foreach (Region region in m_regions.Values)
+				region.StartRegionMgr();
 
-			                      });			
 			return true;
 		}
 
@@ -843,11 +834,8 @@ namespace DOL.GS
 			if (log.IsDebugEnabled)
 				log.Debug("Stopping region managers...");
 			
-			m_regions.FreezeWhile(dict => {
-			                      	foreach (Region reg in dict.Values)
-			                      		reg.StopRegionMgr();
-
-			                      });			
+			foreach (Region region in m_regions.Values)
+				region.StopRegionMgr();
 
 			if (log.IsDebugEnabled)
 				log.Debug("Region managers stopped.");
@@ -1686,13 +1674,8 @@ namespace DOL.GS
 		/// <summary>
 		/// Tries to create an instance with the suggested ID and a given 'skin' (the regionID to display client side).
 		/// </summary>
-		/// <param name="requestedID">0 for random</param>
-		/// <param name="skinID"></param>
-		/// <param name="instanceType"></param>
-		/// <returns></returns>
 		public static BaseInstance CreateInstance(ushort requestedID, ushort skinID, Type instanceType)
 		{
-			//TODO: Typeof field so TaskDungeonInstance, QuestInstance etc can be created.
 			if ((instanceType.IsSubclassOf(typeof(BaseInstance)) || instanceType == typeof(BaseInstance)) == false)
 			{
 				log.Error("Invalid type given for instance creation: " + instanceType + ". Returning null instance now.");
@@ -1700,9 +1683,7 @@ namespace DOL.GS
 			}
 
 			BaseInstance instance = null;
-
-			//To create the instance, we need to select the region relevant to the SkinID.
-			var data = m_regionData[skinID];
+			RegionData data = m_regionData[skinID];
 
 			if (data == null)
 			{
@@ -1710,7 +1691,6 @@ namespace DOL.GS
 				return null;
 			}
 
-			//I've placed constructor info outside of the lock, to prevent a time delay on parallel threads.
 			ConstructorInfo info = instanceType.GetConstructor(new Type[] { typeof(ushort), typeof(RegionData)});
 
 			if (info == null)
@@ -1720,66 +1700,44 @@ namespace DOL.GS
 			}
 
 			bool RequestedAnID = requestedID == 0 ? false : true;
-
 			ushort ID = requestedID;
+			bool success = false;
 
-			//Get the unique ID for this instance or try to create an instance at the requested ID
+			if (RequestedAnID)
+				success = m_regions.TryAdd(ID, instance);
+			else
+			{
+				for (ID = DEFAULT_VALUE_FOR_INSTANCE_ID_SEARCH_START; ID <= ushort.MaxValue; ID++)
+				{
+					if (m_regions.TryAdd(ID, instance))
+					{
+						success = true;
+						break;
+					}
+				}
+			}
 
-			//We need to keep the lock over this whole area until we have successfully inserted the instance,
-			//incase a parallel thread also receives a request to create an instance. We cant have the two colliding!
-			//If they did, one instance generation would fail.
-
-			//I'm welcome to suggestions on how to improve this
-			//              -Dinberg.
-			if (!m_regions.FreezeWhile<bool>(regions => {
-			                                         	if (!RequestedAnID)
-			                                         	{
-			                                         		ID = DEFAULT_VALUE_FOR_INSTANCE_ID_SEARCH_START;
-			                                         		while (ID < ushort.MaxValue)
-			                                         		{
-			                                         			//Look for a space in the regions table...
-			                                         			if (!regions.ContainsKey(ID))
-			                                         				break;
-			                                         			
-			                                         			//If no space here, no worries - move quickly to the next ID and continue.
-			                                         			ID++;
-			                                         		}
-			                                         	}
-			                                         	else if (regions.ContainsKey(ID))
-			                                         	{
-			                                         		// requested ID is in use
-			                                         		return false;
-			                                         	}
-			                                         	//In the unlikely event of 65535 regions, I'd still like to be warned!
-			                                         	if (ID == ushort.MaxValue)
-			                                         	{
-			                                         		log.Warn("ID was ushort.MaxValue - Region Table is full upon instance creation! Aborting now.");
-			                                         		return false;
-			                                         	}
-			                                         	//Having selected the data we need, create the Instance.
-			                                         	try
-			                                         	{
-			                                         		instance = (BaseInstance)info.Invoke(new object[] { ID, data });
-			                                         		regions.Add(ID, instance);
-			                                         	}
-			                                         	catch (Exception e)
-			                                         	{
-			                                         		log.ErrorFormat("Error on instance creation - {0}{1}", e.Message, e.StackTrace);
-			                                         		return false;
-			                                         	}
-			                                         	
-			                                         	return true;
-			                                 }))
+			if (!success)
+			{
+				log.Error($"Failed to add new instance to region table (ID: {ID})");
 				return null;
-			
-			// But its not over there. We need to put a zone into the instance.
+			}
+
+			try
+			{
+				instance = (BaseInstance) info.Invoke(new object[] { ID, data });
+			}
+			catch (Exception e)
+			{
+				log.ErrorFormat("Error on instance creation - {0} {1}", e.Message, e.StackTrace);
+				return null;
+			}
+
 
 			List<ZoneData> list = null;
 
 			if (m_zonesData.ContainsKey(data.Id))
-			{
 				list = m_zonesData[data.Id];
-			}
 
 			if (list == null)
 			{
@@ -1787,63 +1745,36 @@ namespace DOL.GS
 				return null;
 			}
 
-			int zoneID = 0;
+			ushort zoneID = 0;
 
 			foreach (ZoneData dat in list)
 			{
-				//we need to get an id for each one.
-				if (m_zones.FreezeWhile<bool>(zones =>
-				                              {
-				                              	while (zoneID <= ushort.MaxValue && zones.ContainsKey((ushort)zoneID))
-				                              		zoneID++;
-				                              	
-				                              	if (zoneID >= ushort.MaxValue)
-				                              		log.Error("Zone limit reached in instance creation!");
-				                              	
-				                              	if (zoneID > ushort.MaxValue)
-				                              		return false;
-				                              	
-				                              	// register id
-				                              	zones.Add((ushort)zoneID, null);
-				                              	return true;
-				                              }))
+				for (; zoneID <= ushort.MaxValue; ID++)
 				{
-	            	RegisterZone(dat, (ushort)zoneID, ID, string.Format("{0} (Instance)", dat.Description), 0, 0, 0, 0, 0);
+					if (m_zones.TryAdd(zoneID, null))
+					{
+						RegisterZone(dat, zoneID, ID, string.Format("{0} (Instance)", dat.Description), 0, 0, 0, 0, 0);
+						break;
+					}
 				}
 			}
 
-			// Start the instance and execute any final startup tasks
 			instance.Start();
-
 			return instance;
 		}
 
 		/// <summary>
 		/// Removes the given instance from the server.
 		/// </summary>
-		/// <param name="instance"></param>
 		public static void RemoveInstance(BaseInstance instance)
 		{
-			//Remove the region
-			Region reg;
-			m_regions.TryRemove(instance.ID, out reg);
+			m_regions.TryRemove(instance.ID, out _);
 
-			//Remove zones
-			m_zones.FreezeWhile(zones =>
-			                    {
-			                    	foreach (Zone zn in instance.Zones)
-			                    	{
-			                    		if (zones.ContainsKey(zn.ID))
-			                    			zones.Remove(zn.ID);
-			                    	}
-			                    });
+			foreach (Zone zn in instance.Zones)
+				m_zones.TryRemove(zn.ID, out _);
 
 			instance.OnCollapse();
-
-			//Destroy the region once and for all.
-			instance = null;
 		}
-
 
 		#endregion
 	}

GameServer/GameClient.cs
@@ -179,7 +179,7 @@ namespace DOL.GS
 		/// <summary>
 		/// This variable holds the active charindex
 		/// </summary>
-		protected int m_activeCharIndex;
+		protected int m_activeCharIndex = -1;
 
 		/// <summary>
 		/// Holds installed client addons
@@ -196,7 +196,7 @@ namespace DOL.GS
 		/// </summary>
 		protected eClientType m_clientType = eClientType.Unknown;
 
-		protected eClientVersion m_clientVersion;
+		protected eClientVersion m_clientVersion = eClientVersion.VersionNotChecked;
 
 		/// <summary>
 		/// Holds the time of the last UDP ping
@@ -251,18 +251,18 @@ namespace DOL.GS
 		/// <summary>
 		/// Holds the Player Collection of Updated Object with last update time.
 		/// </summary>
-		protected ReaderWriterDictionary<Tuple<ushort, ushort>, long> m_GameObjectUpdateArray;
+		protected ConcurrentDictionary<Tuple<ushort, ushort>, long> m_GameObjectUpdateArray = new();
 
 		/// <summary>
 		/// Holds the Player Collection of Updated House with last update time.
 		/// </summary>
-		protected ReaderWriterDictionary<Tuple<ushort, ushort>, long> m_HouseUpdateArray;
+		protected ConcurrentDictionary<Tuple<ushort, ushort>, long> m_HouseUpdateArray = new();
 
 		// Trainer window Cache, (Object Type, Object ID) => Skill
 		public List<Tuple<Specialization, List<Tuple<int, int, Skill>>>> TrainerSkillCache = null;
 		
 		// Tooltip Request Time Cache, (Object Type => (Object ID => expires))
-		private ConcurrentDictionary<int, ConcurrentDictionary<int, long>> m_tooltipRequestTimes = new ConcurrentDictionary<int, ConcurrentDictionary<int, long>>();
+		private ConcurrentDictionary<int, ConcurrentDictionary<int, long>> m_tooltipRequestTimes = new();
 		
 		/// <summary>
 		/// Try to Send Tooltip to Client, return false if cache hit.
@@ -273,38 +273,26 @@ namespace DOL.GS
 		/// <returns></returns>
 		public bool CanSendTooltip(int type, int id)
 		{
-	        m_tooltipRequestTimes.TryAdd(type, new ConcurrentDictionary<int, long>());
+			m_tooltipRequestTimes.TryAdd(type, new());
 
 			// Queries cleanup
 			foreach (Tuple<int, int> keys in m_tooltipRequestTimes.SelectMany(e => e.Value.Where(it => it.Value < GameLoop.GetCurrentTime()).Select(el => new Tuple<int, int>(e.Key, el.Key))))
-			{
-				long dummy;
-				m_tooltipRequestTimes[keys.Item1].TryRemove(keys.Item2, out dummy);
-			}
+				m_tooltipRequestTimes[keys.Item1].TryRemove(keys.Item2, out _);
 			
 			// Query hit ?
 			if (m_tooltipRequestTimes[type].ContainsKey(id))
 				return false;
 		
 			// Query register
-	        m_tooltipRequestTimes[type].TryAdd(id, GameLoop.GetCurrentTime()+3600000);
-	        return true;
+			m_tooltipRequestTimes[type].TryAdd(id, GameLoop.GetCurrentTime()+3600000);
+			return true;
 		}
 
-		
 		/// <summary>
 		/// Constructor for a game client
 		/// </summary>
 		/// <param name="srvr">The server that's communicating with this client</param>
-		public GameClient(BaseServer srvr)
-			: base(srvr)
-		{
-			m_clientVersion = eClientVersion.VersionNotChecked;
-			m_player = null;
-			m_activeCharIndex = -1; //No character loaded yet!
-			m_GameObjectUpdateArray = new ReaderWriterDictionary<Tuple<ushort, ushort>, long>();
-			m_HouseUpdateArray = new ReaderWriterDictionary<Tuple<ushort, ushort>, long>();
-		}
+		public GameClient(BaseServer srvr) : base(srvr) { }
 
 		/// <summary>
 		/// UDP address for this client
@@ -338,15 +326,15 @@ namespace DOL.GS
 			}
 		}
 
-        /// <summary>
-        /// When the linkdeath occured. 0 if there wasn't any
-        /// </summary>
+		/// <summary>
+		/// When the linkdeath occured. 0 if there wasn't any
+		/// </summary>
 		public long LinkDeathTime { get; set; }
 
-        /// <summary>
-        /// Variable is false if account/player is Ban, for a wrong password, if server is closed etc ... 
-        /// </summary>
-        public bool IsConnected = true;
+		/// <summary>
+		/// Variable is false if account/player is Ban, for a wrong password, if server is closed etc ... 
+		/// </summary>
+		public bool IsConnected = true;
 
 		/// <summary>
 		/// Gets whether or not the client is playing
@@ -512,7 +500,7 @@ namespace DOL.GS
 		/// <summary>
 		/// Get the Game Object Update Array (Read/Write)
 		/// </summary>
-		public ReaderWriterDictionary<Tuple<ushort, ushort>, long> GameObjectUpdateArray
+		public ConcurrentDictionary<Tuple<ushort, ushort>, long> GameObjectUpdateArray
 		{
 			get { return m_GameObjectUpdateArray; }
 		}
@@ -520,7 +508,7 @@ namespace DOL.GS
 		/// <summary>
 		/// Get the House Update Array (Read/Write)
 		/// </summary>
-		public ReaderWriterDictionary<Tuple<ushort, ushort>, long> HouseUpdateArray
+		public ConcurrentDictionary<Tuple<ushort, ushort>, long> HouseUpdateArray
 		{
 			get { return m_HouseUpdateArray; }
 		}
@@ -798,7 +786,6 @@ namespace DOL.GS
 				curPlayer.OnLinkdeath();
 			}
 		}
-
 
 		/// <summary>
 		/// Quits a client from the world

GameServer/gameobjects/GamePlayer.cs
@@ -15205,11 +15205,6 @@ namespace DOL.GS
             m_client = client;
             m_dbCharacter = dbChar;
             m_controlledHorse = new ControlledHorse(this);
-            m_buff1Bonus = new PropertyIndexer((int)eProperty.MaxProperty); // set up a fixed indexer for players
-            m_buff2Bonus = new PropertyIndexer((int)eProperty.MaxProperty);
-            m_debuffBonus = new PropertyIndexer((int)eProperty.MaxProperty);
-            m_buff4Bonus = new PropertyIndexer((int)eProperty.MaxProperty);
-            m_itemBonus = new PropertyIndexer((int)eProperty.MaxProperty);
             m_lastUniqueLocations = new GameLocation[4];
             m_canFly = false;
 

GameServer/gameutils/GroupMgr.cs
@@ -16,7 +16,8 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
-using System;
+
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -28,14 +29,8 @@ namespace DOL.GS
 	/// </summary>
 	public static class GroupMgr
 	{
-		/// <summary>
-		/// Dictionary of all groups in the game
-		/// </summary>
-		static readonly ReaderWriterDictionary<Group, bool> m_groups = new ReaderWriterDictionary<Group, bool>();
-		/// <summary>
-		/// ArrayList of all players looking for a group
-		/// </summary>
-		static readonly ReaderWriterDictionary<GamePlayer, bool> m_lfgPlayers = new ReaderWriterDictionary<GamePlayer, bool>();
+		static readonly ConcurrentDictionary<Group, bool> m_groups = new();
+		static readonly ConcurrentDictionary<GamePlayer, bool> m_lfgPlayers = new();
 
 		/// <summary>
 		/// Adds a group to the list of groups
@@ -44,7 +39,7 @@ namespace DOL.GS
 		/// <returns>True if the function succeeded, otherwise false</returns>
 		public static bool AddGroup(Group group)
 		{
-			return m_groups.AddIfNotExists(group, true);
+			return m_groups.TryAdd(group, true);
 		}
 
 		/// <summary>
@@ -54,8 +49,7 @@ namespace DOL.GS
 		/// <returns></returns>
 		public static bool RemoveGroup(Group group)
 		{
-			bool dummy;
-			return m_groups.TryRemove(group, out dummy);
+			return m_groups.TryRemove(group, out _);
 		}
 
 		/// <summary>
@@ -64,10 +58,8 @@ namespace DOL.GS
 		/// <param name="member">player to add to the list</param>
 		public static void SetPlayerLooking(GamePlayer member)
 		{
-			if (member.LookingForGroup == false && m_lfgPlayers.AddIfNotExists(member, true))
-			{
+			if (member.LookingForGroup == false && m_lfgPlayers.TryAdd(member, true))
 				member.LookingForGroup = true;
-			}
 		}
 
 		/// <summary>

GameServer/gameutils/PropertyCollection.cs
@@ -16,10 +16,11 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
+
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-
 using log4net;
 
 namespace DOL.GS
@@ -37,7 +38,7 @@ namespace DOL.GS
 		/// <summary>
 		/// Container of properties
 		/// </summary>
-		private readonly ReaderWriterDictionary<object, object> _props = new ReaderWriterDictionary<object, object>();
+		private readonly ConcurrentDictionary<object, object> _props = new();
 
 		/// <summary>
 		/// Retrieve a property

commit f635dcd71abfe60b10b976399d530423707180b8

Fix instance creation (caused by d1ba7e9)

GameServer/world/WorldMgr.cs
@@ -1713,7 +1713,7 @@ namespace DOL.GS
 			{
 				for (ID = DEFAULT_VALUE_FOR_INSTANCE_ID_SEARCH_START; ID <= ushort.MaxValue; ID++)
 				{
-					if (m_regions.TryAdd(ID, instance))
+					if (!m_regions.ContainsKey(ID))
 					{
 						success = true;
 						break;
@@ -1730,13 +1730,13 @@ namespace DOL.GS
 			try
 			{
 				instance = (BaseInstance) info.Invoke(new object[] { ID, data });
+				m_regions[ID] = instance;
 			}
 			catch (Exception e)
 			{
 				log.ErrorFormat("Error on instance creation - {0} {1}", e.Message, e.StackTrace);
 				return null;
 			}
-
 
 			List<ZoneData> list = null;
 
@@ -1753,7 +1753,7 @@ namespace DOL.GS
 
 			foreach (ZoneData dat in list)
 			{
-				for (; zoneID <= ushort.MaxValue; ID++)
+				for (; zoneID <= ushort.MaxValue; zoneID++)
 				{
 					if (m_zones.TryAdd(zoneID, null))
 					{