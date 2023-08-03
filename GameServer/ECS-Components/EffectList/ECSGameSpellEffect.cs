OpenDAoC/OpenDAoC-Core

commit c7a627c6c8b1bc0899d3b6ff1986b964b839c4ad

Fix for ImmunityEffects not mapped correctly

GameServer/ECS-Components/EffectList/ECSImmunityEffect.cs
@@ -27,6 +27,11 @@ namespace DOL.GS
             EffectService.RequestStartEffect(this);
         }
 
+        protected ECSImmunityEffect(ECSGameEffectInitParams initParams) : base(initParams)
+        {
+            
+        }
+
         protected eEffect MapImmunityEffect()
         {
             switch (SpellHandler.Spell.SpellType)
@@ -47,10 +52,10 @@ namespace DOL.GS
             }
         }
     }
-    public class NPCECSStunImmunityEffect : ECSGameEffect
+    public class NPCECSStunImmunityEffect : ECSImmunityEffect
     {
         private int timesStunned = 1;
-        public NPCECSStunImmunityEffect(ECSGameEffectInitParams initParams) : base()
+        public NPCECSStunImmunityEffect(ECSGameEffectInitParams initParams) : base(initParams)
         {
             Owner = initParams.Target;
             Duration = 60000;
@@ -66,10 +71,10 @@ namespace DOL.GS
         }
     }
 
-    public class NPCECSMezImmunityEffect : ECSGameEffect
+    public class NPCECSMezImmunityEffect : ECSImmunityEffect
     {
-        private int timesStunned = 1;
-        public NPCECSMezImmunityEffect(ECSGameEffectInitParams initParams) : base()
+        private int timesMezzed = 1;
+        public NPCECSMezImmunityEffect(ECSGameEffectInitParams initParams) : base(initParams)
         {
             Owner = initParams.Target;
             Duration = 60000;
@@ -79,8 +84,8 @@ namespace DOL.GS
 
         public long CalclulateStunDuration(long duration)
         {
-            var retVal = duration / (2 * timesStunned);
-            timesStunned++;
+            var retVal = duration / (2 * timesMezzed);
+            timesMezzed++;
             return retVal;
         }
     }

commit 4386b8b133d0d68852f017b8739084d200057315

`ECSGameSpellEffect`: Fix exception (caused by c7a627c) Having `NPCECSStunImmunityEffect` and `NPCECSMezImmunityEffect` inherit `ECSImmunityEffect` (which inherits `ECSGameSpellEffect`) instead of `ECSGameEffect` means the spell handler has to be passed too.

GameServer/ECS-Components/EffectList/ECSGameSpellEffect.cs
@@ -1,13 +1,10 @@
-using System;
-using DOL.AI.Brain;
 using DOL.Database;
 using DOL.GS.Effects;
-using DOL.GS.Spells;
 using DOL.GS.PacketHandler;
+using DOL.GS.Spells;
 
 namespace DOL.GS
 {
-    
     /// <summary>
     /// Spell-Based Effect
     /// </summary>
@@ -30,12 +27,12 @@ namespace DOL.GS
             PulseFreq = spell.Frequency;
             Caster = SpellHandler.Caster;
 
-            if (spell.SpellType is (eSpellType.SpeedDecrease) or (eSpellType.UnbreakableSpeedDecrease))
+            if (spell.SpellType is eSpellType.SpeedDecrease or eSpellType.UnbreakableSpeedDecrease)
             {
                 TickInterval = 650;
                 NextTick = 1 + (Duration >> 1) + (int)StartTick;
                 if (!spell.Name.Equals("Prevent Flight") && !spell.IsFocus)
-					TriggersImmunity = true;
+                    TriggersImmunity = true;
             }
             else if (spell.IsConcentration)
             {
@@ -45,6 +42,7 @@ namespace DOL.GS
                 PulseFreq = 650;
             }
 
+            // These classes start their effects themselves.
             if (this is not ECSImmunityEffect and not ECSPulseEffect)
                 EffectService.RequestStartEffect(this);
         }
@@ -79,118 +77,116 @@ namespace DOL.GS
                 {
                     if (EffectType == eEffect.Stun)
                     {
-                        NPCECSStunImmunityEffect npcImmune = (NPCECSStunImmunityEffect)EffectListService.GetEffectOnTarget(Owner, eEffect.NPCStunImmunity);
-                        if (npcImmune is null)
-                            new NPCECSStunImmunityEffect(new ECSGameEffectInitParams(Owner, ImmunityDuration, Effectiveness));
+                        if (EffectListService.GetEffectOnTarget(Owner, eEffect.NPCStunImmunity) is not NPCECSStunImmunityEffect)
+                            new NPCECSStunImmunityEffect(new ECSGameEffectInitParams(Owner, ImmunityDuration, Effectiveness, SpellHandler));
                     }
                     else if (EffectType == eEffect.Mez)
                     {
-                        NPCECSMezImmunityEffect npcImmune = (NPCECSMezImmunityEffect)EffectListService.GetEffectOnTarget(Owner, eEffect.NPCMezImmunity);
-                        if (npcImmune is null)
-                            new NPCECSMezImmunityEffect(new ECSGameEffectInitParams(Owner, ImmunityDuration, Effectiveness));
+                        if (EffectListService.GetEffectOnTarget(Owner, eEffect.NPCMezImmunity) is not NPCECSMezImmunityEffect)
+                            new NPCECSMezImmunityEffect(new ECSGameEffectInitParams(Owner, ImmunityDuration, Effectiveness, SpellHandler));
                     }
                 }
             }
         }
-        
+
         /// <summary>
         /// Used for 'OnEffectStartMsg' and 'OnEffectExpiresMsg'. Identifies the entity triggering the effect (sometimes the caster and effect owner are the same entity).
         /// </summary>
         public GameLiving Caster { get; }
-        
+
         #region Effect Start Messages
         /// <summary>
-		/// Sends Spell messages to all nearby/associated players when an ability/spell/style effect becomes active on a target.
-		/// </summary>
-		/// <param name="target">The owner of the effect.</param>
+        /// Sends Spell messages to all nearby/associated players when an ability/spell/style effect becomes active on a target.
+        /// </summary>
+        /// <param name="target">The owner of the effect.</param>
         /// <param name="msgTarget">If 'true', the system sends a first-person spell message to the target/owner of the effect.</param>
-		/// <param name="msgSelf">If 'true', the system sends a third-person spell message to the caster triggering the effect, regardless of their proximity to the target.</param>
-		/// <param name="msgArea">If 'true', the system sends a third-person message to all players within range of the target.</param>
-		/// <returns>'Message1' and 'Message2' values from the 'spell' table.</returns>
-		public void OnEffectStartsMsg(GameLiving target, bool msgTarget, bool msgSelf, bool msgArea)
-		{
-			// If the target variable is at the start of the string, capitalize their name or article
-			var upperCase = SpellHandler.Spell.Message2.StartsWith("{0}");
+        /// <param name="msgSelf">If 'true', the system sends a third-person spell message to the caster triggering the effect, regardless of their proximity to the target.</param>
+        /// <param name="msgArea">If 'true', the system sends a third-person message to all players within range of the target.</param>
+        /// <returns>'Message1' and 'Message2' values from the 'spell' table.</returns>
+        public void OnEffectStartsMsg(GameLiving target, bool msgTarget, bool msgSelf, bool msgArea)
+        {
+            // If the target variable is at the start of the string, capitalize their name or article
+            var upperCase = SpellHandler.Spell.Message2.StartsWith("{0}");
 
-			// Sends a first-person message directly to the caster's target, if they are a player
-			if (msgTarget && target is GamePlayer playerTarget)
-				// "You feel more dexterous!"
-				((SpellHandler)SpellHandler).MessageToLiving(playerTarget, SpellHandler.Spell.Message1, eChatType.CT_Spell);
+            // Sends a first-person message directly to the caster's target, if they are a player
+            if (msgTarget && target is GamePlayer playerTarget)
+                // "You feel more dexterous!"
+                ((SpellHandler)SpellHandler).MessageToLiving(playerTarget, SpellHandler.Spell.Message1, eChatType.CT_Spell);
 
-			// Sends a third-person message to all players surrounding the target
-			if (msgArea)
-			{
-				if (Caster is GamePlayer caster && caster == target)
-					// "{0} looks more agile!"
-					Message.SystemToArea(target, Util.MakeSentence(SpellHandler.Spell.Message2, target.GetName(0, upperCase)), eChatType.CT_Spell, target, Caster);
-				else if (Caster is GameSummonedPet || target is GameSummonedPet or GamePlayer)
-					// "{0} looks more agile!"
-					Message.SystemToArea(target, Util.MakeSentence(SpellHandler.Spell.Message2, target.GetName(0, upperCase)), eChatType.CT_Spell, target);
-			}
+            // Sends a third-person message to all players surrounding the target
+            if (msgArea)
+            {
+                if (Caster is GamePlayer caster && caster == target)
+                    // "{0} looks more agile!"
+                    Message.SystemToArea(target, Util.MakeSentence(SpellHandler.Spell.Message2, target.GetName(0, upperCase)), eChatType.CT_Spell, target, Caster);
+                else if (Caster is GameSummonedPet || target is GameSummonedPet or GamePlayer)
+                    // "{0} looks more agile!"
+                    Message.SystemToArea(target, Util.MakeSentence(SpellHandler.Spell.Message2, target.GetName(0, upperCase)), eChatType.CT_Spell, target);
+            }
             // Sends a third-person message directly to the caster to indicate the spell has ended
             else if (msgSelf && Caster != target && Caster is GamePlayer)
                 // "{0} looks more agile!"
-				((SpellHandler)SpellHandler).MessageToCaster(Util.MakeSentence(SpellHandler.Spell.Message2, target.GetName(0, true)), eChatType.CT_Spell);
-	    }
+                ((SpellHandler)SpellHandler).MessageToCaster(Util.MakeSentence(SpellHandler.Spell.Message2, target.GetName(0, true)), eChatType.CT_Spell);
+        }
         #endregion Effect Start Messages
-        
+
         #region Effect End Messages
-		/// <summary>
-		/// Sends Spell messages to all nearby/associated players when an ability/spell/style effect ends on a target.
-		/// </summary>
-		/// <param name="target">The owner of the effect.</param>
-		/// <param name="msgTarget">If 'true', the system sends a first-person spell message to the target/owner of the effect.</param>
-		/// <param name="msgSelf">If 'true', the system sends a third-person spell message to the caster triggering the effect, regardless of their proximity to the target.</param>
-		/// <param name="msgArea">If 'true', the system sends a third-person message to all players within range of the target.</param>
-		/// <returns>'Message3' and 'Message4' values from the 'spell' table.</returns>
-		public void OnEffectExpiresMsg(GameLiving target, bool msgTarget, bool msgSelf, bool msgArea)
-		{
-			// If the target variable is at the start of the string, capitalize their name or article
-			var upperCase = SpellHandler.Spell.Message4.StartsWith("{0}");
-			
-			// Sends no messages
-			if (msgTarget == false && msgSelf == false && msgArea == false) return;
+        /// <summary>
+        /// Sends Spell messages to all nearby/associated players when an ability/spell/style effect ends on a target.
+        /// </summary>
+        /// <param name="target">The owner of the effect.</param>
+        /// <param name="msgTarget">If 'true', the system sends a first-person spell message to the target/owner of the effect.</param>
+        /// <param name="msgSelf">If 'true', the system sends a third-person spell message to the caster triggering the effect, regardless of their proximity to the target.</param>
+        /// <param name="msgArea">If 'true', the system sends a third-person message to all players within range of the target.</param>
+        /// <returns>'Message3' and 'Message4' values from the 'spell' table.</returns>
+        public void OnEffectExpiresMsg(GameLiving target, bool msgTarget, bool msgSelf, bool msgArea)
+        {
+            // If the target variable is at the start of the string, capitalize their name or article
+            var upperCase = SpellHandler.Spell.Message4.StartsWith("{0}");
 
-			// Sends a first-person message directly to the caster's target, if they are a player
-			if (msgTarget && target is GamePlayer playerTarget)
-				// "Your agility returns to normal."
-				((SpellHandler)SpellHandler).MessageToLiving(playerTarget, SpellHandler.Spell.Message3, eChatType.CT_Spell);
+            // Sends no messages
+            if (msgTarget == false && msgSelf == false && msgArea == false) return;
 
-			// Sends a third-person message directly to the caster to indicate the spell has ended
-			if (msgSelf && Caster is GamePlayer selfCaster)
-				// "{0}'s enhanced agility fades."
-				((SpellHandler)SpellHandler).MessageToCaster(Util.MakeSentence(SpellHandler.Spell.Message4, target.GetName(0, true)), eChatType.CT_Spell);
+            // Sends a first-person message directly to the caster's target, if they are a player
+            if (msgTarget && target is GamePlayer playerTarget)
+                // "Your agility returns to normal."
+                ((SpellHandler)SpellHandler).MessageToLiving(playerTarget, SpellHandler.Spell.Message3, eChatType.CT_Spell);
 
-			// Sends a third-person message to all players surrounding the target
-			if (msgArea)
-			{
-				if (Caster is GamePlayer areaTarget && areaTarget == target)
-					// "{0}'s enhanced agility fades."
-					Message.SystemToArea(target, Util.MakeSentence(SpellHandler.Spell.Message4, target.GetName(0, upperCase)), eChatType.CT_Spell, target, Caster);
-				else if (Caster is GameSummonedPet || target is GameSummonedPet or GamePlayer)
-					// "{0}'s enhanced agility fades."
-					Message.SystemToArea(target, Util.MakeSentence(SpellHandler.Spell.Message4, target.GetName(0, upperCase)), eChatType.CT_Spell, target);
-			}
-		}
-		#endregion Effect End Messages
+            // Sends a third-person message directly to the caster to indicate the spell has ended
+            if (msgSelf && Caster is GamePlayer selfCaster)
+                // "{0}'s enhanced agility fades."
+                ((SpellHandler)SpellHandler).MessageToCaster(Util.MakeSentence(SpellHandler.Spell.Message4, target.GetName(0, true)), eChatType.CT_Spell);
 
-		public override PlayerXEffect getSavedEffect()
-		{
-			if (SpellHandler == null || SpellHandler.Spell == null) return null;
-			
-			PlayerXEffect eff = new PlayerXEffect();
-			eff.Var1 = SpellHandler.Spell.ID;
-			eff.Var2 = Effectiveness;
-			eff.Var3 = (int)SpellHandler.Spell.Value;
-			
-			if (Duration > 0)
-				eff.Duration = (int)(ExpireTick - GameLoop.GameLoopTime);
-			else
-				eff.Duration = 30 * 60 * 1000;
-			
-			eff.IsHandler = true;
-			eff.SpellLine = SpellHandler.SpellLine.KeyName;
-			return eff;
-		}
+            // Sends a third-person message to all players surrounding the target
+            if (msgArea)
+            {
+                if (Caster is GamePlayer areaTarget && areaTarget == target)
+                    // "{0}'s enhanced agility fades."
+                    Message.SystemToArea(target, Util.MakeSentence(SpellHandler.Spell.Message4, target.GetName(0, upperCase)), eChatType.CT_Spell, target, Caster);
+                else if (Caster is GameSummonedPet || target is GameSummonedPet or GamePlayer)
+                    // "{0}'s enhanced agility fades."
+                    Message.SystemToArea(target, Util.MakeSentence(SpellHandler.Spell.Message4, target.GetName(0, upperCase)), eChatType.CT_Spell, target);
+            }
+        }
+        #endregion Effect End Messages
+
+        public override PlayerXEffect getSavedEffect()
+        {
+            if (SpellHandler == null || SpellHandler.Spell == null) return null;
+
+            PlayerXEffect eff = new PlayerXEffect();
+            eff.Var1 = SpellHandler.Spell.ID;
+            eff.Var2 = Effectiveness;
+            eff.Var3 = (int)SpellHandler.Spell.Value;
+
+            if (Duration > 0)
+                eff.Duration = (int)(ExpireTick - GameLoop.GameLoopTime);
+            else
+                eff.Duration = 30 * 60 * 1000;
+
+            eff.IsHandler = true;
+            eff.SpellLine = SpellHandler.SpellLine.KeyName;
+            return eff;
+        }
     }
 }

GameServer/ECS-Components/EffectList/ECSImmunityEffect.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using DOL.GS.Spells;
+﻿using DOL.GS.Spells;
 
 namespace DOL.GS
 {
     public class ECSImmunityEffect : ECSGameSpellEffect
     {
-        public override ushort Icon { get { return SpellHandler.Spell.Icon; } }
-        public override string Name { get { return SpellHandler.Spell.Name; } }
-
         public ECSImmunityEffect(GameLiving owner, ISpellHandler handler, int duration, int pulseFreq, double effectiveness, ushort icon, bool cancelEffect = false)
             : base(new ECSGameEffectInitParams(owner, duration, effectiveness, handler))
         {
@@ -27,10 +23,7 @@ namespace DOL.GS
             EffectService.RequestStartEffect(this);
         }
 
-        protected ECSImmunityEffect(ECSGameEffectInitParams initParams) : base(initParams)
-        {
-            
-        }
+        protected ECSImmunityEffect(ECSGameEffectInitParams initParams) : base(initParams) { }
 
         protected eEffect MapImmunityEffect()
         {
@@ -52,9 +45,11 @@ namespace DOL.GS
             }
         }
     }
+
     public class NPCECSStunImmunityEffect : ECSImmunityEffect
     {
-        private int timesStunned = 1;
+        private int _timesStunned = 1;
+
         public NPCECSStunImmunityEffect(ECSGameEffectInitParams initParams) : base(initParams)
         {
             Owner = initParams.Target;
@@ -63,17 +58,18 @@ namespace DOL.GS
             EffectService.RequestStartEffect(this);
         }
 
-        public long CalclulateStunDuration(long duration)
+        public long CalculateStunDuration(long duration)
         {
-            var retVal = duration / (2 * timesStunned);
-            timesStunned++;
+            var retVal = duration / (2 * _timesStunned);
+            _timesStunned++;
             return retVal;
         }
     }
 
     public class NPCECSMezImmunityEffect : ECSImmunityEffect
     {
-        private int timesMezzed = 1;
+        private int _timesMezzed = 1;
+
         public NPCECSMezImmunityEffect(ECSGameEffectInitParams initParams) : base(initParams)
         {
             Owner = initParams.Target;
@@ -82,10 +78,10 @@ namespace DOL.GS
             EffectService.RequestStartEffect(this);
         }
 
-        public long CalclulateStunDuration(long duration)
+        public long CalculateMezDuration(long duration)
         {
-            var retVal = duration / (2 * timesMezzed);
-            timesMezzed++;
+            var retVal = duration / (2 * _timesMezzed);
+            _timesMezzed++;
             return retVal;
         }
     }

GameServer/ECS-Components/EffectList/ECSPulseEffect.cs
@@ -1,21 +1,14 @@
-﻿using DOL.GS.Spells;
-using DOL.GS.Effects;
+﻿using DOL.GS.Effects;
+using DOL.GS.Spells;
 
 namespace DOL.GS
 {
     public class ECSPulseEffect : ECSGameSpellEffect, IConcentrationEffect
     {
-        string IConcentrationEffect.Name => Name;
-        ushort IConcentrationEffect.Icon => Icon;
-        byte IConcentrationEffect.Concentration => SpellHandler.Spell.Concentration;
-
         /// <summary>
         /// The name of the owner
         /// </summary>
-        public override string OwnerName
-        {
-            get { return "Pulse: " + SpellHandler.Spell.Name; }
-        }
+        public override string OwnerName => $"Pulse: {SpellHandler.Spell.Name}";
 
         public ECSPulseEffect(GameLiving owner, ISpellHandler handler, int duration, int pulseFreq, double effectiveness, ushort icon, bool cancelEffect = false)
             : base (new ECSGameEffectInitParams(owner, duration, effectiveness, handler))

GameServer/spells/CCSpellHandler.cs
@@ -335,7 +335,7 @@ namespace DOL.GS.Spells
             NPCECSMezImmunityEffect npcImmune = (NPCECSMezImmunityEffect)EffectListService.GetEffectOnTarget(target, eEffect.NPCMezImmunity);
 
             if (npcImmune != null)
-                duration = npcImmune.CalclulateStunDuration((long)duration);
+                duration = npcImmune.CalculateMezDuration((long)duration);
 
             if (duration < 1)
                 duration = 1;
@@ -451,7 +451,7 @@ namespace DOL.GS.Spells
             NPCECSStunImmunityEffect npcImmune = (NPCECSStunImmunityEffect)EffectListService.GetEffectOnTarget(target, eEffect.NPCStunImmunity);
 
             if (npcImmune != null)
-                duration = npcImmune.CalclulateStunDuration((long)duration); //target.GetModified(eProperty.StunDurationReduction) * 0.01;
+                duration = npcImmune.CalculateStunDuration((long)duration); //target.GetModified(eProperty.StunDurationReduction) * 0.01;
 
             if (duration < 1)
                 duration = 1;

GameServer/spells/StyleStun.cs
@@ -49,7 +49,7 @@ namespace DOL.GS.Spells
 			NPCECSStunImmunityEffect npcImmune = (NPCECSStunImmunityEffect)EffectListService.GetEffectOnTarget(target, eEffect.NPCStunImmunity);
 			if (npcImmune != null)
 			{
-				int duration = (int)npcImmune.CalclulateStunDuration(Spell.Duration);
+				int duration = (int)npcImmune.CalculateStunDuration(Spell.Duration);
 				return  duration > 1 ? duration : 1;
 			}
 			else