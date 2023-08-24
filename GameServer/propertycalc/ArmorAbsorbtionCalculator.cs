32a49f401e2385be5a59217e23cc0a58f8dcb5da
Baptiste Marie <bm01@users.noreply.github.com>
2023-08-24 22:10:32 +0200

    `ArmorAbsorptionCalculator`: Remove ABS bonus based on buff amount It can make pets overly tanky since it takes all kind of buffs (even pet procs) into account.

--- GameServer/propertycalc/ArmorAbsorbtionCalculator.cs
+++ GameServer/propertycalc/ArmorAbsorbtionCalculator.cs
@@ -16,26 +16,22 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
+
 using System;
-using System.Linq;
 
 namespace DOL.GS.PropertyCalc
 {
-	[PropertyCalculator(eProperty.ArmorAbsorption)]
-	public class ArmorAbsorptionCalculator : PropertyCalculator
-	{
-		public override int CalcValue(GameLiving living, eProperty property)
-		{
-			int buffBonus = living.BaseBuffBonusCategory[property];
-			int debuffMalus = Math.Abs(living.DebuffCategory[property]);
-			int itemBonus = living.ItemBonus[property];
-			int abilityBonus = living.AbilityBonus[property];
-			int hardCap = 50;
-			if (living is GameSummonedPet)
-			{
-				buffBonus += living.effectListComponent.GetAllEffects().Count(e => e is ECSGameSpellEffect spellEffect && spellEffect.SpellHandler.Spell.IsBuff) * 4;
-			}
-			return Math.Min(hardCap, (buffBonus - debuffMalus + itemBonus + abilityBonus));
-		}
-	}
+    [PropertyCalculator(eProperty.ArmorAbsorption)]
+    public class ArmorAbsorptionCalculator : PropertyCalculator
+    {
+        public override int CalcValue(GameLiving living, eProperty property)
+        {
+            int buffBonus = living.BaseBuffBonusCategory[property];
+            int debuffMalus = Math.Abs(living.DebuffCategory[property]);
+            int itemBonus = living.ItemBonus[property];
+            int abilityBonus = living.AbilityBonus[property];
+            int hardCap = 50;
+            return Math.Min(hardCap, buffBonus - debuffMalus + itemBonus + abilityBonus);
+        }
+    }
 }
