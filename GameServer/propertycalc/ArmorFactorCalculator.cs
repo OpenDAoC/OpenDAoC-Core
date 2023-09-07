e3196d5660c626c9e2745273fe3a17a3bb6f6ad7
Baptiste Marie <bm01@users.noreply.github.com>
2023-09-07 15:19:09 +0200

    `ArmorFactorCalculator`: Fix various issues * Ensure at least 1 AF is always returned (fixes exception when a NPC attacks a level 0 NPC). * Fix spec AF buffs on `GameEpicNPC` and `GameEpicBoss` being 6 times stronger than intended (tweaked formulas so that only one division is performed at the end) * Fix `GuardLord` not receiving their bonus due to integer division (now uses it's own divisor, may require tweaking). * Remove Necromancer flat AF bonus to be based on its level instead (now +10 only at level 44) * Add an early out to the attackers iteration if epicScaleFactor drops below 4 (may improve performance when hundreds of players are attacking, TODO: find a way to remove `ToList` call).

--- GameServer/propertycalc/ArmorFactorCalculator.cs
+++ GameServer/propertycalc/ArmorFactorCalculator.cs
@@ -16,128 +16,128 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
+
 using System;
 using System.Linq;
 using DOL.GS.Keeps;
-using log4net.Core;
 
 namespace DOL.GS.PropertyCalc
 {
-	/// <summary>
-	/// The Armor Factor calculator
-	///
-	/// BuffBonusCategory1 is used for base buffs directly in player.GetArmorAF because it must be capped by item AF cap
-	/// BuffBonusCategory2 is used for spec buffs, level*1.875 cap for players
-	/// BuffBonusCategory3 is used for debuff, uncapped
-	/// BuffBonusCategory4 is used for buffs, uncapped
-	/// BuffBonusMultCategory1 unused
-	/// ItemBonus is used for players TOA bonuse, living.Level cap
-	/// </summary>
-	[PropertyCalculator(eProperty.ArmorFactor)]
-	public class ArmorFactorCalculator : PropertyCalculator
-	{
-		public override int CalcValue(GameLiving living, eProperty property)
-		{
-			if (living is GamePlayer || living is GameTrainingDummy)
-			{
-				int af;
-
-				//base AF buffs are calculated in the item's armor calc since they have the same cap
-				//af = living.BaseBuffBonusCategory[(int) property];
-				// 1.5*1.25 spec line buff cap
-				af = Math.Min((int)(living.Level * 1.875), living.SpecBuffBonusCategory[(int)property]);
-				// debuff
-				af -= Math.Abs(living.DebuffCategory[(int)property]);
-				// TrialsOfAtlantis af bonus
-				af += Math.Min(living.Level, living.ItemBonus[(int)property]);
-				// uncapped category
-				af += living.BuffBonusCategory4[(int)property];
-				
-				// buffs should be spread across each armor piece since the damage calculation is based on piece hit
-				if(living is GamePlayer)
-					af /= 6;
-
-				return af;
-			}
-			else if (living is GameKeepDoor || living is GameKeepComponent)
-			{
-				GameKeepComponent component = null;
-				if (living is GameKeepDoor)
-					component = (living as GameKeepDoor).Component;
-				if (living is GameKeepComponent)
-					component = living as GameKeepComponent;
-
-				if (component == null) return 1;
-				
-				double keepLevelMod = 1 + component.Keep.Level * .1;
-
-				int amount = (int)(component.Keep.BaseLevel * 4 * keepLevelMod);
-				if (component.Keep is GameKeep)
-					return amount;
-				else return amount / 2;
-			}
-			else if (living is GameEpicNPC epic || living is GameEpicBoss)
-			{
-				GameLiving bossnpc = living;
-				double epicScaleFactor = 8;
-				int petCap = 16;
-				int petCount = 0;
-
-				if(bossnpc is GameEpicBoss)
+    /// <summary>
+    /// The Armor Factor calculator
+    ///
+    /// BuffBonusCategory1 is used for base buffs directly in player.GetArmorAF because it must be capped by item AF cap
+    /// BuffBonusCategory2 is used for spec buffs, level*1.875 cap for players
+    /// BuffBonusCategory3 is used for debuff, uncapped
+    /// BuffBonusCategory4 is used for buffs, uncapped
+    /// BuffBonusMultCategory1 unused
+    /// ItemBonus is used for players TOA bonuse, living.Level cap
+    /// </summary>
+    [PropertyCalculator(eProperty.ArmorFactor)]
+    public class ArmorFactorCalculator : PropertyCalculator
+    {
+        public override int CalcValue(GameLiving living, eProperty property)
+        {
+            switch (living)
+            {
+                case GamePlayer:
+                case GameTrainingDummy:
+                    return CalculatePlayerArmorFactor(living, property);
+                case GameKeepDoor:
+                case GameKeepComponent:
+                    return CalculateKeepComponentArmorFactor(living);
+                case GameEpicNPC:
+                case GameEpicBoss:
                 {
-					epicScaleFactor *= 2;
-					petCap = 24;
+                    double epicScaleFactor = CalculateEpicScaleFactor(living);
+                    return CalculateLivingArmorFactor(living, property, 12.0 * epicScaleFactor, 50.0, false);
+                }
+                case GameSummonedPet:
+                    return CalculateLivingArmorFactor(living, property, 12.0, living is NecromancerPet ? 121.0 : 175.0, true);
+                default:
+                    return CalculateLivingArmorFactor(living, property, 12.0, living is GuardLord ? 134.0 : 200.0, false);
+            }
+        }
+
+        private static int CalculatePlayerArmorFactor(GameLiving living, eProperty property)
+        {
+            // Base AF buffs are calculated in the item's armor calc since they have the same cap.
+            int armorFactor = Math.Min((int) (living.Level * 1.875), living.SpecBuffBonusCategory[(int) property]);
+            armorFactor -= Math.Abs(living.DebuffCategory[(int) property]);
+            armorFactor += Math.Min(living.Level, living.ItemBonus[(int) property]);
+            armorFactor += living.BuffBonusCategory4[(int) property];
+            armorFactor /= 6;
+            return Math.Max(1, armorFactor);
+        }
+
+        private static int CalculateLivingArmorFactor(GameLiving living, eProperty property, double factor, double divisor, bool useBaseBuff)
+        {
+            int armorFactor = (int) ((1 + living.Level / divisor) * (living.Level * factor));
+
+            if (useBaseBuff)
+                armorFactor += living.BaseBuffBonusCategory[(int) property];
+
+            armorFactor += living.SpecBuffBonusCategory[(int) property];
+            armorFactor -= Math.Abs(living.DebuffCategory[(int) property]);
+            armorFactor += living.BuffBonusCategory4[(int) property];
+            armorFactor /= 6;
+            return Math.Max(1, armorFactor);
+        }
+
+        private static int CalculateKeepComponentArmorFactor(GameLiving living)
+        {
+            GameKeepComponent component = null;
+
+            if (living is GameKeepDoor keepDoor)
+                component = keepDoor.Component;
+            else if (living is GameKeepComponent)
+                component = living as GameKeepComponent;
+
+            if (component == null)
+                return 1;
+
+            double keepLevelMod = 1 + component.Keep.Level * 0.1;
+            int typeMod = component.Keep is GameKeep ? 4 : 2;
+            return Math.Max(1, (int) (component.Keep.BaseLevel * keepLevelMod * typeMod));
+        }
+
+        private static double CalculateEpicScaleFactor(GameLiving living)
+        {
+            double epicScaleFactor;
+            int petCap;
+
+            if (living is GameEpicBoss)
+            {
+                epicScaleFactor = 1.6;
+                petCap = 24;
+            }
+            else
+            {
+                epicScaleFactor = 0.8;
+                petCap = 16;
+            }
+
+            int petCount = 0;
+
+            // TODO: Find a way to remove `ToList` call.
+            foreach (GameObject attacker in living.attackComponent.Attackers.ToList())
+            {
+                if (attacker is GamePlayer)
+                    epicScaleFactor -= 0.04;
+                else if (attacker is GameSummonedPet && petCount <= petCap)
+                {
+                    epicScaleFactor -= 0.01;
+                    petCount++;
                 }
 
-				var attackerList = bossnpc.attackComponent.Attackers.ToList();
-
-                foreach (var attacker in attackerList)
+                if (epicScaleFactor < 0.4)
                 {
-					if(attacker is GamePlayer)
-						epicScaleFactor -= 0.4;
-					if (attacker is GameSummonedPet && petCount <= petCap)
-                    {
-						epicScaleFactor -= 0.1;
-						petCount++;
-					}
-						
-				}
+                    epicScaleFactor = 0.4;
+                    break;
+                }
+            }
 
-				if (epicScaleFactor < 4)
-					epicScaleFactor = 4;
-
-				epicScaleFactor *= .1;
-
-				return (int)((1 + (living.Level / 50.0)) * (living.Level << 1) * epicScaleFactor) 
-				+ living.SpecBuffBonusCategory[(int)property]
-				- Math.Abs(living.DebuffCategory[(int)property])/6
-				+ living.BuffBonusCategory4[(int)property];
-			}
-			else if (living is GameSummonedPet)
-			{
-				int baseVal = (int)((1 + (living.Level / 175.0)) * (living.Level << 1))
-				              + (living.BaseBuffBonusCategory[(int)property] / 6)
-				              + (living.SpecBuffBonusCategory[(int)property] / 6)
-				              - Math.Abs(living.DebuffCategory[(int)property])/6
-				              + living.BuffBonusCategory4[(int)property] / 6;
-
-				if (living is NecromancerPet)
-					baseVal += 10;
-
-				return baseVal;
-			}
-			else
-			{
-				int baseVal = (int)((1 + (living.Level / 200.0)) * (living.Level << 1))
-				+ (living.SpecBuffBonusCategory[(int)property] / 6)
-				- Math.Abs(living.DebuffCategory[(int)property])/6
-				+ living.BuffBonusCategory4[(int)property] / 6;
-
-				if (living is GuardLord)
-					baseVal += 20 * living.Level / 50;
-				
-				return baseVal;
-			}
-		}
-	}
+            return epicScaleFactor;
+        }
+    }
 }
