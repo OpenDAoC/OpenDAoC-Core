b399c5c3cf05fdf27c76312495dfe243981fde2f
Deftness <115406842+deftness02@users.noreply.github.com>
2023-09-23 08:07:52 -0600

    Refactored DOLServer to CoreServer

---
+++ CoreServer/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("OpenDAoC - DOLServer")]
+[assembly: AssemblyDescription("Main entrypoint for starting the server")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OpenDAoC Development Team")]
+[assembly: AssemblyProduct("DAoC Server Side Emulation Package - OpenDAoC")]
+[assembly: AssemblyCopyright("OpenDAoC Development Team")]
+[assembly: AssemblyTrademark("OpenDAoC Development Team")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.*")]
