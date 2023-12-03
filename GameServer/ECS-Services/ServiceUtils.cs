9bca208741f5d8c9ff742e3b3f5006326766acb5
Baptiste Marie <bm01@users.noreply.github.com>
2023-12-03 21:38:41 +0100

    Implement `ServiceUtils.ShouldTick` and variants * `ShouldTickAdjust` is intended to be used by services to prevent repeating ticks from drifting, and allow early tick if we're close to the targeted tick time. * `ShouldTick` only allows early ticks. * `ShouldTickAdjustNoEarly` only prevents repeating ticks from drifting. * `ShouldTickNoEarly` is a simple tick time and gameloop time comparison.

--- GameServer/ECS-Services/ServiceUtils.cs
+++ GameServer/ECS-Services/ServiceUtils.cs
@@ -7,6 +7,53 @@ namespace DOL.GS
     public static class ServiceUtils
     {
         private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        private const long HALF_TICK_RATE = GameLoop.TICK_RATE / 2;
+
+        public static bool ShouldTick(long tickTime)
+        {
+            return GetDelta(tickTime) - HALF_TICK_RATE <= 0;
+        }
+
+        public static bool ShouldTickNoEarly(long tickTime)
+        {
+            return GetDelta(tickTime) <= 0;
+        }
+
+        public static bool ShouldTickAdjust(ref long tickTime)
+        {
+            long delta = GetDelta(tickTime);
+
+            // Consider ticks to be late if the game loop can't keep up with its tick rate.
+            // This is used to adjust services' tick time, allowing them to catch up.
+            if (delta < -GameLoop.TICK_RATE)
+            {
+                tickTime = GameLoop.GameLoopTime;
+                return true;
+            }
+
+            return delta - HALF_TICK_RATE <= 0;
+        }
+
+        public static bool ShouldTickAdjustNoEarly(ref long tickTime)
+        {
+            long delta = GetDelta(tickTime);
+
+            // Consider ticks to be late if the game loop can't keep up with its tick rate.
+            // This is used to adjust services' tick time, allowing them to catch up.
+            if (delta < -GameLoop.TICK_RATE)
+            {
+                tickTime = GameLoop.GameLoopTime;
+                return true;
+            }
+
+            return delta <= 0;
+        }
+
+        private static long GetDelta(long tickTime)
+        {
+            // Positive if we're checking early, negative otherwise.
+            return tickTime - GameLoop.GameLoopTime;
+        }
 
         public static void HandleServiceException<T>(Exception exception, string serviceName, T entity, GameObject entityOwner) where T : class, IManagedEntity
         {
