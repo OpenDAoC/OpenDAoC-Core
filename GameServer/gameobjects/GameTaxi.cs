8cb50746b3b60b449fdb7f96c49249cb6c11af4a
Baptiste Marie <bm01@users.noreply.github.com>
2024-02-05 12:28:07 +0100

    `GameTaxi`: Force `IsVisibleToPlayers` to true. Prevents horses from stopping due to no player in proximity (after their rider dismounted).

--- GameServer/gameobjects/GameTaxi.cs
+++ GameServer/gameobjects/GameTaxi.cs
@@ -1,22 +1,3 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
-using System;
 using DOL.AI.Brain;
 
 namespace DOL.GS
@@ -42,7 +23,9 @@ namespace DOL.GS
 			BlankBrain brain = new BlankBrain();
 			SetOwnBrain(brain);
 		}
-		
+
+		public override bool IsVisibleToPlayers => true;
+
 		public override int MAX_PASSENGERS
 		{
 			get
