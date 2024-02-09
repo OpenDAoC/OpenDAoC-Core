OpenDAoC/OpenDAoC-Core

commit 37d86bd006557593e2a5c4386829cbdc632503af

Rewrite `GameDuel` * Fix pets stopping duels and being ignored in some places. * Make sure effects from player and their pet is removed from the other player and their pet. * Remove event handlers. * Improve effects removal performance on duel stop.

GameServer/gameobjects/GamePlayer.cs
@@ -128,9 +128,6 @@ namespace DOL.GS
         public static readonly string REALM_LOYALTY_KEY = "realm_loyalty";
         public static readonly string CURRENT_LOYALTY_KEY = "current_loyalty_days";
 
-        public static readonly string DUEL_PREVIOUS_LASTATTACKTICKPVP = "DUEL_PREVIOUS_LASTATTACKTICKPVP";
-        public static readonly string DUEL_PREVIOUS_LASTATTACKEDBYENEMYTICKPVP= "DUEL_PREVIOUS_LASTATTACKEDBYENEMYTICKPVP";
-
         /// <summary>
         /// Effectiveness of the rez sick that should be applied. This is set by rez spells just before rezzing.
         /// </summary>
@@ -6019,7 +6016,7 @@ namespace DOL.GS
                 if (spell.InstrumentRequirement != 0)
                 {
                     Out.SendMessage(LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.SwitchWeapon.SpellCancelled"), eChatType.CT_SpellResisted, eChatLoc.CL_SystemWindow);
-                    EffectService.RequestImmediateCancelConcEffect(EffectListService.GetPulseEffectOnTarget(this, spell));
+                    EffectService.RequestImmediateCancelEffect(EffectListService.GetPulseEffectOnTarget(this, spell));
                 }
             }
 
@@ -6386,25 +6383,19 @@ namespace DOL.GS
 
         public override void TakeDamage(GameObject source, eDamageType damageType, int damageAmount, int criticalAmount)
         {
+            if (Duel != null && !IsDuelPartner(source as GameLiving))
+                Duel.Stop();
 
-            if (this.DuelTarget != null && source != this.DuelTarget)
-                this.DuelStop();
-
-            #region PVP DAMAGE
-
-            if (source is GamePlayer || (source is GameNPC && (source as GameNPC).Brain is IControlledBrain && ((source as GameNPC).Brain as IControlledBrain).GetPlayerOwner() != null) || source is GameSiegeWeapon)
+            if (source is GamePlayer || (source is GameNPC npc && npc.Brain is IControlledBrain brain && brain.GetPlayerOwner() != null) || source is GameSiegeWeapon)
             {
                 if (Realm != source.Realm && source.Realm != 0)
-                    DamageRvRMemory += (long)(damageAmount + criticalAmount);
+                    DamageRvRMemory += damageAmount + criticalAmount;
             }
-
-            #endregion PVP DAMAGE
 
             base.TakeDamage(source, damageType, damageAmount, criticalAmount);
-            if(this.HasAbility(Abilities.DefensiveCombatPowerRegeneration))
-            {
-                this.Mana += (int)((damageAmount + criticalAmount) * 0.25);
-            }
+
+            if (HasAbility(Abilities.DefensiveCombatPowerRegeneration))
+                Mana += (int)((damageAmount + criticalAmount) * 0.25);
         }
 
         /// <summary>
@@ -6877,7 +6868,7 @@ namespace DOL.GS
             }
             else
             {
-                if (DuelTarget == killer)
+                if (IsDuelPartner(killer as GameLiving))
                 {
                     m_releaseType = eReleaseType.Duel;
                     messageDistance = WorldMgr.YELL_DISTANCE;
@@ -6898,7 +6889,7 @@ namespace DOL.GS
                         publicMessage = LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.Die.KilledBy", GetName(0, true), killer.GetName(1, false));
                     }
 
-                    if(ConquestService.ConquestManager.IsPlayerInConquestArea(this) && killer.Realm != this.Realm && killer is GamePlayer && killer != this.DuelTarget)
+                    if (ConquestService.ConquestManager.IsPlayerInConquestArea(this) && killer.Realm != Realm && killer is GamePlayer && !IsDuelPartner(killer as GameLiving))
                         ConquestService.ConquestManager.AddContributor(this);
                 }
             }
@@ -6916,7 +6907,7 @@ namespace DOL.GS
 
             KillStreak = 0;
 
-            DuelStop();
+            Duel.Stop();
 
             eChatType messageType;
             if (m_releaseType == eReleaseType.Duel)
@@ -7197,48 +7188,40 @@ namespace DOL.GS
         #endregion
 
         #region Duel
-        /// <summary>
-        /// Gets the duel target of this player
-        /// </summary>
-        public GamePlayer DuelTarget { get { return Duel != null ? Duel.Target : null; }}
 
-        /// <summary>
-        /// Get the GameDuel of this player
-        /// </summary>
-        protected GameDuel Duel { get; set; }
+        public GameDuel Duel { get; private set; }
+        public GamePlayer DuelPartner => Duel?.GetPartnerOf(this);
 
-        /// <summary>
-        /// Starts the duel
-        /// </summary>
-        /// <param name="duelTarget">The duel target</param>
-        public virtual void DuelStart(GamePlayer duelTarget)
+        public void OnDuelStart(GameDuel duel)
         {
-            if (Duel != null)
-                return;
-
-            Duel = new GameDuel(this, duelTarget);
-            Duel.Start();
-
-            //Get PvP Combat ticks before duel.
-            TempProperties.SetProperty(DUEL_PREVIOUS_LASTATTACKTICKPVP, LastAttackTickPvP);
-            TempProperties.SetProperty(DUEL_PREVIOUS_LASTATTACKEDBYENEMYTICKPVP, LastAttackedByEnemyTickPvP);
+            Duel?.Stop();
+            Duel = duel;
         }
 
-        /// <summary>
-        /// Stops the duel if it is running
-        /// </summary>
-        public void DuelStop()
+        public void OnDuelStop()
         {
             if (Duel == null)
                 return;
 
-            Duel.Stop();
             Duel = null;
-
-            //Set PvP Combat ticks to that they were before duel.
-            LastAttackTickPvP = TempProperties.GetProperty<long>(DUEL_PREVIOUS_LASTATTACKTICKPVP);
-            LastAttackedByEnemyTickPvP = TempProperties.GetProperty<long>(DUEL_PREVIOUS_LASTATTACKEDBYENEMYTICKPVP);
         }
+
+        public bool IsDuelPartner(GameLiving living)
+        {
+            if (living == null)
+                return false;
+
+            GamePlayer partner = DuelPartner;
+
+            if (partner == null)
+                return false;
+
+            if (living is GameNPC npc && npc.Brain is ControlledNpcBrain brain)
+                living = brain.GetPlayerOwner();
+
+            return partner == living;
+        }
+
         #endregion
 
         #region Spell cast
@@ -9054,6 +9037,7 @@ namespace DOL.GS
             if (CurrentRegion is BaseInstance instance)
                 instance.OnPlayerLeaveInstance(this);
 
+            Duel?.Stop();
             return true;
         }
 

GameServer/commands/playercommands/duel.cs
@@ -1,22 +1,3 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
-
 using DOL.GS.PacketHandler;
 using DOL.Language;
 
@@ -210,7 +191,8 @@ namespace DOL.GS.Commands
 						if (!CheckDuelStart(client.Player, duelStarter))
 							return;
 
-						client.Player.DuelStart(duelStarter);
+						GameDuel duel = new(duelStarter, client.Player);
+						duel.Start();
 
 						duelStarter.Out.SendMessage(LanguageMgr.GetTranslation(client.Account.Language, "Scripts.Players.Duel.TargetAccept", client.Player.Name), eChatType.CT_Emote, eChatLoc.CL_SystemWindow);
 						client.Player.Out.SendMessage(LanguageMgr.GetTranslation(client.Account.Language, "Scripts.Players.Duel.YouAccept"), eChatType.CT_Emote, eChatLoc.CL_SystemWindow);
@@ -277,15 +259,15 @@ namespace DOL.GS.Commands
 
 					case "surrender":
 					{
-						GamePlayer target = client.Player.DuelTarget;
+						GamePlayer target = client.Player.DuelPartner;
+
 						if (target == null)
 						{
 							client.Out.SendMessage(LanguageMgr.GetTranslation(client.Account.Language, "Scripts.Players.Duel.NotInDuel"), eChatType.CT_Emote, eChatLoc.CL_SystemWindow);
 							return;
 						}
 
-						client.Player.DuelStop();
-
+						client.Player.Duel.Stop();
 						client.Out.SendMessage(LanguageMgr.GetTranslation(client.Account.Language, "Scripts.Players.Duel.YouSurrender", target.Name), eChatType.CT_Emote, eChatLoc.CL_SystemWindow);
 						target.Out.SendMessage(LanguageMgr.GetTranslation(client.Account.Language, "Scripts.Players.Duel.TargetSurrender", client.Player.Name), eChatType.CT_Emote, eChatLoc.CL_SystemWindow);
 						Message.SystemToArea(client.Player, LanguageMgr.GetTranslation(client.Account.Language, "Scripts.Players.Duel.PlayerVsPlayer", client.Player.Name, target.Name), eChatType.CT_Emote, client.Player, target);
@@ -311,12 +293,12 @@ namespace DOL.GS.Commands
 				actionSource.Out.SendMessage(LanguageMgr.GetTranslation(actionSource.Client, "Scripts.Players.Duel.EnemyRealm"), eChatType.CT_Emote, eChatLoc.CL_SystemWindow);
 				return false;
 			}
-			if (actionSource.DuelTarget != null)
+			if (actionSource.DuelPartner != null)
 			{
 				actionSource.Out.SendMessage(LanguageMgr.GetTranslation(actionSource.Client, "Scripts.Players.Duel.YouInDuel"), eChatType.CT_Emote, eChatLoc.CL_SystemWindow);
 				return false;
 			}
-			if (actionTarget.DuelTarget != null)
+			if (actionTarget.DuelPartner != null)
 			{
 				actionSource.Out.SendMessage(LanguageMgr.GetTranslation(actionSource.Client, "Scripts.Players.Duel.TargetInDuel", actionTarget.Name), eChatType.CT_Emote, eChatLoc.CL_SystemWindow);
 				return false;

GameServer/gameutils/GameDuel.cs
@@ -1,220 +1,96 @@
-﻿/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using DOL.Events;
+﻿using System.Collections.Generic;
 using DOL.AI.Brain;
-using DOL.GS.Effects;
-using DOL.Language;
 using DOL.GS.PacketHandler;
 using DOL.GS.Spells;
-using Newtonsoft.Json.Serialization;
+using DOL.Language;
 
 namespace DOL.GS
 {
-	/// <summary>
-	/// GameDuel is an Helper Class for Player Duels
-	/// </summary>
-	public class GameDuel
-	{
-		/// <summary>
-		/// Duel Initiator
-		/// </summary>
-		public GamePlayer Starter { get; protected set; }
-		
-		/// <summary>
-		/// Duel Target
-		/// </summary>
-		public GamePlayer Target { get; protected set; }
-		
-		/// <summary>
-		/// Is Duel Started ?
-		/// </summary>
-		public bool Started { get { return m_started; } protected set { m_started = value; } }
-		protected volatile bool m_started;
-		
-		/// <summary>
-		/// Default Constructor
-		/// </summary>
-		/// <param name="starter"></param>
-		/// <param name="target"></param>
-		public GameDuel(GamePlayer starter, GamePlayer target)
-		{
-			Starter = starter;
-			Target = target;
-			Started = false;
-		}
+    public class GameDuel
+    {
+        private const string DUEL_PREVIOUS_LASTATTACKTICKPVP = "DUEL_PREVIOUS_LASTATTACKTICKPVP";
+        private const string DUEL_PREVIOUS_LASTATTACKEDBYENEMYTICKPVP= "DUEL_PREVIOUS_LASTATTACKEDBYENEMYTICKPVP";
+        public GamePlayer Starter { get; private set; }
+        public GamePlayer Target { get; private set; }
 
-		/// <summary>
-		/// Start Duel if is not running.
-		/// </summary>
-		public virtual void Start()
-		{
-			if (Started)
-				return;
-			
-			Started = true;
-			
-			Target.DuelStart(Starter);
-			GameEventMgr.AddHandler(Starter, GamePlayerEvent.Quit, new DOLEventHandler(DuelOnPlayerQuit));
-			GameEventMgr.AddHandler(Starter, GamePlayerEvent.Linkdeath, new DOLEventHandler(DuelOnPlayerQuit));
-			GameEventMgr.AddHandler(Starter, GamePlayerEvent.RegionChanged, new DOLEventHandler(DuelOnPlayerQuit));
-			GameEventMgr.AddHandler(Starter, GameLivingEvent.AttackedByEnemy, new DOLEventHandler(DuelOnAttack));
-			GameEventMgr.AddHandler(Starter, GameLivingEvent.AttackFinished, new DOLEventHandler(DuelOnAttack));
-		}
-		
-		/// <summary>
-		/// Stops the duel if it is running
-		/// </summary>
-		public virtual void Stop()
-		{
-			if (!Started)
-				return;
+        public GameDuel(GamePlayer starter, GamePlayer target)
+        {
+            Starter = starter;
+            Target = target;
+        }
 
-			StopNegativeEffects(Starter, Target);
-			StopNegativeEffects(Target, Starter);
-			if (Caster is GameSummonedPet casterPet && (casterPet.Owner == Target || casterPet.Owner == Starter))
-			{
-				StopNegativeEffects(Target, casterPet);
-				StopNegativeEffects(Starter, casterPet);
-			}
-			StopImmunityEffects(Target);
-			StopImmunityEffects(Starter);
+        public GamePlayer GetPartnerOf(GameLiving living)
+        {
+            if (living is GameNPC npc && npc.Brain is ControlledNpcBrain brain)
+                living = brain.GetPlayerOwner();
 
-			Started = false;
-			Target?.DuelStop();
-			Target = null;
+            return living == Starter ? Target : Starter;
+        }
 
-			GameEventMgr.RemoveHandler(Starter, GamePlayerEvent.Quit, new DOLEventHandler(DuelOnPlayerQuit));
-			GameEventMgr.RemoveHandler(Starter, GamePlayerEvent.Linkdeath, new DOLEventHandler(DuelOnPlayerQuit));
-			GameEventMgr.RemoveHandler(Starter, GamePlayerEvent.RegionChanged, new DOLEventHandler(DuelOnPlayerQuit));
-			GameEventMgr.RemoveHandler(Starter, GameLivingEvent.AttackedByEnemy, new DOLEventHandler(DuelOnAttack));
-			GameEventMgr.RemoveHandler(Starter, GameLivingEvent.AttackFinished, new DOLEventHandler(DuelOnAttack));
-			
-			lock (Starter.XPGainers.SyncRoot)
-			{
-				Starter.XPGainers.Clear();
-			}
-			
-			Starter.Out.SendMessage(LanguageMgr.GetTranslation(Starter.Client, "GamePlayer.DuelStop.DuelEnds"), eChatType.CT_Emote, eChatLoc.CL_SystemWindow);
-		}
+        public void Start()
+        {
+            HandlePlayer(Starter, this);
+            HandlePlayer(Target, this);
 
-		/// <summary>
-		/// Stops the duel if player attack or is attacked by anything other that duel target
-		/// </summary>
-		/// <param name="e"></param>
-		/// <param name="sender"></param>
-		/// <param name="arguments"></param>
-		protected virtual void DuelOnAttack(DOLEvent e, object sender, EventArgs arguments)
-		{
-			AttackData ad = null;
-			GameLiving target = null;
-			var afea = arguments as AttackFinishedEventArgs;
-			var abeea = arguments as AttackedByEnemyEventArgs;
-			
-			if (afea != null)
-			{
-				ad = afea.AttackData;
-				target = ad.Target;
-			}
-			else if (abeea != null)
-			{
-				ad = abeea.AttackData;
-				target = ad.Attacker;
-			}
+            static void HandlePlayer(GamePlayer player, GameDuel duel)
+            {
+                player.OnDuelStart(duel);
+                player.TempProperties.SetProperty(DUEL_PREVIOUS_LASTATTACKTICKPVP, player.LastAttackTickPvP);
+                player.TempProperties.SetProperty(DUEL_PREVIOUS_LASTATTACKEDBYENEMYTICKPVP, player.LastAttackedByEnemyTickPvP);
+            }
+        }
 
-			if (ad == null)
-				return;
+        public void Stop()
+        {
+            HandlePlayer(Starter, Target);
+            HandlePlayer(Target, Starter);
 
-			// check pets owner for my and enemy attacks
-			GameNPC npc = target as GameNPC;
-			if (npc != null)
-			{
-				IControlledBrain brain = npc.Brain as IControlledBrain;
-				if (brain != null)
-					target = brain.GetPlayerOwner();
-			}
-			
-			// Duel should end if players join group and trys to attack
-			if (ad.Attacker.Group != null && ad.Attacker.Group.IsInTheGroup(ad.Target))
-				Stop();
-			
-			if (ad.IsHit && (target != Target && target != Starter ))
-				Stop();
-		}
+            static void HandlePlayer(GamePlayer player, GamePlayer partner)
+            {
+                player.OnDuelStop();
+                player.LastAttackTickPvP = player.TempProperties.GetProperty<long>(DUEL_PREVIOUS_LASTATTACKTICKPVP);
+                player.LastAttackedByEnemyTickPvP = player.TempProperties.GetProperty<long>(DUEL_PREVIOUS_LASTATTACKEDBYENEMYTICKPVP);
 
-		/// <summary>
-		/// Stops the duel on quit/link death
-		/// </summary>
-		/// <param name="e"></param>
-		/// <param name="sender"></param>
-		/// <param name="arguments"></param>
-		protected virtual void DuelOnPlayerQuit(DOLEvent e, object sender, EventArgs arguments)
-		{
-			Stop();
-		}
-		
-		public GameLiving Caster;
-		
-		/// <summary>
-		/// Stops a negative effect 
-		/// </summary>
-		/// <param name="target"></param>
-		/// <param name="Caster"></param>
-		protected virtual void StopNegativeEffects(GamePlayer target, GameLiving Caster)
-		{
-			if (target == null)
-				return;
-			
-			var effects = target.effectListComponent.GetAllEffects();
-			var spelle = target.effectListComponent.GetSpellEffects();
-			foreach (var spellEffect in spelle.Where(spellEffect => spellEffect != null && !spellEffect.HasPositiveEffect && spellEffect.Caster == Caster && spellEffect.Owner == target))
-			{
-				EffectService.RequestImmediateCancelEffect(EffectListService.GetSpellEffectOnTarget(target, spellEffect.EffectType));
-			}
-			foreach (var effect in effects.Where(effect => effect != null && !effect.HasPositiveEffect && effect.Owner == target))
-			{
-				EffectService.RequestImmediateCancelEffect(EffectListService.GetEffectOnTarget(target, effect.EffectType));
-			}
-		}
-		/// <summary>
-		/// Stops any immunity timers 
-		/// </summary>
-		/// <param name="target"></param>
-		protected virtual void StopImmunityEffects(GamePlayer target)
-		{
-			if (target == null)
-				return;
-			
-			var effects = target.effectListComponent.GetAllEffects();
-			var spelle = target.effectListComponent.GetSpellEffects();
-			foreach (var spellEffect in spelle.Where(spellEffect => spellEffect != null && spellEffect is ECSImmunityEffect && spellEffect.Caster != target))
-			{
-				EffectService.RequestImmediateCancelEffect(EffectListService.GetImmunityEffectOnTarget(target, spellEffect.EffectType));
-			}
-			foreach (var effect in effects.Where(effect => effect != null && effect is ECSImmunityEffect && effect.Owner != Caster))
-			{
-				EffectService.RequestImmediateCancelEffect(EffectListService.GetImmunityEffectOnTarget(target, effect.EffectType));
-			}
-		}
+                lock (player.XPGainers.SyncRoot)
+                {
+                    player.XPGainers.Clear();
+                }
 
-	}
+                player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client, "GamePlayer.DuelStop.DuelEnds"), eChatType.CT_Emote, eChatLoc.CL_SystemWindow);
+                StopEffects(player, partner);
+            }
+
+            static void StopEffects(GamePlayer player, GamePlayer caster)
+            {
+                Loop(player.effectListComponent.GetAllEffects(), caster);
+
+                IControlledBrain controlledBrain = player.ControlledBrain;
+
+                if (controlledBrain != null)
+                    Loop(controlledBrain.Body.effectListComponent.GetAllEffects(), caster);
+
+                static void Loop(List<ECSGameEffect> effects, GamePlayer caster)
+                {
+                    GameNPC petCaster = caster.ControlledBrain?.Body;
+
+                    foreach (ECSGameEffect effect in effects)
+                    {
+                        if (effect.HasPositiveEffect)
+                            continue;
+
+                        ISpellHandler spellHandler = effect.SpellHandler;
+
+                        if (spellHandler == null)
+                            continue;
+
+                        if (spellHandler.Caster == caster || (spellHandler.Caster != null && spellHandler.Caster == petCaster))
+                        {
+                            effect.TriggersImmunity = false;
+                            EffectService.RequestCancelEffect(effect);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }

GameServer/gameutils/Group.cs
@@ -182,15 +182,16 @@ namespace DOL.GS
 														return true; }))
 				return false;
 
-			if (living is GamePlayer p && p.DuelTarget != null) 
-				p.DuelStop();
+			GamePlayer player = living as GamePlayer;
+
+			if (player?.Duel != null)
+				player.Duel.Stop();
 			
 			UpdateGroupWindow();
 			// update icons of joined player to everyone in the group
 			UpdateMember(living, true, false);
 			
 			// update all icons for just joined player
-			var player = living as GamePlayer;
 			if (player != null)
 				player.Out.SendGroupMembersUpdate(true, true);
 

GameServer/gameutils/RealmTimer/RealmTimer.cs
@@ -1,15 +1,9 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Text.Json.Serialization;
-using DOL.GS;
 using DOL.Database;
 using DOL.GS.PacketHandler;
 
-
 namespace DOL.GS.Utils;
-
 
 //Utility class for checking realmtimer of a character/account and acting accordingly 
 public class RealmTimer
@@ -99,11 +93,10 @@ public class RealmTimer
     public static void SaveRealmTimer(GamePlayer player)
     {
         //Don't save realmtimer during duels
-        if(player.DuelTarget != null)
+        if (player.DuelPartner != null)
             return;
 
         DbAccount playerAccount = player.Client.Account;
-        
         DateTime LastCombatTickPvPDateTime = DateTime.Now.AddMilliseconds(-(GameLoop.GameLoopTime - player.LastCombatTickPvP));
 
         //Don't update realmtimer it is still in effect and players realm is not the realm_timer_realm
@@ -161,7 +154,4 @@ public class RealmTimer
         else
             return 0;
     }
-    
-   
-
 }

GameServer/realmabilities_atlasOF/handlers/AtlasOF_Grapple.cs
@@ -1,17 +1,10 @@
-using System;
-using System.Collections;
-using System.Reflection;
-using DOL.GS;
-using DOL.GS.PacketHandler;
-using DOL.GS.Effects;
-using DOL.GS.Spells;
-using DOL.Events;
 using DOL.Database;
-using DOL.GS.API;
+using DOL.GS.PacketHandler;
+using DOL.GS.Spells;
 
 namespace DOL.GS.RealmAbilities
 {
-	public class AtlasOF_Grapple : TimedRealmAbility, ISpellCastingAbilityHandler
+    public class AtlasOF_Grapple : TimedRealmAbility, ISpellCastingAbilityHandler
     {
 		public AtlasOF_Grapple(DbAbility dba, int level) : base(dba, level) { }
 
@@ -71,10 +64,10 @@ namespace DOL.GS.RealmAbilities
 
 			CreateSpell(caster);
 
-			foreach (GamePlayer pl in caster.GetPlayersInRadius(m_range))
+			foreach (GamePlayer playerInRadius in caster.GetPlayersInRadius(m_range))
 			{
-				if(pl.Realm != caster.Realm || caster.DuelTarget == pl)
-					CastSpellOn(pl, caster);
+				if (playerInRadius.Realm != caster.Realm || caster.IsDuelPartner(playerInRadius))
+					CastSpellOn(playerInRadius, caster);
 			}
 
 			foreach (GameNPC npc in caster.GetNPCsInRadius(m_range))
@@ -97,6 +90,5 @@ namespace DOL.GS.RealmAbilities
 		        dd.StartSpell(target);
 	        }
         }
-
 	}
 }

GameServer/serverrules/NormalServerRules.cs
@@ -58,7 +58,7 @@ namespace DOL.GS.ServerRules
 			}
 
 			//Don't allow attacks on same realm members on Normal Servers
-			if (attacker.Realm == defender.Realm && !(attacker is GamePlayer && ((GamePlayer)attacker).DuelTarget == defender))
+			if (attacker.Realm == defender.Realm && !(attacker is GamePlayer && ((GamePlayer) attacker).IsDuelPartner(defender)))
 			{
 				// allow confused mobs to attack same realm
 				if (attacker is GameNPC && (attacker as GameNPC).IsConfused)

GameServer/serverrules/PvEServerRules.cs
@@ -1,24 +1,4 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
-using System;
 using DOL.AI.Brain;
-using DOL.GS.PacketHandler;
 using DOL.Database;
 
 namespace DOL.GS.ServerRules
@@ -69,7 +49,7 @@ namespace DOL.GS.ServerRules
 
 			if (attacker.Realm != eRealm.None && defender.Realm != eRealm.None)
 			{
-				if (attacker is GamePlayer && ((GamePlayer)attacker).DuelTarget == defender)
+				if (attacker is GamePlayer && ((GamePlayer) attacker).IsDuelPartner(defender))
 					return true;
 				if (quiet == false) MessageToLiving(attacker, "You can not attack other players on this server!");
 				return false;

commit e668d47dc16d5c448e23a01935256e4afb58d93c

Fix missing null check
Caused by 37d86bd

GameServer/gameobjects/GamePlayer.cs
@@ -6907,7 +6907,7 @@ namespace DOL.GS
 
             KillStreak = 0;
 
-            Duel.Stop();
+            Duel?.Stop();
 
             eChatType messageType;
             if (m_releaseType == eReleaseType.Duel)