OpenDAoC/OpenDAoC-Core

commit 95dff342906353173c2049287ae322b1b1f466a9

Make `AttackAction` use `ShouldTick`

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -28,12 +28,7 @@ namespace DOL.GS
 
         // Set to current time when a round doesn't result in an attack. Used to prevent combat log spam and kept until reset in AttackComponent.SendAttackingCombatMessages().
         public long RoundWithNoAttackTime { get; set; }
-
-        public long StartTime
-        {
-            get => _startTime;
-            set => _startTime = value + GameLoop.GameLoopTime;
-        }
+        public ref long StartTime => ref _startTime;
 
         protected AttackAction(GameLiving owner)
         {
@@ -52,9 +47,9 @@ namespace DOL.GS
             return null;
         }
 
-        public void Tick(long time)
+        public void Tick()
         {
-            if (time <= StartTime)
+            if (!ServiceUtils.ShouldTickAdjust(ref StartTime))
                 return;
 
             if (!CheckAttackState())
@@ -91,7 +86,7 @@ namespace DOL.GS
                 }
             }
 
-            StartTime = _interval;
+            StartTime += _interval;
         }
 
         public virtual bool CheckInterruptTimer()

GameServer/ECS-Components/AttackComponent.cs
@@ -89,7 +89,7 @@ namespace DOL.GS
             _attackersCheckTimer = new(owner, CheckAttackers);
         }
 
-        public void Tick(long time)
+        public void Tick()
         {
             if (StartAttackRequested)
             {
@@ -97,7 +97,7 @@ namespace DOL.GS
                 StartAttack();
             }
 
-            attackAction?.Tick(time);
+            attackAction?.Tick();
 
             if (weaponAction?.AttackFinished == true)
                 weaponAction = null;

GameServer/gameobjects/GamePlayer.cs
@@ -9626,13 +9626,8 @@ namespace DOL.GS
                     AttackData ad = TempProperties.GetProperty<AttackData>(LAST_ATTACK_DATA, null);
                     if (ad != null && ad.IsMeleeAttack && (ad.AttackResult == eAttackResult.TargetNotVisible || ad.AttackResult == eAttackResult.OutOfRange))
                     {
-                        //Does the target can be attacked ?
-                        //if (ad.Target != null && IsObjectInFront(ad.Target, 120) && this.IsWithinRadius(ad.Target, AttackRange) && m_attackAction != null)
                         if (ad.Target != null && IsObjectInFront(ad.Target, 120) && this.IsWithinRadius(ad.Target, attackComponent.AttackRange) && attackComponent.attackAction != null)
-                        {
-                            //m_attackAction.Start(1);
-                            attackComponent.attackAction.StartTime = 1;
-                        }
+                            attackComponent.attackAction.StartTime = 0;
                     }
                 }
             }
@@ -10003,7 +9998,7 @@ namespace DOL.GS
                     if (ad != null && ad.IsMeleeAttack && (ad.AttackResult == eAttackResult.TargetNotVisible || ad.AttackResult == eAttackResult.OutOfRange))
                     {
                         if (ad.Target != null && IsObjectInFront(ad.Target, 120) && IsWithinRadius(ad.Target, attackComponent.AttackRange) && attackComponent.attackAction != null)
-                            attackComponent.attackAction.StartTime = 1;
+                            attackComponent.attackAction.StartTime = 0;
                     }
                 }
             }

GameServer/ECS-Services/AttackService.cs
@@ -12,7 +12,7 @@ namespace DOL.GS
         private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         private const string SERVICE_NAME = nameof(AttackService);
 
-        public static void Tick(long tick)
+        public static void Tick()
         {
             GameLoop.CurrentServiceTick = SERVICE_NAME;
             Diagnostics.StartPerfCounter(SERVICE_NAME);
@@ -27,8 +27,9 @@ namespace DOL.GS
                 {
                     if (attackComponent?.EntityManagerId.IsSet != true)
                         return;
+
                     long startTick = GameLoop.GetCurrentTime();
-                    attackComponent.Tick(tick);
+                    attackComponent.Tick();
                     long stopTick = GameLoop.GetCurrentTime();
 
                     if (stopTick - startTick > 25)

GameServer/managers/GameLoopManager/GameLoop.cs
@@ -76,7 +76,7 @@ namespace DOL.GS
             {
                 ECS.Debug.Diagnostics.StartPerfCounter(THREAD_NAME);
                 NpcService.Tick();
-                AttackService.Tick(GameLoopTime);
+                AttackService.Tick();
                 CastingService.Tick(GameLoopTime);
                 EffectService.Tick();
                 EffectListService.Tick(GameLoopTime);

commit 20aace775ae4dbf0a71ddba05fc5303d14373c9a

Fix `AttackAction` ticking immediately for ranged attacks * Caused by 95dff34 * Made next tick delay when switching from ranged weapon to melee weapon affect NPCs too. Lowered it to 500ms. * Remove usage of `RANGED_ATTACK_START` temp property. * Replaced some calls to `GetAbilityEffectOnTarget` by more performant `ContainsEffectForEffectType`. * Added an early out to `SwitchWeapon` if we're switching to the slot currently in use.

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -24,17 +24,18 @@ namespace DOL.GS
         protected int _interruptDuration;
         protected int _interval;
         private GameLiving _owner;
-        private long _startTime;
+        private long _nextTick;
 
         // Set to current time when a round doesn't result in an attack. Used to prevent combat log spam and kept until reset in AttackComponent.SendAttackingCombatMessages().
         public long RoundWithNoAttackTime { get; set; }
-        public ref long StartTime => ref _startTime;
+        public long NextTick => _nextTick;
 
         protected AttackAction(GameLiving owner)
         {
             _owner = owner;
             _attackComponent = _owner.attackComponent;
             _styleComponent = _owner.styleComponent;
+            _nextTick = GameLoop.GameLoopTime;
         }
 
         public static AttackAction Create(GameLiving gameLiving)
@@ -49,7 +50,7 @@ namespace DOL.GS
 
         public void Tick()
         {
-            if (!ServiceUtils.ShouldTickAdjust(ref StartTime))
+            if (!ServiceUtils.ShouldTickAdjust(ref _nextTick))
                 return;
 
             if (!CheckAttackState())
@@ -86,7 +87,17 @@ namespace DOL.GS
                 }
             }
 
-            StartTime += _interval;
+            _nextTick += _interval;
+        }
+
+        public void ResetNextTick()
+        {
+            _nextTick = GameLoop.GameLoopTime;
+        }
+
+        public void DelayNextTick(int amount)
+        {
+            _nextTick += amount;
         }
 
         public virtual bool CheckInterruptTimer()
@@ -183,21 +194,39 @@ namespace DOL.GS
 
         protected virtual bool PrepareRangedAttack()
         {
-            eCheckRangeAttackStateResult rangeCheckresult = _owner.rangeAttackComponent.CheckRangeAttackState(_target);
+            int attackSpeed = _owner.attackComponent.AttackSpeed(_weapon);
 
-            if (rangeCheckresult == eCheckRangeAttackStateResult.Hold)
+            if (_owner.rangeAttackComponent.RangedAttackState == eRangedAttackState.None)
+            {
+                _owner.rangeAttackComponent.RangedAttackState = eRangedAttackState.Aim;
+
+                if (_owner is not GamePlayer || !_owner.effectListComponent.ContainsEffectForEffectType(eEffect.Volley))
+                {
+                    // The 'stance' parameter appears to be used to tell whether or not the animation should be held, and doesn't seem to be related to the weapon speed.
+                    foreach (GamePlayer player in _owner.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
+                        player.Out.SendCombatAnimation(_owner, null, (ushort) (_weapon != null ? _weapon.Model : 0), 0, player.Out.BowPrepare, 0x1A, 0x00, 0x00);
+
+                    _interval = _owner.rangeAttackComponent?.RangedAttackType == eRangedAttackType.RapidFire ? Math.Max(1500, attackSpeed / 2) : attackSpeed;
+                }
+
+                return false;
+            }
+
+            eCheckRangeAttackStateResult rangeCheckResult = _owner.rangeAttackComponent.CheckRangeAttackState(_target);
+
+            if (rangeCheckResult == eCheckRangeAttackStateResult.Hold)
             {
                 _interval = TICK_INTERVAL_FOR_NON_ATTACK;
                 return false;
             }
-            else if (rangeCheckresult == eCheckRangeAttackStateResult.Stop || _target == null)
+            else if (rangeCheckResult == eCheckRangeAttackStateResult.Stop || _target == null)
             {
                 _attackComponent.StopAttack();
                 _attackComponent.attackAction?.CleanUp();
                 return false;
             }
 
-            _interval = _attackComponent.AttackSpeed(_weapon);
+            _interval = attackSpeed;
             _interruptDuration = _interval;
             _ticksToTarget = _owner.GetDistanceTo(_target) * 1000 / RangeAttackComponent.PROJECTILE_FLIGHT_SPEED;
             int model = _weapon == null ? 0 : _weapon.Model;
@@ -253,7 +282,7 @@ namespace DOL.GS
                     // and the resulting interrupt will last 1.5 seconds."
 
                     long rapidFireMaxDuration = _attackComponent.AttackSpeed(_weapon);
-                    long elapsedTime = GameLoop.GameLoopTime - _owner.TempProperties.GetProperty<long>(RangeAttackComponent.RANGED_ATTACK_START); // elapsed time before ready to fire
+                    long elapsedTime = GameLoop.GameLoopTime - _owner.rangeAttackComponent.AttackStartTime;
 
                     if (elapsedTime < rapidFireMaxDuration)
                     {
@@ -301,7 +330,7 @@ namespace DOL.GS
             {
                 new ECSGameTimer(_owner, new ECSGameTimer.ECSTimerCallback(_attackComponent.weaponAction.Execute), _ticksToTarget);
 
-                // This is done in weaponAction.Execute(), but we musn't wait for the attack to reach our target.
+                // This is done in weaponAction.Execute(), but we must not wait for the attack to reach our target.
                 _attackComponent.weaponAction.AttackFinished = true;
             }
             else
@@ -342,28 +371,29 @@ namespace DOL.GS
             if (CheckInterruptTimer())
                 return false;
 
-            // Need to find a way to not have to call 'AttackSpeed()' again (currently needed
+            // Need to find a way to not have to call 'AttackSpeed' again.
             _interval = _attackComponent.AttackSpeed(_weapon);
+            _owner.rangeAttackComponent.AttackStartTime = GameLoop.GameLoopTime;
             _owner.rangeAttackComponent.RangedAttackState = eRangedAttackState.Aim;
 
             if (_owner.rangeAttackComponent.RangedAttackType != eRangedAttackType.Long)
             {
                 _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
 
-                if (EffectListService.GetAbilityEffectOnTarget(_owner, eEffect.SureShot) != null)
+                if (_owner.effectListComponent.ContainsEffectForEffectType(eEffect.SureShot))
                     _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.SureShot;
-                if (EffectListService.GetAbilityEffectOnTarget(_owner, eEffect.RapidFire) != null)
+                else if (_owner.effectListComponent.ContainsEffectForEffectType(eEffect.RapidFire))
                 {
                     _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.RapidFire;
                     _interval = Math.Max(1500, _interval /= 2);
                 }
-                if (EffectListService.GetAbilityEffectOnTarget(_owner, eEffect.TrueShot) != null)
+                else if (_owner.effectListComponent.ContainsEffectForEffectType(eEffect.SureShot))
                     _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.Long;
             }
 
             // The 'stance' parameter appears to be used to tell whether or not the animation should be held, and doesn't seem to be related to the weapon speed.
             foreach (GamePlayer player in _owner.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
-                player.Out.SendCombatAnimation(_owner, null, (ushort)(_weapon != null ? _weapon.Model : 0), 0x00, player.Out.BowPrepare, 0x1A, 0x00, 0x00);
+                player.Out.SendCombatAnimation(_owner, null, (ushort) (_weapon != null ? _weapon.Model : 0), 0x00, player.Out.BowPrepare, 0x1A, 0x00, 0x00);
 
             return true;
         }

GameServer/ECS-Components/Actions/NpcAttackAction.cs
@@ -56,7 +56,7 @@ namespace DOL.GS
                 !_npcOwner.IsWithinRadius(_target, 500))
             {
                 _npcOwner.SwitchToRanged(_target);
-                _interval = _attackComponent.AttackSpeed(_weapon);
+                _interval = 0;
                 return false;
             }
 

GameServer/ECS-Components/Actions/PlayerAttackAction.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using DOL.GS.PacketHandler;
+﻿using DOL.GS.PacketHandler;
 using DOL.Language;
 
 namespace DOL.GS
@@ -100,13 +99,7 @@ namespace DOL.GS
                 return false;
             }
 
-            if (base.FinalizeRangedAttack())
-            {
-                _playerOwner.TempProperties.SetProperty(RangeAttackComponent.RANGED_ATTACK_START, GameLoop.GameLoopTime);
-                return true;
-            }
-
-            return false;
+            return base.FinalizeRangedAttack();
         }
     }
 }

GameServer/ECS-Components/AttackComponent.cs
@@ -553,11 +553,6 @@ namespace DOL.GS
             }
         }
 
-        //public double AttackDamage(InventoryItem weapon)
-        //{
-        //    return AttackDamage(weapon, out _);
-        //}
-
         public void RequestStartAttack(GameObject attackTarget)
         {
             if (!StartAttackRequested)
@@ -790,8 +785,6 @@ namespace DOL.GS
                         player.Out.SendAttackMode(AttackState);
                     else
                     {
-                        player.TempProperties.SetProperty(RangeAttackComponent.RANGED_ATTACK_START, GameLoop.GameLoopTime);
-
                         string typeMsg = attackWeapon.Object_Type == (int) eObjectType.Thrown ? "throw" : "shot";
                         string targetMsg;
 
@@ -830,12 +823,6 @@ namespace DOL.GS
                 owner.CancelEngageEffect();
 
             AttackState = true;
-            DbInventoryItem attackWeapon = owner.ActiveWeapon;
-
-            int speed = AttackSpeed(attackWeapon);
-
-            if (speed <= 0)
-                return false;
 
             // NPCs aren't allowed to prepare their ranged attack while moving or out of range.
             if (owner is GameNPC npcOwner && owner.ActiveWeaponSlot == eActiveWeaponSlot.Distance)
@@ -850,23 +837,11 @@ namespace DOL.GS
 
             if (owner.ActiveWeaponSlot == eActiveWeaponSlot.Distance)
             {
-                // Only start another attack action if we aren't already aiming to shoot.
-                if (owner.rangeAttackComponent.RangedAttackState != eRangedAttackState.Aim)
-                {
-                    if (attackAction.CheckInterruptTimer())
-                        return false;
+                if (owner.rangeAttackComponent.RangedAttackState != eRangedAttackState.Aim && attackAction.CheckInterruptTimer())
+                    return false;
 
-                    owner.rangeAttackComponent.RangedAttackState = eRangedAttackState.Aim;
-
-                    if (owner is not GamePlayer || !owner.effectListComponent.ContainsEffectForEffectType(eEffect.Volley))
-                    {
-                        // The 'stance' parameter appears to be used to tell whether or not the animation should be held, and doesn't seem to be related to the weapon speed.
-                        foreach (GamePlayer player in owner.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
-                            player.Out.SendCombatAnimation(owner, null, (ushort)(attackWeapon != null ? attackWeapon.Model : 0), 0, player.Out.BowPrepare, 0x1A, 0x00, 0x00);
-                    }
-
-                    attackAction.StartTime = owner.rangeAttackComponent?.RangedAttackType == eRangedAttackType.RapidFire ? Math.Max(1500, speed / 2) : speed;
-                }
+                Console.WriteLine(GameLoop.GameLoopTime - attackAction.NextTick);
+                owner.rangeAttackComponent.AttackStartTime = GameLoop.GameLoopTime;
             }
 
             return true;
@@ -878,9 +853,9 @@ namespace DOL.GS
             npc.TargetObject = attackTarget;
             npc.StopMovingOnPath();
 
-            if (npc.Brain != null && npc.Brain is IControlledBrain)
+            if (npc.Brain is IControlledBrain brain)
             {
-                if ((npc.Brain as IControlledBrain).AggressionState == eAggressionState.Passive)
+                if (brain.AggressionState == eAggressionState.Passive)
                     return;
             }
 
@@ -898,6 +873,9 @@ namespace DOL.GS
 
         public void StopAttack()
         {
+            AttackAction attackAction = owner.attackComponent.attackAction;
+            attackAction?.ResetNextTick();
+
             if (owner.ActiveWeaponSlot == eActiveWeaponSlot.Distance)
             {
                 // Only cancel the animation if the ranged ammo isn't released already.
@@ -907,8 +885,14 @@ namespace DOL.GS
                         player.Out.SendInterruptAnimation(owner);
                 }
 
-                owner.rangeAttackComponent.RangedAttackState = eRangedAttackState.None;
-                owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
+                RangeAttackComponent rangeAttackComponent = owner.rangeAttackComponent;
+                rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
+
+                if (rangeAttackComponent.RangedAttackState != eRangedAttackState.None)
+                {
+                    attackAction.DelayNextTick(500);
+                    rangeAttackComponent.RangedAttackState = eRangedAttackState.None;
+                }
             }
 
             AttackState = false;

GameServer/ECS-Components/RangeAttackComponent.cs
@@ -14,7 +14,6 @@ namespace DOL.GS
             m_owner = owner;
         }
 
-        internal const string RANGED_ATTACK_START = "RangedAttackStart";
         public const int DEFAULT_ENDURANCE_COST = 5;
         public const int CRITICAL_SHOT_ENDURANCE_COST = 10;
         public const int VOLLEY_ENDURANCE_COST = 15;
@@ -24,6 +23,7 @@ namespace DOL.GS
         public eRangedAttackState RangedAttackState { get; set; }
         public eRangedAttackType RangedAttackType { get; set; }
         public eActiveQuiverSlot ActiveQuiverSlot { get; set; }
+        public long AttackStartTime { get; set; }
         public DbInventoryItem Ammo { get; private set; }
         public bool IsAmmoCompatible { get; private set; }
 
@@ -80,18 +80,13 @@ namespace DOL.GS
         /// </summary>
         public eCheckRangeAttackStateResult CheckRangeAttackState(GameObject target)
         {
+            // Failsafe, but it should never happen.
+            if (AttackStartTime == 0)
+                AttackStartTime = GameLoop.GameLoopTime;
+
             if (m_owner is GamePlayer playerOwner)
             {
-                long attackStart = m_owner.TempProperties.GetProperty<long>(RANGED_ATTACK_START);
-
-                // Failsafe, but it should never happen.
-                if (attackStart == 0)
-                {
-                    attackStart = GameLoop.GameLoopTime;
-                    playerOwner.TempProperties.SetProperty(RANGED_ATTACK_START, attackStart);
-                }
-
-                if ((GameLoop.GameLoopTime - attackStart) > MAX_DRAW_DURATION && playerOwner.ActiveWeapon.Object_Type != (int)eObjectType.Crossbow)
+                if ((GameLoop.GameLoopTime - AttackStartTime) > MAX_DRAW_DURATION && playerOwner.ActiveWeapon.Object_Type != (int)eObjectType.Crossbow)
                 {
                     playerOwner.Out.SendMessage(LanguageMgr.GetTranslation(playerOwner.Client.Account.Language, "GamePlayer.Attack.TooTired"), eChatType.CT_System, eChatLoc.CL_SystemWindow);
                     return eCheckRangeAttackStateResult.Stop;

GameServer/commands/gmcommands/GMinfo.cs
@@ -276,8 +276,8 @@ namespace DOL.GS.Commands
 						info.Add("RegionTick: " + GameLoop.GameLoopTime);
 						if(target.attackComponent.attackAction != null)
 						{
-							info.Add("AttackAction StartTime " + target.attackComponent.attackAction.StartTime);
-							info.Add("AttackAction TimeUntilStart " + (target.attackComponent.attackAction.StartTime - GameLoop.GameLoopTime));
+							info.Add("AttackAction NextTick " + target.attackComponent.attackAction.NextTick);
+							info.Add("AttackAction TimeUntilStart " + (target.attackComponent.attackAction.NextTick - GameLoop.GameLoopTime));
 						}
 					}
 

GameServer/gameobjects/GameLiving.cs
@@ -1165,7 +1165,7 @@ namespace DOL.GS
 					return false;
 			}
 
-			long rangeAttackHoldStart = TempProperties.GetProperty<long>(RangeAttackComponent.RANGED_ATTACK_START);
+			long rangeAttackHoldStart = rangeAttackComponent.AttackStartTime;
 
 			if (rangeAttackHoldStart > 0)
 			{
@@ -2544,10 +2544,8 @@ namespace DOL.GS
 			if (Inventory == null)
 				return;
 
-            //Clean up range attack variables, no matter to what
-            //weapon we switch
-            rangeAttackComponent.RangedAttackState = eRangedAttackState.None;
-            rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
+			rangeAttackComponent.RangedAttackState = eRangedAttackState.None;
+			rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
 
 			DbInventoryItem rightHandSlot = Inventory.GetItem(eInventorySlot.RightHandWeapon);
 			DbInventoryItem leftHandSlot = Inventory.GetItem(eInventorySlot.LeftHandWeapon);
@@ -2558,7 +2556,6 @@ namespace DOL.GS
 			// 0=right hand, 1=left hand, 2=two-hand, 3=range, F=none
 			int rightHand = (VisibleActiveWeaponSlots & 0x0F);
 			int leftHand = (VisibleActiveWeaponSlots & 0xF0) >> 4;
-
 
 			// set new active weapon slot
 			switch (slot)

GameServer/gameobjects/GameNPC.cs
@@ -1493,6 +1493,9 @@ namespace DOL.GS
 		/// <param name="slot">the new eActiveWeaponSlot</param>
 		public override void SwitchWeapon(eActiveWeaponSlot slot)
 		{
+			if (ActiveWeaponSlot == slot)
+				return;
+
 			base.SwitchWeapon(slot);
 			if (ObjectState == eObjectState.Active)
 			{

GameServer/gameobjects/GamePlayer.cs
@@ -5984,25 +5984,13 @@ namespace DOL.GS
         /// <param name="slot">the new eActiveWeaponSlot</param>
         public override void SwitchWeapon(eActiveWeaponSlot slot)
         {
-            //When switching weapons, attackmode is removed!
             if (attackComponent != null && attackComponent.AttackState && ActiveWeapon != null)
-            {
-                if (ActiveWeapon.Item_Type == (int)eInventorySlot.DistanceWeapon 
-                    && rangeAttackComponent.RangedAttackState != eRangedAttackState.None 
-                    && GameLoop.GameLoopTime - this.TempProperties.GetProperty<long>(RangeAttackComponent.RANGED_ATTACK_START) > 100
-                    && attackComponent.attackAction != null)
-                {
-                    attackComponent.attackAction.StartTime = 1000;
-                }
                 attackComponent.StopAttack();
-            }
 
             if (effectListComponent.ContainsEffectForEffectType(eEffect.Volley))
             {
-                AtlasOF_VolleyECSEffect volley = (AtlasOF_VolleyECSEffect)EffectListService.GetEffectOnTarget(this, eEffect.Volley);
-
-                if (volley != null)
-                    volley.OnPlayerSwitchedWeapon();
+                AtlasOF_VolleyECSEffect volley = (AtlasOF_VolleyECSEffect) EffectListService.GetEffectOnTarget(this, eEffect.Volley);
+                volley?.OnPlayerSwitchedWeapon();
             }
 
             if (CurrentSpellHandler != null)
@@ -9608,10 +9596,11 @@ namespace DOL.GS
                 if (attackComponent.AttackState && ActiveWeaponSlot != eActiveWeaponSlot.Distance)
                 {
                     AttackData ad = TempProperties.GetProperty<AttackData>(LAST_ATTACK_DATA, null);
+
                     if (ad != null && ad.IsMeleeAttack && (ad.AttackResult == eAttackResult.TargetNotVisible || ad.AttackResult == eAttackResult.OutOfRange))
                     {
-                        if (ad.Target != null && IsObjectInFront(ad.Target, 120) && this.IsWithinRadius(ad.Target, attackComponent.AttackRange) && attackComponent.attackAction != null)
-                            attackComponent.attackAction.StartTime = 0;
+                        if (ad.Target != null && IsObjectInFront(ad.Target, 120) && IsWithinRadius(ad.Target, attackComponent.AttackRange) && attackComponent.attackAction != null)
+                            attackComponent.attackAction.ResetNextTick();
                     }
                 }
             }
@@ -9982,7 +9971,7 @@ namespace DOL.GS
                     if (ad != null && ad.IsMeleeAttack && (ad.AttackResult == eAttackResult.TargetNotVisible || ad.AttackResult == eAttackResult.OutOfRange))
                     {
                         if (ad.Target != null && IsObjectInFront(ad.Target, 120) && IsWithinRadius(ad.Target, attackComponent.AttackRange) && attackComponent.attackAction != null)
-                            attackComponent.attackAction.StartTime = 0;
+                            attackComponent.attackAction.ResetNextTick();
                     }
                 }
             }

GameServer/scripts/namedmobs/Albion Named Mobs/Classic Zones/WrathOfMordred.cs
@@ -192,7 +192,7 @@ namespace DOL.AI.Brain
 					{
 						AttackAction attackAction = Body.attackComponent.attackAction;
 
-						if (attackAction != null && attackAction.StartTime - GameLoop.GameLoopTime <= 800 && CanWalk == false)
+						if (attackAction != null && attackAction.NextTick - GameLoop.GameLoopTime <= 800 && CanWalk == false)
 						{
 							Body.styleComponent.NextCombatStyle = null;
 							Body.styleComponent.NextCombatBackupStyle = null;