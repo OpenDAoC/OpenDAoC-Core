OpenDAoC/OpenDAoC-Core

commit f82cf8f8dcc5672ca5d26af5c7935a23f99ac4a0

Fix various issues with inactivity timeout * Fixes wrong usage of `KICK_IDLE_PLAYER_TIME` (caused by 490aa7c). * Fixes wrong timestamp being used. We want to know when was the last time the player moved or performed an action (caused by 490aa7c, but previously only the current position against the DB's values was checked). * Added `LastPlayerActivityTime` for this purpose. It's set when the player moves (or is moved), or when it attacks. It's still not perfect but active players that don't move should still be considered active. * Prevented GMs and admins from being kicked due to inactivity. * Moved logic to `ClientService`. If wasn't checked at all if saves were disabled.

GameServer/packets/Client/168/PlayerPositionUpdateHandler.cs
@@ -31,7 +31,7 @@ namespace DOL.GS.PacketHandler.Client.v168
 
         public void HandlePacket(GameClient client, GSPacketIn packet)
         {
-            if (!client.OnUpdatePosition())
+            if (!client.Player.OnUpdatePosition())
                 return;
 
             //Tiv: in very rare cases client send 0xA9 packet before sending S<=C 0xE8 player world initialize
@@ -84,8 +84,7 @@ namespace DOL.GS.PacketHandler.Client.v168
         private static void HandlePacketSince1124(GameClient client, GSPacketIn packet)
         {
             //Tiv: in very rare cases client send 0xA9 packet before sending S<=C 0xE8 player world initialize
-            if ((client.Player.ObjectState != GameObject.eObjectState.Active) ||
-                (client.ClientState != GameClient.eClientState.Playing))
+            if ((client.Player.ObjectState != GameObject.eObjectState.Active) || (client.ClientState != GameClient.eClientState.Playing))
                 return;
 
             long environmentTick = GameLoop.GameLoopTime;
@@ -203,7 +202,7 @@ namespace DOL.GS.PacketHandler.Client.v168
 
             int coordsPerSec = 0;
             int jumpDetect = 0;
-            long timediff = GameLoop.GameLoopTime - client.Player.LastPositionUpdateTick;
+            long timediff = GameLoop.GameLoopTime - client.Player.LastPositionUpdateTime;
             int distance = 0;
 
             if (timediff > 0)
@@ -216,7 +215,10 @@ namespace DOL.GS.PacketHandler.Client.v168
                     jumpDetect = (int) (z - client.Player.LastPositionUpdatePoint.Z);
             }
 
-            client.Player.LastPositionUpdateTick = GameLoop.GameLoopTime;
+            if (distance > 0)
+                client.Player.LastPlayerActivityTime = GameLoop.GameLoopTime;
+
+            client.Player.LastPositionUpdateTime = GameLoop.GameLoopTime;
             client.Player.LastPositionUpdatePoint.X = x;
             client.Player.LastPositionUpdatePoint.Y = y;
             client.Player.LastPositionUpdatePoint.Z = z;
@@ -736,7 +738,7 @@ namespace DOL.GS.PacketHandler.Client.v168
 
             int coordsPerSec = 0;
             int jumpDetect = 0;
-            int timediff = (int) (GameLoop.GameLoopTime - client.Player.LastPositionUpdateTick);
+            int timediff = (int) (GameLoop.GameLoopTime - client.Player.LastPositionUpdateTime);
             int distance = 0;
 
             if (timediff > 0)
@@ -749,7 +751,10 @@ namespace DOL.GS.PacketHandler.Client.v168
                     jumpDetect = realZ - (int) client.Player.LastPositionUpdatePoint.Z;
             }
 
-            client.Player.LastPositionUpdateTick = GameLoop.GameLoopTime;
+            if (distance > 0)
+                client.Player.LastPlayerActivityTime = GameLoop.GameLoopTime;
+
+            client.Player.LastPositionUpdateTime = GameLoop.GameLoopTime;
             client.Player.LastPositionUpdatePoint.X = realX;
             client.Player.LastPositionUpdatePoint.Y = realY;
             client.Player.LastPositionUpdatePoint.Z = realZ;

GameServer/ECS-Services/ClientService.cs
@@ -62,12 +62,12 @@ namespace DOL.GS
                     return;
                 case GameClient.eClientState.CharScreen:
                 {
-                    CheckPingTimeout(client);
+                    CheckCharScreenTimeout(client);
                     break;
                 }
                 case GameClient.eClientState.Playing:
                 {
-                    CheckPositionUpdateTimeout(client);
+                    CheckInGameTimeout(client);
                     GamePlayer player = client.Player;
 
                     if (player?.ObjectState == GameObject.eObjectState.Active)
@@ -531,17 +531,16 @@ namespace DOL.GS
                 CreateObjectForPlayer(player, gameObject);
         }
 
-        private static void CheckPingTimeout(GameClient client)
+        private static void CheckCharScreenTimeout(GameClient client)
         {
             if (ServiceUtils.ShouldTickNoEarly(client.PingTime + PING_TIMEOUT))
             {
-                if (log.IsWarnEnabled)
-                    log.Warn($"Ping timeout for client {client}");
+                if (log.IsInfoEnabled)
+                    log.Info($"Ping timeout on client. Disconnecting. ({client})");
 
                 GameServer.Instance.Disconnect(client);
             }
         }
-
         private static void CheckHardTimeout(GameClient client)
         {
             if (ServiceUtils.ShouldTickNoEarly(client.PingTime + HARD_TIMEOUT))
@@ -553,14 +552,26 @@ namespace DOL.GS
             }
         }
 
-        private static void CheckPositionUpdateTimeout(GameClient client)
+        private static void CheckInGameTimeout(GameClient client)
         {
-            if (ServiceUtils.ShouldTickNoEarly(client.PositionUpdateTime + POSITION_UPDATE_TIMEOUT) && !client.Player.HasLinkDeathTimerActive)
+            if (client.Player.HasLinkDeathTimerActive)
+                return;
+
+            if (ServiceUtils.ShouldTickNoEarly(client.Player.LastPositionUpdateTime + POSITION_UPDATE_TIMEOUT))
             {
-                if (log.IsWarnEnabled)
-                    log.Warn($"Position update timeout for client {client}");
+                if (log.IsInfoEnabled)
+                    log.Info($"Position update timeout on client. Calling link death. ({client})");
 
                 client.OnLinkDeath(true);
+            }
+            else if (Properties.KICK_IDLE_PLAYER_STATUS &&
+                     ServiceUtils.ShouldTickNoEarly(client.Player.LastPlayerActivityTime + Properties.KICK_IDLE_PLAYER_TIME * 60000) &&
+                     client.Account.PrivLevel == 1)
+            {
+                if (log.IsInfoEnabled)
+                    log.Info($"Kicking inactive client to char screen. ({client})");
+
+                ServiceUtils.KickPlayerToCharScreen(client.Player);
             }
         }
 

GameServer/GameClient.cs
@@ -287,7 +287,6 @@ namespace DOL.GS
 				    (oldState != eClientState.CharScreen && value == eClientState.CharScreen))
 				{
 					PingTime = GameLoop.GameLoopTime;
-					PositionUpdateTime = GameLoop.GameLoopTime;
 				}
 
 				m_clientState = value;
@@ -299,8 +298,6 @@ namespace DOL.GS
 		/// When the linkdeath occured. 0 if there wasn't any
 		/// </summary>
 		public long LinkDeathTime { get; set; }
-
-		public long PositionUpdateTime { get; set; }
 
 		public GSPacketIn LastPositionUpdatePacketReceived { get; set; }
 
@@ -661,36 +658,13 @@ namespace DOL.GS
 		{
 			try
 			{
-				if (m_player != null)
-				{
-					//<**loki**>
-					if (Properties.KICK_IDLE_PLAYER_STATUS)
-					{
-						if (ServiceUtils.ShouldTickNoEarly(PositionUpdateTime + Properties.KICK_IDLE_PLAYER_TIME))
-						{
-							ServiceUtils.KickPlayerToCharScreen(m_player);
-
-							if (log.IsInfoEnabled)
-								log.Info($"Player {m_player.Name} kicked due to inactivity.");
-						}
-						else
-							m_player.SaveIntoDatabase();
-					}
-					else
-						m_player.SaveIntoDatabase();
-				}
+				m_player?.SaveIntoDatabase();
 			}
 			catch (Exception e)
 			{
 				if (log.IsErrorEnabled)
 					log.Error("SavePlayer", e);
 			}
-		}
-
-		public bool OnUpdatePosition()
-		{
-			PositionUpdateTime = GameLoop.GameLoopTime;
-			return m_player.OnUpdatePosition();
 		}
 
 		/// <summary>

GameServer/gameobjects/GamePlayer.cs
@@ -6109,6 +6109,12 @@ namespace DOL.GS
             }
         }
 
+        public override void OnAttackEnemy(AttackData ad)
+        {
+            LastPlayerActivityTime = GameLoop.GameLoopTime;
+            base.OnAttackEnemy(ad);
+        }
+
         /// <summary>
         /// This method is called at the end of the attack sequence to
         /// notify objects if they have been attacked/hit by an attack
@@ -6277,11 +6283,7 @@ namespace DOL.GS
                 CraftTimer = null;
                 Out.SendCloseTimerWindow();
             }
-
-
-
         }
-
 
         /// <summary>
         /// Launch any reactive effect on an item
@@ -8961,17 +8963,15 @@ namespace DOL.GS
 
             RefreshItemBonuses();
 
-            var playerDeck = DOLDB<DbCoreCharacterXDeck>.SelectObject(DB.Column("DOLCharactersObjectId").IsEqualTo(this.ObjectId));
-            if (playerDeck != null)
-            {
-                this.RandomNumberDeck.LoadDeckFromJSON((playerDeck.Deck));
-                //Console.WriteLine($"loaded deck. first card: {this.RandomNumberDeck.GetInt()}");
-            }
-            else
-            {
-                this.RandomNumberDeck = new PlayerDeck();
-            }
+            DbCoreCharacterXDeck playerDeck = DOLDB<DbCoreCharacterXDeck>.SelectObject(DB.Column("DOLCharactersObjectId").IsEqualTo(ObjectId));
 
+            if (playerDeck != null)
+                RandomNumberDeck.LoadDeckFromJSON(playerDeck.Deck);
+            else
+                RandomNumberDeck = new PlayerDeck();
+
+            LastPositionUpdateTime = GameLoop.GameLoopTime;
+            LastPlayerActivityTime = GameLoop.GameLoopTime;
             return true;
         }
 
@@ -9098,8 +9098,6 @@ namespace DOL.GS
             }
 
             bool hasPetToMove = false;
-            // Remove the last update tick property to prevent speedhack messages during zoning and teleporting.
-            LastPositionUpdateTick = 0;
 
             if (ControlledBrain != null && ControlledBrain.WalkState != eWalkState.Stay)
             {
@@ -9508,42 +9506,10 @@ namespace DOL.GS
             }
         }
 
-        /// <summary>
-        /// Holds the zone player was in after last position update
-        /// </summary>
-        private Zone m_lastPositionUpdateZone;
-
-        /// <summary>
-        /// Gets or sets the zone after last position update
-        /// </summary>
-        public Zone LastPositionUpdateZone
-        {
-            get { return m_lastPositionUpdateZone; }
-            set { m_lastPositionUpdateZone = value; }
-        }
-
-
-        private long m_lastPositionUpdateTick = 0;
-
-        /// <summary>
-        /// The environment tick count when this players position was last updated
-        /// </summary>
-        public long LastPositionUpdateTick
-        {
-            get { return m_lastPositionUpdateTick; }
-            set { m_lastPositionUpdateTick = value; }
-        }
-
-        private Point3DFloat m_lastPositionUpdatePoint = new Point3DFloat(0, 0, 0);
-
-        /// <summary>
-        /// The last recorded position of this player
-        /// </summary>
-        public Point3DFloat LastPositionUpdatePoint
-        {
-            get { return m_lastPositionUpdatePoint; }
-            set { m_lastPositionUpdatePoint = value; }
-        }
+        public Zone LastPositionUpdateZone { get; set; }
+        public long LastPositionUpdateTime { get; set; }
+        public long LastPlayerActivityTime { get; set; }
+        public Point3DFloat LastPositionUpdatePoint { get; set; } = new(0, 0, 0);
 
         /// <summary>
         /// Holds the players max Z for fall damage

commit f9b02288c743e948cdf2b70c709d609f4b06bc78

`PlayerPositionUpdateHandler`: Fix wrong player state Caused by f82cf8f8

GameServer/packets/Client/168/PlayerPositionUpdateHandler.cs
@@ -389,13 +389,13 @@ namespace DOL.GS.PacketHandler.Client.v168
             // if player has a pet summoned, player action is sent by client as 0x04, but sending to other players this is skipped
             client.Player.IsDiving = (action & 0x02) != 0;
 
-            state = (ushort) ((state >> 10) & 7);
-            client.Player.IsClimbing = state == 7;
-            client.Player.IsSwimming = state == 1;
+            int state2 = (state >> 10) & 7;
+            client.Player.IsClimbing = state2 == 7;
+            client.Player.IsSwimming = state2 == 1;
 
             //int status = (data & 0x1FF ^ data) >> 8;
             //int fly = (flyingflag & 0x1FF ^ flyingflag) >> 8;
-            if (state == 3 && client.Player.TempProperties.GetProperty(GamePlayer.DEBUG_MODE_PROPERTY, false) == false && !client.Player.IsAllowedToFly) //debugFly on, but player not do /debug on (hack)
+            if (state2 == 3 && client.Player.TempProperties.GetProperty(GamePlayer.DEBUG_MODE_PROPERTY, false) == false && !client.Player.IsAllowedToFly) //debugFly on, but player not do /debug on (hack)
             {
                 StringBuilder builder = new();
                 builder.Append("HACK_FLY");