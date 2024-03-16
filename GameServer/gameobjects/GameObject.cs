5dc82fb82956e991ef2cb95d91f7bf9c0b0f8a1e
Baptiste Marie <bm01@users.noreply.github.com>
2024-03-16 09:04:52 +0100

    `AddToWorld`: Add region validation back

--- GameServer/gameobjects/GameObject.cs
+++ GameServer/gameobjects/GameObject.cs
@@ -1,22 +1,3 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
-
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -24,7 +5,6 @@ using System.Linq;
 using System.Text;
 using DOL.Database;
 using DOL.Events;
-using DOL.GS;
 using DOL.GS.Housing;
 using DOL.GS.PacketHandler;
 using DOL.GS.Quests;
@@ -702,11 +682,21 @@ namespace DOL.GS
 			if (m_ObjectState == eObjectState.Active)
 				return false;
 
+			if (CurrentRegion == null)
+			{
+				if (log.IsWarnEnabled)
+					log.Warn($"Invalid region for ({this})");
+
+				return false;
+			}
+
 			Zone zone = CurrentRegion.GetZone(X, Y);
 
 			if (zone == null)
 			{
-				log.Warn($"Couldn't find a zone for (Name: {Name}) (ID: {InternalID})");
+				if (log.IsWarnEnabled)
+					log.Warn($"Couldn't find a zone for ({this})");
+
 				return false;
 			}
 
