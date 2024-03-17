OpenDAoC/OpenDAoC-Core

commit a1e2d080550af8ac561cc1f75645ba2020360ebd

Prevent superfluous NPC position broadcasts

GameServer/ECS-Components/NpcMovementComponent.cs
@@ -26,7 +26,6 @@ namespace DOL.GS
         private MovementRequest _movementRequest;
         private PathCalculator _pathCalculator;
         private AuxECSGameTimer _resetHeadingAction;
-        private int _firstMove;
         private Point3D _positionForUpdatePackets;
         private bool _needsBroadcastUpdate;
 
@@ -288,13 +287,11 @@ namespace DOL.GS
         private void UpdateVelocity(double distanceToTarget)
         {
             MovementStartTick = GameLoop.GameLoopTime;
-            _needsBroadcastUpdate = true;
 
             if (!IsMoving || distanceToTarget < 1)
             {
                 Velocity = Vector3.Zero;
                 HorizontalVelocityForClient = 0.0;
-                _firstMove = 0;
                 return;
             }
 
@@ -318,7 +315,6 @@ namespace DOL.GS
 
             Velocity = new(velocityX, velocityY, velocityZ);
             HorizontalVelocityForClient = Math.Sqrt(velocityX * velocityX + velocityY * velocityY);
-            _firstMove++;
             return;
         }
 
@@ -497,11 +493,9 @@ namespace DOL.GS
                         MoveToNextWaypoint();
                         return;
                     }
-                    else
-                    {
-                        _movementState |= MovementState.AT_WAYPOINT;
-                        _stopAtWaypointUntil = GameLoop.GameLoopTime + CurrentWaypoint.WaitTime * 100;
-                    }
+
+                    _movementState |= MovementState.AT_WAYPOINT;
+                    _stopAtWaypointUntil = GameLoop.GameLoopTime + CurrentWaypoint.WaitTime * 100;
                 }
                 else
                     StopMovingOnPath();
@@ -610,9 +604,17 @@ namespace DOL.GS
             Owner.Z = Owner.Z;
 
             if (destination == null || distanceToTarget < 1)
+            {
+                if (!IsAtDestination)
+                    _needsBroadcastUpdate = true;
+
                 IsDestinationValid = false;
+            }
             else
             {
+                if (CurrentSpeed != speed || !Destination.IsSamePosition(destination))
+                    _needsBroadcastUpdate = true;
+
                 Destination = destination;
                 IsDestinationValid = true;
             }

commit 239b013569fc2d1abb9564762a614de2e5e678d2

Fix NPCs continuing to turn client side after a CC Likely to be caused by a1e2d080. Made modifications to `_turningDisabledCount` thread safe.

GameServer/ECS-Components/MovementComponent.cs
@@ -1,4 +1,6 @@
-﻿namespace DOL.GS
+﻿using System.Threading;
+
+namespace DOL.GS
 {
     public class MovementComponent
     {
@@ -13,7 +15,7 @@
         public short MaxSpeedBase { get; set; } // Currently unused for players.
         public virtual short MaxSpeed => (short) Owner.GetModified(eProperty.MaxSpeed);
         public bool IsMoving => CurrentSpeed > 0;
-        public bool IsTurningDisabled => _turningDisabledCount > 0 && !Owner.effectListComponent.ContainsEffectForEffectType(eEffect.SpeedOfSound);
+        public bool IsTurningDisabled => Interlocked.CompareExchange(ref _turningDisabledCount, 0, 0) > 0 && !Owner.effectListComponent.ContainsEffectForEffectType(eEffect.SpeedOfSound);
 
         protected MovementComponent(GameLiving owner)
         {
@@ -42,12 +44,9 @@
         public virtual void DisableTurning(bool add)
         {
             if (add)
-                _turningDisabledCount++;
+                Interlocked.Increment(ref _turningDisabledCount);
             else
-                _turningDisabledCount--;
-
-            if (_turningDisabledCount < 0)
-                _turningDisabledCount = 0;
+                Interlocked.Decrement(ref _turningDisabledCount);
         }
     }
 }

GameServer/ECS-Components/NpcMovementComponent.cs
@@ -318,6 +318,14 @@ namespace DOL.GS
             Owner.Heading = heading;
         }
 
+        public override void DisableTurning(bool add)
+        {
+            // Trigger an update to make sure the NPC properly starts or stops auto facing client side.
+            // May technically be only necessary if the count is going from 0 to 1 or 1 to 0, but we're skipping that because it would needs to be thread safe.
+            _needsBroadcastUpdate = true;
+            base.DisableTurning(add);
+        }
+
         private void UpdateVelocity(double distanceToTarget)
         {
             MovementStartTick = GameLoop.GameLoopTime;