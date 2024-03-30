0809cf9ad9453c851d80d2c59515c363ac24d852
Baptiste Marie <bm01@users.noreply.github.com>
2024-03-30 05:27:39 +0100

    Make `PlayerTargetHandler` send `ObjectDelete` if the targeted object doesn't exist.

--- GameServer/packets/Client/168/PlayerTargetHandler.cs
+++ GameServer/packets/Client/168/PlayerTargetHandler.cs
@@ -1,22 +1,3 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
-
 namespace DOL.GS.PacketHandler.Client.v168
 {
     [PacketHandlerAttribute(PacketHandlerType.TCP, eClientPackets.PlayerTarget, "Handle Player Target Change.", eClientStatus.PlayerInGame)]
@@ -41,7 +22,7 @@ namespace DOL.GS.PacketHandler.Client.v168
         {
             GameObject target = actionSource.CurrentRegion.GetObject(newTargetId);
 
-            if (target != null && !actionSource.IsWithinRadius(target, WorldMgr.OBJ_UPDATE_DISTANCE))
+            if (!actionSource.IsWithinRadius(target, WorldMgr.OBJ_UPDATE_DISTANCE))
             {
                 actionSource.Out.SendObjectDelete(newTargetId);
                 target = null;
@@ -71,7 +52,7 @@ namespace DOL.GS.PacketHandler.Client.v168
             {
                 if (target is not GameGravestone gravestone || !gravestone.InternalID.Equals(actionSource.InternalID))
                 {
-                    actionSource.Out.SendMessage("You are no longer targetting your grave. Your prayers fail.", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+                    actionSource.Out.SendMessage("You are no longer targeting your grave. Your prayers fail.", eChatType.CT_System, eChatLoc.CL_SystemWindow);
                     actionSource.PrayTimerStop();
                 }
             }
