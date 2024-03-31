OpenDAoC/OpenDAoC-Core

commit 3a1f10e3e972a76554ca01cca49a262f96c33d50

Attempt to smooth NPC movement out * Done by sending fake values to the client. See `NpcMovementComponent.PrepareValuesForClient`. It's very silly and there's probably a better way of doing it, but for now it works quite well. * Fixed vertical movement information missing from object update packet, which led to odd behavior when moving on slopes. * Tweaked follow logic a bit to alleviate janky motion.

GameServer/packets/Server/PacketLib1124.cs
@@ -125,73 +125,88 @@ namespace DOL.GS.PacketHandler
 			if (m_gameClient.Player == null || npc.IsVisibleTo(m_gameClient.Player) == false)
 				return;
 
-			//Added by Suncheck - Mines are not shown to enemy players
+			// Mines are not shown to enemy players.
 			if (npc is GameMine)
 			{
 				if (GameServer.ServerRules.IsAllowedToAttack((npc as GameMine).Owner, m_gameClient.Player, true))
-				{
 					return;
-				}
 			}
 
 			if (npc is GameMovingObject)
-			{
 				SendMovingObjectCreate(npc as GameMovingObject);
-			}
 			else
 			{
 				using (GSTCPPacketOut pak = new GSTCPPacketOut(GetPacketCode(eServerPackets.NPCCreate)))
 				{
-					short speed = 0;
-					ushort speedZ = 0;
-					if (npc == null)
-						return;
-					if (!npc.IsAtTargetPosition)
+					ushort speed = (ushort) npc.movementComponent.HorizontalVelocityForClient;
+					int zSpeed = (int) (npc.movementComponent.Velocity.Z / 4);
+					ushort heading = npc.Heading;
+
+					if (zSpeed < 0)
 					{
-						speed = npc.CurrentSpeed;
-						speedZ = (ushort)npc.movementComponent.TickSpeedZ;
+						zSpeed = -zSpeed;
+						speed |= 0x8000;
 					}
-					pak.WriteShort((ushort)npc.ObjectID);
-					pak.WriteShort((ushort)speed);
-					pak.WriteShort(npc.Heading);
-					pak.WriteShort((ushort)npc.Z);
-					pak.WriteInt((uint)npc.X);
-					pak.WriteInt((uint)npc.Y);
-					pak.WriteShort(speedZ);
+
+					speed |= (ushort) ((zSpeed & 0x70) << 8);
+					heading = (ushort) (((zSpeed & 0x0F) << 12) | (npc.Heading & 0xFFF));
+
+					pak.WriteShort((ushort) npc.ObjectID);
+					pak.WriteShort(speed);
+					pak.WriteShort(heading);
+					pak.WriteShort((ushort) npc.Z);
+					pak.WriteInt((uint) npc.X);
+					pak.WriteInt((uint) npc.Y);
+					pak.WriteShort((ushort) zSpeed);
 					pak.WriteShort(npc.Model);
 					pak.WriteByte(npc.Size);
 					byte level = npc.GetDisplayLevel(m_gameClient.Player);
-					if ((npc.Flags & GameNPC.eFlags.STATUE) != 0)
-					{
-						level |= 0x80;
-					}
-					pak.WriteByte(level);
 
-					byte flags = (byte)(GameServer.ServerRules.GetLivingRealm(m_gameClient.Player, npc) << 6);
-					if ((npc.Flags & GameNPC.eFlags.GHOST) != 0) flags |= 0x01;
-					if (npc.Inventory != null) flags |= 0x02; //If mob has equipment, then only show it after the client gets the 0xBD packet
-					if ((npc.Flags & GameNPC.eFlags.PEACE) != 0) flags |= 0x10;
-					if ((npc.Flags & GameNPC.eFlags.FLYING) != 0) flags |= 0x20;
-					if ((npc.Flags & GameNPC.eFlags.TORCH) != 0) flags |= 0x04;
+					if ((npc.Flags & GameNPC.eFlags.STATUE) != 0)
+						level |= 0x80;
+
+					pak.WriteByte(level);
+					byte flags = (byte) (GameServer.ServerRules.GetLivingRealm(m_gameClient.Player, npc) << 6);
+
+					if ((npc.Flags & GameNPC.eFlags.GHOST) != 0)
+						flags |= 0x01;
+
+					if (npc.Inventory != null)
+						flags |= 0x02; // If mob has equipment, then only show it after the client gets the 0xBD packet.
+
+					if ((npc.Flags & GameNPC.eFlags.PEACE) != 0)
+						flags |= 0x10;
+
+					if ((npc.Flags & GameNPC.eFlags.FLYING) != 0)
+						flags |= 0x20;
+
+					if ((npc.Flags & GameNPC.eFlags.TORCH) != 0)
+						flags |= 0x04;
 
 					pak.WriteByte(flags);
 					pak.WriteByte(0x20); //TODO this is the default maxstick distance
 
 					string add = "";
 					byte flags2 = 0x00;
-					IControlledBrain brain = npc.Brain as IControlledBrain;
 
-					if (brain != null)
-					{
-						flags2 |= 0x80; // have Owner
-					}
+					if (npc.Brain is IControlledBrain)
+						flags2 |= 0x80;
 
 					if ((npc.Flags & GameNPC.eFlags.CANTTARGET) != 0)
-						if (m_gameClient.Account.PrivLevel > 1) add += "-DOR"; // indicates DOR flag for GMs
-						else flags2 |= 0x01;
+					{
+						if (m_gameClient.Account.PrivLevel > 1)
+							add += "-DOR";
+						else
+							flags2 |= 0x01;
+					}
+
 					if ((npc.Flags & GameNPC.eFlags.DONTSHOWNAME) != 0)
-						if (m_gameClient.Account.PrivLevel > 1) add += "-NON"; // indicates NON flag for GMs
-						else flags2 |= 0x02;
+					{
+						if (m_gameClient.Account.PrivLevel > 1)
+							add += "-NON";
+						else
+							flags2 |= 0x02;
+					}
 
 					if ((npc.Flags & GameNPC.eFlags.STEALTH) > 0)
 						flags2 |= 0x04;
@@ -199,41 +214,46 @@ namespace DOL.GS.PacketHandler
 					eQuestIndicator questIndicator = npc.GetQuestIndicator(m_gameClient.Player);
 
 					if (questIndicator == eQuestIndicator.Available)
-						flags2 |= 0x08;//hex 8 - quest available
+						flags2 |= 0x08;
+
 					if (questIndicator == eQuestIndicator.Finish)
-						flags2 |= 0x10;//hex 16 - quest finish
-									   //flags2 |= 0x20;//hex 32 - water mob?
-									   //flags2 |= 0x40;//hex 64 - unknown
-									   //flags2 |= 0x80;//hex 128 - has owner
+						flags2 |= 0x10;
 
+					//flags2 |= 0x20;//hex 32 - water mob?
+					//flags2 |= 0x40;//hex 64 - unknown
+					//flags2 |= 0x80;//hex 128 - has owner
 
-					pak.WriteByte(flags2); // flags 2
+					pak.WriteByte(flags2);
 
 					byte flags3 = 0x00;
+
 					if (questIndicator == eQuestIndicator.Lesson)
 						flags3 |= 0x01;
+
 					if (questIndicator == eQuestIndicator.Lore)
 						flags3 |= 0x02;
-					if (questIndicator == eQuestIndicator.Pending) // new? patch 0031
+
+					if (questIndicator == eQuestIndicator.Pending)
 						flags3 |= 0x20;
+
 					pak.WriteByte(flags3); // new in 1.71 (region instance ID from StoC_0x20) OR flags 3?
 					pak.WriteShort(0x00); // new in 1.71 unknown
 
 					string name = npc.Name;
 					string guildName = npc.GuildName;
 
-					LanguageDataObject translation = LanguageMgr.GetTranslation(m_gameClient, npc);
-					if (translation != null)
+					if (LanguageMgr.GetTranslation(m_gameClient, npc) is DbLanguageGameNpc translation)
 					{
-						if (!string.IsNullOrEmpty(((DbLanguageGameNpc)translation).Name))
-							name = ((DbLanguageGameNpc)translation).Name;
+						if (!string.IsNullOrEmpty(translation.Name))
+							name = translation.Name;
 
-						if (!string.IsNullOrEmpty(((DbLanguageGameNpc)translation).GuildName))
-							guildName = ((DbLanguageGameNpc)translation).GuildName;
+						if (!string.IsNullOrEmpty(translation.GuildName))
+							guildName = translation.GuildName;
 					}
 
-					if (name.Length + add.Length + 2 > 47) // clients crash with too long names
-						name = name.Substring(0, 47 - add.Length - 2);
+					if (name.Length + add.Length + 2 > 47) // Clients crash with too long names.
+						name = name[..(47 - add.Length - 2)];
+
 					if (add.Length > 0)
 						name = string.Format("[{0}]{1}", name, add);
 
@@ -241,7 +261,8 @@ namespace DOL.GS.PacketHandler
 
 					if (guildName.Length > 47)
 						pak.WritePascalString(guildName.Substring(0, 47));
-					else pak.WritePascalString(guildName);
+					else
+						pak.WritePascalString(guildName);
 
 					pak.WriteByte(0x00);
 					SendTCP(pak);

GameServer/packets/Server/PacketLib168.cs
@@ -714,74 +714,87 @@ namespace DOL.GS.PacketHandler
 
 		public virtual void SendObjectUpdate(GameObject obj)
 		{
-			Zone z = obj.CurrentZone;
-
-			if (z == null ||
-				m_gameClient.Player == null ||
-				m_gameClient.Player.IsVisibleTo(obj) == false)
-			{
+			if (m_gameClient.Player == null || !m_gameClient.Player.IsVisibleTo(obj))
 				return;
-			}
 
-			var xOffsetInZone = (ushort)(obj.X - z.XOffset);
-			var yOffsetInZone = (ushort)(obj.Y - z.YOffset);
+			Zone zone = obj.CurrentZone;
+
+			if (zone == null)
+				return;
+
+			ushort xOffsetInZone;
+			ushort yOffsetInZone;
+			ushort z;
 			ushort xOffsetInTargetZone = 0;
 			ushort yOffsetInTargetZone = 0;
 			ushort zOffsetInTargetZone = 0;
 
-			int speed = 0;
-			ushort targetZone = 0;
+			ushort speed = 0;
+			ushort heading;
+			ushort targetZoneSkinId = 0;
 			byte flags = 0;
 			int targetOID = 0;
-			if (obj is GameNPC)
+
+			if (obj is not GameNPC npc)
 			{
-				var npc = obj as GameNPC;
-				flags = (byte)(GameServer.ServerRules.GetLivingRealm(m_gameClient.Player, npc) << 6);
+				xOffsetInZone = (ushort) (obj.X - zone.XOffset);
+				yOffsetInZone = (ushort) (obj.Y - zone.YOffset);
+				z = (ushort) obj.Z;
+				heading = obj.Heading;
+			}
+			else
+			{
+				xOffsetInZone = (ushort) (npc.movementComponent.PositionForClient.X - zone.XOffset);
+				yOffsetInZone = (ushort) (npc.movementComponent.PositionForClient.Y - zone.YOffset);
+				z = (ushort) npc.movementComponent.PositionForClient.Z;
+				flags = (byte) (GameServer.ServerRules.GetLivingRealm(m_gameClient.Player, npc) << 6);
 
 				if (m_gameClient.Account.PrivLevel < 2)
 				{
-					// no name only if normal player
 					if ((npc.Flags & GameNPC.eFlags.CANTTARGET) != 0)
 						flags |= 0x01;
+
 					if ((npc.Flags & GameNPC.eFlags.DONTSHOWNAME) != 0)
 						flags |= 0x02;
 				}
+
 				if ((npc.Flags & GameNPC.eFlags.STATUE) != 0)
-				{
 					flags |= 0x01;
-				}
+
 				if (npc.IsUnderwater)
-				{
 					flags |= 0x10;
-				}
+
 				if ((npc.Flags & GameNPC.eFlags.FLYING) != 0)
-				{
 					flags |= 0x20;
-				}
 
-				if (npc.IsMoving && !npc.IsAtTargetPosition)
+				if (!npc.IsMoving || npc.IsAtDestination)
+					heading = npc.Heading;
+				else
 				{
-					speed = npc.CurrentSpeed;
-					if (npc.IsTargetPositionValid)
+					speed = (ushort) npc.movementComponent.HorizontalVelocityForClient;
+					int zSpeed = (int) (npc.movementComponent.Velocity.Z / 4);
+
+					if (zSpeed < 0)
 					{
-						Zone tz = npc.CurrentRegion.GetZone(npc.TargetPosition.X, npc.TargetPosition.Y);
-						if (tz != null)
-						{
-							xOffsetInTargetZone = (ushort)(npc.TargetPosition.X - tz.XOffset);
-							yOffsetInTargetZone = (ushort)(npc.TargetPosition.Y - tz.YOffset);
-							zOffsetInTargetZone = (ushort)(npc.TargetPosition.Z);
-							//Dinberg:Instances - zoneSkinID for object positioning clientside.
-							targetZone = tz.ZoneSkinID;
-						}
+						zSpeed = -zSpeed;
+						speed |= 0x8000;
 					}
 
-					if (speed > 0x07FF)
+					speed |= (ushort) ((zSpeed & 0x70) << 8);
+					heading = (ushort) (((zSpeed & 0xF) << 12) | (npc.Heading & 0xFFF));
+
+					if (npc.IsDestinationValid)
 					{
-						speed = 0x07FF;
-					}
-					else if (speed < 0)
-					{
-						speed = 0;
+						Zone targetZone = npc.CurrentRegion.GetZone(npc.movementComponent.DestinationForClient.X, npc.movementComponent.DestinationForClient.Y);
+
+						if (targetZone != null)
+						{
+							xOffsetInTargetZone = (ushort) (npc.movementComponent.DestinationForClient.X - targetZone.XOffset);
+							yOffsetInTargetZone = (ushort) (npc.movementComponent.DestinationForClient.Y - targetZone.YOffset);
+							zOffsetInTargetZone = (ushort) npc.movementComponent.DestinationForClient.Z;
+							//Dinberg:Instances - zoneSkinID for object positioning clientside.
+							targetZoneSkinId = targetZone.ZoneSkinID;
+						}
 					}
 				}
 
@@ -796,39 +809,28 @@ namespace DOL.GS.PacketHandler
 
 			using (GSUDPPacketOut pak = new GSUDPPacketOut(GetPacketCode(eServerPackets.ObjectUpdate)))
 			{
-				pak.WriteShort((ushort) speed);
-
-				if (obj is GameNPC)
-				{
-					pak.WriteShort((ushort)(obj.Heading & 0xFFF));
-				}
-				else
-				{
-					pak.WriteShort(obj.Heading);
-				}
+				pak.WriteShort(speed);
+				pak.WriteShort(heading);
 				pak.WriteShort(xOffsetInZone);
 				pak.WriteShort(xOffsetInTargetZone);
 				pak.WriteShort(yOffsetInZone);
 				pak.WriteShort(yOffsetInTargetZone);
-				pak.WriteShort((ushort) obj.Z);
+				pak.WriteShort(z);
 				pak.WriteShort(zOffsetInTargetZone);
 				pak.WriteShort((ushort) obj.ObjectID);
 				pak.WriteShort((ushort) targetOID);
-				//health
+
 				if (obj is GameLiving)
-				{
 					pak.WriteByte((obj as GameLiving).HealthPercent);
-				}
 				else
-				{
 					pak.WriteByte(0);
-				}
+
 				//Dinberg:Instances - zoneskinID for positioning of objects clientside.
-				flags |= (byte) (((z.ZoneSkinID & 0x100) >> 6) | ((targetZone & 0x100) >> 5));
+				flags |= (byte) (((zone.ZoneSkinID & 0x100) >> 6) | ((targetZoneSkinId & 0x100) >> 5));
 				pak.WriteByte(flags);
-				pak.WriteByte((byte) z.ZoneSkinID);
+				pak.WriteByte((byte) zone.ZoneSkinID);
 				//Dinberg:Instances - targetZone already accomodates for this feat.
-				pak.WriteByte((byte) targetZone);
+				pak.WriteByte((byte) targetZoneSkinId);
 				SendUDP(pak);
 			}
 		}
@@ -996,12 +998,13 @@ namespace DOL.GS.PacketHandler
 
 			using (var pak = new GSTCPPacketOut(GetPacketCode(eServerPackets.NPCCreate)))
 			{
+				// Vertical movement may not be shown properly. See `PacketLib1124.SendNPCCreate`.
 				short speed = 0;
 				ushort speedZ = 0;
-				if (npc.IsMoving && !npc.IsAtTargetPosition)
+				if (npc.IsMoving && !npc.IsAtDestination)
 				{
 					speed = npc.CurrentSpeed;
-					speedZ = (ushort) npc.movementComponent.TickSpeedZ;
+					speedZ = (ushort) npc.movementComponent.Velocity.Z;
 				}
 				pak.WriteShort((ushort) npc.ObjectID);
 				pak.WriteShort((ushort) speed);

GameServer/packets/Server/PacketLib171.cs
@@ -144,14 +144,15 @@ namespace DOL.GS.PacketHandler
 
 			using (GSTCPPacketOut pak = new GSTCPPacketOut(GetPacketCode(eServerPackets.NPCCreate)))
 			{
+				// Vertical movement may not be shown properly. See `PacketLib1124.SendNPCCreate`.
 				short speed = 0;
 				ushort speedZ = 0;
 				if (npc == null)
 					return;
-				if (!npc.IsAtTargetPosition)
+				if (!npc.IsAtDestination)
 				{
 					speed = npc.CurrentSpeed;
-					speedZ = (ushort) npc.movementComponent.TickSpeedZ;
+					speedZ = (ushort) npc.movementComponent.Velocity.Z;
 				}
 				pak.WriteShort((ushort)npc.ObjectID);
 				pak.WriteShort((ushort)speed);

GameServer/ECS-Components/MovementComponent.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using static DOL.GS.GameObject;
-
-namespace DOL.GS
+﻿namespace DOL.GS
 {
     public class MovementComponent
     {
@@ -10,20 +7,13 @@ namespace DOL.GS
         private long _nextSubZoneRelocationCheckTick;
         private Point2D _positionDuringLastSubZoneRelocationCheck = new();
         private int _turningDisabledCount;
-        private AuxECSGameTimer _resetHeadingAction;
 
         public GameLiving Owner { get; private set; }
-        public double TickSpeedX { get; private set; }
-        public double TickSpeedY { get; private set; }
-        public double TickSpeedZ { get; private set; }
-        public bool FixedSpeed { get; set; }
         public short CurrentSpeed { get; set; }
         public short MaxSpeedBase { get; set; } // Currently unused for players.
-        public long MovementStartTick { get; set; }
-        public bool IsTurningDisabled => !Owner.effectListComponent.ContainsEffectForEffectType(eEffect.SpeedOfSound) && _turningDisabledCount > 0;
-        public short MaxSpeed => FixedSpeed ? MaxSpeedBase : (short) Owner.GetModified(eProperty.MaxSpeed);
+        public virtual short MaxSpeed => (short) Owner.GetModified(eProperty.MaxSpeed);
         public bool IsMoving => CurrentSpeed > 0;
-        public long MovementElapsedTicks => IsMoving ? GameLoop.GameLoopTime - MovementStartTick : 0;
+        public bool IsTurningDisabled => !Owner.effectListComponent.ContainsEffectForEffectType(eEffect.SpeedOfSound) && _turningDisabledCount > 0;
 
         protected MovementComponent(GameLiving owner)
         {
@@ -58,89 +48,6 @@ namespace DOL.GS
 
             if (_turningDisabledCount < 0)
                 _turningDisabledCount = 0;
-        }
-
-        public virtual void TurnTo(GameObject target, int duration = 0)
-        {
-            if (target == null || target.CurrentRegion != Owner.CurrentRegion)
-                return;
-
-            TurnTo(target.X, target.Y, duration);
-        }
-
-        public virtual void TurnTo(int x, int y, int duration = 0)
-        {
-            TurnTo(Owner.GetHeading(new Point2D(x, y)), duration);
-        }
-
-        public virtual void TurnTo(ushort heading, int duration = 0)
-        {
-            if (Owner.IsStunned || Owner.IsMezzed)
-                return;
-
-            if (Owner.Heading != heading)
-            {
-                if (duration > 0 && _resetHeadingAction == null)
-                {
-                    _resetHeadingAction = new ResetHeadingAction(Owner, this, () => _resetHeadingAction = null);
-                    _resetHeadingAction.Start(duration);
-                }
-
-                Owner.Heading = heading;
-            }
-        }
-
-        protected virtual void UpdateTickSpeed()
-        {
-            if (!IsMoving)
-            {
-                SetTickSpeed(0.0, 0.0, 0.0);
-                return;
-            }
-
-            double heading = Owner.Heading * Point2D.HEADING_TO_RADIAN;
-            double tickSpeed = CurrentSpeed * 0.001;
-            SetTickSpeed(-Math.Sin(heading) * tickSpeed, Math.Cos(heading) * tickSpeed, 0);
-        }
-
-        protected void SetTickSpeed(double x, double y, double z)
-        {
-            TickSpeedX = x;
-            TickSpeedY = y;
-            TickSpeedZ = z;
-        }
-
-        private class ResetHeadingAction : AuxECSGameTimerWrapperBase
-        {
-            private MovementComponent _movementComponent;
-            private ushort _oldHeading;
-            private long _oldMovementStartTick;
-            private Action _onCompletion;
-
-            public ResetHeadingAction(GameObject actionSource, MovementComponent movementComponent, Action onCompletion) : base(actionSource)
-            {
-                _movementComponent = movementComponent;
-                _oldHeading = actionSource.Heading;
-                _oldMovementStartTick = movementComponent.MovementStartTick;
-                _onCompletion = onCompletion;
-            }
-
-            protected override int OnTick(AuxECSGameTimer timer)
-            {
-                GameLiving owner = _movementComponent.Owner;
-
-                if (_oldMovementStartTick == _movementComponent.MovementStartTick &&
-                    !_movementComponent.IsMoving &&
-                    owner.IsAlive &&
-                    owner.ObjectState == eObjectState.Active &&
-                    !owner.attackComponent.AttackState)
-                {
-                    _movementComponent.TurnTo(_oldHeading);
-                }
-
-                _onCompletion();
-                return 0;
-            }
         }
     }
 }

GameServer/ECS-Services/NpcService.cs
@@ -76,12 +76,6 @@ namespace DOL.GS
                     }
 
                     npc.movementComponent.Tick();
-
-                    if (npc.NeedsBroadcastUpdate)
-                    {
-                        ClientService.UpdateObjectForPlayers(npc);
-                        npc.NeedsBroadcastUpdate = false;
-                    }
                 }
                 catch (Exception e)
                 {

GameServer/behaviour/Actions/WalkToAction.cs
@@ -1,27 +1,6 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
 using System;
-using System.Collections.Generic;
-using System.Text;
-using DOL.GS.PacketHandler;
 using DOL.Events;
-using DOL.GS.Behaviour.Attributes;using DOL.GS.Behaviour;
+using DOL.GS.Behaviour.Attributes;
 
 namespace DOL.GS.Behaviour.Actions
 {
@@ -43,7 +22,7 @@ namespace DOL.GS.Behaviour.Actions
         public override void Perform(DOLEvent e, object sender, EventArgs args)
         {
             GamePlayer player = BehaviourUtils.GuessGamePlayerFromNotify(e, sender, args);
-            IPoint3D location = (P is IPoint3D) ? (IPoint3D)P : player;            
+            Point3D location = (P is Point3D) ? P as Point3D : player;            
 
             Q.WalkTo(location, Q.CurrentSpeed);
             

GameServer/gameobjects/GameLiving.cs
@@ -3581,13 +3581,13 @@ namespace DOL.GS
 			}
 		}
 
-		public virtual bool IsTurningDisabled => movementComponent.IsTurningDisabled;
-
 		public virtual short CurrentSpeed
 		{
 			get => movementComponent.CurrentSpeed;
 			set => movementComponent.CurrentSpeed = value;
 		}
+
+		public virtual bool IsTurningDisabled => movementComponent.IsTurningDisabled;
 
 		public virtual short MaxSpeed => movementComponent.MaxSpeed;
 
@@ -3597,65 +3597,11 @@ namespace DOL.GS
 			set => movementComponent.MaxSpeedBase = value;
 		}
 
-		public virtual bool FixedSpeed
-		{
-			set => movementComponent.FixedSpeed = value;
-		}
-
 		public virtual bool IsMoving => movementComponent.IsMoving;
-
-		public long MovementElapsedTicks => movementComponent.MovementElapsedTicks;
-
-		public long MovementStartTick
-		{
-			set => movementComponent.MovementStartTick = value;
-		}
 
 		public virtual void DisableTurning(bool add)
 		{
 			movementComponent.DisableTurning(add);
-		}
-
-		public virtual void TurnTo(ushort heading, int duration = 0)
-		{
-			movementComponent.TurnTo(heading, duration);
-		}
-
-		public virtual void TurnTo(int x, int y, int duration = 0)
-		{
-			movementComponent.TurnTo(x, y, duration);
-		}
-
-		public virtual void TurnTo(GameObject target, int duration = 0)
-		{
-			movementComponent.TurnTo(target, duration);
-		}
-
-		/// <summary>
-		/// The current X position of this living.
-		/// </summary>
-		public override int X
-		{
-			get => IsMoving ? (int) (base.X + MovementElapsedTicks * movementComponent.TickSpeedX) : base.X;
-			set => base.X = value;
-		}
-
-		/// <summary>
-		/// The current Y position of this living.
-		/// </summary>
-		public override int Y
-		{
-			get => IsMoving ? (int) (base.Y + MovementElapsedTicks * movementComponent.TickSpeedY) : base.Y;
-			set => base.Y = value;
-		}
-
-		/// <summary>
-		/// The current Z position of this living.
-		/// </summary>
-		public override int Z
-		{
-			get => IsMoving ? (int) (base.Z + MovementElapsedTicks * movementComponent.TickSpeedZ) : base.Z;
-			set => base.Z = value;
 		}
 
 		/// <summary>

commit 9d870f6eafc18a863749b83a4fdcd15566ba3ed6

Fix clients sometimes seeing NPCs moving at extreme speed on `SendNPCCreate` Caused by 3a1f10e. I assumed the client expected the same values that `SendObjectUpdate` sends for vertical movement, but it doesn't.

GameServer/packets/Server/PacketLib1124.cs
@@ -138,26 +138,22 @@ namespace DOL.GS.PacketHandler
 			{
 				using (GSTCPPacketOut pak = new GSTCPPacketOut(GetPacketCode(eServerPackets.NPCCreate)))
 				{
-					ushort speed = (ushort) npc.movementComponent.HorizontalVelocityForClient;
-					int zSpeed = (int) (npc.movementComponent.Velocity.Z / 4);
-					ushort heading = npc.Heading;
+					short speed = 0;
+					ushort speedZ = 0;
 
-					if (zSpeed < 0)
+					if (npc.IsMoving && !npc.IsAtDestination)
 					{
-						zSpeed = -zSpeed;
-						speed |= 0x8000;
+						speed = npc.CurrentSpeed;
+						speedZ = (ushort) npc.movementComponent.Velocity.Z;
 					}
 
-					speed |= (ushort) ((zSpeed & 0x70) << 8);
-					heading = (ushort) (((zSpeed & 0x0F) << 12) | (npc.Heading & 0xFFF));
-
 					pak.WriteShort((ushort) npc.ObjectID);
-					pak.WriteShort(speed);
-					pak.WriteShort(heading);
+					pak.WriteShort((ushort) speed);
+					pak.WriteShort(npc.Heading);
 					pak.WriteShort((ushort) npc.Z);
 					pak.WriteInt((uint) npc.X);
 					pak.WriteInt((uint) npc.Y);
-					pak.WriteShort((ushort) zSpeed);
+					pak.WriteShort(speedZ);
 					pak.WriteShort(npc.Model);
 					pak.WriteByte(npc.Size);
 					byte level = npc.GetDisplayLevel(m_gameClient.Player);

GameServer/packets/Server/PacketLib168.cs
@@ -998,14 +998,15 @@ namespace DOL.GS.PacketHandler
 
 			using (var pak = new GSTCPPacketOut(GetPacketCode(eServerPackets.NPCCreate)))
 			{
-				// Vertical movement may not be shown properly. See `PacketLib1124.SendNPCCreate`.
 				short speed = 0;
 				ushort speedZ = 0;
+
 				if (npc.IsMoving && !npc.IsAtDestination)
 				{
 					speed = npc.CurrentSpeed;
 					speedZ = (ushort) npc.movementComponent.Velocity.Z;
 				}
+
 				pak.WriteShort((ushort) npc.ObjectID);
 				pak.WriteShort((ushort) speed);
 				pak.WriteShort(npc.Heading);

GameServer/packets/Server/PacketLib171.cs
@@ -144,22 +144,21 @@ namespace DOL.GS.PacketHandler
 
 			using (GSTCPPacketOut pak = new GSTCPPacketOut(GetPacketCode(eServerPackets.NPCCreate)))
 			{
-				// Vertical movement may not be shown properly. See `PacketLib1124.SendNPCCreate`.
 				short speed = 0;
 				ushort speedZ = 0;
-				if (npc == null)
-					return;
-				if (!npc.IsAtDestination)
+
+				if (npc.IsMoving && !npc.IsAtDestination)
 				{
 					speed = npc.CurrentSpeed;
 					speedZ = (ushort) npc.movementComponent.Velocity.Z;
 				}
-				pak.WriteShort((ushort)npc.ObjectID);
-				pak.WriteShort((ushort)speed);
+
+				pak.WriteShort((ushort) npc.ObjectID);
+				pak.WriteShort((ushort) speed);
 				pak.WriteShort(npc.Heading);
-				pak.WriteShort((ushort)npc.Z);
-				pak.WriteInt((uint)npc.X);
-				pak.WriteInt((uint)npc.Y);
+				pak.WriteShort((ushort) npc.Z);
+				pak.WriteInt((uint) npc.X);
+				pak.WriteInt((uint) npc.Y);
 				pak.WriteShort(speedZ);
 				pak.WriteShort(npc.Model);
 				pak.WriteByte(npc.Size);