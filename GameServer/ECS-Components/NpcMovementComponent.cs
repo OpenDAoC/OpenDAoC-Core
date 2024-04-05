OpenDAoC/OpenDAoC-Core

commit a1e2d080550af8ac561cc1f75645ba2020360ebd

Prevent superfluous NPC position broadcasts

GameServer/ECS-Components/NpcMovementComponent.cs
@@ -26,7 +26,6 @@ namespace DOL.GS
         private MovementRequest _movementRequest;
         private PathCalculator _pathCalculator;
         private AuxECSGameTimer _resetHeadingAction;
-        private int _firstMove;
         private Point3D _positionForUpdatePackets;
         private bool _needsBroadcastUpdate;
 
@@ -288,13 +287,11 @@ namespace DOL.GS
         private void UpdateVelocity(double distanceToTarget)
         {
             MovementStartTick = GameLoop.GameLoopTime;
-            _needsBroadcastUpdate = true;
 
             if (!IsMoving || distanceToTarget < 1)
             {
                 Velocity = Vector3.Zero;
                 HorizontalVelocityForClient = 0.0;
-                _firstMove = 0;
                 return;
             }
 
@@ -318,7 +315,6 @@ namespace DOL.GS
 
             Velocity = new(velocityX, velocityY, velocityZ);
             HorizontalVelocityForClient = Math.Sqrt(velocityX * velocityX + velocityY * velocityY);
-            _firstMove++;
             return;
         }
 
@@ -497,11 +493,9 @@ namespace DOL.GS
                         MoveToNextWaypoint();
                         return;
                     }
-                    else
-                    {
-                        _movementState |= MovementState.AT_WAYPOINT;
-                        _stopAtWaypointUntil = GameLoop.GameLoopTime + CurrentWaypoint.WaitTime * 100;
-                    }
+
+                    _movementState |= MovementState.AT_WAYPOINT;
+                    _stopAtWaypointUntil = GameLoop.GameLoopTime + CurrentWaypoint.WaitTime * 100;
                 }
                 else
                     StopMovingOnPath();
@@ -610,9 +604,17 @@ namespace DOL.GS
             Owner.Z = Owner.Z;
 
             if (destination == null || distanceToTarget < 1)
+            {
+                if (!IsAtDestination)
+                    _needsBroadcastUpdate = true;
+
                 IsDestinationValid = false;
+            }
             else
             {
+                if (CurrentSpeed != speed || !Destination.IsSamePosition(destination))
+                    _needsBroadcastUpdate = true;
+
                 Destination = destination;
                 IsDestinationValid = true;
             }

commit eb26a022631a7b6b3bdd7de6cb76077dcaf8bad5

Attempt to fix movement update issues caused by a1e2d08

GameServer/ECS-Components/NpcMovementComponent.cs
@@ -521,7 +521,7 @@ namespace DOL.GS
 
             if (IsReturningToSpawnPoint)
             {
-                UpdateMovement(null, 0.0, 0);
+                SetPositionToDestination();
                 CancelReturnToSpawnPoint();
                 TurnTo(Owner.SpawnHeading);
                 return;
@@ -545,7 +545,15 @@ namespace DOL.GS
             }
 
             if (IsMoving)
+                SetPositionToDestination();
+
+            void SetPositionToDestination()
+            {
+                Owner.X = Destination.X;
+                Owner.Y = Destination.Y;
+                Owner.Z = Destination.Z;
                 UpdateMovement(null, 0.0, 0);
+            }
         }
 
         private void MoveToNextWaypoint()
@@ -648,6 +656,9 @@ namespace DOL.GS
 
             if (destination == null || distanceToTarget < 1)
             {
+                // This appears to be unreliable if we don't forcefully snap the NPC's position to its destination in `OnArrival`.
+                // X, Y, Z are supposed to return the destination. It's possible early ticks prevent that.
+                // We could also simply broadcast, but this would be  wasteful.
                 if (!IsAtDestination)
                     _needsBroadcastUpdate = true;
 

GameServer/ai/ABrain.cs
@@ -48,11 +48,10 @@ namespace DOL.AI
         /// <returns>true if stopped</returns>
         public virtual bool Stop()
         {
+            Body.StopMoving();
+
             if (Body.IsReturningToSpawnPoint)
-            {
-                Body.StopMoving();
                 Body.MoveTo(Body.CurrentRegionID, Body.SpawnPoint.X, Body.SpawnPoint.Y, Body.SpawnPoint.Z, Body.SpawnHeading);
-            }
 
             return EntityManager.Remove(this);
         }