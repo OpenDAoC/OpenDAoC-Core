7bec65f9fb73da0e0ca31da785bc7e17571ee984
Baptiste Marie <bm01@users.noreply.github.com>
2024-04-23 17:32:30 +0200

    Fix /houseface exceptions and lack of feedback when the house doesn't exist

--- GameServer/commands/playercommands/houseface.cs
+++ GameServer/commands/playercommands/houseface.cs
@@ -1,44 +1,40 @@
-using DOL.GS;
 using DOL.GS.Housing;
-using DOL.GS.PacketHandler;
 
 namespace DOL.GS.Commands
 {
-	[CmdAttribute(
-	  "&houseface",
-	  ePrivLevel.Player,
-	  "Points to the specified guildhouse of the guild noted, or the lot number noted in the command. /houseface alone will point to one's personal home.")]
-	public class HouseFaceCommandHandler : AbstractCommandHandler, ICommandHandler
-	{
-		public void OnCommand(GameClient client, string[] args)
-		{
-			int housenumber = 0;
-			if (args.Length > 1)
-			{
-				try
-				{
-					housenumber = int.Parse(args[1]);
-				}
-				catch
-				{
-					DisplaySyntax(client);
-					return;
-				}
-			}
-			else HouseMgr.GetHouseNumberByPlayer(client.Player);
+    [CmdAttribute(
+      "&houseface",
+      ePrivLevel.Player,
+      "Points to the specified guildhouse of the guild noted, or the lot number noted in the command. /houseface alone will point to one's personal home.")]
+    public class HouseFaceCommandHandler : AbstractCommandHandler, ICommandHandler
+    {
+        public void OnCommand(GameClient client, string[] args)
+        {
+            int houseNumber;
 
-			if (housenumber == 0)
-			{
-				DisplayMessage(client, "No house found.");
-				return;
-			}
+            if (args.Length > 1)
+            {
+                if (!int.TryParse(args[1], out houseNumber))
+                {
+                    DisplaySyntax(client);
+                    return;
+                }
+            }
+            else
+                houseNumber = HouseMgr.GetHouseNumberByPlayer(client.Player);
 
-			House house = HouseMgr.GetHouse(housenumber);
+            House house = HouseMgr.GetHouse(houseNumber);
 
-			ushort direction = client.Player.GetHeading(house);
-			client.Player.Heading = direction;
-			client.Out.SendPlayerJump(true);
-			DisplayMessage(client, "You face house " + housenumber);
-		}
-	}
+            if (house == null)
+            {
+                DisplayMessage(client, "No house found.");
+                return;
+            }
+
+            ushort direction = client.Player.GetHeading(house);
+            client.Player.Heading = direction;
+            client.Out.SendPlayerJump(true);
+            DisplayMessage(client, $"You face house {houseNumber}.");
+        }
+    }
 }
