OpenDAoC/OpenDAoC-Core

commit f6c7b5fbfd7129bdf004648733330589686d7f3f

Simplify calls to `GameLiving.Stealth` * Remove `is GamePlayer` checks since `GameLiving` now implements `Stealth`. * Remove `CurrentSpeed` change on `/gmstealth`. * Fix (?) seemingly broken unstealth logic in `MasterLevelBase.OnEffectPulse`.

GameServer/gameobjects/GamePlayer.cs
@@ -6168,14 +6168,14 @@ namespace DOL.GS
                     if (ad.Damage == -1)
                         break;
 
-                    // If attacked by a non-damaging spell, we should not show damage numbers.
-                    // We need to check the damage on the spell here, not in the AD, since this could in theory be a damaging spell that had its damage modified to 0.
-                    if (ad.AttackType == AttackData.eAttackType.Spell && ad.SpellHandler.Spell?.Damage == 0)
-                        break;
-
-                    if (IsStealthed && !effectListComponent.ContainsEffectForEffectType(eEffect.Vanish))
+                    if (ad.AttackType == AttackData.eAttackType.Spell)
                     {
-                        if (!(ad.AttackType == AttackData.eAttackType.Spell && ad.SpellHandler.Spell.SpellType == eSpellType.DamageOverTime))
+                        // If attacked by a non-damaging spell, we should not show damage numbers.
+                        // We need to check the damage on the spell here, not in the AD, since this could in theory be a damaging spell that had its damage modified to 0.
+                        if (ad.SpellHandler.Spell.Damage == 0)
+                            break;
+
+                        if (ad.SpellHandler.Spell.SpellType != eSpellType.DamageOverTime && !effectListComponent.ContainsEffectForEffectType(eEffect.Vanish))
                             Stealth(false);
                     }
 
@@ -9142,7 +9142,7 @@ namespace DOL.GS
                     {
                         if (player != this)
                         {
-                            if (IsStealthed == false || player.CanDetect(this))
+                            if (!IsStealthed || player.CanDetect(this))
                                 player.Out.SendPlayerCreate(this);
                         }
                     }
@@ -9199,12 +9199,10 @@ namespace DOL.GS
             {
                 if (player != null && player != this)
                 {
-                    if (IsStealthed == false || player.CanDetect(this))
-                    {
+                    if (!IsStealthed || player.CanDetect(this))
                         player.Out.SendPlayerCreate(this);
-                    }
 
-                    if (player.IsStealthed == false || CanDetect(player))
+                    if (!player.IsStealthed || CanDetect(player))
                     {
                         Out.SendPlayerCreate(player);
                         Out.SendLivingEquipmentUpdate(player);
@@ -12125,31 +12123,32 @@ namespace DOL.GS
             if (IsStealthed == goStealth)
                 return;
 
-            if (goStealth && CraftTimer != null && CraftTimer.IsAlive)
-            {
-                Out.SendMessage("You can't stealth while crafting!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                return;
-            }
-
-            if (effectListComponent.ContainsEffectForEffectType(eEffect.Pulse))
-            {
-                Out.SendMessage("You currently have an active, pulsing spell effect and cannot hide!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                return;
-            }
-
-            if (IsOnHorse || IsSummoningMount)
-                IsOnHorse = false;
-
             if (goStealth)
-                new StealthECSGameEffect(new ECSGameEffectInitParams(this, 0, 1));
-            else
             {
-                if (effectListComponent.ContainsEffectForEffectType(eEffect.Stealth))
-                    EffectService.RequestImmediateCancelEffect(EffectListService.GetEffectOnTarget(this, eEffect.Stealth), false);
+                if (CraftTimer != null && CraftTimer.IsAlive)
+                {
+                    Out.SendMessage("You can't stealth while crafting!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+                    return;
+                }
 
-                if (effectListComponent.ContainsEffectForEffectType(eEffect.Vanish))
-                    EffectService.RequestImmediateCancelEffect(EffectListService.GetEffectOnTarget(this, eEffect.Vanish));
+                if (effectListComponent.ContainsEffectForEffectType(eEffect.Pulse))
+                {
+                    Out.SendMessage("You currently have an active, pulsing spell effect and cannot hide!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+                    return;
+                }
+
+                if (IsOnHorse || IsSummoningMount)
+                    IsOnHorse = false;
+
+                new StealthECSGameEffect(new ECSGameEffectInitParams(this, 0, 1));
+                return;
             }
+
+            if (effectListComponent.ContainsEffectForEffectType(eEffect.Stealth))
+                EffectService.RequestImmediateCancelEffect(EffectListService.GetEffectOnTarget(this, eEffect.Stealth), false);
+
+            if (effectListComponent.ContainsEffectForEffectType(eEffect.Vanish))
+                EffectService.RequestImmediateCancelEffect(EffectListService.GetEffectOnTarget(this, eEffect.Vanish));
         }
 
         // UncoverStealthAction is what unstealths player if they are too close to mobs.
@@ -12276,7 +12275,7 @@ namespace DOL.GS
         {
             GameObject target = CurrentRegion.GetObject(targetOID);
 
-            if ((target == null) || (player.IsStealthed == false))
+            if (target == null || !player.IsStealthed)
                 return;
 
             if (response is eLosCheckResponse.TRUE)

GameServer/ECS-Components/AttackComponent.cs
@@ -669,11 +669,9 @@ namespace DOL.GS
 
                 if (player.ActiveWeaponSlot == eActiveWeaponSlot.Distance)
                 {
-                    if (ServerProperties.Properties.ALLOW_OLD_ARCHERY == false)
+                    if (Properties.ALLOW_OLD_ARCHERY == false)
                     {
-                        if ((eCharacterClass) player.CharacterClass.ID == eCharacterClass.Scout ||
-                            (eCharacterClass) player.CharacterClass.ID == eCharacterClass.Hunter ||
-                            (eCharacterClass) player.CharacterClass.ID == eCharacterClass.Ranger)
+                        if ((eCharacterClass) player.CharacterClass.ID is eCharacterClass.Scout or eCharacterClass.Hunter or eCharacterClass.Ranger)
                         {
                             // There is no feedback on live when attempting to fire a bow with arrows
                             return;
@@ -729,6 +727,7 @@ namespace DOL.GS
                         // -Chance to unstealth nocking a crit = stealth / level  0.20
                         int stealthSpec = player.GetModifiedSpecLevel(Specs.Stealth);
                         int stayStealthed = stealthSpec * 100 / player.Level;
+
                         if (player.rangeAttackComponent?.RangedAttackType == eRangedAttackType.Critical)
                             stayStealthed -= 20;
 

GameServer/commands/gmcommands/gmstealth.cs
@@ -1,29 +1,4 @@
-﻿/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
-using System;
-using System.Collections.Generic;
-using DOL.GS.Commands;
-using DOL.GS;
-using DOL.Database;
-using DOL.GS.PacketHandler;
-
-namespace DOL.GS.Commands
+﻿namespace DOL.GS.Commands
 {
     [CmdAttribute(
         "&gmstealth",
@@ -35,21 +10,12 @@ namespace DOL.GS.Commands
     {
         public void OnCommand(GameClient client, string[] args)
         {
-        	if (args.Length != 2) {
-        		DisplaySyntax(client);
-        	}
-        	else if (args[1].ToLower().Equals("on")) {
-
-                if (client.Player.IsStealthed != true)
-                {
-                   client.Player.Stealth(true);
-                   client.Player.CurrentSpeed = 191;
-                }
-        	}
-            else if (args[1].ToLower().Equals("off"))
-            {
-                    client.Player.Stealth(false);
-            }
+            if (args.Length != 2)
+                DisplaySyntax(client);
+            else if (args[1].Equals("on", System.StringComparison.OrdinalIgnoreCase))
+                client.Player.Stealth(true);
+            else if (args[1].Equals("off", System.StringComparison.OrdinalIgnoreCase))
+                client.Player.Stealth(false);
         }
     }
 }

GameServer/craft/Salvage.cs
@@ -1,26 +1,6 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
-
 using System;
 using System.Collections.Generic;
 using System.Reflection;
-
 using DOL.Database;
 using DOL.GS.PacketHandler;
 using DOL.GS.SalvageCalc;
@@ -171,11 +151,7 @@ namespace DOL.GS
 				return 0;
 			}
 
-			if (player.IsStealthed)
-			{
-				player.Stealth(false);
-			}
-
+			player.Stealth(false);
 			player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language, "Salvage.BeginWork.BeginSalvage", item.Template.Name), eChatType.CT_System, eChatLoc.CL_SystemWindow);
 
 			if (salvageYield.Count < 1)
@@ -183,16 +159,13 @@ namespace DOL.GS
 				player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language, "Salvage.BeginWork.NoSalvage", item.Template.Name), eChatType.CT_System, eChatLoc.CL_SystemWindow);
 				return 0;
 			}
-			
-			
-			
 
 			player.Out.SendTimerWindow(LanguageMgr.GetTranslation(player.Client.Account.Language, "Salvage.BeginWork.Salvaging", item.Name), salvageYield.Count);
-            player.CraftTimer = new ECSGameTimer(player)
-            {
-                Callback = new ECSGameTimer.ECSTimerCallback(Proceed)
-            };
-            player.CraftTimer.Properties.SetProperty(AbstractCraftingSkill.PLAYER_CRAFTER, player);
+			player.CraftTimer = new ECSGameTimer(player)
+			{
+				Callback = new ECSGameTimer.ECSTimerCallback(Proceed)
+			};
+			player.CraftTimer.Properties.SetProperty(AbstractCraftingSkill.PLAYER_CRAFTER, player);
 			player.CraftTimer.Properties.SetProperty(SALVAGED_ITEM, item);
 			player.CraftTimer.Properties.SetProperty(SALVAGE_YIELD, salvageYield);
 

GameServer/keeps/Relics/GameRelic.cs
@@ -218,6 +218,7 @@ namespace DOL.GS
 				player.Out.SendMessage("You are already carrying a relic.", eChatType.CT_System, eChatLoc.CL_SystemWindow);
 				return;
 			}
+
 			if (player.IsStealthed)
 			{
 				player.Out.SendMessage("You cannot carry a relic while stealthed.", eChatType.CT_System, eChatLoc.CL_SystemWindow);

GameServer/minotaurrelics/MinotaurRelic.cs
@@ -254,8 +254,8 @@ namespace DOL.GS
 					}
 				}
 			}
-			if (player.IsStealthed)
-				player.Stealth(false);
+
+			player.Stealth(false);
 			PlayerTakesRelic(player);
 			return true;
 		}

GameServer/packets/Server/PacketLib175.cs
@@ -1,22 +1,3 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
-
 using System;
 using System.Collections.Generic;
 using System.Reflection;
@@ -480,7 +461,7 @@ namespace DOL.GS.PacketHandler
 				int flags = (GameServer.ServerRules.GetLivingRealm(m_gameClient.Player, playerToCreate) & 0x03) << 2;
 				if (playerToCreate.IsAlive == false) flags |= 0x01;
 				if (playerToCreate.IsUnderwater) flags |= 0x02; //swimming
-				if (playerToCreate.IsStealthed)  flags |= 0x10;
+				if (playerToCreate.IsStealthed) flags |= 0x10;
 				if (playerToCreate.IsWireframe) flags |= 0x20;
 				if (playerToCreate.CharacterClass.ID == (int)eCharacterClass.Vampiir) flags |= 0x40; //Vamp fly
 				pak.WriteByte((byte)flags);

GameServer/realmabilities/effects/XAmelioratingMelodiesEffect.cs
@@ -80,26 +80,29 @@ namespace DOL.GS.RealmAbilities
 			{
 				m_countdown--;
 				GamePlayer player = Owner as GamePlayer;
-				if (player == null) return 0;
-				if (player.Group == null) return 3000;
+
+				if (player == null)
+					return 0;
+
+				if (player.Group == null)
+					return 3000;
+
 				foreach (GamePlayer p in player.Group.GetPlayersInTheGroup())
 				{
 					if ((p != player) && (p.Health < p.MaxHealth) && player.IsWithinRadius(p, m_range) && (p.IsAlive))
 					{
-						if (player.IsStealthed)
-						{
-							player.Stealth(false);
-						}
-
+						player.Stealth(false);
 						int heal = m_heal;
 						if (p.Health + heal > p.MaxHealth) heal = p.MaxHealth - p.Health;
 						p.ChangeHealth(player, eHealthChangeType.Regenerate, heal);
-						player.Out.SendMessage("You heal " + p.Name + " for " + heal.ToString() + " hit points.", eChatType.CT_Spell, eChatLoc.CL_SystemWindow);
-						p.Out.SendMessage(player.Name + " heals you for " + heal.ToString() + " hit points.", eChatType.CT_Spell, eChatLoc.CL_SystemWindow);
+						player.Out.SendMessage($"You heal {p.Name} for {heal} hit points.", eChatType.CT_Spell, eChatLoc.CL_SystemWindow);
+						p.Out.SendMessage($"{player.Name} heals you for {heal} hit points.", eChatType.CT_Spell, eChatLoc.CL_SystemWindow);
 					}
 				}
+
 				return 3000;
 			}
+
 			return 0;
 		}
 

commit 53c8c631d56b5c7b53bf6af1cd1548f1e4f869dd

Fix players not losing stealth after getting damaged Caused by f6c7b5fb.

GameServer/gameobjects/GamePlayer.cs
@@ -6160,17 +6160,14 @@ namespace DOL.GS
                 case eAttackResult.HitStyle:
                 case eAttackResult.HitUnstyled:
                 {
-                    if (ad.Damage == -1)
+                    // If attacked by a non-damaging spell, we should not show damage numbers.
+                    // We need to check the damage on the spell here, not in the AD, since this could in theory be a damaging spell that had its damage modified to 0.
+                    if (ad.AttackType == AttackData.eAttackType.Spell && ad.SpellHandler.Spell?.Damage == 0)
                         break;
 
-                    if (ad.AttackType == AttackData.eAttackType.Spell)
+                    if (IsStealthed && !effectListComponent.ContainsEffectForEffectType(eEffect.Vanish))
                     {
-                        // If attacked by a non-damaging spell, we should not show damage numbers.
-                        // We need to check the damage on the spell here, not in the AD, since this could in theory be a damaging spell that had its damage modified to 0.
-                        if (ad.SpellHandler.Spell.Damage == 0)
-                            break;
-
-                        if (ad.SpellHandler.Spell.SpellType != eSpellType.DamageOverTime && !effectListComponent.ContainsEffectForEffectType(eEffect.Vanish))
+                        if (ad.AttackType != AttackData.eAttackType.Spell || ad.SpellHandler.Spell.SpellType != eSpellType.DamageOverTime)
                             Stealth(false);
                     }
 