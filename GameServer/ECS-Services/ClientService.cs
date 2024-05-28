OpenDAoC/OpenDAoC-Core

commit e5dddde36b605262a97c161ca23663245b45b832

Attempt to fix client crashes on login in very populated areas This is only reproducible on Blackthorn, not on OpenDAoC, but tests on OpenDAoC were done on a local server. In any case: * It was possible for NPC update packets to be sent before NPC create packets if the player logged out to character screen then back in before its remote point expired, and if update packets are sent in UDP. This appears to put some strain on the client as it needs to send a NPC create packet request for every NPC updated that way. This was caused by the client service currently not handling UDP packets, which are handled outside of the game loop. A future commit will rework the network layer to prevent that (and improve performances). * The client service was always sending an update packet, even when the cache didn't contain the NPC being checked. We'll now assume the client doesn't know about the entity yet and send a create packet instead. * The client service didn't add the NPC being checked to the player's cache, resulting in a wasteful update packet being sent immediately after. * `PlayerInitRequestHandler` and `NPCCreationRequestHandler` would send a create packet but not add to the cache, resulting in the client service sending an update packet almost immediately after.

GameServer/ECS-Services/ClientService.cs
@@ -517,18 +517,38 @@ namespace DOL.GS
             }
         }
 
+        private static void OnObjectCreateOrUpdateForPlayer(GamePlayer player, GameObject gameObject)
+        {
+            gameObject.OnUpdateOrCreateForPlayer();
+            AddObjectToPlayerCache(player, gameObject);
+        }
+
         public static void UpdateObjectForPlayer(GamePlayer player, GameObject gameObject)
         {
-            // Doesn't handle houses. They aren't 'GameObject'.
-            gameObject.OnUpdateByPlayerService();
             player.Out.SendObjectUpdate(gameObject);
-            AddObjectToPlayerCache(player, gameObject);
+            OnObjectCreateOrUpdateForPlayer(player, gameObject);
         }
 
         public static void UpdateObjectForPlayers(GameObject gameObject)
         {
             foreach (GamePlayer player in gameObject.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
                 UpdateObjectForPlayer(player, gameObject);
+        }
+
+        public static void CreateNpcForPlayer(GamePlayer player, GameNPC npc)
+        {
+            player.Out.SendNPCCreate(npc);
+
+            if (npc.Inventory != null)
+                player.Out.SendLivingEquipmentUpdate(npc);
+
+            OnObjectCreateOrUpdateForPlayer(player, npc);
+        }
+
+        public static void CreateNpcForPlayers(GameNPC npc)
+        {
+            foreach (GamePlayer playerInRadius in npc.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
+                CreateNpcForPlayer(playerInRadius, npc);
         }
 
         public static void CreateObjectForPlayer(GamePlayer player, GameObject gameObject)
@@ -621,7 +641,7 @@ namespace DOL.GS
                     continue;
 
                 if (!npcUpdateCache.TryGetValue(npcInRange, out CachedNpcValues cachedNpcValues))
-                    UpdateObjectForPlayer(player, npcInRange);
+                    CreateNpcForPlayer(player, npcInRange);
                 else if (ServiceUtils.ShouldTick(cachedNpcValues.Time + Properties.WORLD_NPC_UPDATE_INTERVAL))
                     UpdateObjectForPlayer(player, npcInRange);
                 else if (ServiceUtils.ShouldTick(cachedNpcValues.Time + 250))
@@ -710,7 +730,7 @@ namespace DOL.GS
                 if (!doorUpdateCache.TryGetValue(doorInRange, out long lastUpdate))
                 {
                     CreateObjectForPlayer(player, doorInRange);
-                    player.Out.SendDoorState(doorInRange.CurrentRegion, doorInRange);
+                    player.Out.SendDoorState(doorInRange.CurrentRegion, doorInRange); // Not handled by `CreateObjectForPlayer`.
                 }
                 else if (ServiceUtils.ShouldTick(lastUpdate + Properties.WORLD_OBJECT_UPDATE_INTERVAL))
                     UpdateObjectForPlayer(player, doorInRange);

GameServer/ECS-Effects/CharmECSEffect.cs
@@ -47,14 +47,7 @@ namespace DOL.GS
                     charmSpellHandler.MessageToCaster(LanguageMgr.GetTranslation(casterPlayer.Client, "GamePlayer.GamePet.StartSpell.UnderControl", charmMob.GetName(0, true)), eChatType.CT_Spell);
 
                 casterPlayer.SetControlledBrain(charmSpellHandler.m_controlledBrain);
-
-                foreach (GamePlayer playerInRadius in charmMob.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
-                {
-                    playerInRadius.Out.SendNPCCreate(charmMob);
-
-                    if (charmMob.Inventory != null)
-                        playerInRadius.Out.SendLivingEquipmentUpdate(charmMob);
-                }
+                ClientService.CreateNpcForPlayers(charmMob);
             }
 
             charmSpellHandler.SendEffectAnimation(charmMob, 0, false, 1);

GameServer/gameobjects/CustomNPC/DisplayModel.cs
@@ -51,10 +51,7 @@ namespace DOL.GS
         {
             ObjectState = eObjectState.Active;
             m_spawnTick = GameLoop.GameLoopTime + 120*1000;
-            
-            m_displayedPlayer.Out.SendNPCCreate(this);
-            m_displayedPlayer.Out.SendLivingEquipmentUpdate(this);
-            
+            ClientService.CreateNpcForPlayer(m_displayedPlayer, this);
             return true;
         }
     }

GameServer/gameobjects/GameNPC.cs
@@ -172,16 +172,9 @@ namespace DOL.GS
 
 				if (Level != value)
 				{
+					// This is a newly created NPC, so notify nearby players of its creation
 					if (Level < 1 && ObjectState == eObjectState.Active)
-					{
-						// This is a newly created NPC, so notify nearby players of its creation
-						foreach (GamePlayer player in GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
-						{
-							player.Out.SendNPCCreate(this);
-							if (m_inventory != null)
-								player.Out.SendLivingEquipmentUpdate(this);
-						}
-					}
+						ClientService.CreateNpcForPlayers(this);
 
 					base.Level = value;
 					AutoSetStats();  // Recalculate stats when level changes
@@ -295,15 +288,9 @@ namespace DOL.GS
 			set
 			{
 				base.Realm = value;
+
 				if (ObjectState == eObjectState.Active)
-				{
-					foreach (GamePlayer player in GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
-					{
-						player.Out.SendNPCCreate(this);
-						if (m_inventory != null)
-							player.Out.SendLivingEquipmentUpdate(this);
-					}
-				}
+					ClientService.CreateNpcForPlayers(this);
 			}
 		}
 
@@ -316,15 +303,9 @@ namespace DOL.GS
 			set
 			{
 				base.Name = value;
+
 				if (ObjectState == eObjectState.Active)
-				{
-					foreach (GamePlayer player in GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
-					{
-						player.Out.SendNPCCreate(this);
-						if (m_inventory != null)
-							player.Out.SendLivingEquipmentUpdate(this);
-					}
-				}
+					ClientService.CreateNpcForPlayers(this);
 			}
 		}
 
@@ -361,15 +342,9 @@ namespace DOL.GS
 			set
 			{
 				base.GuildName = value;
+
 				if (ObjectState == eObjectState.Active)
-				{
-					foreach (GamePlayer player in GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
-					{
-						player.Out.SendNPCCreate(this);
-						if (m_inventory != null)
-							player.Out.SendLivingEquipmentUpdate(this);
-					}
-				}
+					ClientService.CreateNpcForPlayers(this);
 			}
 		}
 
@@ -667,17 +642,11 @@ namespace DOL.GS
 			{
 				eFlags oldflags = m_flags;
 				m_flags = value;
+
 				if (ObjectState == eObjectState.Active)
 				{
 					if (oldflags != m_flags)
-					{
-						foreach (GamePlayer player in GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
-						{
-							player.Out.SendNPCCreate(this);
-							if (m_inventory != null)
-								player.Out.SendLivingEquipmentUpdate(this);
-						}
-					}
+						ClientService.CreateNpcForPlayers(this);
 				}
 			}
 		}
@@ -1955,7 +1924,7 @@ namespace DOL.GS
 
 		#region Add/Remove/Create/Remove/Update
 
-		public override void OnUpdateByPlayerService()
+		public override void OnUpdateOrCreateForPlayer()
 		{
 			m_lastVisibleToPlayerTick = GameLoop.GameLoopTime;
 
@@ -1975,24 +1944,7 @@ namespace DOL.GS
 			if (MAX_PASSENGERS > 0)
 				Riders = new GamePlayer[MAX_PASSENGERS];
 
-			bool anyPlayer = false;
-
-			foreach (GamePlayer player in GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
-			{
-				if (player == null)
-					continue;
-
-				player.Out.SendNPCCreate(this);
-
-				if (m_inventory != null)
-					player.Out.SendLivingEquipmentUpdate(this);
-
-				anyPlayer = true;
-			}
-
-			if (anyPlayer)
-				m_lastVisibleToPlayerTick = GameLoop.GameLoopTime;
-
+			ClientService.CreateNpcForPlayers(this);
 			m_spawnPoint.X = X;
 			m_spawnPoint.Y = Y;
 			m_spawnPoint.Z = Z;
@@ -2139,17 +2091,7 @@ namespace DOL.GS
 				player.Out.SendObjectRemove(this);
 
 			// New position.
-			foreach (GamePlayer player in GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
-			{
-				if (player == null)
-					continue;
-
-				player.Out.SendNPCCreate(this);
-
-				if (m_inventory != null)
-					player.Out.SendLivingEquipmentUpdate(this);
-			}
-
+			ClientService.CreateNpcForPlayers(this);
 			return true;
 		}
 

GameServer/gameobjects/GameObject.cs
@@ -1258,6 +1258,6 @@ namespace DOL.GS
 				return m_boat_ownerid;
 		}
 
-		public virtual void OnUpdateByPlayerService() { }
+		public virtual void OnUpdateOrCreateForPlayer() { }
 	}
 }

GameServer/gameobjects/GamePlayer.cs
@@ -9198,12 +9198,8 @@ namespace DOL.GS
         /// </summary>
         public virtual void RefreshWorld()
         {
-            foreach (GameNPC npc in GetNPCsInRadius(WorldMgr.VISIBILITY_DISTANCE * 2))
-            {
-                Out.SendNPCCreate(npc);
-                if (npc.Inventory != null)
-                    Out.SendLivingEquipmentUpdate(npc);
-            }
+            foreach (GameNPC npc in GetNPCsInRadius(WorldMgr.VISIBILITY_DISTANCE))
+                ClientService.CreateNpcForPlayer(this, npc);
 
             foreach (GamePlayer player in GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
             {

GameServer/packets/Client/168/NPCCreationRequestHandler.cs
@@ -1,22 +1,3 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
-
 namespace DOL.GS.PacketHandler.Client.v168
 {
 	[PacketHandlerAttribute(PacketHandlerType.TCP, eClientPackets.CreateNPCRequest, "Handles requests for npcs(0x72) in game", eClientStatus.PlayerInGame)]
@@ -43,18 +24,7 @@ namespace DOL.GS.PacketHandler.Client.v168
 			}
 
 			if (npc != null)
-			{
-				client.Out.SendNPCCreate(npc);
-				
-				if (npc.Inventory != null)
-					client.Out.SendLivingEquipmentUpdate(npc);
-				
-				//DO NOT SEND A NPC UPDATE, it is done in Create anyway
-				//Sending a Update causes a UDP packet to be sent and
-				//the client will get the UDP packet before the TCP Create packet
-				//Causing the client to issue another NPC CREATION REQUEST!
-				//client.Out.SendNPCUpdate(npc); <-- BIG NO NO
-			}
+				ClientService.CreateNpcForPlayer(client.Player, npc);
 		}
 	}
 }

GameServer/packets/Client/168/PlayerInitRequestHandler.cs
@@ -54,7 +54,6 @@ namespace DOL.GS.PacketHandler.Client.v168
 					player.Out.SendPlayerRevive(player);
 				}
 
-				int mobs = SendMobsAndMobEquipmentToPlayer(player);
 				player.Out.SendTime();
 
 				bool checkInstanceLogin = false;
@@ -361,25 +360,6 @@ namespace DOL.GS.PacketHandler.Client.v168
 							eChatType.CT_Important, eChatLoc.CL_SystemWindow);
 					}
 				}
-			}
-
-			private static int SendMobsAndMobEquipmentToPlayer(GamePlayer player)
-			{
-				int mobs = 0;
-
-				if (player.CurrentRegion != null)
-				{
-					var npcs = player.GetNPCsInRadius(WorldMgr.VISIBILITY_DISTANCE);
-					foreach (GameNPC npc in npcs)
-					{
-						player.Out.SendNPCCreate(npc);
-						mobs++;
-						if (npc.Inventory != null)
-							player.Out.SendLivingEquipmentUpdate(npc);
-					}
-				}
-
-				return mobs;
 			}
 		}
 	}

commit 0067e4e942fdb0859094bca51910fc6a5daffc28

Remove duplicate NPC creation packet (oops) Caused by e5dddde3

GameServer/ECS-Services/ClientService.cs
@@ -558,11 +558,6 @@ namespace DOL.GS
             if (player.Client.ClientState is not GameClient.eClientState.Playing)
                 return;
 
-            player.Out.SendNPCCreate(npc);
-
-            if (npc.Inventory != null)
-                player.Out.SendLivingEquipmentUpdate(npc);
-
             CreateNpcForPlayerInternal(player, npc);
         }
 