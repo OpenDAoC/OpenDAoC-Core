OpenDAoC/OpenDAoC-Core

commit 45b37dbbe15097f14b1899995d06e786a902c723

Replace usage of `TempProperties` for last attack data

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using DOL.Database;
 using DOL.GS.Styles;
-using static DOL.GS.GameLiving;
 using static DOL.GS.GameObject;
 
 namespace DOL.GS
@@ -13,7 +12,6 @@ namespace DOL.GS
         protected const int TICK_INTERVAL_FOR_NON_ATTACK = 100;
         private const int MINIMUM_MELEE_DELAY_AFTER_RANGED_ATTACK = 750;
 
-        protected AttackData _lastAttackData;
         protected DbInventoryItem _weapon;
         protected DbInventoryItem _leftWeapon;
         protected Style _combatStyle;
@@ -28,6 +26,7 @@ namespace DOL.GS
 
         // Set to current time when a round doesn't result in an attack. Used to prevent combat log spam and kept until reset in AttackComponent.SendAttackingCombatMessages().
         public long RoundWithNoAttackTime { get; set; }
+        public AttackData LastAttackData { get; set; }
         public long NextTick => _owner.ActiveWeaponSlot != eActiveWeaponSlot.Distance ? _nextMeleeTick : _nextRangedTick;
         protected AttackComponent AttackComponent => _owner.attackComponent;
         protected StyleComponent StyleComponent => _owner.styleComponent;
@@ -60,7 +59,6 @@ namespace DOL.GS
                 return;
             }
 
-            _lastAttackData = _owner.TempProperties.GetProperty<AttackData>(LAST_ATTACK_DATA, null);
             _weapon = _owner.ActiveWeapon;
             _leftWeapon = _owner.Inventory?.GetItem(eInventorySlot.LeftHandWeapon);
             _effectiveness = _owner.Effectiveness;
@@ -157,16 +155,16 @@ namespace DOL.GS
         {
             bool clearOldStyles = false;
 
-            if (_lastAttackData != null)
+            if (LastAttackData != null)
             {
-                switch (_lastAttackData.AttackResult)
+                switch (LastAttackData.AttackResult)
                 {
                     case eAttackResult.Fumbled:
                     {
                         // Skip this attack if the last one fumbled.
                         StyleComponent.NextCombatStyle = null;
                         StyleComponent.NextCombatBackupStyle = null;
-                        _lastAttackData.AttackResult = eAttackResult.Missed;
+                        LastAttackData.AttackResult = eAttackResult.Missed;
                         _interval = AttackComponent.AttackSpeed(_weapon) * 2;
                         return false;
                     }
@@ -336,7 +334,6 @@ namespace DOL.GS
         {
             AttackComponent.weaponAction = new WeaponAction(_owner, _target, _weapon, _leftWeapon, _effectiveness, _interruptDuration, _combatStyle);
             AttackComponent.weaponAction.Execute();
-            _lastAttackData = _owner.TempProperties.GetProperty<AttackData>(LAST_ATTACK_DATA, null);
         }
 
         protected virtual void PerformRangedAttack()
@@ -356,15 +353,13 @@ namespace DOL.GS
             }
             else
                 AttackComponent.weaponAction.Execute();
-
-            _lastAttackData = _owner.TempProperties.GetProperty<AttackData>(LAST_ATTACK_DATA, null);
         }
 
         protected virtual bool FinalizeMeleeAttack()
         {
             // Melee weapons tick every TICK_INTERVAL_FOR_NON_ATTACK if they didn't attack.
-            if (_lastAttackData != null &&
-                _lastAttackData.AttackResult is not eAttackResult.Missed
+            if (LastAttackData != null &&
+                LastAttackData.AttackResult is not eAttackResult.Missed
                 and not eAttackResult.HitUnstyled
                 and not eAttackResult.HitStyle
                 and not eAttackResult.Evaded
@@ -421,7 +416,7 @@ namespace DOL.GS
 
         public virtual void CleanUp()
         {
-            _owner.TempProperties.RemoveProperty(LAST_ATTACK_DATA);
+            LastAttackData = null;
             _target = null;
         }
     }

GameServer/ECS-Components/Actions/WeaponAction.cs
@@ -6,7 +6,6 @@ using DOL.Events;
 using DOL.GS.PacketHandler;
 using DOL.GS.Spells;
 using DOL.GS.Styles;
-using static DOL.GS.GameLiving;
 using static DOL.GS.GameObject;
 
 namespace DOL.GS
@@ -150,7 +149,7 @@ namespace DOL.GS
             else
                 mainHandAD = m_owner.attackComponent.MakeAttack(this, m_target, mainWeapon, style, mainHandEffectiveness, m_interruptDuration, false);
 
-            m_owner.TempProperties.SetProperty(LAST_ATTACK_DATA, mainHandAD);
+            m_owner.attackComponent.attackAction.LastAttackData = mainHandAD;
 
             if (mainHandAD.Target == null ||
                 mainHandAD.AttackResult == eAttackResult.OutOfRange ||

GameServer/ECS-Components/AttackComponent.cs
@@ -16,7 +16,6 @@ using DOL.GS.SkillHandler;
 using DOL.GS.Spells;
 using DOL.GS.Styles;
 using DOL.Language;
-using static DOL.GS.GameLiving;
 using static DOL.GS.GameObject;
 
 namespace DOL.GS
@@ -102,7 +101,7 @@ namespace DOL.GS
             if (weaponAction?.AttackFinished == true)
                 weaponAction = null;
 
-            if (weaponAction is null && !AttackState)
+            if (weaponAction == null && !AttackState)
             {
                 attackAction.CleanUp();
                 EntityManager.Remove(this);
@@ -2012,7 +2011,7 @@ namespace DOL.GS
             GameSpellEffect grapple = null;
             GameSpellEffect brittleguard = null;
 
-            AttackData lastAttackData = owner.TempProperties.GetProperty<AttackData>(LAST_ATTACK_DATA, null);
+            AttackData lastAttackData = owner.attackComponent.attackAction.LastAttackData;
             bool defenseDisabled = ad.Target.IsMezzed | ad.Target.IsStunned | ad.Target.IsSitting;
 
             GamePlayer playerOwner = owner as GamePlayer;

GameServer/ECS-Components/StyleComponent.cs
@@ -118,9 +118,9 @@ namespace DOL.GS
             if (NextCombatStyle == null)
                 return null;
 
-            AttackData lastAttackData = _owner.TempProperties.GetProperty<AttackData>(GameLiving.LAST_ATTACK_DATA, null);
+            AttackData lastAttackData = _owner.attackComponent.attackAction.LastAttackData;
             DbInventoryItem weapon = NextCombatStyle.WeaponTypeRequirement == (int) eObjectType.Shield ? _owner.Inventory.GetItem(eInventorySlot.LeftHandWeapon) : _owner.ActiveWeapon;
-            
+
             //if they've cached a style and then respecced to no longer have access, remove it
             if (AutomaticBackupStyle != null && _owner is GamePlayer player && player.WeaponBaseSpecLevel(weapon) < AutomaticBackupStyle.SpecLevelRequirement)
             {
@@ -156,7 +156,7 @@ namespace DOL.GS
             if (p.Styles == null || p.Styles.Count < 1 || p.TargetObject == null)
                 return null;
 
-            AttackData lastAttackData = p.TempProperties.GetProperty<AttackData>(GameLiving.LAST_ATTACK_DATA, null);
+            AttackData lastAttackData = p.attackComponent.attackAction.LastAttackData;
 
             // Chain and defensive styles are excluded from the chance roll because they would almost never happen otherwise. 
             // For example, an NPC blocks 10% of the time, so the default 20% style chance effectively means the defensive 

GameServer/gameobjects/GameLiving.cs
@@ -36,10 +36,6 @@ namespace DOL.GS
 		public ConcurrentDictionary<eSpellType, Spell> ActivePulseSpells { get; } = new();
 
 		#region Combat
-		/// <summary>
-		/// Holds the AttackData object of last attack
-		/// </summary>
-		public const string LAST_ATTACK_DATA = "LastAttackData";
 
 		public bool isDeadOrDying = false;
 

GameServer/gameobjects/GamePlayer.cs
@@ -9454,7 +9454,7 @@ namespace DOL.GS
 
                 if (attackComponent.AttackState && ActiveWeaponSlot != eActiveWeaponSlot.Distance)
                 {
-                    AttackData ad = TempProperties.GetProperty<AttackData>(LAST_ATTACK_DATA, null);
+                    AttackData ad = attackComponent.attackAction.LastAttackData;
 
                     if (ad != null && ad.IsMeleeAttack && (ad.AttackResult == eAttackResult.TargetNotVisible || ad.AttackResult == eAttackResult.OutOfRange))
                     {
@@ -9828,7 +9828,7 @@ namespace DOL.GS
                 }
                 else
                 {
-                    AttackData ad = TempProperties.GetProperty<AttackData>(LAST_ATTACK_DATA, null);
+                    AttackData ad = attackComponent.attackAction.LastAttackData;
 
                     if (ad != null && ad.IsMeleeAttack && (ad.AttackResult == eAttackResult.TargetNotVisible || ad.AttackResult == eAttackResult.OutOfRange))
                     {

GameServer/scripts/namedmobs/Albion Named Mobs/Classic Zones/WrathOfMordred.cs
@@ -188,7 +188,7 @@ namespace DOL.AI.Brain
 					{
 						AttackAction attackAction = Body.attackComponent.attackAction;
 
-						if (attackAction != null && attackAction.NextTick - GameLoop.GameLoopTime <= 800 && CanWalk == false)
+						if (attackAction.NextTick - GameLoop.GameLoopTime <= 800 && CanWalk == false)
 						{
 							Body.styleComponent.NextCombatStyle = null;
 							Body.styleComponent.NextCombatBackupStyle = null;

GameServer/spells/Archery/Arrow.cs
@@ -123,7 +123,7 @@ namespace DOL.GS.Spells
 
 				int missrate = 100 - m_handler.CalculateToHitChance(target);
 				// add defence bonus from last executed style if any
-				AttackData targetAD = target.TempProperties.GetProperty<AttackData>(GameLiving.LAST_ATTACK_DATA, null);
+				AttackData targetAD = target.attackComponent.attackAction.LastAttackData;
 				if (targetAD != null
 				    && targetAD.AttackResult == eAttackResult.HitStyle
 				    && targetAD.Style != null)

commit 3e47bc6018ac1a2dcd698b8a09497fba7a7ac6d6

Fix NRE in `CalculateEnemyAttackResult` when attacking objects. Caused by 45b37dbb Since objects have an `AttackComponent` for some reason, `AttackAction` shouldn't be allowed to be null.

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -6,7 +6,7 @@ using static DOL.GS.GameObject;
 
 namespace DOL.GS
 {
-    public abstract class AttackAction
+    public class AttackAction
     {
         // Next tick interval for when the current tick doesn't result in an attack.
         protected const int TICK_INTERVAL_FOR_NON_ATTACK = 100;
@@ -44,8 +44,8 @@ namespace DOL.GS
                 return new NpcAttackAction(gameNpc);
             else if (gameLiving is GamePlayer gamePlayer)
                 return new PlayerAttackAction(gamePlayer);
-
-            return null;
+            else
+                return new AttackAction(gameLiving);
         }
 
         public bool Tick()