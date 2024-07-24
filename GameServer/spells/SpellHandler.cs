0da78d086b64bd81e2b957be92e9c25172f1fd5d
Baptiste Marie <bm01@users.noreply.github.com>
2024-07-25 00:14:58 +0200

    Fix spell damage bonuses increasing damage on every DoT tick Caused by 85a98475.

--- GameServer/spells/SpellHandler.cs
+++ GameServer/spells/SpellHandler.cs
@@ -2194,7 +2194,7 @@ namespace DOL.GS.Spells
 		protected virtual double CalculateDamageEffectiveness()
 		{
 			if (Caster is GamePlayer playerCaster)
-				return CasterEffectiveness *= 1 + playerCaster.GetModified(eProperty.SpellDamage) * 0.01;
+				return CasterEffectiveness * 1 + playerCaster.GetModified(eProperty.SpellDamage) * 0.01;
 			else
 				return CasterEffectiveness;
 		}
