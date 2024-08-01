OpenDAoC/OpenDAoC-Core

commit 83db5c7a127e10951ed19ca4571f90021f9c8a71

Fix objects not changing subzones in some cases This would make NPCs or other players invisible for some players. Caused by the fact that objects only changed subzones when being observed by either another object or themselves (via 'GetObjectsInRadius'), which isn't guaranteed to happen after a lag or for NPCs that don't check NPCs in radius (like horses). Fixed by having objects update themselves at a regular interval instead (in 'MovementComponent').

GameServer/gameobjects/SiegeWeapon/GameSiegeRam.cs
@@ -103,7 +103,7 @@ namespace DOL.GS
 			//Limit 2 Rams aimed at door at a time
 			//Check # of rams on the target
 			int ramsAimedAtTarget=0;
-			foreach (GameNPC npc in Owner.CurrentRegion.GetNPCsInRadius(Owner.TargetObject.X, Owner.TargetObject.Y, Owner.TargetObject.Z, 600))
+			foreach (GameNPC npc in Owner.CurrentRegion.GetNPCsInRadius(TargetObject, 600))
 			{
 				if(npc is GameSiegeRam ram)
 				{

GameServer/ECS-Components/MovementComponent.cs
@@ -5,6 +5,10 @@ namespace DOL.GS
 {
     public class MovementComponent
     {
+        private const int SUBZONE_RELOCATION_CHECK_INTERVAL = 500;
+
+        private long _lastSubZoneRelocationCheckTick;
+        private long _lastSpeedDuringLastSubZoneRelocationCheck;
         private int _turningDisabledCount;
         private AuxECSGameTimer _resetHeadingAction;
 
@@ -34,7 +38,16 @@ namespace DOL.GS
                 return new MovementComponent(gameLiving);
         }
 
-        public virtual void Tick(long tick) { }
+        public virtual void Tick(long tick)
+        {
+            // Check for subzone relocation only if we're moving or once after we stopped.
+            if ((IsMoving || _lastSpeedDuringLastSubZoneRelocationCheck > 0) && _lastSubZoneRelocationCheckTick + SUBZONE_RELOCATION_CHECK_INTERVAL < tick)
+            {
+                _lastSubZoneRelocationCheckTick = tick;
+                _lastSpeedDuringLastSubZoneRelocationCheck = CurrentSpeed;
+                Owner.SubZoneObject.CheckForRelocation();
+            }
+        }
 
         public virtual void DisableTurning(bool add)
         {

GameServer/ECS-Components/NpcMovementComponent.cs
@@ -16,7 +16,7 @@ namespace DOL.GS
         private const double FOLLOW_SPEED_SCALAR = 2.5;
 
         private MovementType _movementType;
-        private long _followLastTick;
+        private long _lastFollowTick;
         private int _followTickInterval;
         private long _walkingToEstimatedArrivalTime;
         private short _moveOnPathSpeed;
@@ -52,10 +52,10 @@ namespace DOL.GS
         {
             if (IsSet(MovementType.FOLLOW))
             {
-                if (_followLastTick + _followTickInterval < tick)
+                if (_lastFollowTick + _followTickInterval < tick)
                 {
                     _followTickInterval = FollowTick();
-                    _followLastTick = tick;
+                    _lastFollowTick = tick;
 
                     if (_followTickInterval == 0)
                         _movementType &= ~MovementType.FOLLOW;
@@ -79,6 +79,8 @@ namespace DOL.GS
                     MoveToNextWaypoint();
                 }
             }
+
+            base.Tick(tick);
         }
 
         public void WalkTo(IPoint3D targetPosition, short speed)

GameServer/ECS-Services/PlayerService.cs
@@ -26,10 +26,7 @@ namespace DOL.GS
             {
                 GamePlayer player = list[i];
 
-                if (player == null)
-                    return;
-
-                if (player.LastWorldUpdate + Properties.WORLD_PLAYER_UPDATE_INTERVAL >= tick ||
+                if (player == null||
                     player.Client.ClientState != GameClient.eClientState.Playing ||
                     player.ObjectState != GameObject.eObjectState.Active)
                 {
@@ -38,12 +35,17 @@ namespace DOL.GS
 
                 try
                 {
-                    long startTick = GameLoop.GetCurrentTime();
-                    UpdateWorld(player, tick);
-                    long stopTick = GameLoop.GetCurrentTime();
+                    if (player.LastWorldUpdate + Properties.WORLD_PLAYER_UPDATE_INTERVAL < tick)
+                    {
+                        long startTick = GameLoop.GetCurrentTime();
+                        UpdateWorld(player, tick);
+                        long stopTick = GameLoop.GetCurrentTime();
 
-                    if ((stopTick - startTick) > 25)
-                        log.Warn($"Long UpdateWorld for {player.Name}({player.ObjectID}) Time: {stopTick - startTick}ms");
+                        if ((stopTick - startTick) > 25)
+                            log.Warn($"Long UpdateWorld for {player.Name}({player.ObjectID}) Time: {stopTick - startTick}ms");
+                    }
+
+                    player.movementComponent.Tick(tick);
                 }
                 catch (Exception e)
                 {
@@ -60,13 +62,11 @@ namespace DOL.GS
                 UpdateNpcs(player, tick);
 
             if (Properties.WORLD_OBJECT_UPDATE_INTERVAL > 0)
+            {
                 UpdateItems(player, tick);
-
-            if (Properties.WORLD_OBJECT_UPDATE_INTERVAL > 0)
                 UpdateDoors(player, tick);
-
-            if (Properties.WORLD_OBJECT_UPDATE_INTERVAL > 0)
                 UpdateHouses(player, tick);
+            }
 
             player.LastWorldUpdate = tick;
         }

GameServer/ECS-Services/ZoneService.cs
@@ -31,8 +31,9 @@ namespace DOL.GS
                     return;
 
                 eGameObjectType objectType = objectChangingSubZone.ObjectType;
-                LightConcurrentLinkedList<SubZoneObject>.Node node = objectChangingSubZone.SubZoneObject;
-                SubZoneObject subZoneObject = node.Item;
+                LightConcurrentLinkedList<GameObject>.Node node = objectChangingSubZone.Node;
+                GameObject gameObject = node.Item;
+                SubZoneObject subZoneObject = gameObject.SubZoneObject;
                 Zone currentZone = subZoneObject.CurrentSubZone?.ParentZone;
                 Zone destinationZone = objectChangingSubZone.DestinationZone;
                 bool changingZone = currentZone != destinationZone;
@@ -48,10 +49,7 @@ namespace DOL.GS
                     }
 
                     if (changingZone)
-                    {
-                        subZoneObject.Object.CurrentZone = null;
                         currentZone.OnObjectRemovedFromZone();
-                    }
 
                     subZoneObject.CurrentSubZone = null;
                 }
@@ -69,13 +67,10 @@ namespace DOL.GS
                     }
 
                     subZoneObject.CurrentSubZone = destinationSubZone;
-                    subZoneObject.IsChangingSubZone = false;
+                    subZoneObject.IsSubZoneChangeBeingHandled = false;
 
                     if (changingZone)
-                    {
-                        subZoneObject.Object.CurrentZone = destinationZone;
                         destinationZone.OnObjectAddedToZone();
-                    }
                 }
 
                 EntityManager.Remove(EntityManager.EntityType.ObjectChangingSubZone, objectChangingSubZone);

GameServer/ai/brain/StandardMobBrain/StandardMobBrain.cs
@@ -1343,9 +1343,9 @@ namespace DOL.AI.Brain
 
         public virtual void DetectDoor()
         {
-            ushort range = (ushort)((ThinkInterval / 800) * Body.CurrentWaypoint.MaxSpeed);
+            ushort range = (ushort) (ThinkInterval / 800 * Body.CurrentWaypoint.MaxSpeed);
 
-            foreach (GameDoorBase door in Body.CurrentRegion.GetDoorsInRadius(Body.X, Body.Y, Body.Z, range, false))
+            foreach (GameDoorBase door in Body.CurrentRegion.GetDoorsInRadius(Body, range, false))
             {
                 if (door is GameKeepDoor)
                 {

GameServer/gameobjects/GameDoor.cs
@@ -88,7 +88,7 @@ namespace DOL.GS
             m_model = 0xFFFF;
             DoorID = dbDoor.InternalID;
             m_guildName = dbDoor.Guild;
-            _realm = (eRealm) dbDoor.Realm;
+            Realm = (eRealm) dbDoor.Realm;
             m_level = dbDoor.Level;
             m_health = dbDoor.Health;
             Locked = dbDoor.Locked;

GameServer/gameobjects/GameNPC.cs
@@ -4361,7 +4361,7 @@ namespace DOL.GS
 			{
 				case "b": // Broadcast message without "[Broadcast] {0}:" string start
 				{
-					foreach (GamePlayer player in CurrentRegion.GetPlayersInRadius(X, Y, Z, 25000, false))
+					foreach (GamePlayer player in CurrentRegion.GetPlayersInRadius(this, 25000, false))
 					{
 					  player.Out.SendMessage(text, eChatType.CT_Broadcast, eChatLoc.CL_ChatWindow);
 					}
@@ -4636,7 +4636,7 @@ namespace DOL.GS
 
 			Level = 1;
 			m_health = MaxHealth;
-			_realm = 0;
+			Realm = 0;
 			m_name = "new mob";
 			m_model = 408;
 			MaxSpeedBase = 200;
@@ -4646,7 +4646,7 @@ namespace DOL.GS
 			m_flags = 0;
 			m_maxdistance = 0;
 			RoamingRange = 0;
-			_ownerID = "";
+			OwnerID = "";
 			m_spawnPoint = new Point3D();
 			LinkedFactions = new ArrayList(1);
 

commit b35b04abce3d13e5ce0e9a718e936d7919bf5fb7

`GameSiegeRam.Aim`: Fix NRE Caused by 83db5c7

GameServer/gameobjects/SiegeWeapon/GameSiegeRam.cs
@@ -103,7 +103,7 @@ namespace DOL.GS
 			//Limit 2 Rams aimed at door at a time
 			//Check # of rams on the target
 			int ramsAimedAtTarget=0;
-			foreach (GameNPC npc in TargetObject.GetNPCsInRadius(600))
+			foreach (GameNPC npc in Owner.TargetObject.GetNPCsInRadius(600))
 			{
 				if(npc is GameSiegeRam ram)
 				{