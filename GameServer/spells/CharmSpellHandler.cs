OpenDAoC/OpenDAoC-Core

commit e3ee553e3f1bcff6cf3e90301a42e2cb72925984

Replace usage of con level in combat formulas with level difference * Con level doesn't appear to be used directly on live. This change mostly helps low level players, and when fighting low orange / low red / low purple targets. It prevents big jumps in effectiveness, miss rate, and resist rate from one target to another when the level difference is only 1. It also prevents asymmetric scaling, since for example a level 51 NPC is orange to a level 50 player, but that player is still yellow to that NPC. * Critical shot's effectiveness is now lowered by 0.075 per level difference instead of 0.3 per con difference. Cap is unchanged. * Miss rate, when a NPC is involved, is now modified by a flat 1.33% per level difference instead of a flat 5% per con difference. * Miss/resist rate for new archery is now modified by a flat 1.33% per level difference instead of a flat 5% per con difference. New archery is very outdated and needs to be checked. * Miss rate is no longer reduced by a flat 0.1% per spec level (not accurate for any patch). * Base miss rate of NPCs was reduced from 25% to 18%. * Miss rate returned by `GetMissChance` now has decimals. * Base resist rate for spells is now 12.5% instead of 12% (value from Uthgard, based on live tests). * Resist rate for spells, when a NPC is involved, no longer scales off con difference. It isn’t replaced by anything since the level difference between the spell level and the target level is already used, and an extra penalty or bonus based on caster level is unconfirmed to be accurate. This increases damage against high level targets due to `AdjustDamageForHitChance` only triggering when the resist rate is at least 46%, which requires a level difference (between spell and target) of at least 67. * Resist rates returned by `CalculateToHitChance` and `CalculateSpellResistChance` now have decimals. * Triple Wield damage no longer scales off con difference. * Interrupt chance is now reduced by a flat 3% per level difference instead of

GameServer/spells/CharmSpellHandler.cs
@@ -86,7 +86,7 @@ namespace DOL.GS.Spells
                 return false;
 
             // Ignore SpellResisted values by returning 0
-            if (Util.Chance(CalculateSpellResistChance(target)))
+            if (Util.ChanceDouble(CalculateSpellResistChance(target)))
             {
                 OnSpellResisted(target);
             }
@@ -102,7 +102,7 @@ namespace DOL.GS.Spells
         /// Calculates chance of spell getting resisted
         /// </summary>
         /// <param name="target">The target mob for the spell</param>
-        public override int CalculateSpellResistChance(GameLiving target)
+        public override double CalculateSpellResistChance(GameLiving target)
         {
             return 0;
         }
@@ -399,7 +399,7 @@ namespace DOL.GS.Spells
             if (playerCaster != null)
             {
                 // base resists for all charm spells
-                double resistChance = (short) (100 - (85 + (Caster.Level - target.Level) / 2));
+                double resistChance;
 
                 if (Spell.Pulse != 0) // not permanent
                 {
@@ -427,6 +427,8 @@ namespace DOL.GS.Spells
                         resistChance = Math.Min(resistChance, 99);
                     }
                 }
+                else
+                    resistChance = CalculateToHitChance(target);
 
                 double spellResistChance = resistChance;
                 double resistResult = Util.RandomDouble() * 100;

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -278,15 +278,9 @@ namespace DOL.GS
             {
                 case eRangedAttackType.Critical:
                 {
-                    double tmpEffectiveness = 2 - 0.3 * _owner.GetConLevel(_target);
-
-                    if (tmpEffectiveness > 2)
-                        _effectiveness *= 2;
-                    else if (tmpEffectiveness < 1.1)
-                        _effectiveness *= 1.1;
-                    else
-                        _effectiveness *= tmpEffectiveness;
-
+                    // Reduced effectiveness against higher level targets.
+                    double levelModifier = 2 + (_owner.EffectiveLevel - _target.EffectiveLevel) * 0.075;
+                    _effectiveness *= Math.Clamp(levelModifier, 1.1, 2.0);
                     break;
                 }
 
@@ -314,7 +308,7 @@ namespace DOL.GS
                     if (elapsedTime < rapidFireMaxDuration)
                     {
                         _effectiveness *= 0.25 + elapsedTime * 0.5 / rapidFireMaxDuration;
-                        _attackInteval = (int)(_attackInteval * _effectiveness);
+                        _attackInteval = (int) (_attackInteval * _effectiveness);
                     }
 
                     break;

GameServer/ECS-Components/AttackComponent.cs
@@ -1290,12 +1290,12 @@ namespace DOL.GS
                 static void PrintDetailedCombatLog(GamePlayer player, double armorFactor, double absorb, double armorMod, double baseWeaponSkill, double specModifier, double weaponSkill, double damageMod, double baseDamageCap, double styleDamageCap)
                 {
                     StringBuilder stringBuilder = new();
-                    stringBuilder.Append($"BaseWS: {baseWeaponSkill:0.00} | SpecMod: {specModifier:0.00} | WS: {weaponSkill:0.00}\n");
-                    stringBuilder.Append($"AF: {armorFactor:0.00} | ABS: {absorb * 100:0.00}% | AF/ABS: {armorMod:0.00}\n");
-                    stringBuilder.Append($"DamageMod: {damageMod:0.00} | BaseDamageCap: {baseDamageCap:0.00}");
+                    stringBuilder.Append($"BaseWS: {baseWeaponSkill:0.##} | SpecMod: {specModifier:0.##} | WS: {weaponSkill:0.##}\n");
+                    stringBuilder.Append($"AF: {armorFactor:0.##} | ABS: {absorb * 100:0.##}% | AF/ABS: {armorMod:0.##}\n");
+                    stringBuilder.Append($"DamageMod: {damageMod:0.##} | BaseDamageCap: {baseDamageCap:0.##}");
 
                     if (styleDamageCap > 0)
-                        stringBuilder.Append($" | StyleDamageCap: {styleDamageCap:0.00}");
+                        stringBuilder.Append($" | StyleDamageCap: {styleDamageCap:0.##}");
 
                     player.Out.SendMessage(stringBuilder.ToString(), eChatType.CT_DamageAdd, eChatLoc.CL_SystemWindow);
                 }
@@ -1667,7 +1667,7 @@ namespace DOL.GS
                 // Also prevents negative values.
                 spec = Math.Max(owner.Level < 5 ? 2 : 1, spec);
                 lowerLimit = Math.Min(0.75 * (spec - 1) / (target.Level + 1) + 0.25, 1.0);
-                upperLimit = Math.Min(Math.Max(1.25 + (3.0 * (spec - 1) / (target.Level + 1) - 2) * 0.25, 1.25), 1.50);
+                upperLimit = Math.Min(Math.Max(1.25 + (3.0 * (spec - 1) / (target.Level + 1) - 2) * 0.25, 1.25), 1.5);
             }
             else
             {
@@ -2119,7 +2119,7 @@ namespace DOL.GS
             //     return result;
 
             // Miss chance.
-            int missChance = GetMissChance(action, ad, lastAttackData, attackerWeapon);
+            double missChance = GetMissChance(action, ad, lastAttackData, attackerWeapon);
 
             // Check for dirty trick fumbles before misses.
             DirtyTricksDetrimentalECSGameEffect dt = (DirtyTricksDetrimentalECSGameEffect)EffectListService.GetAbilityEffectOnTarget(ad.Attacker, eEffect.DirtyTricksDetrimental);
@@ -2140,14 +2140,14 @@ namespace DOL.GS
 
                 if (ad.Attacker is GamePlayer misser && misser.UseDetailedCombatLog)
                 {
-                    misser.Out.SendMessage($"miss rate on target: {missChance}% rand: {missRoll * 100:0.##}", eChatType.CT_DamageAdd, eChatLoc.CL_SystemWindow);
+                    misser.Out.SendMessage($"miss rate on target: {missChance:0.##}% rand: {missRoll * 100:0.##}", eChatType.CT_DamageAdd, eChatLoc.CL_SystemWindow);
 
                     if (ad.AttackType != AttackData.eAttackType.Ranged)
                         misser.Out.SendMessage($"Your chance to fumble: {100 * ad.Attacker.ChanceToFumble:0.##}% rand: {100 * missRoll:0.##}", eChatType.CT_DamageAdd, eChatLoc.CL_SystemWindow);
                 }
 
                 if (ad.Target is GamePlayer missee && missee.UseDetailedCombatLog)
-                    missee.Out.SendMessage($"chance to be missed: {missChance}% rand: {missRoll * 100:0.##}", eChatType.CT_DamageAdd, eChatLoc.CL_SystemWindow);
+                    missee.Out.SendMessage($"chance to be missed: {missChance:0.##}% rand: {missRoll * 100:0.##}", eChatType.CT_DamageAdd, eChatLoc.CL_SystemWindow);
 
                 if (missChance > missRoll * 100)
                 {
@@ -2314,7 +2314,7 @@ namespace DOL.GS
                         case eAttackResult.Missed:
                             string message;
                             if (ad.MissChance > 0)
-                                message = LanguageMgr.GetTranslation(p.Client.Account.Language, "GamePlayer.Attack.Miss") + $" ({ad.MissChance}%)";
+                                message = LanguageMgr.GetTranslation(p.Client.Account.Language, "GamePlayer.Attack.Miss") + $" ({ad.MissChance:0.##}%)";
                             else
                                 message = LanguageMgr.GetTranslation(p.Client.Account.Language, "GamePlayer.Attack.StrafMiss");
                             p.Out.SendMessage(message, eChatType.CT_YouHit, eChatLoc.CL_SystemWindow);
@@ -2439,7 +2439,7 @@ namespace DOL.GS
                         case eAttackResult.Missed:
                             string message;
                             if (ad.MissChance > 0)
-                                message = LanguageMgr.GetTranslation(p.Client.Account.Language, "GamePlayer.Attack.Miss") + $" ({ad.MissChance}%)";
+                                message = LanguageMgr.GetTranslation(p.Client.Account.Language, "GamePlayer.Attack.Miss") + $" ({ad.MissChance:0.##}%)";
                             else
                                 message = LanguageMgr.GetTranslation(p.Client.Account.Language, "GamePlayer.Attack.StrafMiss");
                             p.Out.SendMessage(message, eChatType.CT_YouHit, eChatLoc.CL_SystemWindow);
@@ -2574,28 +2574,29 @@ namespace DOL.GS
             }
         }
 
-        public int GetMissChance(WeaponAction action, AttackData ad, AttackData lastAD, DbInventoryItem weapon)
+        public double GetMissChance(WeaponAction action, AttackData ad, AttackData lastAD, DbInventoryItem weapon)
         {
             // No miss if the target is sitting or for Volley attacks.
-            if ((owner is GamePlayer player && player.IsSitting) || action.RangedAttackType == eRangedAttackType.Volley)
+            if ((owner is GamePlayer player && player.IsSitting) || action.RangedAttackType is eRangedAttackType.Volley)
                return 0;
 
             // In 1.117C, every weapon was given the intrinsic 5% flat bonus special weapons (such as artifacts) had, lowering the base miss rate to 13%.
-            int missChance = 18;
+            double missChance = 18;
             missChance -= ad.Attacker.GetModified(eProperty.ToHitBonus);
 
             if (owner is not GamePlayer || ad.Attacker is not GamePlayer)
             {
-                missChance += 5 * ad.Attacker.GetConLevel(owner);
+                // 1.33 per level difference.
+                missChance -= (ad.Attacker.EffectiveLevel - owner.EffectiveLevel) * (1 + 1 / 3.0);
                 missChance -= Math.Max(0, Attackers.Count - 1) * Properties.MISSRATE_REDUCTION_PER_ATTACKERS;
             }
 
             // Weapon and armor bonuses.
             int armorBonus = 0;
 
-            if (ad.Target is GamePlayer p)
+            if (ad.Target is GamePlayer playerTarget)
             {
-                ad.ArmorHitLocation = ((GamePlayer) ad.Target).CalculateArmorHitLocation(ad);
+                ad.ArmorHitLocation = playerTarget.CalculateArmorHitLocation(ad);
 
                 if (ad.Target.Inventory != null)
                 {
@@ -2605,7 +2606,7 @@ namespace DOL.GS
                         armorBonus = armor.Bonus;
                 }
 
-                int bonusCap = GetBonusCapForLevel(p.Level);
+                int bonusCap = GetBonusCapForLevel(playerTarget.Level);
 
                 if (armorBonus > bonusCap)
                     armorBonus = bonusCap;
@@ -2631,10 +2632,10 @@ namespace DOL.GS
             if (ad.Style != null)
                 missChance -= ad.Style.BonusToHit;
 
-            if (lastAD != null && lastAD.AttackResult == eAttackResult.HitStyle && lastAD.Style != null)
+            if (lastAD != null && lastAD.AttackResult is eAttackResult.HitStyle && lastAD.Style != null)
                 missChance += lastAD.Style.BonusToDefense;
 
-            if (action.ActiveWeaponSlot == eActiveWeaponSlot.Distance)
+            if (action.ActiveWeaponSlot is eActiveWeaponSlot.Distance)
             {
                 DbInventoryItem ammo = ad.Attacker.rangeAttackComponent.Ammo;
 
@@ -2644,16 +2645,15 @@ namespace DOL.GS
                     {
                         // http://rothwellhome.org/guides/archery.htm
                         case 0:
-                            missChance += (int) Math.Round(missChance * 0.15);
+                            missChance += missChance * 0.15;
                             break; // Rough
                         //case 1:
-                        //  missrate -= 0;
                         //  break;
                         case 2:
-                            missChance -= (int) Math.Round(missChance * 0.15);
-                            break; // doesn't exist (?)
+                            missChance -= missChance * 0.15;
+                            break; // Doesn't exist (?)
                         case 3:
-                            missChance -= (int) Math.Round(missChance * 0.25);
+                            missChance -= missChance * 0.25;
                             break; // Footed
                     }
                 }
@@ -2728,7 +2728,7 @@ namespace DOL.GS
                 double offhandChance = 25 + (specLevel - 1) * 68 * 0.01 + bonus;
 
                 if (playerOwner != null && playerOwner.UseDetailedCombatLog)
-                    playerOwner.Out.SendMessage($"OH swing%: {offhandChance:0.00} ({bonus}% from RAs) \n", eChatType.CT_DamageAdd, eChatLoc.CL_SystemWindow);
+                    playerOwner.Out.SendMessage($"OH swing%: {offhandChance:0.##} ({bonus}% from RAs) \n", eChatType.CT_DamageAdd, eChatLoc.CL_SystemWindow);
 
                 return random < offhandChance ? 1 : 0;
             }
@@ -2747,7 +2747,7 @@ namespace DOL.GS
                 double quadHitChance = tripleHitChance + specLevel * 0.0625 + bonus * 0.25; // specLevel >> 4
 
                 if (playerOwner != null && playerOwner.UseDetailedCombatLog)
-                    playerOwner.Out.SendMessage( $"Chance for 2 hits: {doubleHitChance:0.00}% | 3 hits: { (specLevel > 25 ? tripleHitChance-doubleHitChance : 0):0.00}% | 4 hits: {(specLevel > 40 ? quadHitChance-tripleHitChance : 0):0.00}% \n", eChatType.CT_DamageAdd, eChatLoc.CL_SystemWindow);
+                    playerOwner.Out.SendMessage( $"Chance for 2 hits: {doubleHitChance:0.##}% | 3 hits: { (specLevel > 25 ? tripleHitChance-doubleHitChance : 0):0.##}% | 4 hits: {(specLevel > 40 ? quadHitChance-tripleHitChance : 0):0.##}% \n", eChatType.CT_DamageAdd, eChatLoc.CL_SystemWindow);
 
                 if (random < doubleHitChance)
                     return 1;

GameServer/ECS-Effects/TripleWieldECSEffect.cs
@@ -61,16 +61,9 @@ namespace DOL.GS
 			//if (attackData.IsOffHand) return; // only react to main hand
 			if (attackData.Weapon == null) return; // no weapon attack
 
-			int modifier = 100;
 			//double dpsCap = (1.2 + 0.3 * attacker.Level) * 0.7;
 			//double dps = Math.Min(atkArgs.AttackData.Weapon.DPS_AF/10.0, dpsCap);
-			double baseDamage = attackData.Weapon.DPS_AF * attackData.Interval * 0.001;
-
-			modifier += 25 * attackData.Target.GetConLevel(attackData.Attacker);
-			modifier = Math.Min(300, modifier);
-			modifier = Math.Max(75, modifier);
-
-			double damage = baseDamage * modifier * 0.001; // attack speed is 10 times higher (2.5spd=25)			
+			double damage = attackData.Weapon.DPS_AF * attackData.Interval * 0.001;
 			double damageResisted = damage * target.GetResist(eDamageType.Body) * -0.01;
 
 			AttackData ad = new AttackData();

GameServer/effects/TripleWieldEffect.cs
@@ -58,18 +58,11 @@ namespace DOL.GS.Effects
 			if (atkArgs.AttackData.IsOffHand) return; // only react to main hand
 			if (atkArgs.AttackData.Weapon == null) return; // no weapon attack
 
-			int modifier = 100;
 			//double dpsCap = (1.2 + 0.3 * attacker.Level) * 0.7;
 			//double dps = Math.Min(atkArgs.AttackData.Weapon.DPS_AF/10.0, dpsCap);
-			double baseDamage = atkArgs.AttackData.Weapon.DPS_AF * atkArgs.AttackData.Interval * 0.001;
-
-			modifier += 25 * atkArgs.AttackData.Target.GetConLevel(atkArgs.AttackData.Attacker);
-			modifier = Math.Min(300, modifier);
-			modifier = Math.Max(75, modifier);
-			
-			double damage = baseDamage * modifier * 0.001; // attack speed is 10 times higher (2.5spd=25)			
+			double damage = atkArgs.AttackData.Weapon.DPS_AF * atkArgs.AttackData.Interval * 0.001;
 			double damageResisted = damage * target.GetResist(eDamageType.Body) * -0.01;
-			
+
 			AttackData ad = new AttackData();
 			ad.Attacker = attacker;
 			ad.Target = target;

GameServer/gameobjects/GameLiving.cs
@@ -956,7 +956,8 @@ namespace DOL.GS
 				return;
 			}
 
-			if (!Util.Chance(100 + GetConLevel(attacker) * 15))
+			// 3% reduced interrupt chance per level difference.
+			if (!Util.Chance(100 + (attacker.EffectiveLevel - EffectiveLevel) * 3))
 				return;
 
 			// Don't replace the current interrupt with a shorter one.

GameServer/gameobjects/GamePlayer.cs
@@ -6077,7 +6077,7 @@ namespace DOL.GS
                     if (ad.AttackType == AttackData.eAttackType.Spell)
                         break;
                     if (ad.Attacker is GameNPC)
-                        Out.SendMessage(LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.Attack.Missed", ad.Attacker.GetName(0, true, Client.Account.Language, (ad.Attacker as GameNPC))) + " (" + Math.Min(ad.MissChance, 100).ToString("0") + "%)", eChatType.CT_Missed, eChatLoc.CL_SystemWindow);
+                        Out.SendMessage(LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.Attack.Missed", ad.Attacker.GetName(0, true, Client.Account.Language, (ad.Attacker as GameNPC))) + " (" + Math.Min(ad.MissChance, 100).ToString("0.0") + "%)", eChatType.CT_Missed, eChatLoc.CL_SystemWindow);
                     else
                         Out.SendMessage(LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.Attack.Missed", ad.Attacker.GetName(0, true)), eChatType.CT_Missed, eChatLoc.CL_SystemWindow);
                     break;

GameServer/serverproperty/ServerProperties.cs
@@ -829,12 +829,6 @@ namespace DOL.GS.ServerProperties
 		public static double PVE_SPELL_DAMAGE = 1.0;
 
 		/// <summary>
-		/// The percent per con difference (-1 = blue, 0 = yellow, 1 = OJ, 2 = red ...) subtracted to hitchance for spells in PVE.  0 is none, 5 is 5% per con, etc.  Default is 10%
-		/// </summary>
-		[ServerProperty("rates", "pve_spell_conhitpercent", "The percent per con (1 = OJ, 2 = red ...) subtracted to hitchance for spells in PVE  Must be >= 0.  0 is none, 5 is 5% per level, etc.  Default is 10%", (uint)10)]
-		public static uint PVE_SPELL_CONHITPERCENT;
-
-		/// <summary>
 		/// The damage players do against players with melee
 		/// </summary>
 		[ServerProperty("rates", "pvp_melee_damage", "The PvP Melee Damage Modifier - Edit this to change the amount of melee damage done when fighting players e.g 1.5 is 50% more damage 2.0 is twice the damage (100%) 0.5 is half the damage (50%)", 1.0)]

commit 882cf360538abd0a850e2232036ffcc228d2897c

Fix permanent charm spells inverted resist chance Caused by e3ee553e

GameServer/spells/CharmSpellHandler.cs
@@ -428,7 +428,7 @@ namespace DOL.GS.Spells
                     }
                 }
                 else
-                    resistChance = CalculateToHitChance(target);
+                    resistChance = 100 - CalculateToHitChance(target);
 
                 double spellResistChance = resistChance;
                 double resistResult = Util.RandomDouble() * 100;