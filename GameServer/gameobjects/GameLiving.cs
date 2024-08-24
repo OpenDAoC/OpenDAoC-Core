OpenDAoC/OpenDAoC-Core

commit 6b564b1641a23e675a0909d83c6e830c051fcaff

`CheckWeaponMagicalEffect`: Change proc chance to a double instead of rounding up A 2.5 SPD and 2.8 SPD weapon for example had the same proc rate (8%). It will now be ~7.143% and 8% respectively.

GameServer/gameobjects/GameLiving.cs
@@ -1046,7 +1046,7 @@ namespace DOL.GS
 
 			// Proc chance is 2.5% per SPD, i.e. 10% for a 3.5 SPD weapon. - Tolakram, changed average speed to 3.5
 
-			int procChance = (int)Math.Ceiling(((weapon.ProcChance > 0 ? weapon.ProcChance : 10) * (weapon.SPD_ABS / 35.0)));
+            double procChance = (weapon.ProcChance > 0 ? weapon.ProcChance : 10) * (weapon.SPD_ABS / 35.0);
 
             //Error protection and log for Item Proc's
             Spell procSpell = null;
@@ -1068,11 +1068,11 @@ namespace DOL.GS
             }
 
             // Proc #1
-            if (procSpell != null && Util.Chance(procChance))
+            if (procSpell != null && Util.ChanceDouble(procChance))
                 StartWeaponMagicalEffect(weapon, ad, SkillBase.GetSpellLine(GlobalSpellsLines.Item_Effects), weapon.ProcSpellID, false);
 
             // Proc #2
-            if (procSpell1 != null && Util.Chance(procChance))
+            if (procSpell1 != null && Util.ChanceDouble(procChance))
                 StartWeaponMagicalEffect(weapon, ad, SkillBase.GetSpellLine(GlobalSpellsLines.Item_Effects), weapon.ProcSpellID1, false);
 
 			// Poison

commit 98aa5c7bb658bb12d1775bf6b974aff44e576e41

Fix weapon proc chance Caused by 6b564b16.

GameServer/gameobjects/GameLiving.cs
@@ -1026,7 +1026,7 @@ namespace DOL.GS
 
 			// Proc chance is 2.5% per SPD, i.e. 10% for a 3.5 SPD weapon. - Tolakram, changed average speed to 3.5
 
-            double procChance = (weapon.ProcChance > 0 ? weapon.ProcChance : 10) * (weapon.SPD_ABS / 35.0);
+            double procChance = (weapon.ProcChance > 0 ? weapon.ProcChance : 10) * (weapon.SPD_ABS / 35.0) * 0.01;
 
             //Error protection and log for Item Proc's
             Spell procSpell = null;