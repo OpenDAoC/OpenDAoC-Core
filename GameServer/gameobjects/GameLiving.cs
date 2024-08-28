OpenDAoC/OpenDAoC-Core

commit 3929dca7f8ec95d7fa6090ca01311a1a54f83d68

Disable `StartEnduranceRegeneration, `StartPowerRegeneration` for non players

GameServer/gameobjects/GameLiving.cs
@@ -2738,8 +2738,9 @@ namespace DOL.GS
 		/// </summary>
 		public virtual void StartHealthRegeneration()
 		{
-			if (!IsAlive || ObjectState != eObjectState.Active)
+			if (!IsAlive || ObjectState is not eObjectState.Active)
 				return;
+
 			lock (m_regenTimerLock)
 			{
 				if (m_healthRegenerationTimer == null)
@@ -2755,50 +2756,17 @@ namespace DOL.GS
 				m_healthRegenerationTimer.Start(HealthRegenerationPeriod);
 			}
 		}
+
 		/// <summary>
 		/// Starts the power regeneration
 		/// </summary>
-		public virtual void StartPowerRegeneration()
-		{
-			
-			if (ObjectState != eObjectState.Active)
-				return;
-			lock (m_regenTimerLock)
-			{
-				if (m_powerRegenerationTimer == null)
-				{
-					m_powerRegenerationTimer = new ECSGameTimer(this);
-					m_powerRegenerationTimer.Callback = new ECSGameTimer.ECSTimerCallback(PowerRegenerationTimerCallback);
-				}
-				else if (m_powerRegenerationTimer.IsAlive)
-				{
-					return;
-				}
+		public virtual void StartPowerRegeneration() { }
 
-				m_powerRegenerationTimer.Start(PowerRegenerationPeriod);
-			}
-		}
 		/// <summary>
 		/// Starts the endurance regeneration
 		/// </summary>
-		public virtual void StartEnduranceRegeneration()
-		{
-			if (ObjectState != eObjectState.Active)
-				return;
-			lock (m_regenTimerLock)
-			{
-				if (m_enduRegenerationTimer == null)
-				{
-					m_enduRegenerationTimer = new ECSGameTimer(this);
-					m_enduRegenerationTimer.Callback = new ECSGameTimer.ECSTimerCallback(EnduranceRegenerationTimerCallback);
-				}
-				else if (m_enduRegenerationTimer.IsAlive)
-				{
-					return;
-				}
-				m_enduRegenerationTimer.Start(EnduranceRegenerationPeriod);
-			}
-		}
+		public virtual void StartEnduranceRegeneration() { }
+
 		/// <summary>
 		/// Stop the health regeneration
 		/// </summary>
@@ -2812,6 +2780,7 @@ namespace DOL.GS
 				m_healthRegenerationTimer = null;
 			}
 		}
+
 		/// <summary>
 		/// Stop the power regeneration
 		/// </summary>
@@ -2825,6 +2794,7 @@ namespace DOL.GS
 				m_powerRegenerationTimer = null;
 			}
 		}
+
 		/// <summary>
 		/// Stop the endurance regeneration
 		/// </summary>

GameServer/gameobjects/GamePlayer.cs
@@ -2510,7 +2510,6 @@ namespace DOL.GS
             if (ObjectState is not eObjectState.Active || m_enduRegenerationTimer.IsAlive)
                 return;
 
-
             m_enduRegenerationTimer ??= new(this, new ECSGameTimer.ECSTimerCallback(EnduranceRegenerationTimerCallback));
             m_enduRegenerationTimer.Start(m_enduranceRegenerationPeriod);
         }

GameServer/gameobjects/GameMovingObject.cs
@@ -77,21 +77,5 @@ namespace DOL.GS
 				}
 			}
 		}
-		/// <summary>
-		/// Starts the power regeneration
-		/// </summary>
-		public override void StartPowerRegeneration()
-		{
-			//No regeneration for moving objects
-			return;
-		}
-		/// <summary>
-		/// Starts the endurance regeneration
-		/// </summary>
-		public override void StartEnduranceRegeneration()
-		{
-			//No regeneration for moving objects
-			return;
-		}
 	}
 }

GameServer/keeps/Gameobjects/GameKeepDoor.cs
@@ -544,23 +544,6 @@ namespace DOL.GS.Keeps
 			return "the " + base.GetName(article, firstLetterUppercase);
 		}
 
-		/// <summary>
-		/// Starts the power regeneration
-		/// </summary>
-		public override void StartPowerRegeneration()
-		{
-			// No regeneration for doors
-			return;
-		}
-		/// <summary>
-		/// Starts the endurance regeneration
-		/// </summary>
-		public override void StartEnduranceRegeneration()
-		{
-			// No regeneration for doors
-			return;
-		}
-
 		public override void StartHealthRegeneration()
 		{
 			if (!IsAttackableDoor)

GameServer/keeps/Gameobjects/GameRelicDoor.cs
@@ -166,27 +166,11 @@ namespace DOL.GS.Keeps
 			return "the " + base.GetName(article, firstLetterUppercase);
 		}
 
-		/// <summary>
-		/// Starts the power regeneration
-		/// </summary>
-		public override void StartPowerRegeneration()
-		{
-			//No regeneration for doors
-			return;
-		}
-		/// <summary>
-		/// Starts the endurance regeneration
-		/// </summary>
-		public override void StartEnduranceRegeneration()
-		{
-			//No regeneration for doors
-			return;
-		}
-
 		public override void StartHealthRegeneration()
 		{
 			return;
 		}
+
 		#endregion
 
 		#region Save/load DB

commit 4996c969fc61eb7b2290797f1e8b7f248de0d7f4

Fix null reference exception Caused by 3929dca7 Removed unneeded regeneration timer initialization in `GamePlayer.AddToWorld`

GameServer/gameobjects/GameLiving.cs
@@ -2733,10 +2733,14 @@ namespace DOL.GS
 		/// </summary>
 		public virtual void StartHealthRegeneration()
 		{
-			if (!IsAlive || ObjectState is not eObjectState.Active || m_healthRegenerationTimer.IsAlive)
+			if (!IsAlive || ObjectState is not eObjectState.Active)
 				return;
 
-			m_healthRegenerationTimer ??= new(this, new ECSGameTimer.ECSTimerCallback(HealthRegenerationTimerCallback));
+			if (m_healthRegenerationTimer == null)
+				m_healthRegenerationTimer = new(this, new ECSGameTimer.ECSTimerCallback(HealthRegenerationTimerCallback));
+			else if (m_healthRegenerationTimer.IsAlive)
+				return;
+
 			m_healthRegenerationTimer.Start(m_healthRegenerationPeriod);
 		}
 

GameServer/gameobjects/GamePlayer.cs
@@ -2489,19 +2489,27 @@ namespace DOL.GS
 
         public override void StartPowerRegeneration()
         {
-            if (ObjectState is not eObjectState.Active || m_powerRegenerationTimer.IsAlive)
+            if (!IsAlive || ObjectState is not eObjectState.Active)
                 return;
 
-            m_powerRegenerationTimer ??= new(this, new ECSGameTimer.ECSTimerCallback(PowerRegenerationTimerCallback));
+            if (m_powerRegenerationTimer == null)
+                m_powerRegenerationTimer = new(this, new ECSGameTimer.ECSTimerCallback(PowerRegenerationTimerCallback));
+            else if (m_powerRegenerationTimer.IsAlive)
+                return;
+
             m_powerRegenerationTimer.Start(m_powerRegenerationPeriod);
         }
 
         public override void StartEnduranceRegeneration()
         {
-            if (ObjectState is not eObjectState.Active || m_enduRegenerationTimer.IsAlive)
+            if (!IsAlive || ObjectState is not eObjectState.Active)
                 return;
 
-            m_enduRegenerationTimer ??= new(this, new ECSGameTimer.ECSTimerCallback(EnduranceRegenerationTimerCallback));
+            if (m_enduRegenerationTimer == null)
+                m_enduRegenerationTimer = new(this, new ECSGameTimer.ECSTimerCallback(EnduranceRegenerationTimerCallback));
+            else if (m_enduRegenerationTimer.IsAlive)
+                return;
+
             m_enduRegenerationTimer.Start(m_enduranceRegenerationPeriod);
         }
 
@@ -8734,13 +8742,7 @@ namespace DOL.GS
 
             IsJumping = false;
             m_invulnerabilityTick = 0;
-            m_healthRegenerationTimer = new ECSGameTimer(this);
-            m_powerRegenerationTimer = new ECSGameTimer(this);
-            m_enduRegenerationTimer = new ECSGameTimer(this);
             craftComponent = new CraftComponent(this);
-            m_healthRegenerationTimer.Callback = new ECSGameTimer.ECSTimerCallback(HealthRegenerationTimerCallback);
-            m_powerRegenerationTimer.Callback = new ECSGameTimer.ECSTimerCallback(PowerRegenerationTimerCallback);
-            m_enduRegenerationTimer.Callback = new ECSGameTimer.ECSTimerCallback(EnduranceRegenerationTimerCallback);
 
             foreach (GamePlayer player in GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
             {