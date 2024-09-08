OpenDAoC/OpenDAoC-Core

commit 775a0f439c1936131bd4bf2a0f53d0537407d6b5

Remove call to `SendUpdatePlayerSkills` when unequipping or equipping items * I don't think a character can gain / lose abilities / spells by changing gear. And since this rebuilds our cache, having many players repeatedly switch weapons for example might cause performance issues. * Added missing updates in `OnItemBonusChanged` (unused, this is for artifacts).

GameServer/gameobjects/GamePlayer.cs
@@ -1252,6 +1252,7 @@ namespace DOL.GS
             {
                 bool wasInCombat = InCombat;
                 base.LastAttackTickPvP = value;
+
                 if (!wasInCombat && InCombat)
                     Out.SendUpdateMaxSpeed();
 
@@ -10188,14 +10189,14 @@ namespace DOL.GS
                 }
             }*/
 
-            if (ObjectState == eObjectState.Active)
+            if (ObjectState is eObjectState.Active)
             {
                 Out.SendCharStatsUpdate();
                 Out.SendCharResistsUpdate();
                 Out.SendUpdateWeaponAndArmorStats();
                 Out.SendUpdateMaxSpeed();
                 Out.SendEncumberance();
-                Out.SendUpdatePlayerSkills();
+                // Out.SendUpdatePlayerSkills();
                 UpdatePlayerStatus();
 
                 if (IsAlive)
@@ -10383,14 +10384,14 @@ namespace DOL.GS
             if (item is IGameInventoryItem inventoryItem)
                 inventoryItem.OnUnEquipped(this);
 
-            if (ObjectState == eObjectState.Active)
+            if (ObjectState is eObjectState.Active)
             {
                 Out.SendCharStatsUpdate();
                 Out.SendCharResistsUpdate();
                 Out.SendUpdateWeaponAndArmorStats();
                 Out.SendUpdateMaxSpeed();
                 Out.SendEncumberance();
-                Out.SendUpdatePlayerSkills();
+                // Out.SendUpdatePlayerSkills();
                 UpdatePlayerStatus();
 
                 if (IsAlive)
@@ -10542,12 +10543,14 @@ namespace DOL.GS
 
             ItemBonus[bonusType] += bonusAmount;
 
-            if (ObjectState == eObjectState.Active)
+            if (ObjectState is eObjectState.Active)
             {
                 Out.SendCharStatsUpdate();
                 Out.SendCharResistsUpdate();
                 Out.SendUpdateWeaponAndArmorStats();
-                Out.SendUpdatePlayerSkills();
+                Out.SendUpdateMaxSpeed();
+                Out.SendEncumberance();
+                // Out.SendUpdatePlayerSkills();
                 UpdatePlayerStatus();
 
                 if (IsAlive)

commit c8f0f92374dee87b3b0c657b3028e778beb1dc61

Fix character sheet not updating skill bonuses when equipping and unequipping items * Caused by 775a0f439c1936131bd4bf2a0f53d0537407d6b5 * However we still don't want to update the internal cache. Added a parameter to `SendUpdatePlayerSkills` for that purpose. * Made `WorldInitRequestHandler` update the cache only on the first call (so not when changing region).

CoreServer/ConsolePacketLib.cs
@@ -124,7 +124,7 @@ namespace DOLGameServerConsole
 		public void SendPlayerDied(GamePlayer killedPlayer, GameObject killer) { }
 		public void SendPlayerRevive(GamePlayer revivedPlayer) { }
 		public void SendUpdatePlayer() { }
-		public void SendUpdatePlayerSkills() { }
+		public void SendUpdatePlayerSkills(bool updateInternalCache) { }
 		public void SendUpdateWeaponAndArmorStats() { }
 		public void SendCustomTextWindow(string caption, IList<string> text) { }
 		public void SendEncumberance() { }

GameServer/commands/gmcommands/Player.cs
@@ -422,7 +422,7 @@ namespace DOL.GS.Commands
 						player.MLLine = line;
 						player.SaveIntoDatabase();
 						player.RefreshSpecDependantSkills(true);
-						player.Out.SendUpdatePlayerSkills();
+						player.Out.SendUpdatePlayerSkills(true);
 						player.Out.SendUpdatePlayer();
 						player.Out.SendMasterLevelWindow((byte)player.MLLevel);
 						client.Out.SendMessage(player.Name + " Master Line is set to " + line + "!", eChatType.CT_Important, eChatLoc.CL_SystemWindow);
@@ -2477,7 +2477,7 @@ namespace DOL.GS.Commands
             target.OnLevelUp(0);
 
             target.Out.SendUpdatePlayer();
-            target.Out.SendUpdatePlayerSkills();
+            target.Out.SendUpdatePlayerSkills(true);
             target.Out.SendUpdatePoints();
         }
 	}

GameServer/commands/playercommands/respec.cs
@@ -248,7 +248,7 @@ namespace DOL.GS.Commands
 			}
 			player.RefreshSpecDependantSkills(false);
 			// Notify Player of points
-			player.Out.SendUpdatePlayerSkills();
+			player.Out.SendUpdatePlayerSkills(true);
 			player.Out.SendUpdatePoints();
 			player.Out.SendUpdatePlayer();
 			player.SendTrainerWindow();

GameServer/gameobjects/CustomNPC/GameTrainer.cs
@@ -192,7 +192,7 @@ namespace DOL.GS
 					}
 					player.RefreshSpecDependantSkills(false);
 					// Notify Player of points
-					player.Out.SendUpdatePlayerSkills();
+					player.Out.SendUpdatePlayerSkills(true);
 					player.Out.SendUpdatePoints();
 					player.Out.SendUpdatePlayer();
 					player.Out.SendTrainerWindow();
@@ -345,7 +345,7 @@ namespace DOL.GS
 				player.CharacterClass.OnLevelUp(player, player.Level);
 				player.RefreshSpecDependantSkills(true);
 				player.StartPowerRegeneration();
-				player.Out.SendUpdatePlayerSkills();
+				player.Out.SendUpdatePlayerSkills(true);
 				player.Out.SendUpdatePlayer();
 				// drop any non usable item
 				CheckAbilityToUseItem(player);

GameServer/gameobjects/GamePlayer.cs
@@ -3742,7 +3742,7 @@ namespace DOL.GS
         /// <returns></returns>
         public virtual List<Tuple<Skill, Skill>> GetAllUsableSkills(bool update = false)
         {
-            List<Tuple<Skill, Skill>> results = new List<Tuple<Skill, Skill>>();
+            List<Tuple<Skill, Skill>> results = [];
 
             if (!update)
             {
@@ -3944,7 +3944,7 @@ namespace DOL.GS
             CharacterClass.OnSkillTrained(this, skill);
             RefreshSpecDependantSkills(true);
 
-            Out.SendUpdatePlayerSkills();
+            Out.SendUpdatePlayerSkills(true);
         }
 
         /// <summary>
@@ -4176,7 +4176,7 @@ namespace DOL.GS
                 Out.SendMessage(LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.GainRealmPoints.GainedLevel"), eChatType.CT_System, eChatLoc.CL_SystemWindow);
                 if (RealmLevel % 10 == 0)
                 {
-                    Out.SendUpdatePlayerSkills();
+                    Out.SendUpdatePlayerSkills(true);
                     Out.SendMessage(LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.GainRealmPoints.GainedRank"), eChatType.CT_System, eChatLoc.CL_SystemWindow);
                     Out.SendMessage(LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.GainRealmPoints.ReachedRank", (RealmLevel / 10) + 1), eChatType.CT_ScreenCenter, eChatLoc.CL_SystemWindow);
                     Out.SendMessage(LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.GainRealmPoints.NewRealmTitle", RealmRankTitle(Client.Account.Language)), eChatType.CT_System, eChatLoc.CL_SystemWindow);
@@ -5439,7 +5439,7 @@ namespace DOL.GS
             Out.SendUpdatePlayer(); // Update player level
             Out.SendCharStatsUpdate(); // Update Stats and MaxHitpoints
             Out.SendCharResistsUpdate();
-            Out.SendUpdatePlayerSkills();
+            Out.SendUpdatePlayerSkills(true);
             Out.SendUpdatePoints();
             UpdatePlayerStatus();
 
@@ -5515,7 +5515,7 @@ namespace DOL.GS
             }
             Out.SendUpdatePlayer(); // Update player level
             Out.SendCharStatsUpdate(); // Update Stats and MaxHitpoints
-            Out.SendUpdatePlayerSkills();
+            Out.SendUpdatePlayerSkills(true);
             Out.SendUpdatePoints();
             UpdatePlayerStatus();
             // save player to database
@@ -10201,7 +10201,7 @@ namespace DOL.GS
                 Owner.Out.SendUpdateWeaponAndArmorStats();
                 Owner.Out.SendUpdateMaxSpeed();
                 Owner.Out.SendEncumberance();
-                // Out.SendUpdatePlayerSkills();
+                Owner.Out.SendUpdatePlayerSkills(false);
                 Owner.UpdatePlayerStatus();
 
                 if (!IsAlive)
@@ -14237,7 +14237,6 @@ namespace DOL.GS
             Out.SendUpdatePoints();
         }
 
-
         /// <summary>
         /// Reset all Champion skills for this player
         /// </summary>
@@ -14251,10 +14250,9 @@ namespace DOL.GS
             RefreshSpecDependantSkills(false);
             Out.SendUpdatePlayer();
             Out.SendUpdatePoints();
-            Out.SendUpdatePlayerSkills();
+            Out.SendUpdatePlayerSkills(true);
             UpdatePlayerStatus();
         }
-
 
         /// <summary>
         /// Remove all Champion levels and XP from this character.
@@ -14281,7 +14279,7 @@ namespace DOL.GS
             }
 
             RefreshSpecDependantSkills(true);
-            Out.SendUpdatePlayerSkills();
+            Out.SendUpdatePlayerSkills(true);
 
             Notify(GamePlayerEvent.ChampionLevelUp, this);
             Out.SendMessage("You have gained one champion level!", eChatType.CT_Important, eChatLoc.CL_SystemWindow);

GameServer/packets/Client/168/PlayerTrainRequestHandler.cs
@@ -79,7 +79,7 @@ namespace DOL.GS.PacketHandler.Client.v168
 						client.Player.RefreshSpecDependantSkills(false);
 						client.Player.Out.SendUpdatePlayer();
 						client.Player.Out.SendUpdatePoints();
-						client.Player.Out.SendUpdatePlayerSkills();
+						client.Player.Out.SendUpdatePlayerSkills(true);
 						client.Player.UpdatePlayerStatus();
 						client.Player.Out.SendChampionTrainerWindow(idLine);
 
@@ -153,7 +153,7 @@ namespace DOL.GS.PacketHandler.Client.v168
 							client.Player.AddRealmAbility(ra, true);
 							client.Out.SendUpdatePoints();
 							client.Out.SendUpdatePlayer();
-							client.Out.SendUpdatePlayerSkills();
+							client.Out.SendUpdatePlayerSkills(true);
 							client.Out.SendTrainerWindow();
 						}
 						else
@@ -301,7 +301,7 @@ namespace DOL.GS.PacketHandler.Client.v168
 							client.Out.SendUpdatePlayer();
 							client.Out.SendCharResistsUpdate();
 							client.Out.SendCharStatsUpdate();
-							client.Out.SendUpdatePlayerSkills();
+							client.Out.SendUpdatePlayerSkills(true);
 							client.Out.SendTrainerWindow();
 							trained = true;
 						}

GameServer/packets/Client/168/WorldInitRequestHandler.cs
@@ -114,6 +114,7 @@ namespace DOL.GS.PacketHandler.Client.v168
                     }
                 }
 
+                bool wasOnCharScreen = player.Client.ClientState is GameClient.eClientState.CharScreen;
                 player.Client.ClientState = GameClient.eClientState.WorldEnter;
                 // 0x88 - Position
                 // 0x6D - FriendList
@@ -174,7 +175,7 @@ namespace DOL.GS.PacketHandler.Client.v168
                 player.Out.SendStatusUpdate();
                 player.Out.SendInventoryItemsUpdate(eInventoryWindowType.Equipment, player.Inventory.EquippedItems);
                 player.Out.SendInventoryItemsUpdate(eInventoryWindowType.Inventory, player.Inventory.GetItemRange(eInventorySlot.FirstBackpack, eInventorySlot.LastBagHorse));
-                player.Out.SendUpdatePlayerSkills();   //TODO Insert 0xBE - 08 Various in SendUpdatePlayerSkills() before send spells
+                player.Out.SendUpdatePlayerSkills(wasOnCharScreen);   //TODO Insert 0xBE - 08 Various in SendUpdatePlayerSkills() before send spells
                 player.Out.SendUpdateCraftingSkills(); // ^
                 player.Out.SendUpdatePlayer();
                 player.Out.SendUpdateMoney();

GameServer/packets/Server/IPacketLib.cs
@@ -717,7 +717,7 @@ namespace DOL.GS.PacketHandler
 		void SendPlayerDied(GamePlayer killedPlayer, GameObject killer);
 		void SendPlayerRevive(GamePlayer revivedPlayer);
 		void SendUpdatePlayer();
-		void SendUpdatePlayerSkills();
+		void SendUpdatePlayerSkills(bool updateInternalCache);
 		void SendUpdateWeaponAndArmorStats();
 		void SendCustomTextWindow(string caption, IList<string> text);
 		void SendPlayerTitles();

GameServer/packets/Server/PacketLib1112.cs
@@ -22,13 +22,13 @@ namespace DOL.GS.PacketHandler
 		{
 		}
 
-		public override void SendUpdatePlayerSkills()
+		public override void SendUpdatePlayerSkills(bool updateInternalCache)
 		{
 			if (m_gameClient.Player == null)
 				return;
 
 			// Get Skills as "Usable Skills" which are in network order ! (with forced update)
-			List<Tuple<Skill, Skill>> usableSkills = m_gameClient.Player.GetAllUsableSkills(true);
+			List<Tuple<Skill, Skill>> usableSkills = m_gameClient.Player.GetAllUsableSkills(updateInternalCache);
 
 			bool sent = false; // set to true once we can't send packet anymore !
 			int index = 0; // index of our position in the list !

GameServer/packets/Server/PacketLib168.cs
@@ -2106,13 +2106,13 @@ namespace DOL.GS.PacketHandler
 			maxSkills++;
 		}
 
-		public virtual void SendUpdatePlayerSkills()
+		public virtual void SendUpdatePlayerSkills(bool updateInternalCache)
 		{
 			if (m_gameClient.Player == null)
 				return;
 
 			// Get Skills as "Usable Skills" which are in network order ! (with forced update)
-			List<Tuple<Skill, Skill>> usableSkills = m_gameClient.Player.GetAllUsableSkills(true);
+			List<Tuple<Skill, Skill>> usableSkills = m_gameClient.Player.GetAllUsableSkills(updateInternalCache);
 
 			bool sent = false; // set to true once we can't send packet anymore !
 			int index = 0; // index of our position in the list !

GameServer/packets/Server/PacketLib180.cs
@@ -220,13 +220,13 @@ namespace DOL.GS.PacketHandler
 		}
 
 
-		public override void SendUpdatePlayerSkills()
+		public override void SendUpdatePlayerSkills(bool updateInternalCache)
 		{
 			if (m_gameClient.Player == null)
 				return;
 
 			// Get Skills as "Usable Skills" which are in network order ! (with forced update)
-			List<Tuple<Skill, Skill>> usableSkills = m_gameClient.Player.GetAllUsableSkills(true);
+			List<Tuple<Skill, Skill>> usableSkills = m_gameClient.Player.GetAllUsableSkills(updateInternalCache);
 
 			bool sent = false; // set to true once we can't send packet anymore !
 			int index = 0; // index of our position in the list !

GameServer/spells/Climbing.cs
@@ -36,7 +36,7 @@ namespace DOL.GS.Spells
 			if (gp != null)
 			{
 				gp.AddAbility(SkillBase.GetAbility(Abilities.Climbing));
-				gp.Out.SendUpdatePlayerSkills();
+				gp.Out.SendUpdatePlayerSkills(true);
 			}
 		}
 
@@ -46,7 +46,7 @@ namespace DOL.GS.Spells
 			if (gp != null)
 			{
 				gp.RemoveAbility(Abilities.Climbing);
-				gp.Out.SendUpdatePlayerSkills();
+				gp.Out.SendUpdatePlayerSkills(true);
 			}
 			return 0;
 		}

Tests/IntegrationTests/TestPacketLib.cs
@@ -464,10 +464,10 @@ namespace DOL.Tests
 		{
 			if (SendUpdatePlayerMethod != null) SendUpdatePlayerMethod(this);
 		}
-		public Action<TestPacketLib> SendUpdatePlayerSkillsMethod { get; set; }
-		public void SendUpdatePlayerSkills()
+		public Action<TestPacketLib, bool> SendUpdatePlayerSkillsMethod { get; set; }
+		public void SendUpdatePlayerSkills(bool updateInternalCache)
 		{
-			if (SendUpdatePlayerSkillsMethod != null) SendUpdatePlayerSkillsMethod(this);
+			if (SendUpdatePlayerSkillsMethod != null) SendUpdatePlayerSkillsMethod(this, updateInternalCache);
 		}
 		public Action<TestPacketLib> SendUpdateWeaponAndArmorStatsMethod { get; set; }
 		public void SendUpdateWeaponAndArmorStats()