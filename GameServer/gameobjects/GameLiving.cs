OpenDAoC/OpenDAoC-Core

commit 1e70f878cf2ec298cbfc9ed18bd29bf7c820476a

Fix Unnatural Storm memory leak / client crash * Timers showing spell effects were never removed because they were only checking `IsAlive`. * Fixed by making `RemoveFromWorld` set health to 0 too.

GameServer/gameobjects/GameLiving.cs
@@ -3753,6 +3753,7 @@ namespace DOL.GS
 			if (!base.RemoveFromWorld())
 				return false;
 
+			Health = 0; // So that `IsAlive` returns false too.
 			attackComponent.StopAttack();
 
 			foreach (GameObject attacker in attackComponent.Attackers.Keys)

GameServer/scripts/mobs/AlbionMobs/UnnaturalStorm.cs
@@ -252,26 +252,28 @@ namespace DOL.AI.Brain
 		{
 			uint hour = WorldMgr.GetCurrentGameTime() / 1000 / 60 / 60;
 			uint minute = WorldMgr.GetCurrentGameTime() / 1000 / 60 % 60;
-			//log.Warn("Current time: " + hour + ":" + minute);
-			foreach (GameNPC npc in Body.GetNPCsInRadius(8000))
-			{
-				if (npc != null && npc.IsAlive && npc.Brain is UnnaturalStormBrain brain)
-				{
-					if (!brain.HasAggro && hour >= 7 && hour < 18)
-					{
-						npc.RemoveFromWorld();
 
-						foreach (GameNPC adds in Body.GetNPCsInRadius(8000))
+			if (hour >= 7 && hour < 18)
+			{
+				foreach (GameNPC npc in Body.GetNPCsInRadius(WorldMgr.VISIBILITY_DISTANCE))
+				{
+					if (npc.IsAlive && npc.Brain is UnnaturalStormBrain brain)
+					{
+						if (!brain.HasAggro)
 						{
-							if (adds != null && adds.IsAlive && adds.Brain is UnnaturalStormAddsBrain)
-								adds.RemoveFromWorld();
+							npc.RemoveFromWorld();
+
+							foreach (GameNPC adds in Body.GetNPCsInRadius(WorldMgr.VISIBILITY_DISTANCE))
+							{
+								if (adds.IsAlive && adds.Brain is UnnaturalStormAddsBrain)
+									adds.RemoveFromWorld();
+							}
 						}
 					}
 				}
 			}
-			if (hour == 18 && minute == 30)
+			else if (hour == 18 && minute == 30)
 				SpawnUnnaturalStorm();
-			
 		}
 
 		public override void KillFSM()

commit 05b3f6b6a9f6851aa38acb13fb9f3f750cac6bac

Fix players entering dead state after changing region * Caused by 1e70f878 and the use of a dead flag in `IsAlive` for players instead of simply checking health. Fixed by making `IsAlive` check for health instead. * The use of a flag instead of checking health was probably an attempt at fixing concurrent issues regarding health. It's not a good solution, but may have helped to prevent dead players with HP > 0 for example. Added a `isDeadOrDying` check to `IsAlive` until a `HealthComponent` is properly implemented. `isDeadOrDying` can only be set to true by one thread and is only set back to false by the reaper service. * Changed `isDeadOrDying` to `IsBeingHandledByReaperService`.

GameServer/gameobjects/GameLiving.cs
@@ -35,7 +35,7 @@ namespace DOL.GS
 
 		#region Combat
 
-		public bool isDeadOrDying = false;
+		public bool IsBeingHandledByReaperService { get; set; }
 
 		protected string m_lastInterruptMessage;
 		public string LastInterruptMessage
@@ -610,10 +610,7 @@ namespace DOL.GS
 		/// <summary>
 		/// returns if this living is alive
 		/// </summary>
-		public virtual bool IsAlive
-		{
-			get { return Health > 0; }
-		}
+		public virtual bool IsAlive => Health > 0 && !IsBeingHandledByReaperService;
 
 		/// <summary>
 		/// True if living is low on health, else false.
@@ -1487,9 +1484,9 @@ namespace DOL.GS
 
 			Health -= damageAmount + criticalAmount;
 
-			if (!IsAlive && Monitor.TryEnter(dieLock) && !isDeadOrDying)
+			if (!IsAlive && Monitor.TryEnter(dieLock) && !IsBeingHandledByReaperService)
 			{
-				isDeadOrDying = true;
+				IsBeingHandledByReaperService = true;
 
 				try
 				{
@@ -1967,8 +1964,7 @@ namespace DOL.GS
 		/// </summary>
 		public virtual void Die(GameObject killer)
 		{
-			isDeadOrDying = true;
-			//Console.WriteLine($"Dead or Dying set to {this.isDeadOrDying} for {this.Name} in living");
+			IsBeingHandledByReaperService = true;
 			ReaperService.KillLiving(this, killer);
 		}
 
@@ -2069,8 +2065,7 @@ namespace DOL.GS
 			}
 			finally
 			{
-				//isDying flag is ALWAYS set to false even if exception happens so it can get remove from the list
-				isDeadOrDying = false;
+				IsBeingHandledByReaperService = false;
 			}
 		}
 

GameServer/gameobjects/GameNPC.cs
@@ -2900,7 +2900,7 @@ namespace DOL.GS
 			}
 			finally
 			{
-				if (isDeadOrDying == true)
+				if (IsBeingHandledByReaperService)
 					base.ProcessDeath(killer);
 			}
 		}

GameServer/gameobjects/GamePlayer.cs
@@ -1819,8 +1819,6 @@ namespace DOL.GS
                 }
             }
 
-            //Update health&sit state first!
-            m_isDead = false;
             Health = MaxHealth;
             Endurance = MaxEndurance;
             Mana = MaxMana;
@@ -1966,10 +1964,7 @@ namespace DOL.GS
         /// </summary>
         public virtual void OnRevive(DOLEvent e, object sender, EventArgs args)
         {
-            GamePlayer player = (GamePlayer)sender;
-            //effectListComponent.CancelAll();
-            m_isDead = false;
-
+            GamePlayer player = sender as GamePlayer;
             bool applyRezSick = true;
 
             // Used by spells like Perfect Recovery
@@ -2591,9 +2586,6 @@ namespace DOL.GS
                 if (DBCharacter != null)
                     DBCharacter.Health = value;
                 base.Health = value;
-
-                if (m_health == 0)
-                    m_isDead = true;
 
                 if (oldPercent != HealthPercent)
                 {
@@ -12633,16 +12625,6 @@ namespace DOL.GS
         public bool IsSalvagingOrRepairing
         {
             get { return (m_crafttimer != null && m_crafttimer.IsAlive); }
-        }
-
-        protected bool m_isDead = false;
-        /// <summary>
-        /// returns if this living is alive
-        /// </summary>
-        public override bool IsAlive
-        {
-            //get { return Health > 0; }
-            get { return !m_isDead; }
         }
 
         /// <summary>

GameServer/keeps/Gameobjects/Guards/Lord.cs
@@ -140,9 +140,9 @@ namespace DOL.GS.Keeps
         /// <param name="killer">The killer object</param>
         public override void Die(GameObject killer)
         {
-            if (this.isDeadOrDying == false)
+            if (!IsBeingHandledByReaperService)
             {
-                this.isDeadOrDying = true;
+                this.IsBeingHandledByReaperService = true;
                 m_lastRealm = eRealm.None;
 
                 if (Properties.LOG_KEEP_CAPTURES)

GameServer/realmabilities_atlasOF/effects/AtlasOF_ForestheartAmbusherECSEffect.cs
@@ -29,7 +29,7 @@ namespace DOL.GS.Effects
         public override void OnStopEffect()
         {
             // The effect can be cancelled before the spell if fired by the casting service, in which case 'PetSpellHander' can be null.
-            if (PetSpellHander?.Pet.isDeadOrDying == false)
+            if (PetSpellHander?.Pet.IsBeingHandledByReaperService == false)
             {
                 PetSpellHander.Pet.Health = 0; // To send proper remove packet.
                 PetSpellHander.Pet.Delete();