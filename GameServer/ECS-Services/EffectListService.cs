OpenDAoC/OpenDAoC-Core

commit 1e70f878cf2ec298cbfc9ed18bd29bf7c820476a

Fix Unnatural Storm memory leak / client crash * Timers showing spell effects were never removed because they were only checking `IsAlive`. * Fixed by making `RemoveFromWorld` set health to 0 too.

GameServer/gameobjects/GameLiving.cs
@@ -3753,6 +3753,7 @@ namespace DOL.GS
 			if (!base.RemoveFromWorld())
 				return false;
 
+			Health = 0; // So that `IsAlive` returns false too.
 			attackComponent.StopAttack();
 
 			foreach (GameObject attacker in attackComponent.Attackers.Keys)

GameServer/scripts/mobs/AlbionMobs/UnnaturalStorm.cs
@@ -252,26 +252,28 @@ namespace DOL.AI.Brain
 		{
 			uint hour = WorldMgr.GetCurrentGameTime() / 1000 / 60 / 60;
 			uint minute = WorldMgr.GetCurrentGameTime() / 1000 / 60 % 60;
-			//log.Warn("Current time: " + hour + ":" + minute);
-			foreach (GameNPC npc in Body.GetNPCsInRadius(8000))
-			{
-				if (npc != null && npc.IsAlive && npc.Brain is UnnaturalStormBrain brain)
-				{
-					if (!brain.HasAggro && hour >= 7 && hour < 18)
-					{
-						npc.RemoveFromWorld();
 
-						foreach (GameNPC adds in Body.GetNPCsInRadius(8000))
+			if (hour >= 7 && hour < 18)
+			{
+				foreach (GameNPC npc in Body.GetNPCsInRadius(WorldMgr.VISIBILITY_DISTANCE))
+				{
+					if (npc.IsAlive && npc.Brain is UnnaturalStormBrain brain)
+					{
+						if (!brain.HasAggro)
 						{
-							if (adds != null && adds.IsAlive && adds.Brain is UnnaturalStormAddsBrain)
-								adds.RemoveFromWorld();
+							npc.RemoveFromWorld();
+
+							foreach (GameNPC adds in Body.GetNPCsInRadius(WorldMgr.VISIBILITY_DISTANCE))
+							{
+								if (adds.IsAlive && adds.Brain is UnnaturalStormAddsBrain)
+									adds.RemoveFromWorld();
+							}
 						}
 					}
 				}
 			}
-			if (hour == 18 && minute == 30)
+			else if (hour == 18 && minute == 30)
 				SpawnUnnaturalStorm();
-			
 		}
 
 		public override void KillFSM()

commit 24177e9de5eff924ede21bdc5c93bc8c8df35cdf

Fix effect cancelling when changing region Caused by 1e70f878 (which made `RemoveFromWorld` set HP to 0 to fix `IsAlive` returning true for inactive objects) and `EffectListComponent` only checking `IsAlive`. A better solution than setting HP to 0 is to make `IsAlive` return true only if if the object state is set to active, then have `EffectListComponent` pause if the object is inactive.

GameServer/ECS-Services/EffectListService.cs
@@ -56,6 +56,11 @@ namespace DOL.GS
                 return;
             }
 
+            // Pause if the owner is changing region.
+            // Also includes respawning NPCs, so this relies on the reaper service ticking last, otherwise effects wouldn't be cancelled.
+            if (effectListComponent.Owner.ObjectState is GameObject.eObjectState.Inactive)
+                return;
+
             List<ECSGameEffect> effectsList = [];
 
             lock (effectListComponent.EffectsLock)
@@ -66,7 +71,7 @@ namespace DOL.GS
 
             foreach (ECSGameEffect e in effectsList)
             {
-                if (!e.Owner.IsAlive || e.Owner.ObjectState == GameObject.eObjectState.Deleted)
+                if (!e.Owner.IsAlive)
                 {
                     EffectService.RequestCancelEffect(e);
                     continue;

GameServer/gameobjects/GameLiving.cs
@@ -588,29 +588,17 @@ namespace DOL.GS
 		/// <summary>
 		/// Whether this living is crowd controlled.
 		/// </summary>
-		public virtual bool IsCrowdControlled
-		{
-			get
-			{
-				return (IsStunned || IsMezzed);
-			}
-		}
-
-		/// <summary>
-		/// Whether this living can actually do anything.
-		/// </summary>
-		public virtual bool IsIncapacitated
-		{
-			get
-			{
-				return (ObjectState != eObjectState.Active || !IsAlive || IsStunned || IsMezzed);
-			}
-		}
+		public virtual bool IsCrowdControlled => IsStunned || IsMezzed;
 
 		/// <summary>
 		/// returns if this living is alive
 		/// </summary>
-		public virtual bool IsAlive => Health > 0 && !IsBeingHandledByReaperService;
+		public virtual bool IsAlive => ObjectState is eObjectState.Active && Health > 0 && !IsBeingHandledByReaperService;
+
+		/// <summary>
+		/// Whether this living can actually do anything.
+		/// </summary>
+		public virtual bool IsIncapacitated => !IsAlive || IsCrowdControlled;
 
 		/// <summary>
 		/// True if living is low on health, else false.
@@ -3748,7 +3736,6 @@ namespace DOL.GS
 			if (!base.RemoveFromWorld())
 				return false;
 
-			Health = 0; // So that `IsAlive` returns false too.
 			attackComponent.StopAttack();
 
 			foreach (GameObject attacker in attackComponent.Attackers.Keys)