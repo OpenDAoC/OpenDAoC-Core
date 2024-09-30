OpenDAoC/OpenDAoC-Core

commit a55da1971f2080c57e180df43f43e6b9ba7a03b0

`GameLiving.TakeDamage`: Fix improper thread synchronization around call to `Die` * It was very unlikely to happen but two threads could still end up calling `Die`. * Remove unused `GameLivingEvent.LowHealth`

GameServer/gameobjects/GameLiving.cs
@@ -1522,36 +1522,25 @@ namespace DOL.GS
 				}
 			}*/
 
-			bool wasAlive = IsAlive;
-
 			Health -= damageAmount + criticalAmount;
 
-			if (!IsAlive && wasAlive && isDeadOrDying == false)
-            {
-					if (Monitor.TryEnter(deadLock))
-					{
-						try
-						{
-						isDeadOrDying = true;
-						Die(source);
-						}
-						finally
-						{
-							Monitor.Exit(deadLock);
-						}
-					}
-					else
-					{
-					return;
-					}
-			}
-			else
+			if (!IsAlive && Monitor.TryEnter(dieLock) && !isDeadOrDying)
 			{
-				if (IsLowHealth)
-					Notify(GameLivingEvent.LowHealth, this, null);
+				isDeadOrDying = true;
+
+				try
+				{
+					Die(source);
+				}
+				finally
+				{
+					Monitor.Exit(dieLock);
+				}
 			}
 		}
-		object deadLock = new object();
+
+		object dieLock = new();
+
 		/// <summary>
 		/// Called on the attacker when attacking an enemy.
 		/// </summary>

GameServer/events/gameobjects/GameLivingEvent.cs
@@ -135,10 +135,6 @@ namespace DOL.Events
 		/// </summary>
 		public static readonly GameLivingEvent RegionChanging = new GameLivingEvent("GameLiving.RegionChanging");
         /// <summary>
-        /// The LowHealth event is fired when a living's health drops below a certain threshold
-        /// </summary>
-        public static readonly GameLivingEvent LowHealth = new GameLivingEvent("GameLiving.LowHealth");
-        /// <summary>
         /// The CrowdControlled event is fired when a living is crowd controlled
         /// </summary>
         public static readonly GameLivingEvent CrowdControlled = new GameLivingEvent("GameLiving.CrowdControlled");

commit 93afa1ea295e897b03cbedb2b750fcf5e21e3f97

`GameLiving.TakeDamage`: Fix wrong usage of locks (again) Caused by a55da197

GameServer/gameobjects/GameLiving.cs
@@ -1472,13 +1472,18 @@ namespace DOL.GS
 
 			Health -= damageAmount + criticalAmount;
 
-			if (!IsAlive && Monitor.TryEnter(dieLock) && !IsBeingHandledByReaperService)
-			{
-				IsBeingHandledByReaperService = true;
+			if (IsAlive)
+				return;
 
+			if (Monitor.TryEnter(dieLock))
+			{
 				try
 				{
-					Die(source);
+					if (!IsBeingHandledByReaperService)
+					{
+						IsBeingHandledByReaperService = true;
+						Die(source);
+					}
 				}
 				finally
 				{
@@ -2046,10 +2051,6 @@ namespace DOL.GS
 
 				//Let's send the notification at the end
 				Notify(GameLivingEvent.Dying, this, new DyingEventArgs(killer));
-			}
-			catch(Exception e)
-			{
-				Console.WriteLine(e.Message);
 			}
 			finally
 			{