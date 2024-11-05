d2d7a2e20082c72578f6444681138db1391a34e3
Baptiste Marie <bm01@users.noreply.github.com>
2024-11-05 05:32:41 +0100

    Tweak `CorpseSummoner` to create fewer timers

--- GameServer/keeps/Gameobjects/Guards/CorspeSummoner.cs
+++ GameServer/keeps/Gameobjects/Guards/CorspeSummoner.cs
@@ -1,138 +1,106 @@
 ﻿using DOL.AI.Brain;
+using DOL.GS;
+using DOL.GS.Keeps;
 
 namespace DOL.GS.Keeps
 {
     public class GuardCorpseSummoner : GameKeepGuard
     {
+        private static int RESPAWN_INTERVAL = 60000;
 
-        private static new readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public override int MaxHealth => base.MaxHealth * 3;
 
-        private eRealm m_lastRealm = eRealm.None;
-
-
-        /// <summary>
-        /// Lord needs more health at the moment
-        /// </summary>
-        public override int MaxHealth
+        public override void StartRespawn()
         {
-            get
-            {
-                return base.MaxHealth * 3;
-            }
-        }
-
-
-        public virtual void StartModifiedRespawn()
-        {
-            if (IsAlive) return;
-            if (Brain is IControlledBrain)
+            if (IsAlive || Brain is IControlledBrain)
                 return;
 
-            int respawnInt = 60 * 1000;
-
-            if (respawnInt > 0)
+            if (m_respawnTimer == null)
             {
-                int reloadrespawntimer = 0;
-                lock (m_respawnTimerLock)
-                {
-                    if (m_respawnTimer != null)
-                    {
-                        reloadrespawntimer = m_respawnTimer.TimeUntilElapsed;
-                        if (reloadrespawntimer < 0)
-                            reloadrespawntimer = 1;
-
-                        m_respawnTimer.Stop();
-                        m_respawnTimer = null;
-                    }
-
-                    m_respawnTimer = new ECSGameTimer(this);
-                    m_respawnTimer.Callback = new ECSGameTimer.ECSTimerCallback(ModdedRespawnTimerCallback);
-                    m_respawnTimer.Start(reloadrespawntimer > 0 ? reloadrespawntimer : respawnInt);
-                }
-            }
-        }
-        protected virtual int ModdedRespawnTimerCallback(ECSGameTimer respawnTimer)
-        {
-            lock (m_respawnTimerLock)
-            {
-                if (m_respawnTimer != null)
-                {
-                    m_respawnTimer.Stop();
-                    m_respawnTimer = null;
-                }
+                m_respawnTimer = new(this, new ECSGameTimer.ECSTimerCallback(RespawnTimerCallback));
+                m_respawnTimer.Start(RESPAWN_INTERVAL);
             }
 
-            //DOLConsole.WriteLine("respawn");
-            //TODO some real respawn handling
-            if (IsAlive) return 0;
-            if (ObjectState == eObjectState.Active) return 0;
-            if (this.Component.Keep.Level < 10)
+            int RespawnTimerCallback(ECSGameTimer timer)
             {
-                this.StartModifiedRespawn();
+                if (IsAlive || ObjectState is eObjectState.Active)
+                    return 0;
+
+                // I guess this loops indefinitely until the keep is level 10?
+                // This is wasteful.
+                if (Component.Keep.Level < 10)
+                    return timer.Interval;
+
+                Health = MaxHealth;
+                Mana = MaxMana;
+                Endurance = MaxEndurance;
+                int origSpawnX = m_spawnPoint.X;
+                int origSpawnY = m_spawnPoint.Y;
+                X = m_spawnPoint.X;
+                Y = m_spawnPoint.Y;
+                Z = m_spawnPoint.Z;
+                Heading = m_spawnHeading;
+                AddToWorld(); // Assumes it's not failing.
+                m_spawnPoint.X = origSpawnX;
+                m_spawnPoint.Y = origSpawnY;
                 return 0;
             }
-            //Heal this mob, move it to the spawnlocation
-            Health = MaxHealth;
-            Mana = MaxMana;
-            Endurance = MaxEndurance;
-            int origSpawnX = m_spawnPoint.X;
-            int origSpawnY = m_spawnPoint.Y;
-            //X=(m_spawnX+Random(750)-350); //new SpawnX = oldSpawn +- 350 coords
-            //Y=(m_spawnY+Random(750)-350);	//new SpawnX = oldSpawn +- 350 coords
-            X = m_spawnPoint.X;
-            Y = m_spawnPoint.Y;
-            Z = m_spawnPoint.Z;
-            Heading = m_spawnHeading;
-            AddToWorld();
-            m_spawnPoint.X = origSpawnX;
-            m_spawnPoint.Y = origSpawnY;
-            return 0;
         }
+
         public override bool AddToWorld()
         {
-            if (this.Component.Keep.Level < 10)
+            if (Component.Keep.Level < 10)
             {
-                this.StartModifiedRespawn();
+                StartRespawn();
                 return false;
             }
+
             if (base.AddToWorld())
             {
                 Name = "Corpse Summoner";
                 Model = 25;
                 Size = 60;
                 MaxSpeedBase = 0;
-                var corpseSummonerBrain = new CorpseSummonerBrain();
-                SetOwnBrain(corpseSummonerBrain);
-                m_lastRealm = Realm;
+                SetOwnBrain(new CorpseSummonerBrain());
                 return true;
             }
 
             return false;
- 
         }
-
     }
 }
 
 namespace DOL.AI.Brain
 {
-
     public class CorpseSummonerBrain : KeepGuardBrain
     {
-        public CorpseSummonerBrain()
-            : base()
+        protected GuardCorpseSummoner _guardCorpseSummonerBody;
+
+        public override GameNPC Body
+        {
+            get => _guardCorpseSummonerBody ?? base.Body;
+            set
+            {
+                _guardCorpseSummonerBody = value as GuardCorpseSummoner;
+                base.Body = value;
+            }
+        }
+
+        public CorpseSummonerBrain() : base()
         {
             AggroLevel = 90;
             AggroRange = 1500;
         }
-        /*public override void Think()
+
+        public override void Think()
         {
             base.Think();
-            if ((Body as GuardCorpseSummoner).Component.Keep != null && (Body as GuardCorpseSummoner).Component.Keep.Level < 10)
+
+            if (_guardCorpseSummonerBody.Component.Keep != null && _guardCorpseSummonerBody.Component.Keep.Level < 10)
             {
-                Body.RemoveFromWorld();
-                (Body as GuardCorpseSummoner).StartModifiedRespawn();
+                _guardCorpseSummonerBody.RemoveFromWorld();
+                _guardCorpseSummonerBody.StartRespawn();
             }
-        }*/
+        }
     }
 }
