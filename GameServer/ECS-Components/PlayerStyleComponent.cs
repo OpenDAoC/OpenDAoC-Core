OpenDAoC/OpenDAoC-Core

commit 9fbc727da4290b96f0fa2c26a5488e3644bf6b96

Add persistence to /backupstyle Added `NpcStyleComponent`, `PlayerStyleComponent` classes.

GameServer/ECS-Components/PlayerStyleComponent.cs
@@ -1,0 +1,208 @@
+﻿using System.Collections.Generic;
+using DOL.Database;
+using DOL.GS.PacketHandler;
+using DOL.GS.Styles;
+using DOL.Language;
+
+namespace DOL.GS
+{
+    public class PlayerStyleComponent : StyleComponent
+    {
+        private GamePlayer _playerOwner;
+        private bool _awaitingBackupInput;
+        private Style _automaticBackupStyle;
+
+        public override bool CancelStyle
+        {
+            get => _playerOwner.DBCharacter != null && _playerOwner.DBCharacter.CancelStyle;
+            set
+            {
+                if (_playerOwner.DBCharacter != null)
+                    _playerOwner.DBCharacter.CancelStyle = value;
+            }
+        }
+
+        public override bool AwaitingBackupInput
+        {
+            get => _awaitingBackupInput;
+            set => _awaitingBackupInput = value;
+        }
+
+        public override Style AutomaticBackupStyle
+        {
+            get => _automaticBackupStyle;
+            set => _automaticBackupStyle = value;
+        }
+
+        public PlayerStyleComponent(GamePlayer playerOwner) : base(playerOwner)
+        {
+            _playerOwner = playerOwner;
+        }
+
+        public void OnPlayerLoadFromDatabase()
+        {
+            DbCoreCharacter dbCharacter = _playerOwner.DBCharacter;
+
+            if (dbCharacter == null)
+                return;
+
+            AutomaticBackupStyle = SkillBase.GetStyleByID(dbCharacter.AutomaticBackupStyleId, _playerOwner.CharacterClass.ID);
+        }
+
+        public void OnPlayerSaveIntoDatabase()
+        {
+            DbCoreCharacter dbCharacter = _playerOwner.DBCharacter;
+
+            if (dbCharacter == null || AutomaticBackupStyle.ID == dbCharacter.AutomaticBackupStyleId)
+                return;
+
+            dbCharacter.AutomaticBackupStyleId = (ushort) AutomaticBackupStyle.ID;
+        }
+
+        public override Style GetStyleToUse()
+        {
+            if (NextCombatStyle == null)
+                return null;
+
+            // If the player no longer access to the style.
+            if (AutomaticBackupStyle != null && _playerOwner.GetBaseSpecLevel(AutomaticBackupStyle.Spec) < AutomaticBackupStyle.SpecLevelRequirement)
+            {
+                _playerOwner.Out.SendMessage($"{AutomaticBackupStyle.Name} is no longer a valid backup style for your spec level and has been cleared.", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+                AutomaticBackupStyle = null;
+            }
+
+            AttackData lastAttackData = Owner.attackComponent.attackAction.LastAttackData;
+            DbInventoryItem weapon = NextCombatStyle.WeaponTypeRequirement == (int) eObjectType.Shield ? Owner.Inventory.GetItem(eInventorySlot.LeftHandWeapon) : Owner.ActiveWeapon;
+
+            //determine which style will actually be used
+            Style styleToUse;
+
+            if (StyleProcessor.CanUseStyle(lastAttackData, Owner, NextCombatStyle, weapon))
+                styleToUse = NextCombatStyle;
+            else if (NextCombatBackupStyle != null)
+                styleToUse = NextCombatBackupStyle;
+            else if (AutomaticBackupStyle != null)
+            {
+                StyleProcessor.TryToUseStyle(Owner, AutomaticBackupStyle);
+                styleToUse = NextCombatBackupStyle; // `NextCombatBackupStyle` became `AutomaticBackupStyle` if `TryToUse` succeeded.
+            }
+            else
+                styleToUse = NextCombatStyle; // Not sure why.
+
+            return styleToUse;
+        }
+
+        public override void DelveWeaponStyle(List<string> delveInfo, Style style)
+        {
+            StyleProcessor.DelveWeaponStyle(delveInfo, style, _playerOwner);
+        }
+
+        public override void AddStyle(Style style, bool notify)
+        {
+            lock (lockStyleList)
+            {
+                if (m_styles.TryGetValue(style.ID, out Style existingStyle))
+                {
+                    existingStyle.Level = style.Level;
+                    return;
+                }
+
+                m_styles.Add(style.ID, style);
+
+                if (!notify)
+                    return;
+
+                _playerOwner.Out.SendMessage(LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.RefreshSpec.YouLearn", style.Name), eChatType.CT_System, eChatLoc.CL_SystemWindow);
+                string message = null;
+
+                if (style.OpeningRequirementType is Style.eOpening.Offensive)
+                {
+                    switch (style.AttackResultRequirement)
+                    {
+                        case Style.eAttackResultRequirement.Style:
+                        case Style.eAttackResultRequirement.Hit: // TODO: make own message for hit after styles DB is updated
+                        {
+                            Style reqStyle = SkillBase.GetStyleByID(style.OpeningRequirementValue, _playerOwner.CharacterClass.ID);
+
+                            if (reqStyle == null)
+                                message = LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.RefreshSpec.AfterStyle", "(style " + style.OpeningRequirementValue + " not found)");
+                            else
+                                message = LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.RefreshSpec.AfterStyle", reqStyle.Name);
+
+                            break;
+                        }
+                        case Style.eAttackResultRequirement.Miss:
+                        {
+                            message = LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.RefreshSpec.AfterMissed");
+                            break;
+                        }
+                        case Style.eAttackResultRequirement.Parry:
+                        {
+                            message = LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.RefreshSpec.AfterParried");
+                            break;
+                        }
+                        case Style.eAttackResultRequirement.Block:
+                        {
+                            message = LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.RefreshSpec.AfterBlocked");
+                            break;
+                        }
+                        case Style.eAttackResultRequirement.Evade:
+                        {
+                            message = LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.RefreshSpec.AfterEvaded");
+                            break;
+                        }
+                        case Style.eAttackResultRequirement.Fumble:
+                        {
+                            message = LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.RefreshSpec.AfterFumbles");
+                            break;
+                        }
+                    }
+                }
+                else if (style.OpeningRequirementType is Style.eOpening.Defensive)
+                {
+                    switch (style.AttackResultRequirement)
+                    {
+                        case Style.eAttackResultRequirement.Miss:
+                        {
+                            message = LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.RefreshSpec.TargetMisses");
+                            break;
+                        }
+                        case Style.eAttackResultRequirement.Hit:
+                        {
+                            message = LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.RefreshSpec.TargetHits");
+                            break;
+                        }
+                        case Style.eAttackResultRequirement.Parry:
+                        {
+                            message = LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.RefreshSpec.TargetParried");
+                            break;
+                        }
+                        case Style.eAttackResultRequirement.Block:
+                        {
+                            message = LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.RefreshSpec.TargetBlocked");
+                            break;
+                        }
+                        case Style.eAttackResultRequirement.Evade:
+                        {
+                            message = LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.RefreshSpec.TargetEvaded");
+                            break;
+                        }
+                        case Style.eAttackResultRequirement.Fumble:
+                        {
+                            message = LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.RefreshSpec.TargetFumbles");
+                            break;
+                        }
+                        case Style.eAttackResultRequirement.Style:
+                        {
+                            message = LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.RefreshSpec.TargetStyle");
+                            break;
+                        }
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(message))
+                    _playerOwner.Out.SendMessage(message, eChatType.CT_System, eChatLoc.CL_SystemWindow);
+            }
+        }
+    }
+}

CoreBase/Network/BaseServer.cs
@@ -169,7 +169,7 @@ namespace DOL.Network
                         catch (SocketException e)
                         {
                             if (log.IsDebugEnabled)
-                                log.Error($"Socket exception on UDP receive (Code: {e.SocketErrorCode})");
+                                log.Debug($"Socket exception on UDP receive (Code: {e.SocketErrorCode})");
                         }
                         catch (Exception e)
                         {

CoreDatabase/Tables/DbCoreCharacter.cs
@@ -63,14 +63,14 @@ namespace DOL.Database
 		private int m_mithril;
 		private int m_currentModel;
 
-		private int m_constitution = 0;
-		private int m_dexterity = 0;
-		private int m_strength = 0;
-		private int m_quickness = 0;
-		private int m_intelligence = 0;
-		private int m_piety = 0;
-		private int m_empathy = 0;
-		private int m_charisma = 0;
+		private int m_constitution;
+		private int m_dexterity;
+		private int m_strength;
+		private int m_quickness;
+		private int m_intelligence;
+		private int m_piety;
+		private int m_empathy;
+		private int m_charisma;
 
 		//This needs to be uint and ushort!
 		private int m_xpos;
@@ -130,19 +130,19 @@ namespace DOL.Database
 		private bool m_isLevelRespecUsed;
 		private int m_respecBought; // /respec buy
 		private bool m_safetyFlag;
-		private int m_craftingPrimarySkill = 0;
+		private int m_craftingPrimarySkill;
 		private bool m_cancelStyle;
 		private bool m_isAnonymous;
 
-		private byte m_customisationStep = 1;
+		private byte m_customisationStep;
 
-		private byte m_eyesize = 0;
-		private byte m_lipsize = 0;
-		private byte m_eyecolor = 0;
-		private byte m_hairColor = 0;
-		private byte m_facetype = 0;
-		private byte m_hairstyle = 0;
-		private byte m_moodtype = 0;
+		private byte m_eyesize;
+		private byte m_lipsize;
+		private byte m_eyecolor;
+		private byte m_hairColor;
+		private byte m_facetype;
+		private byte m_hairstyle;
+		private byte m_moodtype;
 
 		private bool m_gainXP;
 		private bool m_gainRP;
@@ -167,15 +167,15 @@ namespace DOL.Database
 		private bool m_mlGranted;
 
 		// Should this player stats be ignored when tabulating statistics?
-		private bool m_ignoreStatistics = false;
+		private bool m_ignoreStatistics;
 		
 		// What should the Herald display of this character?
-		private byte m_notDisplayedInHerald = 0;
+		private byte m_notDisplayedInHerald;
 		
 		// Should we hide the detailed specialization of this player in the APIs?
 		private bool m_hideSpecializationAPI;
 
-		private byte m_activeSaddleBags = 0;
+		private byte m_activeSaddleBags;
 
         private DateTime m_lastLevelUp;
 
@@ -188,46 +188,28 @@ namespace DOL.Database
         private bool m_receiveROG; // toggle receiving ROGs for the player
         private bool m_boosted; // set to true if player has used a free level/rr NPC
 
+        // Controls automation
+        private ushort _automaticBackupStyleId;
+
         /// <summary>
         /// Create the character row in table
         /// </summary>
         public DbCoreCharacter()
-		{
-			m_creationDate = DateTime.Now;
-			m_concentration = 100;
-			m_exp = 0;
-			m_bntyPts = 0;
-			m_realmPts = 0;
-
-			m_lastPlayed = DateTime.Now; // Prevent /played crash.
-			m_playedTime = 0;  // /played startup
-			m_deathTime = long.MinValue;
-			m_respecAmountAllSkill = 0;
-			m_respecAmountSingleSkill = 0;
-			m_respecAmountRealmSkill = 0;
-			m_respecAmountDOL = 0;
-			m_respecBought = 0;
-			
-			m_isLevelRespecUsed = true;
-			m_safetyFlag = true;
-			m_craftingPrimarySkill = 0;
-			m_usedLevelCommand = false;
-			m_spellQueue = true;
-			m_gainXP = true;
-			m_gainRP = true;
-			m_autoloot = true;
-			m_showXFireInfo = false;
-			m_noHelp = false;
-			m_showGuildLogins = false;
-			m_roleplay = false;
-			m_ignoreStatistics = false;
+        {
+            m_creationDate = DateTime.Now;
+            m_concentration = 100;
+            m_lastPlayed = DateTime.Now; // Prevent /played crash.
+            m_deathTime = long.MinValue;
+            m_isLevelRespecUsed = true;
+            m_customisationStep = 1;
+            m_safetyFlag = true;
+            m_spellQueue = true;
+            m_gainXP = true;
+            m_gainRP = true;
+            m_autoloot = true;
             m_lastLevelUp = DateTime.Now;
-            m_playedTimeSinceLevel = 0;
             m_receiveROG = true;
-            m_hardcore = false;
-            m_hardcoreCompleted = false;
-            m_boosted = false;
-		}
+        }
 
 		/// <summary>
 		/// Gets/sets if this character has xp in a gravestone
@@ -2246,6 +2228,17 @@ namespace DOL.Database
 			{
 				Dirty = true;
 				m_boosted = value;
+			}
+		}
+
+		[DataElement(AllowDbNull = false)]
+		public ushort AutomaticBackupStyleId
+		{
+			get => _automaticBackupStyleId;
+			set
+			{
+				Dirty = true;
+				_automaticBackupStyleId = value;
 			}
 		}
 

GameServer/ECS-Components/Actions/NpcAttackAction.cs
@@ -91,7 +91,7 @@ namespace DOL.GS
                 }
             }
 
-            _combatStyle = StyleComponent.NPCGetStyleToUse();
+            _combatStyle = StyleComponent.GetStyleToUse();
 
             if (!base.PrepareMeleeAttack())
                 return false;

GameServer/ECS-Components/NpcStyleComponent.cs
@@ -1,0 +1,86 @@
+﻿using DOL.GS.ServerProperties;
+using DOL.GS.Styles;
+
+namespace DOL.GS
+{
+    public class NpcStyleComponent : StyleComponent
+    {
+        private GameNPC _npcOwner;
+
+        public NpcStyleComponent(GameNPC npcOwner) : base(npcOwner)
+        {
+            _npcOwner = npcOwner;
+        }
+
+        public override Style GetStyleToUse()
+        {
+            if (_npcOwner.Styles == null || _npcOwner.Styles.Count < 1 || _npcOwner.TargetObject == null)
+                return null;
+
+            AttackData lastAttackData = _npcOwner.attackComponent.attackAction.LastAttackData;
+
+            // Chain and defensive styles are excluded from the chance roll because they would almost never happen otherwise. 
+            // For example, an NPC blocks 10% of the time, so the default 20% style chance effectively means the defensive 
+            // style would only actually occur during 2% of of a mob's attacks. In comparison, a style chain would only happen 
+            // 0.4% of the time.
+            if (_npcOwner.StylesChain != null && _npcOwner.StylesChain.Count > 0)
+            {
+                foreach (Style style in _npcOwner.StylesChain)
+                {
+                    if (StyleProcessor.CanUseStyle(lastAttackData, _npcOwner, style, _npcOwner.ActiveWeapon))
+                        return style;
+                }
+            }
+
+            if (_npcOwner.StylesDefensive != null && _npcOwner.StylesDefensive.Count > 0)
+            {
+                foreach (Style style in _npcOwner.StylesDefensive)
+                {
+                    if (StyleProcessor.CanUseStyle(lastAttackData, _npcOwner, style, _npcOwner.ActiveWeapon) && _npcOwner.CheckStyleStun(style)) // Make sure we don't spam stun styles like Brutalize
+                        return style;
+                }
+            }
+
+            if (Util.Chance(Properties.GAMENPC_CHANCES_TO_STYLE))
+            {
+                // All of the remaining lists are randomly picked from,
+                // as this creates more variety with each combat result.
+                // For example, a mob with both Pincer and Ice Storm
+                // styles could potentially use one or the other with
+                // each attack roll that succeeds.
+
+                // First, check positional styles (in order of back, side, front)
+                // in case the defender is facing another direction
+                if (_npcOwner.StylesBack != null && _npcOwner.StylesBack.Count > 0)
+                {
+                    Style style = _npcOwner.StylesBack[Util.Random(0, _npcOwner.StylesBack.Count - 1)];
+
+                    if (StyleProcessor.CanUseStyle(lastAttackData, _npcOwner, style, _npcOwner.ActiveWeapon))
+                        return style;
+                }
+
+                if (_npcOwner.StylesSide != null && _npcOwner.StylesSide.Count > 0)
+                {
+                    Style style = _npcOwner.StylesSide[Util.Random(0, _npcOwner.StylesSide.Count - 1)];
+
+                    if (StyleProcessor.CanUseStyle(lastAttackData, _npcOwner, style, _npcOwner.ActiveWeapon))
+                        return style;
+                }
+
+                if (_npcOwner.StylesFront != null && _npcOwner.StylesFront.Count > 0)
+                {
+                    Style style = _npcOwner.StylesFront[Util.Random(0, _npcOwner.StylesFront.Count - 1)];
+
+                    if (StyleProcessor.CanUseStyle(lastAttackData, _npcOwner, style, _npcOwner.ActiveWeapon))
+                        return style;
+                }
+
+                // Pick a random anytime style
+                if (_npcOwner.StylesAnytime != null && _npcOwner.StylesAnytime.Count > 0)
+                    return _npcOwner.StylesAnytime[Util.Random(0, _npcOwner.StylesAnytime.Count - 1)];
+            }
+
+            return null;
+        }
+    }
+}

commit 1ff0710cda34af900537d8d6b0e27712dbaef0d1

`PlayerStyleComponent`: Fix null reference exception Caused by 9fbc727

GameServer/ECS-Components/PlayerStyleComponent.cs
@@ -53,7 +53,16 @@ namespace DOL.GS
         {
             DbCoreCharacter dbCharacter = _playerOwner.DBCharacter;
 
-            if (dbCharacter == null || AutomaticBackupStyle.ID == dbCharacter.AutomaticBackupStyleId)
+            if (dbCharacter == null)
+                return;
+
+            if (AutomaticBackupStyle == null)
+            {
+                dbCharacter.AutomaticBackupStyleId = 0;
+                return;
+            }
+
+            if (AutomaticBackupStyle.ID == dbCharacter.AutomaticBackupStyleId)
                 return;
 
             dbCharacter.AutomaticBackupStyleId = (ushort) AutomaticBackupStyle.ID;