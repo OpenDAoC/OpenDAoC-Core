OpenDAoC/OpenDAoC-Core

commit 7c41ff781030b8117438c9241a5f45bbd35f682e

Reduce amount of `AttackMode` packets sent Only send one if `AttackState` actually changes.

GameServer/ECS-Components/AttackComponent.cs
@@ -771,6 +771,9 @@ namespace DOL.GS
                     if (DreamweaverRR5 != null)
                         DreamweaverRR5.Cancel(false);
                 }*/
+
+                bool oldAttackState = AttackState;
+
                 if (LivingStartAttack())
                 {
                     if (player.castingComponent.SpellHandler?.Spell.Uninterruptible == false)
@@ -780,7 +783,7 @@ namespace DOL.GS
                     }
 
                     if (player.ActiveWeaponSlot != eActiveWeaponSlot.Distance)
-                        player.Out.SendAttackMode(AttackState);
+                        player.Out.SendAttackMode(AttackState && !oldAttackState);
                     else
                     {
                         string typeMsg = attackWeapon.Object_Type == (int) eObjectType.Thrown ? "throw" : "shot";
@@ -889,12 +892,13 @@ namespace DOL.GS
                 }
             }
 
+            bool oldAttackState = AttackState;
             AttackState = false;
             owner.CancelEngageEffect();
             owner.styleComponent.NextCombatStyle = null;
             owner.styleComponent.NextCombatBackupStyle = null;
 
-            if (owner is GamePlayer playerOwner && playerOwner.IsAlive)
+            if (oldAttackState && owner is GamePlayer playerOwner && playerOwner.IsAlive)
                 playerOwner.Out.SendAttackMode(AttackState);
             else if (owner is GameNPC npcOwner && npcOwner.Inventory?.GetItem(eInventorySlot.DistanceWeapon) != null && npcOwner.ActiveWeaponSlot != eActiveWeaponSlot.Distance)
                 npcOwner.SwitchWeapon(eActiveWeaponSlot.Distance);

commit 46b3cb6a333b5ea38a96649962dd4ed8881e4994

Fix attack mode issues with Engage Partially caused by 7c41ff7

GameServer/ECS-Components/AttackComponent.cs
@@ -722,7 +722,10 @@ namespace DOL.GS
                     }
 
                     if (player.ActiveWeaponSlot is not eActiveWeaponSlot.Distance)
-                        player.Out.SendAttackMode(AttackState && !oldAttackState);
+                    {
+                        if (oldAttackState != AttackState)
+                            player.Out.SendAttackMode(AttackState);
+                    }
                     else
                     {
                         string typeMsg = (eObjectType) attackWeapon.Object_Type is eObjectType.Thrown ? "throw" : "shot";

GameServer/ECS-Effects/EngageECSEffect.cs
@@ -5,7 +5,7 @@ namespace DOL.GS
 {
     public class EngageECSGameEffect : ECSGameAbilityEffect
     {
-        private bool _startAttackAfterCancel;
+        private bool _manualCancel;
 
         public GameLiving EngageTarget { get; set; }
         public override ushort Icon => 421;
@@ -13,10 +13,12 @@ namespace DOL.GS
         {
             get
             {
-                if (EngageTarget != null)
-                    return LanguageMgr.GetTranslation(((GamePlayer)Owner).Client, "Effects.EngageEffect.EngageName", EngageTarget.GetName(0, false));
+                GamePlayer player = Owner as GamePlayer;
 
-                return LanguageMgr.GetTranslation(((GamePlayer)Owner).Client, "Effects.EngageEffect.Name");
+                if (EngageTarget != null)
+                    return LanguageMgr.GetTranslation(player.Client, "Effects.EngageEffect.EngageName", EngageTarget.GetName(0, false));
+
+                return LanguageMgr.GetTranslation(player.Client, "Effects.EngageEffect.Name");
             }
         }
         public override bool HasPositiveEffect => true;
@@ -47,25 +49,26 @@ namespace DOL.GS
 
         public override void OnStopEffect()
         {
-            Owner.IsEngaging = false;
-
-            if (_startAttackAfterCancel)
-                Owner.attackComponent.RequestStartAttack(OwnerPlayer == null ? Owner.TargetObject : null);
-        }
-
-        public void Cancel(bool manualCancel, bool startAttackAfterCancel)
-        {
-            _startAttackAfterCancel = startAttackAfterCancel;
-            EffectService.RequestImmediateCancelEffect(this, manualCancel);
-
-
             if (OwnerPlayer != null)
             {
-                if (manualCancel)
+                if (_manualCancel)
                     OwnerPlayer.Out.SendMessage(LanguageMgr.GetTranslation(OwnerPlayer.Client, "Effects.EngageEffect.YouNoConcOnBlock"), eChatType.CT_System, eChatLoc.CL_SystemWindow);
                 else
                     OwnerPlayer.Out.SendMessage(LanguageMgr.GetTranslation(OwnerPlayer.Client, "Effects.EngageEffect.YouNoAttemptToEngageT"), eChatType.CT_System, eChatLoc.CL_SystemWindow);
             }
+
+            Owner.IsEngaging = false;
+
+            if (Owner.TargetObject != null)
+                Owner.attackComponent.RequestStartAttack();
+            else
+                Owner.attackComponent.StopAttack();
+        }
+
+        public void Cancel(bool manualCancel, bool startAttackAfterCancel)
+        {
+            _manualCancel = manualCancel;
+            EffectService.RequestImmediateCancelEffect(this, manualCancel);
         }
     }
 }