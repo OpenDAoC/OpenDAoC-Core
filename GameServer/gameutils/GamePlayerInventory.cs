OpenDAoC/OpenDAoC-Core

commit 207e2dc9712f143780b421b2b5aea41d3cb53a76

Fix max speed reduction from encumbrance sometimes not clearing properly * Fix incorrect max speed reduction when the player's base speed is different than the default base speed (probably only affected GMs using /speed). * Optimize encumbrance calculation to reduce amount of inventory iterations.

GameServer/gameutils/GamePlayerInventory.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using DOL.Database;
 using DOL.GS.PacketHandler;
 
@@ -144,6 +143,7 @@ namespace DOL.GS
 							m_player.OnItemEquipped(item, slot);
 					}
 
+					UpdateInventoryWeight();
 					return true;
 				}
 				catch (Exception e)
@@ -972,13 +972,8 @@ namespace DOL.GS
 			    fromItem.SlotPosition > (int) eInventorySlot.LastBackpack)
 				return false;
 
-			if (fromItem is IGameInventoryItem)
-			{
-				if ((fromItem as IGameInventoryItem).Combine(m_player, toItem))
-				{
-					return true;
-				}
-			}
+			if (fromItem is IGameInventoryItem gameInventoryItem)
+				return gameInventoryItem.Combine(m_player, toItem);
 
 			//Is the fromItem a dye or dyepack?
 			//TODO shouldn't be done with model check
@@ -1003,26 +998,24 @@ namespace DOL.GS
 		/// <returns>true if items stacked successfully</returns>
 		protected override bool StackItems(eInventorySlot fromSlot, eInventorySlot toSlot, int itemCount)
 		{
-			DbInventoryItem fromItem;
-			DbInventoryItem toItem;
+			m_items.TryGetValue(fromSlot, out DbInventoryItem fromItem);
+			m_items.TryGetValue(toSlot, out DbInventoryItem toItem);
 
-			m_items.TryGetValue(fromSlot, out fromItem);
-			m_items.TryGetValue(toSlot, out toItem);
-
-			if ((toSlot > eInventorySlot.HorseArmor && toSlot < eInventorySlot.FirstQuiver)
-			    || (toSlot > eInventorySlot.FourthQuiver && toSlot < eInventorySlot.FirstBackpack))
+			if (toSlot is
+				(> eInventorySlot.HorseArmor and < eInventorySlot.FirstQuiver) or
+				(> eInventorySlot.FourthQuiver and < eInventorySlot.FirstBackpack))
+			{
 				return false;
+			}
 
 			if (itemCount == 0)
-			{
 				itemCount = fromItem.Count > 0 ? fromItem.Count : 1;
-			}
 
 			if (toItem != null && toItem.IsStackable && toItem.Name.Equals(fromItem.Name))
 			{
 				if (fromItem.Count + toItem.Count > fromItem.MaxCount)
 				{
-					fromItem.Count -= (toItem.MaxCount - toItem.Count);
+					fromItem.Count -= toItem.MaxCount - toItem.Count;
 					toItem.Count = toItem.MaxCount;
 				}
 				else
@@ -1036,7 +1029,7 @@ namespace DOL.GS
 
 			if (toItem == null && fromItem.Count > itemCount)
 			{
-				var newItem = (DbInventoryItem) fromItem.Clone();
+				DbInventoryItem newItem = fromItem.Clone() as DbInventoryItem;
 				m_items[toSlot] = newItem;
 				newItem.Count = itemCount;
 				newItem.SlotPosition = (int) toSlot;
@@ -1055,45 +1048,31 @@ namespace DOL.GS
 		/// <param name="fromSlot">First SlotPosition</param>
 		/// <param name="toSlot">Second SlotPosition</param>
 		/// <returns>true if items exchanged successfully</returns>
-		protected override bool ExchangeItems(eInventorySlot fromSlot, eInventorySlot toSlot)
+		protected override bool SwapItems(eInventorySlot fromSlot, eInventorySlot toSlot)
 		{
-			DbInventoryItem fromItem;
-			DbInventoryItem toItem;
-
-			m_items.TryGetValue(fromSlot, out fromItem);
-			m_items.TryGetValue(toSlot, out toItem);
+			m_items.TryGetValue(fromSlot, out DbInventoryItem fromItem);
+			m_items.TryGetValue(toSlot, out DbInventoryItem toItem);
 
 			bool fromSlotEquipped = IsEquippedSlot(fromSlot);
 			bool toSlotEquipped = IsEquippedSlot(toSlot);
 
-			base.ExchangeItems(fromSlot, toSlot);
+			if (!base.SwapItems(fromSlot, toSlot) || fromSlotEquipped == toSlotEquipped)
+				return false;
 
-			// notify handlers if items changing state
-			if (fromSlotEquipped != toSlotEquipped)
+			if (toItem != null)
 			{
-				if (toItem != null)
-				{
-					if (toSlotEquipped) // item was equipped
-					{
-						m_player.OnItemUnequipped(toItem, toSlot);
-					}
-					else
-					{
-						m_player.OnItemEquipped(toItem, toSlot);
-					}
-				}
+				if (toSlotEquipped)
+					m_player.OnItemUnequipped(toItem, toSlot);
+				else
+					m_player.OnItemEquipped(toItem, toSlot);
+			}
 
-				if (fromItem != null)
-				{
-					if (fromSlotEquipped) // item was equipped
-					{
-						m_player.OnItemUnequipped(fromItem, fromSlot);
-					}
-					else
-					{
-						m_player.OnItemEquipped(fromItem, fromSlot);
-					}
-				}
+			if (fromItem != null)
+			{
+				if (fromSlotEquipped)
+					m_player.OnItemUnequipped(fromItem, fromSlot);
+				else
+					m_player.OnItemEquipped(fromItem, fromSlot);
 			}
 
 			return true;
@@ -1139,29 +1118,39 @@ namespace DOL.GS
 		#endregion Combine/Exchange/Stack Items
 
 		#region Encumberance
-		
-		public override int InventoryWeight
-		{
-			get
-			{
-				var weight = 0;
-				IList<DbInventoryItem> items;
 
-				lock (LockObject)
+		private int _inventoryWeight;
+		protected bool _requiresInventoryWeightUpdate = true; // Must be set to true every time something happens in the player's inventory.
+		public override int InventoryWeight => _inventoryWeight;
+
+		public override bool UpdateInventoryWeight()
+		{
+			int newInventoryWeight = 0;
+
+			lock (LockObject)
+			{
+				foreach (var pair in m_items)
 				{
-					items = new List<DbInventoryItem>(m_items.Values);
+					if (IsValidSlot(pair.Key))
+						newInventoryWeight += pair.Value.Weight;
 				}
-				
-				foreach (var item in items)
+
+				newInventoryWeight /= 10;
+
+				if (_inventoryWeight != newInventoryWeight)
 				{
-					if ((eInventorySlot) item.SlotPosition < eInventorySlot.FirstBackpack || (eInventorySlot)item.SlotPosition > eInventorySlot.LastBackpack)
-						continue;
-					var itemWeight = item.Weight;
-					if (item.Description.Contains("Atlas ROG")) itemWeight = (int)(itemWeight * 0.75);
-					weight += itemWeight;
+					_inventoryWeight = newInventoryWeight;
+					return true;
 				}
-				
-				return weight/10 + base.InventoryWeight;;
+			}
+
+			return false;
+
+			static bool IsValidSlot(eInventorySlot slot)
+			{
+				return slot is
+					(>= eInventorySlot.FirstBackpack and <= eInventorySlot.LastBackpack) or
+					(>= eInventorySlot.MinEquipable and <= eInventorySlot.MaxEquipable);
 			}
 		}
 
@@ -1242,27 +1231,14 @@ namespace DOL.GS
 		/// </summary>
 		protected override void UpdateChangedSlots()
 		{
-			
-			lock (m_changedSlots)
-			{
-				var invSlots = m_changedSlots.ToList();
-				var slotsToUpdate = new List<int>();
-				foreach (var inv in invSlots)
-				{
-					slotsToUpdate.Add((int)inv);
-				}
-				m_player.Out.SendInventorySlotsUpdate(slotsToUpdate);
-			}
-
 			bool statsUpdated = false;
 			bool appearanceUpdated = false;
-			bool encumberanceUpdated = false;
 
 			lock (InventorySlotLock)
 			{
 				foreach (eInventorySlot updatedSlot in m_changedSlots)
 				{
-					// update appearance if one of changed slots is visible
+					// Update appearance if one of changed slots is visible.
 					if (!appearanceUpdated)
 					{
 						foreach (eInventorySlot visibleSlot in VISIBLE_SLOTS)
@@ -1275,32 +1251,22 @@ namespace DOL.GS
 						}
 					}
 
-					// update stats if equipped item has changed
-					if (!statsUpdated && updatedSlot <= eInventorySlot.RightRing &&
-					    updatedSlot >= eInventorySlot.RightHandWeapon)
-					{
+					// Update stats if equipped item has changed.
+					if (!statsUpdated && updatedSlot <= eInventorySlot.RightRing && updatedSlot >= eInventorySlot.RightHandWeapon)
 						statsUpdated = true;
-					}
-
-					// update encumberance if changed slot was in inventory or equipped
-					if (!encumberanceUpdated &&
-					    //					(updatedSlot >=(int)eInventorySlot.FirstVault && updatedSlot<=(int)eInventorySlot.LastVault) ||
-					    (updatedSlot >= eInventorySlot.RightHandWeapon && updatedSlot <= eInventorySlot.RightRing) ||
-					    (updatedSlot >= eInventorySlot.FirstBackpack && updatedSlot <= eInventorySlot.LastBackpack))
-					{
-						encumberanceUpdated = true;
-					}
 				}
+
+				m_player.Out.SendInventorySlotsUpdate(m_changedSlots);
 			}
-			
-			if(appearanceUpdated)
+
+			UpdateInventoryWeight();
+			m_player.UpdateEncumbrance();
+
+			if (appearanceUpdated)
 				m_player.UpdateEquipmentAppearance();
-				
-			if(statsUpdated)
+
+			if (statsUpdated)
 				m_player.Out.SendUpdateWeaponAndArmorStats();
-				
-			if(encumberanceUpdated)
-				m_player.UpdateEncumberance();
 
 			base.UpdateChangedSlots();
 		}

CoreServer/ConsolePacketLib.cs
@@ -109,7 +109,7 @@ namespace DOLGameServerConsole
 		public void SendGroupMemberUpdate(bool updateIcons, bool updateMap, GameLiving living) { }
 		public void SendGroupMembersUpdate(bool updateIcons, bool updateMap) { }
 		public void SendInventoryItemsUpdate(ICollection<DbInventoryItem> itemsToUpdate) { }
-		public void SendInventorySlotsUpdate(ICollection<int> slots) { }
+		public void SendInventorySlotsUpdate(ICollection<eInventorySlot> slots) { }
 		public void SendInventoryItemsUpdate(eInventoryWindowType windowType, ICollection<DbInventoryItem> itemsToUpdate) { }
 		public void SendInventoryItemsUpdate(IDictionary<int, DbInventoryItem> updateItems, eInventoryWindowType windowType) { }
 		public void SendInventoryItemsPartialUpdate(IDictionary<int, DbInventoryItem> items, eInventoryWindowType windowType) { }
@@ -123,7 +123,7 @@ namespace DOLGameServerConsole
 		public void SendUpdatePlayerSkills(bool updateInternalCache) { }
 		public void SendUpdateWeaponAndArmorStats() { }
 		public void SendCustomTextWindow(string caption, IList<string> text) { }
-		public void SendEncumberance() { }
+		public void SendEncumbrance() { }
 		public void SendAddFriends(string[] friendNames) { }
 		public void SendRemoveFriends(string[] friendNames) { }
 		public void SendTimerWindow(string title, int seconds) { }

GameServer/ECS-Components/EffectListComponent.cs
@@ -219,7 +219,7 @@ namespace DOL.GS
                     playerOwner.Out.SendUpdateWeaponAndArmorStats();
 
                 if ((RequestedPlayerUpdates & EffectService.PlayerUpdate.ENCUMBERANCE) != 0)
-                    playerOwner.Out.SendEncumberance();
+                    playerOwner.Out.SendEncumbrance();
 
                 if ((RequestedPlayerUpdates & EffectService.PlayerUpdate.CONCENTRATION) != 0)
                     playerOwner.Out.SendConcentrationList();

GameServer/behaviour/Requirements/EncumbranceMaxRequirement.cs
@@ -73,7 +73,7 @@ namespace DOL.GS.Behaviour.Requirements
 			bool result = true;
             GamePlayer player = BehaviourUtils.GuessGamePlayerFromNotify(e, sender, args);
 
-            result = compare(player.MaxEncumberance, N, Comparator);
+            result = compare(player.MaxCarryingCapacity, N, Comparator);
 
 			return result;
 		}

GameServer/behaviour/Requirements/EncumbranceRequirement.cs
@@ -73,7 +73,7 @@ namespace DOL.GS.Behaviour.Requirements
 			bool result = true;
             GamePlayer player = BehaviourUtils.GuessGamePlayerFromNotify(e, sender, args);
 
-            result = compare(player.Encumberance, N, Comparator);
+            result = compare(player.Inventory.InventoryWeight, N, Comparator);
 
 			return result;
 		}

GameServer/commands/gmcommands/item.cs
@@ -199,7 +199,7 @@ namespace DOL.GS.Commands
 							}
 
 							client.Out.SendInventoryItemsUpdate(new DbInventoryItem[] { item });
-							client.Player.UpdateEncumberance();
+							client.Player.UpdateEncumbrance();
 							break;
 						}
 						#endregion Count

GameServer/craft/Repair.cs
@@ -119,7 +119,7 @@ namespace DOL.GS
 					item.Durability -= toRecoverCond;
 				}
 
-				player.Out.SendInventorySlotsUpdate(new int[] { item.SlotPosition });
+				player.Out.SendInventorySlotsUpdate([(eInventorySlot) item.SlotPosition]);
 
 				player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language, "Repair.Proceed.FullyRepaired1", item.Name), eChatType.CT_System, eChatLoc.CL_SystemWindow);
 				if (tradePartner != null) tradePartner.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language, "Repair.Proceed.FullyRepaired2", player.Name, item.Name), eChatType.CT_System, eChatLoc.CL_SystemWindow);

GameServer/gameobjects/GameInventoryItem.cs
@@ -288,7 +288,7 @@ namespace DOL.GS
                             player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language, "GamePlayer.Attack.NeedRepairDire", Name), eChatType.CT_System, eChatLoc.CL_SystemWindow);
 
                         player.Out.SendUpdateWeaponAndArmorStats();
-                        player.Out.SendInventorySlotsUpdate(new int[] { SlotPosition });
+                        player.Out.SendInventorySlotsUpdate([(eInventorySlot) SlotPosition]);
                     }
                 }
             }
@@ -329,7 +329,7 @@ namespace DOL.GS
                             player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language, "GamePlayer.Attack.NeedRepairDire", Name), eChatType.CT_System, eChatLoc.CL_SystemWindow);
 
                         player.Out.SendUpdateWeaponAndArmorStats();
-                        player.Out.SendInventorySlotsUpdate(new int[] { SlotPosition });
+                        player.Out.SendInventorySlotsUpdate([(eInventorySlot) SlotPosition]);
                     }
                 }
             }

commit dcc0b2b50abe791937b12165cbe3b684a873f29f

Fix dyes (caused by 207e2dc)

GameServer/gameutils/GamePlayerInventory.cs
@@ -973,8 +973,8 @@ namespace DOL.GS
 			    fromItem.SlotPosition > (int) eInventorySlot.LastBackpack)
 				return false;
 
-			if (fromItem is IGameInventoryItem gameInventoryItem)
-				return gameInventoryItem.Combine(m_player, toItem);
+			if (fromItem is IGameInventoryItem gameInventoryItem && gameInventoryItem.Combine(m_player, toItem))
+				return true;
 
 			//Is the fromItem a dye or dyepack?
 			//TODO shouldn't be done with model check