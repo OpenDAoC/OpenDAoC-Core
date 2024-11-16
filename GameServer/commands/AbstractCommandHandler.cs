b4237e449b3eda2bf71ceba04136e539174b6555
Baptiste Marie <bm01@users.noreply.github.com>
2024-11-16 19:33:01 +0100

    Make GM / admin accounts be unaffected by command spam check

--- GameServer/commands/AbstractCommandHandler.cs
+++ GameServer/commands/AbstractCommandHandler.cs
@@ -1,24 +1,3 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
-using System.Reflection;
-using log4net;
-
 namespace DOL.GS.Commands
 {
 	/// <summary>
@@ -26,16 +5,13 @@ namespace DOL.GS.Commands
 	/// </summary>
 	public abstract class AbstractCommandHandler
 	{
-		private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-
-
 		/// <summary>
 		/// Is this player spamming this command
 		/// </summary>
 		/// <param name="player"></param>
 		/// <param name="commandName"></param>
 		/// <returns></returns>
-		public bool IsSpammingCommand(GamePlayer player, string commandName)
+		public static bool IsSpammingCommand(GamePlayer player, string commandName)
 		{
 			return IsSpammingCommand(player, commandName, ServerProperties.Properties.COMMAND_SPAM_DELAY);
 		}
@@ -47,8 +23,11 @@ namespace DOL.GS.Commands
 		/// <param name="commandName"></param>
 		/// <param name="delay">How long is the spam delay in milliseconds</param>
 		/// <returns>true if less than spam protection interval</returns>
-		public bool IsSpammingCommand(GamePlayer player, string commandName, int delay)
+		public static bool IsSpammingCommand(GamePlayer player, string commandName, int delay)
 		{
+			if ((ePrivLevel) player.Client.Account.PrivLevel > ePrivLevel.Player)
+				return false;
+
 			string spamKey = commandName + "NOSPAM";
 			long tick = player.TempProperties.GetProperty<long>(spamKey);
 
