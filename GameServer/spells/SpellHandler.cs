OpenDAoC/OpenDAoC-Core

commit c6ff6d57dd27f01c953d2d4640bc620e49e45550

Fix issues with bolt damage * The physical portion of the damage was using half of the magical portion of the damage modified by resistances, instead of half of the base damage. Meaning it was lower than intended, on top of involving resistances twice. * Since this fix results in much higher physical damage for bolts, adjusting the fake weaponskill is necessary. It is now 50% of its previous value. * This results in the same damage against a targets with no resist. Quite higher damage against targets with positive resists. And a little lower damage against targets with negative resists. * If bolt damage is too low but other spells' damage is fine, that fake weaponskill can be easily increased. * Made `AdjustDamageForHitChance`, `ModifyDamageWithTargetResist` take and return doubles for better precision and reduce the amount of roundings.

GameServer/spells/SpellHandler.cs
@@ -3142,17 +3142,12 @@ namespace DOL.GS.Spells
 		/// <summary>
 		/// Adjust damage based on chance to hit.
 		/// </summary>
-		/// <param name="damage"></param>
-		/// <param name="hitChance"></param>
-		/// <returns></returns>
-		public virtual int AdjustDamageForHitChance(int damage, double hitChance)
+		public virtual double AdjustDamageForHitChance(double damage, double hitChance)
 		{
-			int adjustedDamage = damage;
-
 			if (hitChance < 55)
-				adjustedDamage += (int) (adjustedDamage * (hitChance - 55) * Properties.SPELL_HITCHANCE_DAMAGE_REDUCTION_MULTIPLIER * 0.01);
+				damage *= (hitChance - 55) * Properties.SPELL_HITCHANCE_DAMAGE_REDUCTION_MULTIPLIER * 0.01;
 
-			return Math.Max(adjustedDamage, 1);
+			return damage;
 		}
 
 		public virtual AttackData CalculateDamageToTarget(GameLiving target)
@@ -3181,7 +3176,8 @@ namespace DOL.GS.Spells
 			if (DistanceFallOff > 0)
 				spellDamage *= 1 - DistanceFallOff;
 
-			int finalDamage = Util.Random((int)(minVariance * spellDamage), (int)(maxVariance * spellDamage));
+			double variance = minVariance + Util.RandomDoubleIncl() * (maxVariance - minVariance);
+			double finalDamage = spellDamage * variance;
 
 			// Live testing done Summer 2009 by Bluraven, Tolakram. Levels 40, 45, 50, 55, 60, 65, 70.
 			// Damage reduced by chance < 55, no extra damage increase noted with hitchance > 100.
@@ -3191,21 +3187,20 @@ namespace DOL.GS.Spells
 			if (m_caster is GamePlayer || (m_caster is GameNPC && (m_caster as GameNPC).Brain is IControlledBrain && m_caster.Realm != 0))
 			{
 				if (target is GamePlayer)
-					finalDamage = (int) (finalDamage * Properties.PVP_SPELL_DAMAGE);
+					finalDamage *= Properties.PVP_SPELL_DAMAGE;
 				else if (target is GameNPC)
-					finalDamage = (int) (finalDamage * Properties.PVE_SPELL_DAMAGE);
+					finalDamage *= Properties.PVE_SPELL_DAMAGE;
 			}
 
 			// Calculate resistances and conversion.
 			finalDamage = ModifyDamageWithTargetResist(ad, finalDamage);
 			double conversionMod = AttackComponent.CalculateTargetConversion(ad.Target);
-			int preConversionDamage = finalDamage;
-			finalDamage = (int) (finalDamage * conversionMod);
-			ad.Modifier += finalDamage - preConversionDamage;
+			double preConversionDamage = finalDamage;
+			finalDamage = finalDamage * conversionMod;
+			ad.Modifier += (int) Math.Floor(finalDamage - preConversionDamage);
 
 			// Apply damage cap.
-			if (finalDamage > DamageCap(effectiveness))
-				finalDamage = (int) DamageCap(effectiveness);
+			finalDamage = Math.Min(finalDamage, DamageCap(effectiveness));
 
 			// Apply conversion.
 			if (conversionMod < 1)
@@ -3228,18 +3223,18 @@ namespace DOL.GS.Spells
 
 			if (criticalCap > randNum && finalDamage > 0)
 			{
-				int criticalMax = (ad.Target is GamePlayer) ? finalDamage / 2 : finalDamage;
-				criticalDamage = Util.Random(finalDamage / 10, criticalMax);
+				int criticalMax = ad.Target is GamePlayer ? (int) finalDamage / 2 : (int) finalDamage;
+				criticalDamage = Util.Random((int) finalDamage / 10, criticalMax);
 			}
 
-			ad.Damage = finalDamage;
+			ad.Damage = (int) finalDamage;
 			ad.CriticalDamage = criticalDamage;
 			ad.Target.ModifyAttack(ad); // Attacked living may modify the attack data. Primarily used for keep doors and components.
 			m_lastAttackData = ad;
 			return ad;
 		}
 
-		public virtual int ModifyDamageWithTargetResist(AttackData ad, int damage)
+		public virtual double ModifyDamageWithTargetResist(AttackData ad, double damage)
 		{
 			// Since 1.65 there are different categories of resist.
 			// - First category contains Item / Race/ Buff / RvrBanners resists.
@@ -3256,21 +3251,19 @@ namespace DOL.GS.Spells
 			// Resist Pierce is a special bonus which has been introduced with ToA.
 			// It reduces the resistance that the victim receives through items by the specified percentage.
 			// http://de.daocpedia.eu/index.php/Resistenz_durchdringen (translated)
-			int resitPierce = Caster.GetModified(eProperty.ResistPierce);
+			int resistPierce = Caster.GetModified(eProperty.ResistPierce);
 
-			// Substract max ItemBonus of property of target, but at least 0.
-			if (resitPierce > 0 && Spell.SpellType != eSpellType.Archery)
-				primaryResistModifier -= Math.Max(0, Math.Min(ad.Target.ItemBonus[(int) property], resitPierce));
+			// Subtract max ItemBonus of property of target, but at least 0.
+			if (resistPierce > 0 && Spell.SpellType != eSpellType.Archery)
+				primaryResistModifier -= Math.Max(0, Math.Min(ad.Target.ItemBonus[(int) property], resistPierce));
 
-			int resistModifier = 0;
-			resistModifier += (int)(damage * (double) primaryResistModifier * -0.01);
-			resistModifier += (int)((damage + (double) resistModifier) * secondaryResistModifier * -0.01);
+			double resistModifier = damage * primaryResistModifier * -0.01;
+			resistModifier += (damage + resistModifier) * secondaryResistModifier * -0.01;
 			damage += resistModifier;
 
 			// Update AttackData.
-			ad.Modifier = resistModifier;
+			ad.Modifier = (int) Math.Floor(resistModifier);
 			ad.DamageType = damageType;
-
 			return damage;
 		}
 

GameServer/spells/Archery/Archery.cs
@@ -86,16 +86,12 @@ namespace DOL.GS.Spells
 		/// <summary>
 		/// Adjust damage based on chance to hit.
 		/// </summary>
-		public override int AdjustDamageForHitChance(int damage, double hitChance)
+		public override double AdjustDamageForHitChance(double damage, double hitChance)
 		{
-			int adjustedDamage = damage;
-
 			if (hitChance < 85)
-			{
-				adjustedDamage += (int)(adjustedDamage * (hitChance - 85) * 0.038);
-			}
+				damage *= (hitChance - 85) * 0.038;
 
-			return adjustedDamage;
+			return damage;
 		}
 
 		/// <summary>

GameServer/spells/BoltSpellHandler.cs
@@ -57,11 +57,12 @@ namespace DOL.GS.Spells
             target.StartInterruptTimer(target.SpellInterruptDuration, ad.AttackType, Caster);
         }
 
-        public override int ModifyDamageWithTargetResist(AttackData ad, int damage)
+        public override double ModifyDamageWithTargetResist(AttackData ad, double damage)
         {
             // Modify half of the damage using magic resists. The other half is modified by the target's armor, or discarded if the target blocks.
             // Resources indicate that resistances aren't applied on the physical part of the damage.
-            damage = base.ModifyDamageWithTargetResist(ad, damage / 2);
+            double halfBaseDamage = damage * 0.5;
+            damage = base.ModifyDamageWithTargetResist(ad, halfBaseDamage);
 
             if (!ad.Target.attackComponent.CheckBlock(ad) || ad.Target.attackComponent.CheckGuard(ad, false))
             {
@@ -71,9 +72,9 @@ namespace DOL.GS.Spells
 
                 // We need a fake weapon skill for the target's armor to have something to be compared with.
                 // Since 'damage' is already modified by intelligence, power relics, spell variance, and everything else; we can use a constant only modified by the caster's level.
-                double weaponSkill = Caster.attackComponent.CalculateWeaponSkill(Caster.Level * 5, 1.0, 1.0);
+                double weaponSkill = Caster.attackComponent.CalculateWeaponSkill(Caster.Level * 2.5, 1.0, 1.0);
                 double targetArmor = AttackComponent.CalculateTargetArmor(ad.Target, ad.ArmorHitLocation, out _, out _);
-                damage += (int) (weaponSkill / targetArmor * damage / 2);
+                damage += weaponSkill / targetArmor * halfBaseDamage;
             }
             else
             {

commit 56a322edd34302ae44223faa3a80262c1f7e53b8

Fix `AdjustDamageForHitChance` Caused by c6ff6d57. Oops.

GameServer/spells/SpellHandler.cs
@@ -3117,7 +3117,7 @@ namespace DOL.GS.Spells
 		public virtual double AdjustDamageForHitChance(double damage, double hitChance)
 		{
 			if (hitChance < 55)
-				damage *= (hitChance - 55) * Properties.SPELL_HITCHANCE_DAMAGE_REDUCTION_MULTIPLIER * 0.01;
+				damage += (hitChance - 55) * Properties.SPELL_HITCHANCE_DAMAGE_REDUCTION_MULTIPLIER * 0.01;
 
 			return damage;
 		}