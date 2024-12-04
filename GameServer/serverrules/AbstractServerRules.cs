OpenDAoC/OpenDAoC-Core

commit b276c1fa214809e7e01d8f45f5501d6a2aac83e4

Fix group experience / RP bonus logic * It was 12.5% per group member, but since the minimum group size is 2, it started at 25%., * Interestingly, due to the way base experience is first divided by group size, it made the group experience bonus to always be the same amount regardless of group size (12.5% of the monster's base experience). * Because of this, using a high bonus, it was theoretically possible for the group experience bonus to do more than simply work against the base experience reduction from group size. Logically, a group experience bonus of 100% should simply nullify it. * Hence the group size has to be reduced by 1. * Applied the same logic to RPs.

GameServer/serverrules/AbstractServerRules.cs
@@ -1461,12 +1461,12 @@ namespace DOL.GS.ServerRules
 
 			long CalculateGroupBonus()
 			{
-				long groupBonus = 0;
+				if (player.Group == null || plrGrpExp.ContainsKey(player.Group))
+					return 0;
 
-				if (player.Group != null && plrGrpExp.ContainsKey(player.Group))
-					groupBonus = (long) (0.125 * baseXpReward * player.Group.MemberCount);
-
-				return groupBonus;
+				// Group size is reduced by 1 to prevent the bonus from doing more than simply working against the base experience reduction done in `CalculateNpcExperienceValueModifiedByGroup`.
+				// For example, a bonus of 100% should nullify that reduction. If the group size wasn't reduced by 1, duos would actually gain more experience than solo players (ignoring other bonuses).
+				return (long) (baseXpReward * (player.Group.MemberCount - 1) * 0.125);
 			}
 
 			long CalculateBafBonus()
@@ -1639,7 +1639,7 @@ namespace DOL.GS.ServerRules
 					{
 						lock (killerPlayer.Group)
 						{
-							int count = 0;
+							int count = -1;
 							foreach (GamePlayer player in killerPlayer.Group.GetPlayersInTheGroup())
 							{
 								if (!player.IsWithinRadius(killedLiving, WorldMgr.MAX_EXPFORKILL_DISTANCE)) continue;
@@ -1869,7 +1869,7 @@ namespace DOL.GS.ServerRules
 						{
 							lock (killerPlayer.Group)
 							{
-								int count = 0;
+								int count = -1;
 								foreach (GamePlayer player in killerPlayer.Group.GetPlayersInTheGroup())
 								{
 									if (!player.IsWithinRadius(killedPlayer, WorldMgr.MAX_EXPFORKILL_DISTANCE)) continue;

commit b64eae63394dce802a0ec5414d5f6323093e24db

Fix group XP bonus (caused by b276c1f)

GameServer/serverrules/AbstractServerRules.cs
@@ -1462,7 +1462,7 @@ namespace DOL.GS.ServerRules
 
 			long CalculateGroupBonus()
 			{
-				if (player.Group == null || plrGrpExp.ContainsKey(player.Group))
+				if (player.Group == null || !plrGrpExp.ContainsKey(player.Group))
 					return 0;
 
 				// Group size is reduced by 1 to prevent the bonus from doing more than simply working against the base experience reduction done in `CalculateNpcExperienceValueModifiedByGroup`.