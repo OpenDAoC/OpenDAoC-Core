7b4a8b74328804a672adb5e1fdbe36f97ed02c34
Baptiste Marie <bm01@users.noreply.github.com>
2024-12-08 15:30:31 +0100

    Fix delayed interuptions from DD resists, caused by undesired LoS check

--- GameServer/spells/DirectDamageSpellHandler.cs
+++ GameServer/spells/DirectDamageSpellHandler.cs
@@ -70,38 +70,28 @@ namespace DOL.GS.Spells
 			if (target == null)
 				return;
 
-			if (Spell.Target == eSpellTarget.CONE) //  || (Spell.Target == eSpellTarget.ENEMY && Spell.IsPBAoE)  <-- pbaoe skips los per 1.65
+			// 1.65 compliance. No LoS check on PBAoE or AoE spells.
+			if (Spell.Target is eSpellTarget.CONE)
 			{
 				GamePlayer checkPlayer = null;
+
 				if (target is GamePlayer)
-				{
 					checkPlayer = target as GamePlayer;
-				}
 				else
 				{
 					if (Caster is GamePlayer)
-					{
 						checkPlayer = Caster as GamePlayer;
-					}
-					else if (Caster is GameNPC && (Caster as GameNPC).Brain is IControlledBrain)
-					{
-						IControlledBrain brain = (Caster as GameNPC).Brain as IControlledBrain;
-						checkPlayer = brain.GetPlayerOwner();
-					}
+					else if (Caster is GameNPC npcCaster && npcCaster.Brain is IControlledBrain npcCasterBrain)
+						checkPlayer = npcCasterBrain.GetPlayerOwner();
 				}
+
 				if (checkPlayer != null)
-				{
 					checkPlayer.Out.SendCheckLos(Caster, target, new CheckLosResponse(DealDamageCheckLos));
-				}
 				else
-				{
 					DealDamage(target);
-				}
 			}
 			else
-			{
 				DealDamage(target);
-			}
 		}
 
 		protected virtual void DealDamageCheckLos(GamePlayer player, eLosCheckResponse response, ushort sourceOID, ushort targetOID)
@@ -149,14 +139,29 @@ namespace DOL.GS.Spells
 
 		/*
 		 * We need to send resist spell los check packets because spell resist is calculated first, and
-		 * so you could be inside keep and resist the spell and be interupted when not in view
+		 * so you could be inside keep and resist the spell and be interrupted when not in view
 		 */
 		protected override void OnSpellResisted(GameLiving target)
 		{
-			if (target is GamePlayer)
+			// 1.65 compliance. No LoS check on PBAoE or AoE spells.
+			if (Spell.Target is eSpellTarget.CONE)
 			{
-				GamePlayer player = target as GamePlayer;
-				player.Out.SendCheckLos(Caster, player, new CheckLosResponse(ResistSpellCheckLos));
+				GamePlayer checkPlayer = null;
+
+				if (target is GamePlayer)
+					checkPlayer = target as GamePlayer;
+				else
+				{
+					if (Caster is GamePlayer)
+						checkPlayer = Caster as GamePlayer;
+					else if (Caster is GameNPC npcCaster && npcCaster.Brain is IControlledBrain npcCasterBrain)
+						checkPlayer = npcCasterBrain.GetPlayerOwner();
+				}
+
+				if (checkPlayer != null)
+					checkPlayer.Out.SendCheckLos(Caster, target, new CheckLosResponse(ResistSpellCheckLos));
+				else
+					base.OnSpellResisted(target);
 			}
 			else
 				base.OnSpellResisted(target);
