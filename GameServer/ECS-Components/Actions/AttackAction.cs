OpenDAoC/OpenDAoC-Core

commit 495f2f41867903b69d425c9a21ccd7620ec0fc27

Allow NPCs to swing their left hand again Made /targetstats display it the swing rate properly Now gets dual wield defense penetration too

GameServer/ECS-Components/Actions/WeaponAction.cs
@@ -64,7 +64,7 @@ namespace DOL.GS
             // 1.88
             //- Monsters, pets and Non-Player Characters (NPCs) will now halt their pursuit when the character being chased stealths.
 
-            _leftHandSwingCount = CalculateLeftHandSwingCount(_attackWeapon, _leftWeapon, _owner);
+            _leftHandSwingCount = _owner.attackComponent.CalculateLeftHandSwingCount(_attackWeapon, _leftWeapon);
             _isDualWieldAttack = IsDualWieldAttack(_attackWeapon, _leftWeapon, _owner, _leftHandSwingCount);
 
             if (!MakeMainHandAttack(_attackWeapon, _leftWeapon, _combatStyle, _effectiveness, out AttackData mainHandAttackData))
@@ -233,27 +233,14 @@ namespace DOL.GS
             }
         }
 
-        public static int CalculateLeftHandSwingCount(DbInventoryItem mainWeapon, DbInventoryItem leftWeapon, GameLiving attacker)
-        {
-            if (attacker.attackComponent.CanUseLefthandedWeapon)
-            {
-                // NPCs can dual swing even with no weapon.
-                if (attacker is GameNPC ||
-                    (leftWeapon != null && (eObjectType) leftWeapon.Object_Type is not eObjectType.Shield && mainWeapon != null && mainWeapon.Hand != 1))
-                {
-                    return attacker.attackComponent.CalculateLeftHandSwingCount();
-                }
-            }
-
-            return 0;
-        }
-
         public static bool IsDualWieldAttack(DbInventoryItem mainWeapon, DbInventoryItem leftWeapon, GameLiving attacker, int leftHandSwingCount)
         {
             if (leftHandSwingCount > 0)
             {
-                return attacker is not GameNPC &&
-                    (eObjectType) mainWeapon.Object_Type is not eObjectType.HandToHand &&
+                if (attacker is GameNPC)
+                    return true;
+
+                return (eObjectType) mainWeapon.Object_Type is not eObjectType.HandToHand &&
                     (eObjectType) leftWeapon?.Object_Type is not eObjectType.HandToHand &&
                     (eObjectType) mainWeapon.Object_Type is not eObjectType.TwoHandedWeapon &&
                     (eObjectType) mainWeapon.Object_Type is not eObjectType.Thrown &&
@@ -261,9 +248,10 @@ namespace DOL.GS
             }
             else if (mainWeapon != null)
             {
-                if (attacker is GameNPC || mainWeapon.Item_Type is Slot.TWOHAND || mainWeapon.SlotPosition is Slot.RANGED)
+                if (mainWeapon.Item_Type is Slot.TWOHAND || mainWeapon.SlotPosition is Slot.RANGED)
                     return false;
-                else if (leftWeapon != null && (eObjectType) leftWeapon.Object_Type is not eObjectType.Shield)
+
+                if (leftWeapon != null && (eObjectType) leftWeapon.Object_Type is not eObjectType.Shield)
                     return true;
             }
 

GameServer/ECS-Components/AttackComponent.cs
@@ -2638,10 +2638,12 @@ namespace DOL.GS
         {
             get
             {
-                if (owner is GamePlayer)
-                    return (owner as GamePlayer).CharacterClass.CanUseLefthandedWeapon;
-                else
-                    return false;
+                if (owner is GamePlayer playerOwner)
+                    return playerOwner.CharacterClass.CanUseLefthandedWeapon;
+                else if (owner is GameNPC)
+                    return true;
+
+                return false;
             }
         }
 
@@ -2660,13 +2662,11 @@ namespace DOL.GS
             return 0;
         }
 
-        public (double, double, double) CalculateHthSwingChances()
+        public (double, double, double) CalculateHthSwingChances(DbInventoryItem leftWeapon)
         {
             int specLevel = owner.GetModifiedSpecLevel(Specs.HandToHand);
-            DbInventoryItem attackWeapon = owner.ActiveWeapon;
-            DbInventoryItem leftWeapon = owner.Inventory?.GetItem(eInventorySlot.LeftHandWeapon);
 
-            if (specLevel <= 0 || attackWeapon == null || leftWeapon == null || (eObjectType) leftWeapon.Object_Type is not eObjectType.HandToHand)
+            if (specLevel <= 0 || (eObjectType) leftWeapon.Object_Type is not eObjectType.HandToHand)
                 return (0, 0, 0);
 
             double doubleSwingChance = specLevel * 0.5; // specLevel >> 1
@@ -2680,12 +2680,20 @@ namespace DOL.GS
         /// <summary>
         /// Calculates how many times left hand swings
         /// </summary>
-        public int CalculateLeftHandSwingCount()
+        public int CalculateLeftHandSwingCount(DbInventoryItem mainWeapon, DbInventoryItem leftWeapon)
         {
-            if (!CanUseLefthandedWeapon)
+            // Let's make NPCs require an actual weapon too. It looks silly otherwise.
+            if (leftWeapon == null || !CanUseLefthandedWeapon) 
                 return 0;
 
-            GamePlayer playerOwner = owner as GamePlayer;
+            if (owner is GameNPC npcOwner)
+            {
+                double random = Util.RandomDouble() * 100;
+                return random < npcOwner.LeftHandSwingChance ? 1 : 0;
+            }
+
+            if (owner is not GamePlayer playerOwner || (eObjectType) leftWeapon.Object_Type is eObjectType.Shield || mainWeapon == null)
+                return 0;
 
             if (owner.GetBaseSpecLevel(Specs.Left_Axe) > 0)
             {
@@ -2693,10 +2701,10 @@ namespace DOL.GS
                 {
                     // This shouldn't be done here.
                     double effectiveness = CalculateLeftAxeModifier();
-                    playerOwner.Out.SendMessage($"{Math.Round(effectiveness * 100, 2)}% dmg (after LA penalty) \n", eChatType.CT_DamageAdd, eChatLoc.CL_SystemWindow);
+                    playerOwner.Out.SendMessage($"{Math.Round(effectiveness * 100, 2)}% dmg (after LA penalty)\n", eChatType.CT_DamageAdd, eChatLoc.CL_SystemWindow);
                 }
 
-                return 1; // always use left axe
+                return 1; // Always use left axe.
             }
 
             double leftHandSwingChance = CalculateDwCdLeftHandSwingChance();
@@ -2711,7 +2719,7 @@ namespace DOL.GS
                 return random < leftHandSwingChance ? 1 : 0;
             }
 
-            (double doubleSwingChance, double tripleSwingChance, double quadSwingChance) hthSwingChances = CalculateHthSwingChances();
+            (double doubleSwingChance, double tripleSwingChance, double quadSwingChance) hthSwingChances = CalculateHthSwingChances(leftWeapon);
 
             if (hthSwingChances.doubleSwingChance > 0)
             {

commit bdbf01af59c8b7e82d33c5188a4a76d8c4f0d780

Fix left hand swinging with two-handed weapon Caused by 495f2f41. Instead of re-adding removed checks, added `GameLiving.ActiveLeftWeapon`, updated by `SwitchWeapon`. This also reduce the amount of inventory accesses to get the left hand, and weird `ActiveWeapon` cache in `GameLiving`. This assumes `SwitchWeapon` is correctly called when changing region, or when a NPC respawns.

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -66,7 +66,7 @@ namespace DOL.GS
             }
 
             _weapon = _owner.ActiveWeapon;
-            _leftWeapon = _owner.Inventory?.GetItem(eInventorySlot.LeftHandWeapon);
+            _leftWeapon = _owner.ActiveLeftWeapon;
             _effectiveness = _owner.Effectiveness;
 
             if (_owner.ActiveWeaponSlot != eActiveWeaponSlot.Distance)

GameServer/ECS-Components/Actions/WeaponAction.cs
@@ -181,7 +181,7 @@ namespace DOL.GS
 
                         if (defender.Inventory != null)
                         {
-                            DbInventoryItem lefthand = defender.Inventory.GetItem(eInventorySlot.LeftHandWeapon);
+                            DbInventoryItem lefthand = defender.ActiveLeftWeapon;
 
                             if (lefthand != null && (eObjectType) lefthand.Object_Type is eObjectType.Shield)
                                 defendersWeapon = lefthand.Model;
@@ -238,7 +238,7 @@ namespace DOL.GS
             if (attacker is GameNPC npcAttacker)
                 return npcAttacker.LeftHandSwingChance > 0; // We can't rely on object types for NPCs.
 
-            // I'm not sure this has to be that complicated. We shouldn't even need `leftHandSwingCount`.
+            // I'm not sure this has to be that complicated for players.
             if (leftHandSwingCount > 0)
             {
                 return (eObjectType) mainWeapon.Object_Type is not eObjectType.HandToHand &&

GameServer/ECS-Components/AttackComponent.cs
@@ -471,7 +471,7 @@ namespace DOL.GS
 
                     if (weapon.Item_Type == Slot.TWOHAND)
                         damageCap *= CalculateTwoHandedDamageModifier(weapon);
-                    else if (player.Inventory?.GetItem(eInventorySlot.LeftHandWeapon) != null)
+                    else if (player.ActiveLeftWeapon != null)
                         damageCap *= CalculateLeftAxeModifier();
                 }
 
@@ -943,7 +943,7 @@ namespace DOL.GS
                             List<GameObject> extraTargets = new();
                             List<GameObject> listAvailableTargets = new();
                             DbInventoryItem attackWeapon = owner.ActiveWeapon;
-                            DbInventoryItem leftWeapon = playerOwner.Inventory?.GetItem(eInventorySlot.LeftHandWeapon);
+                            DbInventoryItem leftWeapon = owner.ActiveLeftWeapon;
 
                             int numTargetsCanHit = style.ID switch
                             {
@@ -1740,8 +1740,8 @@ namespace DOL.GS
                     !guard.Source.IsWithinRadius(guard.Target, GuardAbilityHandler.GUARD_DISTANCE))
                     continue;
 
-                DbInventoryItem leftHand = source.Inventory.GetItem(eInventorySlot.LeftHandWeapon);
                 DbInventoryItem rightHand = source.ActiveWeapon;
+                DbInventoryItem leftHand = source.ActiveLeftWeapon;
 
                 if (((rightHand != null && rightHand.Hand == 1) || leftHand == null || (eObjectType) leftHand.Object_Type is not eObjectType.Shield) && source is not GameNPC)
                     continue;
@@ -1823,8 +1823,8 @@ namespace DOL.GS
             if (!dashing.GuardSource.IsWithinRadius(dashing.GuardTarget, DashingDefenseEffect.GUARD_DISTANCE))
                 return false;
 
-            DbInventoryItem leftHand = dashing.GuardSource.Inventory.GetItem(eInventorySlot.LeftHandWeapon);
             DbInventoryItem rightHand = dashing.GuardSource.ActiveWeapon;
+            DbInventoryItem leftHand = dashing.GuardSource.ActiveLeftWeapon;
 
             if ((rightHand == null || rightHand.Hand != 1) && leftHand != null && leftHand.Object_Type == (int) eObjectType.Shield)
             {
@@ -2683,7 +2683,7 @@ namespace DOL.GS
         public int CalculateLeftHandSwingCount(DbInventoryItem mainWeapon, DbInventoryItem leftWeapon)
         {
             // Let's make NPCs require an actual weapon too. It looks silly otherwise.
-            if (leftWeapon == null || !CanUseLefthandedWeapon)
+            if (!CanUseLefthandedWeapon || leftWeapon == null || (eObjectType) leftWeapon.Object_Type is eObjectType.Shield)
                 return 0;
 
             if (owner is GameNPC npcOwner)

GameServer/ECS-Components/PlayerStyleComponent.cs
@@ -81,7 +81,7 @@ namespace DOL.GS
             }
 
             AttackData lastAttackData = Owner.attackComponent.attackAction.LastAttackData;
-            DbInventoryItem weapon = NextCombatStyle.WeaponTypeRequirement == (int) eObjectType.Shield ? Owner.Inventory.GetItem(eInventorySlot.LeftHandWeapon) : Owner.ActiveWeapon;
+            DbInventoryItem weapon = NextCombatStyle.WeaponTypeRequirement == (int) eObjectType.Shield ? Owner.ActiveLeftWeapon : Owner.ActiveWeapon;
 
             //determine which style will actually be used
             Style styleToUse;

GameServer/ECS-Debug/ECSDebug.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using DOL.Database;
 using DOL.Events;
 using DOL.GS;
 using ECS.Debug;
@@ -305,53 +304,6 @@ namespace DOL.GS.Commands
                 NpcService.DebugTickCount = tickcount;
                 DisplayMessage(client, "Debugging next " + tickcount + " NPCThinkService tick(s)");
             }
-        }
-    }
-
-    // This should be moved outside of this file if we want this as a real player-facing feature.
-    [CmdAttribute(
-        "&charstats",
-        ePrivLevel.GM,
-        "Shows normally hidden character stats.")]
-    public class CharStatsCommandHandler : AbstractCommandHandler, ICommandHandler
-    {
-        public void OnCommand(GameClient client, string[] args)
-        {
-            List<string> messages = new();
-            string header = "Hidden Character Stats";
-            GamePlayer player = client.Player;
-            DbInventoryItem lefthand = player.Inventory.GetItem(eInventorySlot.LeftHandWeapon);
-
-            // Melee crit chance.
-            int meleeCritChance = player.GetModified(eProperty.CriticalMeleeHitChance);
-            messages.Add($"Melee Crit Chance: {meleeCritChance}%");
-
-            // Spell crit chance
-            int spellCritChance = player.GetModified(eProperty.CriticalSpellHitChance);
-            messages.Add($"Spell Crit Chance: {spellCritChance}");
-
-            // Spell casting speed bonus.
-            int spellCastSpeed = player.GetModified(eProperty.CastingSpeed);
-            messages.Add($"Spell Casting Speed Bonus: {spellCastSpeed}%");
-
-            // Heal crit chance.
-            int healCritChance = player.GetModified(eProperty.CriticalHealHitChance);
-            messages.Add($"Heal Crit Chance: {healCritChance}%");
-
-            // Archery crit chance.
-            if (player.HasSpecialization(Specs.Archery)
-                || player.HasSpecialization(Specs.CompositeBow)
-                || player.HasSpecialization(Specs.RecurveBow)
-                || player.HasSpecialization(Specs.ShortBow)
-                || player.HasSpecialization(Specs.Crossbow)
-                || player.HasSpecialization(Specs.Longbow))
-            {
-                int archeryCritChance = player.GetModified(eProperty.CriticalArcheryHitChance);
-                messages.Add($"Archery Crit Chance: {archeryCritChance}%");
-            }
-
-            // Finalize.
-            player.Out.SendCustomTextWindow(header, messages);
         }
     }
 }

GameServer/commands/playercommands/targetstats.cs
@@ -50,7 +50,7 @@ namespace DOL.GS.Commands
                 info.Add($"Natural:  {naturalResist}%");
 
             DbInventoryItem mainWeapon = target.ActiveWeapon;
-            DbInventoryItem leftWeapon = target.Inventory?.GetItem(eInventorySlot.LeftHandWeapon);
+            DbInventoryItem leftWeapon = target.ActiveLeftWeapon;
             int leftHandSwingCount = target.attackComponent.CalculateLeftHandSwingCount(mainWeapon, leftWeapon);
             bool isDualWieldAttack = WeaponAction.IsDualWieldAttack(mainWeapon, leftWeapon, target, leftHandSwingCount);
             AttackData.eAttackType attackType = AttackData.GetAttackType(mainWeapon, isDualWieldAttack, target);

GameServer/gameobjects/GameLiving.cs
@@ -463,57 +463,19 @@ namespace DOL.GS
 			return 0;
 		}
 
-		private (DbInventoryItem item, eActiveWeaponSlot slot, long time) _cachedActiveWeapon;
+		private DbInventoryItem _activeWeapon;
+		private DbInventoryItem _activeLeftWeapon;
 
-        /// <summary>
-        /// Returns the currently active weapon, null=natural
-        /// </summary>
-        public virtual DbInventoryItem ActiveWeapon
-        {
-            get
-            {
-                if (Inventory == null)
-                    return null;
+		/// <summary>
+		/// Returns the currently active weapon, null=natural
+		/// </summary>
+		public virtual DbInventoryItem ActiveWeapon => _activeWeapon;
+		public virtual DbInventoryItem ActiveLeftWeapon => _activeLeftWeapon;
 
-                // We cache the weapon since 'ActiveWeapon' can be called multiple times per tick and 'GameInventory.GetItem' is potentially expensive.
-                if (_cachedActiveWeapon.item != null && _cachedActiveWeapon.slot == ActiveWeaponSlot && _cachedActiveWeapon.time >= GameLoop.GameLoopTime)
-                    return _cachedActiveWeapon.item;
-
-                _cachedActiveWeapon.time = GameLoop.GameLoopTime;
-                _cachedActiveWeapon.slot = ActiveWeaponSlot;
-
-                switch (ActiveWeaponSlot)
-                {
-                    case eActiveWeaponSlot.Standard:
-                    {
-                        _cachedActiveWeapon.item = Inventory.GetItem(eInventorySlot.RightHandWeapon);
-                        break;
-                    }
-                    case eActiveWeaponSlot.TwoHanded:
-                    {
-                        _cachedActiveWeapon.item = Inventory.GetItem(eInventorySlot.TwoHandWeapon);
-                        break;
-                    }
-                    case eActiveWeaponSlot.Distance:
-                    {
-                        _cachedActiveWeapon.item = Inventory.GetItem(eInventorySlot.DistanceWeapon);
-                        break;
-                    }
-                    default:
-                    {
-                        _cachedActiveWeapon.item = null;
-                        break;
-                    }
-                }
-
-                return _cachedActiveWeapon.item;
-            }
-        }
-
-        /// <summary>
-        /// Returns the chance for a critical hit with a spell
-        /// </summary>
-        public virtual int SpellCriticalChance
+		/// <summary>
+		/// Returns the chance for a critical hit with a spell
+		/// </summary>
+		public virtual int SpellCriticalChance
 		{
 			get { return GetModified(eProperty.CriticalSpellHitChance); }
 			set { }
@@ -1285,7 +1247,7 @@ namespace DOL.GS
 			//your friend is most likely using a player crafted shield. The quality of the player crafted item will make a significant difference  try it and see.
 
 			double blockChance = 0;
-			DbInventoryItem leftHand = Inventory?.GetItem(eInventorySlot.LeftHandWeapon);
+			DbInventoryItem leftHand = ActiveLeftWeapon;
 
 			if (leftHand != null && (eObjectType) leftHand.Object_Type is not eObjectType.Shield)
 				leftHand = null;
@@ -2151,14 +2113,26 @@ namespace DOL.GS
 				case eActiveWeaponSlot.Standard:
 				{
 					if (rightHandSlot == null)
+					{
 						rightHand = 0xFF;
+						_activeWeapon = null;
+					}
 					else
+					{
 						rightHand = 0x00;
+						_activeWeapon = rightHandSlot;
+					}
 
 					if (leftHandSlot == null)
+					{
 						leftHand = 0xFF;
+						_activeLeftWeapon = null;
+					}
 					else
+					{
 						leftHand = 0x01;
+						_activeLeftWeapon = leftHandSlot;
+					}
 
 					break;
 				}
@@ -2168,19 +2142,33 @@ namespace DOL.GS
 					if (twoHandSlot != null && (twoHandSlot.Hand == 1 || this is GameNPC)) // 2h
 					{
 						rightHand = leftHand = 0x02;
+						_activeWeapon = twoHandSlot;
+						_activeLeftWeapon = null;
 						break;
 					}
 
 					// 1h weapon in 2h slot
 					if (twoHandSlot == null)
+					{
 						rightHand = 0xFF;
+						_activeWeapon = null;
+					}
 					else
+					{
 						rightHand = 0x02;
+						_activeWeapon = twoHandSlot;
+					}
 
 					if (leftHandSlot == null)
+					{
 						leftHand = 0xFF;
+						_activeLeftWeapon = null;
+					}
 					else
+					{
 						leftHand = 0x01;
+						_activeLeftWeapon = leftHandSlot;
+					}
 
 					break;
 				}
@@ -2188,14 +2176,19 @@ namespace DOL.GS
 				case eActiveWeaponSlot.Distance:
 				{
 					leftHand = 0xFF; // cannot use left-handed weapons if ranged slot active
+					_activeLeftWeapon = null;
 
 					if (distanceSlot == null)
+					{
 						rightHand = 0xFF;
+						_activeWeapon = null;
+					}
 					else if (distanceSlot.Hand == 1 || this is GameNPC) // NPC equipment does not have hand so always assume 2 handed bow
 						rightHand = leftHand = 0x03; // bows use 2 hands, throwing axes 1h
 					else
 						rightHand = 0x03;
 
+					_activeWeapon = distanceSlot;
 					break;
 				}
 			}

GameServer/gameobjects/GamePlayer.cs
@@ -5981,7 +5981,7 @@ namespace DOL.GS
                 }
                 case eAttackResult.Blocked:
                 {
-                    DbInventoryItem reactiveItem = Inventory.GetItem(eInventorySlot.LeftHandWeapon);
+                    DbInventoryItem reactiveItem = ActiveLeftWeapon;
                     if (reactiveItem != null && reactiveItem.Object_Type == (int)eObjectType.Shield)
                     {
                         TryReactiveEffect(reactiveItem, ad.Attacker);
@@ -7595,7 +7595,7 @@ namespace DOL.GS
                         if (useItem.Object_Type == (int) eObjectType.Poison)
                         {
                             DbInventoryItem mainHand = ActiveWeapon;
-                            DbInventoryItem leftHand = Inventory.GetItem(eInventorySlot.LeftHandWeapon);
+                            DbInventoryItem leftHand = ActiveLeftWeapon;
 
                             if (mainHand != null && mainHand.PoisonSpellID == 0)
                                 ApplyPoison(useItem, mainHand);

GameServer/gameobjects/Necromancer/NecromancerPet.cs
@@ -516,16 +516,16 @@ namespace DOL.GS
 				}
 				else
 				{
-					if ((item = Inventory.GetItem(eInventorySlot.RightHandWeapon)) != null)
+					if (ActiveWeapon != null)
 					{
-						item.DPS_AF = (int)(Level * 3.3);
-						item.SPD_ABS = 37;
+						ActiveWeapon.DPS_AF = (int)(Level * 3.3);
+						ActiveWeapon.SPD_ABS = 37;
 					}
 
-					if ((item = Inventory.GetItem(eInventorySlot.LeftHandWeapon)) != null)
+					if (ActiveLeftWeapon != null)
 					{
-						item.DPS_AF = (int)(Level * 3.3);
-						item.SPD_ABS = 37;
+						ActiveLeftWeapon.DPS_AF = (int)(Level * 3.3);
+						ActiveLeftWeapon.SPD_ABS = 37;
 					}
 
 					SwitchWeapon(eActiveWeaponSlot.Standard);

GameServer/keeps/Gameobjects/Guards/GameKeepGuard.cs
@@ -713,7 +713,7 @@ namespace DOL.GS.Keeps
 			int emblem = 0;
 			if (guild != null)
 				emblem = guild.Emblem;
-			DbInventoryItem lefthand = Inventory.GetItem(eInventorySlot.LeftHandWeapon);
+			DbInventoryItem lefthand = ActiveLeftWeapon;
 			if (lefthand != null)
 				lefthand.Emblem = emblem;
 

GameServer/realmabilities/effects/rr5/TestudoEffect.cs
@@ -41,7 +41,7 @@ namespace DOL.GS.Effects
 		{
 			GameLiving living = sender as GameLiving;
 			if (living == null) return;
-			DbInventoryItem shield = living.Inventory.GetItem(eInventorySlot.LeftHandWeapon);
+			DbInventoryItem shield = living.ActiveLeftWeapon;
 			if (shield == null)
 				return;
 			if (shield.Object_Type != (int)eObjectType.Shield)

GameServer/realmabilities/handlers/rr5/ShieldTripAbility.cs
@@ -19,7 +19,7 @@ namespace DOL.GS.RealmAbilities
 		public override void Execute(GameLiving living)
 		{
 			if (CheckPreconditions(living, DEAD | SITTING | MEZZED | STUNNED)) return;
-			DbInventoryItem shield = living.Inventory.GetItem(eInventorySlot.LeftHandWeapon);
+			DbInventoryItem shield = living.ActiveLeftWeapon;
 			if (shield == null)
 				return;
 			if (shield.Object_Type != (int)eObjectType.Shield)

GameServer/realmabilities/handlers/rr5/TestudoAbilty.cs
@@ -21,7 +21,7 @@ namespace DOL.GS.RealmAbilities
 			if (CheckPreconditions(living, DEAD | SITTING | MEZZED | STUNNED)) return;
 
 
-			DbInventoryItem shield = living.Inventory.GetItem(eInventorySlot.LeftHandWeapon);
+			DbInventoryItem shield = living.ActiveLeftWeapon;
 			if (shield == null)
 				return;
 			if (shield.Object_Type != (int)eObjectType.Shield)

GameServer/spells/Archery/Arrow.cs
@@ -174,7 +174,7 @@ namespace DOL.GS.Spells
 				if (target is GamePlayer && !target.IsStunned && !target.IsMezzed && !target.IsSitting && m_handler.Spell.LifeDrainReturn != (int)Archery.eShotType.Critical)
 				{
 					GamePlayer player = (GamePlayer)target;
-					DbInventoryItem lefthand = player.Inventory.GetItem(eInventorySlot.LeftHandWeapon);
+					DbInventoryItem lefthand = player.ActiveLeftWeapon;
 					if (lefthand != null && (player.ActiveWeapon == null || player.ActiveWeapon.Item_Type == Slot.RIGHTHAND || player.ActiveWeapon.Item_Type == Slot.LEFTHAND))
 					{
 						if (target.IsObjectInFront(caster, 180) && lefthand.Object_Type == (int)eObjectType.Shield)

GameServer/spells/Masterlevel/Battlemaster.cs
@@ -200,7 +200,7 @@ namespace DOL.GS.Spells
                 if (sender is GamePlayer)
                 {
                     GamePlayer player = (GamePlayer)sender;
-                    DbInventoryItem leftWeapon = player.Inventory.GetItem(eInventorySlot.LeftHandWeapon);
+                    DbInventoryItem leftWeapon = player.ActiveLeftWeapon;
                     // if we can use left weapon, we have currently a weapon in left hand and we still have endurance,
                     // we can assume that we are using the two weapons.
                     if (player.attackComponent.CanUseLefthandedWeapon && leftWeapon != null && leftWeapon.Object_Type != (int)eObjectType.Shield)
@@ -385,7 +385,7 @@ namespace DOL.GS.Spells
                         resultByte = 2;
                         if (ad.Target != null && ad.Target.Inventory != null)
                         {
-                            DbInventoryItem lefthand = ad.Target.Inventory.GetItem(eInventorySlot.LeftHandWeapon);
+                            DbInventoryItem lefthand = ad.Target.ActiveLeftWeapon;
                             if (lefthand != null && lefthand.Object_Type == (int)eObjectType.Shield)
                             {
                                 defendersWeapon = lefthand.Model;

GameServer/spells/Valkyrie/ValkyrieProc.cs
@@ -60,7 +60,7 @@ namespace DOL.GS.Spells
                 if (sender is GamePlayer)
                 {
                     GamePlayer player = (GamePlayer)sender;
-                    DbInventoryItem leftWeapon = player.Inventory.GetItem(eInventorySlot.LeftHandWeapon);
+                    DbInventoryItem leftWeapon = player.ActiveLeftWeapon;
                     // if we can use left weapon, we have currently a weapon in left hand and we still have endurance,
                     // we can assume that we are using the two weapons.
                     if (player.attackComponent.CanUseLefthandedWeapon && leftWeapon != null && leftWeapon.Object_Type != (int)eObjectType.Shield)

GameServer/styles/StyleProcessor.cs
@@ -175,7 +175,7 @@ namespace DOL.GS.Styles
 					return;
 				}
 
-				DbInventoryItem weapon = (style.WeaponTypeRequirement == (int) eObjectType.Shield) ? player.Inventory.GetItem(eInventorySlot.LeftHandWeapon) : player.ActiveWeapon;
+				DbInventoryItem weapon = (eObjectType) style.WeaponTypeRequirement is eObjectType.Shield ? player.ActiveLeftWeapon : player.ActiveWeapon;
 
 				if (!CheckWeaponType(style, player, weapon))
 				{
@@ -538,8 +538,9 @@ namespace DOL.GS.Styles
 		{
 			if (living is GameNPC)
 				return true;
-			GamePlayer player = living as GamePlayer;
-			if (player == null) return false;
+
+			if (living is not GamePlayer player)
+				return false;
 
 			switch (style.WeaponTypeRequirement)
 			{
@@ -548,17 +549,17 @@ namespace DOL.GS.Styles
 					// both weapons are needed to use style,
 					// shield is not a weapon here
 					DbInventoryItem rightHand = player.ActiveWeapon;
-					DbInventoryItem leftHand = player.Inventory.GetItem(eInventorySlot.LeftHandWeapon);
+					DbInventoryItem leftHand = player.ActiveLeftWeapon;
 
-					if (rightHand == null || leftHand == null || (rightHand.Item_Type != Slot.RIGHTHAND && rightHand.Item_Type != Slot.LEFTHAND))
+					if (rightHand == null || leftHand == null || (rightHand.Item_Type is not Slot.RIGHTHAND and not Slot.LEFTHAND))
 						return false;
 
-					if (style.Spec == Specs.HandToHand && (rightHand.Object_Type != (int)eObjectType.HandToHand || leftHand.Object_Type != (int)eObjectType.HandToHand))
+					if (style.Spec == Specs.HandToHand && (eObjectType) rightHand.Object_Type is not eObjectType.HandToHand || (eObjectType) leftHand.Object_Type is not eObjectType.HandToHand)
 						return false;
-					else if (style.Spec == Specs.Fist_Wraps && (rightHand.Object_Type != (int)eObjectType.FistWraps || leftHand.Object_Type != (int)eObjectType.FistWraps))
+					else if (style.Spec == Specs.Fist_Wraps && (eObjectType) rightHand.Object_Type is not eObjectType.FistWraps || (eObjectType) leftHand.Object_Type is not eObjectType.FistWraps)
 						return false;
 
-					return leftHand.Object_Type != (int)eObjectType.Shield;
+					return (eObjectType) leftHand.Object_Type is not eObjectType.Shield;
 				}
 				case Style.SpecialWeaponType.AnyWeapon:
 				{