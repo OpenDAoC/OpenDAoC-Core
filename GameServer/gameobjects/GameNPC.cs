OpenDAoC/OpenDAoC-Core

commit a928a48b2e29a6eccfa0fedc757ebceb1ca81401

Fix potential issues with NPCs not sorting their spells correctly * This would happen if a NPC changed it's brain state before calling `Think` at least once while it's still `WAKING_UP` * `CheckAbilities` and `SortSpells` can't be called too soon, since brains tend to be created and their state set before it gets a body. A way to solve this is to set the state in `GameNPC.SetOwnBrain` instead of in the brain's constructor.

GameServer/gameobjects/GameNPC.cs
@@ -2154,15 +2154,20 @@ namespace DOL.GS
 		{
 			if (brain == null)
 				return null;
+
 			if (brain.IsActive)
 				throw new ArgumentException("The new brain is already active.", "brain");
 
 			ABrain oldBrain = m_ownBrain;
 			bool activate = oldBrain.IsActive;
+
 			if (activate)
 				oldBrain.Stop();
+
 			m_ownBrain = brain;
 			m_ownBrain.Body = this;
+			m_ownBrain.FSM?.SetCurrentState(eFSMStateType.WAKING_UP);
+
 			if (activate)
 				m_ownBrain.Start();
 

GameServer/ai/brain/ControlledMob/ControlledMobBrain.cs
@@ -72,7 +72,6 @@ namespace DOL.AI.Brain
 			FSM.Add(new ControlledMobState_DEFENSIVE(this));
 			FSM.Add(new ControlledMobState_AGGRO(this));
 			FSM.Add(new ControlledMobState_PASSIVE(this));
-			FSM.SetCurrentState(eFSMStateType.WAKING_UP);
 		}
 
 		protected bool m_isMainPet = true;

GameServer/ai/brain/ControlledMob/ControlledMobState.cs
@@ -11,16 +11,20 @@ namespace DOL.AI.Brain
             StateType = eFSMStateType.WAKING_UP;
         }
 
+        public override void Enter()
+        {
+            if (_abilitiesChecked)
+                return;
+
+            ControlledMobBrain brain = _brain as ControlledMobBrain;
+            brain.CheckAbilities();
+            brain.Body.SortSpells();
+            _abilitiesChecked = true;
+        }
+
         public override void Think()
         {
             ControlledMobBrain brain = _brain as ControlledMobBrain;
-
-            if (!_abilitiesChecked)
-            {
-                brain.CheckAbilities();
-                brain.Body.SortSpells();
-                _abilitiesChecked = true;
-            }
 
             if (brain.AggressionState == eAggressionState.Aggressive)
                 brain.FSM.SetCurrentState(eFSMStateType.AGGRO);

GameServer/ai/brain/Necromancer/NecromancerPetBrain.cs
@@ -22,7 +22,6 @@ namespace DOL.AI.Brain
             FSM.Add(new NecromancerPetState_DEFENSIVE(this));
             FSM.Add(new NecromancerPetState_AGGRO(this));
             FSM.Add(new NecromancerPetState_PASSIVE(this));
-            FSM.SetCurrentState(eFSMStateType.WAKING_UP);
         }
 
         public override int ThinkInterval => 500;

GameServer/ai/brain/StandardMob/StandardMobBrain.cs
@@ -41,7 +41,6 @@ namespace DOL.AI.Brain
             FSM.Add(new StandardMobState_RETURN_TO_SPAWN(this));
             FSM.Add(new StandardMobState_PATROLLING(this));
             FSM.Add(new StandardMobState_ROAMING(this));
-            FSM.SetCurrentState(eFSMStateType.WAKING_UP);
         }
 
         /// <summary>

GameServer/scripts/namedmobs/Dodens/AI/ArosBrain/ArosBrain.cs
@@ -30,7 +30,6 @@ namespace DOL.AI.Brain
             FSM.Add(new ArosState_IDLE(this));
             FSM.Add(new ArosState_AGGRO(this));
             FSM.Add(new ArosState_RETURN_TO_SPAWN(this));
-            FSM.SetCurrentState(eFSMStateType.WAKING_UP);
         }
 
         /// <summary>

commit 4bb1655b6a9d81f136413439815ba638636af461

Fix default brains not having their initial state set Caused by a928a48b

GameServer/gameobjects/GameNPC.cs
@@ -2159,18 +2159,14 @@ namespace DOL.GS
 				throw new ArgumentException("The new brain is already active.", "brain");
 
 			ABrain oldBrain = m_ownBrain;
-			bool activate = oldBrain.IsActive;
 
-			if (activate)
+			if (oldBrain != null)
 				oldBrain.Stop();
 
 			m_ownBrain = brain;
 			m_ownBrain.Body = this;
 			m_ownBrain.FSM?.SetCurrentState(eFSMStateType.WAKING_UP);
-
-			if (activate)
-				m_ownBrain.Start();
-
+			m_ownBrain.Start();
 			return oldBrain;
 		}
 
@@ -4218,8 +4214,8 @@ namespace DOL.GS
 
 			if (m_ownBrain == null)
 			{
-				m_ownBrain = defaultBrain;
-				m_ownBrain.Body = this;
+				defaultBrain.Body = this;
+				SetOwnBrain(defaultBrain);
 			}
 		}
 