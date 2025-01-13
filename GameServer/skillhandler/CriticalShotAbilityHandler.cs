057a2f017151478f20dbfffc67f4ea6d80e83109
Baptiste Marie <bm01@users.noreply.github.com>
2025-01-13 20:33:13 +0100

    Revert previous commit This was added in 1.88.

--- GameServer/skillhandler/CriticalShotAbilityHandler.cs
+++ GameServer/skillhandler/CriticalShotAbilityHandler.cs
@@ -1,72 +1,83 @@
+/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+using System;
 using DOL.GS.PacketHandler;
+using DOL.GS.Effects;
 using DOL.Language;
 
 namespace DOL.GS.SkillHandler
 {
-    /// <summary>
-    /// Handler for Critical Shot ability
-    /// </summary>
-    [SkillHandlerAttribute(Abilities.Critical_Shot)]
-    public class CriticalShotAbilityHandler : IAbilityActionHandler
-    {
-        public void Execute(Ability ab, GamePlayer player)
-        {
-            if (player.ActiveWeaponSlot is not eActiveWeaponSlot.Distance)
-            {
+	/// <summary>
+	/// Handler for Critical Shot ability
+	/// </summary>
+	[SkillHandlerAttribute(Abilities.Critical_Shot)]
+	public class CriticalShotAbilityHandler : IAbilityActionHandler
+	{
+		public void Execute(Ability ab, GamePlayer player)
+		{
+			if (player.ActiveWeaponSlot != eActiveWeaponSlot.Distance)
+			{
                 player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language, "Skill.Ability.CannotUse.CriticalShot.NoRangedWeapons"), eChatType.CT_Important, eChatLoc.CL_SystemWindow);
                 return;
-            }
-
-            if (player.IsSitting)
-            {
+			}
+			if (player.IsSitting)
+			{
                 player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language, "Skill.Ability.CannotUse.CriticalShot.MustBeStanding"), eChatType.CT_YouHit, eChatLoc.CL_SystemWindow);
                 return;
-            }
+			}
 
-            if (!player.IsStealthed)
+			// cancel rapid fire effect
+			RapidFireECSGameEffect rapidFire = (RapidFireECSGameEffect)EffectListService.GetAbilityEffectOnTarget(player, eEffect.RapidFire);
+			if (rapidFire != null)
+				EffectService.RequestImmediateCancelEffect(rapidFire, false);
+
+			// cancel sure shot effect
+			SureShotECSGameEffect sureShot = (SureShotECSGameEffect)EffectListService.GetAbilityEffectOnTarget(player, eEffect.SureShot);
+			if (sureShot != null)
+				EffectService.RequestImmediateCancelEffect(sureShot);
+
+			TrueShotECSGameEffect trueshot = (TrueShotECSGameEffect)EffectListService.GetAbilityEffectOnTarget(player, eEffect.TrueShot);
+			if (trueshot != null)
+				EffectService.RequestImmediateCancelEffect(trueshot, false);
+
+			ECSGameEffect volley = EffectListService.GetEffectOnTarget(player, eEffect.Volley);
+			if (volley != null)
             {
-                player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language, "Skill.Ability.CannotUse.CriticalShot.MustBeStealthed"), eChatType.CT_YouHit, eChatLoc.CL_SystemWindow);
-                return;
-            }
+				player.Out.SendMessage("You can't use Critical-Shot while Volley is active!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+				return;
+			}
 
-            RapidFireECSGameEffect rapidFire = EffectListService.GetAbilityEffectOnTarget(player, eEffect.RapidFire) as RapidFireECSGameEffect;
-
-            if (rapidFire != null)
-                EffectService.RequestImmediateCancelEffect(rapidFire, false);
-
-            SureShotECSGameEffect sureShot = EffectListService.GetAbilityEffectOnTarget(player, eEffect.SureShot) as SureShotECSGameEffect;
-
-            if (sureShot != null)
-                EffectService.RequestImmediateCancelEffect(sureShot);
-
-            TrueShotECSGameEffect trueShot = EffectListService.GetAbilityEffectOnTarget(player, eEffect.TrueShot) as TrueShotECSGameEffect;
-
-            if (trueShot != null)
-                EffectService.RequestImmediateCancelEffect(trueShot, false);
-
-            ECSGameEffect volley = EffectListService.GetEffectOnTarget(player, eEffect.Volley);
-
-            if (volley != null)
-            {
-                player.Out.SendMessage("You can't use Critical-Shot while Volley is active!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                return;
-            }
-
-            if (player.attackComponent.AttackState)
-            {
-                if (player.rangeAttackComponent.RangedAttackType is eRangedAttackType.Critical)
-                {
+			if (player.attackComponent.AttackState)
+			{
+				if (player.rangeAttackComponent.RangedAttackType == eRangedAttackType.Critical)
+				{
                     player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language, "Skill.Ability.CriticalShot.SwitchToRegular"), eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                    player.rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
-                }
-                else
+					player.rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
+				}
+				else
+				{
                     player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language, "Skill.Ability.CriticalShot.AlreadyFiring"), eChatType.CT_Important, eChatLoc.CL_SystemWindow);
-
-                return;
-            }
-
-            player.rangeAttackComponent.RangedAttackType = eRangedAttackType.Critical;
-            player.attackComponent.RequestStartAttack();
-        }
-    }
+				}
+				return;
+			}
+			player.rangeAttackComponent.RangedAttackType = eRangedAttackType.Critical;
+			player.attackComponent.RequestStartAttack();
+		}
+	}
 }
