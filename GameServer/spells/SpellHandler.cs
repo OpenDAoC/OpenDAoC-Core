966945073d6bdb4dbd178a48fe358dc7cea745be
Baptiste Marie <bm01@users.noreply.github.com>
2025-01-14 23:08:42 +0100

    Revert 9153a75 and apply it to Necromancer (shade) instant spells only

--- GameServer/spells/SpellHandler.cs
+++ GameServer/spells/SpellHandler.cs
@@ -1094,7 +1094,17 @@ namespace DOL.GS.Spells
 							if (!CheckEndCast(Target))
 								CastState = eCastState.Interrupted;
 							else
+							{
+								// Unsure about this. Calling 'SendCastAnimation' on non-harmful instant spells plays an annoying deep hum that overlaps with the
+								// sound of the spell effect (but is fine to have on harmful ones). For certain spells (like Skald's resist chants) it instead
+								// plays the audio of the spell effect a second time.
+								// It may prevent certain animations from playing, but I don't think there's any non-harmful instant spell with a casting animation.
+								// We're also excluding Necromancer's instant spells because it can get a little overwhelming and was often reported.
+								if (Spell.IsHarmful && Spell.SpellType is not eSpellType.PetSpell)
+									SendCastAnimation(0);
+
 								CastState = eCastState.Finished;
+							}
 						}
 						else
 						{
