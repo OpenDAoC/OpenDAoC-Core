OpenDAoC/OpenDAoC-Core

commit 29bd76e9273e433030c16c27661ddb3213b925cb

Fix various cases of focus spells not being canceled properly * When drawing weapon or attacking something in melee (the spell would remain and still pulse) * When cancelling it by clicking on it again (the pulse effect would be removed but the damage shield would remain for the effect duration on the target). * Note: They still don't get cancelled when getting attacked because of `SpellHandler.CasterIsAttacked` checking for `IsInCastingPhase` (which is false when in focus state); not sure if intended. * Note: Hard CC effects still cancel them. * `CastingComponnet.InterruptCasting` is now responsible for cancelling focus spells. * Changed accessibility of `SpellHandler.InterruptCasting` to protected.

GameServer/ECS-Effects/ECSPulseEffect.cs
@@ -32,6 +32,12 @@ namespace DOL.GS
         public override void OnStopEffect()
         {
             Owner.ActivePulseSpells.TryRemove(SpellHandler.Spell.SpellType, out _);
+
+            foreach (ECSGameEffect petEffect in SpellHandler.Target.effectListComponent.GetSpellEffects(eEffect.FocusShield))
+            {
+                if (petEffect.SpellHandler.Spell.IsFocus)
+                    EffectService.RequestImmediateCancelEffect(petEffect);
+            }
         }
     }
 }

GameServer/ECS-Components/CastingComponent.cs
@@ -79,12 +79,17 @@ namespace DOL.GS
                 startSkillRequest.StartSkill();
         }
 
-        public void InterruptCasting()
+        public void InterruptCasting(bool moving)
         {
-            if (SpellHandler?.IsInCastingPhase == true)
+            if (SpellHandler != null)
             {
-                foreach (GamePlayer player in Owner.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
-                    player.Out.SendInterruptAnimation(Owner);
+                if (SpellHandler.IsInCastingPhase)
+                {
+                    foreach (GamePlayer player in Owner.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
+                        player.Out.SendInterruptAnimation(Owner);
+                }
+
+                CancelFocusSpells(moving);
             }
 
             ClearUpSpellHandlers();
@@ -249,7 +254,7 @@ namespace DOL.GS
                     // Otherwise the only way to cancel an out of LoS / range flute mez is to swap weapons.
                     if (currentSpellHandler.CastState is eCastState.CastingRetry)
                     {
-                        currentSpellHandler.InterruptCasting();
+                        CastingComponent.InterruptCasting(false);
 
                         if (newSpell.SpellType is eSpellType.Mesmerize && newSpell.InstrumentRequirement != 0)
                         {

GameServer/gameobjects/GameLiving.cs
@@ -3678,7 +3678,7 @@ namespace DOL.GS
 		/// </summary>
 		public virtual void StopCurrentSpellcast()
 		{
-			castingComponent.InterruptCasting();
+			castingComponent.InterruptCasting(IsMoving);
 		}
 
 		public virtual bool CastSpell(Spell spell, SpellLine line, ISpellCastingAbilityHandler spellCastingAbilityHandler = null)

GameServer/spells/ISpellHandler.cs
@@ -23,12 +23,6 @@ namespace DOL.GS.Spells
 		bool StartSpell(GameLiving target, DbInventoryItem item);
 
 		/// <summary>
-		/// Whenever the current casting sequence is to be interrupted
-		/// this callback is called
-		/// </summary>
-		void InterruptCasting();
-
-		/// <summary>
 		/// Has to be called when the caster moves
 		/// </summary>
 		void CasterMoves();

GameServer/spells/SpellHandler.cs
@@ -328,14 +328,6 @@ namespace DOL.GS.Spells
 			if (Spell.MoveCast)
 				return;
 
-			if (Caster is GamePlayer playerCaster)
-			{
-				if (CastState is not eCastState.Focusing)
-					playerCaster.Out.SendMessage(LanguageMgr.GetTranslation(playerCaster.Client, "SpellHandler.CasterMove"), eChatType.CT_Important, eChatLoc.CL_SystemWindow);
-				else
-					CancelFocusSpells(true);
-			}
-
 			InterruptCasting();
 		}
 
@@ -1277,13 +1269,13 @@ namespace DOL.GS.Spells
 		/// <summary>
 		/// Called whenever the casters casting sequence is to interrupt immediately
 		/// </summary>
-		public virtual void InterruptCasting()
+		protected virtual void InterruptCasting()
 		{
 			if (m_interrupted)
 				return;
 
 			m_interrupted = true;
-			Caster.castingComponent.InterruptCasting();
+			Caster.castingComponent.InterruptCasting(Caster.IsMoving);
 			CastState = eCastState.Interrupted;
 			m_startReuseTimer = false;
 		}
@@ -2028,10 +2020,7 @@ namespace DOL.GS.Spells
 		public virtual bool StartSpell(GameLiving target)
 		{
 			if (Caster.IsMezzed || Caster.IsStunned)
-			{
-				CancelFocusSpells(false);
 				return false;
-			}
 
 			if (Spell.SpellType is not eSpellType.TurretPBAoE && Spell.IsPBAoE)
 				Target = Caster;
@@ -2628,12 +2617,6 @@ namespace DOL.GS.Spells
 
 				if (EffectService.RequestImmediateCancelEffect(pulseSpell))
 					cancelled = true;
-
-				foreach (ECSGameEffect petEffect in pulseSpell.SpellHandler.Target.effectListComponent.GetSpellEffects(eEffect.FocusShield))
-				{
-					if (petEffect.SpellHandler.Spell.IsFocus)
-						EffectService.RequestImmediateCancelEffect(petEffect);
-				}
 			}
 
 			if (cancelled)

GameServer/spells/Teleport/GatewayPersonalBind.cs
@@ -94,9 +94,6 @@ namespace DOL.GS.Spells
         public override void CasterMoves()
         {
             InterruptCasting();
-
-            if (Caster is GamePlayer playerCaster)
-                playerCaster.Out.SendMessage(LanguageMgr.GetTranslation(playerCaster.Client, "SpellHandler.CasterMove"), eChatType.CT_Important, eChatLoc.CL_SystemWindow);
         }
 
         public override IList<string> DelveInfo

commit 3a548ab071aebef7e55059e780d794281e1ab8c6

Fix NRE on pulse spell cancel caused by 29bd76e

GameServer/ECS-Effects/ECSPulseEffect.cs
@@ -33,10 +33,15 @@ namespace DOL.GS
         {
             Owner.ActivePulseSpells.TryRemove(SpellHandler.Spell.SpellType, out _);
 
-            foreach (ECSGameEffect petEffect in SpellHandler.Target.effectListComponent.GetSpellEffects(eEffect.FocusShield))
+            if (!SpellHandler.Spell.IsFocus)
+                return;
+
+            foreach (var pair in ChildEffects)
             {
-                if (petEffect.SpellHandler.Spell.IsFocus)
-                    EffectService.RequestImmediateCancelEffect(petEffect);
+                ECSGameSpellEffect effect = pair.Value;
+
+                if (effect.EffectType is eEffect.FocusShield)
+                    EffectService.RequestImmediateCancelEffect(effect);
             }
         }
     }