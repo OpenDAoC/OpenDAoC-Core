OpenDAoC/OpenDAoC-Core

commit 8c59037ad09383961c91790cad3eea3cdbcd7127

Change potion targeting logic to accomodate for custom spells There's no reason why target should be different than `this`.

GameServer/gameobjects/GamePlayer.cs
@@ -7635,23 +7635,6 @@ namespace DOL.GS
                                         {
                                             if (ScriptMgr.CreateSpellHandler(this, spell, potionEffectLine) is SpellHandler spellHandler)
                                             {
-                                                GameLiving target = TargetObject as GameLiving;
-
-                                                if (spellHandler is AllStatsBarrel)
-                                                    target = this;
-
-                                                if (spellHandler is AllRegenBuff)
-                                                    target = this;
-
-                                                if (spell.Target == eSpellTarget.ENEMY)
-                                                {
-                                                    if (!GameServer.ServerRules.IsAllowedToAttack(this, target, true))
-                                                    {
-                                                        Out.SendMessage("You need a target for this ability!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                                                        return;
-                                                    }
-                                                }
-
                                                 if (spell.IsHealing && spell.CastTime == 0 && IsAttacking)
                                                 {
                                                     Out.SendMessage(LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.UseSlot.CantUseAttacking", useItem.GetName(0, false)), eChatType.CT_System, eChatLoc.CL_SystemWindow);
@@ -7668,7 +7651,7 @@ namespace DOL.GS
                                                         TempProperties.SetProperty(NEXT_SPELL_AVAIL_TIME_BECAUSE_USE_POTION, 6 * 1000 + CurrentRegion.Time);
                                                 }
 
-                                                if (spellHandler.StartSpell(target, useItem))
+                                                if (spellHandler.StartSpell(this, useItem))
                                                 {
                                                     if (useItem.Count > 1)
                                                     {

commit 7cfe58e30bd1ff1dfd4cb604a877f2797ba22b08

Fix rams not targetting doors Caused by 8c59037 and the fact that rams actually are potions.

GameServer/gameobjects/GamePlayer.cs
@@ -7489,23 +7489,23 @@ namespace DOL.GS
                                     {
                                         int requiredLevel = useItem.Template.LevelRequirement > 0 ? useItem.Template.LevelRequirement : Math.Min(MaxLevel, useItem.Level);
 
-                                    if (requiredLevel <= Level)
-                                    {
-                                        if (spell.CastTime > 0 && attackComponent.AttackState)
-                                            Out.SendMessage(LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.UseSlot.CantUseInCombat"), eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                                        else if ((IsStunned && !(Steed != null && Steed.Name == "Forceful Zephyr")) || IsMezzed || !IsAlive)
-                                            Out.SendMessage(LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.UseSlot.CantUseState", useItem.GetName(0, false)), eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                                        else if (spell.CastTime > 0 && IsCasting)
-                                            Out.SendMessage(LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.UseSlot.CantUseCast", useItem.GetName(0, false)), eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                                        else
+                                        if (requiredLevel <= Level)
                                         {
-                                            if (ScriptMgr.CreateSpellHandler(this, spell, potionEffectLine) is SpellHandler spellHandler)
+                                            if (spell.CastTime > 0 && attackComponent.AttackState)
+                                                Out.SendMessage(LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.UseSlot.CantUseInCombat"), eChatType.CT_System, eChatLoc.CL_SystemWindow);
+                                            else if ((IsStunned && !(Steed != null && Steed.Name == "Forceful Zephyr")) || IsMezzed || !IsAlive)
+                                                Out.SendMessage(LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.UseSlot.CantUseState", useItem.GetName(0, false)), eChatType.CT_System, eChatLoc.CL_SystemWindow);
+                                            else if (spell.CastTime > 0 && IsCasting)
+                                                Out.SendMessage(LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.UseSlot.CantUseCast", useItem.GetName(0, false)), eChatType.CT_System, eChatLoc.CL_SystemWindow);
+                                            else
                                             {
-                                                if (spell.IsHealing && spell.CastTime == 0 && IsAttacking)
+                                                if (ScriptMgr.CreateSpellHandler(this, spell, potionEffectLine) is SpellHandler spellHandler)
                                                 {
-                                                    Out.SendMessage(LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.UseSlot.CantUseAttacking", useItem.GetName(0, false)), eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                                                    return;
-                                                }
+                                                    if (spell.IsHealing && spell.CastTime == 0 && IsAttacking)
+                                                    {
+                                                        Out.SendMessage(LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.UseSlot.CantUseAttacking", useItem.GetName(0, false)), eChatType.CT_System, eChatLoc.CL_SystemWindow);
+                                                        return;
+                                                    }
 
                                                     Stealth(false);
 
@@ -7517,16 +7517,16 @@ namespace DOL.GS
                                                             TempProperties.SetProperty(NEXT_SPELL_AVAIL_TIME_BECAUSE_USE_POTION, 6 * 1000 + CurrentRegion.Time);
                                                     }
 
-                                                if (spellHandler.StartSpell(this, useItem))
-                                                {
-                                                    if (useItem.Count > 1)
+                                                    if (spellHandler.StartSpell(this, useItem))
                                                     {
-                                                        Inventory.RemoveCountFromStack(useItem, 1);
-                                                        InventoryLogging.LogInventoryAction(this, "(potion)", eInventoryActionType.Other, useItem.Template);
-                                                    }
-                                                    else
-                                                    {
-                                                        useItem.Charges--;
+                                                        if (useItem.Count > 1)
+                                                        {
+                                                            Inventory.RemoveCountFromStack(useItem, 1);
+                                                            InventoryLogging.LogInventoryAction(this, "(potion)", eInventoryActionType.Other, useItem.Template);
+                                                        }
+                                                        else
+                                                        {
+                                                            useItem.Charges--;
 
                                                             if (useItem.Charges < 1)
                                                             {

GameServer/scripts/siege/SummonSiegeRam.cs
@@ -7,134 +7,146 @@ namespace DOL.GS.Spells
     [SpellHandler(eSpellType.SummonSiegeRam)]
     public class SummonSiegeRam : SpellHandler
     {
-	    public SummonSiegeRam(GameLiving caster, Spell spell, SpellLine line)
-            : base(caster, spell, line) { }
-
+        public SummonSiegeRam(GameLiving caster, Spell spell, SpellLine line) : base(caster, spell, line) { }
 
         public override bool StartSpell(GameLiving target)
         {
-	        if (!Caster.CurrentZone.IsOF || Caster.CurrentRegion.IsDungeon){
-		        MessageToCaster("You cannot use siege weapons here!", eChatType.CT_SpellResisted);
-		        return false;
-	        }
-
-	        if (Caster is not GamePlayer player)
-		        return false;
-
-	        if (!player.CurrentZone.IsOF || player.CurrentRegion.IsDungeon)
-	        {
-		        MessageToCaster("You cannot use siege weapons here!", eChatType.CT_SpellResisted);
-		        return false;
-	        }
-	        
-	        if (target is not (GameKeepDoor or GameRelicDoor))
-	        {
-		        MessageToCaster("You need to target a door!", eChatType.CT_SpellResisted);
-		        return false;
-	        }
-            
-	        if (!target.IsAttackable)
-	        {
-		        MessageToCaster("You cannot attack your target.", eChatType.CT_SpellResisted);
-		        return false;
-	        }
-	        
-	        if (!Caster.IsWithinRadius(target, 500))
-	        {
-		        player.Out.SendMessage("You are too far away to attack " + Caster.TargetObject.Name, eChatType.CT_SpellResisted, eChatLoc.CL_SystemWindow);
-		        return false;
-	        }
-	        
-	        if (Caster.GetDistanceTo(target) < 200)
-	        {
-		        player.Out.SendMessage("You are too close to attack " + Caster.TargetObject.Name, eChatType.CT_SpellResisted, eChatLoc.CL_SystemWindow);
-		        return false;
-	        }
-	        
-	        //Limit 2 Rams in a certain radius
-			int ramSummonRadius = 200;
-            int ramsInRadius = 0;
-			foreach (GameNPC npc in Caster.GetNPCsInRadius((ushort) ramSummonRadius))
-			{
-				if(npc is GameSiegeRam ram && ram.Realm == Caster.Realm)
-					ramsInRadius++;	
-			}
-
-			if (ramsInRadius >= 2)
-			{
-				MessageToCaster("Too many rams in this area and you cannot summon another ram here!", eChatType.CT_SpellResisted);
+            if (!Caster.CurrentZone.IsOF || Caster.CurrentRegion.IsDungeon)
+            {
+                MessageToCaster("You cannot use siege weapons here!", eChatType.CT_SpellResisted);
                 return false;
-			}
+            }
+
+            if (Caster is not GamePlayer player)
+                return false;
+
+            if (!player.CurrentZone.IsOF || player.CurrentRegion.IsDungeon)
+            {
+                MessageToCaster("You cannot use siege weapons here!", eChatType.CT_SpellResisted);
+                return false;
+            }
+
+            // Overwrite the target, since this is used like a potion.
+            target = Caster.TargetObject as GameLiving;
+
+            if (target is not (GameKeepDoor or GameRelicDoor))
+            {
+                MessageToCaster("You need to target a door!", eChatType.CT_SpellResisted);
+                return false;
+            }
+
+            if (!target.IsAttackable)
+            {
+                MessageToCaster("You cannot attack your target.", eChatType.CT_SpellResisted);
+                return false;
+            }
+
+            if (!Caster.IsWithinRadius(target, 500))
+            {
+                player.Out.SendMessage($"You are too far away to attack {Caster.TargetObject.Name}", eChatType.CT_SpellResisted, eChatLoc.CL_SystemWindow);
+                return false;
+            }
+
+            if (Caster.GetDistanceTo(target) < 200)
+            {
+                player.Out.SendMessage($"You are too close to attack {Caster.TargetObject.Name}", eChatType.CT_SpellResisted, eChatLoc.CL_SystemWindow);
+                return false;
+            }
+
+            // Limit 2 Rams in a certain radius.
+            int ramSummonRadius = 200;
+            int ramsInRadius = 0;
+
+            foreach (GameNPC npc in Caster.GetNPCsInRadius((ushort) ramSummonRadius))
+            {
+                if (npc is GameSiegeRam ram && ram.Realm == Caster.Realm)
+                    ramsInRadius++;
+            }
+
+            if (ramsInRadius >= 2)
+            {
+                MessageToCaster("Too many rams in this area and you cannot summon another ram here!", eChatType.CT_SpellResisted);
+                return false;
+            }
 
             return base.StartSpell(target);
         }
 
+        public override void ApplyEffectOnTarget(GameLiving target)
+        {
+            base.ApplyEffectOnTarget(target);
 
-	    public override void ApplyEffectOnTarget(GameLiving target)
-	    {
-		    base.ApplyEffectOnTarget(target);
+            if (Caster is not GamePlayer player)
+                return;
 
-		    if (Caster is not GamePlayer player) return;
-
-		    GameSiegeRam ram = new GameSiegeRam();
-            ram.X = Caster.X;
-            ram.Y = Caster.Y;
-            ram.Z = Caster.Z;
-            ram.Heading = Caster.Heading;
-            ram.CurrentRegion = Caster.CurrentRegion;
-            ram.Realm = Caster.Realm;
+            GameSiegeRam ram = new()
+            {
+                X = Caster.X,
+                Y = Caster.Y,
+                Z = Caster.Z,
+                Heading = Caster.Heading,
+                CurrentRegion = Caster.CurrentRegion,
+                Realm = Caster.Realm
+            };
 
             //determine the ram level based on Spell Damage
-            switch(Spell.Damage)
+            switch (Spell.Damage)
             {
                 case 0:
+                {
                     ram.Level = 0;
                     ram.Name = "mini siege ram";
                     ram.Model = 2605;
                     break;
+                }
                 case 1:
+                {
                     ram.Level = 1;
                     ram.Name = "light siege ram";
                     ram.Model = 2600;
                     break;
+                }
                 case 2:
+                {
                     ram.Level = 2;
                     ram.Name = "medium siege ram";
                     ram.Model = 2601;
                     break;
+                }
                 case 3:
+                {
                     ram.Level = 3;
                     ram.Name = "heavy siege ram";
                     ram.Model = 2602;
                     break;
+                }
             }
 
             ram.AddToWorld();
-
             player.MountSteed(ram,true);
             ram.TakeControl(player);
-	    }
+        }
 
         public override bool CheckBeginCast(GameLiving selectedTarget)
         {
-	        if (selectedTarget is not (GameKeepDoor or GameRelicDoor))
-	        {
-		        MessageToCaster("You need to target a door!", eChatType.CT_SpellResisted);
-		        return false;
-	        }
-	        
+            if (selectedTarget is not (GameKeepDoor or GameRelicDoor))
+            {
+                MessageToCaster("You need to target a door!", eChatType.CT_SpellResisted);
+                return false;
+            }
+
             return base.CheckBeginCast(selectedTarget);
         }
 
         public override IList<string> DelveInfo
-		{
-			get
-			{
-				var list = new List<string>();
-				list.Add(Spell.Description);
-
-				return list;
-			}
-		}
+        {
+            get
+            {
+                return new List<string>
+                {
+                    Spell.Description
+                };
+            }
+        }
     }
 }