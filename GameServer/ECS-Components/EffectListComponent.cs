OpenDAoC/OpenDAoC-Core

commit 0358f2dd5fbf29d3cbccab353bc99ad7ff1d684c

Attempt to fix some buffs / debuffs not being refreshed properly

GameServer/ECS-Components/EffectListComponent.cs
@@ -104,15 +104,15 @@ namespace DOL.GS
                                         if (newSpell.IsPulsing)
                                             newSpellEffect.RenewEffect = true;
 
-                                        // If the effectiveness changed (for example after resurrection illness expired), we need to stop the effect.
-                                        // It will be restarted in 'EffectService.HandlePropertyModification()'.
-                                        // Also needed for movement speed debuffs' since their effect decrease over time.
-                                        if (existingEffect.Effectiveness != newSpellEffect.Effectiveness ||
-                                            existingSpell.SpellType is eSpellType.SpeedDecrease or eSpellType.UnbreakableSpeedDecrease)
-                                        {
-                                            existingEffect.OnStopEffect();
-                                        }
-
+                                        // Always stop the current effect and let the new one be started by 'EffectService.HandlePropertyModification()'.
+                                        // Not calling `OnStopEffect` on the current effect and `OnStartEffect` on the new effect means some initialization may not be performed.
+                                        // To give some examples:
+                                        // * Effectiveness changes from resurrection illness expiring.
+                                        // * Champion debuffs being forced to spec debuffs in `OnStartEffect`.
+                                        // * Movement speed debuffs effectiveness decreasing over time.
+                                        existingEffect.IsSilent = true;
+                                        existingEffect.OnStopEffect();
+                                        newSpellEffect.IsSilent = true;
                                         newSpellEffect.IsDisabled = existingEffect.IsDisabled;
                                         newSpellEffect.IsBuffActive = existingEffect.IsBuffActive;
                                         newSpellEffect.PreviousPosition = GetAllEffects().IndexOf(existingEffect);

GameServer/ECS-Effects/ECSGameSpellEffect.cs
@@ -21,6 +21,7 @@ namespace DOL.GS
         public override string Name => SpellHandler.Spell.Name;
         public override bool HasPositiveEffect => SpellHandler != null && SpellHandler.HasPositiveEffect;
         public bool IsAllowedToPulse => NextTick > 0 && PulseFreq > 0;
+        public bool IsSilent { get; set; } // Used externally to force an effect to be silent before being enabled or disabled. Resets itself.
 
         public ECSGameSpellEffect(ECSGameEffectInitParams initParams) : base(initParams)
         {
@@ -106,7 +107,10 @@ namespace DOL.GS
         /// <returns>'Message1' and 'Message2' values from the 'spell' table.</returns>
         public void OnEffectStartsMsg(GameLiving target, bool msgTarget, bool msgSelf, bool msgArea)
         {
-            SendMessages(target, msgTarget, msgSelf, msgArea, SpellHandler.Spell.Message1, SpellHandler.Spell.Message2);
+            if (!IsSilent)
+                SendMessages(target, msgTarget, msgSelf, msgArea, SpellHandler.Spell.Message1, SpellHandler.Spell.Message2);
+            else
+                IsSilent = false;
         }
 
         /// <summary>
@@ -119,7 +123,10 @@ namespace DOL.GS
         /// <returns>'Message3' and 'Message4' values from the 'spell' table.</returns>
         public void OnEffectExpiresMsg(GameLiving target, bool msgTarget, bool msgSelf, bool msgArea)
         {
-            SendMessages(target, msgTarget, msgSelf, msgArea, SpellHandler.Spell.Message3, SpellHandler.Spell.Message4);
+            if (!IsSilent)
+                SendMessages(target, msgTarget, msgSelf, msgArea, SpellHandler.Spell.Message3, SpellHandler.Spell.Message4);
+            else
+                IsSilent = false;
         }
 
         private void SendMessages(GameLiving target, bool msgTarget, bool msgSelf, bool msgArea, string firstPersonMessage, string thirdPersonMessage)

commit 4195069ec8960b52468f2492488f20482a44d62e

Amend 0358f2d Caused pusling charm spells to fail. It's probably safer to ignore every pulsing spells for now.

GameServer/ECS-Components/EffectListComponent.cs
@@ -110,9 +110,14 @@ namespace DOL.GS
                                         // * Effectiveness changes from resurrection illness expiring.
                                         // * Champion debuffs being forced to spec debuffs in `OnStartEffect`.
                                         // * Movement speed debuffs effectiveness decreasing over time.
-                                        existingEffect.IsSilent = true;
-                                        existingEffect.OnStopEffect();
-                                        newSpellEffect.IsSilent = true;
+                                        // This doesn't work will pulsing charm spells, and it's probably safer to exclude every pulsing spell for now.
+                                        if (!newSpell.IsPulsing)
+                                        {
+                                            existingEffect.IsSilent = true;
+                                            existingEffect.OnStopEffect();
+                                            newSpellEffect.IsSilent = true;
+                                        }
+
                                         newSpellEffect.IsDisabled = existingEffect.IsDisabled;
                                         newSpellEffect.IsBuffActive = existingEffect.IsBuffActive;
                                         newSpellEffect.PreviousPosition = GetAllEffects().IndexOf(existingEffect);