OpenDAoC/OpenDAoC-Core

commit 72deac07fcbd749fa5fe9b660435dababce59c03

Remove `DataObject.PendingDatabaseAction` This was added because `IsPersisted` was unreliable with items. It turns out the reason why it's unreliable is because items are actually recreated after being loaded (see `GameInventoryItem.Create`), which discards `IsPersisted`. Despite that, `DataObject` also contains a cloning method, performing memberwise cloning, which would also clone `IsPersisted`. Both were modified to set `IsPersisted` to false.

GameServer/gameobjects/GameInventoryObject.cs
@@ -311,7 +311,6 @@ namespace DOL.GS
 
                     toItem.SlotPosition = toClientSlot - thisObject.FirstClientSlot + thisObject.FirstDbSlot;
                     toItem.OwnerID = thisObject.GetOwner(player);
-                    toItem.PendingDatabaseAction = PendingDatabaseAction.ADD;
                     thisObject.OnAddItem(player, toItem);
 
                     if (!SaveItem(toItem))
@@ -336,7 +335,6 @@ namespace DOL.GS
 
                     toItem.SlotPosition = toClientSlot - thisObject.FirstClientSlot + thisObject.FirstDbSlot;
                     toItem.OwnerID = thisObject.GetOwner(player);
-                    toItem.PendingDatabaseAction = PendingDatabaseAction.ADD;
                     thisObject.OnAddItem(player, toItem);
 
                     if (!SaveItem(toItem))
@@ -443,8 +441,6 @@ namespace DOL.GS
             {
                 if (fromItem.Count - count <= 0)
                 {
-                    fromItem.PendingDatabaseAction = PendingDatabaseAction.DELETE;
-
                     if (!SaveItem(fromItem))
                     {
                         SendErrorMessage(player, nameof(StackItemsFromCharacterInventoryToHousingInventory), fromClientSlot, toClientSlot, fromItem, toItem, 0);
@@ -479,8 +475,6 @@ namespace DOL.GS
             {
                 if (fromItem.Count - count <= 0)
                 {
-                    fromItem.PendingDatabaseAction = PendingDatabaseAction.DELETE;
-
                     if (!SaveItem(fromItem))
                     {
                         SendErrorMessage(player, nameof(StackItemsFromHousingInventoryToCharacterInventory), fromClientSlot, toClientSlot, fromItem, toItem, 0);
@@ -603,29 +597,14 @@ namespace DOL.GS
             }
         }
 
-        private static bool SaveItem(DbInventoryItem item)
+        public static bool SaveItem(DbInventoryItem item)
         {
-            if (item.PendingDatabaseAction is PendingDatabaseAction.ADD)
-            {
-                item.PendingDatabaseAction = PendingDatabaseAction.SAVE;
+            return item.IsPersisted ? GameServer.Database.SaveObject(item) : GameServer.Database.AddObject(item);
+        }
 
-                if (!GameServer.Database.AddObject(item))
-                    return false;
-            }
-            else if (item.PendingDatabaseAction is PendingDatabaseAction.DELETE)
-            {
-                item.PendingDatabaseAction = PendingDatabaseAction.SAVE;
-
-                if (!GameServer.Database.DeleteObject(item))
-                    return false;
-            }
-            else if (item.PendingDatabaseAction is PendingDatabaseAction.SAVE)
-            {
-                if (!GameServer.Database.SaveObject(item))
-                    return false;
-            }
-
-            return true;
+        public static bool DeleteItem(DbInventoryItem item)
+        {
+            return GameServer.Database.DeleteObject(item);
         }
 
         public static bool IsHousingInventorySlot(eInventorySlot slot)

CoreDatabase/DataObject.cs
@@ -61,7 +61,7 @@ namespace DOL.Database
 		/// Can this object added to the DB?
 		/// </summary>
 		[Browsable(false)]
-		public virtual bool AllowAdd 
+		public virtual bool AllowAdd
 		{
 			get { return m_allowAdd; }
 			set { m_allowAdd = value; }
@@ -117,6 +117,7 @@ namespace DOL.Database
 		public object Clone()
 		{
 			var obj = (DataObject) MemberwiseClone();
+			obj.IsPersisted = false;
 			obj.ObjectId = IdGenerator.GenerateID();
 			return obj;
 		}
@@ -127,14 +128,5 @@ namespace DOL.Database
 		{
 			return string.Format("DataObject: {0}, ObjectId{{{1}}}", TableName, ObjectId);
 		}
-	}
-
-	// Used to allow manipulation of inventory items and delay their database action.
-	public enum PendingDatabaseAction
-	{
-		NONE,
-		SAVE,
-		ADD,
-		DELETE
 	}
 }

CoreDatabase/Tables/DbInventoryItem.cs
@@ -18,24 +18,6 @@ namespace DOL.Database
 		public const string BLANK_ITEM = "blank_item";
 
 		protected bool m_hasLoggedError = false;
-		private PendingDatabaseAction _pendingDatabaseAction = PendingDatabaseAction.SAVE;
-
-		public PendingDatabaseAction PendingDatabaseAction
-		{
-			get => _pendingDatabaseAction;
-			set
-			{
-				// If an addition is cancelled by a deletion, no action will be necessary.
-				if (_pendingDatabaseAction is PendingDatabaseAction.ADD && value is PendingDatabaseAction.DELETE)
-				{
-					_pendingDatabaseAction = PendingDatabaseAction.NONE;
-					return;
-				}
-
-				_pendingDatabaseAction = value;
-				Dirty = true;
-			}
-		}
 
 		#region Inventory fields
 
@@ -333,7 +315,6 @@ namespace DOL.Database
 		{
 			m_utemplate_id = template.Id_nb;
 			InitializeFromTemplate(template);
-
 		}
 
 		private void InitializeFromTemplate(DbItemTemplate template)
@@ -357,30 +338,31 @@ namespace DOL.Database
 		/// Creates a new Inventoryitem based on the given InventoryItem
 		/// </summary>
 		/// <param name="inventoryItem"></param>
-		protected DbInventoryItem(DbInventoryItem template)
+		protected DbInventoryItem(DbInventoryItem dbInventoryItem)
 		{
-			Template = template.Template;
-			m_itemplate_id = template.ITemplate_Id;
-			m_utemplate_id = template.UTemplate_Id;
-			m_color = template.Color;
-			m_extension = template.Extension;
-			m_salvageextension = template.SalvageExtension;
-			m_slot_pos = template.SlotPosition;
-			m_count = template.Count;
-			m_creator = template.Creator;
-			m_iscrafted = template.IsCrafted;
-			m_sellPrice = template.SellPrice;
-			m_condition = template.Condition;
-			m_durability = template.Durability;
-			m_emblem = template.Emblem;
-			m_cooldown = template.Cooldown;
-			m_charges = template.Charges;
-			m_charges1 = template.Charges1;
-			m_poisonCharges = template.PoisonCharges ;
-			m_poisonMaxCharges = template.PoisonMaxCharges ;
-			m_poisonSpellID = template.PoisonSpellID;
-			m_experience = template.Experience;
-			m_ownerLot = template.OwnerLot;
+			Template = dbInventoryItem.Template;
+			IsPersisted = dbInventoryItem.IsPersisted;
+			m_itemplate_id = dbInventoryItem.ITemplate_Id;
+			m_utemplate_id = dbInventoryItem.UTemplate_Id;
+			m_color = dbInventoryItem.Color;
+			m_extension = dbInventoryItem.Extension;
+			m_salvageextension = dbInventoryItem.SalvageExtension;
+			m_slot_pos = dbInventoryItem.SlotPosition;
+			m_count = dbInventoryItem.Count;
+			m_creator = dbInventoryItem.Creator;
+			m_iscrafted = dbInventoryItem.IsCrafted;
+			m_sellPrice = dbInventoryItem.SellPrice;
+			m_condition = dbInventoryItem.Condition;
+			m_durability = dbInventoryItem.Durability;
+			m_emblem = dbInventoryItem.Emblem;
+			m_cooldown = dbInventoryItem.Cooldown;
+			m_charges = dbInventoryItem.Charges;
+			m_charges1 = dbInventoryItem.Charges1;
+			m_poisonCharges = dbInventoryItem.PoisonCharges ;
+			m_poisonMaxCharges = dbInventoryItem.PoisonMaxCharges ;
+			m_poisonSpellID = dbInventoryItem.PoisonSpellID;
+			m_experience = dbInventoryItem.Experience;
+			m_ownerLot = dbInventoryItem.OwnerLot;
 		}
 
 		public virtual void SetCooldown()

GameServer/gameutils/DeckOfCardsRandomizer/Deck.cs
@@ -88,7 +88,7 @@ namespace DOL.GS.Utils
 
             if (dbCoreCharacterXDeck?.IsPersisted == true)
                 GameServer.Database.SaveObject(dbCoreCharacterXDeck);
-            else 
+            else
                 GameServer.Database.AddObject(dbCoreCharacterXDeck);
 
             string SerializeCards()

GameServer/gameutils/GamePlayerInventory.cs
@@ -206,11 +206,7 @@ namespace DOL.GS
 				if (!canPersist)
 					return;
 
-				if (item.PendingDatabaseAction is PendingDatabaseAction.DELETE)
-				{
-					item.PendingDatabaseAction = PendingDatabaseAction.SAVE;
-					GameServer.Database.DeleteObject(item);
-				}
+				GameInventoryObjectExtensions.DeleteItem(item);
 			}
 
 			void SaveItem(KeyValuePair<eInventorySlot, DbInventoryItem> pair)
@@ -254,13 +250,7 @@ namespace DOL.GS
 					return;
 				}
 
-				if (item.PendingDatabaseAction is PendingDatabaseAction.ADD)
-				{
-					item.PendingDatabaseAction = PendingDatabaseAction.SAVE;
-					GameServer.Database.AddObject(item);
-				}
-				else if (item.PendingDatabaseAction is PendingDatabaseAction.SAVE)
-					GameServer.Database.SaveObject(item);
+				GameInventoryObjectExtensions.SaveItem(item);
 			}
 		}
 
@@ -294,9 +284,6 @@ namespace DOL.GS
 
 				if (canPersist)
 				{
-					// Clean up our items awaiting deletion list in case this is an item that was dropped then picked up again between two saves.
-					bool removedFromItemsAwaitingDeletion = false;
-
 					for (int i = _itemsAwaitingDeletion.Count - 1; i >= 0; i--)
 					{
 						DbInventoryItem _itemAwaitingDeletion = _itemsAwaitingDeletion[i];
@@ -304,12 +291,9 @@ namespace DOL.GS
 						if (_itemAwaitingDeletion == item)
 						{
 							_itemsAwaitingDeletion.SwapRemoveAt(i);
-							removedFromItemsAwaitingDeletion = true;
 							break;
 						}
 					}
-
-					item.PendingDatabaseAction = removedFromItemsAwaitingDeletion ? PendingDatabaseAction.SAVE : PendingDatabaseAction.ADD;
 				}
 			}
 
@@ -355,17 +339,9 @@ namespace DOL.GS
 				if (item is GameInventoryItem gameItem)
 					canPersist = gameItem.CanPersist;
 
-				if (canPersist)
-				{
-					// Only add the item to our items awaiting deletion list if it wasn't about to be added to the database.
-					if (item.PendingDatabaseAction == PendingDatabaseAction.ADD)
-						item.PendingDatabaseAction = PendingDatabaseAction.NONE;
-					else
-					{
-						item.PendingDatabaseAction = PendingDatabaseAction.DELETE;
-						_itemsAwaitingDeletion.Add(item);
-					}
-				}
+				// Only add the item to our items awaiting deletion list if it was actually added to the database.
+				if (canPersist && item.IsPersisted)
+					_itemsAwaitingDeletion.Add(item);
 			}
 
 			m_player.TradeWindow?.RemoveItemToTrade(item);
@@ -402,9 +378,6 @@ namespace DOL.GS
 
 				return false;
 			}
-
-			if (item.Count <= 0)
-				item.PendingDatabaseAction = PendingDatabaseAction.DELETE;
 
 			return base.RemoveCountFromStack(item, count);
 		}
@@ -1028,19 +1001,16 @@ namespace DOL.GS
 				return true;
 			}
 
-			if (toItem == null && fromItem.Count > itemCount)
-			{
-				DbInventoryItem newItem = fromItem.Clone() as DbInventoryItem;
-				m_items[toSlot] = newItem;
-				newItem.Count = itemCount;
-				newItem.SlotPosition = (int) toSlot;
-				fromItem.Count -= itemCount;
-				newItem.AllowAdd = fromItem.Template.AllowAdd;
-				newItem.PendingDatabaseAction = PendingDatabaseAction.ADD;
-				return true;
-			}
+			if (toItem != null || fromItem.Count <= itemCount)
+				return false;
 
-			return false;
+			DbInventoryItem newItem = fromItem.Clone() as DbInventoryItem;
+			m_items[toSlot] = newItem;
+			newItem.Count = itemCount;
+			newItem.SlotPosition = (int) toSlot;
+			fromItem.Count -= itemCount;
+			newItem.AllowAdd = fromItem.Template.AllowAdd;
+			return true;
 		}
 
 		/// <summary>

commit b842203ab38cb9b765211cf9ee2beb01b6ecf939

Fix stacking item duplication caused by 72deac0

GameServer/gameobjects/GameInventoryObject.cs
@@ -444,6 +444,8 @@ namespace DOL.GS
             {
                 if (fromItem.Count - count <= 0)
                 {
+                    fromItem.Count = 0;
+
                     if (!SaveItem(fromItem))
                     {
                         SendErrorMessage(player, nameof(StackItemsFromCharacterInventoryToHousingInventory), fromClientSlot, toClientSlot, fromItem, toItem, 0);
@@ -478,6 +480,8 @@ namespace DOL.GS
             {
                 if (fromItem.Count - count <= 0)
                 {
+                    fromItem.Count -= count;
+
                     if (!SaveItem(fromItem))
                     {
                         SendErrorMessage(player, nameof(StackItemsFromHousingInventoryToCharacterInventory), fromClientSlot, toClientSlot, fromItem, toItem, 0);
@@ -602,6 +606,9 @@ namespace DOL.GS
 
         public static bool SaveItem(DbInventoryItem item)
         {
+            if (item.IsStackable && item.Count == 0)
+                return DeleteItem(item);
+
             bool result = item.IsPersisted ? GameServer.Database.SaveObject(item) : GameServer.Database.AddObject(item);
 
             if (!result)