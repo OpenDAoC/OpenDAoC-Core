7063e2a53cf969aa835c1ba9b4df2e609ecf1126
Baptiste Marie <bm01@users.noreply.github.com>
2025-02-16 13:27:11 +0100

    Remove debug message

--- GameServer/realmabilities_atlasOF/handlers/AtlasOF_FirstAid.cs
+++ GameServer/realmabilities_atlasOF/handlers/AtlasOF_FirstAid.cs
@@ -40,9 +40,6 @@ namespace DOL.GS.RealmAbilities
 			GamePlayer player = living as GamePlayer;
 			var scaleLevel = (double)player.Level / 50;
 			healAmount = (int)(currentLevelAbility * 300 * scaleLevel);
-
-			log.InfoFormat("healAmount is {0}", healAmount);
-
 			int healed = living.ChangeHealth(living, eHealthChangeType.Spell, healAmount);
 
 			SendCasterSpellEffectAndCastMessage(living, 7001, healed > 0);
