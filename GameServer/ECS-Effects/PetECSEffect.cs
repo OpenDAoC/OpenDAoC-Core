6e8ce69ab8153fcb28d724c8379c8a0b3b7d252f
Baptiste Marie <bm01@users.noreply.github.com>
2025-02-17 06:35:21 +0100

    Update `PetECSGameEffect` to call `Die` Instead of setting HP to 0 then calll `Delete`. Fixes concentration buffs not being removed from released pets.

--- GameServer/ECS-Effects/PetECSEffect.cs
+++ GameServer/ECS-Effects/PetECSEffect.cs
@@ -10,8 +10,7 @@ namespace DOL.GS
         {
             SpellHandler.Caster.UpdatePetCount(false);
             (SpellHandler as SummonSpellHandler)?.OnPetReleased(Owner as GameSummonedPet); // Should be done before setting health to 0.
-            Owner.Health = 0; // To send proper remove packet.
-            Owner.Delete();
+            Owner.Die(null);
         }
     }
 }
