3bde99fd88ca0179e10226128e4aaa4303a69d77
Baptiste Marie <bm01@users.noreply.github.com>
2025-02-22 20:23:57 +0100

    Update /version to show build time * Assembly version is unused since wildcards mess up with Hot Reload. * Removed every AssemblyInfo.cs (now auto generated, only way I found to make `AssemblyMetadata` work dynamically)

--- GameServer/scripts/commands/version.cs
+++ GameServer/scripts/commands/version.cs
@@ -1,37 +1,53 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
+using System;
+using System.Globalization;
 using System.Reflection;
+using System.Threading;
 using DOL.GS.PacketHandler;
 
 namespace DOL.GS.Commands
 {
-	[CmdAttribute(
-		"&version",
-		ePrivLevel.Player,
-		"Get the version of the GameServer",
-		"/version")]
-	public class VersionCommandHandler : AbstractCommandHandler, ICommandHandler
-	{
-		public void OnCommand(GameClient client, string[] args)
-		{
-			AssemblyName an = Assembly.GetAssembly(typeof(GameServer)).GetName();
-			client.Out.SendMessage("Dawn of Light " + an.Name + " Version: " + an.Version, eChatType.CT_System, eChatLoc.CL_SystemWindow);
-		}
-	}
+    [Cmd(
+        "&version",
+        ePrivLevel.Player,
+        "Get the version of the game server",
+        "/version")]
+    public class VersionCommandHandler : AbstractCommandHandler, ICommandHandler
+    {
+        private static DateTime? _buildTime;
+        private static Lock _lock = new();
+
+        public void OnCommand(GameClient client, string[] args)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            if (!_buildTime.HasValue)
+            {
+                lock (_lock)
+                {
+                    if (!_buildTime.HasValue)
+                        _buildTime = GetBuildDate(assembly);
+                }
+            }
+
+            client.Out.SendMessage($"{assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title}: {_buildTime.Value}", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+
+            static DateTime GetBuildDate(Assembly assembly)
+            {
+                string buildTime = string.Empty;
+
+                foreach (AssemblyMetadataAttribute attribute in assembly.GetCustomAttributes<AssemblyMetadataAttribute>())
+                {
+                    if (!attribute.Key.Equals("BuildTime"))
+                        continue;
+
+                    buildTime = attribute.Value;
+                }
+
+                if (string.IsNullOrEmpty(buildTime))
+                    return default;
+
+                return DateTime.TryParse(buildTime, out DateTime result) ? result : default;
+            }
+        }
+    }
 }
