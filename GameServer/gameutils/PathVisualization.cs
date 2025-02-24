f2647f9e8f75b6afbd0c2695f4b70861d90ceae1
Baptiste Marie <bm01@users.noreply.github.com>
2025-02-24 15:23:44 +0100

    Introduce `PathVisualization`

---
+++ GameServer/gameutils/PathVisualization.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+
+namespace DOL.GS.Movement
+{
+    public class PathVisualization
+    {
+        private const int AUTOMATIC_CLEANUP_INTERVAL = 600000;
+
+        private List<GameNPC> _markers;
+        private ECSGameTimer _cleanupTimer;
+
+        public PathVisualization() { }
+
+        public void Visualize(Queue<WrappedPathPoint> pathPoints, Region region)
+        {
+            if (pathPoints == null)
+                return;
+
+            Prepare();
+
+            foreach (WrappedPathPoint point in pathPoints)
+                _markers.Add(CreateMarker((int) point.Position.X, (int) point.Position.Y, (int) point.Position.Z, region, GetModel(point.Flags)));
+
+            StartCleanupTimer(AUTOMATIC_CLEANUP_INTERVAL);
+
+            static EMarkerModel GetModel(dtPolyFlags flags)
+            {
+                if ((flags & dtPolyFlags.SWIM) != 0)
+                    return EMarkerModel.Blue;
+
+                if ((flags & dtPolyFlags.DOOR) != 0)
+                    return EMarkerModel.Red;
+
+                if ((flags & dtPolyFlags.WALK) != 0)
+                    return EMarkerModel.Yellow;
+
+                return EMarkerModel.Green;
+            }
+        }
+
+        public void Visualize(PathPoint pathPoint, Region region)
+        {
+            if (pathPoint == null)
+                return;
+
+            Prepare();
+
+            do
+            {
+                _markers.Add(CreateMarker(pathPoint.X, pathPoint.Y, pathPoint.Z, region, EMarkerModel.Yellow));
+                pathPoint = pathPoint.Next;
+            } while (pathPoint != null);
+
+            StartCleanupTimer(AUTOMATIC_CLEANUP_INTERVAL);
+        }
+
+        public void StartCleanupTimer(int interval)
+        {
+            if (_cleanupTimer != null)
+            {
+                _cleanupTimer.Start(interval);
+                return;
+            }
+
+            _cleanupTimer = new ECSGameTimer(null, (timer) =>
+            {
+                CleanUp();
+                return 0;
+            }, interval);
+        }
+
+        public void CleanUp()
+        {
+            if (_markers == null)
+                return;
+
+            foreach (GameNPC marker in _markers)
+                marker.RemoveFromWorld();
+
+            _markers.Clear();
+        }
+
+        private static GameNPC CreateMarker(int x, int y, int z, Region region, EMarkerModel model)
+        {
+            GameNPC npc = new()
+            {
+                X = x,
+                Y = y,
+                Z = z,
+                CurrentRegion = region,
+                Level = 1,
+                Model = (ushort) model,
+                Size = 35
+            };
+
+            npc.Flags |= GameNPC.eFlags.PEACE | GameNPC.eFlags.FLYING;
+            npc.AddToWorld();
+            return npc;
+        }
+
+        private void Prepare()
+        {
+            if (_markers == null)
+                _markers = new();
+            else
+                CleanUp();
+        }
+
+        private enum EMarkerModel : ushort
+        {
+            Red = 408,
+            Yellow = 409,
+            Blue = 410,
+            Green = 411
+        }
+    }
+}
