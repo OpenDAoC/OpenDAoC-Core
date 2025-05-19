9027b8574e51c50f429cc9fb2b6b5b2fbe2c9e9e
Baptiste Marie <bm01@users.noreply.github.com>
2025-05-19 20:41:16 +0200

    Fix attempt at renaming folder duplicating it

--- GameServer/managers/ServiceObject/IServiceObject.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DOL.GS
-{
-    // Interface to be implemented by classes that are to be managed by the entity manager.
-    public interface IServiceObject
-    {
-        public ServiceObjectId ServiceObjectId { get; set; }
-    }
-}
