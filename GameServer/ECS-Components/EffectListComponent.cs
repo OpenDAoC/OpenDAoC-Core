OpenDAoC/OpenDAoC-Core

commit 8443609c89cabbc4ffb3b27f392f5c63cb2f930c

Make `EffectListService` tick `EffectListComponent` again Making `EffectListComponent` be ticked by `ClientService` and `NpcService` sounded like a good idea to reduce the amount of work performed by the game loop thread, but this is no longer the case since the recent improvements to `ServiceObjectStore`.

GameServer/ECS-Components/EffectListComponent.cs
@@ -7,11 +7,10 @@ using System.Threading;
 using DOL.AI.Brain;
 using DOL.GS.PacketHandler;
 using DOL.GS.Spells;
-using ECS.Debug;
 
 namespace DOL.GS
 {
-    public class EffectListComponent
+    public class EffectListComponent : IServiceObject
     {
         private static readonly Logging.Logger log = Logging.LoggerManager.Create(MethodBase.GetCurrentMethod().DeclaringType);
 
@@ -35,6 +34,7 @@ namespace DOL.GS
 
         public GameLiving Owner { get; }
         public int UsedConcentration => Volatile.Read(ref _usedConcentration);
+        public ServiceObjectId ServiceObjectId { get; set; } = new(ServiceObjectType.EffectListComponent);
 
         public EffectListComponent(GameLiving owner)
         {
@@ -43,8 +43,6 @@ namespace DOL.GS
 
         public void Tick()
         {
-            long startTick = GameLoop.GetCurrentTime();
-
             if (Volatile.Read(ref _pendingEffectCount) > 0)
             {
                 Interlocked.Exchange(ref _pendingEffectCount, 0);
@@ -68,18 +66,17 @@ namespace DOL.GS
                     }
                 }
             }
+            else
+                ServiceObjectStore.Remove(this);
 
             SendPlayerUpdates();
-            long stopTick = GameLoop.GetCurrentTime();
-
-            if (stopTick - startTick > Diagnostics.LongTickThreshold)
-                log.Warn($"Long {nameof(EffectListComponent)}.{nameof(Tick)} for {Owner.Name}({Owner.ObjectID}) Time: {stopTick - startTick}ms");
         }
 
         public void AddPendingEffect(ECSGameEffect effect)
         {
             Interlocked.Increment(ref _pendingEffectCount);
             _pendingEffects.Enqueue(effect);
+            ServiceObjectStore.Add(this);
         }
 
         public void StopConcentrationEffect(int index, bool playerCancelled)
@@ -268,6 +265,7 @@ namespace DOL.GS
             lock (_playerUpdatesLock)
             {
                 _requestedPlayerUpdates |= playerUpdate;
+                ServiceObjectStore.Add(this);
             }
         }
 

GameServer/ECS-Services/ClientService.cs
@@ -94,7 +94,6 @@ namespace DOL.GS
                         if (ServiceUtils.ShouldTick(player.LastWorldUpdate + Properties.WORLD_PLAYER_UPDATE_INTERVAL))
                             UpdateWorld(player);
 
-                        player.effectListComponent.Tick();
                         player.movementComponent.Tick();
                         break;
                     }

GameServer/ECS-Services/EffectListService.cs
@@ -1,10 +1,55 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using ECS.Debug;
+using System.Reflection;
+using System.Threading;
 
 namespace DOL.GS
 {
     public static class EffectListService
     {
+        private static readonly Logging.Logger log = Logging.LoggerManager.Create(MethodBase.GetCurrentMethod().DeclaringType);
+        private const string SERVICE_NAME = nameof(EffectListService);
+        private static List<EffectListComponent> _list;
+        private static int _entityCount;
+
+        public static void Tick()
+        {
+            GameLoop.CurrentServiceTick = SERVICE_NAME;
+            Diagnostics.StartPerfCounter(SERVICE_NAME);
+            _list = ServiceObjectStore.UpdateAndGetAll<EffectListComponent>(ServiceObjectType.EffectListComponent, out int lastValidIndex);
+            GameLoop.ExecuteWork(lastValidIndex + 1, TickInternal);
+
+            if (Diagnostics.CheckEntityCounts)
+                Diagnostics.PrintEntityCount(SERVICE_NAME, ref _entityCount, _list.Count);
+
+            Diagnostics.StopPerfCounter(SERVICE_NAME);
+        }
+
+        private static void TickInternal(int index)
+        {
+            EffectListComponent effectListComponent = null;
+
+            try
+            {
+                if (Diagnostics.CheckEntityCounts)
+                    Interlocked.Increment(ref _entityCount);
+
+                effectListComponent = _list[index];
+                long startTick = GameLoop.GetCurrentTime();
+                effectListComponent.Tick();
+                long stopTick = GameLoop.GetCurrentTime();
+
+                if (stopTick - startTick > Diagnostics.LongTickThreshold)
+                    log.Warn($"Long {SERVICE_NAME}.{nameof(Tick)} for: {effectListComponent.Owner.Name}({effectListComponent.Owner.ObjectID}) Time: {stopTick - startTick}ms");
+            }
+            catch (Exception e)
+            {
+                ServiceUtils.HandleServiceException(e, SERVICE_NAME, effectListComponent, effectListComponent.Owner);
+            }
+        }
+
         public static ECSGameEffect GetEffectOnTarget(GameLiving target, eEffect effectType, eSpellType spellType = eSpellType.None)
         {
             if (spellType is eSpellType.None)

GameServer/ECS-Services/NpcService.cs
@@ -57,7 +57,6 @@ namespace DOL.GS
                     brain.NextThinkTick += brain.ThinkInterval;
                 }
 
-                npc.effectListComponent.Tick();
                 npc.movementComponent.Tick();
             }
             catch (Exception e)

GameServer/Managers/GameLoop/GameLoop.cs
@@ -146,6 +146,7 @@ namespace DOL.GS
                 NpcService.Tick();
                 AttackService.Tick();
                 CastingService.Tick();
+                EffectListService.Tick();
                 ZoneService.Tick();
                 CraftingService.Tick();
                 ReaperService.Tick();

GameServer/Managers/ServiceObject/ServiceObjectStore.cs
@@ -16,6 +16,7 @@ namespace DOL.GS
             { ServiceObjectType.Brain, new ServiceObjectArray<ABrain>(ServerProperties.Properties.MAX_ENTITIES) },
             { ServiceObjectType.AttackComponent, new ServiceObjectArray<AttackComponent>(1250) },
             { ServiceObjectType.CastingComponent, new ServiceObjectArray<CastingComponent>(1250) },
+            { ServiceObjectType.EffectListComponent, new ServiceObjectArray<EffectListComponent>(1250) },
             { ServiceObjectType.CraftComponent, new ServiceObjectArray<CraftComponent>(100) },
             { ServiceObjectType.ObjectChangingSubZone, new ServiceObjectArray<ObjectChangingSubZone>(ServerProperties.Properties.MAX_ENTITIES) },
             { ServiceObjectType.LivingBeingKilled, new ServiceObjectArray<LivingBeingKilled>(200) },

GameServer/Managers/ServiceObject/ServiceObjectType.cs
@@ -6,6 +6,7 @@
         Brain,
         AttackComponent,
         CastingComponent,
+        EffectListComponent,
         CraftComponent,
         ObjectChangingSubZone,
         LivingBeingKilled,

commit 647471ffee86e95ce3a51c56f92eeed3e5e1003b

Fix regression caused by 8443609c89cabbc4ffb3b27f392f5c63cb2f930c

GameServer/ECS-Components/EffectListComponent.cs
@@ -53,7 +53,7 @@ namespace DOL.GS
 
             if (_effects.Count != 0)
             {
-                if (!Owner.IsAlive)
+                if (Owner.Health <= 0) // Don't check `IsAlive`, since it returns false during region change.
                     CancelAll();
                 else
                 {