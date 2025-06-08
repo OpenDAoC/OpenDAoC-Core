70502222a721982db23d8009488d389a08744a73
Baptiste Marie <bm01@users.noreply.github.com>
2025-06-08 19:47:04 +0200

    Fix /bonuses 2b9a835372ad5923e29d84eae44fc0c207759bce wasn't enough

--- GameServer/gameutils/GamePlayerUtils.cs
+++ GameServer/gameutils/GamePlayerUtils.cs
@@ -249,10 +249,10 @@ namespace DOL.GS
 			info.Add(LanguageMgr.GetTranslation(player.Client.Account.Language, "PlayerBonusesListRequestHandler.HandlePacket.Special"));
 
 			//This is an Array of the bonuses that show up in the Bonus Snapshot on Live, the only ones that really need to be there.
-			int[] bonusToBeDisplayed = new int[37] { 9, 10, 150, 151, 153, 154, 155, 173, 174, 179, 180, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 210, 247, 248, 251, 252, 253, 254};
+			int[] bonusToBeDisplayed = [9, 10, 150, 151, 153, 154, 155, 173, 174, 179, 180, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 210, 247, 248, 251, 252, 253, 254];
 			foreach (eProperty property in Enum.GetValues<eProperty>())
 			{
-				if ((player.ItemBonus[property] > 0) && ((Array.BinarySearch(bonusToBeDisplayed, property)) >= 0)) //Tiny edit here to add the binary serach to weed out the non essential bonuses
+				if ((player.ItemBonus[property] > 0) && (Array.BinarySearch(bonusToBeDisplayed, (int) property) >= 0)) //Tiny edit here to add the binary serach to weed out the non essential bonuses
 				{
 					if (player.ItemBonus[property] != 0)
 					{
