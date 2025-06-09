45fe6216b646d4d0ab446a6d8de1ffca92b4753f
Baptiste Marie <bm01@users.noreply.github.com>
2025-06-09 05:59:33 +0200

    Revert fd40407f92c82b29fd7e1355e74af5b46796c1aa Mix of bad memory and people getting confused by the fact that interrupt timers on freeshards work differently compared to live, where timers actively interrupted and didn't simply prevent immediate spellcast or bow draw attempts. There should be an interrupt timer, but it may be shorter than 3 seconds when getting hit by spells according to some videos, hard to say, requires more research. In the meantime, the timer should be re-added.

--- GameServer/ECS-Components/AttackComponent.cs
+++ GameServer/ECS-Components/AttackComponent.cs
@@ -776,7 +776,12 @@ namespace DOL.GS
                 owner.CancelEngageEffect();
 
             if (owner.ActiveWeaponSlot is eActiveWeaponSlot.Distance)
+            {
+                if (owner.rangeAttackComponent.RangedAttackState is not eRangedAttackState.Aim && attackAction.CheckInterruptTimer())
+                    return false;
+
                 owner.rangeAttackComponent.AttackStartTime = GameLoop.GameLoopTime;
+            }
 
             AttackState = true;
             return true;
