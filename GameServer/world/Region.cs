OpenDAoC/OpenDAoC-Core

commit c49568c058ef7f74a786b4652da16510ca3a3e5c

Optimize `GetObjectsInRadius`: Avoid redundant distance checks for entities in the reference subzone when fully enclosed by radius
Previously, only non-reference subzones fully within the radius skipped per-entity distance checks. Now, the reference subzone also benefits from this optimization.

GameServer/world/Zone.cs
@@ -334,7 +334,6 @@ namespace DOL.GS
         public void GetObjectsInRadius<T>(int x, int y, int z, eGameObjectType objectType, ushort radius, List<T> listToAppendTo) where T : GameObject
         {
             uint sqRadius = (uint) radius * radius;
-            int referenceSubZoneIndex = GetSubZoneIndex(x, y);
 
             int xInZone = x - XOffset; // x in zone coordinates.
             int yInZone = y - YOffset; // y in zone coordinates.
@@ -373,8 +372,6 @@ namespace DOL.GS
                     if (subZone[objectType].Count == 0)
                         continue;
 
-                    if (subZoneIndex != referenceSubZoneIndex)
-                    {
                         int xLeft = column << SUBZONE_SHIFT;
                         int xRight = xLeft + SUBZONE_SIZE;
                         int yTop = line << SUBZONE_SHIFT;
@@ -386,16 +383,13 @@ namespace DOL.GS
 
                         // If the subzone being checked is fully enclosed within the radius and we don't care about Z, add all objects without checking the distance.
                         ignoreDistance = CheckSubZoneMaxDistance(xInZone, yInZone, xLeft, xRight, yTop, yBottom, sqRadius);
-                    }
-                    else
-                        ignoreDistance = false;
 
                     foreach (LinkedListNode<GameObject> node in subZone[objectType])
                     {
                         GameObject gameObject = node.Value;
 
                         // Inactive or deleted objects can't remove themselves.
-                        if (gameObject.ObjectState != GameObject.eObjectState.Active || gameObject.CurrentRegion != ZoneRegion)
+                        if (gameObject.ObjectState is not GameObject.eObjectState.Active || gameObject.CurrentRegion != ZoneRegion)
                         {
                             SubZoneObject subZoneObject = gameObject.SubZoneObject;
 

commit 28757d2345f234b707fbd9e14358df5f91175abf

Fix regression caused by c49568c058ef7f74a786b4652da16510ca3a3e5c `CheckSubZoneMinDistance` doesn't work when referenceSubZoneIndex == subZoneIndex.

GameServer/world/Region.cs
@@ -1438,13 +1438,13 @@ namespace DOL.GS
             if (startingZone == null)
                 return;
 
-            startingZone.GetObjectsInRadius<T>(point, objectType, radius, list);
+            startingZone.GetObjectsInRadius(point, objectType, radius, list);
             uint sqRadius = (uint) radius * radius;
 
             foreach (Zone currentZone in m_zones)
             {
                 if (currentZone != startingZone && currentZone.ObjectCount > 0 && CheckShortestDistance(currentZone, point.X, point.Y, sqRadius))
-                    currentZone.GetObjectsInRadius<T>(point, objectType, radius, list);
+                    currentZone.GetObjectsInRadius(point, objectType, radius, list);
             }
         }
 

GameServer/world/Zone.cs
@@ -323,7 +323,7 @@ namespace DOL.GS
         /// <param name="listToAppendTo">a non-null list</param>
         public void GetObjectsInRadius<T>(Point3D point, eGameObjectType objectType, ushort radius, List<T> listToAppendTo) where T : GameObject
         {
-            GetObjectsInRadius<T>(point.X, point.Y, point.Z, objectType, radius, listToAppendTo);
+            GetObjectsInRadius(point.X, point.Y, point.Z, objectType, radius, listToAppendTo);
         }
 
         /// <summary>
@@ -358,6 +358,8 @@ namespace DOL.GS
             if (maxLine > (SUBZONE_NBR_ON_ZONE_SIDE - 1))
                 maxLine = SUBZONE_NBR_ON_ZONE_SIDE - 1;
 
+            int referenceSubZoneIndex = GetSubZoneIndex(x, y);
+
             int subZoneIndex;
             SubZone subZone;
             bool ignoreDistance;
@@ -378,7 +380,7 @@ namespace DOL.GS
                     int yBottom = yTop + SUBZONE_SIZE;
 
                     // Filter out subzones that are too far away.
-                    if (!CheckSubZoneMinDistance(xInZone, yInZone, xLeft, xRight, yTop, yBottom, sqRadius))
+                    if (referenceSubZoneIndex != subZoneIndex && !CheckSubZoneMinDistance(xInZone, yInZone, xLeft, xRight, yTop, yBottom, sqRadius))
                         continue;
 
                     // If the subzone being checked is fully enclosed within the radius and we don't care about Z, add all objects without checking the distance.