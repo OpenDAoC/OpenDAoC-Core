OpenDAoC/OpenDAoC-Core

commit 61e5796d1480b1375a432e9c05900515789224cf

Switch game loop to fixed timestep * Replaced real-time `GameLoopTime` with fixed timestep that always increments by `TickRate` (unless it's ticking too fast, which should never happen). This eliminates event synchronization issues where delayed events would cluster together, resulting in inconsistent server tick rate, and preserves relative timing between game events during server lag spikes. * Updated `PlayerMovementMonitor` to use `GetCurrentTime` for movement validation since player input should remain responsive to real-time. * Updated `ServiceUtil` to only leave `ShouldTick`, as other methods are not needed with a fixed timestep. * Renamed `GetCurrentTime` to `GetRealTime`.

GameServer/ECS-Services/ClientService.cs
@@ -17,7 +17,6 @@ namespace DOL.GS
         private const string SERVICE_NAME = nameof(ClientService);
         private const string SERVICE_NAME_BEGIN = $"{SERVICE_NAME}_Begin";
         private const string SERVICE_NAME_END = $"{SERVICE_NAME}_End";
-        private const int PING_TIMEOUT = 60000;
         private const int HARD_TIMEOUT = 150000;
         private const int STATIC_OBJECT_UPDATE_MIN_DISTANCE = 4000;
 
@@ -74,27 +73,30 @@ namespace DOL.GS
                     case GameClient.eClientState.WorldEnter:
                     {
                         Receive(client);
-                        CheckHardTimeout(client);
+                        CheckPingTimeout(client);
                         break;
                     }
                     case GameClient.eClientState.Playing:
                     {
                         Receive(client);
-                        CheckHardTimeout(client);
+                        CheckPingTimeout(client);
 
                         GamePlayer player = client.Player;
 
                         if (player == null)
                             break;
 
-                        CheckInGameTimeout(client);
+                        CheckInGameActivityTimeout(client);
 
                         // The client state might have been modified by an inbound packet.
                         if (client.ClientState is not GameClient.eClientState.Playing || player.ObjectState is not GameObject.eObjectState.Active)
                             break;
 
                         if (ServiceUtils.ShouldTick(player.LastWorldUpdate + Properties.WORLD_PLAYER_UPDATE_INTERVAL))
+                        {
                             UpdateWorld(player);
+                            player.LastWorldUpdate = GameLoop.GameLoopTime + Properties.WORLD_PLAYER_UPDATE_INTERVAL;
+                        }
 
                         break;
                     }
@@ -141,9 +143,9 @@ namespace DOL.GS
 
         private static void Receive(GameClient client)
         {
-            long startTick = GameLoop.GetCurrentTime();
+            long startTick = GameLoop.GetRealTime();
             client.Receive();
-            long stopTick = GameLoop.GetCurrentTime();
+            long stopTick = GameLoop.GetRealTime();
 
             if (stopTick - startTick > Diagnostics.LongTickThreshold)
                 log.Warn($"Long {SERVICE_NAME_BEGIN}.{nameof(Receive)} for {client.Account?.Name}({client.SessionID}) Time: {stopTick - startTick}ms");
@@ -151,9 +153,9 @@ namespace DOL.GS
 
         private static void Send(GameClient client)
         {
-            long startTick = GameLoop.GetCurrentTime();
+            long startTick = GameLoop.GetRealTime();
             client.PacketProcessor.SendPendingPackets();
-            long stopTick = GameLoop.GetCurrentTime();
+            long stopTick = GameLoop.GetRealTime();
 
             if (stopTick - startTick > Diagnostics.LongTickThreshold)
                 log.Warn($"Long {SERVICE_NAME_END}.{nameof(Send)} for {client.Account.Name}({client.SessionID}) Time: {stopTick - startTick}ms");
@@ -703,9 +705,9 @@ namespace DOL.GS
                 CreateObjectForPlayer(player, gameObject);
         }
 
-        private static void CheckHardTimeout(GameClient client)
+        private static void CheckPingTimeout(GameClient client)
         {
-            if (ServiceUtils.ShouldTickNoEarly(client.PingTime + HARD_TIMEOUT))
+            if (ServiceUtils.ShouldTick(client.PingTime + HARD_TIMEOUT))
             {
                 if (log.IsWarnEnabled)
                     log.Warn($"Hard timeout on client. Disconnecting. ({client})");
@@ -714,10 +716,10 @@ namespace DOL.GS
             }
         }
 
-        private static void CheckInGameTimeout(GameClient client)
+        private static void CheckInGameActivityTimeout(GameClient client)
         {
             if (Properties.KICK_IDLE_PLAYER_STATUS &&
-                ServiceUtils.ShouldTickNoEarly(client.Player.LastPlayerActivityTime + Properties.KICK_IDLE_PLAYER_TIME * 60000) &&
+                ServiceUtils.ShouldTick(client.Player.LastPlayerActivityTime + Properties.KICK_IDLE_PLAYER_TIME * 60000) &&
                 client.Account.PrivLevel == 1)
             {
                 if (log.IsInfoEnabled)
@@ -730,13 +732,12 @@ namespace DOL.GS
         private static void UpdateWorld(GamePlayer player)
         {
             // Players aren't updated here on purpose.
-            long startTick = GameLoop.GetCurrentTime();
+            long startTick = GameLoop.GetRealTime();
             UpdateNpcs(player);
             UpdateItems(player);
             UpdateDoors(player);
             UpdateHouses(player);
-            player.LastWorldUpdate = GameLoop.GameLoopTime;
-            long stopTick = GameLoop.GetCurrentTime();
+            long stopTick = GameLoop.GetRealTime();
 
             if (stopTick - startTick > Diagnostics.LongTickThreshold)
                 log.Warn($"Long {SERVICE_NAME_BEGIN}.{nameof(UpdateWorld)} for {player.Name}({player.ObjectID}) Time: {stopTick - startTick}ms");

GameServer/gameobjects/GamePlayer.cs
@@ -8861,18 +8861,11 @@ namespace DOL.GS
             set
             {
                 base.Heading = value;
-                if (DBCharacter != null) DBCharacter.Direction = value;
 
-                if (attackComponent.AttackState && ActiveWeaponSlot != eActiveWeaponSlot.Distance)
-                {
-                    AttackData ad = attackComponent.attackAction.LastAttackData;
+                if (DBCharacter != null)
+                    DBCharacter.Direction = value;
 
-                    if (ad != null && ad.IsMeleeAttack && (ad.AttackResult == eAttackResult.TargetNotVisible || ad.AttackResult == eAttackResult.OutOfRange))
-                    {
-                        if (ad.Target != null && IsObjectInFront(ad.Target, 120) && IsWithinRadius(ad.Target, attackComponent.AttackRange))
-                            attackComponent.attackAction.OnEnterMeleeRange();
-                    }
-                }
+                attackComponent.attackAction.OnHeadingUpdate();
             }
         }
 
@@ -9245,7 +9238,7 @@ namespace DOL.GS
                     if (ad != null && ad.IsMeleeAttack && (ad.AttackResult == eAttackResult.TargetNotVisible || ad.AttackResult == eAttackResult.OutOfRange))
                     {
                         if (ad.Target != null && IsObjectInFront(ad.Target, 120) && IsWithinRadius(ad.Target, attackComponent.AttackRange))
-                            attackComponent.attackAction.OnEnterMeleeRange();
+                            attackComponent.attackAction.OnHeadingUpdate();
                     }
                 }
             }

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -21,6 +21,7 @@ namespace DOL.GS
         private GameLiving _owner;
         private long _nextMeleeTick;
         private long _nextRangedTick;
+        private bool _firstTick = true;
 
         // Set to current time when a round doesn't result in an attack. Used to prevent combat log spam and kept until reset in AttackComponent.SendAttackingCombatMessages().
         public long RoundWithNoAttackTime { get; set; }
@@ -32,8 +33,6 @@ namespace DOL.GS
         protected AttackAction(GameLiving owner)
         {
             _owner = owner;
-            _nextMeleeTick = GameLoop.GameLoopTime;
-            _nextRangedTick = GameLoop.GameLoopTime;
         }
 
         public static AttackAction Create(GameLiving living)
@@ -48,6 +47,13 @@ namespace DOL.GS
 
         public bool Tick()
         {
+            if (_firstTick)
+            {
+                _nextMeleeTick = Math.Max(_nextMeleeTick, GameLoop.GameLoopTime);
+                _nextRangedTick = Math.Max(_nextRangedTick, GameLoop.GameLoopTime);
+                _firstTick = false;
+            }
+
             if (!ShouldTick())
                 return true;
 
@@ -103,17 +109,35 @@ namespace DOL.GS
                 _nextRangedTick += _interval;
         }
 
-        public void OnRangedAttackStop()
+        public void OnStopAttack()
         {
+            if (_owner.ActiveWeaponSlot is not eActiveWeaponSlot.Distance)
+                return;
+
+            RangeAttackComponent rangeAttackComponent = _owner.rangeAttackComponent;
+            rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
+
+            if (rangeAttackComponent.RangedAttackState is eRangedAttackState.None)
+                return;
+
             if (GameLoop.GameLoopTime - _nextMeleeTick > MINIMUM_MELEE_DELAY_AFTER_RANGED_ATTACK)
                 _nextMeleeTick = GameLoop.GameLoopTime + MINIMUM_MELEE_DELAY_AFTER_RANGED_ATTACK;
 
-            _nextRangedTick = GameLoop.GameLoopTime;
+            rangeAttackComponent.RangedAttackState = eRangedAttackState.None;
         }
 
-        public void OnEnterMeleeRange()
+        public void OnHeadingUpdate()
         {
-            _nextMeleeTick = GameLoop.GameLoopTime;
+            if (!AttackComponent.AttackState || _owner.ActiveWeaponSlot is eActiveWeaponSlot.Distance)
+                return;
+
+            AttackData ad = LastAttackData;
+
+            if (ad == null || !ad.IsMeleeAttack || (ad.AttackResult is not eAttackResult.TargetNotVisible and not eAttackResult.OutOfRange))
+                return;
+
+            if (ad.Target != null && _owner.IsObjectInFront(ad.Target, 120) && _owner.IsWithinRadius(ad.Target, AttackComponent.AttackRange))
+                _firstTick = true;
         }
 
         public virtual bool CheckInterruptTimer()
@@ -138,8 +162,8 @@ namespace DOL.GS
         private bool ShouldTick()
         {
             return _owner.ActiveWeaponSlot != eActiveWeaponSlot.Distance
-                ? ServiceUtils.ShouldTickAdjust(ref _nextMeleeTick)
-                : ServiceUtils.ShouldTickAdjust(ref _nextRangedTick);
+                ? ServiceUtils.ShouldTick(_nextMeleeTick)
+                : ServiceUtils.ShouldTick(_nextRangedTick);
         }
 
         protected virtual bool CanPerformAction()
@@ -400,6 +424,7 @@ namespace DOL.GS
         {
             LastAttackData = null;
             _target = null;
+            _firstTick = true;
         }
     }
 }

GameServer/ECS-Components/AttackComponent.cs
@@ -849,17 +849,9 @@ namespace DOL.GS
                             player.Out.SendInterruptAnimation(owner);
                     }
                 }
-
-                RangeAttackComponent rangeAttackComponent = owner.rangeAttackComponent;
-                rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
-
-                if (rangeAttackComponent.RangedAttackState is not eRangedAttackState.None)
-                {
-                    attackAction.OnRangedAttackStop();
-                    rangeAttackComponent.RangedAttackState = eRangedAttackState.None;
-                }
             }
 
+            attackAction.OnStopAttack();
             bool oldAttackState = AttackState;
             AttackState = false;
             owner.CancelEngageEffect();

GameServer/ECS-Components/EffectListComponent.cs
@@ -288,7 +288,7 @@ namespace DOL.GS
 
             static void TickAbilityEffect(ECSGameAbilityEffect abilityEffect)
             {
-                if (abilityEffect.NextTick != 0 && ServiceUtils.ShouldTickAdjust(ref abilityEffect.NextTick))
+                if (abilityEffect.NextTick != 0 && ServiceUtils.ShouldTick(abilityEffect.NextTick))
                 {
                     abilityEffect.OnEffectPulse();
                     abilityEffect.NextTick += abilityEffect.PulseFreq;
@@ -325,7 +325,7 @@ namespace DOL.GS
 
                 static void TickConcentrationEffect(ECSGameSpellEffect spellEffect)
                 {
-                    if (!ServiceUtils.ShouldTickAdjust(ref spellEffect.NextTick))
+                    if (!ServiceUtils.ShouldTick(spellEffect.NextTick))
                         return;
 
                     ISpellHandler spellHandler = spellEffect.SpellHandler;
@@ -388,7 +388,7 @@ namespace DOL.GS
 
                 static void TickPulsingEffect(ECSGameSpellEffect spellEffect, Spell spell, ISpellHandler spellHandler, GameLiving caster)
                 {
-                    if (!ServiceUtils.ShouldTickAdjust(ref spellEffect.NextTick))
+                    if (!ServiceUtils.ShouldTick(spellEffect.NextTick))
                         return;
 
                     // Not every pulsing effect is a `ECSPulseEffect`. Snares and roots decreasing effect are also handled as pulsing spells for example.
@@ -427,7 +427,7 @@ namespace DOL.GS
                             {
                                 ECSGameSpellEffect childEffect = pair.Value;
 
-                                if (ServiceUtils.ShouldTickNoEarly(childEffect.ExpireTick))
+                                if (ServiceUtils.ShouldTick(childEffect.ExpireTick))
                                 {
                                     livings ??= new();
                                     livings.Add(pair.Key);

GameServer/ECS-Components/MovementComponent.cs
@@ -1,11 +1,9 @@
-﻿using System.Reflection;
-using System.Threading;
+﻿using System.Threading;
 
 namespace DOL.GS
 {
     public class MovementComponent : IServiceObject
     {
-        private static readonly Logging.Logger log = Logging.LoggerManager.Create(MethodBase.GetCurrentMethod().DeclaringType);
         private const int SUBZONE_RELOCATION_CHECK_INTERVAL = 500;
 
         private long _nextSubZoneRelocationCheckTick;
@@ -57,9 +55,9 @@ namespace DOL.GS
         protected virtual void TickInternal()
         {
             // Only check for subzone relocation if we moved.
-            if (!Owner.IsSamePosition(_positionDuringLastSubZoneRelocationCheck) && ServiceUtils.ShouldTickAdjust(ref _nextSubZoneRelocationCheckTick))
+            if (!Owner.IsSamePosition(_positionDuringLastSubZoneRelocationCheck) && ServiceUtils.ShouldTick(_nextSubZoneRelocationCheckTick))
             {
-                _nextSubZoneRelocationCheckTick += SUBZONE_RELOCATION_CHECK_INTERVAL;
+                _nextSubZoneRelocationCheckTick = GameLoop.GameLoopTime + SUBZONE_RELOCATION_CHECK_INTERVAL;
                 _positionDuringLastSubZoneRelocationCheck = new Point2D(Owner.X, Owner.Y);
                 Owner.SubZoneObject.CheckForRelocation();
             }

GameServer/ECS-Components/NpcMovementComponent.cs
@@ -84,12 +84,12 @@ namespace DOL.GS
 
             if (IsFlagSet(MovementState.FOLLOW))
             {
-                if (ServiceUtils.ShouldTickAdjust(ref _nextFollowTick))
+                if (ServiceUtils.ShouldTick(_nextFollowTick))
                 {
                     _followTickInterval = FollowTick();
 
                     if (_followTickInterval != 0)
-                        _nextFollowTick += _followTickInterval;
+                        _nextFollowTick = GameLoop.GameLoopTime + _followTickInterval;
                     else
                         UnsetFlag(MovementState.WALK_TO);
                 }

GameServer/ECS-Components/PlayerMovementComponent.cs
@@ -12,7 +12,6 @@ namespace DOL.GS
         private const int BROADCAST_MINIMUM_INTERVAL = 200; // Clients send a position or heading update packet every 200ms at most (when moving or rotating).
         private const int SOFT_LINK_DEATH_THRESHOLD = 5000; // How long does it take without receiving a packet for a client to enter the soft link death state.
 
-        private long _lastPositionUpdatePacketReceivedTime;
         private long _nextPositionBroadcast;
         private bool _needBroadcastPosition;
 
@@ -26,7 +25,7 @@ namespace DOL.GS
 
         public new GamePlayer Owner { get; }
         public int MaxSpeedPercent => MaxSpeed * 100 / GamePlayer.PLAYER_BASE_SPEED;
-        public ref long LastPositionUpdatePacketReceivedTime => ref _lastPositionUpdatePacketReceivedTime;
+        public long LastPositionUpdatePacketReceivedTime { get; set; }
 
         public PlayerMovementComponent(GameLiving owner) : base(owner)
         {
@@ -38,7 +37,7 @@ namespace DOL.GS
         {
             if (!Owner.IsLinkDeathTimerRunning)
             {
-                if (ServiceUtils.ShouldTickNoEarly(_lastPositionUpdatePacketReceivedTime + SOFT_LINK_DEATH_THRESHOLD))
+                if (ServiceUtils.ShouldTick(LastPositionUpdatePacketReceivedTime + SOFT_LINK_DEATH_THRESHOLD))
                 {
                     if (log.IsInfoEnabled)
                         log.Info($"Position update timeout on client. Calling link death. ({Owner.Client})");
@@ -56,16 +55,16 @@ namespace DOL.GS
                 }
 
                 // Position and heading broadcasts are mutually exclusive.
-                if (_needBroadcastPosition && ServiceUtils.ShouldTickAdjust(ref _nextPositionBroadcast))
+                if (_needBroadcastPosition && ServiceUtils.ShouldTick(_nextPositionBroadcast))
                 {
                     BroadcastPosition();
-                    _nextPositionBroadcast += BROADCAST_MINIMUM_INTERVAL;
+                    _nextPositionBroadcast = GameLoop.GameLoopTime + BROADCAST_MINIMUM_INTERVAL;
                     _needBroadcastPosition = false;
                 }
-                else if (_needBroadcastHeading && ServiceUtils.ShouldTickAdjust(ref _nextHeadingBroadcast))
+                else if (_needBroadcastHeading && ServiceUtils.ShouldTick(_nextHeadingBroadcast))
                 {
                     BroadcastHeading();
-                    _nextHeadingBroadcast += BROADCAST_MINIMUM_INTERVAL;
+                    _nextHeadingBroadcast = GameLoop.GameLoopTime + BROADCAST_MINIMUM_INTERVAL;
                     _needBroadcastHeading = false;
                 }
             }
@@ -87,8 +86,10 @@ namespace DOL.GS
         {
             _needBroadcastPosition = true;
             _validateMovementOnNextTick = true;
-            _lastPositionUpdatePacketReceivedTime = GameLoop.GameLoopTime;
-            Owner.LastPlayerActivityTime = GameLoop.GameLoopTime;
+            LastPositionUpdatePacketReceivedTime = GameLoop.GameLoopTime;
+
+            if (IsMoving)
+                Owner.LastPlayerActivityTime = GameLoop.GameLoopTime;
 
             if (Owner.IsEncumbered)
             {

commit d07bdcf184f63fb1a691830f36fa278642068437

Fix `ClientService.UpdateWorld` ticking less often than intended Caused by 61e5796d1480b1375a432e9c05900515789224cf

GameServer/ECS-Services/ClientService.cs
@@ -92,10 +92,10 @@ namespace DOL.GS
                         if (client.ClientState is not GameClient.eClientState.Playing || player.ObjectState is not GameObject.eObjectState.Active)
                             break;
 
-                        if (ServiceUtils.ShouldTick(player.LastWorldUpdate + Properties.WORLD_PLAYER_UPDATE_INTERVAL))
+                        if (ServiceUtils.ShouldTick(player.NextWorldUpdate))
                         {
                             UpdateWorld(player);
-                            player.LastWorldUpdate = GameLoop.GameLoopTime + Properties.WORLD_PLAYER_UPDATE_INTERVAL;
+                            player.NextWorldUpdate = GameLoop.GameLoopTime + Properties.WORLD_PLAYER_UPDATE_INTERVAL;
                         }
 
                         break;

GameServer/gameobjects/GamePlayer.cs
@@ -48,7 +48,7 @@ namespace DOL.GS
         public override eGameObjectType GameObjectType => eGameObjectType.PLAYER;
         public ChainedActions ChainedActions { get; }
         public double SpecLock { get; set; }
-        public long LastWorldUpdate { get; set; }
+        public long NextWorldUpdate { get; set; }
 
         public ECSGameTimer PredatorTimeoutTimer
         {