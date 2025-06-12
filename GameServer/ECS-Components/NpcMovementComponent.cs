OpenDAoC/OpenDAoC-Core

commit 4b1a47fb82ad66d4b3f0fc0001a12bf70c854ac0

Introduce `MovementService`, ticking `MovementComponent`. Instead of being ticked by `ClientService` and `NpcService`. `MovementComponent` now implements `IServiceObject`.

GameServer/ECS-Components/NpcMovementComponent.cs
@@ -124,6 +124,9 @@ namespace DOL.GS
                     ClientService.UpdateNpcForPlayers(Owner);
                     _needsBroadcastUpdate = false;
                 }
+
+                if (_movementState is MovementState.NONE)
+                    RemoveFromServiceObjectStore();
             }
         }
 
@@ -133,6 +136,7 @@ namespace DOL.GS
             destination = new Point3D(destination.X, destination.Y, destination.Z);
             _movementRequest = new(destination, speed, WalkToInternal);
             SetFlag(MovementState.REQUEST);
+            AddToServiceObjectStore();
         }
 
         public void PathTo(Point3D destination, short speed)
@@ -141,6 +145,7 @@ namespace DOL.GS
             destination = new Point3D(destination.X, destination.Y, destination.Z);
             _movementRequest = new(destination, speed, PathToInternal);
             SetFlag(MovementState.REQUEST);
+            AddToServiceObjectStore();
         }
 
         public void StopMoving()
@@ -331,6 +336,7 @@ namespace DOL.GS
 
             _needsBroadcastUpdate = true;
             Owner.Heading = heading;
+            AddToServiceObjectStore();
         }
 
         public override void DisableTurning(bool add)

GameServer/ECS-Components/MovementComponent.cs
@@ -1,10 +1,9 @@
 ﻿using System.Reflection;
 using System.Threading;
-using ECS.Debug;
 
 namespace DOL.GS
 {
-    public class MovementComponent
+    public class MovementComponent : IServiceObject
     {
         private static readonly Logging.Logger log = Logging.LoggerManager.Create(MethodBase.GetCurrentMethod().DeclaringType);
         private const int SUBZONE_RELOCATION_CHECK_INTERVAL = 500;
@@ -19,6 +18,7 @@ namespace DOL.GS
         public virtual short MaxSpeed => (short) Owner.GetModified(eProperty.MaxSpeed);
         public bool IsMoving => CurrentSpeed != 0;
         public bool IsTurningDisabled => Interlocked.CompareExchange(ref _turningDisabledCount, 0, 0) > 0 && !Owner.effectListComponent.ContainsEffectForEffectType(eEffect.SpeedOfSound);
+        public ServiceObjectId ServiceObjectId { get; set; } = new(ServiceObjectType.MovementComponent);
 
         protected MovementComponent(GameLiving owner)
         {
@@ -37,12 +37,21 @@ namespace DOL.GS
 
         public void Tick()
         {
-            long startTick = GameLoop.GetCurrentTime();
-            TickInternal();
-            long stopTick = GameLoop.GetCurrentTime();
+            if (Owner.ObjectState is GameObject.eObjectState.Deleted)
+            {
+                RemoveFromServiceObjectStore();
+                return;
+            }
 
-            if (stopTick - startTick > Diagnostics.LongTickThreshold)
-                log.Warn($"Long {nameof(MovementComponent)}.{nameof(TickInternal)} for {Owner.Name}({Owner.ObjectID}) Time: {stopTick - startTick}ms");
+            TickInternal();
+        }
+
+        public virtual void DisableTurning(bool add)
+        {
+            if (add)
+                Interlocked.Increment(ref _turningDisabledCount);
+            else
+                Interlocked.Decrement(ref _turningDisabledCount);
         }
 
         protected virtual void TickInternal()
@@ -56,12 +65,14 @@ namespace DOL.GS
             }
         }
 
-        public virtual void DisableTurning(bool add)
+        protected void AddToServiceObjectStore()
         {
-            if (add)
-                Interlocked.Increment(ref _turningDisabledCount);
-            else
-                Interlocked.Decrement(ref _turningDisabledCount);
+            ServiceObjectStore.Add(this);
+        }
+
+        protected void RemoveFromServiceObjectStore()
+        {
+            ServiceObjectStore.Remove(this);
         }
     }
 }

GameServer/ECS-Components/PlayerMovementComponent.cs
@@ -110,6 +110,7 @@ namespace DOL.GS
             }
 
             _playerMovementMonitor.RecordPosition();
+            AddToServiceObjectStore();
 
             static void SendEncumberedMessage(GamePlayer player)
             {

GameServer/ECS-Services/ClientService.cs
@@ -94,7 +94,6 @@ namespace DOL.GS
                         if (ServiceUtils.ShouldTick(player.LastWorldUpdate + Properties.WORLD_PLAYER_UPDATE_INTERVAL))
                             UpdateWorld(player);
 
-                        player.movementComponent.Tick();
                         break;
                     }
                     default:

GameServer/ECS-Services/MovementService.cs
@@ -1,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using ECS.Debug;
+
+namespace DOL.GS
+{
+    public static class MovementService
+    {
+        private static readonly Logging.Logger log = Logging.LoggerManager.Create(MethodBase.GetCurrentMethod().DeclaringType);
+        private const string SERVICE_NAME = nameof(MovementService);
+        private static List<MovementComponent> _list;
+        private static int _entityCount;
+
+        public static void Tick()
+        {
+            GameLoop.CurrentServiceTick = SERVICE_NAME;
+            Diagnostics.StartPerfCounter(SERVICE_NAME);
+            _list = ServiceObjectStore.UpdateAndGetAll<MovementComponent>(ServiceObjectType.MovementComponent, out int lastValidIndex);
+            GameLoop.ExecuteWork(lastValidIndex + 1, TickInternal);
+
+            if (Diagnostics.CheckEntityCounts)
+                Diagnostics.PrintEntityCount(SERVICE_NAME, ref _entityCount, _list.Count);
+
+            Diagnostics.StopPerfCounter(SERVICE_NAME);
+        }
+
+        private static void TickInternal(int index)
+        {
+            MovementComponent movementComponent = null;
+
+            try
+            {
+                if (Diagnostics.CheckEntityCounts)
+                    Interlocked.Increment(ref _entityCount);
+
+                movementComponent = _list[index];
+                long startTick = GameLoop.GetCurrentTime();
+                movementComponent.Tick();
+                long stopTick = GameLoop.GetCurrentTime();
+
+                if (stopTick - startTick > Diagnostics.LongTickThreshold)
+                    log.Warn($"Long {SERVICE_NAME}.{nameof(Tick)} for: {movementComponent.Owner.Name}({movementComponent.Owner.ObjectID}) Time: {stopTick - startTick}ms");
+            }
+            catch (Exception e)
+            {
+                ServiceUtils.HandleServiceException(e, SERVICE_NAME, movementComponent, movementComponent.Owner);
+            }
+        }
+    }
+}

GameServer/ECS-Services/NpcService.cs
@@ -56,8 +56,6 @@ namespace DOL.GS
 
                     brain.NextThinkTick += brain.ThinkInterval;
                 }
-
-                npc.movementComponent.Tick();
             }
             catch (Exception e)
             {

GameServer/Managers/GameLoop/GameLoop.cs
@@ -147,6 +147,7 @@ namespace DOL.GS
                 AttackService.Tick();
                 CastingService.Tick();
                 EffectListService.Tick();
+                MovementService.Tick();
                 ZoneService.Tick();
                 CraftingService.Tick();
                 ReaperService.Tick();

GameServer/Managers/ServiceObject/ServiceObjectStore.cs
@@ -14,6 +14,7 @@ namespace DOL.GS
         {
             { ServiceObjectType.Client, new ServiceObjectArray<GameClient>(ServerProperties.Properties.MAX_PLAYERS) },
             { ServiceObjectType.Brain, new ServiceObjectArray<ABrain>(ServerProperties.Properties.MAX_ENTITIES) },
+            { ServiceObjectType.MovementComponent, new ServiceObjectArray<MovementComponent>(1250) },
             { ServiceObjectType.AttackComponent, new ServiceObjectArray<AttackComponent>(1250) },
             { ServiceObjectType.CastingComponent, new ServiceObjectArray<CastingComponent>(1250) },
             { ServiceObjectType.EffectListComponent, new ServiceObjectArray<EffectListComponent>(1250) },

commit 235a89e467464e46d90a80a3bd741021a793191c

Fix patrols / horses not moving when pathfinding is disabled Caused by 4b1a47fb82ad66d4b3f0fc0001a12bf70c854ac0 Fixed by making `MoveToNextWaypoint` call `PathTo` instead of `PathToInternal`, which is a tiny bit less efficient but ensures `AddToServiceObjectStore` is called.

GameServer/ECS-Components/NpcMovementComponent.cs
@@ -652,7 +652,7 @@ namespace DOL.GS
             oldPathPoint.FiredFlag = !oldPathPoint.FiredFlag;
 
             if (CurrentWaypoint != null)
-                PathToInternal(CurrentWaypoint, Math.Min(_moveOnPathSpeed, CurrentWaypoint.MaxSpeed));
+                PathTo(CurrentWaypoint, Math.Min(_moveOnPathSpeed, CurrentWaypoint.MaxSpeed));
             else
                 StopMovingOnPath();
         }