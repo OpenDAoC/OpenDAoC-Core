OpenDAoC/OpenDAoC-Core

commit 94773f7bd6f3e1dc05a1645f2b3d73ec0905bad9

Fix wrong usage of ToA melee and style damage bonuses * Melee damage bonus was added to base damage, which was then used to calculate the styled portion of thee damage, which was then increased by style damage bonus. * Instead, both melee damage and style damage bonus are calculated on base damage. The first one is added to base damage after the styled portion of the damage is calculated, the second one is added to the styled damage portion.
* Let's say we do a 200 base damage attack with a modified GR of 0.5, and have 10% melee damage and style damage bonus:
Before: (200*1.1)+(200*1.1*0.5*1.1)=341.
Now: (200*1.1)+(200*0.5+200*0.1)=340.

* With 10% melee damage and 0% style damage bonus:
Before: (200*1.1)+(200*1.1*0.5)=330.
Now: (200*1.1)+(200*0.5)=320.

* With 0% melee damage and 10% style damage bonus:
Before: 200+(200*0.5*1.1)=310.
Now: 200+(200*0.5+200*0.1)=320.

* This change also allows styles with a GR of 0 to do style damage, which is confirmed to be accurate according to various old forum posts.

GameServer/ECS-Components/AttackComponent.cs
@@ -436,7 +436,6 @@ namespace DOL.GS
 
         public double AttackDamage(DbInventoryItem weapon, WeaponAction action, out double damageCap)
         {
-            double effectiveness = 1;
             damageCap = 0;
 
             if (owner is GamePlayer player)
@@ -468,31 +467,15 @@ namespace DOL.GS
                                 break; // Broadhead (X-heavy) +25%.
                         }
                     }
-
-                    if ((eObjectType) weapon.Object_Type is eObjectType.Longbow or eObjectType.RecurvedBow or eObjectType.CompositeBow)
-                    {
-                        if (Properties.ALLOW_OLD_ARCHERY)
-                            effectiveness += player.GetModified(eProperty.RangedDamage) * 0.01;
-                        else
-                        {
-                            effectiveness += owner.GetModified(eProperty.RangedDamage) * 0.01;
-                            effectiveness += owner.GetModified(eProperty.SpellDamage) * 0.01;
-                        }
-                    }
-                    else
-                        effectiveness += player.GetModified(eProperty.RangedDamage) * 0.01;
                 }
                 else if (weapon.Item_Type is Slot.RIGHTHAND or Slot.LEFTHAND or Slot.TWOHAND)
                 {
-                    effectiveness += player.GetModified(eProperty.MeleeDamage) * 0.01;
-
                     if (weapon.Item_Type == Slot.TWOHAND)
                         damageCap *= CalculateTwoHandedDamageModifier(weapon);
                     else if (player.ActiveLeftWeapon != null)
                         damageCap *= CalculateLeftAxeModifier();
                 }
 
-                damageCap *= effectiveness;
                 double damage = GamePlayer.ApplyWeaponQualityAndConditionToDamage(weapon, damageCap);
                 damageCap *= 3;
                 return damage;
@@ -504,30 +487,6 @@ namespace DOL.GS
                 if (owner is GameNPC npc)
                     damage *= npc.DamageFactor;
 
-                if (weapon == null ||
-                    weapon.SlotPosition == Slot.RIGHTHAND ||
-                    weapon.SlotPosition == Slot.LEFTHAND ||
-                    weapon.SlotPosition == Slot.TWOHAND)
-                {
-                    effectiveness += owner.GetModified(eProperty.MeleeDamage) * 0.01;
-                }
-                else if (weapon.SlotPosition is Slot.RANGED)
-                {
-                    if ((eObjectType) weapon.Object_Type is eObjectType.Longbow or eObjectType.RecurvedBow or eObjectType.CompositeBow)
-                    {
-                        if (Properties.ALLOW_OLD_ARCHERY)
-                            effectiveness += owner.GetModified(eProperty.RangedDamage) * 0.01;
-                        else
-                        {
-                            effectiveness += owner.GetModified(eProperty.RangedDamage) * 0.01;
-                            effectiveness += owner.GetModified(eProperty.SpellDamage) * 0.01;
-                        }
-                    }
-                    else
-                        effectiveness += owner.GetModified(eProperty.RangedDamage) * 0.01;
-                }
-
-                damage *= effectiveness;
                 damageCap = damage * 3;
 
                 if (owner is GameEpicBoss)
@@ -1185,7 +1144,7 @@ namespace DOL.GS
                 case eAttackResult.HitUnstyled:
                 case eAttackResult.HitStyle:
                 {
-                    double damage = AttackDamage(weapon, action, out double baseDamageCap) * effectiveness;
+                    double damage = AttackDamage(weapon, action, out double baseDamageCap);
                     DbInventoryItem armor = null;
 
                     if (ad.Target.Inventory != null)
@@ -1194,9 +1153,6 @@ namespace DOL.GS
                     double weaponSkill = CalculateWeaponSkill(weapon, ad.Target, out int spec, out (double, double) varianceRange, out double specModifier, out double baseWeaponSkill);
                     double armorMod = CalculateTargetArmor(ad.Target, ad.ArmorHitLocation, out double armorFactor, out double absorb);
                     double damageMod = weaponSkill / armorMod;
-
-                    if (action.RangedAttackType is eRangedAttackType.Critical)
-                        baseDamageCap *= 2; // This may be incorrect. Critical shot doesn't double damage on >yellow targets.
 
                     // Badge Of Valor Calculation 1+ absorb or 1- absorb
                     // if (ad.Attacker.EffectList.GetOfType<BadgeOfValorEffect>() != null)
@@ -1217,17 +1173,37 @@ namespace DOL.GS
                     }
 
                     damage *= damageMod;
+
+                    // Melee damage and style damage ToA bonuses are pretty weird.
+                    // * They're both calculated from base damage.
+                    // * They effectively add the same amount of damage when used independently.
+                    // * Style damage bonus works even on styles that have no growth rate.
+                    // * However, the first one will be added to base damage, and the second one to style damage. This is really just for display purposes.
+                    // * They stack multiplicatively. Assuming a GR of 0, two 10% bonuses result in the attack doing 21% more damage.
+                    // * The higher the GR, the lower their contribution to total damage is (since GR is actually ignored).
+
+                    effectiveness *= CalculateEffectiveness(weapon); // Augment the passed effectiveness with the weapon's effectiveness (ToA bonuses, etc.).
+                    double preEffectivenessDamage = damage; // Damage snapshot before applying effectiveness, to be used to calculate style damage.
+                    double preEffectivenessBaseDamageCap = baseDamageCap; // Damage cap snapshot before applying effectiveness, to be used to calculate style damage.
+                    damage *= effectiveness;
+                    baseDamageCap *= effectiveness;
+
                     double conversionMod = CalculateTargetConversion(ad.Target);
                     double primarySecondaryResistMod = CalculateTargetResistance(ad.Target, ad.DamageType, armor);
                     double primarySecondaryResistConversionMod = primarySecondaryResistMod * conversionMod;
-                    double preResistBaseDamage = damage;
-                    damage = Math.Min(baseDamageCap, preResistBaseDamage * primarySecondaryResistConversionMod);
-                    // This makes capped unstyled hits have weird modifiers and no longer match the actual damage reduction from resistances; for example 150 (-1432) an a naked target.
-                    // But inaccurate modifiers when the cap is hit appear to be live like.
-                    double modifier = damage - preResistBaseDamage;
 
-                    if (StyleProcessor.ExecuteStyle(owner, ad.Target, ad.Style, weapon, preResistBaseDamage, baseDamageCap, ad.ArmorHitLocation, ad.StyleEffects, out double styleDamage, out double styleDamageCap, out int animationId))
+                    // This makes capped unstyled hits have weird modifiers, and no longer match the actual damage reduction from resistances; for example 150 (-1432) against a naked target.
+                    // But inaccurate modifiers when the cap is hit appears to be live like.
+                    double preResistDamage = damage; // Pre resist damage snapshot in case we need to add style damage bonus.
+                    double modifier = Math.Min(baseDamageCap, preResistDamage * primarySecondaryResistConversionMod) - damage;
+                    damage += modifier;
+
+                    if (StyleProcessor.ExecuteStyle(owner, ad.Target, ad.Style, weapon, preEffectivenessDamage, preEffectivenessBaseDamageCap, ad.ArmorHitLocation, ad.StyleEffects, out double styleDamage, out double styleDamageCap, out int animationId))
                     {
+                        double styleDamageBonus = preResistDamage * owner.GetModified(eProperty.StyleDamage) * 0.01;
+                        styleDamage += styleDamageBonus;
+                        styleDamageCap += styleDamageBonus;
+
                         double preResistStyleDamage = styleDamage;
                         ad.StyleDamage = (int) preResistStyleDamage; // We show uncapped and unmodified by resistances style damage. This should only be used by the combat log.
                         // We have to calculate damage reduction again because `ExecuteStyle` works with pre resist base damage. Static growth styles also don't use it.
@@ -1576,6 +1552,31 @@ namespace DOL.GS
 
             owner.OnAttackEnemy(ad);
             return ad;
+        }
+
+        public double CalculateEffectiveness(DbInventoryItem weapon)
+        {
+            double effectiveness = 1.0;
+
+            if (weapon.Item_Type is Slot.RANGED)
+            {
+                if ((eObjectType) weapon.Object_Type is eObjectType.Longbow or eObjectType.RecurvedBow or eObjectType.CompositeBow)
+                {
+                    if (Properties.ALLOW_OLD_ARCHERY)
+                        effectiveness += owner.GetModified(eProperty.RangedDamage) * 0.01;
+                    else
+                    {
+                        effectiveness += owner.GetModified(eProperty.RangedDamage) * 0.01;
+                        effectiveness += owner.GetModified(eProperty.SpellDamage) * 0.01;
+                    }
+                }
+                else
+                    effectiveness += owner.GetModified(eProperty.RangedDamage) * 0.01;
+            }
+            else if (weapon.Item_Type is Slot.RIGHTHAND or Slot.LEFTHAND or Slot.TWOHAND)
+                effectiveness += owner.GetModified(eProperty.MeleeDamage) * 0.01;
+
+            return effectiveness;
         }
 
         public double CalculateWeaponSkill(DbInventoryItem weapon, GameLiving target, out int spec, out (double, double) varianceRange, out double specModifier, out double baseWeaponSkill)

GameServer/commands/playercommands/targetstats.cs
@@ -115,6 +115,9 @@ namespace DOL.GS.Commands
             void DisplayWeaponInfo(DbInventoryItem weapon, AttackData.eAttackType attackType)
             {
                 double weaponDamage = target.attackComponent.AttackDamage(weapon, null, out double weaponDamageCap);
+                double effectiveness = target.attackComponent.CalculateEffectiveness(weapon);
+                weaponDamage *= effectiveness;
+                weaponDamageCap *= effectiveness;
                 info.Add($"Weapon damage:  {weaponDamage:0}  |  {weaponDamageCap:0}");
 
                 _ = target.attackComponent.CalculateWeaponSkill(weapon, client.Player, out _, out (double lowerLimit, double upperLimit) varianceRange, out _, out double baseWeaponSkill);

GameServer/propertycalc/StyleDamagePercentCalculator.cs
@@ -1,42 +1,22 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
 using System;
 
 namespace DOL.GS.PropertyCalc
 {
-	/// <summary>
-	/// The Spell Range bonus percent calculator
-	///
-	/// BuffBonusCategory1 unused
-	/// BuffBonusCategory2 unused
-	/// BuffBonusCategory3 is used for debuff
-	/// BuffBonusCategory4 unused
-	/// BuffBonusMultCategory1 unused
-	/// </summary>
-	[PropertyCalculator(eProperty.StyleDamage)]
-	public class StyleDamagePercentCalculator : PropertyCalculator
-	{
-		public override int CalcValue(GameLiving living, eProperty property)
-		{
-			return Math.Max(0, 100
-				+Math.Min(10,living.ItemBonus[property]));
-//				-living.BuffBonusCategory3[(int)property]);
-		}
-	}
+    /// <summary>
+    /// The Spell Range bonus percent calculator
+    ///
+    /// BuffBonusCategory1 unused
+    /// BuffBonusCategory2 unused
+    /// BuffBonusCategory3 is used for debuff
+    /// BuffBonusCategory4 unused
+    /// BuffBonusMultCategory1 unused
+    /// </summary>
+    [PropertyCalculator(eProperty.StyleDamage)]
+    public class StyleDamagePercentCalculator : PropertyCalculator
+    {
+        public override int CalcValue(GameLiving living, eProperty property)
+        {
+            return Math.Min(10, living.ItemBonus[property]);
+        }
+    }
 }

GameServer/spells/Masterlevel/Battlemaster.cs
@@ -571,7 +571,7 @@ namespace DOL.GS.Spells
                     break;
             }
 
-            double effectiveness = CalculateDamageEffectiveness();
+            double effectiveness = CalculateDamageEffectiveness(); // Spell damage? Seems wrong.
 
             //Throw Weapon is subject to all the conventional attack results, parry, evade, block, etc.
             ad.AttackResult = ad.Target.attackComponent.CalculateEnemyAttackResult(null, ad, weapon, ref effectiveness);

GameServer/styles/StyleProcessor.cs
@@ -364,8 +364,8 @@ namespace DOL.GS.Styles
 					}
 
 					// Styles with a static growth don't use unstyled damage, so armor has to be taken into account here.
-					DbInventoryItem armor = target.Inventory?.GetItem((eInventorySlot) armorHitLocation);
-					styleDamage = styleDamage * (1.0 - target.GetArmorAbsorb(armorHitLocation));
+					// AF isn't taken into account because we don't have a weaponskill to compare it to. This may be a problem.
+					styleDamage *= 1.0 - target.GetArmorAbsorb(armorHitLocation);
 					styleDamageCap = -1; // Uncapped. Is there supposed to be one?
 				}
 				else
@@ -384,9 +384,6 @@ namespace DOL.GS.Styles
 						styleDamageCap = 0;
 					}
 				}
-
-				if (player != null)
-					styleDamage = styleDamage * player.GetModified(eProperty.StyleDamage) * 0.01;
 
 				// Style absorb bonus.
 				if (target is GamePlayer)

commit 5758666fcaec52e8e1608385f31326567c6a9e4e

Fix null reference exception caused by 94773f7bd6f3e1dc05a1645f2b3d73ec0905bad9

GameServer/ECS-Components/AttackComponent.cs
@@ -1556,27 +1556,22 @@ namespace DOL.GS
 
         public double CalculateEffectiveness(DbInventoryItem weapon)
         {
-            double effectiveness = 1.0;
+            double effectiveness = 100;
 
-            if (weapon.Item_Type is Slot.RANGED)
+            if (weapon == null || weapon.Item_Type is Slot.RIGHTHAND or Slot.LEFTHAND or Slot.TWOHAND)
+                effectiveness += owner.GetModified(eProperty.MeleeDamage);
+            else if (weapon.Item_Type is Slot.RANGED)
             {
+                effectiveness += owner.GetModified(eProperty.RangedDamage);
+
                 if ((eObjectType) weapon.Object_Type is eObjectType.Longbow or eObjectType.RecurvedBow or eObjectType.CompositeBow)
                 {
-                    if (Properties.ALLOW_OLD_ARCHERY)
-                        effectiveness += owner.GetModified(eProperty.RangedDamage) * 0.01;
-                    else
-                    {
-                        effectiveness += owner.GetModified(eProperty.RangedDamage) * 0.01;
-                        effectiveness += owner.GetModified(eProperty.SpellDamage) * 0.01;
-                    }
+                    if (!Properties.ALLOW_OLD_ARCHERY)
+                        effectiveness += owner.GetModified(eProperty.SpellDamage);
                 }
-                else
-                    effectiveness += owner.GetModified(eProperty.RangedDamage) * 0.01;
             }
-            else if (weapon.Item_Type is Slot.RIGHTHAND or Slot.LEFTHAND or Slot.TWOHAND)
-                effectiveness += owner.GetModified(eProperty.MeleeDamage) * 0.01;
 
-            return effectiveness;
+            return effectiveness * 0.01;
         }
 
         public double CalculateWeaponSkill(DbInventoryItem weapon, GameLiving target, out int spec, out (double, double) varianceRange, out double specModifier, out double baseWeaponSkill)