OpenDAoC/OpenDAoC-Core

commit 61e5796d1480b1375a432e9c05900515789224cf

Switch game loop to fixed timestep * Replaced real-time `GameLoopTime` with fixed timestep that always increments by `TickRate` (unless it's ticking too fast, which should never happen). This eliminates event synchronization issues where delayed events would cluster together, resulting in inconsistent server tick rate, and preserves relative timing between game events during server lag spikes. * Updated `PlayerMovementMonitor` to use `GetCurrentTime` for movement validation since player input should remain responsive to real-time. * Updated `ServiceUtil` to only leave `ShouldTick`, as other methods are not needed with a fixed timestep. * Renamed `GetCurrentTime` to `GetRealTime`.

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -21,6 +21,7 @@ namespace DOL.GS
         private GameLiving _owner;
         private long _nextMeleeTick;
         private long _nextRangedTick;
+        private bool _firstTick = true;
 
         // Set to current time when a round doesn't result in an attack. Used to prevent combat log spam and kept until reset in AttackComponent.SendAttackingCombatMessages().
         public long RoundWithNoAttackTime { get; set; }
@@ -32,8 +33,6 @@ namespace DOL.GS
         protected AttackAction(GameLiving owner)
         {
             _owner = owner;
-            _nextMeleeTick = GameLoop.GameLoopTime;
-            _nextRangedTick = GameLoop.GameLoopTime;
         }
 
         public static AttackAction Create(GameLiving living)
@@ -48,6 +47,13 @@ namespace DOL.GS
 
         public bool Tick()
         {
+            if (_firstTick)
+            {
+                _nextMeleeTick = Math.Max(_nextMeleeTick, GameLoop.GameLoopTime);
+                _nextRangedTick = Math.Max(_nextRangedTick, GameLoop.GameLoopTime);
+                _firstTick = false;
+            }
+
             if (!ShouldTick())
                 return true;
 
@@ -103,17 +109,35 @@ namespace DOL.GS
                 _nextRangedTick += _interval;
         }
 
-        public void OnRangedAttackStop()
+        public void OnStopAttack()
         {
+            if (_owner.ActiveWeaponSlot is not eActiveWeaponSlot.Distance)
+                return;
+
+            RangeAttackComponent rangeAttackComponent = _owner.rangeAttackComponent;
+            rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
+
+            if (rangeAttackComponent.RangedAttackState is eRangedAttackState.None)
+                return;
+
             if (GameLoop.GameLoopTime - _nextMeleeTick > MINIMUM_MELEE_DELAY_AFTER_RANGED_ATTACK)
                 _nextMeleeTick = GameLoop.GameLoopTime + MINIMUM_MELEE_DELAY_AFTER_RANGED_ATTACK;
 
-            _nextRangedTick = GameLoop.GameLoopTime;
+            rangeAttackComponent.RangedAttackState = eRangedAttackState.None;
         }
 
-        public void OnEnterMeleeRange()
+        public void OnHeadingUpdate()
         {
-            _nextMeleeTick = GameLoop.GameLoopTime;
+            if (!AttackComponent.AttackState || _owner.ActiveWeaponSlot is eActiveWeaponSlot.Distance)
+                return;
+
+            AttackData ad = LastAttackData;
+
+            if (ad == null || !ad.IsMeleeAttack || (ad.AttackResult is not eAttackResult.TargetNotVisible and not eAttackResult.OutOfRange))
+                return;
+
+            if (ad.Target != null && _owner.IsObjectInFront(ad.Target, 120) && _owner.IsWithinRadius(ad.Target, AttackComponent.AttackRange))
+                _firstTick = true;
         }
 
         public virtual bool CheckInterruptTimer()
@@ -138,8 +162,8 @@ namespace DOL.GS
         private bool ShouldTick()
         {
             return _owner.ActiveWeaponSlot != eActiveWeaponSlot.Distance
-                ? ServiceUtils.ShouldTickAdjust(ref _nextMeleeTick)
-                : ServiceUtils.ShouldTickAdjust(ref _nextRangedTick);
+                ? ServiceUtils.ShouldTick(_nextMeleeTick)
+                : ServiceUtils.ShouldTick(_nextRangedTick);
         }
 
         protected virtual bool CanPerformAction()
@@ -400,6 +424,7 @@ namespace DOL.GS
         {
             LastAttackData = null;
             _target = null;
+            _firstTick = true;
         }
     }
 }

GameServer/gameobjects/GameNPC.cs
@@ -3350,12 +3350,12 @@ namespace DOL.GS
 		public bool CanCastInstantMiscSpells => InstantMiscSpells != null && InstantMiscSpells.Count > 0;
 
 		private long _nextInstantHarmfulSpell;
-		public virtual bool IsInstantHarmfulSpellCastingLocked => !ServiceUtils.ShouldTickAdjust(ref _nextInstantHarmfulSpell);
+		public virtual bool IsInstantHarmfulSpellCastingLocked => !ServiceUtils.ShouldTick(_nextInstantHarmfulSpell);
 
 		public virtual void ApplyInstantHarmfulSpellDelay()
 		{
 			// Delay the next spell by 1~6 seconds (triangular distribution).
-			_nextInstantHarmfulSpell += 1000 + Util.Random(2500) + Util.Random(2500);;
+			_nextInstantHarmfulSpell = GameLoop.GameLoopTime + 1000 + Util.Random(2500) + Util.Random(2500);
 		}
 
 		/// <summary>

GameServer/ECS-Components/AttackComponent.cs
@@ -849,17 +849,9 @@ namespace DOL.GS
                             player.Out.SendInterruptAnimation(owner);
                     }
                 }
-
-                RangeAttackComponent rangeAttackComponent = owner.rangeAttackComponent;
-                rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
-
-                if (rangeAttackComponent.RangedAttackState is not eRangedAttackState.None)
-                {
-                    attackAction.OnRangedAttackStop();
-                    rangeAttackComponent.RangedAttackState = eRangedAttackState.None;
-                }
             }
 
+            attackAction.OnStopAttack();
             bool oldAttackState = AttackState;
             AttackState = false;
             owner.CancelEngageEffect();

GameServer/ECS-Components/EffectListComponent.cs
@@ -288,7 +288,7 @@ namespace DOL.GS
 
             static void TickAbilityEffect(ECSGameAbilityEffect abilityEffect)
             {
-                if (abilityEffect.NextTick != 0 && ServiceUtils.ShouldTickAdjust(ref abilityEffect.NextTick))
+                if (abilityEffect.NextTick != 0 && ServiceUtils.ShouldTick(abilityEffect.NextTick))
                 {
                     abilityEffect.OnEffectPulse();
                     abilityEffect.NextTick += abilityEffect.PulseFreq;
@@ -325,7 +325,7 @@ namespace DOL.GS
 
                 static void TickConcentrationEffect(ECSGameSpellEffect spellEffect)
                 {
-                    if (!ServiceUtils.ShouldTickAdjust(ref spellEffect.NextTick))
+                    if (!ServiceUtils.ShouldTick(spellEffect.NextTick))
                         return;
 
                     ISpellHandler spellHandler = spellEffect.SpellHandler;
@@ -388,7 +388,7 @@ namespace DOL.GS
 
                 static void TickPulsingEffect(ECSGameSpellEffect spellEffect, Spell spell, ISpellHandler spellHandler, GameLiving caster)
                 {
-                    if (!ServiceUtils.ShouldTickAdjust(ref spellEffect.NextTick))
+                    if (!ServiceUtils.ShouldTick(spellEffect.NextTick))
                         return;
 
                     // Not every pulsing effect is a `ECSPulseEffect`. Snares and roots decreasing effect are also handled as pulsing spells for example.
@@ -427,7 +427,7 @@ namespace DOL.GS
                             {
                                 ECSGameSpellEffect childEffect = pair.Value;
 
-                                if (ServiceUtils.ShouldTickNoEarly(childEffect.ExpireTick))
+                                if (ServiceUtils.ShouldTick(childEffect.ExpireTick))
                                 {
                                     livings ??= new();
                                     livings.Add(pair.Key);

GameServer/ECS-Components/MovementComponent.cs
@@ -1,11 +1,9 @@
-﻿using System.Reflection;
-using System.Threading;
+﻿using System.Threading;
 
 namespace DOL.GS
 {
     public class MovementComponent : IServiceObject
     {
-        private static readonly Logging.Logger log = Logging.LoggerManager.Create(MethodBase.GetCurrentMethod().DeclaringType);
         private const int SUBZONE_RELOCATION_CHECK_INTERVAL = 500;
 
         private long _nextSubZoneRelocationCheckTick;
@@ -57,9 +55,9 @@ namespace DOL.GS
         protected virtual void TickInternal()
         {
             // Only check for subzone relocation if we moved.
-            if (!Owner.IsSamePosition(_positionDuringLastSubZoneRelocationCheck) && ServiceUtils.ShouldTickAdjust(ref _nextSubZoneRelocationCheckTick))
+            if (!Owner.IsSamePosition(_positionDuringLastSubZoneRelocationCheck) && ServiceUtils.ShouldTick(_nextSubZoneRelocationCheckTick))
             {
-                _nextSubZoneRelocationCheckTick += SUBZONE_RELOCATION_CHECK_INTERVAL;
+                _nextSubZoneRelocationCheckTick = GameLoop.GameLoopTime + SUBZONE_RELOCATION_CHECK_INTERVAL;
                 _positionDuringLastSubZoneRelocationCheck = new Point2D(Owner.X, Owner.Y);
                 Owner.SubZoneObject.CheckForRelocation();
             }

GameServer/ECS-Components/NpcMovementComponent.cs
@@ -84,12 +84,12 @@ namespace DOL.GS
 
             if (IsFlagSet(MovementState.FOLLOW))
             {
-                if (ServiceUtils.ShouldTickAdjust(ref _nextFollowTick))
+                if (ServiceUtils.ShouldTick(_nextFollowTick))
                 {
                     _followTickInterval = FollowTick();
 
                     if (_followTickInterval != 0)
-                        _nextFollowTick += _followTickInterval;
+                        _nextFollowTick = GameLoop.GameLoopTime + _followTickInterval;
                     else
                         UnsetFlag(MovementState.WALK_TO);
                 }

GameServer/ECS-Components/PlayerMovementComponent.cs
@@ -12,7 +12,6 @@ namespace DOL.GS
         private const int BROADCAST_MINIMUM_INTERVAL = 200; // Clients send a position or heading update packet every 200ms at most (when moving or rotating).
         private const int SOFT_LINK_DEATH_THRESHOLD = 5000; // How long does it take without receiving a packet for a client to enter the soft link death state.
 
-        private long _lastPositionUpdatePacketReceivedTime;
         private long _nextPositionBroadcast;
         private bool _needBroadcastPosition;
 
@@ -26,7 +25,7 @@ namespace DOL.GS
 
         public new GamePlayer Owner { get; }
         public int MaxSpeedPercent => MaxSpeed * 100 / GamePlayer.PLAYER_BASE_SPEED;
-        public ref long LastPositionUpdatePacketReceivedTime => ref _lastPositionUpdatePacketReceivedTime;
+        public long LastPositionUpdatePacketReceivedTime { get; set; }
 
         public PlayerMovementComponent(GameLiving owner) : base(owner)
         {
@@ -38,7 +37,7 @@ namespace DOL.GS
         {
             if (!Owner.IsLinkDeathTimerRunning)
             {
-                if (ServiceUtils.ShouldTickNoEarly(_lastPositionUpdatePacketReceivedTime + SOFT_LINK_DEATH_THRESHOLD))
+                if (ServiceUtils.ShouldTick(LastPositionUpdatePacketReceivedTime + SOFT_LINK_DEATH_THRESHOLD))
                 {
                     if (log.IsInfoEnabled)
                         log.Info($"Position update timeout on client. Calling link death. ({Owner.Client})");
@@ -56,16 +55,16 @@ namespace DOL.GS
                 }
 
                 // Position and heading broadcasts are mutually exclusive.
-                if (_needBroadcastPosition && ServiceUtils.ShouldTickAdjust(ref _nextPositionBroadcast))
+                if (_needBroadcastPosition && ServiceUtils.ShouldTick(_nextPositionBroadcast))
                 {
                     BroadcastPosition();
-                    _nextPositionBroadcast += BROADCAST_MINIMUM_INTERVAL;
+                    _nextPositionBroadcast = GameLoop.GameLoopTime + BROADCAST_MINIMUM_INTERVAL;
                     _needBroadcastPosition = false;
                 }
-                else if (_needBroadcastHeading && ServiceUtils.ShouldTickAdjust(ref _nextHeadingBroadcast))
+                else if (_needBroadcastHeading && ServiceUtils.ShouldTick(_nextHeadingBroadcast))
                 {
                     BroadcastHeading();
-                    _nextHeadingBroadcast += BROADCAST_MINIMUM_INTERVAL;
+                    _nextHeadingBroadcast = GameLoop.GameLoopTime + BROADCAST_MINIMUM_INTERVAL;
                     _needBroadcastHeading = false;
                 }
             }
@@ -87,8 +86,10 @@ namespace DOL.GS
         {
             _needBroadcastPosition = true;
             _validateMovementOnNextTick = true;
-            _lastPositionUpdatePacketReceivedTime = GameLoop.GameLoopTime;
-            Owner.LastPlayerActivityTime = GameLoop.GameLoopTime;
+            LastPositionUpdatePacketReceivedTime = GameLoop.GameLoopTime;
+
+            if (IsMoving)
+                Owner.LastPlayerActivityTime = GameLoop.GameLoopTime;
 
             if (Owner.IsEncumbered)
             {

GameServer/ECS-Debug/ECSDebug.cs
@@ -62,7 +62,7 @@ namespace ECS.Debug
 
             if (_gameEventMgrNotifyProfilingEnabled)
             {
-                if ((GameLoop.GetCurrentTime() - _gameEventMgrNotifyTimerStartTick) > _gameEventMgrNotifyTimerInterval)
+                if ((GameLoop.GetRealTime() - _gameEventMgrNotifyTimerStartTick) > _gameEventMgrNotifyTimerInterval)
                     ReportGameEventMgrNotifyTimes();
             }
 
@@ -173,7 +173,7 @@ namespace ECS.Debug
 
             _gameEventMgrNotifyProfilingEnabled = true;
             _gameEventMgrNotifyTimerInterval = IntervalMilliseconds;
-            _gameEventMgrNotifyTimerStartTick = GameLoop.GetCurrentTime();
+            _gameEventMgrNotifyTimerStartTick = GameLoop.GetRealTime();
         }
 
         public static void StopGameEventMgrNotifyTimeReporting()
@@ -187,7 +187,7 @@ namespace ECS.Debug
 
         private static void ReportGameEventMgrNotifyTimes()
         {
-            string ActualInterval = Util.TruncateString((GameLoop.GetCurrentTime() - _gameEventMgrNotifyTimerStartTick).ToString(), 5);
+            string ActualInterval = Util.TruncateString((GameLoop.GetRealTime() - _gameEventMgrNotifyTimerStartTick).ToString(), 5);
             log.Debug($"==== GameEventMgr Notify() Costs (Requested Interval: {_gameEventMgrNotifyTimerInterval}ms | Actual Interval: {ActualInterval}ms) ====");
 
             lock (_gameEventMgrNotifyLock)
@@ -222,7 +222,7 @@ namespace ECS.Debug
                 }
 
                 _gameEventMgrNotifyTimes.Clear();
-                _gameEventMgrNotifyTimerStartTick = GameLoop.GetCurrentTime();
+                _gameEventMgrNotifyTimerStartTick = GameLoop.GetRealTime();
                 log.Debug("---------------------------------------------------------------------------");
             }
         }

commit 7647ef380f20095852c43a2ad83efede3b344b97

Fix another regression caused by 61e5796d1480b1375a432e9c05900515789224cf Fixes ranged NPCs able to buffer ranged attacks after switching to melee. * Moved `AttackState` check above `_firstTick` check in `AttackAction.Tick`. * Removed redudant call to `CleanUp` in `AttackAction.PrepareRangedAttacks` (will be called by `AttackAction.Tick`). * Removed redundant call to `CleanUp` in `PlayerAttackAction.FinalizeRangedAttack` (will be called by `AttackAction.Tick`). * Clarified comment about `AttackState` in `GameNPC.SwitchWeapon`.

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -48,6 +48,12 @@ namespace DOL.GS
 
         public bool Tick()
         {
+            if (!AttackComponent.AttackState)
+            {
+                CleanUp();
+                return false;
+            }
+
             if (_firstTick)
             {
                 _nextMeleeTick = Math.Max(_nextMeleeTick, GameLoop.GameLoopTime);
@@ -62,12 +68,6 @@ namespace DOL.GS
             {
                 _interval = TICK_INTERVAL_FOR_NON_ATTACK;
                 return true;
-            }
-
-            if (!AttackComponent.AttackState)
-            {
-                CleanUp();
-                return false;
             }
 
             _weapon = _owner.ActiveWeapon;
@@ -112,6 +112,14 @@ namespace DOL.GS
 
         public void OnStopAttack()
         {
+            // This method serves two purposes:
+            // * Delay the next melee attack a little after aborting a ranged attack, or getting interrupted.
+            // * Ensure we're not buffering. For this, we need to explicitly set `_firstTick` to true,
+            // because NPCs are able to stop and start an attack during the same tick, which prevents `CleanUp` from being called by `Tick`.
+            // * Ensure we're not delaying the next ranged attack's preparation. This means resetting `_nextRangedTick`.
+
+            _firstTick = true;
+
             if (_owner.ActiveWeaponSlot is not eActiveWeaponSlot.Distance)
                 return;
 
@@ -265,7 +273,6 @@ namespace DOL.GS
             else if (rangeCheckResult == eCheckRangeAttackStateResult.Stop || _target == null)
             {
                 AttackComponent.StopAttack();
-                AttackComponent.attackAction.CleanUp();
                 return false;
             }
 

GameServer/ECS-Components/Actions/PlayerAttackAction.cs
@@ -94,7 +94,6 @@ namespace DOL.GS
             if (stopAttack)
             {
                 AttackComponent.StopAttack();
-                CleanUp();
                 return false;
             }
 

GameServer/gameobjects/GameNPC.cs
@@ -1500,6 +1500,10 @@ namespace DOL.GS
 		/// <param name="slot">the new eActiveWeaponSlot</param>
 		public override void SwitchWeapon(eActiveWeaponSlot slot)
 		{
+			// `AttackState` is normally false here, unless `SwitchWeapon` was called directly. This is the case with scripted NPCs.
+			// We use `wasInAttackState` and `OnForcedWeaponSwitch so that automatic weapon switch works as intended.
+			// Eventually, this behavior should be able to be disabled for scripted NPCs. but for now, they must comply.
+
 			bool wasInAttackState = attackComponent.AttackState;
 
 			// Stop attack before changing weapon so that animations play correctly.
@@ -1509,8 +1513,6 @@ namespace DOL.GS
 			eActiveWeaponSlot previousActiveWeaponSlot = ActiveWeaponSlot;
 			base.SwitchWeapon(slot);
 
-			// Resume attack and notify `attackAction` (disables or reenables automatic ranged weapon switch for NPCs).
-			// This is to comply with scripted NPCs and doesn't happen if `StartAttackWithMeleeWeapon` was called instead.
 			if (wasInAttackState)
 			{
 				attackComponent.attackAction.OnForcedWeaponSwitch();