73deffa3bc3db59aaadacdd02555c248bb5fa0c8
Baptiste Marie <bm01@users.noreply.github.com>
2025-07-06 07:52:06 +0200

    Change savage buffs to drop health even when there isn't enough for the full cost Spells cost at least 1 but leave at 1 HP. Supposedly 1.65 accurate.

--- GameServer/ECS-Effects/SavageBuffECSEffect.cs
+++ GameServer/ECS-Effects/SavageBuffECSEffect.cs
@@ -5,8 +5,7 @@ namespace DOL.GS
 {
     public class SavageBuffECSGameEffect : StatBuffECSEffect
     {
-        public SavageBuffECSGameEffect(ECSGameEffectInitParams initParams)
-            : base(initParams) { }
+        public SavageBuffECSGameEffect(ECSGameEffectInitParams initParams) : base(initParams) { }
 
         public override void OnStartEffect()
         {
@@ -26,21 +25,25 @@ namespace DOL.GS
         public override void OnStopEffect()
         {
             ApplyBonus(Owner, (SpellHandler as AbstractSavageBuff).BonusCategory1, (SpellHandler as AbstractSavageBuff).Property1, SpellHandler.Spell.Value, Effectiveness, true);
-            OnHealthCost();           
+            OnHealthCost();
         }
 
         private void OnHealthCost()
         {
-            if (SpellHandler.Spell.Power != 0)
-            {
-                int cost = 0;
-                if (SpellHandler.Spell.Power < 0)
-                    cost = (int)(SpellHandler.Caster.MaxHealth * Math.Abs(SpellHandler.Spell.Power) * 0.01);
-                else
-                    cost = SpellHandler.Spell.Power;
-                if (Owner.Health > cost)
-                    Owner.ChangeHealth(Owner, eHealthChangeType.Spell, -cost);
-            }
+            if (SpellHandler.Spell.Power == 0)
+                return;
+
+            int cost;
+            int maxHp = SpellHandler.Caster.MaxHealth;
+
+            if (SpellHandler.Spell.Power < 0)
+                cost = (int) (maxHp * Math.Abs(SpellHandler.Spell.Power) * 0.01);
+            else
+                cost = SpellHandler.Spell.Power;
+
+            // Costs at least 1 HP, leaves at least 1 HP.
+            cost = Math.Min(Math.Max(1, cost), Owner.Health - 1);
+            Owner.ChangeHealth(Owner, eHealthChangeType.Spell, -cost);
         }
     }
 }
