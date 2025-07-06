OpenDAoC/OpenDAoC-Core

commit 6eb16f78ce01e27a5b9de37f4154b1375dc37153

Fix next melee attack delay after ranged attack abort / interrupt not working The way I understand past commits, the next melee attack should be delayed if the entity is interrupted or interrupts its aim by moving or switching weapons (but not if it switches weapon after releasing the arrow).

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -112,7 +112,7 @@ namespace DOL.GS
 
         public void OnStopAttack()
         {
-            // This method serves two purposes:
+            // This method serves three purposes:
             // * Delay the next melee attack a little after aborting a ranged attack, or getting interrupted.
             // * Ensure we're not buffering. For this, we need to explicitly set `_firstTick` to true,
             // because NPCs are able to stop and start an attack during the same tick, which prevents `CleanUp` from being called by `Tick`.
@@ -124,15 +124,15 @@ namespace DOL.GS
                 return;
 
             RangeAttackComponent rangeAttackComponent = _owner.rangeAttackComponent;
-            rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
 
-            if (rangeAttackComponent.RangedAttackState is eRangedAttackState.None)
-                return;
-
-            if (GameLoop.GameLoopTime - _nextMeleeTick > MINIMUM_MELEE_DELAY_AFTER_RANGED_ATTACK)
-                _nextMeleeTick = GameLoop.GameLoopTime + MINIMUM_MELEE_DELAY_AFTER_RANGED_ATTACK;
+            if (rangeAttackComponent.RangedAttackState is not eRangedAttackState.None)
+            {
+                long _nextDelayedMeleeTick = GameLoop.GameLoopTime + MINIMUM_MELEE_DELAY_AFTER_RANGED_ATTACK;
+                _nextMeleeTick = Math.Max(_nextMeleeTick, _nextDelayedMeleeTick);
+            }
 
             _nextRangedTick = GameLoop.GameLoopTime;
+            rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
             rangeAttackComponent.RangedAttackState = eRangedAttackState.None;
         }
 

GameServer/ECS-Components/Actions/PlayerAttackAction.cs
@@ -77,7 +77,10 @@ namespace DOL.GS
             bool stopAttack = false;
 
             if (_playerOwner.rangeAttackComponent.RangedAttackState is not eRangedAttackState.AimFireReload)
+            {
+                _playerOwner.rangeAttackComponent.RangedAttackState = eRangedAttackState.None;
                 stopAttack = true;
+            }
             else if (_playerOwner.Endurance < RangeAttackComponent.DEFAULT_ENDURANCE_COST)
             {
                 stopAttack = true;

commit 9af10b7dc7fb67dd21d5a752841a7920daf203fe

Fix last attack data being cleared immediately when stopping attack Caused by 6eb16f78ce01e27a5b9de37f4154b1375dc37153 This change makes setting `_firstTick` to true in `OnStopAttack` unreliable, but apparently all I needed to do was to always update `_nextRangedTick` instead.

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -48,12 +48,6 @@ namespace DOL.GS
 
         public bool Tick()
         {
-            if (!AttackComponent.AttackState)
-            {
-                CleanUp();
-                return false;
-            }
-
             if (_firstTick)
             {
                 _nextMeleeTick = Math.Max(_nextMeleeTick, GameLoop.GameLoopTime);
@@ -63,6 +57,13 @@ namespace DOL.GS
 
             if (!ShouldTick())
                 return true;
+
+            // This must be checked after `ShouldTick` so that the last attack data remains valid for the whole attack interval.
+            if (!AttackComponent.AttackState)
+            {
+                CleanUp();
+                return false;
+            }
 
             if (!CanPerformAction())
             {
@@ -74,7 +75,7 @@ namespace DOL.GS
             _leftWeapon = _owner.ActiveLeftWeapon;
             _effectiveness = _owner.Effectiveness;
 
-            if (_owner.ActiveWeaponSlot != eActiveWeaponSlot.Distance)
+            if (_owner.ActiveWeaponSlot is not eActiveWeaponSlot.Distance)
                 TickMeleeAttack();
             else
                 TickRangedAttack();
@@ -113,12 +114,11 @@ namespace DOL.GS
         public void OnStopAttack()
         {
             // This method serves three purposes:
+            // * Ensure we're not buffering (NPCs) or delaying (players) ranged attacks. For this, we need to update `_nextRangedTick` so that it's never higher than `GameLoop.GameLoopTime`.
             // * Delay the next melee attack a little after aborting a ranged attack, or getting interrupted.
-            // * Ensure we're not buffering. For this, we need to explicitly set `_firstTick` to true,
-            // because NPCs are able to stop and start an attack during the same tick, which prevents `CleanUp` from being called by `Tick`.
-            // * Ensure we're not delaying the next ranged attack's preparation. This means resetting `_nextRangedTick`.
+            // * Clean up ranged attack type and state.
 
-            _firstTick = true;
+            _nextRangedTick = GameLoop.GameLoopTime;
 
             if (_owner.ActiveWeaponSlot is not eActiveWeaponSlot.Distance)
                 return;
@@ -131,7 +131,6 @@ namespace DOL.GS
                 _nextMeleeTick = Math.Max(_nextMeleeTick, _nextDelayedMeleeTick);
             }
 
-            _nextRangedTick = GameLoop.GameLoopTime;
             rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
             rangeAttackComponent.RangedAttackState = eRangedAttackState.None;
         }