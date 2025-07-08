OpenDAoC/OpenDAoC-Core

commit 1a066cf60e9f03e78f2eaf199a61e4b4b9137acb

Solve race condition in `CastingComponent.InterruptCasting`

GameServer/ECS-Components/CastingComponent.cs
@@ -88,18 +88,16 @@ namespace DOL.GS
 
         public void InterruptCasting(bool moving)
         {
-            if (SpellHandler != null)
+            // A race condition can occur here.
+            if (SpellHandler?.IsInCastingPhase == true)
             {
-                if (SpellHandler.IsInCastingPhase)
-                {
-                    foreach (GamePlayer player in Owner.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
-                        player.Out.SendInterruptAnimation(Owner);
-                }
+                foreach (GamePlayer player in Owner.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
+                    player.Out.SendInterruptAnimation(Owner);
 
                 CancelFocusSpells(moving);
             }
 
-            ClearUpSpellHandlers();
+            ClearSpellHandlers();
         }
 
         public void CancelFocusSpells(bool moving)
@@ -107,7 +105,7 @@ namespace DOL.GS
             SpellHandler?.CancelFocusSpells(moving);
         }
 
-        public virtual void ClearUpSpellHandlers()
+        public virtual void ClearSpellHandlers()
         {
             QueuedSpellHandler = null;
             SpellHandler = null;

GameServer/ECS-Components/NpcCastingComponent.cs
@@ -21,12 +21,12 @@ namespace DOL.GS
             _npcOwner.ApplyInstantHarmfulSpellDelay();
         }
 
-        public override void ClearUpSpellHandlers()
+        public override void ClearSpellHandlers()
         {
             // Make sure NPCs don't start casting pending spells after being told to stop.
             _startSkillRequests.Clear(); // This also clears pending abilities.
             _npcOwner.ClearSpellsWaitingForLosCheck();
-            base.ClearUpSpellHandlers();
+            base.ClearSpellHandlers();
         }
 
         public bool IsAllowedToFollow(GameObject target)

GameServer/spells/AmnesiaSpellHandler.cs
@@ -36,7 +36,7 @@ namespace DOL.GS.Spells
 
             // Amnesia only affects normal spells and not song activation (still affects pulses from songs though)
             if (target.CurrentSpellHandler?.Spell.InstrumentRequirement == 0)
-                target.castingComponent.ClearUpSpellHandlers();
+                target.castingComponent.ClearSpellHandlers();
 
             target.rangeAttackComponent.AutoFireTarget = null;
 

commit 3995bf3a8bf089a476a6ab3912dde9b239f08ef9

Fix focus spells not cancelling when moving Regression caused by 1a066cf60e9f03e78f2eaf199a61e4b4b9137acb

GameServer/ECS-Components/CastingComponent.cs
@@ -88,11 +88,16 @@ namespace DOL.GS
 
         public void InterruptCasting(bool moving)
         {
-            // A race condition can occur here.
-            if (SpellHandler?.IsInCastingPhase == true)
+            // A race condition can happen here.
+            SpellHandler spellHandler = SpellHandler;
+
+            if (spellHandler != null)
             {
-                foreach (GamePlayer player in Owner.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
-                    player.Out.SendInterruptAnimation(Owner);
+                if (spellHandler.IsInCastingPhase)
+                {
+                    foreach (GamePlayer player in Owner.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
+                        player.Out.SendInterruptAnimation(Owner);
+                }
 
                 CancelFocusSpells(moving);
             }
@@ -102,7 +107,7 @@ namespace DOL.GS
 
         public void CancelFocusSpells(bool moving)
         {
-            SpellHandler?.CancelFocusSpells(moving);
+            SpellHandler?.CancelFocusSpells();
         }
 
         public virtual void ClearSpellHandlers()