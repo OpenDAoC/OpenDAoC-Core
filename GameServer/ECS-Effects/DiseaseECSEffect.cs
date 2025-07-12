d4a6fd5796ba52bc6abfbe5cce69429bbe3a9053
Baptiste Marie <bm01@users.noreply.github.com>
2025-07-12 08:13:25 +0200

    Re-enable Wild Arcana crits on disease spells (strength debuff and snare)

--- GameServer/ECS-Effects/DiseaseECSEffect.cs
+++ GameServer/ECS-Effects/DiseaseECSEffect.cs
@@ -4,8 +4,7 @@ namespace DOL.GS
 {
     public class DiseaseECSGameEffect : ECSGameSpellEffect
     {
-        public DiseaseECSGameEffect(ECSGameEffectInitParams initParams)
-            : base(initParams) { }
+        public DiseaseECSGameEffect(ECSGameEffectInitParams initParams) : base(initParams) { }
 
         public override void OnStartEffect()
         {
@@ -13,27 +12,13 @@ namespace DOL.GS
             double baseSpeedDebuff = 0.15;
             double baseStrDebuff = 0.075;
 
-            /*
-            if (SpellHandler.Caster.HasAbilityType(typeof(AtlasOF_WildArcanaAbility)))
-            {
-                if (Util.Chance(SpellHandler.Caster.SpellCriticalChance))
-                {
-                    double modSpeed = baseSpeedDebuff * 2;
-                    double modStr = baseStrDebuff * (1 + Util.Random(1, 10) * .1);
-                    if(Caster is GamePlayer c) c.Out.SendMessage($"Your {SpellHandler.Spell.Name} critically debuffs the enemy for {Math.Round(modStr - baseStrDebuff) * 100}% additional effect!", eChatType.CT_SpellResisted, eChatLoc.CL_SystemWindow);
-                    baseSpeedDebuff = modSpeed;
-                    baseStrDebuff = modStr;
-                }
-            }*/
-
-            Owner.BuffBonusMultCategory1.Set((int) eProperty.MaxSpeed, this, 1.0 - baseSpeedDebuff);
-            Owner.BuffBonusMultCategory1.Set((int) eProperty.Strength, this, 1.0 - baseStrDebuff);
+            Owner.BuffBonusMultCategory1.Set((int) eProperty.MaxSpeed, this, 1.0 - baseSpeedDebuff * Effectiveness);
+            Owner.BuffBonusMultCategory1.Set((int) eProperty.Strength, this, 1.0 - baseStrDebuff * Effectiveness);
             Owner.OnMaxSpeedChange();
 
             // "You are diseased!"
             // "{0} is diseased!"
             OnEffectStartsMsg(true, true, true);
-            //Owner.StartInterruptTimer(Owner.SpellInterruptDuration, AttackData.eAttackType.Spell, SpellHandler.Caster);
 
             if (Owner is GameNPC npcOwner)
             {
