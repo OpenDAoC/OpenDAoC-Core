OpenDAoC/OpenDAoC-Core

commit 001a7829c85eca233389d88327343b68ae548045

`EffectListComponent`: Fix concurrency issue on `_effects` Can call `Remove` on it from `TickEffect`.

GameServer/ECS-Components/EffectListComponent.cs
@@ -21,6 +21,7 @@ namespace DOL.GS
         // Pending effects.
         private ConcurrentQueue<ECSGameEffect> _pendingEffects = new();     // Queue for pending effects to be processed in the next tick.
         private int _pendingEffectCount;                                    // Number of pending effects to be processed.
+        private List<eEffect> _effectTypesToRemove = new();                 // List of effect types to be removed from the effects dictionary.
 
         // Concentration.
         private List<ECSGameSpellEffect> _concentrationEffects = new(20);   // List of concentration effects currently active on the player.
@@ -61,8 +62,18 @@ namespace DOL.GS
                 {
                     List<ECSGameEffect> list = pair.Value;
 
+                    // Reverse loop to allow removing effects in `TickEffect` while iterating.
+                    // To remove from `_effects`, we use `_effectTypesToRemove` as an intermediate list.
                     for (int i = list.Count - 1; i >= 0; i--)
                         TickEffect(list[i]);
+                }
+
+                if (_effectTypesToRemove.Count > 0)
+                {
+                    for (int i = 0; i < _effectTypesToRemove.Count; i++)
+                        _effects.Remove(_effectTypesToRemove[i]);
+
+                    _effectTypesToRemove.Clear();
                 }
             }
 
@@ -830,7 +841,7 @@ namespace DOL.GS
                         _effectIdToEffect.Remove(effect.Icon);
 
                         if (existingEffects.Count == 0)
-                            _effects.Remove(effect.EffectType);
+                            _effectTypesToRemove.Add(effect.EffectType);
 
                         HandleConcentration(effect as ECSGameSpellEffect);
                         return RemoveEffectResult.Removed;

commit 9f897adcc9cabd58a35ba4bc53f7bd97709dfa84

Fix cancelling sprint not correctly updating max speed Caused by 001a7829c85eca233389d88327343b68ae548045. Fixed by checking Count on the list instead of ContainsKey. Lists now stays empty for a server tick before being removed. May have affected other effects or abilities.

GameServer/ECS-Components/EffectListComponent.cs
@@ -254,7 +254,7 @@ namespace DOL.GS
 
         public bool ContainsEffectForEffectType(eEffect effectType)
         {
-            return _effects.ContainsKey(effectType);
+            return _effects.TryGetValue(effectType, out var effects) && effects.Count > 0;
         }
 
         public void CancelAll()