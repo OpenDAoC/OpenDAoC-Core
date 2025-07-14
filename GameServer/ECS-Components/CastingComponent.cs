OpenDAoC/OpenDAoC-Core

commit 2ef2170a633b44c9a7944006de77557dabd62763

Add missing spell cancel message when moving Was probably inadverenetly removed at some point. Moved focus spell cancel message from `SpellHandler.CancelFocusSpells` to `CastingComponent.SendSpellCancelMessage`

GameServer/ECS-Components/CastingComponent.cs
@@ -99,16 +99,18 @@ namespace DOL.GS
                         player.Out.SendInterruptAnimation(Owner);
                 }
 
-                CancelFocusSpells(moving);
+                bool focusSpell = spellHandler.Spell.IsFocus;
+
+                if (focusSpell)
+                    spellHandler.CancelFocusSpells();
+
+                SendSpellCancelMessage(moving, focusSpell);
             }
 
             ClearSpellHandlers();
         }
 
-        public void CancelFocusSpells(bool moving)
-        {
-            SpellHandler?.CancelFocusSpells();
-        }
+        protected virtual void SendSpellCancelMessage(bool moving, bool focusSpell) { }
 
         public virtual void ClearSpellHandlers()
         {

GameServer/gameobjects/GameLiving.cs
@@ -1398,7 +1398,7 @@ namespace DOL.GS
 
             // Don't cancel offensive focus spell
             if (ad.AttackType != eAttackType.Spell)
-                castingComponent.CancelFocusSpells(false);
+                castingComponent.InterruptCasting(false);
         }
 
 		/// <summary>

GameServer/ECS-Components/PlayerCastingComponent.cs
@@ -70,5 +70,18 @@ namespace DOL.GS
 
             return true;
         }
+
+        protected override void SendSpellCancelMessage(bool moving, bool focusSpell)
+        {
+            if (focusSpell)
+            {
+                if (moving)
+                    _playerOwner.Out.SendMessage("You move and interrupt your focus!", eChatType.CT_Important, eChatLoc.CL_SystemWindow);
+                else
+                    _playerOwner.Out.SendMessage($"You lose your focus on your spell.", eChatType.CT_SpellExpires, eChatLoc.CL_SystemWindow);
+            }
+            else if (moving)
+                _playerOwner.Out.SendMessage(LanguageMgr.GetTranslation(_playerOwner.Client, "SpellHandler.CasterMove"), eChatType.CT_Important, eChatLoc.CL_SystemWindow);
+        }
     }
 }

GameServer/ECS-Effects/DamageShieldECSEffect.cs
@@ -84,7 +84,7 @@ namespace DOL.GS
                     return Interval;
                 }
 
-                (_pulseEffect.SpellHandler as SpellHandler).CancelFocusSpells(false);
+                (_pulseEffect.SpellHandler as SpellHandler).CancelFocusSpells();
                 return 0;
             }
         }

GameServer/spells/FocusShellHandler.cs
@@ -38,7 +38,7 @@ namespace DOL.GS.Spells
 					Caster.ActivePulseSpells.TryGetValue(m_spell.SpellType, out Spell currentSpell);
 
 					if (currentSpell != null && currentSpell == Spell)
-						CancelFocusSpells(false);
+						CancelFocusSpells();
 
 					FSTarget = selectedTarget as GamePlayer;
 				}
@@ -95,7 +95,7 @@ namespace DOL.GS.Spells
 		private void CancelSpell(DOLEvent e, object sender, EventArgs args)
 		{
 			//Send the cancel signal, we need to use the faster as the sender!
-			CancelFocusSpells(false);
+			CancelFocusSpells();
 		}
 
 		private void OnAttacked(DOLEvent e, object sender, EventArgs args)

GameServer/spells/RampingDamageFocus.cs
@@ -45,7 +45,7 @@ namespace DOL.GS.Spells
 			else
 			{
 				MessageToCaster("You do not have enough power and your spell was canceled.", eChatType.CT_SpellExpires);
-				CancelFocusSpells(false);
+				CancelFocusSpells();
 				effect.Cancel(false);
 			}
 		}

GameServer/spells/SpellHandler.cs
@@ -456,7 +456,7 @@ namespace DOL.GS.Spells
 				}
 			}
 
-			CancelFocusSpells(false);
+			CancelFocusSpells();
 			_quickcast = EffectListService.GetAbilityEffectOnTarget(m_caster, eEffect.QuickCast) as QuickCastECSGameEffect;
 
 			if (IsQuickCasting)
@@ -1981,7 +1981,7 @@ namespace DOL.GS.Spells
 			{
 				if (Spell.IsFocus && (!Target.IsAlive || !Caster.IsWithinRadius(Target, Spell.Range)))
 				{
-					CancelFocusSpells(false);
+					CancelFocusSpells();
 					return false;
 				}
 
@@ -2560,26 +2560,16 @@ namespace DOL.GS.Spells
 			}
 		}
 
-		public virtual void CancelFocusSpells(bool moving)
+		public virtual void CancelFocusSpells()
 		{
 			CastState = eCastState.Cleanup;
-			bool cancelled = false;
 
 			foreach (ECSGameSpellEffect pulseSpell in Caster.effectListComponent.GetSpellEffects(eEffect.Pulse))
 			{
 				if (!pulseSpell.SpellHandler.Spell.IsFocus)
 					continue;
 
-				if (pulseSpell.Stop())
-					cancelled = true;
-			}
-
-			if (cancelled)
-			{
-				if (moving)
-					MessageToCaster("You move and interrupt your focus!", eChatType.CT_Important);
-				else
-					MessageToCaster($"You lose your focus on your {Spell.Name} spell.", eChatType.CT_SpellExpires);
+				pulseSpell.Stop();
 			}
 		}
 

commit d00187721d20ca9c99eab024e98a7a301bf8983e

Fix Necromancer pet clearing its pending spells when attacking in melee Regression caused by 2ef2170a633b44c9a7944006de77557dabd62763

GameServer/ECS-Components/CastingComponent.cs
@@ -102,15 +102,24 @@ namespace DOL.GS
                         player.Out.SendInterruptAnimation(Owner);
                 }
 
-                bool focusSpell = spellHandler.Spell.IsFocus;
-
-                if (focusSpell)
-                    spellHandler.CancelFocusSpells();
-
-                SendSpellCancelMessage(moving, focusSpell);
+                // Only send a spell cancel message if we're not cancelling a focus spell (already handled by `CancelFocusSpells`).
+                if (!CancelFocusSpells(moving))
+                    SendSpellCancelMessage(moving, false);
             }
 
             ClearSpellHandlers();
+        }
+
+        public bool CancelFocusSpells(bool moving)
+        {
+            SpellHandler spellHandler = SpellHandler;
+
+            if (spellHandler == null || !spellHandler.Spell.IsFocus)
+                return false;
+
+            spellHandler.CancelFocusSpells();
+            SendSpellCancelMessage(moving, true);
+            return true;
         }
 
         protected virtual void SendSpellCancelMessage(bool moving, bool focusSpell) { }

GameServer/gameobjects/GameLiving.cs
@@ -1430,7 +1430,7 @@ namespace DOL.GS
 
             // Don't cancel offensive focus spell.
             if (ad.AttackType is not eAttackType.Spell)
-                castingComponent.InterruptCasting(false);
+                castingComponent.CancelFocusSpells(false);
         }
 
         /// <summary>