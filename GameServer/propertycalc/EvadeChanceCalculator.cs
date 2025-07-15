a108e7a870d593b18dee3eb5dcbe16c2b9e65901
Baptiste Marie <bm01@users.noreply.github.com>
2025-07-15 07:39:36 +0200

    Revert evade formula to DoL / Uthgard's Evade is apparently not supposed to add a flat "5 * evade level" to the base evade rate, but instead should act as a scaling factor to quickness + dexterity. Fixes low evade levels being stronger than intended.

--- GameServer/propertycalc/EvadeChanceCalculator.cs
+++ GameServer/propertycalc/EvadeChanceCalculator.cs
@@ -1,22 +1,3 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
-
 namespace DOL.GS.PropertyCalc
 {
     /// <summary>
@@ -38,7 +19,7 @@ namespace DOL.GS.PropertyCalc
             if (living is GamePlayer player)
             {
                 if (player.HasAbility(Abilities.Evade))
-                    chance += (int) ((((player.Dexterity + player.Quickness) / 2 - 50) * 0.05 + player.GetAbilityLevel(Abilities.Evade) * 5) * 10);
+                    chance += (900 + player.Quickness + player.Dexterity) * player.GetAbilityLevel(Abilities.Evade) * 5 / 100;
 
                 chance += player.BaseBuffBonusCategory[property] * 10;
                 chance += player.SpecBuffBonusCategory[property] * 10;
