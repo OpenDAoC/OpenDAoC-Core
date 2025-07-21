OpenDAoC/OpenDAoC-Core

commit bf2176d89a03a8dd0394ddcff3c4df36e3bc3822

Fix style snares not being affected by snare decay

GameServer/ECS-Effects/ECSGameSpellEffect.cs
@@ -29,7 +29,7 @@ namespace DOL.GS
             EffectType = EffectService.GetEffectFromSpell(SpellHandler.Spell);
             PulseFreq = spell.Frequency;
 
-            if (spell.SpellType is eSpellType.SpeedDecrease or eSpellType.UnbreakableSpeedDecrease)
+            if (spell.SpellType is eSpellType.SpeedDecrease or eSpellType.StyleSpeedDecrease or eSpellType.UnbreakableSpeedDecrease)
             {
                 PulseFreq = 250;
                 NextTick = 1 + Duration / 2 + StartTick + PulseFreq;

GameServer/ECS-Services/EffectListService.cs
@@ -234,7 +234,7 @@ namespace DOL.GS
                             }
                         }
                     }
-                    else if (spellEffect is not ECSImmunityEffect && spellEffect.EffectType is not eEffect.Pulse && spellEffect.SpellHandler.Spell.SpellType is eSpellType.SpeedDecrease)
+                    else if (spellEffect is not ECSImmunityEffect && spellEffect.EffectType is not eEffect.Pulse && spellEffect.SpellHandler.Spell.SpellType is eSpellType.SpeedDecrease or eSpellType.StyleSpeedDecrease)
                     {
                         double factor = 2.0 - (spellEffect.Duration - spellEffect.GetRemainingTimeForClient()) / (spellEffect.Duration * 0.5);
 

commit 8873981eda6b25ce74e9b8421258a709e5c3e7e4

Fix style snares adding an immunity icon when expiring Caused by bf2176d89a03a8dd0394ddcff3c4df36e3bc3822

GameServer/ECS-Effects/ECSGameSpellEffect.cs
@@ -32,9 +32,7 @@ namespace DOL.GS
             {
                 PulseFreq = 250;
                 NextTick = 1 + Duration / 2 + StartTick + PulseFreq;
-
-                if (!spell.Name.Equals("Prevent Flight", StringComparison.OrdinalIgnoreCase) && !spell.IsFocus)
-                    TriggersImmunity = true;
+                TriggersImmunity = spell.SpellType is not eSpellType.StyleSpeedDecrease && !spell.IsFocus && !spell.Name.Equals("Prevent Flight", StringComparison.OrdinalIgnoreCase);
             }
             else if (spell.IsConcentration)
             {