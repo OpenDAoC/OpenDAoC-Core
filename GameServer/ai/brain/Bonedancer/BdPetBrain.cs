OpenDAoC/OpenDAoC-Core

commit 232535c939dd10a90cb9d4fe3752d92d4f2bfa9c

Introduce `AttackerTracker` Moved relevant code from `AttackComponent` Now counts melee attackers.

GameServer/ai/brain/Bonedancer/BdPetBrain.cs
@@ -63,7 +63,7 @@ namespace DOL.AI.Brain
         /// </summary>
         public override bool CheckFormation(ref int x, ref int y, ref int z)
         {
-            if (!Body.IsCasting && !Body.attackComponent.AttackState && Body.attackComponent.Attackers.IsEmpty)
+            if (!Body.IsCasting && !Body.attackComponent.AttackState && Body.attackComponent.AttackerTracker.Count > 0)
             {
                 GameNPC commander = (GameNPC)Owner;
                 double heading = commander.Heading * Point2D.HEADING_TO_RADIAN;

GameServer/ECS-Components/AttackComponent.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +21,6 @@ namespace DOL.GS
 {
     public class AttackComponent : IServiceObject
     {
-        public const int CHECK_ATTACKERS_INTERVAL = 1000;
         public const double INHERENT_WEAPON_SKILL = 15.0;
         public const double INHERENT_ARMOR_FACTOR = 12.5;
 
@@ -30,56 +28,10 @@ namespace DOL.GS
         public WeaponAction weaponAction; // This represents the current weapon action, which may become outdated when resolving ranged attacks.
         public AttackAction attackAction;
         public ServiceObjectId ServiceObjectId { get; set; } = new(ServiceObjectType.AttackComponent);
+        public AttackerTracker AttackerTracker { get; private set; }
 
-        /// <summary>
-        /// Returns the list of attackers
-        /// </summary>
-        public ConcurrentDictionary<GameLiving, long> Attackers { get; } = new();
-
-        private AttackersCheckTimer _attackersCheckTimer;
         private BlockRoundHandler _blockRoundHandler;
-
-        public void AddAttacker(GameLiving attacker, long duration)
-        {
-            if (attacker == owner)
-                return;
-
-            if (!_attackersCheckTimer.IsAlive)
-            {
-                lock (_attackersCheckTimer.Lock)
-                {
-                    if (!_attackersCheckTimer.IsAlive)
-                    {
-                       _attackersCheckTimer.Interval = CHECK_ATTACKERS_INTERVAL;
-                       _attackersCheckTimer.Start();
-                    }
-                }
-            }
-
-            if (duration <= 0)
-                duration = Properties.SPELL_INTERRUPT_DURATION;
-
-            Attackers.AddOrUpdate(attacker, Add, Update, GameLoop.GameLoopTime + duration);
-
-            static long Add(GameLiving key, long arg)
-            {
-                return arg;
-            }
-
-            static long Update(GameLiving key, long oldValue, long arg)
-            {
-                return arg;
-            }
-        }
-
-        /// <summary>
-        /// The target that was passed when 'StartAttackRequest' was called and the request accepted.
-        /// </summary>
         private GameObject _startAttackTarget;
-
-        /// <summary>
-        /// Actually a boolean. Use 'StartAttackRequested' to preserve thread safety.
-        /// </summary>
         private int _startAttackRequested;
 
         public bool StartAttackRequested
@@ -92,7 +44,7 @@ namespace DOL.GS
         {
             this.owner = owner;
             attackAction = AttackAction.Create(owner);
-            _attackersCheckTimer = AttackersCheckTimer.Create(owner);
+            AttackerTracker = new(owner);
             _blockRoundHandler = new(owner);
         }
 
@@ -113,6 +65,12 @@ namespace DOL.GS
 
             if (!attackAction.Tick())
                 ServiceObjectStore.Remove(this);
+        }
+
+        public void AddAttacker(AttackData attackData)
+        {
+            long expireTime = GameLoop.GameLoopTime + (attackData.Interval > 0 ? attackData.Interval : Properties.SPELL_INTERRUPT_DURATION);
+            AttackerTracker.AddOrUpdate(attackData.Attacker, attackData.IsMeleeAttack, expireTime);
         }
 
         /// <summary>
@@ -1124,7 +1082,7 @@ namespace DOL.GS
             }
 
             // Add ourselves to the target's attackers list before going further.
-            ad.Target.attackComponent.AddAttacker(owner, ad.Interval);
+            ad.Target.attackComponent.AddAttacker(ad);
 
             // Calculate our attack result and attack damage.
             ad.AttackResult = ad.Target.attackComponent.CalculateEnemyAttackResult(action, ad, weapon, ref effectiveness);
@@ -1886,8 +1844,8 @@ namespace DOL.GS
                 if (leftHand != null)
                     shieldSize = leftHand.Type_Damage;
 
-                if (Attackers.Count > shieldSize)
-                    guardchance *= shieldSize / (double) Attackers.Count;
+                if (AttackerTracker.Count > shieldSize)
+                    guardchance *= shieldSize / (double) AttackerTracker.Count;
 
                 if (ad.AttackType == AttackData.eAttackType.MeleeDualWield)
                     guardchance /= 2;
@@ -1901,8 +1859,8 @@ namespace DOL.GS
                     if (parrychance > 0.99)
                         parrychance = 0.99;
 
-                    if (Attackers.Count > 1)
-                        parrychance /= Attackers.Count / 2;
+                    if (AttackerTracker.Count > 1)
+                        parrychance /= AttackerTracker.Count / 2;
                 }
 
                 if (Util.ChanceDouble(guardchance))
@@ -1929,8 +1887,8 @@ namespace DOL.GS
                     if (parrychance > 0.99)
                         parrychance = 0.99;
 
-                    if (Attackers.Count > 1)
-                        parrychance /= Attackers.Count / 2;
+                    if (AttackerTracker.Count > 1)
+                        parrychance /= AttackerTracker.Count / 2;
                 }
 
                 if (Util.ChanceDouble(parrychance))
@@ -2085,7 +2043,7 @@ namespace DOL.GS
 
                 if (ad.IsMeleeAttack)
                 {
-                    double parryChance = owner.TryParry(ad, lastAttackData, Attackers.Count);
+                    double parryChance = owner.TryParry(ad, lastAttackData, AttackerTracker.Count);
                     ad.ParryChance = parryChance * 100;
                     double parryRoll;
 
@@ -2606,7 +2564,7 @@ namespace DOL.GS
             {
                 // 1.33 per level difference.
                 missChance -= (ad.Attacker.EffectiveLevel - owner.EffectiveLevel) * (1 + 1 / 3.0);
-                missChance -= Math.Max(0, Attackers.Count - 1) * Properties.MISSRATE_REDUCTION_PER_ATTACKERS;
+                missChance -= Math.Max(0, AttackerTracker.Count - 1) * Properties.MISSRATE_REDUCTION_PER_ATTACKERS;
             }
 
             // Weapon and armor bonuses.
@@ -2885,85 +2843,6 @@ namespace DOL.GS
                     _decrementBlockRoundCount();
                     return 0;
                 }
-            }
-        }
-
-        public class StandardAttackersCheckTimer : AttackersCheckTimer
-        {
-            public StandardAttackersCheckTimer(GameObject owner) : base(owner) { }
-
-            protected override int OnTick(ECSGameTimer timer)
-            {
-                foreach (var pair in _owner.attackComponent.Attackers)
-                    TryRemoveAttacker(pair);
-
-                return base.OnTick(timer);
-            }
-        }
-
-        public class EpicNpcAttackersCheckTimer : AttackersCheckTimer
-        {
-            private IGameEpicNpc _epicNpc;
-
-            public EpicNpcAttackersCheckTimer(GameObject owner) : base(owner)
-            {
-                _epicNpc = owner as IGameEpicNpc;
-            }
-
-            protected override int OnTick(ECSGameTimer timer)
-            {
-                // Update `ArmorFactorScalingFactor`.
-                double armorFactorScalingFactor = _epicNpc.DefaultArmorFactorScalingFactor;
-                int petCount = 0;
-
-                foreach (var pair in _owner.attackComponent.Attackers)
-                {
-                    if (TryRemoveAttacker(pair))
-                        continue;
-
-                    if (pair.Key is GamePlayer)
-                        armorFactorScalingFactor -= 0.04;
-                    else if (pair.Key is GameSummonedPet && petCount <= _epicNpc.ArmorFactorScalingFactorPetCap)
-                    {
-                        armorFactorScalingFactor -= 0.01;
-                        petCount++;
-                    }
-
-                    if (armorFactorScalingFactor < 0.4)
-                    {
-                        armorFactorScalingFactor = 0.4;
-                        break;
-                    }
-                }
-
-                _epicNpc.ArmorFactorScalingFactor = armorFactorScalingFactor;
-                return base.OnTick(timer);
-            }
-        }
-
-        public abstract class AttackersCheckTimer : ECSGameTimerWrapperBase
-        {
-            public readonly Lock Lock = new();
-            protected GameLiving _owner;
-
-            public AttackersCheckTimer(GameObject owner) : base(owner)
-            {
-                _owner = owner as GameLiving;
-            }
-
-            public static AttackersCheckTimer Create(GameLiving owner)
-            {
-                return owner is IGameEpicNpc ? new EpicNpcAttackersCheckTimer(owner) : new StandardAttackersCheckTimer(owner);
-            }
-
-            protected override int OnTick(ECSGameTimer timer)
-            {
-                return _owner.attackComponent.Attackers.IsEmpty ? 0 : CHECK_ATTACKERS_INTERVAL;
-            }
-
-            protected bool TryRemoveAttacker(in KeyValuePair<GameLiving, long> pair)
-            {
-                return pair.Value < GameLoop.GameLoopTime && _owner.attackComponent.Attackers.TryRemove(pair);
             }
         }
     }

GameServer/ECS-Effects/CharmECSEffect.cs
@@ -77,7 +77,7 @@ namespace DOL.GS
                 }
 
                 // Remove NPC with new brain from all attackers aggro list.
-                foreach (GameLiving attacker in charmNpc.attackComponent.Attackers.Keys)
+                foreach (GameLiving attacker in charmNpc.attackComponent.AttackerTracker.Attackers)
                 {
                     if (attacker is GameNPC npcAttacker && npcAttacker.Brain is IOldAggressiveBrain attackerAggroBrain)
                     {

GameServer/commands/gmcommands/GMinfo.cs
@@ -109,8 +109,9 @@ namespace DOL.GS.Commands
 					info.Add(" + Size " + target.Size);
 					info.Add(string.Format(" + Flags: {0} (0x{1})", ((GameNPC.eFlags)target.Flags).ToString("G"), target.Flags.ToString("X")));
 					info.Add(" ");
-					info.Add(" + Attacker Count: " + target.attackComponent.Attackers.Count);
-					
+					info.Add(" + Attacker count: " + target.attackComponent.AttackerTracker.Count);
+					info.Add(" + Melee attacker count: " + target.attackComponent.AttackerTracker.MeleeCount);
+
 					IOldAggressiveBrain aggroBrain = target.Brain as IOldAggressiveBrain;
 					if (aggroBrain != null)
 					{
@@ -245,12 +246,12 @@ namespace DOL.GS.Commands
 						}
 					}
 
-					if (target.attackComponent.Attackers != null && !target.attackComponent.Attackers.IsEmpty)
+					if (target.attackComponent.AttackerTracker.Count > 0)
 					{
 						info.Add("");
 						info.Add("Attacker List:");
 
-						foreach (GameObject attacker in target.attackComponent.Attackers.Keys)
+						foreach (GameObject attacker in target.attackComponent.AttackerTracker.Attackers)
 							info.Add(attacker.Name);
 					}
 

GameServer/commands/gmcommands/mob.cs
@@ -3000,12 +3000,12 @@ namespace DOL.GS.Commands
 				}
 			}
 
-			if (targetMob.attackComponent.Attackers != null && !targetMob.attackComponent.Attackers.IsEmpty)
+			if (targetMob.attackComponent.AttackerTracker.Count > 0)
 			{
 				text.Add("");
 				text.Add("Attacker List:");
 
-				foreach (GameLiving attacker in targetMob.attackComponent.Attackers.Keys)
+				foreach (GameLiving attacker in targetMob.attackComponent.AttackerTracker.Attackers)
 					text.Add(attacker.Name);
 			}
 

GameServer/gameobjects/CharacterClasses/Albion/CharacterClassNecromancer.cs
@@ -80,7 +80,7 @@ namespace DOL.GS
                     return true;
 
                 // Necromancer has become a shade. Have any previous NPC attacker aggro the pet now, as they can't attack the necromancer any longer.
-                foreach (GameObject attacker in Player.attackComponent.Attackers.Keys)
+                foreach (GameObject attacker in Player.attackComponent.AttackerTracker.Attackers)
                 {
                     if (attacker is not GameNPC npcAttacker || !npcAttacker.attackComponent.AttackState || npcAttacker.Brain is not IOldAggressiveBrain npcAttackerBrain)
                         continue;

GameServer/gameobjects/GameLiving.cs
@@ -1422,7 +1422,7 @@ namespace DOL.GS
 				if (this is GameNPC gameNpc && ActiveWeaponSlot is eActiveWeaponSlot.Distance && IsWithinRadius(ad.Attacker, 150))
 					gameNpc.StartAttackWithMeleeWeapon(ad.Attacker);
 
-				attackComponent.AddAttacker(ad.Attacker, ad.Interval);
+				attackComponent.AddAttacker(ad);
 
 				if (ad.SpellHandler == null || (ad.SpellHandler != null && ad.SpellHandler is not DoTSpellHandler))
 				{
@@ -1684,7 +1684,7 @@ namespace DOL.GS
 			{
 				EnemyHealedEventArgs args = new(this, changeSource, healthChangeType, healthChanged);
 
-				foreach (GameLiving attacker in attackComponent.Attackers.Keys)
+				foreach (GameLiving attacker in attackComponent.AttackerTracker.Attackers)
 				{
 					if (attacker is not GameLiving attackerLiving)
 						continue;
@@ -1752,13 +1752,9 @@ namespace DOL.GS
 			try
 			{
 				attackComponent.StopAttack();
-
-				if (killer is GameLiving livingKiller)
-					attackComponent.Attackers.TryAdd(livingKiller, long.MaxValue);
-
 				List<GamePlayer> playerAttackers = new();
 
-				foreach (GameObject attacker in attackComponent.Attackers.Keys)
+				foreach (GameObject attacker in attackComponent.AttackerTracker.Attackers)
 				{
 					if (attacker is not GameLiving livingAttacker)
 						continue;
@@ -1772,7 +1768,7 @@ namespace DOL.GS
 
 						if (player != null)
 						{
-							if (!attackComponent.Attackers.ContainsKey(player))
+							if (!attackComponent.AttackerTracker.ContainsAttacker(player))
 							{
 								if (!playerAttackers.Contains(player))
 									playerAttackers.Add(player);
@@ -1807,7 +1803,7 @@ namespace DOL.GS
 				foreach (Quests.DataQuest q in DataQuestList)
 					q.Notify(GameLivingEvent.Dying, this, new DyingEventArgs(killer, playerAttackers));
 
-				attackComponent.Attackers.Clear();
+				attackComponent.AttackerTracker.Clear();
 
 				// clear all of our targets
 				rangeAttackComponent.AutoFireTarget = null;
@@ -3418,7 +3414,7 @@ namespace DOL.GS
 
 			attackComponent.StopAttack();
 
-			foreach (GameObject attacker in attackComponent.Attackers.Keys)
+			foreach (GameObject attacker in attackComponent.AttackerTracker.Attackers)
 			{
 				if (attacker is not GameLiving attackerLiving)
 					continue;
@@ -3426,7 +3422,7 @@ namespace DOL.GS
 				attackerLiving.EnemyKilled(this);
 			}
 
-			attackComponent.Attackers.Clear();
+			attackComponent.AttackerTracker.Clear();
 			StopHealthRegeneration();
 			StopPowerRegeneration();
 			StopEnduranceRegeneration();

GameServer/gameobjects/GamePlayer.cs
@@ -6499,7 +6499,7 @@ namespace DOL.GS
                     if (player == this)
                         continue;
 
-                    if (enemy.attackComponent.Attackers.ContainsKey(player))
+                    if (enemy.attackComponent.AttackerTracker.ContainsAttacker(player))
                         continue;
 
                     if (IsWithinRadius(player, WorldMgr.MAX_EXPFORKILL_DISTANCE))

commit ab1232d01ee8267d931c4d3ff60aa2e0fa9d7f8f

Fix BD pet formations Regression caused by 232535c939dd10a90cb9d4fe3752d92d4f2bfa9c

GameServer/ai/brain/Bonedancer/BdPetBrain.cs
@@ -63,7 +63,7 @@ namespace DOL.AI.Brain
         /// </summary>
         public override bool CheckFormation(ref int x, ref int y, ref int z)
         {
-            if (!Body.IsCasting && !Body.attackComponent.AttackState && Body.attackComponent.AttackerTracker.Count > 0)
+            if (!Body.IsCasting && !Body.attackComponent.AttackState && Body.attackComponent.AttackerTracker.Count == 0)
             {
                 GameNPC commander = (GameNPC)Owner;
                 double heading = commander.Heading * Point2D.HEADING_TO_RADIAN;