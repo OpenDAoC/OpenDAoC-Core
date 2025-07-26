799cd6432bd61d7243fed8bf12bec6a5bac31e43
Baptiste Marie <bm01@users.noreply.github.com>
2025-07-26 10:59:05 +0200

    Reduce memory footprint of `PropertyIndexer`

--- GameServer/propertycalc/IPropertyIndexer.cs
+++ GameServer/propertycalc/IPropertyIndexer.cs
@@ -1,12 +1,8 @@
-using System;
-
 namespace DOL.GS.PropertyCalc
 {
     public interface IPropertyIndexer
     {
         void Clear();
         int this[eProperty index] { get; set; }
-        [Obsolete("Use the eProperty indexer instead. This will be removed in a future version.")]
-        int this[int index] { get; set; }
     }
 }
--- GameServer/propertycalc/PropertyIndexer.cs
+++ GameServer/propertycalc/PropertyIndexer.cs
@@ -1,28 +1,40 @@
-using System;
-using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace DOL.GS.PropertyCalc
 {
     public sealed class PropertyIndexer : IPropertyIndexer
     {
+        private readonly Lock _lock = new();
+        private readonly Dictionary<eProperty, int> _properties = new();
+
         public void Clear()
         {
-            m_propDict.Clear();
+            lock (_lock)
+            {
+                _properties?.Clear();
+            }
         }
-
-        private readonly ConcurrentDictionary<eProperty, int> m_propDict = new();
 
         public int this[eProperty index]
         {
-            get => m_propDict.TryGetValue(index, out int value) ? value : 0;
-            set => m_propDict[index] = value;
-        }
-
-        [Obsolete("Use the eProperty indexer instead. This will be removed in a future version.")]
-        public int this[int index]
-        {
-            get => this[(eProperty) index];
-            set => this[(eProperty) index] = value;
+            get
+            {
+                lock (_lock)
+                {
+                    return _properties.TryGetValue(index, out int value) ? value : 0;
+                }
+            }
+            set
+            {
+                lock (_lock)
+                {
+                    if (value == 0)
+                        _properties.Remove(index);
+                    else
+                        _properties[index] = value;
+                }
+            }
         }
     }
 }
