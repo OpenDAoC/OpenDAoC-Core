2f56acc56117dfdfa491c6ef337785101e58accf
Baptiste Marie <bm01@users.noreply.github.com>
2025-07-28 15:19:35 +0200

    Fix `Util.IsInRange` when z == 0

--- GameServer/gameutils/Util.cs
+++ GameServer/gameutils/Util.cs
@@ -158,9 +158,6 @@ namespace DOL.GS
 
         public static bool IsInRange(this Vector3 value, Vector3 target, float range)
         {
-            // SH: Removed Z checks when one of the two Z values is zero(on ground)
-            if (value.Z == 0 || target.Z == 0)
-                return Vector2.DistanceSquared(value.ToVector2(), target.ToVector2()) <= range * range;
             return Vector3.DistanceSquared(value, target) <= range * range;
         }
 
