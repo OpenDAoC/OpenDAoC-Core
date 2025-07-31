OpenDAoC/OpenDAoC-Core

commit 4106600bd6b1a82eb3f930f6e671eb651e526e87

Fix taunt spells not interrupting players. The stealth check on top of incorrect was also redundant. Non-damaging spells don't unstealth. See `GameLiving.OnAttackedByEnemy`.

GameServer/spells/TauntSpellHandler.cs
@@ -3,79 +3,49 @@ using DOL.AI.Brain;
 
 namespace DOL.GS.Spells
 {
-	/// <summary>
-	/// Summary description for TauntSpellHandler.
-	/// </summary>
-	[SpellHandler(eSpellType.Taunt)]
-	public class TauntSpellHandler : SpellHandler
-	{
-		/// <summary>
-		/// called after normal spell cast is completed and effect has to be started
-		/// </summary>
-		public override void FinishSpellCast(GameLiving target)
-		{
-			Caster.Mana -= PowerCost(target);
-			base.FinishSpellCast(target);
-		}
+    [SpellHandler(eSpellType.Taunt)]
+    public class TauntSpellHandler : SpellHandler
+    {
+        public TauntSpellHandler(GameLiving caster, Spell spell, SpellLine spellLine) : base(caster, spell, spellLine) { }
 
-		public override void OnDirectEffect(GameLiving target)
-		{
-			if (target == null || !target.IsAlive || target.ObjectState != GameObject.eObjectState.Active)
-				return;
+        public override void FinishSpellCast(GameLiving target)
+        {
+            Caster.Mana -= PowerCost(target);
+            base.FinishSpellCast(target);
+        }
 
-			// No effect on stealthed players
-			if (target is GamePlayer || target.IsStealthed)
-				return;
+        public override void OnDirectEffect(GameLiving target)
+        {
+            if (target == null || !target.IsAlive || target.ObjectState is GameObject.eObjectState.Active)
+                return;
 
-			SendEffectAnimation(target, 0, false, 1);
+            SendEffectAnimation(target, 0, false, 1);
+            AttackData ad = CalculateDamageToTarget(target);
+            DamageTarget(ad, false);
 
-			// Create attack data.
-			AttackData ad = CalculateDamageToTarget(target);
-			DamageTarget(ad, false);
+            // Interrupt only if target is actually casting.
+            if (target.IsCasting && Spell.Target is not eSpellTarget.CONE)
+                target.StartInterruptTimer(target.SpellInterruptDuration, ad.AttackType, Caster);
+        }
 
-			// Interrupt only if target is actually casting
-			if (target.IsCasting && Spell.Target != eSpellTarget.CONE)
-				target.StartInterruptTimer(target.SpellInterruptDuration, ad.AttackType, Caster);
-		}
+        protected override void OnSpellNegated(GameLiving target, SpellNegatedReason reason)
+        {
+            base.OnSpellNegated(target, reason);
 
-		/// <summary>
-		/// When spell was resisted
-		/// </summary>
-		/// <param name="target">the target that resisted the spell</param>
-		protected override void OnSpellNegated(GameLiving target, SpellNegatedReason reason)
-		{
-			base.OnSpellNegated(target, reason);
+            // Interrupt only if target is actually casting.
+            if (target.IsCasting && Spell.Target is not eSpellTarget.CONE)
+                target.StartInterruptTimer(target.SpellInterruptDuration, AttackData.eAttackType.Spell, Caster);
+        }
 
-			// Interrupt only if target is actually casting
-			if (target.IsCasting && Spell.Target != eSpellTarget.CONE)
-				target.StartInterruptTimer(target.SpellInterruptDuration, AttackData.eAttackType.Spell, Caster);
-		}
+        public override void DamageTarget(AttackData ad, bool showEffectAnimation, int attackResult)
+        {
+            base.DamageTarget(ad, showEffectAnimation, attackResult);
 
-		/// <summary>
-		/// Apply the extra aggression
-		/// </summary>
-		/// <param name="ad"></param>
-		/// <param name="showEffectAnimation"></param>
-		/// <param name="attackResult"></param>
-		public override void DamageTarget(AttackData ad, bool showEffectAnimation, int attackResult)
-		{
-			base.DamageTarget(ad, showEffectAnimation, attackResult);
+            // The taunt amount is a wild guess.
+            if (Spell.Value > 0 && ad.Target is GameNPC npc && npc.Brain is IOldAggressiveBrain brain)
+                brain.AddToAggroList(Caster, Math.Max(1, (int) (Spell.Value * Caster.Level * 0.1)));
 
-			if (ad.Target is GameNPC && Spell.Value > 0)
-			{
-				IOldAggressiveBrain aggroBrain = ((GameNPC)ad.Target).Brain as IOldAggressiveBrain;
-				if (aggroBrain != null)
-				{
-					// this amount is a wild guess - Tolakram
-					aggroBrain.AddToAggroList(Caster, Math.Max(1, (int)(Spell.Value * Caster.Level * 0.1)));
-					//log.DebugFormat("Damage: {0}, Taunt Value: {1}, Taunt Amount {2}", ad.Damage, Spell.Value, Math.Max(1, (int)(Spell.Value * Caster.Level * 0.1)));
-				}
-			}
-
-			m_lastAttackData = ad;
-		}
-
-
-		public TauntSpellHandler(GameLiving caster, Spell spell, SpellLine spellLine) : base(caster, spell, spellLine) {}
-	}
+            m_lastAttackData = ad;
+        }
+    }
 }

commit 99f49c58e2cff7030f12ade2a03f84de0a74e017

Fix taunt spells No longer worked at all. Regression caused by 4106600bd6b1a82eb3f930f6e671eb651e526e87

GameServer/spells/TauntSpellHandler.cs
@@ -16,7 +16,7 @@ namespace DOL.GS.Spells
 
         public override void OnDirectEffect(GameLiving target)
         {
-            if (target == null || !target.IsAlive || target.ObjectState is GameObject.eObjectState.Active)
+            if (target == null || !target.IsAlive || target.ObjectState is not GameObject.eObjectState.Active)
                 return;
 
             SendEffectAnimation(target, 0, false, 1);