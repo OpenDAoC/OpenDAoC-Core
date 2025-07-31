OpenDAoC/OpenDAoC-Core

commit 748b9c3dd49f8cdf4da886cb2220046acbf57ebc

Fix Reaver's melee damage debuff being weaker than intended * Caused by a DoL bug that caps debuffs to 10% in `MeleeDamagePercentCalculator`. * Removed similar cap in `RangedDamagePercentCalculator`. * For consistency sake, updated `SpellDamagePercentCalculator` to apply the 10% cap on item bonus only instead of the final result (`TOABonusCalculator.cs` seems to be older).

GameServer/propertycalc/TOABonusCalculator.cs
@@ -127,12 +127,11 @@ namespace DOL.GS.PropertyCalc
             else
                 livingToCheck = living;
 
-            // Hardcap at 10%
-            int percent = Math.Min(10, living.BaseBuffBonusCategory[property] + livingToCheck.ItemBonus[property] - living.DebuffCategory[property]);
-
-            // Add RA bonus
-            percent += livingToCheck.AbilityBonus[property];
-            return percent;
+            int abilityBonus = living.AbilityBonus[property];
+            int itemBonus = Math.Min(10, livingToCheck.ItemBonus[property]);
+            int buffBonus = living.BaseBuffBonusCategory[property] + living.SpecBuffBonusCategory[property];
+            int debuffMalus = Math.Abs(livingToCheck.DebuffCategory[property]);
+            return abilityBonus + buffBonus + itemBonus - debuffMalus;
         }
     }
 }

GameServer/propertycalc/MeleeDamagePercentCalculator.cs
@@ -7,11 +7,10 @@ namespace DOL.GS.PropertyCalc
     {
         public override int CalcValue(GameLiving living, eProperty property)
         {
-            int hardCap = 10;
             int abilityBonus = living.AbilityBonus[property];
-            int itemBonus = Math.Min(hardCap, living.ItemBonus[property]);
+            int itemBonus = Math.Min(10, living.ItemBonus[property]);
             int buffBonus = living.BaseBuffBonusCategory[property] + living.SpecBuffBonusCategory[property];
-            int debuffMalus = Math.Min(hardCap, Math.Abs(living.DebuffCategory[property]));
+            int debuffMalus = Math.Abs(living.DebuffCategory[property]);
             return abilityBonus + buffBonus + itemBonus - debuffMalus;
         }
     }

GameServer/propertycalc/RangedDamagePercentCalculator.cs
@@ -7,11 +7,10 @@ namespace DOL.GS.PropertyCalc
     {
         public override int CalcValue(GameLiving living, eProperty property)
         {
-            int hardCap = 10;
             int abilityBonus = living.AbilityBonus[property];
-            int itemBonus = Math.Min(hardCap, living.ItemBonus[property]);
+            int itemBonus = Math.Min(10, living.ItemBonus[property]);
             int buffBonus = living.BaseBuffBonusCategory[property] + living.SpecBuffBonusCategory[property];
-            int debuffMalus = Math.Min(hardCap, Math.Abs(living.DebuffCategory[property]));
+            int debuffMalus = Math.Abs(living.DebuffCategory[property]);
             return abilityBonus + buffBonus + itemBonus - debuffMalus;
         }
     }

commit a70e698f813933cb8b68f5b7b489f5b86ca2441c

`SpellDamagePercentCalculator`: Fix ability bonus not being applied to necromancer pets Regression caused by 748b9c3dd49f8cdf4da886cb2220046acbf57ebc

GameServer/propertycalc/TOABonusCalculator.cs
@@ -127,7 +127,7 @@ namespace DOL.GS.PropertyCalc
             else
                 livingToCheck = living;
 
-            int abilityBonus = living.AbilityBonus[property];
+            int abilityBonus = livingToCheck.AbilityBonus[property];
             int itemBonus = Math.Min(10, livingToCheck.ItemBonus[property]);
             int buffBonus = living.BaseBuffBonusCategory[property] + living.SpecBuffBonusCategory[property];
             int debuffMalus = Math.Abs(livingToCheck.DebuffCategory[property]);