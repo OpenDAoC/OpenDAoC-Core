OpenDAoC/OpenDAoC-Core

commit 74cb55fb9f2c8980d220178c3cc31f265ad763c5

Remove superfluous `CheckWeaponMagicalEffect` parameter

GameServer/gameobjects/GameLiving.cs
@@ -774,10 +774,10 @@ namespace DOL.GS
 		/// <summary>
 		/// Check if we can make a proc on a weapon go off.  Weapon Procs
 		/// </summary>
-		/// <param name="ad"></param>
-		/// <param name="weapon"></param>
-		public virtual void CheckWeaponMagicalEffect(AttackData ad, DbInventoryItem weapon)
+		public virtual void CheckWeaponMagicalEffect(AttackData ad)
 		{
+			DbInventoryItem weapon = ad.Weapon;
+
 			if (weapon == null || (ad.AttackResult != eAttackResult.HitStyle && ad.AttackResult != eAttackResult.HitUnstyled))
 				return;
 
@@ -1443,15 +1443,6 @@ namespace DOL.GS
 						else
 							effect.RemainingValue = ablativeHp;
 					}
-				}
-
-				// Handle DefensiveProcs.
-				List<ECSGameSpellEffect> dProcEffects = effectListComponent.GetSpellEffects(eEffect.DefensiveProc);
-
-				if (ad.Target == this && dProcEffects != null && ad.AttackType != eAttackType.Spell)
-				{
-					for (int i = 0; i < dProcEffects.Count; i++)
-						(dProcEffects[i].SpellHandler as DefensiveProcSpellHandler).EventHandler(ad);
 				}
 			}
 			else if (ad.IsSpellResisted && ad.Target is GameNPC npc)

GameServer/ECS-Components/Actions/WeaponAction.cs
@@ -345,7 +345,7 @@ namespace DOL.GS
             if (attackData.AttackResult is eAttackResult.HitUnstyled or eAttackResult.HitStyle)
             {
                 _owner.DealDamage(attackData);
-                _owner.CheckWeaponMagicalEffect(attackData, attackData.Weapon);
+                _owner.CheckWeaponMagicalEffect(attackData);
                 HandleDamageAdd(_owner, attackData);
             }
 

GameServer/spells/Archery/Arrow.cs
@@ -321,7 +321,7 @@ namespace DOL.GS.Spells
 				{
 					if (ad.AttackResult == eAttackResult.HitUnstyled || ad.AttackResult == eAttackResult.HitStyle)
 					{
-						caster.CheckWeaponMagicalEffect(ad, m_handler.Caster.ActiveWeapon);
+						caster.CheckWeaponMagicalEffect(ad);
 					}
 				}
 

GameServer/spells/SpellHandler.cs
@@ -3009,6 +3009,7 @@ namespace DOL.GS.Spells
 				Attacker = m_caster,
 				Target = target,
 				AttackType = AttackData.eAttackType.Spell,
+				Weapon = m_caster.ActiveWeapon,
 				SpellHandler = this,
 				AttackResult = eAttackResult.HitUnstyled
 			};

commit 993dbf0afd54dc61dadf35f58863f5cc748f1438

Fix spell based reactive procs Regression caused by 74cb55fb9f2c8980d220178c3cc31f265ad763c5. Was removed by mistake.

GameServer/gameobjects/GameLiving.cs
@@ -1540,6 +1540,15 @@ namespace DOL.GS
 							effect.RemainingValue = ablativeHp;
 					}
 				}
+
+				// Handle DefensiveProcs.
+				List<ECSGameSpellEffect> dProcEffects = effectListComponent.GetSpellEffects(eEffect.DefensiveProc);
+
+				if (ad.Target == this && dProcEffects != null && ad.AttackType is not eAttackType.Spell)
+				{
+					for (int i = 0; i < dProcEffects.Count; i++)
+						(dProcEffects[i].SpellHandler as DefensiveProcSpellHandler).EventHandler(ad);
+				}
 			}
 			else if (ad.IsSpellResisted && ad.Target is GameNPC npc)
 				npc.CancelReturnToSpawnPoint();