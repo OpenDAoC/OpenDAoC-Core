bfd1de4a1c1e79c6e6eff38a3d879b7d81338220
Baptiste Marie <bm01@users.noreply.github.com>
2025-08-07 02:07:12 +0200

    Optimize `AttributeUtil`

--- CoreDatabase/Attributes/AttributeUtil.cs
+++ CoreDatabase/Attributes/AttributeUtil.cs
@@ -1,104 +1,87 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Reflection;
 
 namespace DOL.Database.Attributes
 {
-	/// <summary>
-	/// Utils Method for Handling DOL Database Attributes
-	/// </summary>
-	public static class AttributeUtil
-	{
-		/// <summary>
-		/// Returns the TableName from Type if DataTable Attribute is found 
-		/// </summary>
-		/// <param name="type">Type inherited from DataObject</param>
-		/// <returns>Table Name from DataTable Attribute or ClassName</returns>
-		public static string GetTableName(Type type)
-		{
-			// Check if Type is Element
-			if (type.HasElementType)
-				type = type.GetElementType();
-			
-			var dataTable = type.GetCustomAttributes<DataTable>(true).FirstOrDefault();
-			
-			if (dataTable != null && !string.IsNullOrEmpty(dataTable.TableName))
-				return dataTable.TableName;
-			
-			return type.Name;
-		}
+    /// <summary>
+    /// Utils Method for Handling DOL Database Attributes
+    /// </summary>
+    public static class AttributeUtil
+    {
+        private static readonly ConcurrentDictionary<Type, DataTable> _dataTableCache = new();
 
-		/// <summary>
-		/// Returns the ViewName from Type if DataTable Attribute is found 
-		/// </summary>
-		/// <param name="type">Type inherited from DataObject</param>
-		/// <returns>View Name from DataTable Attribute or null</returns>
-		public static string GetViewName(Type type)
-		{
-			// Check if Type is Element
-			if (type.HasElementType)
-				type = type.GetElementType();
-			
-			var dataTable = type.GetCustomAttributes<DataTable>(true).FirstOrDefault();
-			
-			if (dataTable != null && !string.IsNullOrEmpty(dataTable.ViewName))
-				return dataTable.ViewName;
-			
-			return null;
-		}
-		
-		/// <summary>
-		/// Returns the View Select As Query from Type if DataTable Attribute is found 
-		/// </summary>
-		/// <param name="type">Type inherited from DataObject</param>
-		/// <returns>View Select As Query from DataTable Attribute or null</returns>
-		public static string GetViewAs(Type type)
-		{
-			// Check if Type is Element
-			if (type.HasElementType)
-				type = type.GetElementType();
-			
-			var dataTable = type.GetCustomAttributes<DataTable>(true).FirstOrDefault();
-			
-			if (dataTable != null && !string.IsNullOrEmpty(dataTable.ViewAs))
-				return dataTable.ViewAs;
-			
-			return null;
-		}
+        /// <summary>
+        /// A private helper method that gets the DataTable attribute for a given type,
+        /// using a cache to avoid repeated reflection calls.
+        /// </summary>
+        /// <param name="type">The type to inspect.</param>
+        /// <returns>The cached DataTable attribute, or null if not found.</returns>
+        private static DataTable GetCachedDataTableAttribute(Type type)
+        {
+            // Normalize the type first, so we use the same key for both a type and an array of that type.
+            Type normalizedType = type.HasElementType ? type.GetElementType() : type;
+            return _dataTableCache.GetOrAdd(normalizedType, static t => t.GetCustomAttributes<DataTable>(true).FirstOrDefault());
+        }
 
-		/// <summary>
-		/// Return Table View or Table Name
-		/// </summary>
-		/// <param name="type">Type inherited from DataObject</param>
-		/// <returns>View Name if available, Table Name default</returns>
-		public static string GetTableOrViewName(Type type)
-		{
-			// Graveen: introducing view selection hack (before rewriting the layer :D)
-			// basically, a view must exist and is created with the following:
-			//
-			//	[DataTable(TableName="InventoryItem",ViewName = "MarketItem")]
-			//	public class SomeMarketItems : InventoryItem {};
-			//
-			//  here, we rely on the view called MarketItem,
-			//  based on the InventoryItem table. We have to tell to the code
-			//  only to bypass the id generated with FROM by the above
-			//  code.
-			return GetViewName(type) ?? GetTableName(type);
-		}
-		
-		/// <summary>
-		/// Is this Data Table Pre-Cached on startup?
-		/// </summary>
-		/// <param name="type">Type inherited from DataObject</param>
-		/// <returns>True if Pre-Cached Flag is set</returns>
-		public static bool GetPreCachedFlag(Type type)
-		{
-			var dataTable = type.GetCustomAttributes<DataTable>(true).FirstOrDefault();
-			
-			if (dataTable != null)
-				return dataTable.PreCache;
+        /// <summary>
+        /// Returns the TableName from Type if DataTable Attribute is found 
+        /// </summary>
+        /// <param name="type">Type inherited from DataObject</param>
+        /// <returns>Table Name from DataTable Attribute or ClassName</returns>
+        public static string GetTableName(Type type)
+        {
+            DataTable dataTable = GetCachedDataTableAttribute(type);
 
-			return false;
-		}
-	}
+            if (dataTable != null && !string.IsNullOrEmpty(dataTable.TableName))
+                return dataTable.TableName;
+
+            // Fallback to the class name (use the normalized type name).
+            return (type.HasElementType ? type.GetElementType() : type).Name;
+        }
+
+        /// <summary>
+        /// Returns the ViewName from Type if DataTable Attribute is found 
+        /// </summary>
+        /// <param name="type">Type inherited from DataObject</param>
+        /// <returns>View Name from DataTable Attribute or null</returns>
+        public static string GetViewName(Type type)
+        {
+            DataTable dataTable = GetCachedDataTableAttribute(type);
+            return dataTable != null && !string.IsNullOrEmpty(dataTable.ViewName) ? dataTable.ViewName : null;
+        }
+
+        /// <summary>
+        /// Returns the View Select As Query from Type if DataTable Attribute is found 
+        /// </summary>
+        /// <param name="type">Type inherited from DataObject</param>
+        /// <returns>View Select As Query from DataTable Attribute or null</returns>
+        public static string GetViewAs(Type type)
+        {
+            DataTable dataTable = GetCachedDataTableAttribute(type);
+            return dataTable != null && !string.IsNullOrEmpty(dataTable.ViewAs) ? dataTable.ViewAs : null;
+        }
+
+        /// <summary>
+        /// Return Table View or Table Name
+        /// </summary>
+        /// <param name="type">Type inherited from DataObject</param>
+        /// <returns>View Name if available, Table Name default</returns>
+        public static string GetTableOrViewName(Type type)
+        {
+            return GetViewName(type) ?? GetTableName(type);
+        }
+
+        /// <summary>
+        /// Is this Data Table Pre-Cached on startup?
+        /// </summary>
+        /// <param name="type">Type inherited from DataObject</param>
+        /// <returns>True if Pre-Cached Flag is set</returns>
+        public static bool GetPreCachedFlag(Type type)
+        {
+            DataTable dataTable = GetCachedDataTableAttribute(type);
+            return dataTable?.PreCache ?? false;
+        }
+    }
 }
