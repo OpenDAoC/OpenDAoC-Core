b21b50da3928444882bba0d437119508141ccfa9
Baptiste Marie <bm01@users.noreply.github.com>
2025-08-08 22:39:09 +0200

    Fix `CurrentProcessCpuUsagePercentStatistic` linux implementation

--- GameServer/PerformanceStatistics/CurrentProcessCpuUsageStatistics.cs
+++ GameServer/PerformanceStatistics/CurrentProcessCpuUsageStatistics.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Runtime.InteropServices;
+using System.Linq;
 using System.Runtime.Versioning;
 
 namespace DOL.GS.PerformanceStatistics
 {
     public class CurrentProcessCpuUsagePercentStatistic : IPerformanceStatistic
     {
-        private IPerformanceStatistic _processorTimeRatioStatistic;
+        private readonly IPerformanceStatistic _processorTimeRatioStatistic;
 
         public CurrentProcessCpuUsagePercentStatistic()
         {
-            _processorTimeRatioStatistic = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ?
-                                           new PerformanceCounterStatistic("Process", "% processor time", Process.GetCurrentProcess().ProcessName) :
-                                           new LinuxCurrentProcessUsagePercentStatistic();
+            if (OperatingSystem.IsWindows())
+                _processorTimeRatioStatistic = new WindowsProcessCpuUsagePercentStatistic();
+            else if (OperatingSystem.IsLinux())
+                _processorTimeRatioStatistic = new LinuxCurrentProcessUsagePercentStatistic();
+            else
+                throw new PlatformNotSupportedException("Current process CPU usage percent statistic is not supported on this platform.");
         }
 
         public double GetNextValue()
@@ -23,42 +26,74 @@ namespace DOL.GS.PerformanceStatistics
         }
     }
 
-#if NET
-    [UnsupportedOSPlatform("Windows")]
-#endif
-    public class LinuxCurrentProcessUsagePercentStatistic : IPerformanceStatistic
+    [SupportedOSPlatform("windows")]
+    public class WindowsProcessCpuUsagePercentStatistic : IPerformanceStatistic
     {
-        // private IPerformanceStatistic _idleProcessorTimeStatistic;
-        private IPerformanceStatistic _totalProcessorTimeStatistic ;
-        private IPerformanceStatistic _currentProcessProcessorTimeStatistic;
+        private readonly Process _process;
+        private readonly IPerformanceStatistic _performanceCounter;
 
-        public LinuxCurrentProcessUsagePercentStatistic()
+        public WindowsProcessCpuUsagePercentStatistic()
         {
-            // _idleProcessorTimeStatistic = new PerSecondStatistic(new LinuxSystemIdleProcessorTimeInSeconds());
-            _totalProcessorTimeStatistic = new PerSecondStatistic(new LinuxTotalProcessorTimeInSeconds());
-            _currentProcessProcessorTimeStatistic = new PerSecondStatistic(new LinuxCurrentProcessProcessorTimeInSeconds());
+            // NOTE: This can be ambiguous if multiple processes share the same name.
+            _process = Process.GetCurrentProcess();
+            _performanceCounter = new PerformanceCounterStatistic("Process", "% Processor Time", _process.ProcessName);
         }
 
         public double GetNextValue()
         {
-            // double idleTime = _idleProcessorTimeStatistic.GetNextValue();
-            double totalTime = _totalProcessorTimeStatistic.GetNextValue();
-            double processTime = _currentProcessProcessorTimeStatistic.GetNextValue();
-            return processTime / totalTime * 100 * Environment.ProcessorCount;
+            return _performanceCounter.GetNextValue() / Environment.ProcessorCount;
         }
     }
 
-#if NET
-    [UnsupportedOSPlatform("Windows")]
-#endif
-    public class LinuxCurrentProcessProcessorTimeInSeconds : IPerformanceStatistic
+    [SupportedOSPlatform("linux")]
+    public class LinuxCurrentProcessUsagePercentStatistic : IPerformanceStatistic
     {
+        private long _previousTotalSystemTime;
+        private long _previousProcessTime;
+
+        public LinuxCurrentProcessUsagePercentStatistic()
+        {
+            GetNextValue();
+        }
+
         public double GetNextValue()
         {
-            int pid = Environment.ProcessId;
-            string[] statArray = File.ReadAllText($"/proc/{pid}/stat").Split(' ');
-            long processorTime = Convert.ToInt64(statArray[13]) + Convert.ToInt64(statArray[14]);
-            return processorTime * 0.001f;
+            long currentTotalSystemTime;
+            long currentProcessTime;
+
+            try
+            {
+                string systemStatLine = File.ReadLines("/proc/stat").First();
+
+                var systemNumbers = systemStatLine.Split([' '], StringSplitOptions.RemoveEmptyEntries)
+                                                  .Skip(1)
+                                                  .Select(long.Parse);
+                currentTotalSystemTime = systemNumbers.Sum();
+
+                string processStatContent = File.ReadAllText($"/proc/{Environment.ProcessId}/stat");
+                int lastParen = processStatContent.LastIndexOf(')');
+                string[] processStatParts = processStatContent[(lastParen + 2)..].Split(' ');
+
+                long utime = long.Parse(processStatParts[11]);
+                long stime = long.Parse(processStatParts[12]);
+                currentProcessTime = utime + stime;
+            }
+            catch (Exception)
+            {
+                return 0.0;
+            }
+
+            long totalSystemDelta = currentTotalSystemTime - _previousTotalSystemTime;
+            long processDelta = currentProcessTime - _previousProcessTime;
+
+            _previousTotalSystemTime = currentTotalSystemTime;
+            _previousProcessTime = currentProcessTime;
+
+            if (totalSystemDelta <= 0)
+                return 0.0;
+
+            double cpuUsage = (double) processDelta / totalSystemDelta * 100.0;
+            return Math.Clamp(cpuUsage, 0, 100);
         }
     }
 }
