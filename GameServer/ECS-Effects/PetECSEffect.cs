OpenDAoC/OpenDAoC-Core

commit 254adb6079cc6bfdff09efd48eb0a53f9c12624c

Fix releasing a controlled pet decreasing the player's pet counter The pet counter is only used by Animists and Theurgists. This allowed Animists to bypass FnF summon cap.

GameServer/ECS-Effects/PetECSEffect.cs
@@ -9,8 +9,7 @@ namespace DOL.GS
         public override void OnStopEffect()
         {
             // `GameObject.Die` shouldn't be called here, since it messes up with Necromancer HP.
-            SpellHandler.Caster.UpdatePetCount(false);
-            (SpellHandler as SummonSpellHandler)?.OnPetReleased(Owner as GameSummonedPet); // Should be done before setting health to 0.
+            (SpellHandler as SummonSpellHandler)?.OnPetReleased(); // Should be done before setting health to 0.
             Owner.effectListComponent.CancelAll();
             Owner.Health = 0; // To send proper remove packet.
             Owner.Delete();

GameServer/spells/Animist/BomberSpellHandler.cs
@@ -41,7 +41,7 @@ namespace DOL.GS.Spells
             }
         }
 
-        public override void OnPetReleased(GameSummonedPet pet) { }
+        public override void OnPetReleased() { }
 
         protected override IControlledBrain GetPetBrain(GameLiving owner)
         {

GameServer/spells/Animist/SummonAnimistAmbusher.cs
@@ -22,14 +22,14 @@ namespace DOL.GS.Spells
 			((ControlledMobBrain) m_pet.Brain).Stay();
 		}
 
-		public override void OnPetReleased(GameSummonedPet pet)
+		public override void OnPetReleased()
 		{
-			if (pet.Brain is not ForestheartAmbusherBrain forestheartAmbusherBrain)
+			if (Pet.Brain is not ForestheartAmbusherBrain forestheartAmbusherBrain)
 				return;
 
 			AtlasOF_ForestheartAmbusherECSEffect effect = EffectListService.GetEffectOnTarget(Caster, eEffect.ForestheartAmbusher) as AtlasOF_ForestheartAmbusherECSEffect;
 			effect?.Cancel(false);
-			base.OnPetReleased(pet);
+			base.OnPetReleased();
 		}
 
 		protected override GameSummonedPet GetGamePet(INpcTemplate template)

GameServer/spells/AstralPetSummon.cs
@@ -17,9 +17,9 @@ namespace DOL.GS.Spells
 
         }
 
-        public override void OnPetReleased(GameSummonedPet pet)
+        public override void OnPetReleased()
         {
-            Effects.GameSpellEffect effect = FindEffectOnTarget(pet, this);
+            Effects.GameSpellEffect effect = FindEffectOnTarget(Pet, this);
             effect?.Cancel(false);
         }
 

GameServer/spells/Bonedancer/SummonCommanderPet.cs
@@ -22,9 +22,9 @@ namespace DOL.GS.Spells
             return base.CheckBeginCast(selectedTarget);
         }
 
-        public override void OnPetReleased(GameSummonedPet pet)
+        public override void OnPetReleased()
         {
-            CommanderPet commanderPet = pet as CommanderPet;
+            CommanderPet commanderPet = Pet as CommanderPet;
 
             if (commanderPet == null)
                 return;
@@ -35,7 +35,7 @@ namespace DOL.GS.Spells
                     bdPetBrain?.OnRelease();
             }
 
-            base.OnPetReleased(commanderPet);
+            base.OnPetReleased();
         }
 
         protected override IControlledBrain GetPetBrain(GameLiving owner)

GameServer/spells/Cabalist/SummonJuggernaut.cs
@@ -8,14 +8,14 @@ namespace DOL.GS.Spells
 	{
 		public SummonJuggernaut(GameLiving caster, Spell spell, SpellLine line) : base(caster, spell, line) { }
 
-		public override void OnPetReleased(GameSummonedPet pet)
+		public override void OnPetReleased()
 		{
-			if (pet.Brain is not JuggernautBrain juggernautBrain)
+			if (Pet.Brain is not JuggernautBrain juggernautBrain)
 				return;
 
 			AtlasOF_JuggernautECSEffect effect = EffectListService.GetEffectOnTarget(Target, eEffect.Juggernaut) as AtlasOF_JuggernautECSEffect;
 			effect?.Cancel(false);
-			base.OnPetReleased(pet);
+			base.OnPetReleased();
 		}
 
 		protected override IControlledBrain GetPetBrain(GameLiving owner)

GameServer/spells/SummonSpellHandler.cs
@@ -154,9 +154,9 @@ namespace DOL.GS.Spells
 			Caster.OnPetSummoned(m_pet);
 		}
 
-		public virtual void OnPetReleased(GameSummonedPet pet)
+		public virtual void OnPetReleased()
 		{
-			if (pet.Brain is not IControlledBrain petBrain)
+			if (Pet.Brain is not IControlledBrain petBrain)
 				return;
 
 			GameLiving petOwner = petBrain.Owner;

commit 37e694d8153fc777e580d9c8dde09e4bad59e46f

Fix pet count tracking Regression caused by 254adb6079cc6bfdff09efd48eb0a53f9c12624c The methods calling Caster.UpdatePetCount(false) in spell handlers are unused. Added GameSummonedPet.CountsTowardsPetLimit property to be able to tell the difference between a FnF, and permanent or vanity pets for example.

GameServer/ECS-Effects/PetECSEffect.cs
@@ -9,6 +9,10 @@ namespace DOL.GS
         public override void OnStopEffect()
         {
             // `GameObject.Die` shouldn't be called here, since it messes up with Necromancer HP.
+
+            if (Owner is GameSummonedPet pet && pet.CountsTowardsPetLimit)
+                SpellHandler.Caster.UpdatePetCount(false);
+
             (SpellHandler as SummonSpellHandler)?.OnPetReleased(); // Should be done before setting health to 0.
             Owner.effectListComponent.CancelAll();
             Owner.Health = 0; // To send proper remove packet.

GameServer/gameobjects/Bonedancer/CommanderPet.cs
@@ -849,7 +849,12 @@ namespace DOL.GS
 						if (Brain is IControlledBrain commanderBrain)
 							controlledBrain.SetAggressionState(commanderBrain.AggressionState);
 
-						UpdatePetCount(true);
+						if (controlledBrain.Body is GameSummonedPet pet)
+						{
+							UpdatePetCount(true);
+							pet.CountsTowardsPetLimit = true;
+						}
+
 						break;
 					}
 				}

GameServer/gameobjects/GameSummonedPet.cs
@@ -21,6 +21,8 @@ namespace DOL.GS
 			set { m_targetInView = value; }
 		}
 
+		public bool CountsTowardsPetLimit { get; set; }
+
 		public GameSummonedPet(INpcTemplate template) : base(template) { }
 
 		public GameSummonedPet(ABrain brain) : base(brain) { }

GameServer/spells/Animist/SummonAnimistFnF.cs
@@ -76,6 +76,7 @@ namespace DOL.GS.Spells
                 (m_pet as TurretPet).TurretSpell = m_pet.Spells[0];
 
             (m_pet.Brain as TurretBrain).IsMainPet = false;
+            m_pet.CountsTowardsPetLimit = true;
             Caster.UpdatePetCount(true);
         }
 

GameServer/spells/Theurgist/SummonTheurgistPet.cs
@@ -83,6 +83,7 @@ namespace DOL.GS.Spells
 			m_pet.TargetObject = target;
 			(m_pet.Brain as IOldAggressiveBrain).AddToAggroList(target, 1);
 			m_pet.Brain.Think();
+			m_pet.CountsTowardsPetLimit = true;
 			Caster.UpdatePetCount(true);
 		}
 