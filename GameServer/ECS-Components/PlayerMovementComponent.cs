OpenDAoC/OpenDAoC-Core

commit dbc79cdc80e445efe81cde91ffa988bf32180059

Improve movement / spell cancelation interaction Calls to `CasterMoves` in `SpellHandler` were redundant and detrimential. Made `MovementComponent` call `CasterMoves` when packets are received, instead of when it ticks. Solves an issue where a position change could be detected too late.

GameServer/ECS-Components/PlayerMovementComponent.cs
@@ -83,12 +83,6 @@ namespace DOL.GS
             base.TickInternal();
         }
 
-        protected override void OnOwnerMoved()
-        {
-            Owner.OnPlayerMove();
-            base.OnOwnerMoved();
-        }
-
         public void BroadcastPosition()
         {
             PlayerPositionUpdateHandler.BroadcastPosition(Owner.Client);
@@ -99,11 +93,19 @@ namespace DOL.GS
             PlayerHeadingUpdateHandler.BroadcastHeading(Owner.Client);
         }
 
-        public void OnPositionUpdateFromPacket()
+        public override void OnPositionUpdate()
         {
-            _needBroadcastPosition = true;
-            _validateMovementOnNextTick = true;
-            LastPositionUpdatePacketReceivedTime = GameLoop.GameLoopTime;
+            base.OnPositionUpdate();
+
+            if (UpdatePosition())
+            {
+                Owner.OnPlayerMove();
+                _playerMovementMonitor.RecordPosition();
+                _validateMovementOnNextTick = true;
+                Owner.LastPlayerActivityTime = GameLoop.GameLoopTime;
+            }
+            else
+                _needBroadcastPosition = true;
 
             if (IsMoving)
                 Owner.LastPlayerActivityTime = GameLoop.GameLoopTime;
@@ -127,7 +129,6 @@ namespace DOL.GS
                 }
             }
 
-            _playerMovementMonitor.RecordPosition();
             AddToServiceObjectStore();
 
             static void SendEncumberedMessage(GamePlayer player)
@@ -136,7 +137,7 @@ namespace DOL.GS
             }
         }
 
-        public void OnHeadingPacketReceived()
+        public void OnHeadingUpdate()
         {
             _needBroadcastHeading = true;
         }

GameServer/ECS-Components/MovementComponent.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System.Numerics;
+using System.Threading;
 
 namespace DOL.GS
 {
@@ -6,8 +7,8 @@ namespace DOL.GS
     {
         private const int SUBZONE_RELOCATION_CHECK_INTERVAL = 500;
 
-        private Point2D _lastPosition = new();
-        private bool _relocationPending;
+        protected Vector3 _ownerPosition;
+        private bool _relocationCheckPending;
         private long _nextRelocationCheckTick;
         private int _turningDisabledCount;
 
@@ -46,6 +47,22 @@ namespace DOL.GS
             TickInternal();
         }
 
+        protected virtual void TickInternal()
+        {
+            if (!_relocationCheckPending || !GameServiceUtils.ShouldTick(_nextRelocationCheckTick))
+                return;
+
+            Owner.SubZoneObject.CheckForRelocation();
+            _nextRelocationCheckTick = GameLoop.GameLoopTime + SUBZONE_RELOCATION_CHECK_INTERVAL;
+            _relocationCheckPending = false;
+        }
+
+        public virtual void OnPositionUpdate()
+        {
+            _relocationCheckPending = true;
+            LastMovementTick = GameLoop.GameLoopTime;
+        }
+
         public virtual void DisableTurning(bool add)
         {
             if (add)
@@ -54,26 +71,17 @@ namespace DOL.GS
                 Interlocked.Decrement(ref _turningDisabledCount);
         }
 
-        protected virtual void TickInternal()
+        protected bool UpdatePosition()
         {
-            if (!Owner.IsSamePosition(_lastPosition))
-            {
-                _lastPosition.X = Owner.X;
-                _lastPosition.Y = Owner.Y;
-                _relocationPending = true;
-                LastMovementTick = GameLoop.GameLoopTime;
-                OnOwnerMoved();
-            }
+            // _ownerPosition is currently only used by the movement component.
+            Vector3 newPosition = new(Owner.X, Owner.Y, Owner.Z);
 
-            if (_relocationPending && GameServiceUtils.ShouldTick(_nextRelocationCheckTick))
-            {
-                Owner.SubZoneObject.CheckForRelocation();
-                _nextRelocationCheckTick = GameLoop.GameLoopTime + SUBZONE_RELOCATION_CHECK_INTERVAL;
-                _relocationPending = false;
-            }
+            if (newPosition.Equals(_ownerPosition))
+                return false;
+
+            _ownerPosition = newPosition;
+            return true;
         }
-
-        protected virtual void OnOwnerMoved() { }
 
         protected void AddToServiceObjectStore()
         {

GameServer/ECS-Components/NpcMovementComponent.cs
@@ -14,7 +14,6 @@ namespace DOL.GS
 
         public const short DEFAULT_WALK_SPEED = 70;
 
-        private Vector3 _ownerPosition;
         private MovementState _movementState;
         private Vector3 _velocity;
         private Vector3 _destination;
@@ -78,7 +77,8 @@ namespace DOL.GS
 
         protected override void TickInternal()
         {
-            _ownerPosition = new(Owner.X, Owner.Y, Owner.Z);
+            // Update the component's position first for correct calculations, since NPCs position (GameNPC X,Y,Z) is interpolated.
+            UpdatePosition();
 
             if (IsFlagSet(MovementState.TURN_TO))
             {
@@ -138,8 +138,9 @@ namespace DOL.GS
 
                 if (_needsBroadcastUpdate)
                 {
-                    ClientService.UpdateNpcForPlayers(Owner);
                     _needsBroadcastUpdate = false;
+                    OnPositionUpdate();
+                    ClientService.UpdateNpcForPlayers(Owner);
                 }
 
                 if (_movementState is MovementState.NONE)

GameServer/gameobjects/GamePlayer.cs
@@ -904,12 +904,12 @@ namespace DOL.GS
 
         public void OnPositionUpdateFromPacket()
         {
-            movementComponent.OnPositionUpdateFromPacket();
+            movementComponent.OnPositionUpdate();
         }
 
         public void OnHeadingPacketReceived()
         {
-            movementComponent.OnHeadingPacketReceived();
+            movementComponent.OnHeadingUpdate();
         }
 
         public void OnLinkDeath()

GameServer/spells/SpellHandler.cs
@@ -1014,12 +1014,6 @@ namespace DOL.GS.Spells
 				}
 			}
 
-			if ((m_caster.IsMoving || m_caster.IsStrafing) && !Spell.MoveCast)
-			{
-				CasterMoves();
-				return false;
-			}
-
 			return true;
 		}
 
@@ -1093,16 +1087,6 @@ namespace DOL.GS.Spells
 						}
 						else
 							CastState = eCastState.Finished;
-					}
-
-					break;
-				}
-				case eCastState.Focusing:
-				{
-					if (Caster.IsStrafing || Caster.IsMoving)
-					{
-						CasterMoves();
-						CastState = eCastState.Cleanup;
 					}
 
 					break;

commit b3fc00b13b49bde2bbf099a7064797df0f4f1886

Fix lack of player position / movement broadcast Regression caused by dbc79cdc80e445efe81cde91ffa988bf32180059. _needBroadcastPosition should always be set to true when receiving a packet.

GameServer/ECS-Components/PlayerMovementComponent.cs
@@ -104,8 +104,8 @@ namespace DOL.GS
                 _validateMovementOnNextTick = true;
                 Owner.LastPlayerActivityTime = GameLoop.GameLoopTime;
             }
-            else
-                _needBroadcastPosition = true;
+
+            _needBroadcastPosition = true;
 
             if (IsMoving)
                 Owner.LastPlayerActivityTime = GameLoop.GameLoopTime;