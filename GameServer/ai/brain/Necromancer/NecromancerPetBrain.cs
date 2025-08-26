OpenDAoC/OpenDAoC-Core

commit 6748737b4bdabba494d7d7e9bd0d78f65103ebb4

Fix Necromancer pets not checking attack spell queue before normal queue Issue made apparent by 580c1d90225d0f198529c9e115ffc7b2c1adcc30. It prevented FP to be activated first.

GameServer/ai/brain/Necromancer/NecromancerPetBrain.cs
@@ -121,6 +121,10 @@ namespace DOL.AI.Brain
                 return false;
             }
 
+            // Check the attack spell queue before casting spells.
+            // This allows instant spells such as FP to be activated first.
+            CheckAttackSpellQueue();
+
             SpellQueueEntry entry = GetSpellFromQueue();
 
             if (entry == null || !CastSpell(entry.Spell, entry.SpellLine, entry.Target, true))

GameServer/ECS-Components/Actions/NpcAttackAction.cs
@@ -108,7 +108,7 @@ namespace DOL.GS
             int maxSpeed = _npcOwner.MaxSpeed;
 
             if (maxSpeed > 0)
-                meleeAttackRange = meleeAttackRange + (int) (TIME_TO_TARGET_THRESHOLD_BEFORE_RANGED_SWITCH * maxSpeed * 0.001);
+                meleeAttackRange += (int) (TIME_TO_TARGET_THRESHOLD_BEFORE_RANGED_SWITCH * maxSpeed * 0.001);
 
             // NPCs try to switch to their ranged weapon whenever possible.
             if (!_npcOwner.IsBeingInterrupted &&

commit 89dfaccbe7d34cb34b71f29e5cfc2b803f57bdc7

Fix Necromancer pets being allowed to cast instant spells while casting Regression caused by 6748737b4bdabba494d7d7e9bd0d78f65103ebb4. The fix was good, but was missing a Body.IsCasting check.

GameServer/ai/brain/Necromancer/NecromancerPetBrain.cs
@@ -121,7 +121,8 @@ namespace DOL.AI.Brain
 
             // Check the attack spell queue before casting spells.
             // This allows instant spells such as FP to be activated first.
-            CheckAttackSpellQueue();
+            if (!Body.IsCasting)
+                CheckAttackSpellQueue();
 
             SpellQueueEntry entry = GetSpellFromQueue();
 