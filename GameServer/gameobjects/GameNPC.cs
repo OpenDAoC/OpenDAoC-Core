0bd81827c29c4d5d8514b6cbc681ad995183e954
Baptiste Marie <bm01@users.noreply.github.com>
2025-08-26 17:14:36 +0200

    `GameNPC`: Change `SaveIntoDatabase`, `AddToWorld` to no longer use interpolated positions This is now causing issues since interpolated positions are cached.

--- GameServer/gameobjects/GameNPC.cs
+++ GameServer/gameobjects/GameNPC.cs
@@ -1155,9 +1155,9 @@ namespace DOL.GS
 			mob.Guild = GuildName;
 			mob.ExamineArticle = ExamineArticle;
 			mob.MessageArticle = MessageArticle;
-			mob.X = X;
-			mob.Y = Y;
-			mob.Z = Z;
+			mob.X = m_x;
+			mob.Y = m_y;
+			mob.Z = m_z;
 			mob.Heading = Heading;
 			mob.Speed = MaxSpeedBase;
 			mob.Region = CurrentRegionID;
@@ -1960,9 +1960,9 @@ namespace DOL.GS
 				Riders = new GamePlayer[MAX_PASSENGERS];
 
 			ClientService.CreateObjectForPlayers(this);
-			m_spawnPoint.X = X;
-			m_spawnPoint.Y = Y;
-			m_spawnPoint.Z = Z;
+			m_spawnPoint.X = m_x;
+			m_spawnPoint.Y = m_y;
+			m_spawnPoint.Z = m_z;
 			m_spawnHeading = Heading;
 
 			Brain?.Start();
@@ -2004,9 +2004,9 @@ namespace DOL.GS
 					{
 						Name = string.Empty,
 						Model = 1923,
-						X = X,
-						Y = Y,
-						Z = Z + 1,
+						X = m_x,
+						Y = m_y,
+						Z = m_z + 1,
 						CurrentRegionID = CurrentRegionID,
 						Flags = eFlags.PEACE | eFlags.CANTTARGET | eFlags.DONTSHOWNAME | eFlags.FLYING
 					};
@@ -4146,9 +4146,9 @@ namespace DOL.GS
 			copyTarget.SaveInDB = SaveInDB;
 			copyTarget.Strength = Strength;
 			copyTarget.TetherRange = TetherRange;
-			copyTarget.X = X;
-			copyTarget.Y = Y;
-			copyTarget.Z = Z;
+			copyTarget.X = m_x;
+			copyTarget.Y = m_y;
+			copyTarget.Z = m_z;
 			copyTarget.OwnerID = OwnerID;
 			copyTarget.PackageID = PackageID;
 
