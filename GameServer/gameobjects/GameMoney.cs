OpenDAoC/OpenDAoC-Core

commit 54a1c10a807c4185799ea72548aa69e0f52d3bb0

Make `AbstractServerRules.DropLoot` spawn items only if auto pick up failed This might be a bit tricky due to the item's state not being marked as active yet, but allowing inactive ones and only ignoring deleted ones in `TryAutoPickUp` seems to be enough.

GameServer/gameobjects/GameMoney.cs
@@ -41,7 +41,7 @@ namespace DOL.GS
         {
             lock (_pickUpLock)
             {
-                return ObjectState is eObjectState.Active && itemOwner.TryAutoPickUpMoney(this);
+                return ObjectState is not eObjectState.Deleted && itemOwner.TryAutoPickUpMoney(this);
             }
         }
 
@@ -49,7 +49,7 @@ namespace DOL.GS
         {
             lock (_pickUpLock)
             {
-                return ObjectState is eObjectState.Active ? itemOwner.TryPickUpMoney(source, this) : TryPickUpResult.FAILED;
+                return ObjectState is not eObjectState.Deleted ? itemOwner.TryPickUpMoney(source, this) : TryPickUpResult.FAILED;
             }
         }
     }

GameServer/gameobjects/WorldInventoryItem.cs
@@ -143,7 +143,7 @@ namespace DOL.GS
         {
             lock (_pickUpLock)
             {
-                return ObjectState is eObjectState.Active && itemOwner.TryAutoPickUpItem(this);
+                return ObjectState is not eObjectState.Deleted && itemOwner.TryAutoPickUpItem(this);
             }
         }
 
@@ -151,7 +151,7 @@ namespace DOL.GS
         {
             lock (_pickUpLock)
             {
-                return ObjectState is eObjectState.Active ? itemOwner.TryPickUpItem(source, this) : TryPickUpResult.FAILED;
+                return ObjectState is not eObjectState.Deleted ? itemOwner.TryPickUpItem(source, this) : TryPickUpResult.FAILED;
             }
         }
     }

GameServer/serverrules/AbstractServerRules.cs
@@ -1529,7 +1529,6 @@ namespace DOL.GS.ServerRules
                 };
 
                 NotifyNearbyPlayers(killedNpc, money, playersInRadius);
-                money.AddToWorld();
 
                 // Attempt auto pick up.
                 foreach (ItemOwnerTotalDamagePair itemOwner in itemOwners)
@@ -1539,6 +1538,8 @@ namespace DOL.GS.ServerRules
                     if (money.TryAutoPickUp(itemOwner.Owner))
                         return;
                 }
+
+                money.AddToWorld();
             }
 
             static void CreateItem(GameNPC killedNpc, DbItemTemplate itemTemplate, SortedSet<ItemOwnerTotalDamagePair> itemOwners, List<GamePlayer> nearbyPlayers)
@@ -1575,7 +1576,6 @@ namespace DOL.GS.ServerRules
                 };
 
                 NotifyNearbyPlayers(killedNpc, item, nearbyPlayers);
-                item.AddToWorld();
 
                 // Attempt auto pick up.
                 foreach (ItemOwnerTotalDamagePair itemOwner in itemOwners)
@@ -1585,6 +1585,8 @@ namespace DOL.GS.ServerRules
                     if (item.TryAutoPickUp(itemOwner.Owner))
                         return;
                 }
+
+                item.AddToWorld();
             }
 
             static void NotifyNearbyPlayers(GameNPC killedNpc, GameStaticItemTimed item, List<GamePlayer> nearbyPlayers)

commit 81025bada68412f29b55dbe0a926941be4034998

Fix items not spawning on ground Regression caused by 54a1c10a807c4185799ea72548aa69e0f52d3bb0

GameServer/gameobjects/GameMoney.cs
@@ -37,11 +37,11 @@ namespace DOL.GS
             return _names.Contains(name);
         }
 
-        public override bool TryAutoPickUp(IGameStaticItemOwner itemOwner)
+        public override TryPickUpResult TryAutoPickUp(IGameStaticItemOwner itemOwner)
         {
             lock (_pickUpLock)
             {
-                return ObjectState is not eObjectState.Deleted && itemOwner.TryAutoPickUpMoney(this);
+                return ObjectState is eObjectState.Deleted ? TryPickUpResult.DoesNotWant : itemOwner.TryAutoPickUpMoney(this);
             }
         }
 
@@ -49,7 +49,7 @@ namespace DOL.GS
         {
             lock (_pickUpLock)
             {
-                return ObjectState is not eObjectState.Deleted ? itemOwner.TryPickUpMoney(source, this) : TryPickUpResult.FAILED;
+                return ObjectState is eObjectState.Deleted ? TryPickUpResult.DoesNotWant : itemOwner.TryPickUpMoney(source, this);
             }
         }
     }

GameServer/gameobjects/GamePlayer.cs
@@ -9899,11 +9899,11 @@ namespace DOL.GS
 
                 BattleGroup battleGroup = TempProperties.GetProperty<BattleGroup>(BattleGroup.BATTLEGROUP_PROPERTY);
 
-                if (battleGroup == null || floorItem.TryPickUp(this, battleGroup) is TryPickUpResult.DOES_NOT_HANDLE)
+                if (battleGroup == null || floorItem.TryPickUp(this, battleGroup) is TryPickUpResult.DoesNotWant)
                 {
                     Group group = Group;
 
-                    if (group == null || floorItem.TryPickUp(this, group) is TryPickUpResult.DOES_NOT_HANDLE)
+                    if (group == null || floorItem.TryPickUp(this, group) is TryPickUpResult.DoesNotWant)
                         floorItem.TryPickUp(this, this);
                 }
 
@@ -9914,11 +9914,11 @@ namespace DOL.GS
             {
                 BattleGroup battleGroup = TempProperties.GetProperty<BattleGroup>(BattleGroup.BATTLEGROUP_PROPERTY);
 
-                if (battleGroup == null || money.TryPickUp(this, battleGroup) is TryPickUpResult.DOES_NOT_HANDLE)
+                if (battleGroup == null || money.TryPickUp(this, battleGroup) is TryPickUpResult.DoesNotWant)
                 {
                     Group group = Group;
 
-                    if (group == null || money.TryPickUp(this, group) is TryPickUpResult.DOES_NOT_HANDLE)
+                    if (group == null || money.TryPickUp(this, group) is TryPickUpResult.DoesNotWant)
                         money.TryPickUp(this, this);
                 }
 
@@ -9969,14 +9969,14 @@ namespace DOL.GS
 
         public object GameStaticItemOwnerComparand => AccountName;
 
-        public bool TryAutoPickUpMoney(GameMoney money)
+        public TryPickUpResult TryAutoPickUpMoney(GameMoney money)
         {
-            return Autoloot && TryPickUpMoney(this, money) is not TryPickUpResult.DOES_NOT_HANDLE;
+            return Autoloot ? TryPickUpMoney(this, money) : TryPickUpResult.DoesNotWant;
         }
 
-        public bool TryAutoPickUpItem(WorldInventoryItem inventoryItem)
+        public TryPickUpResult TryAutoPickUpItem(WorldInventoryItem inventoryItem)
         {
-            return Autoloot && TryPickUpItem(this, inventoryItem) is not TryPickUpResult.DOES_NOT_HANDLE;
+            return Autoloot ? TryPickUpItem(this, inventoryItem) : TryPickUpResult.DoesNotWant;
         }
 
         public TryPickUpResult TryPickUpMoney(GamePlayer source, GameMoney money)
@@ -9988,7 +9988,7 @@ namespace DOL.GS
                 if (log.IsErrorEnabled)
                     log.Error($"The passed down {nameof(source)} isn't equal to 'this'. Money pick up aborted. ({nameof(source)}: {source}) (this: {this})");
 
-                return TryPickUpResult.DOES_NOT_HANDLE;
+                return TryPickUpResult.DoesNotWant;
             }
 
             long moneyToPlayer = ApplyGuildDues(money.Value);
@@ -10000,7 +10000,7 @@ namespace DOL.GS
             }
 
             money.RemoveFromWorld();
-            return TryPickUpResult.SUCCESS;
+            return TryPickUpResult.Success;
         }
 
         public TryPickUpResult TryPickUpItem(GamePlayer source, WorldInventoryItem item)
@@ -10012,20 +10012,20 @@ namespace DOL.GS
                 if (log.IsErrorEnabled)
                     log.Error($"The passed down {nameof(source)} isn't equal to 'this'. Item pick up aborted. ({nameof(source)}: {source}) (this: {this})");
 
-                return TryPickUpResult.DOES_NOT_HANDLE;
+                return TryPickUpResult.DoesNotWant;
             }
 
             if (!GiveItem(this, item.Item))
             {
                 Out.SendMessage(LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.PickupObject.BackpackFull"), eChatType.CT_System, eChatLoc.CL_SystemWindow);
-                return TryPickUpResult.FAILED;
+                return TryPickUpResult.Blocked;
             }
 
             Out.SendMessage(LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.PickupObject.YouGet", item.Item.GetName(1, false)), eChatType.CT_System, eChatLoc.CL_SystemWindow);
             Message.SystemToOthers(this, LanguageMgr.GetTranslation(Client.Account.Language, "GamePlayer.PickupObject.GroupMemberPicksUp", Name, item.Item.GetName(1, false)), eChatType.CT_System);
             InventoryLogging.LogInventoryAction("(ground)", this, eInventoryActionType.Loot, item.Item.Template, item.Item.IsStackable ? item.Item.Count : 1);
             item.RemoveFromWorld();
-            return TryPickUpResult.SUCCESS;
+            return TryPickUpResult.Success;
 
             static bool GiveItem(GamePlayer player, DbInventoryItem item)
             {

GameServer/gameobjects/GameStaticItemTimed.cs
@@ -79,7 +79,7 @@ namespace DOL.GS
                 throw new InvalidOperationException($"{Name} is not locked by the current thread.");
         }
 
-        public abstract bool TryAutoPickUp(IGameStaticItemOwner itemOwner);
+        public abstract TryPickUpResult TryAutoPickUp(IGameStaticItemOwner itemOwner);
         public abstract TryPickUpResult TryPickUp(GamePlayer source, IGameStaticItemOwner itemOwner);
 
         protected class RemoveItemAction : ECSGameTimerWrapperBase
@@ -100,8 +100,8 @@ namespace DOL.GS
 
         string Name { get; }
         object GameStaticItemOwnerComparand { get; } // Used by `GameStaticItemOwnerEqualityComparer`. Can be null, in which case it will defer to the object's `Equals` and `GetHashCode`.
-        bool TryAutoPickUpMoney(GameMoney money);
-        bool TryAutoPickUpItem(WorldInventoryItem item);
+        TryPickUpResult TryAutoPickUpMoney(GameMoney money);
+        TryPickUpResult TryAutoPickUpItem(WorldInventoryItem item);
         TryPickUpResult TryPickUpMoney(GamePlayer source, GameMoney money); // Expected to return false only if the object shouldn't try to pick up the item at all.
         TryPickUpResult TryPickUpItem(GamePlayer source, WorldInventoryItem item); // Expected to return false only if the object shouldn't try to pick up the item at all.
 
@@ -139,8 +139,8 @@ namespace DOL.GS
 
     public enum TryPickUpResult
     {
-        SUCCESS,               // The item was picked up.
-        DOES_NOT_HANDLE,       // The item cannot be handled by this owner and we should fall back to the next one.
-        FAILED                 // The item can be handled by this owner, but failed (inventory full, no one in range, already picked up, etc.)
+        Success,    // The item was picked up.
+        Blocked,    // The item couldn't be picked up due to inventory being full, player not in range, etc.
+        DoesNotWant // The item is rejected due to player, group, or battlegroup settings.
     }
 }

GameServer/gameobjects/WorldInventoryItem.cs
@@ -139,11 +139,11 @@ namespace DOL.GS
             _pickupTimer = null;
         }
 
-        public override bool TryAutoPickUp(IGameStaticItemOwner itemOwner)
+        public override TryPickUpResult TryAutoPickUp(IGameStaticItemOwner itemOwner)
         {
             lock (_pickUpLock)
             {
-                return ObjectState is not eObjectState.Deleted && itemOwner.TryAutoPickUpItem(this);
+                return ObjectState is eObjectState.Deleted ? TryPickUpResult.DoesNotWant : itemOwner.TryAutoPickUpItem(this);
             }
         }
 
@@ -151,7 +151,7 @@ namespace DOL.GS
         {
             lock (_pickUpLock)
             {
-                return ObjectState is not eObjectState.Deleted ? itemOwner.TryPickUpItem(source, this) : TryPickUpResult.FAILED;
+                return ObjectState is eObjectState.Deleted ? TryPickUpResult.DoesNotWant : itemOwner.TryPickUpItem(source, this);
             }
         }
     }

GameServer/gameutils/BattleGroup.cs
@@ -291,16 +291,16 @@ namespace DOL.GS
 
 		public object GameStaticItemOwnerComparand => null;
 
-		public bool TryAutoPickUpMoney(GameMoney money)
+		public TryPickUpResult TryAutoPickUpMoney(GameMoney money)
 		{
-			return TryPickUpMoney(Leader as GamePlayer, money) is not TryPickUpResult.DOES_NOT_HANDLE;
+			return TryPickUpMoney(Leader as GamePlayer, money);
 		}
 
-		public bool TryAutoPickUpItem(WorldInventoryItem worldItem)
+		public TryPickUpResult TryAutoPickUpItem(WorldInventoryItem worldItem)
 		{
 			// We don't care if players have auto loot enabled, or if they can see the item (the item isn't added to the world yet anyway), or who attacked last, etc.
-			// This is especially important for battlegroups since can have an illimited amount of players.
-			return TryPickUpItem(battlegroupTreasurer, worldItem) is not TryPickUpResult.DOES_NOT_HANDLE;
+			// This is especially important for battlegroups since they can have an illimited amount of players.
+			return TryPickUpItem(battlegroupTreasurer, worldItem);
 		}
 
 		public TryPickUpResult TryPickUpMoney(GamePlayer source, GameMoney money)
@@ -308,7 +308,7 @@ namespace DOL.GS
 			money.AssertLockAcquisition();
 
 			// Splitting money in a battlegroup could cause performance issues. Let it fallback to group then solo logic.
-			return TryPickUpResult.DOES_NOT_HANDLE;
+			return TryPickUpResult.DoesNotWant;
 		}
 
 		public TryPickUpResult TryPickUpItem(GamePlayer source, WorldInventoryItem item)
@@ -320,19 +320,19 @@ namespace DOL.GS
 			// If his inventory is full, the item should simply stay on the ground until he makes some room, or another treasurer is appointed.
 			// Items discarded by players can only be picked up by those same players.
 			if (!GetBGLootType() || battlegroupTreasurer == null || item.IsPlayerDiscarded)
-				return TryPickUpResult.DOES_NOT_HANDLE;
+				return TryPickUpResult.DoesNotWant;
 
 			if (!GiveItem(battlegroupTreasurer, item.Item))
 			{
 				battlegroupTreasurer.Out.SendMessage(LanguageMgr.GetTranslation(battlegroupTreasurer.Client.Account.Language, "GamePlayer.PickupObject.BackpackFull"), eChatType.CT_System, eChatLoc.CL_SystemWindow);
-				return TryPickUpResult.FAILED;
+				return TryPickUpResult.Blocked;
 			}
 
 			battlegroupTreasurer.Out.SendMessage(LanguageMgr.GetTranslation(battlegroupTreasurer.Client.Account.Language, "GamePlayer.PickupObject.YouGet", item.Item.GetName(1, false)), eChatType.CT_System, eChatLoc.CL_SystemWindow);
 			Message.SystemToOthers(source, LanguageMgr.GetTranslation(battlegroupTreasurer.Client.Account.Language, "GamePlayer.PickupObject.GroupMemberPicksUp", Name, item.Item.GetName(1, false)), eChatType.CT_System);
 			InventoryLogging.LogInventoryAction("(ground)", battlegroupTreasurer, eInventoryActionType.Loot, item.Item.Template, item.Item.IsStackable ? item.Item.Count : 1);
 			item.RemoveFromWorld();
-			return TryPickUpResult.SUCCESS;
+			return TryPickUpResult.Success;
 
 			static bool GiveItem(GamePlayer player, DbInventoryItem item)
 			{

GameServer/gameutils/Group.cs
@@ -623,15 +623,15 @@ namespace DOL.GS
 
 		public object GameStaticItemOwnerComparand => null;
 
-		public bool TryAutoPickUpMoney(GameMoney money)
+		public TryPickUpResult TryAutoPickUpMoney(GameMoney money)
 		{
-			return TryPickUpMoney(Leader, money) is not TryPickUpResult.DOES_NOT_HANDLE;
+			return TryPickUpMoney(Leader, money);
 		}
 
-		public bool TryAutoPickUpItem(WorldInventoryItem inventoryItem)
+		public TryPickUpResult TryAutoPickUpItem(WorldInventoryItem inventoryItem)
 		{
 			// We don't care if players have auto loot enabled, or if they can see the item (the item isn't added to the world yet anyway), or who attacked last, etc.
-			return TryPickUpItem(Leader, inventoryItem) is not TryPickUpResult.DOES_NOT_HANDLE;
+			return TryPickUpItem(Leader, inventoryItem);
 		}
 
 		public TryPickUpResult TryPickUpMoney(GamePlayer source, GameMoney money)
@@ -639,7 +639,7 @@ namespace DOL.GS
 			money.AssertLockAcquisition();
 
 			if (!AutosplitCoins)
-				return TryPickUpResult.DOES_NOT_HANDLE;
+				return TryPickUpResult.DoesNotWant;
 
 			List<GamePlayer> eligibleMembers = new(8);
 
@@ -654,12 +654,12 @@ namespace DOL.GS
 			if (eligibleMembers.Count == 0)
 			{
 				source.Out.SendMessage(LanguageMgr.GetTranslation(source.Client.Account.Language, "GamePlayer.PickupObject.NoOneGroupWantsMoney"), eChatType.CT_System, eChatLoc.CL_SystemWindow);
-				return TryPickUpResult.FAILED;
+				return TryPickUpResult.Blocked;
 			}
 
 			SplitMoneyBetweenEligibleMembers(eligibleMembers, money);
 			money.RemoveFromWorld();
-			return TryPickUpResult.SUCCESS;
+			return TryPickUpResult.Success;
 
 			static void SplitMoneyBetweenEligibleMembers(List<GamePlayer> eligibleMembers, GameMoney money)
 			{
@@ -689,7 +689,7 @@ namespace DOL.GS
 			// If there is none, the item should simply stays on the ground.
 			// Items discarded by players can only be picked up by those same players.
 			if (!AutosplitLoot || item.IsPlayerDiscarded)
-				return TryPickUpResult.DOES_NOT_HANDLE;
+				return TryPickUpResult.DoesNotWant;
 
 			List<GamePlayer> eligibleMembers = new(8);
 
@@ -703,17 +703,17 @@ namespace DOL.GS
 			if (eligibleMembers.Count == 0)
 			{
 				source.Out.SendMessage(LanguageMgr.GetTranslation(source.Client.Account.Language, "GamePlayer.PickupObject.NoOneWantsThis", item.Name), eChatType.CT_System, eChatLoc.CL_SystemWindow);
-				return TryPickUpResult.FAILED;
+				return TryPickUpResult.Blocked;
 			}
 
 			if (!GiveItemToRandomEligibleMember(eligibleMembers, item.Item, out GamePlayer eligibleMember))
-				return TryPickUpResult.FAILED;
+				return TryPickUpResult.Blocked;
 
 			Message.SystemToOthers(source, LanguageMgr.GetTranslation(source.Client.Account.Language, "GamePlayer.PickupObject.GroupMemberPicksUp", Name, item.Item.GetName(1, false)), eChatType.CT_System);
 			SendMessageToGroupMembers(LanguageMgr.GetTranslation(source.Client.Account.Language, "GamePlayer.PickupObject.Autosplit", item.Item.GetName(1, true), eligibleMember.Name), eChatType.CT_System, eChatLoc.CL_SystemWindow);
 			InventoryLogging.LogInventoryAction("(ground)", eligibleMember, eInventoryActionType.Loot, item.Item.Template, item.Item.IsStackable ? item.Item.Count : 1);
 			item.RemoveFromWorld();
-			return TryPickUpResult.SUCCESS;
+			return TryPickUpResult.Success;
 
 			static bool GiveItemToRandomEligibleMember(List<GamePlayer> eligibleMembers, DbInventoryItem item, out GamePlayer eligibleMember)
 			{

GameServer/serverrules/AbstractServerRules.cs
@@ -1534,9 +1534,14 @@ namespace DOL.GS.ServerRules
                 foreach (ItemOwnerTotalDamagePair itemOwner in itemOwners)
                 {
                     money.AddOwner(itemOwner.Owner);
+                    TryPickUpResult result = money.TryAutoPickUp(itemOwner.Owner);
 
-                    if (money.TryAutoPickUp(itemOwner.Owner))
+                    if (result is TryPickUpResult.Success)
                         return;
+                    else if (result is TryPickUpResult.DoesNotWant)
+                        continue;
+                    else if (result is TryPickUpResult.Blocked)
+                        break;
                 }
 
                 money.AddToWorld();
@@ -1581,9 +1586,14 @@ namespace DOL.GS.ServerRules
                 foreach (ItemOwnerTotalDamagePair itemOwner in itemOwners)
                 {
                     item.AddOwner(itemOwner.Owner);
+                    TryPickUpResult result = item.TryAutoPickUp(itemOwner.Owner);
 
-                    if (item.TryAutoPickUp(itemOwner.Owner))
+                    if (result is TryPickUpResult.Success)
                         return;
+                    else if (result is TryPickUpResult.DoesNotWant)
+                        continue;
+                    else if (result is TryPickUpResult.Blocked)
+                        break;
                 }
 
                 item.AddToWorld();