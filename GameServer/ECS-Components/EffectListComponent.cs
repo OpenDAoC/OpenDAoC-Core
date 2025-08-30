OpenDAoC/OpenDAoC-Core

commit a4e9f9cb64cf80e067c27d885440e4e6ba2c787f

`EffectListComponent`: Merge pending effect queues Enqueuing order must be preserved when dequeuing. Note: `OnStartEffect` is no longer called if `effect.IsActive` isn't true. `OnStopEffect` is no longer called if `effect.IsStopped` isn't true. And error will be logged if both are false, since this can result in permanent effects.

GameServer/ECS-Components/EffectListComponent.cs
@@ -22,8 +22,7 @@ namespace DOL.GS
         private readonly Dictionary<int, ECSGameEffect> _effectIdToEffect = new();   // Dictionary of effects by their icon ID.
 
         // Pending effects.
-        private readonly Queue<ECSGameEffect> _effectsToStop = new();                // Queue for effects to stop after their state has been finalized following removal or disabling.
-        private readonly Queue<ECSGameEffect> _effectsToStart = new();               // Queue for effects to start after their state has been finalized following addition or enabling.
+        private readonly Queue<ECSGameEffect> _effectsToStartOrStop = new();         // Queue for effects to start or stop after their state has been finalized.
 
         // Concentration.
         private readonly List<ECSGameSpellEffect> _concentrationEffects = new(20);   // List of concentration effects currently active on the player.
@@ -48,29 +47,28 @@ namespace DOL.GS
 
         public void Tick()
         {
-            while (_effectsToStart.TryDequeue(out ECSGameEffect effect))
+            while (_effectsToStartOrStop.TryDequeue(out ECSGameEffect effect))
             {
                 if (effect.IsActive)
-                    ServiceObjectStore.Add(effect);
-
-                effect.OnStartEffect();
-                effect.IsBeingReplaced = false;
-            }
-
-            while (_effectsToStop.TryDequeue(out ECSGameEffect effect))
-            {
-                if (effect.IsStopped)
-                    ServiceObjectStore.Remove(effect);
-
-                effect.OnStopEffect();
-
-                if (!effect.IsBeingReplaced)
                 {
-                    effect.TryApplyImmunity();
-                    TryEnableBestEffectOfSameType(effect);
+                    ServiceObjectStore.Add(effect);
+                    effect.OnStartEffect();
                 }
-                else
-                    effect.IsBeingReplaced = false;
+                else if (effect.IsStopped)
+                {
+                    ServiceObjectStore.Remove(effect);
+                    effect.OnStopEffect();
+
+                    if (!effect.IsBeingReplaced)
+                    {
+                        effect.TryApplyImmunity();
+                        TryEnableBestEffectOfSameType(effect);
+                    }
+                }
+                else if (log.IsErrorEnabled)
+                    log.Error($"Effect was enqueued to start or stop but is neither active nor stopped: {effect.Name} ({effect.EffectType}) on {Owner}");
+
+                effect.IsBeingReplaced = false;
             }
 
             if (_effects.Count == 0)
@@ -729,7 +727,7 @@ namespace DOL.GS
                             EffectHelper.SendSpellAnimation(spellEffect);
                     }
 
-                    _effectsToStart.Enqueue(effect);
+                    _effectsToStartOrStop.Enqueue(effect);
                 }
                 catch (Exception e)
                 {
@@ -831,7 +829,7 @@ namespace DOL.GS
                     RequestPlayerUpdate(EffectHelper.GetPlayerUpdateFromEffect(effect.EffectType));
 
                     if (effect.FinalizeState(result))
-                        _effectsToStop.Enqueue(effect);
+                        _effectsToStartOrStop.Enqueue(effect);
                 }
                 catch (Exception e)
                 {

commit 7093446b0883b2600b52df13989e0e22d0641fb9

Fix effect issues caused by a4e9f9cb64cf80e067c27d885440e4e6ba2c787f * `ECSGameEffect` is now reponsible to add to or remove itself from `ServiceObjectStore` * Added missing handling of disabled effects in `EffectListComponent.Tick`

GameServer/ECS-Components/EffectListComponent.cs
@@ -50,13 +50,9 @@ namespace DOL.GS
             while (_effectsToStartOrStop.TryDequeue(out ECSGameEffect effect))
             {
                 if (effect.IsActive)
-                {
-                    ServiceObjectStore.Add(effect);
                     effect.OnStartEffect();
-                }
-                else if (effect.IsStopped)
+                else if (effect.IsDisabled || effect.IsStopped)
                 {
-                    ServiceObjectStore.Remove(effect);
                     effect.OnStopEffect();
 
                     if (!effect.IsBeingReplaced)
@@ -704,6 +700,8 @@ namespace DOL.GS
                     if (!effect.FinalizeState(result))
                         return;
 
+                    _effectsToStartOrStop.Enqueue(effect);
+
                     if (effect is ECSGameSpellEffect spellEffect)
                     {
                         ISpellHandler spellHandler = spellEffect.SpellHandler;
@@ -726,8 +724,6 @@ namespace DOL.GS
                         else if (spellEffect is not ECSImmunityEffect)
                             EffectHelper.SendSpellAnimation(spellEffect);
                     }
-
-                    _effectsToStartOrStop.Enqueue(effect);
                 }
                 catch (Exception e)
                 {

GameServer/ECS-Effects/ECSGameEffect.cs
@@ -211,21 +211,26 @@ namespace DOL.GS
                 {
                     case EffectListComponent.AddEffectResult.Added:
                     {
+                        ServiceObjectStore.Add(this);
                         _state = State.Active;
                         return true;
                     }
                     case EffectListComponent.AddEffectResult.RenewedActive:
                     {
+                        ServiceObjectStore.Add(this);
                         _state = State.Active;
                         return false;
                     }
                     case EffectListComponent.AddEffectResult.Disabled:
                     {
+                        ServiceObjectStore.Add(this);
                         _state = State.Disabled;
                         return false;
                     }
                     case EffectListComponent.AddEffectResult.RenewedDisabled:
                     {
+                        ServiceObjectStore.Add(this);
+
                         if (IsDisabled)
                         {
                             _state = State.Active;
@@ -238,7 +243,6 @@ namespace DOL.GS
                         }
                     }
                     case EffectListComponent.AddEffectResult.Failed:
-                        return false;
                     default:
                         throw new InvalidOperationException($"Unhandled result: {result}.");
                 }
@@ -258,18 +262,19 @@ namespace DOL.GS
                 {
                     case EffectListComponent.RemoveEffectResult.Removed:
                     {
+                        ServiceObjectStore.Remove(this);
                         bool shouldBeStopped = IsActive;
                         _state = State.Stopped;
                         return shouldBeStopped;
                     }
                     case EffectListComponent.RemoveEffectResult.Disabled:
                     {
+                        ServiceObjectStore.Add(this);
                         bool shouldBeStopped = IsActive;
                         _state = State.Disabled;
                         return shouldBeStopped;
                     }
                     case EffectListComponent.RemoveEffectResult.Failed:
-                        return false;
                     default:
                         throw new InvalidOperationException($"Unhandled result: {result}.");
                 }