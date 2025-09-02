OpenDAoC/OpenDAoC-Core

commit 0268238e0f49291ecfbc28f3186d17e22ead8519

Make `EffectListcomponent` use `TickListPoolManager`

GameServer/ECS-Components/EffectListComponent.cs
@@ -131,15 +131,19 @@ namespace DOL.GS
 
         public List<ECSGameSpellEffect> GetConcentrationEffects()
         {
+            List<ECSGameSpellEffect> temp = GameLoop.GetListForTick<ECSGameSpellEffect>();
+
             lock (_concentrationEffectsLock)
             {
-                return _concentrationEffects.ToList();
+                temp.AddRange(_concentrationEffects);
             }
+
+            return temp;
         }
 
         public List<ECSGameEffect> GetEffects()
         {
-            List<ECSGameEffect> temp = new();
+            List<ECSGameEffect> temp = GameLoop.GetListForTick<ECSGameEffect>();
 
             foreach (var pair in _effects)
             {
@@ -157,7 +161,7 @@ namespace DOL.GS
 
         public List<ECSGameEffect> GetEffects(eEffect effectType)
         {
-            List<ECSGameEffect> temp = new();
+            List<ECSGameEffect> temp = GameLoop.GetListForTick<ECSGameEffect>();
 
             if (_effects.TryGetValue(effectType, out List<ECSGameEffect> effects))
             {
@@ -173,7 +177,7 @@ namespace DOL.GS
 
         public List<ECSPulseEffect> GetPulseEffects()
         {
-            List<ECSPulseEffect> temp = new();
+            List<ECSPulseEffect> temp = GameLoop.GetListForTick<ECSPulseEffect>();
 
             foreach (var pair in _effects)
             {
@@ -191,7 +195,7 @@ namespace DOL.GS
 
         public ECSGameSpellEffect GetBestDisabledSpellEffect(eEffect effectType)
         {
-            List<ECSGameSpellEffect> effects = GetSpellEffects(effectType);
+            List<ECSGameSpellEffect> effects = GameLoop.GetListForTick<ECSGameSpellEffect>();
 
             if (effects == null || effects.Count == 0)
                 return null;
@@ -220,7 +224,7 @@ namespace DOL.GS
 
         public List<ECSGameSpellEffect> GetSpellEffects()
         {
-            List<ECSGameSpellEffect> temp = new();
+            List<ECSGameSpellEffect> temp = GameLoop.GetListForTick<ECSGameSpellEffect>();
 
             foreach (var pair in _effects)
             {
@@ -238,7 +242,7 @@ namespace DOL.GS
 
         public List<ECSGameSpellEffect> GetSpellEffects(eEffect effectType)
         {
-            List<ECSGameSpellEffect> temp = new();
+            List<ECSGameSpellEffect> temp = GameLoop.GetListForTick<ECSGameSpellEffect>();
 
             if (_effects.TryGetValue(effectType, out List<ECSGameEffect> effects))
             {
@@ -254,7 +258,7 @@ namespace DOL.GS
 
         public List<ECSGameAbilityEffect> GetAbilityEffects()
         {
-            List<ECSGameAbilityEffect> temp = new();
+            List<ECSGameAbilityEffect> temp = GameLoop.GetListForTick<ECSGameAbilityEffect>();
 
             foreach (var pair in _effects)
             {
@@ -272,7 +276,7 @@ namespace DOL.GS
 
         public List<ECSGameAbilityEffect> GetAbilityEffects(eEffect effectType)
         {
-            List<ECSGameAbilityEffect> temp = new();
+            List<ECSGameAbilityEffect> temp = GameLoop.GetListForTick<ECSGameAbilityEffect>();
 
             if (_effects.TryGetValue(effectType, out List<ECSGameEffect> effects))
             {
@@ -600,13 +604,13 @@ namespace DOL.GS
                                     {
                                         if (!existingEffect.IsDisabled && !existingEffect.IsDisabling)
                                         {
-                                            effectsToDisable ??= new(1);
+                                            effectsToDisable ??= GameLoop.GetListForTick<ECSGameEffect>();
                                             effectsToDisable.Add(existingEffect);
                                         }
                                     }
                                     else
                                     {
-                                        effectsToStop ??= new(1);
+                                        effectsToStop ??= GameLoop.GetListForTick<ECSGameEffect>();
                                         effectsToStop.Add(existingEffect);
                                     }
                                 }

GameServer/ECS-Effects/ECSGameAbilityEffect.cs
@@ -3,13 +3,10 @@ namespace DOL.GS
     /// <summary>
     /// Ability-Based Effect
     /// </summary>
-    public class ECSGameAbilityEffect : ECSGameEffect
+    public class ECSGameAbilityEffect : ECSGameEffect, IPooledList<ECSGameAbilityEffect>
     {
         public override string Name { get { return "Default Ability Name"; } }
 
-        public ECSGameAbilityEffect(in ECSGameEffectInitParams initParams) : base(initParams)
-        {
-            //EffectService.RequestStartEffect(this);
-        }
+        public ECSGameAbilityEffect(in ECSGameEffectInitParams initParams) : base(initParams) { }
     }
 }

GameServer/ECS-Effects/ECSGameEffect.cs
@@ -8,7 +8,7 @@ using DOL.Language;
 
 namespace DOL.GS
 {
-    public abstract class ECSGameEffect : IServiceObject
+    public abstract class ECSGameEffect : IServiceObject, IPooledList<ECSGameEffect>
     {
         private State _state;
         private TransitionalState _transitionalState;

GameServer/ECS-Effects/ECSGameSpellEffect.cs
@@ -9,7 +9,7 @@ namespace DOL.GS
     /// <summary>
     /// Spell-Based Effect
     /// </summary>
-    public class ECSGameSpellEffect : ECSGameEffect, IConcentrationEffect
+    public class ECSGameSpellEffect : ECSGameEffect, IConcentrationEffect, IPooledList<ECSGameSpellEffect>
     {
         public new ISpellHandler SpellHandler;
         string IConcentrationEffect.Name => Name;

GameServer/ECS-Effects/ECSPulseEffect.cs
@@ -3,7 +3,7 @@ using DOL.GS.Effects;
 
 namespace DOL.GS
 {
-    public class ECSPulseEffect : ECSGameSpellEffect, IConcentrationEffect
+    public class ECSPulseEffect : ECSGameSpellEffect, IConcentrationEffect, IPooledList<ECSPulseEffect>
     {
         /// <summary>
         /// The name of the owner

commit 56510d34754630e369ac7f4298fa39acc0d81728

Fix `GetBestDisabledSpellEffect` Regression caused by 0268238e0f49291ecfbc28f3186d17e22ead8519

GameServer/ECS-Components/EffectListComponent.cs
@@ -197,7 +197,7 @@ namespace DOL.GS
 
         public ECSGameSpellEffect GetBestDisabledSpellEffect(eEffect effectType)
         {
-            List<ECSGameSpellEffect> effects = GameLoop.GetListForTick<ECSGameSpellEffect>();
+            List<ECSGameSpellEffect> effects = GetSpellEffects(effectType);
 
             if (effects == null || effects.Count == 0)
                 return null;