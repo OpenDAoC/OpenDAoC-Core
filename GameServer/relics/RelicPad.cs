3d793086763fdcfaa7b9d30289f9ac70c82845d7
Baptiste Marie <bm01@users.noreply.github.com>
2025-09-04 08:59:10 +0200

    `RelicPad`: Fix thread unsafeness on `PlayersOnPad` Just in case it gets used some day.

--- GameServer/relics/RelicPad.cs
+++ GameServer/relics/RelicPad.cs
@@ -1,113 +1,74 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
+using System.Threading;
 
 namespace DOL.GS.Relics
 {
-	/// <summary>
-	/// Class representing a relic pad.
-	/// </summary>
-	/// <author>Aredhel</author>
-	public class RelicPad : GameObject
-	{
-		/// <summary>
-		/// The pillar this pad triggers.
-		/// </summary>
-		private RelicPillar m_relicPillar;
+    /// <summary>
+    /// Class representing a relic pad.
+    /// </summary>
+    public class RelicPad : GameObject
+    {
+        /// <summary>
+        /// The pillar this pad triggers.
+        /// </summary>
+        private RelicPillar _relicPillar;
+        private int _playersOnPad;
+        private Lock _lock = new();
 
-		public override eGameObjectType GameObjectType => eGameObjectType.KEEP_COMPONENT;
+        static public int Radius => 200;
+        public override eGameObjectType GameObjectType => eGameObjectType.KEEP_COMPONENT;
 
-		public RelicPad(RelicPillar relicPillar)
-		{
-			m_relicPillar = relicPillar;
-		}
+        public RelicPad(RelicPillar relicPillar)
+        {
+            _relicPillar = relicPillar;
+        }
 
-		/// <summary>
-		/// Relic pad radius.
-		/// </summary>
-		static public int Radius
-		{
-			get { return 200; }
-		}
+        public void OnPlayerEnter(GamePlayer player)
+        {
+            lock (_lock)
+            {
+                _playersOnPad++;
+                CheckPadState();
+            }
+        }
 
-		private int m_playersOnPad = 0;
+        public void OnPlayerLeave(GamePlayer player)
+        {
+            lock (_lock)
+            {
+                _playersOnPad--;
+                CheckPadState();
+            }
+        }
 
-		/// <summary>
-		/// The number of players currently on the pad.
-		/// </summary>
-		public int PlayersOnPad
-		{
-			get { return m_playersOnPad; }
-			set 
-			{
-				if (value < 0)
-					return;
+        private void CheckPadState()
+        {
+            if (_playersOnPad >= ServerProperties.Properties.RELIC_PLAYERS_REQUIRED_ON_PAD && _relicPillar.State is eDoorState.Closed)
+                _relicPillar.Open();
+            else if (_relicPillar.State is eDoorState.Open && _playersOnPad <= 0)
+                _relicPillar.Close();
+        }
 
-				m_playersOnPad = value;
+        /// <summary>
+        /// Class to register players entering or leaving the pad.
+        /// </summary>
+        public class Surface : Area.Circle
+        {
+            private RelicPad _relicPad;
 
-				if (m_playersOnPad >= ServerProperties.Properties.RELIC_PLAYERS_REQUIRED_ON_PAD &&
-					m_relicPillar.State == eDoorState.Closed)
-					m_relicPillar.Open();
-				else if (m_relicPillar.State == eDoorState.Open && m_playersOnPad <= 0)
-					m_relicPillar.Close();
-			}
-		}
+            public Surface(RelicPad relicPad) : base("", relicPad.X, relicPad.Y, relicPad.Z, RelicPad.Radius)
+            {
+                _relicPad = relicPad;
+            }
 
-		/// <summary>
-		/// Called when a players steps on the pad.
-		/// </summary>
-		/// <param name="player"></param>
-		public void OnPlayerEnter(GamePlayer player)
-		{
-			PlayersOnPad++;
-		}
+            public override void OnPlayerEnter(GamePlayer player)
+            {
+                _relicPad.OnPlayerEnter(player);
+            }
 
-		/// <summary>
-		/// Called when a player steps off the pad.
-		/// </summary>
-		/// <param name="player"></param>
-		public void OnPlayerLeave(GamePlayer player)
-		{
-			PlayersOnPad--;
-		}
-
-		/// <summary>
-		/// Class to register players entering or leaving the pad.
-		/// </summary>
-		public class Surface : Area.Circle
-		{
-			private RelicPad m_relicPad;
-
-			public Surface(RelicPad relicPad)
-				: base("", relicPad.X, relicPad.Y, relicPad.Z, RelicPad.Radius)
-			{
-				m_relicPad = relicPad;
-			}
-
-			public override void OnPlayerEnter(GamePlayer player)
-			{
-				m_relicPad.OnPlayerEnter(player);
-			}
-
-			public override void OnPlayerLeave(GamePlayer player)
-			{
-				m_relicPad.OnPlayerLeave(player);
-			}
-		}
-	}
+            public override void OnPlayerLeave(GamePlayer player)
+            {
+                _relicPad.OnPlayerLeave(player);
+            }
+        }
+    }
 }
