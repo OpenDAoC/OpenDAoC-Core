2bddb3c9991376b2d09c358b8aba30e5de4fbbcd
Baptiste Marie <bm01@users.noreply.github.com>
2025-09-07 00:58:52 +0200

    Amend previous commit

--- GameServer/gameobjects/GamePlayer.cs
+++ GameServer/gameobjects/GamePlayer.cs
@@ -4664,13 +4664,14 @@ namespace DOL.GS
             get { return DBCharacter != null ? (byte)DBCharacter.Level : base.Level; }
             set
             {
+                int oldLevel = Level;
+
                 // `base.Level` ends up having to call the getter (in `SendLivingDataUpdate`).
                 // Which, in the case of `GamePlayer`, accesses `DBCharacter`.
                 // For this reason, `DBCharacter.Level` must be set first.
                 if (DBCharacter != null)
                     DBCharacter.Level = value;
 
-                int oldLevel = Level;
                 base.Level = value;
 
                 if (oldLevel > 0)
