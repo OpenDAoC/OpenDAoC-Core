41a354628724cf19c73ed856ba43516e7eaf0345
Baptiste Marie <bm01@users.noreply.github.com>
2025-09-21 02:19:16 +0200

    `StartupEquipment`: Don't change the template's Realm

--- GameServer/scripts/startup/script/StartupEquipment.cs
+++ GameServer/scripts/startup/script/StartupEquipment.cs
@@ -1,31 +1,10 @@
-﻿/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
-
-using System;
-using System.Reflection;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
-
+using System.Reflection;
+using DOL.Database;
 using DOL.Events;
 using DOL.GS.ServerProperties;
-using DOL.Database;
 
 namespace DOL.GS.GameEvents
 {
@@ -151,8 +130,7 @@ namespace DOL.GS.GameEvents
 						// create Inventory item and set to owner.
 						DbInventoryItem inventoryItem = GameInventoryItem.Create(item);
 						inventoryItem.OwnerID = ch.ObjectId;
-						inventoryItem.Realm = ch.Realm;
-						
+
 						bool itemChoosen = false;
 		
 						// if equipable item, find equippable slot
