OpenDAoC/OpenDAoC-Core

commit abec9a9f97d485493d1a18e726c7153546e6b2da

Reduce NPC / pet follow distance when out of combat The main concern is about AoE CCs. Bonedancer subpets were also not sticking correctly to their commander.

GameServer/ECS-Components/NpcMovementComponent.cs
@@ -499,6 +499,7 @@ namespace DOL.GS
                 return;
             }
 
+            TurnTo(FollowTarget);
             float distanceToTarget = Owner.GetDistanceTo(destination);
             int ticksToArrive = (int) (distanceToTarget * 1000 / speed);
 
@@ -509,9 +510,6 @@ namespace DOL.GS
 
                 return;
             }
-
-            if (distanceToTarget > 25)
-                TurnTo((int) destination.X, (int) destination.Y);
 
             // Assume either the destination or speed has changed.
             UpdateMovement(destination, distanceToTarget, speed);
@@ -567,7 +565,7 @@ namespace DOL.GS
         private int FollowTick()
         {
             // Stop moving if the NPC is casting or using ranged weapons.
-            if (Owner.IsCasting || (Owner.IsAttacking && Owner.ActiveWeaponSlot == eActiveWeaponSlot.Distance))
+            if (Owner.IsCasting || (Owner.IsAttacking && Owner.ActiveWeaponSlot is eActiveWeaponSlot.Distance))
             {
                 StopMoving();
                 return Properties.GAMENPC_FOLLOWCHECK_TIME;
@@ -580,17 +578,18 @@ namespace DOL.GS
             }
 
             Vector3 targetPos = new(FollowTarget.X, FollowTarget.Y, FollowTarget.Z);
-            bool isInFormation = false;
 
             if (Owner.Brain is StandardMobBrain brain && FollowTarget.Realm == Owner.Realm)
             {
-                int tx = (int) targetPos.X, ty = (int) targetPos.Y, tz = (int) targetPos.Z;
+                int tx = (int) targetPos.X;
+                int ty = (int) targetPos.Y;
+                int tz = (int) targetPos.Z;
 
                 // Update to formation-adjusted position.
                 if (brain.CheckFormation(ref tx, ref ty, ref tz))
                 {
-                    isInFormation = true;
                     targetPos = new(tx, ty, tz);
+                    MinFollowDistance = 0;
                 }
             }
 
@@ -604,7 +603,7 @@ namespace DOL.GS
                 return 0;
             }
 
-            if (!isInFormation && distanceSquared <= MinFollowDistance * MinFollowDistance)
+            if (distanceSquared <= MinFollowDistance * MinFollowDistance)
             {
                 TurnTo(FollowTarget);
 
@@ -626,9 +625,9 @@ namespace DOL.GS
             if (Owner.IsAttacking && distance > Owner.MeleeAttackRange)
                 speed = MaxSpeed;
             else
-                speed = (short) Math.Min(MaxSpeed, (distance - MinFollowDistance) * 2);
+                speed = (short) Math.Min(MaxSpeed, (distance - MinFollowDistance) * 2.5);
 
-            PathToInternal(destination, Math.Min(MaxSpeed, speed));
+            PathToInternal(destination, Math.Max((short) 10, speed));
             return Properties.GAMENPC_FOLLOWCHECK_TIME;
         }
 

GameServer/ai/brain/ControlledMob/ControlledMobBrain.cs
@@ -24,7 +24,7 @@ namespace DOL.AI.Brain
 		private static readonly Logging.Logger log = Logging.LoggerManager.Create(MethodBase.GetCurrentMethod().DeclaringType);
 
 		public const int MAX_PET_AGGRO_DISTANCE = 512; // Tolakram - Live test with caby pet - I was extremely close before auto aggro
-		public const short MIN_OWNER_FOLLOW_DIST = 100;
+		public const short MIN_OWNER_FOLLOW_DIST = 80;
 		public const short MAX_OWNER_FOLLOW_DIST = 10000;
 
 		protected int m_tempX = 0;

commit 8fbd707addde1b138b88d356d4966c3b1bdbf697

Fix roaming NPCs not updating their heading correctly on arrival Caused by abec9a9f97d485493d1a18e726c7153546e6b2da

GameServer/ECS-Components/NpcMovementComponent.cs
@@ -499,8 +499,13 @@ namespace DOL.GS
                 return;
             }
 
-            TurnTo(FollowTarget);
-            float distanceToTarget = Owner.GetDistanceTo(destination);
+            float distanceToTarget = (_ownerPosition - destination).Length();
+
+            if (distanceToTarget > 25)
+                TurnTo((int) destination.X, (int) destination.Y);
+            else
+                TurnTo(FollowTarget);
+
             int ticksToArrive = (int) (distanceToTarget * 1000 / speed);
 
             if (ticksToArrive <= 0)