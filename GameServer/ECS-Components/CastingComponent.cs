OpenDAoC/OpenDAoC-Core

commit dbe8b15407d30b4f4cf4f3cda2d51af94bc518c8

Move NPC initial LoS check on spell cast from `GameNPC` to `NpcCastingComponent` Replace `ConcurrentDictionary` with `Dictionary` + lock (potentially over 50MB saved).

GameServer/ECS-Components/NpcCastingComponent.cs
@@ -1,4 +1,6 @@
-﻿using DOL.AI.Brain;
+﻿using System.Collections.Generic;
+using System.Threading;
+using DOL.AI.Brain;
 using DOL.GS.Keeps;
 
 namespace DOL.GS
@@ -6,12 +8,60 @@ namespace DOL.GS
     public class NpcCastingComponent : CastingComponent
     {
         private GameNPC _npcOwner;
+        private Dictionary<GameObject, List<SpellWaitingForLosCheck>> _spellsWaitingForLosCheck = new();
+        private Lock _spellsWaitingForLosCheckLock = new();
 
         private bool IsCasterGuardOrImmobile => _npcOwner is GuardCaster || _npcOwner.MaxSpeedBase == 0;
 
         public NpcCastingComponent(GameNPC npcOwner) : base(npcOwner)
         {
             _npcOwner = npcOwner;
+        }
+
+        public override bool RequestCastSpell(Spell spell, SpellLine spellLine, ISpellCastingAbilityHandler spellCastingAbilityHandler = null, GameLiving target = null)
+        {
+            Spell spellToCast = null;
+
+            if (spellLine.KeyName is GlobalSpellsLines.Mob_Spells)
+            {
+                // NPC spells will get the level equal to their caster
+                spellToCast = (Spell) spell.Clone();
+                spellToCast.Level = _npcOwner.Level;
+            }
+            else
+                spellToCast = spell;
+
+            if (target == _npcOwner || target == null)
+                return base.RequestCastSpell(spellToCast, spellLine);
+
+            GamePlayer losChecker = target as GamePlayer;
+
+            if (losChecker == null && _npcOwner.Brain is IControlledBrain controlledBrain)
+                losChecker = controlledBrain.GetPlayerOwner();
+
+            if (losChecker == null && _npcOwner.Brain is StandardMobBrain brain)
+            {
+                List<GamePlayer> playersInRadius = _npcOwner.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE);
+
+                if (playersInRadius.Count > 0)
+                    losChecker = playersInRadius[Util.Random(playersInRadius.Count - 1)];
+            }
+
+            if (losChecker == null)
+                return base.RequestCastSpell(spellToCast, spellLine);
+
+            SpellWaitingForLosCheck spellWaitingForLosCheck = new(spellToCast, spellLine);
+
+            lock (_spellsWaitingForLosCheckLock)
+            {
+                if (_spellsWaitingForLosCheck.TryGetValue(target, out var list))
+                    list.Add(spellWaitingForLosCheck);
+                else
+                    _spellsWaitingForLosCheck[target] = [spellWaitingForLosCheck];
+            }
+
+            losChecker.Out.SendCheckLos(_npcOwner, target, CastSpellLosCheckReply);
+            return true; // Consider the NPC is casting while waiting for the reply to prevent it from moving.
         }
 
         public override void OnSpellCast(Spell spell)
@@ -25,7 +75,11 @@ namespace DOL.GS
         public override void ClearSpellHandlers()
         {
             // Make sure NPCs don't start casting pending spells after being told to stop.
-            _npcOwner.ClearSpellsWaitingForLosCheck();
+
+            lock (_spellsWaitingForLosCheckLock)
+            {
+                _spellsWaitingForLosCheck.Clear();
+            }
 
             // Don't clear the attack spell queue here.
             if (_npcOwner.Brain is NecromancerPetBrain necromancerPetBrain)
@@ -43,6 +97,47 @@ namespace DOL.GS
                 return false;
 
             return livingTarget.ActiveWeaponSlot is not eActiveWeaponSlot.Distance && livingTarget.IsWithinRadius(_npcOwner, livingTarget.attackComponent.AttackRange);
+        }
+
+        private void CastSpellLosCheckReply(GamePlayer player, LosCheckResponse response, ushort sourceOID, ushort targetOID)
+        {
+            GameObject target = _npcOwner.CurrentRegion.GetObject(targetOID);
+
+            if (target == null)
+                return;
+
+            lock (_spellsWaitingForLosCheckLock)
+            {
+                if (!_spellsWaitingForLosCheck.TryGetValue(target, out var list))
+                    return;
+
+                bool success = response is LosCheckResponse.True;
+
+                foreach (SpellWaitingForLosCheck spellWaitingForLosCheck in list)
+                {
+                    Spell spell = spellWaitingForLosCheck.Spell;
+                    SpellLine spellLine = spellWaitingForLosCheck.SpellLine;
+
+                    if (success && spellLine != null && spell != null)
+                        base.RequestCastSpell(spell, spellLine, null, target as GameLiving);
+                    else
+                        _npcOwner.OnCastSpellLosCheckFail(target);
+                }
+
+                list.Clear();
+            }
+        }
+
+        private readonly struct SpellWaitingForLosCheck
+        {
+            public readonly Spell Spell;
+            public readonly SpellLine SpellLine;
+
+            public SpellWaitingForLosCheck(Spell spell, SpellLine spellLine)
+            {
+                Spell = spell;
+                SpellLine = spellLine;
+            }
         }
     }
 }

GameServer/gameobjects/GameNPC.cs
@@ -3230,26 +3230,6 @@ namespace DOL.GS
 		#region Spell
 
 		private List<Spell> m_spells = [];
-		private ConcurrentDictionary<GameObject, List<SpellWaitingForLosCheck>> _spellsWaitingForLosCheck = new();
-
-		public void ClearSpellsWaitingForLosCheck()
-		{
-			_spellsWaitingForLosCheck.Clear();
-		}
-
-		public class SpellWaitingForLosCheck
-		{
-			public Spell Spell { get; set; }
-			public SpellLine SpellLine { get; set; }
-			public long RequestTime { get; set; }
-
-			public SpellWaitingForLosCheck(Spell spell, SpellLine spellLine, long requestTime)
-			{
-				Spell = spell;
-				SpellLine = spellLine;
-				RequestTime = requestTime;
-			}
-		}
 
 		/// <summary>
 		/// property of spell array of NPC
@@ -3507,29 +3487,9 @@ namespace DOL.GS
 			return casted;
 		}
 
-		/// <summary>
-		/// Cast a spell with LoS check if possible.
-		/// </summary>
-		/// <returns>True if the spellcast started successfully. False otherwise or if a LoS check was initiated.</returns>
 		public override bool CastSpell(Spell spell, SpellLine line, ISpellCastingAbilityHandler spellCastingAbilityHandler = null)
 		{
-			// Clean up our '_spellsWaitingForLosCheck'. Entries older than 2 seconds are removed.
-			foreach (var pair in _spellsWaitingForLosCheck)
-			{
-				List<SpellWaitingForLosCheck> list = pair.Value;
-
-				for (int i = list.Count - 1; i >= 0; i--)
-				{
-					if (GameServiceUtils.ShouldTick(list[i].RequestTime + 2000))
-						list.SwapRemoveAt(i);
-				}
-
-				// We can keep the list if we're about to add something to it.
-				if (list.Count == 0 && TargetObject != pair.Key)
-					_spellsWaitingForLosCheck.TryRemove(pair.Key, out _);
-			}
-
-			Spell spellToCast = null;
+			Spell spellToCast;
 
 			if (line.KeyName is GlobalSpellsLines.Mob_Spells)
 			{
@@ -3540,67 +3500,7 @@ namespace DOL.GS
 			else
 				spellToCast = spell;
 
-			if (TargetObject == this || TargetObject == null)
-				return base.CastSpell(spellToCast, line);
-
-			GamePlayer LosChecker = TargetObject as GamePlayer;
-
-			if (LosChecker == null && Brain is IControlledBrain controlledBrain)
-				LosChecker = controlledBrain.GetPlayerOwner();
-
-			if (LosChecker == null && Brain is StandardMobBrain brain)
-			{
-				List<GamePlayer> playersInRadius = GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE);
-
-				if (playersInRadius.Count > 0)
-					LosChecker = playersInRadius[Util.Random(playersInRadius.Count - 1)];
-			}
-
-			if (LosChecker == null)
-				return base.CastSpell(spellToCast, line, spellCastingAbilityHandler);
-
-			_spellsWaitingForLosCheck.AddOrUpdate(TargetObject, Add, Update, new SpellWaitingForLosCheck(spellToCast, line, GameLoop.GameLoopTime));
-			LosChecker.Out.SendCheckLos(this, TargetObject, new(CastSpellLosCheckReply));
-			return true; // Consider the NPC is casting while waiting for the reply to prevent it from moving.
-
-			List<SpellWaitingForLosCheck> Add(GameObject key, SpellWaitingForLosCheck arg)
-			{
-				return [arg];
-			}
-
-			List<SpellWaitingForLosCheck> Update(GameObject key, List<SpellWaitingForLosCheck> oldValue, SpellWaitingForLosCheck arg)
-			{
-				oldValue.Add(arg);
-				return oldValue;
-			}
-		}
-
-		public virtual void CastSpellLosCheckReply(GamePlayer player, LosCheckResponse response, ushort sourceOID, ushort targetOID)
-		{
-			GameObject target = CurrentRegion.GetObject(targetOID);
-
-			if (target == null || !_spellsWaitingForLosCheck.TryGetValue(target, out List<SpellWaitingForLosCheck> list))
-				return;
-
-			bool success = response is LosCheckResponse.True;
-
-			foreach (SpellWaitingForLosCheck spellWaitingForLosCheck in list)
-			{
-				Spell spell = spellWaitingForLosCheck.Spell;
-				SpellLine spellLine = spellWaitingForLosCheck.SpellLine;
-
-				if (success && spellLine != null && spell != null)
-					OnCastSpellLosCheckSuccess(target, spell, spellLine);
-				else
-					OnCastSpellLosCheckFail(target);
-			}
-
-			list.Clear();
-		}
-
-		public virtual void OnCastSpellLosCheckSuccess(GameObject target, Spell spell, SpellLine spellLine)
-		{
-			CastSpell(spell, spellLine, target as GameLiving);
+			return base.CastSpell(spellToCast, line, spellCastingAbilityHandler);
 		}
 
 		public virtual void OnCastSpellLosCheckFail(GameObject target)

commit 15527bec764ae2caccd15e967e0352882d07357f

Fix LoS checks being forced for NPCs Regression probably caused by dbe8b15407d30b4f4cf4f3cda2d51af94bc518c8 Notably resulted in Hateners following their target if they didn't have line of sight.

GameServer/ECS-Components/CastingComponent.cs
@@ -61,12 +61,22 @@ namespace DOL.GS
                 ServiceObjectStore.Remove(this);
         }
 
-        public virtual bool RequestCastSpell(Spell spell, SpellLine spellLine, ISpellCastingAbilityHandler spellCastingAbilityHandler = null, GameLiving target = null)
+        public bool RequestCastSpell(
+            Spell spell,
+            SpellLine spellLine,
+            ISpellCastingAbilityHandler spellCastingAbilityHandler = null,
+            GameLiving target = null,
+            bool checkLos = true)
         {
-            return RequestCastSpellInternal(spell, spellLine, spellCastingAbilityHandler, target);
+            return RequestCastSpellInternal(spell, spellLine, spellCastingAbilityHandler, target, checkLos ? GetLosChecker(target) : null);
         }
 
-        protected bool RequestCastSpellInternal(Spell spell, SpellLine spellLine, ISpellCastingAbilityHandler spellCastingAbilityHandler = null, GameLiving target = null, GamePlayer losChecker = null)
+        protected virtual bool RequestCastSpellInternal(
+            Spell spell,
+            SpellLine spellLine,
+            ISpellCastingAbilityHandler spellCastingAbilityHandler,
+            GameLiving target,
+            GamePlayer losChecker)
         {
             if (Owner.IsIncapacitated)
                 Owner.Notify(GameLivingEvent.CastFailed, this, new CastFailedEventArgs(null, CastFailedEventArgs.Reasons.CrowdControlled));
@@ -85,6 +95,11 @@ namespace DOL.GS
 
             ServiceObjectStore.Add(this);
             return true;
+        }
+
+        protected virtual GamePlayer GetLosChecker(GameLiving target)
+        {
+            return null;
         }
 
         public void RequestUseAbility(Ability ability)

GameServer/ECS-Components/NpcCastingComponent.cs
@@ -18,28 +18,15 @@ namespace DOL.GS
             _npcOwner = npcOwner;
         }
 
-        public override bool RequestCastSpell(Spell spell, SpellLine spellLine, ISpellCastingAbilityHandler spellCastingAbilityHandler = null, GameLiving target = null)
+        protected override bool RequestCastSpellInternal(
+            Spell spell,
+            SpellLine spellLine,
+            ISpellCastingAbilityHandler spellCastingAbilityHandler,
+            GameLiving target,
+            GamePlayer losChecker)
         {
-            // `spellCastingAbilityHandler` is unused for NPCs.
-
-            if (target == _npcOwner || target == null)
-                return RequestCastSpellInternal(spell, spellLine, null, target);
-
-            GamePlayer losChecker = target as GamePlayer;
-
-            if (losChecker == null && _npcOwner.Brain is IControlledBrain controlledBrain)
-                losChecker = controlledBrain.GetPlayerOwner();
-
-            if (losChecker == null && _npcOwner.Brain is StandardMobBrain brain)
-            {
-                List<GamePlayer> playersInRadius = _npcOwner.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE);
-
-                if (playersInRadius.Count > 0)
-                    losChecker = playersInRadius[Util.Random(playersInRadius.Count - 1)];
-            }
-
-            if (losChecker == null)
-                return RequestCastSpellInternal(spell, spellLine, null, target);
+            if (losChecker == null || spell.Range == 0)
+                return base.RequestCastSpellInternal(spell, spellLine, spellCastingAbilityHandler, target, losChecker);
 
             SpellWaitingForLosCheck spellWaitingForLosCheck = new(spell, spellLine);
 
@@ -53,6 +40,24 @@ namespace DOL.GS
 
             losChecker.Out.SendCheckLos(_npcOwner, target, CastSpellLosCheckReply);
             return true; // Consider the NPC is casting while waiting for the reply to prevent it from moving.
+        }
+
+        protected override GamePlayer GetLosChecker(GameLiving target)
+        {
+            GamePlayer losChecker = target as GamePlayer;
+
+            if (losChecker == null && _npcOwner.Brain is IControlledBrain controlledBrain)
+                losChecker = controlledBrain.GetPlayerOwner();
+
+            if (losChecker == null && _npcOwner.Brain is StandardMobBrain brain)
+            {
+                List<GamePlayer> playersInRadius = _npcOwner.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE);
+
+                if (playersInRadius.Count > 0)
+                    losChecker = playersInRadius[Util.Random(playersInRadius.Count - 1)];
+            }
+
+            return losChecker;
         }
 
         public override void OnSpellCast(Spell spell)
@@ -103,15 +108,19 @@ namespace DOL.GS
 
                 bool success = response is LosCheckResponse.True;
 
-                foreach (SpellWaitingForLosCheck spellWaitingForLosCheck in list)
+                // Only if this is still the correct target.
+                if (target == _npcOwner.TargetObject)
                 {
-                    Spell spell = spellWaitingForLosCheck.Spell;
-                    SpellLine spellLine = spellWaitingForLosCheck.SpellLine;
+                    foreach (SpellWaitingForLosCheck spellWaitingForLosCheck in list)
+                    {
+                        Spell spell = spellWaitingForLosCheck.Spell;
+                        SpellLine spellLine = spellWaitingForLosCheck.SpellLine;
 
-                    if (success && spellLine != null && spell != null)
-                        RequestCastSpellInternal(spell, spellLine, null, target as GameLiving, losChecker);
-                    else
-                        _npcOwner.OnCastSpellLosCheckFail(target);
+                        if (success && spellLine != null && spell != null)
+                            base.RequestCastSpellInternal(spell, spellLine, null, target as GameLiving, losChecker);
+                        else
+                            _npcOwner.OnCastSpellLosCheckFail(target);
+                    }
                 }
 
                 list.Clear();

GameServer/ECS-Components/PlayerCastingComponent.cs
@@ -12,6 +12,11 @@ namespace DOL.GS
             _playerOwner = playerOwner;
         }
 
+        protected override GamePlayer GetLosChecker(GameLiving target)
+        {
+            return _playerOwner;
+        }
+
         protected override bool CanCastSpell()
         {
             if (_playerOwner.effectListComponent.ContainsEffectForEffectType(eEffect.Volley))

GameServer/gameobjects/GameLiving.cs
@@ -3618,16 +3618,9 @@ namespace DOL.GS
 			castingComponent.InterruptCasting(IsMoving);
 		}
 
-		public virtual bool CastSpell(Spell spell, SpellLine line, ISpellCastingAbilityHandler spellCastingAbilityHandler = null)
+		public virtual bool CastSpell(Spell spell, SpellLine line, ISpellCastingAbilityHandler spellCastingAbilityHandler = null, bool checkLos = true)
 		{
-			return castingComponent.RequestCastSpell(spell, line, spellCastingAbilityHandler, TargetObject as GameLiving);
-		}
-
-		// Should only be used when the target of the spell is different than the currently selected one.
-		// Which can happen during LoS checks, since we're not waiting for the check to complete to perform other actions.
-		protected bool CastSpell(Spell spell, SpellLine line, GameLiving target, ISpellCastingAbilityHandler spellCastingAbilityHandle = null)
-		{
-			return castingComponent.RequestCastSpell(spell, line, spellCastingAbilityHandle, target);
+			return castingComponent.RequestCastSpell(spell, line, spellCastingAbilityHandler, TargetObject as GameLiving, checkLos);
 		}
 
 		public virtual bool CastSpell(ISpellCastingAbilityHandler ab)

GameServer/gameobjects/GameNPC.cs
@@ -3415,28 +3415,7 @@ namespace DOL.GS
 		/// </summary>
 		public virtual bool CastSpell(Spell spell, SpellLine line, bool checkLos)
 		{
-			bool casted;
-
-			// Don't check for LoS if the spell has no range.
-			if (checkLos && spell.Range > 0)
-				casted = CastSpell(spell, line);
-			else
-			{
-				Spell spellToCast;
-
-				if (line.KeyName == GlobalSpellsLines.Mob_Spells)
-				{
-					// NPC spells will get the level equal to their caster
-					spellToCast = (Spell)spell.Clone();
-					spellToCast.Level = Level;
-				}
-				else
-					spellToCast = spell;
-
-				casted = base.CastSpell(spellToCast, line);
-			}
-
-			return casted;
+			return base.CastSpell(spell, line, null, checkLos);
 		}
 
 		public virtual void OnCastSpellLosCheckFail(GameObject target)

GameServer/gameobjects/GamePlayer.cs
@@ -6365,10 +6365,10 @@ namespace DOL.GS
             m_nextSpellTarget = null;
         }
 
-        public override bool CastSpell(Spell spell, SpellLine line, ISpellCastingAbilityHandler spellCastingAbilityHandler = null)
+        public override bool CastSpell(Spell spell, SpellLine line, ISpellCastingAbilityHandler spellCastingAbilityHandler = null, bool checkLos = true)
         {
             // Don't pass the current target to the casting component. It's supposed to be the one at cast time, not the one at queue time.
-            return castingComponent.RequestCastSpell(spell, line, spellCastingAbilityHandler);
+            return castingComponent.RequestCastSpell(spell, line, spellCastingAbilityHandler, null, checkLos);
         }
 
         public override bool CastSpell(ISpellCastingAbilityHandler ab)

GameServer/spells/SpellHandler.cs
@@ -999,12 +999,14 @@ namespace DOL.GS.Spells
 			{
 				_lastDuringCastLosCheckTime = GameLoop.GameLoopTime;
 
-				if (m_spell.Target is not eSpellTarget.SELF and not eSpellTarget.GROUP and not eSpellTarget.CONE and not eSpellTarget.PET && m_spell.Range > 0)
+				if (m_spell.Target is not eSpellTarget.SELF and not eSpellTarget.GROUP and not eSpellTarget.CONE and not eSpellTarget.PET &&
+					m_spell.Range > 0 &&
+					LosChecker != null)
 				{
 					if (Caster is GameNPC npc)
-						LosChecker?.Out.SendCheckLos(npc, target, CheckNpcLosDuringCastCallback);
+						LosChecker.Out.SendCheckLos(npc, target, CheckNpcLosDuringCastCallback);
 					else if (Caster is GamePlayer player)
-						player.Out.SendCheckLos(player, target, CheckPlayerLosDuringCastCallback);
+						LosChecker.Out.SendCheckLos(player, target, CheckPlayerLosDuringCastCallback);
 				}
 			}
 