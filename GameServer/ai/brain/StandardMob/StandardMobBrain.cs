OpenDAoC/OpenDAoC-Core

commit 2a2fc82d7d8bec51e8c317bdd5da664676943367

Make a player's pet and subpets be added to the NPC's aggro list when attacked by a player or his group members

GameServer/ai/brain/StandardMob/StandardMobBrain.cs
@@ -324,13 +324,21 @@ namespace DOL.AI.Brain
 
             if (living is GamePlayer player)
             {
+                AddPetAndSubPetsToAggroList(player);
+
                 // Add the whole group to the aggro list.
+                // This is done on every attack, but we may consider doing it only once per group, somehow.
                 if (player.Group != null)
                 {
                     foreach (GamePlayer playerInGroup in player.Group.GetPlayersInTheGroup())
                     {
                         if (playerInGroup != living)
-                            AggroList.TryAdd(playerInGroup, new());
+                        {
+                            if (!AggroList.ContainsKey(playerInGroup))
+                                AggroList.TryAdd(playerInGroup, new(0));
+
+                            AddPetAndSubPetsToAggroList(playerInGroup);
+                        }
                     }
                 }
             }
@@ -340,6 +348,26 @@ namespace DOL.AI.Brain
             {
                 FSM.SetCurrentState(eFSMStateType.AGGRO);
                 NextThinkTick = GameLoop.GameLoopTime;
+            }
+
+            void AddPetAndSubPetsToAggroList(GamePlayer player)
+            {
+                GameNPC pet = player.ControlledBrain.Body;
+
+                if (pet == null)
+                    return;
+
+                if (!AggroList.ContainsKey(pet))
+                    AggroList.TryAdd(pet, new(0));
+
+                if (pet.ControlledNpcList == null)
+                    return;
+
+                foreach (GameNPC subpet in pet.ControlledNpcList)
+                {
+                    if (!AggroList.ContainsKey(subpet))
+                        AggroList.TryAdd(subpet, new(0));
+                }
             }
 
             static AggroAmount Add(GameLiving key, long arg)

commit 313807673b4ec229c6b02652af2295467b65e8ee

Fix exception caused by Bonedancer pet Regression since 2a2fc82d7d8bec51e8c317bdd5da664676943367

GameServer/ai/brain/StandardMob/StandardMobBrain.cs
@@ -360,13 +360,20 @@ namespace DOL.AI.Brain
                 if (!AggroList.ContainsKey(pet))
                     AggroList.TryAdd(pet, new(0));
 
-                if (pet.ControlledNpcList == null)
+                IControlledBrain[] controlledBrains = pet.ControlledNpcList;
+
+                if (controlledBrains == null)
                     return;
 
-                foreach (GameNPC subpet in pet.ControlledNpcList)
+                foreach (IControlledBrain subPetBrain in controlledBrains)
                 {
-                    if (!AggroList.ContainsKey(subpet))
-                        AggroList.TryAdd(subpet, new(0));
+                    GameNPC subPet = subPetBrain.Body;
+
+                    if (subPet == null)
+                        continue;
+
+                    if (!AggroList.ContainsKey(subPet))
+                        AggroList.TryAdd(subPet, new(0));
                 }
             }
 