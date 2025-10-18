db95cbe5ba794348314ba92bd23cb6593ade3d82
Baptiste Marie <bm01@users.noreply.github.com>
2025-10-18 18:48:09 +0200

    Change `START_AS_BASE_CLASS` default value to true

--- GameServer/scripts/startup/script/StartAsBaseClass.cs
+++ GameServer/scripts/startup/script/StartAsBaseClass.cs
@@ -33,7 +33,7 @@ namespace DOL.GS.GameEvents
 		/// <summary>
 		/// Should the server start characters as Base Class?
 		/// </summary>
-		[ServerProperty("startup", "start_as_base_class", "Should we start all players as their base class? true if yes (e.g. Armsmen become Fighters on Creation)", false)]
+		[ServerProperty("startup", "start_as_base_class", "Should we start all players as their base class? true if yes (e.g. Armsmen become Fighters on Creation)", true)]
 		public static bool START_AS_BASE_CLASS;
 
 		/// <summary>
