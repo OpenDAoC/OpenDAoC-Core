OpenDAoC/OpenDAoC-Core

commit aa9138f5f137e07f33474b207a2e7c044f62afec

Fix detail window ignoring RR5 for effective DPS calculation Added `GamePlayer.GetWeaponDpsCap`

GameServer/gameobjects/GamePlayer.cs
@@ -5860,25 +5860,27 @@ namespace DOL.GS
             return ApplyWeaponQualityAndConditionToDamage(weapon, WeaponDamageWithoutQualityAndCondition(weapon));
         }
 
+        public double GetWeaponDpsCap()
+        {
+            double dpsCap = 1.2 + 0.3 * Level;
+
+            if (RealmLevel > 39)
+                dpsCap += 0.3;
+
+            return dpsCap;
+        }
+
         public double WeaponDamageWithoutQualityAndCondition(DbInventoryItem weapon)
         {
             if (weapon == null)
                 return 0;
 
-            double Dps = weapon.DPS_AF;
-
             // Apply dps cap before quality and condition.
             // http://www.classesofcamelot.com/faq.asp?mode=view&cat=10
-            int dpsCap = 12 + 3 * Level;
-
-            if (RealmLevel > 39)
-                dpsCap += 3;
-
-            if (Dps > dpsCap)
-                Dps = dpsCap;
-
-            Dps *= 1 + GetModified(eProperty.DPS) * 0.01;
-            return Dps * 0.1;
+            double weaponDps = weapon.DPS_AF * 0.1;
+            double dps = Math.Min(weaponDps, GetWeaponDpsCap());
+            weaponDps *= 1 + GetModified(eProperty.DPS) * 0.01;
+            return weaponDps;
         }
 
         public static double ApplyWeaponQualityAndConditionToDamage(DbInventoryItem weapon, double damage)

GameServer/packets/Client/168/DetailDisplayHandler.cs
@@ -1201,43 +1201,37 @@ namespace DOL.GS.PacketHandler.Client.v168
 		/// </summary>
 		public static void WriteClassicWeaponInfos(IList<string> output, DbInventoryItem item, GameClient client)
 		{
-			double itemDPS = item.DPS_AF / 10.0;
-			double clampedDPS = Math.Min(itemDPS, 1.2 + 0.3 * client.Player.Level);
-			double itemSPD = item.SPD_ABS / 10.0;
-			double effectiveDPS = clampedDPS * item.Quality * 0.01 * item.ConditionPercent * 0.01;
+			double itemDps = item.DPS_AF * 0.1;
+			double clampedDps = Math.Min(itemDps, client.Player.GetWeaponDpsCap());
+			double itemSpd = item.SPD_ABS * 0.1;
+			double effectiveDps = clampedDps * item.Quality * 0.01 * item.ConditionPercent * 0.01;
+			string damageType = item.Type_Damage == 0 ? "None" : GlobalConstants.WeaponDamageTypeToName(item.Type_Damage);
 
 			output.Add(" ");
 			output.Add(" ");
 			output.Add(LanguageMgr.GetTranslation(client.Account.Language, "DetailDisplayHandler.WriteClassicWeaponInfos.DamageMod"));
-			if (itemDPS != 0)
+
+			if (itemDps != 0)
 			{
-				output.Add(LanguageMgr.GetTranslation(client.Account.Language, "DetailDisplayHandler.WriteClassicWeaponInfos.BaseDPS", itemDPS.ToString("0.0")));
-				output.Add(LanguageMgr.GetTranslation(client.Account.Language, "DetailDisplayHandler.WriteClassicWeaponInfos.ClampDPS", clampedDPS.ToString("0.0")));
+				output.Add(LanguageMgr.GetTranslation(client.Account.Language, "DetailDisplayHandler.WriteClassicWeaponInfos.BaseDPS", itemDps.ToString("0.0")));
+				output.Add(LanguageMgr.GetTranslation(client.Account.Language, "DetailDisplayHandler.WriteClassicWeaponInfos.ClampDPS", clampedDps.ToString("0.0")));
 			}
 
 			if (item.SPD_ABS >= 0)
-			{
-				output.Add(LanguageMgr.GetTranslation(client.Account.Language, "DetailDisplayHandler.WriteClassicWeaponInfos.SPD", itemSPD.ToString("0.0")));
-			}
+				output.Add(LanguageMgr.GetTranslation(client.Account.Language, "DetailDisplayHandler.WriteClassicWeaponInfos.SPD", itemSpd.ToString("0.0")));
 
 			if (item.Quality != 0)
-			{
 				output.Add(LanguageMgr.GetTranslation(client.Account.Language, "DetailDisplayHandler.WriteClassicWeaponInfos.Quality", item.Quality));
-			}
+
 			if (item.Condition != 0)
-			{
 				output.Add(LanguageMgr.GetTranslation(client.Account.Language, "DetailDisplayHandler.WriteClassicWeaponInfos.Condition", item.ConditionPercent));
-			}
 
-			output.Add(LanguageMgr.GetTranslation(client.Account.Language, "DetailDisplayHandler.WriteClassicWeaponInfos.DamageType",
-			                                      (item.Type_Damage == 0 ? "None" : GlobalConstants.WeaponDamageTypeToName(item.Type_Damage))));
+			output.Add(LanguageMgr.GetTranslation(client.Account.Language, "DetailDisplayHandler.WriteClassicWeaponInfos.DamageType", damageType));
 			output.Add(" ");
-
 			output.Add(LanguageMgr.GetTranslation(client.Account.Language, "DetailDisplayHandler.WriteClassicWeaponInfos.EffDamage"));
-			if (itemDPS != 0)
-			{
-				output.Add("- " + effectiveDPS.ToString("0.0") + " DPS");
-			}
+
+			if (itemDps != 0)
+				output.Add($"- {effectiveDps:0.0} DPS");
 		}
 
 		public void WriteUsableClasses(IList<string> output, DbInventoryItem item, GameClient client)

commit 6a6c8d6ba9e58e00065665a5c49f7d855c85a93c

Fix weapon DPS cap, regression by aa9138f5f137e07f33474b207a2e7c044f62afec

GameServer/gameobjects/GamePlayer.cs
@@ -5879,8 +5879,8 @@ namespace DOL.GS
             // http://www.classesofcamelot.com/faq.asp?mode=view&cat=10
             double weaponDps = weapon.DPS_AF * 0.1;
             double dps = Math.Min(weaponDps, GetWeaponDpsCap());
-            weaponDps *= 1 + GetModified(eProperty.DPS) * 0.01;
-            return weaponDps;
+            dps *= 1 + GetModified(eProperty.DPS) * 0.01;
+            return dps;
         }
 
         public static double ApplyWeaponQualityAndConditionToDamage(DbInventoryItem weapon, double damage)