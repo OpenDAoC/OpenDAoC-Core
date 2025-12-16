OpenDAoC/OpenDAoC-Core

commit 8f1b841155744ffa1d37f912ca619b743ab26ff8

`SendSpellEffectAnimation`, `SendCombatAnimation`: Fix lack of animation when attacker is out of range / invisible

GameServer/packets/Server/PacketLib174.cs
@@ -385,7 +385,17 @@ namespace DOL.GS.PacketHandler
 		{
 			using (var pak = PooledObjectFactory.GetForTick<GSTCPPacketOut>().Init(GetPacketCode(eServerPackets.SpellEffectAnimation)))
 			{
-				pak.WriteShort((ushort)spellCaster.ObjectID);
+				ushort casterObjectId;
+
+				// Fixes issues with spell effects from out of range or invisible players.
+				if (!spellTarget.IsWithinRadius(spellCaster, WorldMgr.VISIBILITY_DISTANCE))
+					casterObjectId = 0;
+				else if (spellTarget is not GamePlayer playerTarget || !playerTarget.CanDetect(spellCaster))
+					casterObjectId = 0;
+				else
+					casterObjectId = spellCaster.ObjectID;
+
+				pak.WriteShort(casterObjectId);
 				pak.WriteShort(spellid);
 				pak.WriteShort((ushort)(spellTarget == null ? 0 : spellTarget.ObjectID));
 				pak.WriteShort(boltTime);

GameServer/packets/Server/PacketLib186.cs
@@ -56,12 +56,24 @@ namespace DOL.GS.PacketHandler
 			using (var pak = PooledObjectFactory.GetForTick<GSTCPPacketOut>().Init(GetPacketCode(eServerPackets.CombatAnimation)))
 			{
 				if (attacker != null)
-					pak.WriteShort((ushort)attacker.ObjectID);
+				{
+					ushort attackerObjectId;
+
+					// Fixes issues with spell effects from out of range or invisible players.
+					if (!defender.IsWithinRadius(attacker, WorldMgr.VISIBILITY_DISTANCE))
+						attackerObjectId = 0;
+					else if (defender is not GamePlayer playerTarget || !playerTarget.CanDetect(attacker))
+						attackerObjectId = 0;
+					else
+						attackerObjectId = attacker.ObjectID;
+
+					pak.WriteShort(attackerObjectId);
+				}
 				else
 					pak.WriteShort(0x00);
 
 				if (defender != null)
-					pak.WriteShort((ushort)defender.ObjectID);
+					pak.WriteShort(defender.ObjectID);
 				else
 					pak.WriteShort(0x00);
 

commit 092aadc39ef0341c4cc27db40cc76144dccab662

Fix lack of combat animation, caused by 8f1b841155744ffa1d37f912ca619b743ab26ff8

GameServer/packets/Server/PacketLib174.cs
@@ -387,10 +387,10 @@ namespace DOL.GS.PacketHandler
 			{
 				ushort casterObjectId;
 
-				// Fixes issues with spell effects from out of range or invisible players.
+				// Fixes issues with spell effects caused by out of range or invisible attackers.
 				if (!spellTarget.IsWithinRadius(spellCaster, WorldMgr.VISIBILITY_DISTANCE))
 					casterObjectId = 0;
-				else if (spellTarget is not GamePlayer playerTarget || !playerTarget.CanDetect(spellCaster))
+				else if (spellTarget is GamePlayer playerTarget && !playerTarget.CanDetect(spellCaster))
 					casterObjectId = 0;
 				else
 					casterObjectId = spellCaster.ObjectID;

GameServer/packets/Server/PacketLib186.cs
@@ -57,12 +57,13 @@ namespace DOL.GS.PacketHandler
 			{
 				if (attacker != null)
 				{
+
 					ushort attackerObjectId;
 
-					// Fixes issues with spell effects from out of range or invisible players.
+					// Fixes issues with combat animations caused by out of range or invisible attackers.
 					if (!defender.IsWithinRadius(attacker, WorldMgr.VISIBILITY_DISTANCE))
 						attackerObjectId = 0;
-					else if (defender is not GamePlayer playerTarget || !playerTarget.CanDetect(attacker))
+					else if (defender is GamePlayer playerTarget && !playerTarget.CanDetect(attacker))
 						attackerObjectId = 0;
 					else
 						attackerObjectId = attacker.ObjectID;