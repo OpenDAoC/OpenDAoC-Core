c5c776da9f1297e72bc125c260c11c2311ad5a4e
Baptiste Marie <bm01@users.noreply.github.com>
2025-12-16 08:20:58 +0100

    Fix resurrection illness costing endurance

--- GameServer/spells/IllnessSpellHandler.cs
+++ GameServer/spells/IllnessSpellHandler.cs
@@ -1,144 +1,54 @@
 using System.Collections.Generic;
-using DOL.Database;
-using DOL.GS.Effects;
+using DOL.GS.RealmAbilities;
 
 namespace DOL.GS.Spells
 {
-	/// <summary>
-	/// Pve Resurrection Illness
-	/// </summary>
-	[SpellHandler(eSpellType.PveResurrectionIllness)]
-	public class PveResurrectionIllness : AbstractIllnessSpellHandler
-	{
-		public override string ShortDescription => string.Empty;
+    [SpellHandler(eSpellType.PveResurrectionIllness)]
+    public class PveResurrectionIllness : AbstractIllnessSpellHandler
+    {
+        public override string ShortDescription => "The player's effectiveness is greatly reduced due to being recently resurrected.";
 
-		public PveResurrectionIllness(GameLiving caster, Spell spell, SpellLine spellLine) : base(caster, spell, spellLine) { }
+        public PveResurrectionIllness(GameLiving caster, Spell spell, SpellLine spellLine) : base(caster, spell, spellLine) { }
 
-		public override ECSGameSpellEffect CreateECSEffect(in ECSGameEffectInitParams initParams)
-		{
-			return ECSGameEffectFactory.Create(initParams, static (in i) => new ResurrectionIllnessECSGameEffect(i));
-		}
-
-		/// <summary>
-		/// When an applied effect starts
-		/// duration spells only
-		/// </summary>
-		/// <param name="effect"></param>
-		public override void OnEffectStart(GameSpellEffect effect)
-		{
-			//GamePlayer player = effect.Owner as GamePlayer;
-			//if (player != null)
-			//{
-			//	player.Effectiveness -= Spell.Value * 0.01;
-			//	player.Out.SendUpdateWeaponAndArmorStats();
-			//	player.Out.SendStatusUpdate();
-			//}
-		}
-		
-		/// <summary>
-		/// When an applied effect expires.
-		/// Duration spells only.
-		/// </summary>
-		/// <param name="effect">The expired effect</param>
-		/// <param name="noMessages">true, when no messages should be sent to player and surrounding</param>
-		/// <returns>immunity duration in milliseconds</returns>
-		public override int OnEffectExpires(GameSpellEffect effect, bool noMessages)
-		{
-			//GamePlayer player = effect.Owner as GamePlayer;
-			//if (player != null)
-			//{
-			//	player.Effectiveness += Spell.Value * 0.01;
-			//	player.Out.SendUpdateWeaponAndArmorStats();
-			//	player.Out.SendStatusUpdate();
-			//}
-			return 0;
-		}
-
-		/// <summary>
-		/// Delve Info
-		/// </summary>
-		public override IList<string> DelveInfo 
-		{
-			get 
-			{
-				/*
-				<Begin Info: Rusurrection Illness>
- 
-				The player's effectiveness is greatly reduced due to being recently resurrected.
- 
-				- Effectiveness penality: 50%
-				- 4:56 remaining time
- 
-				<End Info>
-				*/
-				var list = new List<string>();
-
-				list.Add(" "); //empty line
-				list.Add(ShortDescription);
-				list.Add(" "); //empty line
-				list.Add("- Effectiveness penality: "+Spell.Value+"%");
-				return list;
-			}
-		}
-
-        /// <summary>
-        /// Saves the effect when player quits
-        /// </summary>        
-        public override DbPlayerXEffect GetSavedEffect(GameSpellEffect e)
+        public override ECSGameSpellEffect CreateECSEffect(in ECSGameEffectInitParams initParams)
         {
-            DbPlayerXEffect eff = new DbPlayerXEffect();
-            eff.Var1 = Spell.ID;
-            eff.Duration = e.RemainingTime;
-            eff.IsHandler = true;
-            eff.SpellLine = SpellLine.KeyName;
-            return eff;
+            return ECSGameEffectFactory.Create(initParams, static (in i) => new ResurrectionIllnessECSGameEffect(i));
         }
 
-        /// <summary>
-        /// Restart the effects of resurrection illness
-        /// </summary>
-        public override void OnEffectRestored(GameSpellEffect effect, int[] vars)
-		{
-			OnEffectStart(effect);
-		}
+        public override IList<string> DelveInfo =>
+            [
+                " ",
+                ShortDescription,
+                " ",
+                "- Effectiveness penality: " + Spell.Value + "%"
+            ];
+    }
 
-        /// <summary>
-        /// Remove the effects of resurrection illness 
-        /// </summary>
-		public override int OnRestoredEffectExpires(GameSpellEffect effect, int[] vars, bool noMessages)
-		{
-			return OnEffectExpires(effect, false);
-		}
-	}
+    public class AbstractIllnessSpellHandler : SpellHandler
+    {
+        public AbstractIllnessSpellHandler(GameLiving caster, Spell spell, SpellLine spellLine) : base(caster, spell, spellLine) { }
 
-	/// <summary>
-	/// Contains all common code for illness spell handlers (and negative spell effects without animation) 
-	/// </summary>
-	public class AbstractIllnessSpellHandler : SpellHandler
-	{
-		public override bool HasPositiveEffect 
-		{
-			get 
-			{ 
-				return false;
-			}
-		}
+        public override bool HasPositiveEffect => false;
 
-		public override double CalculateSpellResistChance(GameLiving target)
-		{
-			return 0;
-		}
+        public override int CalculateEnduranceCost()
+        {
+            return 0;
+        }
 
-		protected override int CalculateEffectDuration(GameLiving target)
-		{
-			double modifier = 1.0;
-			RealmAbilities.VeilRecoveryAbility ab = target.GetAbility<RealmAbilities.VeilRecoveryAbility>();
-			if (ab != null)
-				modifier -= ((double)ab.Amount / 100);
+        public override double CalculateSpellResistChance(GameLiving target)
+        {
+            return 0;
+        }
 
-			return (int)((double)Spell.Duration * modifier); 
-		}
+        protected override int CalculateEffectDuration(GameLiving target)
+        {
+            double modifier = 1.0;
+            VeilRecoveryAbility veilRecovery = target.GetAbility<VeilRecoveryAbility>();
 
-		public AbstractIllnessSpellHandler(GameLiving caster, Spell spell, SpellLine spellLine) : base(caster, spell, spellLine) {}
-	}
+            if (veilRecovery != null)
+                modifier -= veilRecovery.Amount * 0.01;
+
+            return (int) (Spell.Duration * modifier);
+        }
+    }
 }
