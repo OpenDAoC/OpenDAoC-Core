e2915a43dd6f76d32b508965134b7fcc187ac439
Baptiste Marie <bm01@users.noreply.github.com>
2025-12-27 16:09:31 +0100

    Dissallow "None" as guild name This is a reserved name by the client.

--- GameServer/commands/playercommands/guild.cs
+++ GameServer/commands/playercommands/guild.cs
@@ -32,12 +32,9 @@ namespace DOL.GS.Commands
 		/// <returns></returns>
 		public static bool IsValidGuildName(string guildName)
 		{
-			if (!Regex.IsMatch(guildName, @"^[a-zA-Z àâäèéêëîïôœùûüÿçÀÂÄÈÉÊËÎÏÔŒÙÛÜŸÇ]+$") || guildName.Length < 0)
-
-			{
-				return false;
-			}
-			return true;
+			return Regex.IsMatch(guildName, @"^[a-zA-Z àâäèéêëîïôœùûüÿçÀÂÄÈÉÊËÎÏÔŒÙÛÜŸÇ]+$") &&
+				guildName.Length >= 0 &&
+				!guildName.Equals("None", StringComparison.OrdinalIgnoreCase);
 		}
 		private static bool IsNearRegistrar(GamePlayer player)
 		{
