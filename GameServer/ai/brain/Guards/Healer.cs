OpenDAoC/OpenDAoC-Core

commit 375edfd9d788dfbb6a32ac6e90fa99b4f9d205b6

Refactor LoS checks to use `ILosCheckListener` interface Replaces delegate-based LoS check callbacks with the ILosCheckListener interface for better performance and maintainability. * `StandardMobBrain`: Added `AggroLosCheckListener`. * `NpcAttackAction`: Implements `ILosCheckListener` for ranged attacks. * `CastingComponent`: Added `DuringCastLosCheckListener`, `EndOfCastLosCheckListener`, to be used by spell handlers. * `NpcCastingComponent`: Implements `ILosCheckListener` for initial spell LoS check (not needed for players since we rely on UseSpellHandler's packet content). * `GameSiegeBallista`: Implements `ILosCheckListener` for LoS check before firing. Fixed "Target is not in view!" message. * `GameSiegeCatapult`: Removed unused and broken GT LoS check logic. * `UncoverStealthAction`: Implements `ILosCheckListener`. * `GameKeepGuard`: Removed `GuardStartSpellHealCheckLos`, seemed redundant and outdated, now relies on `CastingComponent` exclusively. * `SpellMgr`: Removed some unused / duplicate code. * `Arrow`: Removed unused `DealDamageCheckLOS`. * `BainsheePulseDmgSpellHandler`: Removed redundant LoS checker selection, now relies on `CastingComponent`. * `DirectDamageDebuffSpellHandler`: Removed redundant LoS checker selection, now relies on `CastingComponent`. * `DirectDamageSpellHandler`: Removed redundant LoS checker selection, now relies on `CastingComponent`. Removed `m_castFailed` (incorrect logic, can't prevent mana cost on late end of cast LoS checks, this is for AoE spells). * `FZSpellHandler`: Implements `ILosCheckListener`. * `SpellHandler`: Replaced local function in `CheckEndCast`.

GameServer/keeps/Gameobjects/Guards/GameKeepGuard.cs
@@ -134,21 +134,6 @@ namespace DOL.GS.Keeps
 
 		#region Combat
 
-		public void GuardStartSpellHealCheckLos(GamePlayer player, LosCheckResponse response, ushort sourceOID, ushort targetOID)
-		{
-			if (response is LosCheckResponse.True && HealTarget != null)
-			{
-				Spell healSpell = GetGuardHealSmallSpell(Realm);
-
-				if (healSpell != null && !IsStunned && !IsMezzed)
-				{
-					attackComponent.StopAttack();
-					TargetObject = HealTarget;
-					CastSpell(healSpell, GuardSpellLine);
-				}
-			}
-		}
-
 		private static Spell GetGuardHealSmallSpell(eRealm realm)
 		{
 			switch (realm)
@@ -167,13 +152,12 @@ namespace DOL.GS.Keeps
 		public void CheckAreaForHeals()
 		{
 			GameLiving target = null;
-			GamePlayer LOSChecker = null;
 
 			foreach (GamePlayer player in GetPlayersInRadius(2000))
 			{
-				LOSChecker = player;
+				if (!player.IsAlive)
+					continue;
 
-				if (!player.IsAlive) continue;
 				if (GameServer.ServerRules.IsSameRealm(player, this, true))
 				{
 					if (player.HealthPercent < Properties.KEEP_HEAL_THRESHOLD)
@@ -188,7 +172,9 @@ namespace DOL.GS.Keeps
 			{
 				foreach (GameNPC npc in GetNPCsInRadius(2000))
 				{
-					if (npc is GameSiegeWeapon) continue;
+					if (npc is GameSiegeWeapon)
+						continue;
+
 					if (GameServer.ServerRules.IsSameRealm(npc, this, true))
 					{
 						if (npc.HealthPercent < Properties.KEEP_HEAL_THRESHOLD)
@@ -202,20 +188,11 @@ namespace DOL.GS.Keeps
 
 			if (target != null)
 			{
-				if (LOSChecker == null)
-				{
-					foreach (GamePlayer player in GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
-					{
-						LOSChecker = player;
-						break;
-					}
-				}
-				if (LOSChecker == null)
+				if (!target.IsAlive)
 					return;
-				if (!target.IsAlive) return;
 
 				HealTarget = target;
-				LOSChecker.Out.SendCheckLos(this, target, new CheckLosResponse(GuardStartSpellHealCheckLos));
+				CastSpell(GetGuardHealSmallSpell(Realm), SkillBase.GetSpellLine(GlobalSpellsLines.Mob_Spells), true);
 			}
 		}
 

CoreServer/ConsolePacketLib.cs
@@ -192,7 +192,7 @@ namespace DOL.GameServerConsole
 		public void SendWarmapUpdate(ICollection<IGameKeep> list) { }
 		public void SendWarmapDetailUpdate(List<List<byte>> fights, List<List<byte>> groups) { }
 		public void SendWarmapBonuses() { }
-		public bool SendCheckLos(GameObject source, GameObject target, CheckLosResponse callback) { return true; }
+		public bool SendLosCheckRequest(GameObject source, GameObject target, ILosCheckListener listener) { return true; }
 		public void SendLivingDataUpdate(GameLiving living, bool updateStrings) { }
 		public void SendPlayerTitles() { }
 		public void SendPlayerTitleUpdate(GamePlayer player) { }

GameServer/ECS-Components/Actions/NpcAttackAction.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using DOL.AI.Brain;
+﻿using DOL.AI.Brain;
 using DOL.GS.Keeps;
 using DOL.GS.ServerProperties;
 using static DOL.GS.GameObject;
 
 namespace DOL.GS
 {
-    public class NpcAttackAction : AttackAction
+    public class NpcAttackAction : AttackAction, ILosCheckListener
     {
         private const double TIME_TO_TARGET_THRESHOLD_BEFORE_RANGED_SWITCH = 500; // NPCs will switch to ranged if further than melee range + (this * maxSpeed * 0.001).
 
@@ -170,7 +169,7 @@ namespace DOL.GS
             if (Properties.CHECK_LOS_BEFORE_NPC_RANGED_ATTACK)
             {
                 if (_checkLosTimer == null)
-                    _checkLosTimer = new CheckLosTimer(_npcOwner, _target, OnLosCheck, ForceLos);
+                    _checkLosTimer = new(_npcOwner, _target, this);
                 else if (_losCheckTarget != _target)
                 {
                     _hasLos = false;
@@ -204,6 +203,24 @@ namespace DOL.GS
             base.CleanUp();
         }
 
+        public void HandleLosCheckResponse(GamePlayer player, LosCheckResponse response, ushort targetId)
+        {
+            _losCheckTarget = _npcOwner.CurrentRegion.GetObject(targetId);
+
+            if (_losCheckTarget == null || _losCheckTarget != _target)
+                _hasLos = false;
+            else
+                _hasLos = response is LosCheckResponse.True;
+
+            if (!_hasLos)
+            {
+                OnOutOfRangeOrNoLosRangedAttack();
+                return;
+            }
+
+            _npcOwner.TurnTo(_losCheckTarget);
+        }
+
         private void SwitchToMeleeAndTick()
         {
             if (_npcOwner.ActiveWeaponSlot is not eActiveWeaponSlot.Distance)
@@ -220,24 +237,6 @@ namespace DOL.GS
             _npcOwner.StartAttackWithRangedWeapon(_target);
         }
 
-        private void OnLosCheck(GamePlayer player, LosCheckResponse response, ushort sourceOID, ushort targetOID)
-        {
-            _losCheckTarget = _npcOwner.CurrentRegion.GetObject(targetOID);
-
-            if (_losCheckTarget == null || _losCheckTarget != _target)
-                _hasLos = false;
-            else
-                _hasLos = response is LosCheckResponse.True;
-
-            if (!_hasLos)
-            {
-                OnOutOfRangeOrNoLosRangedAttack();
-                return;
-            }
-
-            _npcOwner.TurnTo(_losCheckTarget);
-        }
-
         private void ForceLos()
         {
             _hasLos = true;
@@ -248,15 +247,13 @@ namespace DOL.GS
         {
             private GameNPC _npcOwner;
             private GameObject _target;
-            private CheckLosResponse _callback;
-            private Action _forceLos;
+            private NpcAttackAction _attackAction;
             private GamePlayer _losChecker;
 
-            public CheckLosTimer(GameObject owner, GameObject target, CheckLosResponse callback, Action forceLos) : base(owner)
+            public CheckLosTimer(GameObject owner, GameObject target, NpcAttackAction attackAction) : base(owner)
             {
                 _npcOwner = owner as GameNPC;
-                _callback = callback;
-                _forceLos = forceLos;
+                _attackAction = attackAction;
                 ChangeTarget(target);
             }
 
@@ -283,7 +280,7 @@ namespace DOL.GS
                 // Don't bother starting the timer if there's no one to perform the LoS check.
                 if (_losChecker == null)
                 {
-                    _forceLos();
+                    _attackAction.ForceLos();
                     return;
                 }
 
@@ -300,7 +297,7 @@ namespace DOL.GS
                 if (!_npcOwner.attackComponent.AttackState || _npcOwner.ObjectState is not eObjectState.Active)
                     return 0;
 
-                _losChecker.Out.SendCheckLos(_npcOwner, _target, new CheckLosResponse(_callback));
+                _losChecker.Out.SendLosCheckRequest(_npcOwner, _target, _attackAction);
                 return LosCheckInterval;
             }
         }

GameServer/ECS-Components/CastingComponent.cs
@@ -6,12 +6,15 @@ using DOL.Events;
 using DOL.GS.PacketHandler;
 using DOL.GS.Spells;
 using DOL.Language;
+using DOL.Logging;
 using static DOL.GS.GameObject;
 
 namespace DOL.GS
 {
     public class CastingComponent : IServiceObject
     {
+        private static readonly Logger log = LoggerManager.Create(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         private const string ALREADY_CASTING_MESSAGE = "You are already casting a spell!";
         private const int NO_QUEUE_INPUT_BUFFER = 250; // 250ms is roughly equivalent to the delay between inputs imposed by the client.
 
@@ -19,6 +22,8 @@ namespace DOL.GS
         private readonly Queue<CastSpellRequest> _castSpellRequestPool = new();
         private readonly Queue<UseAbilityRequest> _useAbilityRequestPool = new();
         private readonly Lock _startSkillRequestsLock = new();
+        private readonly DuringCastLosCheckListener _duringCastLosCheckListener;
+        private readonly EndOfCastLosCheckListener _endOfCastLosCheckListener;
 
         public GameLiving Owner { get; }
         public SpellHandler SpellHandler { get; protected set; }
@@ -29,6 +34,8 @@ namespace DOL.GS
         protected CastingComponent(GameLiving owner)
         {
             Owner = owner;
+            _duringCastLosCheckListener = new(this);
+            _endOfCastLosCheckListener = new(this);
         }
 
         public static CastingComponent Create(GameLiving living)
@@ -106,6 +113,28 @@ namespace DOL.GS
             return null;
         }
 
+        public bool StartDuringCastLosCheck(GameLiving target)
+        {
+            // Target check may appear redundant since CastingComponent is supposed to set LosChecker to null when no LoS check is required.
+            // But for player casted spells, the target is determined by SpellHandler.
+            if (SpellHandler.LosChecker == null || target == Owner)
+                return false;
+
+            _duringCastLosCheckListener.SpellHandler = SpellHandler;
+            SpellHandler.LosChecker.Out.SendLosCheckRequest(Owner, target, _duringCastLosCheckListener);
+            return true;
+        }
+
+        public bool StartEndOfCastLosCheck(GameLiving target, SpellHandler spellHandler)
+        {
+            if (SpellHandler.LosChecker == null || target == Owner)
+                return false;
+
+            _endOfCastLosCheckListener.AddPendingLosCheck(target, spellHandler);
+            SpellHandler.LosChecker.Out.SendLosCheckRequest(Owner, target, _endOfCastLosCheckListener);
+            return true;
+        }
+
         public void RequestUseAbility(Ability ability)
         {
             // Always allowed. The handler will check if the ability can be used or not.
@@ -175,11 +204,8 @@ namespace DOL.GS
 
         public virtual void OnSpellCast(Spell spell) { }
 
-        public void OnSpellHandlerCleanUp(Spell currentSpell)
+        public void PromoteQueuedSpellHandler()
         {
-            if (currentSpell.CastTime <= 0)
-                return;
-
             if (Owner is NecromancerPet necroPet && necroPet.Brain is NecromancerPetBrain necroBrain)
                 necroBrain.CheckAttackSpellQueue();
 
@@ -392,6 +418,79 @@ namespace DOL.GS
             }
 
             public virtual void StartSkill() { }
+        }
+
+        private class DuringCastLosCheckListener : ILosCheckListener
+        {
+            private CastingComponent _castingComponent;
+
+            public SpellHandler SpellHandler { get; set; }
+
+            public DuringCastLosCheckListener(CastingComponent castingComponent)
+            {
+                _castingComponent = castingComponent;
+            }
+
+            public void HandleLosCheckResponse(GamePlayer player, LosCheckResponse response, ushort targetId)
+            {
+                // Ensure the spell handler is still relevant.
+                if (SpellHandler == null || SpellHandler != _castingComponent.SpellHandler)
+                    return;
+
+                // Let the spell handler handle the response on its next tick.
+                SpellHandler.HasLos = response is LosCheckResponse.True;
+            }
+        }
+
+        private class EndOfCastLosCheckListener : ILosCheckListener
+        {
+            private CastingComponent _castingComponent;
+            private Dictionary<ushort, List<SpellHandler>> _pendingLosChecks;
+
+            public EndOfCastLosCheckListener(CastingComponent castingComponent)
+            {
+                _castingComponent = castingComponent;
+            }
+
+            public void AddPendingLosCheck(GameLiving target, SpellHandler spellHandler)
+            {
+                _pendingLosChecks ??= new();
+
+                if (_pendingLosChecks.TryGetValue(target.ObjectID, out var list))
+                    list.Add(spellHandler);
+                else
+                    _pendingLosChecks[target.ObjectID] = [spellHandler]; // Consider pooling if end of cast LoS checks become common.
+            }
+
+            public void HandleLosCheckResponse(GamePlayer player, LosCheckResponse response, ushort targetId)
+            {
+                if (_pendingLosChecks == null)
+                {
+                    if (log.IsErrorEnabled)
+                        log.Error($"{nameof(EndOfCastLosCheckListener)} encountered null {nameof(_pendingLosChecks)}");
+
+                    return;
+                }
+
+                if (!_pendingLosChecks.Remove(targetId, out var spellHandlers))
+                    return;
+
+                if (_castingComponent.Owner.CurrentRegion.GetObject(targetId) is not GameLiving target)
+                    return;
+
+                foreach (SpellHandler spellHandler in spellHandlers)
+                {
+                    if (spellHandler.CastState is not eCastState.Finished)
+                    {
+                        if (log.IsWarnEnabled)
+                            log.Warn($"{nameof(EndOfCastLosCheckListener)} received LoS response for spell handler not in {nameof(eCastState.Finished)} state. (Spell handler: {spellHandler})");
+
+                        continue;
+                    }
+
+                    spellHandler.OnEndOfCastLosCheck(target, response);
+                }
+            }
         }
     }
 }

GameServer/ECS-Components/NpcCastingComponent.cs
@@ -5,7 +5,7 @@ using DOL.GS.Keeps;
 
 namespace DOL.GS
 {
-    public class NpcCastingComponent : CastingComponent
+    public class NpcCastingComponent : CastingComponent, ILosCheckListener
     {
         private GameNPC _npcOwner;
         private Dictionary<GameObject, List<SpellWaitingForLosCheck>> _spellsWaitingForLosCheck = new();
@@ -38,7 +38,7 @@ namespace DOL.GS
                     _spellsWaitingForLosCheck[target] = [spellWaitingForLosCheck];
             }
 
-            losChecker.Out.SendCheckLos(_npcOwner, target, CastSpellLosCheckReply);
+            losChecker.Out.SendLosCheckRequest(_npcOwner, target, this);
             return true; // Consider the NPC is casting while waiting for the reply to prevent it from moving.
         }
 
@@ -97,9 +97,9 @@ namespace DOL.GS
             return livingTarget.ActiveWeaponSlot is not eActiveWeaponSlot.Distance && livingTarget.IsWithinRadius(_npcOwner, livingTarget.attackComponent.AttackRange);
         }
 
-        private void CastSpellLosCheckReply(GamePlayer losChecker, LosCheckResponse response, ushort sourceOID, ushort targetOID)
+        public void HandleLosCheckResponse(GamePlayer losChecker, LosCheckResponse response, ushort targetId)
         {
-            GameObject target = _npcOwner.CurrentRegion.GetObject(targetOID);
+            GameObject target = _npcOwner.CurrentRegion.GetObject(targetId);
 
             if (target == null)
                 return;

GameServer/ai/brain/Animist/TurretFNFBrain.cs
@@ -43,7 +43,7 @@ namespace DOL.AI.Brain
                     continue;
 
                 if (Properties.CHECK_LOS_BEFORE_AGGRO_FNF)
-                    SendLosCheckForAggro(player, player);
+                    SendAggroLosCheck(player, player);
                 else
                     AddToAggroList(player);
             }
@@ -64,12 +64,12 @@ namespace DOL.AI.Brain
                 {
                     if (npc.Brain is ControlledMobBrain theirControlledNpcBrain && theirControlledNpcBrain.GetPlayerOwner() is GamePlayer theirOwner)
                     {
-                        SendLosCheckForAggro(theirOwner, npc);
+                        SendAggroLosCheck(theirOwner, npc);
                         continue;
                     }
                     else if (GetPlayerOwner() is GamePlayer ourOwner)
                     {
-                        SendLosCheckForAggro(ourOwner, npc);
+                        SendAggroLosCheck(ourOwner, npc);
                         continue;
                     }
                 }

GameServer/ai/brain/Guards/GuardBrain.cs
@@ -26,7 +26,7 @@ namespace DOL.AI.Brain
 				if (player.effectListComponent.ContainsEffectForEffectType(eEffect.Shade))
 					continue;
 
-				SendLosCheckForAggro(player, player);
+				SendAggroLosCheck(player, player);
 				// We don't know if the LoS check will be positive, so we have to ask other players
 			}
 		}

GameServer/ai/brain/Guards/KeepGuardBrain.cs
@@ -44,7 +44,7 @@ namespace DOL.AI.Brain
                     continue;
 
                 WarMapMgr.AddGroup((byte) player.CurrentZone.ID, player.X, player.Y, player.Name, (byte) player.Realm);
-                SendLosCheckForAggro(player, player);
+                SendAggroLosCheck(player, player);
                 // We don't know if the LoS check will be positive, so we have to ask other players
             }
         }
@@ -66,7 +66,7 @@ namespace DOL.AI.Brain
                     continue;
 
                 WarMapMgr.AddGroup((byte) player.CurrentZone.ID, player.X, player.Y, player.Name, (byte) player.Realm);
-                SendLosCheckForAggro(player, npc);
+                SendAggroLosCheck(player, npc);
                 // We don't know if the LoS check will be positive, so we have to ask other players.
             }
         }

commit aa9f4a7146ebb3996e0efdea66d5dbc15b5e5c56

Fix healer guards Regression caused by 375edfd9d788dfbb6a32ac6e90fa99b4f9d205b6

GameServer/ai/brain/Guards/Healer.cs
@@ -1,44 +1,40 @@
 using DOL.Events;
-using DOL.GS.Keeps;
 
 namespace DOL.AI.Brain
 {
-	/// <summary>
-	/// Healer Guard Brain
-	/// </summary>
-	public class HealerBrain : KeepGuardBrain
-	{
-		/// <summary>
-		/// Brain Think Method
-		/// </summary>
-		public override void Think()
-		{
-			CheckAreaForHealing();
-			base.Think();
-		}
+    /// <summary>
+    /// Healer Guard Brain
+    /// </summary>
+    public class HealerBrain : KeepGuardBrain
+    {
+        /// <summary>
+        /// Brain Think Method
+        /// </summary>
+        public override void Think()
+        {
+            if (_keepGuardBody.CanUseRanged && _keepGuardBody.CheckAreaForHeals())
+            {
+                Body.StopMoving();
+                Body.StopAttack();
+            }
+            else
+                base.Think();
+        }
 
-		/// <summary>
-		/// Checks the areas for friendlies to heal if we can cast spells
-		/// </summary>
-		private void CheckAreaForHealing()
-		{
-			if (_keepGuardBody.CanUseRanged)
-				_keepGuardBody.CheckAreaForHeals();
-		}
+        /// <summary>
+        /// To be honest, I don't really know what this does,
+        /// I think it stops telling all mobs in area that this guard has healed
+        /// I had some problems before with guards wanting to attack themselves and each other
+        /// </summary>
+        /// <param name="e">The event</param>
+        /// <param name="sender">The sender</param>
+        /// <param name="args">The arguments</param>
+        public override void Notify(DOLEvent e, object sender, System.EventArgs args)
+        {
+            if (e == GameLivingEvent.EnemyHealed)
+                return;
 
-		/// <summary>
-		/// To be honest, I don't really know what this does, 
-		/// I think it stops telling all mobs in area that this guard has healed
-		/// I had some problems before with guards wanting to attack themselves and eachother
-		/// </summary>
-		/// <param name="e">The event</param>
-		/// <param name="sender">The sender</param>
-		/// <param name="args">The arguments</param>
-		public override void Notify(DOLEvent e, object sender, System.EventArgs args)
-		{
-			if (e == GameLivingEvent.EnemyHealed)
-				return;
-			base.Notify(e, sender, args);
-		}
-	}
+            base.Notify(e, sender, args);
+        }
+    }
 }

GameServer/keeps/Gameobjects/Guards/GameKeepGuard.cs
@@ -149,7 +149,7 @@ namespace DOL.GS.Keeps
 			return null;
 		}
 
-		public void CheckAreaForHeals()
+		public bool CheckAreaForHeals()
 		{
 			GameLiving target = null;
 
@@ -172,7 +172,7 @@ namespace DOL.GS.Keeps
 			{
 				foreach (GameNPC npc in GetNPCsInRadius(2000))
 				{
-					if (npc is GameSiegeWeapon)
+					if (npc is GameSiegeWeapon || !npc.IsAlive)
 						continue;
 
 					if (GameServer.ServerRules.IsSameRealm(npc, this, true))
@@ -186,14 +186,14 @@ namespace DOL.GS.Keeps
 				}
 			}
 
-			if (target != null)
-			{
-				if (!target.IsAlive)
-					return;
+			if (target == null)
+				return false;
 
-				HealTarget = target;
-				CastSpell(GetGuardHealSmallSpell(Realm), SkillBase.GetSpellLine(GlobalSpellsLines.Mob_Spells), true);
-			}
+			GameObject oldTarget = TargetObject;
+			TargetObject = target;
+			bool cast = CastSpell(GetGuardHealSmallSpell(Realm), SkillBase.GetSpellLine(GlobalSpellsLines.Mob_Spells), true);
+			TargetObject = oldTarget;
+			return cast;
 		}
 
 		/// <summary>