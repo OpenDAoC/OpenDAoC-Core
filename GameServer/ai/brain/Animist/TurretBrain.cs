OpenDAoC/OpenDAoC-Core

commit 2c0e3ed43e2ed3d0b5cc745634bed79739bbea52

`TurretBrain`: Remove spell type restrictions Removed redundant call to `FindTargetForDefensiveSpell`.

GameServer/ai/brain/Animist/TurretBrain.cs
@@ -38,48 +38,25 @@ namespace DOL.AI.Brain
             {
                 case eCheckSpellType.Defensive:
                 {
-                    switch (spell.SpellType)
-                    {
-                        case eSpellType.HeatColdMatterBuff:
-                        case eSpellType.BodySpiritEnergyBuff:
-                        case eSpellType.ArmorAbsorptionBuff:
-                        case eSpellType.AblativeArmor:
-                        {
-                            GameLiving target = FindTargetForDefensiveSpell(spell);
+                    GameLiving target = FindTargetForDefensiveSpell(spell);
 
-                            if (target != null)
-                                return TrustCast(spell, eCheckSpellType.Defensive, FindTargetForDefensiveSpell(spell), CheckLosBeforeCastingDefensiveSpells);
+                    if (target != null)
+                        return TrustCast(spell, eCheckSpellType.Defensive, target, CheckLosBeforeCastingDefensiveSpells);
 
-                            break;
-                        }
-                    }
-
-                    return false;
+                    break;
                 }
                 case eCheckSpellType.Offensive:
                 {
-                    switch (spell.SpellType)
-                    {
-                        case eSpellType.DirectDamage:
-                        case eSpellType.DamageSpeedDecrease:
-                        case eSpellType.SpeedDecrease:
-                        case eSpellType.Taunt:
-                        case eSpellType.MeleeDamageDebuff:
-                        {
-                            GameLiving target = CalculateNextAttackTarget();
+                    GameLiving target = CalculateNextAttackTarget();
 
-                            if (target != null)
-                                return TrustCast(spell, eCheckSpellType.Offensive, target, CheckLosBeforeCastingOffensiveSpells);
+                    if (target != null)
+                        return TrustCast(spell, eCheckSpellType.Offensive, target, CheckLosBeforeCastingOffensiveSpells);
 
-                            break;
-                        }
-                    }
-
-                    return false;
+                    break;
                 }
-                default:
-                    return false;
             }
+
+            return false;
         }
 
         protected override GameLiving FindTargetForDefensiveSpell(Spell spell)

GameServer/ai/brain/ControlledMob/ControlledMobBrain.cs
@@ -458,6 +458,7 @@ namespace DOL.AI.Brain
         {
             GameLiving target = null;
 
+            // This does not include every spell type.
             switch (spell.SpellType)
             {
                 #region Buffs

GameServer/ai/brain/StandardMob/StandardMobBrain.cs
@@ -1144,6 +1144,7 @@ namespace DOL.AI.Brain
         {
             GameLiving target = null;
 
+            // This does not include every spell type.
             switch (spell.SpellType)
             {
                 #region Buffs

commit 09f1bebfa95bc1f43babfddb8803db0a29ddb82d

Fix FnF turrets trying to cast spells on wrong targets Regression caused by 2c0e3ed43e2ed3d0b5cc745634bed79739bbea52

GameServer/ai/brain/Animist/TurretBrain.cs
@@ -38,6 +38,9 @@ namespace DOL.AI.Brain
             {
                 case eCheckSpellType.Defensive:
                 {
+                    if (spell.IsHarmful)
+                        return false;
+
                     GameLiving target = FindTargetForDefensiveSpell(spell);
 
                     if (target != null)
@@ -47,6 +50,9 @@ namespace DOL.AI.Brain
                 }
                 case eCheckSpellType.Offensive:
                 {
+                    if (!spell.IsHarmful)
+                        return false;
+
                     GameLiving target = CalculateNextAttackTarget();
 
                     if (target != null)