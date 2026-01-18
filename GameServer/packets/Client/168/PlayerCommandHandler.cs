c7aa6a073d22fee3d42685e661900b221d24c4f7
Baptiste Marie <bm01@users.noreply.github.com>
2026-01-18 01:39:42 +0100

    `PlayerCommandHandler`: Add override to `GetLogContext`

--- GameServer/packets/Client/168/PlayerCommandHandler.cs
+++ GameServer/packets/Client/168/PlayerCommandHandler.cs
@@ -1,20 +1,31 @@
 namespace DOL.GS.PacketHandler.Client.v168
 {
-	[PacketHandlerAttribute(PacketHandlerType.TCP, eClientPackets.CommandHandler, "Handles the players commands", eClientStatus.PlayerInGame)]
-	public class PlayerCommandHandler : PacketHandler
-	{
-		protected override void HandlePacketInternal(GameClient client, GSPacketIn packet)
-		{
-			packet.Skip(1);
-			if (client.Version < GameClient.eClientVersion.Version1127)
-				packet.Skip(7);
-			string cmdLine = packet.ReadString(255);
-			if(!ScriptMgr.HandleCommand(client, cmdLine))
-			{
-				if (cmdLine[0] == '&')
-					cmdLine = "/" + cmdLine.Remove(0, 1);
-				client.Out.SendMessage($"No such command ({cmdLine})", eChatType.CT_System,eChatLoc.CL_SystemWindow);
-			}
-		}
-	}
+    [PacketHandlerAttribute(PacketHandlerType.TCP, eClientPackets.CommandHandler, "Handles the players commands", eClientStatus.PlayerInGame)]
+    public class PlayerCommandHandler : PacketHandler
+    {
+        private string _lastUsedCommand = string.Empty;
+
+        protected override void HandlePacketInternal(GameClient client, GSPacketIn packet)
+        {
+            packet.Skip(1);
+
+            if (client.Version < GameClient.eClientVersion.Version1127)
+                packet.Skip(7);
+
+            _lastUsedCommand = packet.ReadString(255);
+
+            if (!ScriptMgr.HandleCommand(client, _lastUsedCommand))
+            {
+                if (_lastUsedCommand.Length > 0 && _lastUsedCommand[0] == '&')
+                    _lastUsedCommand = "/" + _lastUsedCommand[1..];
+
+                client.Out.SendMessage($"No such command ({_lastUsedCommand})", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+            }
+        }
+
+        protected override string GetLogContext()
+        {
+            return _lastUsedCommand;
+        }
+    }
 }
