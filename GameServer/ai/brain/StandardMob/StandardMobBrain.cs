e49c11c5c17cae45bcff15fa391f9db3f7c0c56b
Baptiste Marie <bm01@users.noreply.github.com>
2026-02-14 07:00:40 +0100

    Change confusion expiration to keep the current aggro list if the previous one is empty

--- GameServer/ai/brain/StandardMob/StandardMobBrain.cs
+++ GameServer/ai/brain/StandardMob/StandardMobBrain.cs
@@ -425,7 +425,9 @@ namespace DOL.AI.Brain
 
         public bool UnsetTemporaryAggroList()
         {
-            if (_tempAggroList == null)
+            // Keep the current aggro list if the previous one is empty.
+            // This can happen when amnesia is used during confusion.
+            if (_tempAggroList == null || _tempAggroList.IsEmpty)
                 return false;
 
             AggroList = _tempAggroList;
