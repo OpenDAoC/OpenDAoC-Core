OpenDAoC/OpenDAoC-Core

commit 566a654ecc3b967af636e96213a54d8142357ce6

Implement distinction between ABS buffs and ABS debuffs Live makes a distinction between armor absorption buffs / debuffs and physical absorption buffs / debuffs. The former affects only the armor, the second is applied similarly to a second resist layer. As far as I can tell, every single ABS buff is meant to be applied as a physical ABS buff, whereas every single ABS debuff is meant to be applied as an armor ABS debuff. If this isn't the case, a new spell type will need to be used and database values be updated. For now, one spell type but two effect types is good enough. * Added `PhysicalAbsorptionCalculator`, `eProperty.PhysicalAbsorption`, `eEffect.PhysicalAbsorptionBuff`. * Removed `eEffect.ArmorAbsorptionBuff` (no longer relevant). * Updated `AttackComponent` to use `PhysicalAbsorptionCalculator`. * Updated `GamePlayer.GetArmorAbsorb` and `GameLiving.GetArmorAbsorb` to apply `eProperty.ArmorAbsorption` multiplicatively. Normally, some debuffs are subtractive to armor absorb, some are multiplicative, but subtractive ones don't exist yet in 1.65. * Updated various classes (ML and artifact related) that were using `eProperty.ArmorAbsorption` directly to either use `eProperty.ArmorAbsorption` or `eProperty.PhysicalAbsorption` based on what they seem to be supposed to do. Probably incorrect for some.

GameServer/ECS-Components/AttackComponent.cs
@@ -1095,22 +1095,16 @@ namespace DOL.GS
                     double armorMod = CalculateTargetArmor(ad.Target, ad.ArmorHitLocation, out double armorFactor, out double absorb);
                     double damageMod = weaponSkill / armorMod;
 
-                    // Badge Of Valor Calculation 1+ absorb or 1- absorb
-                    // if (ad.Attacker.EffectList.GetOfType<BadgeOfValorEffect>() != null)
-                    //     damage *= 1.0 + Math.Min(0.85, ad.Target.GetArmorAbsorb(ad.ArmorHitLocation));
-                    // else
-                    //     damage *= 1.0 - Math.Min(0.85, ad.Target.GetArmorAbsorb(ad.ArmorHitLocation));
-
                     if (ad.IsOffHand)
-                        damage *= 1 + owner.GetModified(eProperty.OffhandDamage) * 0.01;
+                        damageMod *= 1 + owner.GetModified(eProperty.OffhandDamage) * 0.01;
 
                     // If the target is another player's pet, shouldn't 'PVP_MELEE_DAMAGE' be used?
                     if (owner is GamePlayer || (owner is GameNPC npcOwner && npcOwner.Brain is IControlledBrain && owner.Realm != 0))
                     {
                         if (target is GamePlayer)
-                            damage *= Properties.PVP_MELEE_DAMAGE;
+                            damageMod *= Properties.PVP_MELEE_DAMAGE;
                         else if (target is GameNPC)
-                            damage *= Properties.PVE_MELEE_DAMAGE;
+                            damageMod *= Properties.PVE_MELEE_DAMAGE;
                     }
 
                     damage *= damageMod;
@@ -1347,12 +1341,16 @@ namespace DOL.GS
         {
             armorFactor = target.GetArmorAF(armorSlot);
 
-            // Gives an extra 0.4~20 bonus AF to players. Ideally this should be done in `ArmorFactorCalculator`.
+            // Give an extra 0.4~20 bonus AF to players. Ideally this should be done in `ArmorFactorCalculator`.
             if (target is GamePlayer or GameTrainingDummy)
                 armorFactor += target.Level * 20 / 50.0;
 
-            absorb = target.GetArmorAbsorb(armorSlot);
-            return absorb >= 1 ? double.MaxValue : armorFactor / (1 - absorb);
+            double armorAbsorb = target.GetArmorAbsorb(armorSlot);
+            // Badge of Valor check should go here.
+            double physicalAbsorb = target.GetModified(eProperty.PhysicalAbsorption) * 0.01;
+            double absorbDivisor = (1 - armorAbsorb) * (1 - physicalAbsorb);
+            absorb = 1 - absorbDivisor;
+            return absorb >= 1 ? double.MaxValue : armorFactor / absorbDivisor;
         }
 
         public static double CalculateTargetResistance(GameLiving target, eDamageType damageType, DbInventoryItem armor)

GameServer/gameobjects/GameLiving.cs
@@ -328,17 +328,24 @@ namespace DOL.GS
 		/// </summary>
 		public virtual double GetArmorAbsorb(eArmorSlot slot)
 		{
-			double baseAbsorb = 0;
+			double baseAbsorb;
 
 			if (this is NecromancerPet necromancerPet)
 				baseAbsorb = necromancerPet.Owner.Level * 0.0068; // 34% at owner level 50.
 			else
 				baseAbsorb = Level * 0.0054; // 27% at level 50.
 
-			double absorbBonus = GetModified(eProperty.ArmorAbsorption) / 100.0;
+			// Stat buffs can be very high, and absorption should not be allowed to reach 100%.
+			// However, stat debuffs can freely drop absorption to 0%.
 			double absorptionFromConstitution = StatCalculator.CalculateBuffContributionToAbsorbOrResist(this, eProperty.Constitution) / 5.5;
 			double absorptionFromDexterity = StatCalculator.CalculateBuffContributionToAbsorbOrResist(this, eProperty.Dexterity) / 5.5;
-			double absorb = 1 - (1 - baseAbsorb) * (1 - absorbBonus) * (1 - absorptionFromConstitution) * (1 - absorptionFromDexterity);
+
+			// eProperty.ArmorAbsorption is normally used as an armor effectiveness debuff, where 50% increases damage taken by 50%.
+			// If used for buffs, this could push ABS to 100%.
+			double armorAbsorptionBuffDebuffMod = 1 + GetModified(eProperty.ArmorAbsorption) * 0.01;
+
+			double absorb = 1 - (1 - baseAbsorb) * (1 - absorptionFromConstitution) * (1 - absorptionFromDexterity);
+			absorb *= armorAbsorptionBuffDebuffMod;
 			return Math.Clamp(absorb, 0, 1);
 		}
 

GameServer/Enums/eEffect.cs
@@ -2,7 +2,6 @@ namespace DOL.GS
 {
     public enum eEffect
     {
-        
         #region Positive Effects
         //positive effects
         Bladeturn,
@@ -28,7 +27,7 @@ namespace DOL.GS
         BaseAFBuff,
         SpecAFBuff,
         PaladinAf,
-        ArmorAbsorptionBuff,
+        PhysicalAbsorptionBuff,
 
         //resists
         BodyResistBuff,

GameServer/Enums/eProperty.cs
@@ -307,6 +307,7 @@
         RealmPoints = 253,
         ArcaneSyphon = 254,
         LivingEffectiveness = 255,
-        MaxProperty = 255
+        PhysicalAbsorption = 256,
+        MaxProperty = 256
     }
 }

GameServer/gameobjects/GamePlayer.cs
@@ -5614,7 +5614,7 @@ namespace DOL.GS
         /// </summary>
         public override double GetArmorAbsorb(eArmorSlot slot)
         {
-            if (slot == eArmorSlot.NOTSET)
+            if (slot is eArmorSlot.NOTSET)
                 return 0;
 
             DbInventoryItem item = Inventory.GetItem((eInventorySlot)slot);
@@ -5623,7 +5623,9 @@ namespace DOL.GS
                 return 0;
 
             // Debuffs can't lower absorb below 0%: https://darkageofcamelot.com/article/friday-grab-bag-08302019
-            return Math.Clamp((item.SPD_ABS + GetModified(eProperty.ArmorAbsorption)) * 0.01, 0, 1);
+            // ABS debuffs can either be multiplicative (if appended with a minus sign) or flat. In 1.65, all debuffs are believed to be multiplicative.
+            double absorb = item.SPD_ABS * 0.01 * (1 + GetModified(eProperty.ArmorAbsorption) * 0.01);
+            return Math.Clamp(absorb, 0, 1);
         }
 
         /// <summary>

GameServer/gameutils/EffectHelper.cs
@@ -79,7 +79,7 @@ namespace DOL.GS
                 case eSpellType.AcuityBuff:
                     return eEffect.AcuityBuff;
                 case eSpellType.ArmorAbsorptionBuff:
-                    return eEffect.ArmorAbsorptionBuff;
+                    return eEffect.PhysicalAbsorptionBuff; // Every ABS buff are applied as secondary ABS buff and don't modify armor ABS.
                 case eSpellType.BaseArmorFactorBuff:
                     return eEffect.BaseAFBuff;
                 case eSpellType.SpecArmorFactorBuff:
@@ -348,9 +348,11 @@ namespace DOL.GS
                 case eEffect.ArmorFactorDebuff:
                     list.Add(eProperty.ArmorFactor);
                     return list;
-                case eEffect.ArmorAbsorptionBuff:
                 case eEffect.ArmorAbsorptionDebuff:
                     list.Add(eProperty.ArmorAbsorption);
+                    return list;
+                case eEffect.PhysicalAbsorptionBuff:
+                    list.Add(eProperty.PhysicalAbsorption);
                     return list;
                 case eEffect.MeleeDamageBuff:
                 case eEffect.MeleeDamageDebuff:

GameServer/propertycalc/PhysicalAbsorptionCalculator.cs
@@ -1,0 +1,16 @@
+﻿namespace DOL.GS.PropertyCalc
+{
+    [PropertyCalculator(eProperty.PhysicalAbsorption)]
+    public class PhysicalAbsorptionCalculator : PropertyCalculator
+    {
+        public override int CalcValue(GameLiving living, eProperty property)
+        {
+            // Placeholder, caps and usages unknown.
+            int abilityBonus = living.AbilityBonus[property];
+            int itemBonus = living.ItemBonus[property];
+            int buffBonus = living.BaseBuffBonusCategory[property] + living.SpecBuffBonusCategory[property];
+            int debuffMalus = living.DebuffCategory[property];
+            return abilityBonus + buffBonus + itemBonus - debuffMalus;
+        }
+    }
+}

GameServer/realmabilities/effects/BarrierOfFortitudeEffect.cs
@@ -42,7 +42,7 @@ namespace DOL.GS.Effects
 
 			GameEventMgr.AddHandler(m_player, GamePlayerEvent.Quit, new DOLEventHandler(PlayerLeftWorld));
 
-			m_player.AbilityBonus[eProperty.ArmorAbsorption] += m_value;
+			m_player.AbilityBonus[eProperty.PhysicalAbsorption] += m_value;
 
 			m_player.EffectList.Add(this);
 			player.TempProperties.SetProperty(RealmAbilities.BarrierOfFortitudeAbility.BofBaSb, true);
@@ -73,7 +73,7 @@ namespace DOL.GS.Effects
 		{
 
 			StopTimers();
-			m_player.AbilityBonus[eProperty.ArmorAbsorption] -= m_value;
+			m_player.AbilityBonus[eProperty.PhysicalAbsorption] -= m_value;
 			m_player.EffectList.Remove(this);
 			GameEventMgr.RemoveHandler(m_player, GamePlayerEvent.Quit, new DOLEventHandler(PlayerLeftWorld));
 			m_player.TempProperties.RemoveProperty(RealmAbilities.BarrierOfFortitudeAbility.BofBaSb);

commit e681b989fc0f40dbb28ae076ef8b752183d1382c

Fix stealth openers not taking ABS buffs into account * Regression caused by 566a654ecc3b967af636e96213a54d8142357ce6 * Extracted absorb factor calculation from AttackComponent.CalculateTargetArmor to CalculateTargetAbsorbFactor * Renamed CalculateTargetResistance to CalculateTargetResistanceFactor, and CalculateTargetConversion to CalculateTargetConversionFactor to better reflect what the results are.

GameServer/ECS-Components/AttackComponent.cs
@@ -1122,7 +1122,7 @@ namespace DOL.GS
 
                     // The wiki applies resistance on the damage mod then eventually caps it at 3.
                     // We apply the resistances later, and calculate the cap differently. But it should be mathematically equivalent.
-                    double resistMod = CalculateTargetResistance(ad.Target, ad.DamageType, armor);
+                    double resistMod = CalculateTargetResistanceFactor(ad.Target, ad.DamageType, armor);
 
                     // Melee damage and style damage ToA bonuses are pretty weird.
                     // * They're both calculated from base damage.
@@ -1168,7 +1168,7 @@ namespace DOL.GS
                     damage = postResistDamage;
 
                     // Apply conversion.
-                    double conversionMod = CalculateTargetConversion(ad.Target);
+                    double conversionMod = CalculateTargetConversionFactor(ad.Target);
                     damage *= conversionMod;
                     styleDamage *= conversionMod;
 
@@ -1353,27 +1353,32 @@ namespace DOL.GS
             if (target is GamePlayer or GameTrainingDummy)
                 armorFactor += target.Level * PLAYER_EXTRA_AF_PER_LEVEL;
 
+            double absorbFactor = CalculateTargetAbsorbFactor(target, armorSlot);
+            absorb = 1 - absorbFactor;
+            return absorb >= 1 ? double.MaxValue : Math.Max(1, armorFactor / absorbFactor);
+        }
+
+        public static double CalculateTargetAbsorbFactor(GameLiving target, eArmorSlot armorSlot)
+        {
             double armorAbsorb = target.GetArmorAbsorb(armorSlot);
             // Badge of Valor check should go here.
             double physicalAbsorb = target.GetModified(eProperty.PhysicalAbsorption) * 0.01;
-            double absorbDivisor = (1 - armorAbsorb) * (1 - physicalAbsorb);
-            absorb = 1 - absorbDivisor;
-            return absorb >= 1 ? double.MaxValue : Math.Max(1, armorFactor / absorbDivisor);
+            return (1 - armorAbsorb) * (1 - physicalAbsorb);
         }
 
-        public static double CalculateTargetResistance(GameLiving target, eDamageType damageType, DbInventoryItem armor)
+        public static double CalculateTargetResistanceFactor(GameLiving target, eDamageType damageType, DbInventoryItem armor)
         {
-            double damageModifier = 1.0;
-            damageModifier *= 1.0 - (target.GetResist(damageType) + SkillBase.GetArmorResist(armor, damageType)) * 0.01;
-            return damageModifier;
+            int targetResist = target.GetResist(damageType);
+            int armorResist = SkillBase.GetArmorResist(armor, damageType);
+            return 1 - (targetResist + armorResist) * 0.01;
         }
 
-        public static double CalculateTargetConversion(GameLiving target)
+        public static double CalculateTargetConversionFactor(GameLiving target)
         {
             if (target is not GamePlayer)
                 return 1.0;
 
-            double conversionMod = 1 - target.GetModified(eProperty.Conversion) / 100.0;
+            double conversionMod = 1 - target.GetModified(eProperty.Conversion) * 0.01;
             return Math.Min(1.0, conversionMod);
         }
 

GameServer/gameobjects/GameLiving.cs
@@ -321,7 +321,9 @@ namespace DOL.GS
 		public virtual double GetArmorAbsorb(eArmorSlot slot)
 		{
 			// Combine base and stats using multiplicative stacking for diminishing returns.
-			// This doesn't apply to `eProperty.ArmorAbsorption` (normally used as debuffs).
+			// This doesn't apply to eProperty.ArmorAbsorption (normally used as debuffs).
+			// This method doesn't handle eProperty.PhysicalAbsorption (handled by AttackComponent),
+			// and thus doesn't reflect the final absorb.
 			double absorb = 1 - (1 - GetBaseAbsorb()) * (1 - GetStatContributionToArmorAbsorb());
 			absorb *= 1 + GetModified(eProperty.ArmorAbsorption) * 0.01;
 			return Math.Clamp(absorb, 0, 1);

GameServer/spells/Masterlevel/Battlemaster.cs
@@ -526,6 +526,8 @@ namespace DOL.GS.Spells
 
         public override AttackData CalculateDamageToTarget(GameLiving target)
         {
+            // A lot of things here is outdated and need to be rewritten.
+
             GamePlayer player = Caster as GamePlayer;
 
             if (player == null)

GameServer/spells/SpellHandler.cs
@@ -3013,7 +3013,7 @@ namespace DOL.GS.Spells
 			finalDamage = ModifyDamageWithTargetResist(ad, finalDamage);
 
 			// Apply conversion.
-			double conversionMod = AttackComponent.CalculateTargetConversion(ad.Target);
+			double conversionMod = AttackComponent.CalculateTargetConversionFactor(ad.Target);
 			finalDamage *= conversionMod;
 
 			// Apply damage cap.

GameServer/styles/StyleProcessor.cs
@@ -344,7 +344,7 @@ namespace DOL.GS.Styles
 					// Stealth openers are unaffected by weapon speed.
 					// Styles with a static growth don't use unstyled damage, so armor has to be taken into account here.
 					// AF isn't taken into account because we don't have a weaponskill to compare it to. This may be a problem.
-					styleDamage *= 1.0 - target.GetArmorAbsorb(attackData.ArmorHitLocation);
+					styleDamage *= AttackComponent.CalculateTargetAbsorbFactor(target, attackData.ArmorHitLocation);
 					styleDamageCap = -1; // Uncapped. Is there supposed to be one?
 				}
 				else