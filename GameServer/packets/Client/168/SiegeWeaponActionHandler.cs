OpenDAoC/OpenDAoC-Core

commit 094ba909bc66e4c7fdf3b7440b8627f55726f599

Use `IsCrowdControlled`, `IsIncapacitated` where possible

GameServer/packets/Client/168/SiegeWeaponActionHandler.cs
@@ -23,7 +23,7 @@ namespace DOL.GS.PacketHandler.Client.v168
 				client.Out.SendMessage("You can't fire a siege weapon while sitting!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
 				return;
 			}
-			if (!client.Player.IsAlive || client.Player.IsMezzed || client.Player.IsStunned)
+			if (!client.Player.IsIncapacitated)
 			{
 				client.Out.SendMessage("You can't control a siege weapon now!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
 				return;

GameServer/ECS-Components/Actions/CraftAction.cs
@@ -32,7 +32,7 @@ namespace DOL.GS
 
         public void Tick()
         {
-            if (_owner.IsMezzed || _owner.IsStunned)
+            if (_owner.IsCrowdControlled)
             {
                 CleanupCraftAction();
                 return;

GameServer/ECS-Components/CastingComponent.cs
@@ -220,7 +220,7 @@ namespace DOL.GS
 
         protected virtual bool CanCastSpell()
         {
-            return !Owner.IsStunned && !Owner.IsMezzed && !Owner.IsSilenced;
+            return !Owner.IsCrowdControlled && !Owner.IsSilenced;
         }
 
         public void ReturnToPool(CastSpellRequest request)

GameServer/ECS-Components/NpcMovementComponent.cs
@@ -322,7 +322,7 @@ namespace DOL.GS
 
         public void TurnTo(ushort heading, int duration = 0)
         {
-            if (Owner.Heading == heading || Owner.IsStunned || Owner.IsMezzed || IsTurningDisabled)
+            if (Owner.Heading == heading || Owner.IsCrowdControlled || IsTurningDisabled)
                 return;
 
             if (duration > 0)

GameServer/ECS-Services/EffectService.cs
@@ -176,7 +176,7 @@ namespace DOL.GS
 
                     if (spell.IsHarmful && spell.SpellType is not eSpellType.SpeedDecrease)
                     {
-                        if (!pulseEffect.Owner.IsMezzed && !pulseEffect.Owner.IsStunned)
+                        if (!pulseEffect.Owner.IsCrowdControlled)
                             (spellHandler as SpellHandler).SendCastAnimation();
                     }
                 }

GameServer/commands/playercommands/emotes.cs
@@ -95,7 +95,7 @@ namespace DOL.GS.Commands
 			}
 
 			// no emotes in combat / mez / stun
-			if (client.Player.attackComponent.AttackState || client.Player.IsMezzed || client.Player.IsStunned)
+			if (client.Player.attackComponent.AttackState || client.Player.IsCrowdControlled)
 			{
 				DisplayMessage(client, "You can't do that, you're busy!");
 				return;

GameServer/gameobjects/GameSiegeWeapon.cs
@@ -536,7 +536,7 @@ namespace DOL.GS
 			if (Owner == null)
 				return false;
 			Owner.Stealth(false);
-			if (!Owner.IsAlive || Owner.IsMezzed || Owner.IsStunned)
+			if (Owner.IsIncapacitated)
 			{
 				this.Owner.Out.SendMessage("You can't use this siegeweapon now!", eChatType.CT_Say, eChatLoc.CL_SystemWindow);
 				return false;

GameServer/keeps/Gameobjects/Guards/GateKeeper.cs
@@ -53,7 +53,7 @@ namespace DOL.GS.Keeps
 
             if (player.Client.Account.PrivLevel == 1)
             {
-                if (IsCasting || player.Realm != this.Realm || !player.IsAlive || player.IsStunned || player.IsMezzed || !IsWithinRadius(player, 350))
+                if (IsCasting || player.Realm != this.Realm || player.IsIncapacitated || !IsWithinRadius(player, 350))
                     return false;
             }
             this.LoadDestinations();
@@ -81,7 +81,7 @@ namespace DOL.GS.Keeps
 
             if (player.Client.Account.PrivLevel == 1)
             {
-                if (player.Realm != this.Realm || !player.IsAlive || player.IsStunned || player.IsMezzed || !IsWithinRadius(player, 350))
+                if (player.Realm != this.Realm || player.IsIncapacitated || !IsWithinRadius(player, 350))
                     return false;
             }
 
@@ -245,7 +245,7 @@ namespace DOL.GS.Keeps
 
             if (player.Client.Account.PrivLevel == 1)
             {
-                if (IsCasting || player.Realm != this.Realm || !player.IsAlive || player.IsStunned || player.IsMezzed || !IsWithinRadius(player, 350))
+                if (IsCasting || player.Realm != this.Realm || player.IsIncapacitated || !IsWithinRadius(player, 350))
                     return false;
             }
 
@@ -274,7 +274,7 @@ namespace DOL.GS.Keeps
 
             if (player.Client.Account.PrivLevel == 1)
             {
-                if (player.Realm != this.Realm || !player.IsAlive || player.IsStunned || player.IsMezzed || !IsWithinRadius(player, 350))
+                if (player.Realm != this.Realm || player.IsIncapacitated || !IsWithinRadius(player, 350))
                     return false;
             }
 

commit 0eef9827a064fe6d7083d4825a37831ba579b7d0

`SiegeWeaponActionHandler`: Fix regression caused by 094ba909bc66e4c7fdf3b7440b8627f55726f599

GameServer/packets/Client/168/SiegeWeaponActionHandler.cs
@@ -23,7 +23,7 @@ namespace DOL.GS.PacketHandler.Client.v168
 				client.Out.SendMessage("You can't fire a siege weapon while sitting!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
 				return;
 			}
-			if (!client.Player.IsIncapacitated)
+			if (client.Player.IsIncapacitated)
 			{
 				client.Out.SendMessage("You can't control a siege weapon now!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
 				return;