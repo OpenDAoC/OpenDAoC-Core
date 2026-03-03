OpenDAoC/OpenDAoC-Core

commit bdbf89a3ac83ce5a06a729ea23a4ee88e9bcaaae

Fix immunity effects starting on dead players * Fixed by adding a IsAlive check in ECSGameEffect.Start. Alternatively, this check could have been moved to TryApplyImmunity, but unless some effects are supposed to be allowed to start on dead players, this is safer. * Updated SpellHandler.OnDurationEffectApply to ensure only started effects (technically effects that will start) are added to PulseEffect.ChildEffects.

GameServer/spells/SpellHandler.cs
@@ -2192,7 +2192,7 @@ namespace DOL.GS.Spells
 
 			ECSGameSpellEffect effect = CreateECSEffect(new(target, CalculateEffectDuration(target), CalculateBuffDebuffEffectiveness(), this));
 
-			if (PulseEffect != null)
+			if (PulseEffect != null && effect.IsStarting)
 				PulseEffect.ChildEffects[target] = effect;
 		}
 		

GameServer/ECS-Effects/ECSGameEffect.cs
@@ -68,7 +68,7 @@ namespace DOL.GS
 
         public bool Start()
         {
-            if (!CanStart)
+            if (!CanStart || !Owner.IsAlive)
                 return false;
 
             lock (_stateLock)

commit 020f1b8e8fede7cdbc0b6266c158bb16f48ea83f

Fix pulsing effects not being cancelled properly in combat Regression caused by bdbf89a3ac83ce5a06a729ea23a4ee88e9bcaaae. `IsStarting` isn't meant to be used here, the transitional state is already changed to none if everything goes right.

GameServer/spells/SpellHandler.cs
@@ -2192,7 +2192,7 @@ namespace DOL.GS.Spells
 
 			ECSGameSpellEffect effect = CreateECSEffect(new(target, CalculateEffectDuration(target), CalculateBuffDebuffEffectiveness(), this));
 
-			if (PulseEffect != null && effect.IsStarting)
+			if (PulseEffect != null)
 				PulseEffect.ChildEffects[target] = effect;
 		}
 		