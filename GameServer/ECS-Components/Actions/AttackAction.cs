OpenDAoC/OpenDAoC-Core

commit f88ce5f8bda7c42ee54cdc3dce924604730a6463

`AttackAction`: Hide `NextTick`, add `AttackRoundEndTime`. Using NextTick to tell when the current attack round ends isn't causing any confirmed issue, but because it's shared between ranged and melee attack depending on the currently equiped weapon, this could easily break in the future. Used by: * Necromancer pets since they currently don't use self interrupt when attacking in melee. * BlockRoundCountDecrementTimer. * Wrath of Mordred for some reason. * Critical Shot in the next commit, instead of relying on the target's LastAttackData (might get overwritten by a ranged attack in rare cases I think) and an extra field in AttackData (which is already a fairly heavy class, instantiated on every attack).

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -24,10 +24,14 @@ namespace DOL.GS
         private long _nextRangedTick;
         private bool _firstTick = true;
 
-        // Set to current time when a round doesn't result in an attack. Used to prevent combat log spam and kept until reset in AttackComponent.SendAttackingCombatMessages().
+        // Set to current time when a round doesn't result in an attack.
+        // Used to prevent combat log spam and kept until reset by AttackComponent.SendAttackingCombatMessages.
         public long RoundWithNoAttackTime { get; set; }
+
+        // When the current melee attack round is scheduled to end.
+        public long AttackRoundEndTime { get; private set; }
+
         public AttackData LastAttackData { get; set; }
-        public long NextTick => _owner.ActiveWeaponSlot != eActiveWeaponSlot.Distance ? _nextMeleeTick : _nextRangedTick;
         protected AttackComponent AttackComponent => _owner.attackComponent;
         protected StyleComponent StyleComponent => _owner.styleComponent;
 
@@ -360,7 +364,7 @@ namespace DOL.GS
 
             // A positive ticksToTarget means the effects of our attack will be delayed. Typically used for ranged attacks.
             if (_ticksToTarget > 0)
-                new ECSGameTimer(_owner, new ECSGameTimer.ECSTimerCallback(AttackComponent.weaponAction.Execute), _ticksToTarget);
+                _ = new ECSGameTimer(_owner, AttackComponent.weaponAction.Execute, _ticksToTarget);
             else
                 AttackComponent.weaponAction.Execute();
         }
@@ -396,6 +400,7 @@ namespace DOL.GS
             }
 
             _interval = AttackComponent.AttackSpeed(_weapon, _leftWeapon);
+            AttackRoundEndTime = _nextMeleeTick + _interval;
             return true;
         }
 

GameServer/ECS-Components/AttackComponent.cs
@@ -2429,8 +2429,9 @@ namespace DOL.GS
                 }
 
                 // Decrement the count after a duration equal to the attack interval.
-                // We need to make sure it ticks before the attacker's next attack. We can't use `AttackData.Interval` only because `AttackAction.NextTick` is adjusted by `ServiceUtil.ShouldTickAdjust`.
-                new BlockRoundCountDecrementTimer(_owner, Relinquish).Start((int) (attackData.Attacker.attackComponent.attackAction.NextTick - GameLoop.GameLoopTime + attackData.Interval));
+                // We need to make sure it ticks before the attacker's next attack.
+                // We can't do GameLoop.GameLoopTime + AttackData.Interval to calculate when the attack round ends, because AttackAction catches up on lost time caused by the game loop's resolution.
+                new BlockRoundCountDecrementTimer(_owner, Relinquish).Start((int) (attackData.Attacker.attackComponent.attackAction.AttackRoundEndTime - GameLoop.GameLoopTime));
                 return true;
             }
 

GameServer/ai/brain/Necromancer/NecromancerPetBrain.cs
@@ -113,7 +113,7 @@ namespace DOL.AI.Brain
         {
             // Only start casting if the pet has finished his attack round.
             // This will be false most of the time, unless called from the attack component directly.
-            if (!GameServiceUtils.ShouldTick(Body.attackComponent.attackAction.NextTick))
+            if (!GameServiceUtils.ShouldTick(Body.attackComponent.attackAction.AttackRoundEndTime))
             {
                 MessageToOwner(LanguageMgr.GetTranslation((Owner as GamePlayer).Client.Account.Language, "AI.Brain.Necromancer.CastSpellAfterAction", Body.Name), eChatType.CT_System, Owner as GamePlayer);
                 return false;

GameServer/scripts/namedmobs/Albion Named Mobs/Classic Zones/WrathOfMordred.cs
@@ -177,7 +177,7 @@ namespace DOL.AI.Brain
 					{
 						AttackAction attackAction = Body.attackComponent.attackAction;
 
-						if (attackAction.NextTick - GameLoop.GameLoopTime <= 800 && CanWalk == false)
+						if (attackAction.AttackRoundEndTime - GameLoop.GameLoopTime <= 800 && CanWalk == false)
 						{
 							Body.styleComponent.NextCombatStyle = null;
 							Body.styleComponent.NextCombatBackupStyle = null;
@@ -218,4 +218,3 @@ namespace DOL.AI.Brain
         }
 	}
 }
-

GameServer/commands/gmcommands/GMinfo.cs
@@ -216,14 +216,6 @@ namespace DOL.GS.Commands
 					info.Add("LastCombatPVP: " + target.LastCombatTickPvP);
 					info.Add("AttackAction: " + target.attackComponent.attackAction);
 					info.Add("WeaponAction: " + target.attackComponent.weaponAction);
-
-					if (target.InCombat || target.attackComponent.AttackState)
-					{
-						info.Add("RegionTick: " + GameLoop.GameLoopTime);
-						info.Add("AttackAction NextTick " + target.attackComponent.attackAction.NextTick);
-						info.Add("AttackAction TimeUntilStart " + (target.attackComponent.attackAction.NextTick - GameLoop.GameLoopTime));
-					}
-
 					info.Add("");
 
 					if (target.TargetObject != null)

commit 6b986f8c34811dafdd948107aa21b7cfe864c4d2

`AttackAction`: Fix regression related to `BlockRoundHandler` Caused by f88ce5f8bda7c42ee54cdc3dce924604730a6463 WeaponAction now holds AttackRoundEndTime, and AttackAction passes an adjusted interval to take into account the fact that it can tick a bit early.

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -28,10 +28,6 @@ namespace DOL.GS
         // Used to prevent combat log spam and kept until reset by AttackComponent.SendAttackingCombatMessages.
         public long RoundWithNoAttackTime { get; set; }
 
-        // When the current melee attack round is scheduled to end.
-        public long AttackRoundEndTime { get; private set; }
-        public bool IsAttackRoundFinished => GameServiceUtils.ShouldTick(AttackRoundEndTime);
-
         public AttackData LastAttackData { get; set; }
         protected AttackComponent AttackComponent => _owner.attackComponent;
         protected StyleComponent StyleComponent => _owner.styleComponent;
@@ -350,13 +346,31 @@ namespace DOL.GS
 
         protected virtual void PerformMeleeAttack()
         {
-            AttackComponent.weaponAction = new WeaponAction(_owner, _target, _weapon, _leftWeapon, _effectiveness, _attackInterval, _combatStyle);
+            // AttackAction ticks in a way that prevent drifting,
+            // but we must pass the real interval to WeaponAction to ensure correct interrupt / block round durations.
+            AttackComponent.weaponAction = new(
+                _owner,
+                _target,
+                _weapon,
+                _leftWeapon,
+                _effectiveness,
+                (int) (_nextMeleeTick - GameLoop.GameLoopTime + _attackInterval),
+                _combatStyle);
             AttackComponent.weaponAction.Execute();
         }
 
         protected virtual void PerformRangedAttack()
         {
-            AttackComponent.weaponAction = new WeaponAction(_owner, _target, _weapon, _effectiveness, _attackInterval, _owner.rangeAttackComponent.RangedAttackType, _owner.rangeAttackComponent.Ammo);
+            // AttackAction ticks in a way that prevent drifting,
+            // but we must pass the real interval to WeaponAction to ensure correct interrupt / block round durations.
+            AttackComponent.weaponAction = new(
+                _owner,
+                _target,
+                _weapon,
+                _effectiveness,
+                (int) (_nextRangedTick - GameLoop.GameLoopTime + _attackInterval),
+                _owner.rangeAttackComponent.RangedAttackType,
+                _owner.rangeAttackComponent.Ammo);
 
             if (_owner.rangeAttackComponent.RangedAttackType is eRangedAttackType.Critical)
                 _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
@@ -401,7 +415,6 @@ namespace DOL.GS
             }
 
             _interval = AttackComponent.AttackSpeed(_weapon, _leftWeapon);
-            AttackRoundEndTime = _nextMeleeTick + _interval;
             return true;
         }
 

GameServer/ECS-Components/Actions/WeaponAction.cs
@@ -21,11 +21,13 @@ namespace DOL.GS
         private int _leftHandSwingCount;
         private bool _isDualWieldAttack; // Not necessarily true even if _leftHandSwingCount is > 0, for example H2H isn't technically dual wield.
 
+        public long AttackRoundEndTime { get; private set; }
+        public bool IsAttackRoundFinished => GameServiceUtils.ShouldTick(AttackRoundEndTime);
+
         // The active weapon slot, ranged attack type, and ammo at the time the ammo was released
         public eActiveWeaponSlot ActiveWeaponSlot { get; }
         public eRangedAttackType RangedAttackType { get; }
         public DbInventoryItem Ammo { get; }
-
         public bool HasAmmoReachedTarget { get; private set; } // Used to not cancel the release animation. A bit clunky, may not work perfectly.
 
         public WeaponAction(GameLiving owner, GameObject target, DbInventoryItem attackWeapon, DbInventoryItem leftWeapon, double effectiveness, int interval, Style combatStyle)
@@ -61,6 +63,8 @@ namespace DOL.GS
 
         public void Execute()
         {
+            AttackRoundEndTime = GameLoop.GameLoopTime + _interval;
+
             // 1.89
             //- Pets will no longer continue to attack a character after the character has stealthed.
             // 1.88

GameServer/ECS-Components/AttackComponent.cs
@@ -2439,10 +2439,7 @@ namespace DOL.GS
                     return false;
                 }
 
-                // Decrement the count after a duration equal to the attack interval.
-                // We need to make sure it ticks before the attacker's next attack.
-                // We can't do GameLoop.GameLoopTime + AttackData.Interval to calculate when the attack round ends, because AttackAction catches up on lost time caused by the game loop's resolution.
-                new BlockRoundCountDecrementTimer(_owner, Relinquish).Start((int) (attackData.Attacker.attackComponent.attackAction.AttackRoundEndTime - GameLoop.GameLoopTime));
+                new BlockRoundCountDecrementTimer(_owner, Relinquish).Start(attackData.Interval);
                 return true;
             }
 

GameServer/ECS-Components/RangeAttackComponent.cs
@@ -143,7 +143,7 @@ namespace DOL.GS
                              * delays) than against fast piercing/thrusting weapon wielders.
                              */
 
-                            if (!living.attackComponent.attackAction.IsAttackRoundFinished ||
+                            if (living.attackComponent.weaponAction?.IsAttackRoundFinished == false ||
                                 living.CurrentSpeed > 90 ||
                                 living.effectListComponent.ContainsEffectForEffectType(eEffect.Mez))
                             {

GameServer/ai/brain/Necromancer/NecromancerPetBrain.cs
@@ -113,7 +113,7 @@ namespace DOL.AI.Brain
         {
             // Only start casting if the pet has finished his attack round.
             // This will be false most of the time, unless called from the attack component directly.
-            if (!Body.attackComponent.attackAction.IsAttackRoundFinished)
+            if (Body.attackComponent.weaponAction?.IsAttackRoundFinished == false)
             {
                 MessageToOwner(LanguageMgr.GetTranslation((Owner as GamePlayer).Client.Account.Language, "AI.Brain.Necromancer.CastSpellAfterAction", Body.Name), eChatType.CT_System, Owner as GamePlayer);
                 return false;

GameServer/scripts/namedmobs/Albion Named Mobs/Classic Zones/WrathOfMordred.cs
@@ -175,9 +175,9 @@ namespace DOL.AI.Brain
 					}
 					if (!living.effectListComponent.ContainsEffectForEffectType(eEffect.Stun))
 					{
-						AttackAction attackAction = Body.attackComponent.attackAction;
+						WeaponAction weaponAction = Body.attackComponent.weaponAction;
 
-						if (attackAction.AttackRoundEndTime - GameLoop.GameLoopTime <= 800 && CanWalk == false)
+						if (weaponAction != null && weaponAction.AttackRoundEndTime - GameLoop.GameLoopTime <= 800 && CanWalk == false)
 						{
 							Body.styleComponent.NextCombatStyle = null;
 							Body.styleComponent.NextCombatBackupStyle = null;