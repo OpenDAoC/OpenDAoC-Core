OpenDAoC/OpenDAoC-Core

commit 3326e0906b90ce661944afd24c1b3217051edb3a

`VaultBanker`, `AccountVaultKeeper`: Minor clean up

GameServer/scripts/customnpc/AccountVaultKeeper.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using DOL.Database;
 using DOL.GS.PacketHandler;
 
@@ -126,14 +125,5 @@ namespace DOL.GS
         {
             return $"{player.Client.Account.Name}_{player.Realm}";
         }
-
-        public override IEnumerable<DbInventoryItem> GetDbItems(GamePlayer player)
-        {
-            return GameServer.Database.SelectObjects<DbInventoryItem>(DB.Column("OwnerID").IsEqualTo(GetOwner(player)).And(DB.Column("SlotPosition").IsGreaterOrEqualTo(FirstDbSlot).And(DB.Column("SlotPosition").IsLessOrEqualTo(LastDbSlot))));
-        }
-
-        public override int FirstDbSlot => (int) eInventorySlot.AccountVault_First + VaultSize * _vaultIndex;
-
-        public override int LastDbSlot => FirstDbSlot + (VaultSize -1);
     }
 }

GameServer/gameobjects/CustomNPC/VaultBanker.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using DOL.Database;
 using DOL.GS.Housing;
 using DOL.GS.PacketHandler;
@@ -240,16 +239,10 @@ namespace DOL.GS
 
     public abstract class BankerVaultBase : GameHouseVault
     {
-        private string _vaultOwner;
-        private int _vaultIndex;
-
         protected BankerVaultBase(GamePlayer player, DbItemTemplate itemTemplate, int vaultIndex) : base(itemTemplate, vaultIndex)
         {
             if (vaultIndex is < 0)
                 throw new ArgumentOutOfRangeException(nameof(vaultIndex), $"{nameof(vaultIndex)} must not be negative.");
-
-            _vaultOwner = GetOwner(player);
-            _vaultIndex = vaultIndex;
 
             DbHouse dbHouse = new()
             {
@@ -257,7 +250,7 @@ namespace DOL.GS
                 GuildHouse = false,
                 HouseNumber = player.ObjectID,
                 Name = "Vault",
-                OwnerID = _vaultOwner,
+                OwnerID = GetOwner(player),
                 RegionID = player.CurrentRegionID
             };
 
@@ -268,15 +261,6 @@ namespace DOL.GS
         {
             return false;
         }
-
-        public override IEnumerable<DbInventoryItem> GetDbItems(GamePlayer player)
-        {
-            return GameServer.Database.SelectObjects<DbInventoryItem>(DB.Column("OwnerID").IsEqualTo(GetOwner(player)).And(DB.Column("SlotPosition").IsGreaterOrEqualTo(FirstDbSlot).And(DB.Column("SlotPosition").IsLessOrEqualTo(LastDbSlot))));
-        }
-
-        public override int FirstDbSlot => (int) eInventorySlot.HouseVault_First + VaultSize * _vaultIndex;
-
-        public override int LastDbSlot => FirstDbSlot + (VaultSize -1);
     }
 
     public class BankerPersonalVault : BankerVaultBase

GameServer/gameobjects/GameVault.cs
@@ -52,7 +52,7 @@ namespace DOL.GS
         /// <summary>
         /// Last slot in the DB.
         /// </summary>
-        public virtual int LastDbSlot => (int) eInventorySlot.HouseVault_First + VaultSize * (Index + 1) - 1;
+        public virtual int LastDbSlot => FirstDbSlot + (VaultSize - 1);
 
         private readonly Lock _lock = new();
         public Lock Lock => _lock;

commit 6bde6d02f8c5a6f6a09a3236ba8a113478e0634c

`AccountVaultKeeper`: Fix regression caused by 3326e0906b90ce661944afd24c1b3217051edb3a Incorrect slot

GameServer/scripts/customnpc/AccountVaultKeeper.cs
@@ -83,7 +83,6 @@ namespace DOL.GS
     public class AccountVault : GameHouseVault
     {
         private string _vaultOwner;
-        private int _vaultIndex;
 
         public AccountVault(GamePlayer player, int vaultIndex, DbItemTemplate dummyTemplate) : base(dummyTemplate, vaultIndex)
         {
@@ -91,7 +90,6 @@ namespace DOL.GS
                 throw new ArgumentOutOfRangeException(nameof(vaultIndex), $"{nameof(vaultIndex)} must be either 0 or 1.");
 
             _vaultOwner = GetOwner(player);
-            _vaultIndex = vaultIndex;
 
             DbHouse dbHouse = new()
             {
@@ -125,5 +123,9 @@ namespace DOL.GS
         {
             return $"{player.Client.Account.Name}_{player.Realm}";
         }
+
+        public override int FirstDbSlot => (int) eInventorySlot.AccountVault_First + VaultSize * Index;
+
+        public override int LastDbSlot => FirstDbSlot + (VaultSize -1);
     }
 }