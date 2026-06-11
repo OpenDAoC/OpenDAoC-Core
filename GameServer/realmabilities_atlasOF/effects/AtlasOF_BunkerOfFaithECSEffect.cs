06b6c6fc5eca8c5fb834c86ef1697b715433e430
Baptiste Marie <bm01@users.noreply.github.com>
2026-06-11 15:20:50 +0200

    Fix Bunker of Faith's icon

--- GameServer/realmabilities_atlasOF/effects/AtlasOF_BunkerOfFaithECSEffect.cs
+++ GameServer/realmabilities_atlasOF/effects/AtlasOF_BunkerOfFaithECSEffect.cs
@@ -2,7 +2,6 @@ namespace DOL.GS.Effects
 {
     public class BunkerOfFaithECSEffect : StatBuffECSEffect
     {
-        public override ushort Icon => 4242;
         public override string Name => "Bunker of Faith";
         public override bool HasPositiveEffect => true;
 
