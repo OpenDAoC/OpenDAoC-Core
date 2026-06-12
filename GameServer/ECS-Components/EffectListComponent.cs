OpenDAoC/OpenDAoC-Core

commit 14bcb2b7a756b31bc582a451812c26a1ed202ceb

`PlayerEffectListComponent`: Restore "changed effects" optimization IPacketLib.SendUpdateIcons is meant to take a list of changed effects, so that it doesn't have to send the whole list every time an effect is added or removed. * Added LastClientIndex property to ECSGameEffect. * Rewrote the way PacketLib1110.SendUpdateIcons handles the list of changed effects because I couldn't make the previous one work. * Changed EffectListComponent's queues to List<T>, and PendingEffect's visibility.

GameServer/ECS-Components/EffectListComponent.cs
@@ -31,8 +31,8 @@ namespace DOL.GS
 
         // Active and pending effects.
         private readonly Dictionary<eEffect, List<ECSGameEffect>> _effects = new();  // Dictionary of effects by their type.
-        private Queue<PendingEffect> _pendingEffects = new();                        // Queue for effects to be started, stopped.
-        private Queue<PendingEffect> _effectsToProcess = new();                      // Temporary queue for processing pending effects.
+        private List<PendingEffect> _pendingEffects = new();                         // Queue for effects to be started, stopped.
+        protected List<PendingEffect> _effectsToProcess = new();                     // Temporary queue for processing pending effects.
 
         // Concentration.
         private readonly List<ECSGameSpellEffect> _concentrationEffects = new(20);   // List of concentration effects currently active on the player.
@@ -60,7 +60,7 @@ namespace DOL.GS
                 return new EffectListComponent(living);
         }
 
-        public virtual void BeginTick()
+        public void BeginTick()
         {
             // This can become an infinite loop if effects add pending effects recursively during processing.
             // It's however important for immunity effects to be started immediately and not leave a gap.
@@ -78,10 +78,12 @@ namespace DOL.GS
 
                     // Process effects outside of the lock for safety. Some effects may try to acquire locks on other components.
                     // Currently the case for Guard, Intercept, and Protect.
-                    while (_effectsToProcess.TryDequeue(out PendingEffect pendingEffect))
-                        pendingEffect.Process();
+                    foreach (PendingEffect effectToProcess in _effectsToProcess)
+                        effectToProcess.Process();
                 } while (true);
             }
+
+            OnEffectsProcessed();
 
             // At this point, new effects can still be enqueued by other components, but they'll be processed on the next tick.
         }
@@ -492,6 +494,11 @@ namespace DOL.GS
             }
         }
 
+        protected virtual void OnEffectsProcessed()
+        {
+            _effectsToProcess.Clear();
+        }
+
         protected virtual void MapTooltipIdToEffect(ECSGameEffect effect)
         {
             // Only used by players.
@@ -517,7 +524,7 @@ namespace DOL.GS
                 {
                     bool start = effect.FinalizeState(result);
 
-                    component._pendingEffects.Enqueue(new(effect, static (effect, start) =>
+                    component._pendingEffects.Add(new(effect, static (effect, start) =>
                     {
                         try
                         {
@@ -572,7 +579,7 @@ namespace DOL.GS
             {
                 try
                 {
-                    component._pendingEffects.Enqueue(new(effect, static (effect, _) =>
+                    component._pendingEffects.Add(new(effect, static (effect, _) =>
                     {
                         try
                         {
@@ -907,7 +914,7 @@ namespace DOL.GS
                 {
                     bool stop = effect.FinalizeState(result);
 
-                    component._pendingEffects.Enqueue(new(effect, static (effect, stop) =>
+                    component._pendingEffects.Add(new(effect, static (effect, stop) =>
                     {
                         try
                         {
@@ -1028,7 +1035,7 @@ namespace DOL.GS
             Failed
         }
 
-        private readonly record struct PendingEffect(ECSGameEffect Effect, Action<ECSGameEffect, bool> Action, bool State)
+        protected readonly record struct PendingEffect(ECSGameEffect Effect, Action<ECSGameEffect, bool> Action, bool State)
         {
             public void Process()
             {

GameServer/ECS-Components/PlayerEffectListComponent.cs
@@ -17,12 +17,6 @@ namespace DOL.GS
             _owner = owner;
         }
 
-        public override void BeginTick()
-        {
-            base.BeginTick();
-            SendPlayerUpdates();
-        }
-
         public override void RequestPlayerUpdate(EffectHelper.PlayerUpdate playerUpdate)
         {
             lock (_playerUpdatesLock)
@@ -42,6 +36,12 @@ namespace DOL.GS
             }
 
             return effect;
+        }
+
+        protected override void OnEffectsProcessed()
+        {
+            SendPlayerUpdates(); // Accesses _effectsToProcess.
+            base.OnEffectsProcessed(); // Clears _effectsToProcess.
         }
 
         protected override void MapTooltipIdToEffect(ECSGameEffect effect)
@@ -75,7 +75,7 @@ namespace DOL.GS
             if ((requestedUpdates & EffectHelper.PlayerUpdate.Icons) != 0)
             {
                 _owner.Group?.UpdateMember(_owner, true, false);
-                _owner.Out.SendUpdateIcons(GetSortedEffects(), ref _lastUpdateEffectsCount);
+                _owner.Out.SendUpdateIcons(_effectsToProcess, ref _lastUpdateEffectsCount);
             }
 
             if ((requestedUpdates & EffectHelper.PlayerUpdate.Status) != 0)

GameServer/ECS-Effects/ECSGameEffect.cs
@@ -35,6 +35,7 @@ namespace DOL.GS
         public bool TriggersImmunity { get; set; }
         public int ImmunityDuration { get; protected set; } = 60000;
         public bool IsBeingReplaced { get; set; } // Used externally to force an effect to be silent (no message, no immunity) when being refreshed.
+        public int LastClientIndex { get; set; } = -1; // Used eternally for effect list updates.
         public ServiceObjectId ServiceObjectId { get; } = new(ServiceObjectType.Effect);
 
         // State properties.

GameServer/ai/brain/Necromancer/NecromancerPetBrain.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using DOL.Events;
 using DOL.GS;
 using DOL.GS.PacketHandler;

GameServer/packets/Server/PacketLib1110.cs
@@ -68,10 +68,22 @@ namespace DOL.GS.PacketHandler
 						continue;
 
 					fxcount++;
-					if (changedEffects != null && !changedEffects.Contains(effect))
+					int currentClientIndex = fxcount - 1;
+					bool needsUpdate = false;
+
+					// Did the effect's data change?
+					if (changedEffects != null && changedEffects.Contains(effect))
+						needsUpdate = true;
+
+					// Did the effect's position shift because an earlier effect was removed?
+					if (effect.LastClientIndex != currentClientIndex)
 					{
-						continue;
+						needsUpdate = true;
+						effect.LastClientIndex = currentClientIndex;
 					}
+
+					if (!needsUpdate)
+						continue;
 
 					// store tooltip update for gamespelleffect.
 					if (ForceTooltipUpdate && effect is ECSGameSpellEffect gameEffect)

commit 3330828a8be6399391b0bc1905ef98ef8ff9c4ea

`EffectListComponent`: Fix possible infinite loop Caused by 14bcb2b7a756b31bc582a451812c26a1ed202ceb

GameServer/ECS-Components/EffectListComponent.cs
@@ -80,10 +80,10 @@ namespace DOL.GS
                     // Currently the case for Guard, Intercept, and Protect.
                     foreach (PendingEffect effectToProcess in _effectsToProcess)
                         effectToProcess.Process();
+
+                    OnEffectsProcessed();
                 } while (true);
             }
-
-            OnEffectsProcessed();
 
             // At this point, new effects can still be enqueued by other components, but they'll be processed on the next tick.
         }