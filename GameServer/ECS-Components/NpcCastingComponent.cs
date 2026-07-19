OpenDAoC/OpenDAoC-Core

commit 678b8f16faaf2ba218d04273d9032cc5c90c107f

Allow immobile / guard caster NPCs to change target on LoS check fail against their current one Mimics how archer NPCs work. This doesn't stop their casting animation and isn't related to whether the spell lands or not. Instead, the current target is forgotten and queued spell handler cleared if we lose LoS on a new spell cast. Removed GameNPC.OnCastSpellLosCheckFail and updated TurretPet and NecromancerPet accordingly.

GameServer/ECS-Components/NpcCastingComponent.cs
@@ -2,6 +2,8 @@
 using System.Threading;
 using DOL.AI.Brain;
 using DOL.GS.Keeps;
+using DOL.GS.PacketHandler;
+using DOL.Language;
 
 namespace DOL.GS
 {
@@ -86,6 +88,18 @@ namespace DOL.GS
             base.ClearSpellHandlers();
         }
 
+        public override void OnOutOfRangeOrNoLos(GameObject target)
+        {
+            if (QueuedSpellHandler?.Target == target)
+                ClearUpQueuedSpellHandler();
+
+            // Immobile NPCs and caster guards forget about the target, other NPCs will try to move into range and line of sight.
+            if (IsCasterGuardOrImmobile)
+                (_npcOwner.Brain as StandardMobBrain)?.RemoveFromAggroList(target as GameLiving);
+            else if (_npcOwner.TargetObject == target)
+                _npcOwner.Follow(target, _npcOwner.StickMinimumRange, _npcOwner.StickMaximumRange);
+        }
+
         public bool IsAllowedToFollow(GameObject target)
         {
             if (!IsCasterGuardOrImmobile)
@@ -109,20 +123,28 @@ namespace DOL.GS
                 if (!_spellsWaitingForLosCheck.TryGetValue(target, out var list))
                     return;
 
-                bool success = response is LosCheckResponse.True;
-
-                foreach (SpellWaitingForLosCheck spellWaitingForLosCheck in list)
+                if (response is LosCheckResponse.True)
                 {
-                    Spell spell = spellWaitingForLosCheck.Spell;
-                    SpellLine spellLine = spellWaitingForLosCheck.SpellLine;
+                    foreach (SpellWaitingForLosCheck spellWaitingForLosCheck in list)
+                    {
+                        Spell spell = spellWaitingForLosCheck.Spell;
+                        SpellLine spellLine = spellWaitingForLosCheck.SpellLine;
 
-                    if (success && spellLine != null && spell != null)
-                        base.RequestCastSpellInternal(spell, spellLine, null, target as GameLiving, losChecker);
-                    else
-                        _npcOwner.OnCastSpellLosCheckFail(target);
+                        if (spellLine != null && spell != null)
+                            base.RequestCastSpellInternal(spell, spellLine, null, target as GameLiving, losChecker);
+                    }
+
+                    return;
                 }
 
                 list.Clear();
+                OnOutOfRangeOrNoLos(target);
+
+                if (_npcOwner is NecromancerPet necromancerPet && necromancerPet.Owner is GamePlayer playerOwner)
+                {
+                    string message = LanguageMgr.GetTranslation(playerOwner.Client.Account.Language, "AI.Brain.Necromancer.PetCantSeeTarget", _npcOwner.Name);
+                    NecromancerPetBrain.MessageToOwner(message, eChatType.CT_SpellResisted, playerOwner);
+                }
             }
         }
 

GameServer/ECS-Components/CastingComponent.cs
@@ -197,6 +197,8 @@ namespace DOL.GS
             SpellHandler = null;
         }
 
+        public virtual void OnOutOfRangeOrNoLos(GameObject target) { }
+
         public void ClearUpQueuedSpellHandler()
         {
             QueuedSpellHandler = null;

GameServer/ai/brain/Necromancer/NecromancerPetBrain.cs
@@ -83,11 +83,6 @@ namespace DOL.AI.Brain
                             "AI.Brain.Necromancer.ServantFarAwayToCast"), eChatType.CT_SpellResisted, Owner as GamePlayer);
                         break;
 
-                    case CastFailedEventArgs.Reasons.TargetNotInView:
-                        MessageToOwner(LanguageMgr.GetTranslation((Owner as GamePlayer).Client.Account.Language, 
-                            "AI.Brain.Necromancer.PetCantSeeTarget", Body.Name), eChatType.CT_SpellResisted, Owner as GamePlayer);
-                        break;
-
                     case CastFailedEventArgs.Reasons.NotEnoughPower:
                         RemoveSpellFromQueue();
                         MessageToOwner(LanguageMgr.GetTranslation((Owner as GamePlayer).Client.Account.Language,

GameServer/gameobjects/Animist/TurretPet.cs
@@ -45,15 +45,5 @@ namespace DOL.GS
             // Don't interrupt turrets (1.90 EU).
             return;
         }
-
-        public override void OnCastSpellLosCheckFail(GameObject target)
-        {
-            base.OnCastSpellLosCheckFail(target);
-
-            // This is where FnF turrets with LoS check on aggro enabled clear their list of hidden targets.
-            // This can create a delay between attacks in some cases because turrets only ask one target at a time.
-            if (Brain is TurretFNFBrain fnfBrain)
-                fnfBrain.RemoveFromAggroList(target as GameLiving);
-        }
     }
 }

GameServer/gameobjects/GameNPC.cs
@@ -3356,17 +3356,6 @@ namespace DOL.GS
 			return base.CastSpell(spell, line, null, checkLos);
 		}
 
-		public virtual void OnCastSpellLosCheckFail(GameObject target)
-		{
-			// In case the NPC changes target while casting on the current one and the first LoS check was positive.
-			if (castingComponent.QueuedSpellHandler?.Target == target)
-				castingComponent.ClearUpQueuedSpellHandler();
-
-			// Start following the target if there is no LoS.
-			if (TargetObject == target)
-				Follow(target, StickMinimumRange, StickMaximumRange);
-		}
-
 		#endregion
 
 		#region Styles

GameServer/gameobjects/Necromancer/NecromancerPet.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using DOL.AI.Brain;
 using DOL.Database;
-using DOL.Events;
 using DOL.GS.Effects;
 using DOL.GS.PacketHandler;
 using DOL.GS.ServerProperties;
@@ -288,12 +287,6 @@ namespace DOL.GS
 
             if (tauntSpell != null && GetSkillDisabledDuration(tauntSpell) == 0)
                 CastSpell(tauntSpell, chantsLine);
-        }
-
-        public override void OnCastSpellLosCheckFail(GameObject target)
-        {
-            base.OnCastSpellLosCheckFail(target);
-            Notify(GameLivingEvent.CastFailed, this, new CastFailedEventArgs(null, CastFailedEventArgs.Reasons.TargetNotInView));
         }
 
         // Necromancer pets shouldn't delay their instant spells.

GameServer/spells/SpellHandler.cs
@@ -898,6 +898,7 @@ namespace DOL.GS.Spells
 						if (verbose)
 							MessageToCaster("That target is too far away!", eChatType.CT_SpellResisted);
 
+						m_caster.castingComponent.OnOutOfRangeOrNoLos(target);
 						return false;
 					}
 				}

commit d390e54edc7bb3a8c057a73fa9101a70161abeba

`NpcCastingComponent`: Fix _spellsWaitingForLosCheck's lists not being cleared Regression caused by 678b8f16faaf2ba218d04273d9032cc5c90c107f

GameServer/ECS-Components/NpcCastingComponent.cs
@@ -106,18 +106,19 @@ namespace DOL.GS
                         if (spellLine != null && spell != null)
                             base.RequestCastSpellInternal(spell, spellLine, null, target as GameLiving, losChecker);
                     }
+                }
+                else
+                {
+                    OnOutOfRangeOrNoLos(target);
 
-                    return;
+                    if (_npcOwner is NecromancerPet necromancerPet && necromancerPet.Owner is GamePlayer playerOwner)
+                    {
+                        string message = LanguageMgr.GetTranslation(playerOwner.Client.Account.Language, "AI.Brain.Necromancer.PetCantSeeTarget", _npcOwner.Name);
+                        NecromancerPetBrain.MessageToOwner(message, eChatType.CT_SpellResisted, playerOwner);
+                    }
                 }
 
                 list.Clear();
-                OnOutOfRangeOrNoLos(target);
-
-                if (_npcOwner is NecromancerPet necromancerPet && necromancerPet.Owner is GamePlayer playerOwner)
-                {
-                    string message = LanguageMgr.GetTranslation(playerOwner.Client.Account.Language, "AI.Brain.Necromancer.PetCantSeeTarget", _npcOwner.Name);
-                    NecromancerPetBrain.MessageToOwner(message, eChatType.CT_SpellResisted, playerOwner);
-                }
             }
         }
 