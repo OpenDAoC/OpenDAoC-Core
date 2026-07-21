OpenDAoC/OpenDAoC-Core

commit 21eda0f903a5e3d46d6d6a5a622e6ffd7ec6e3de

`SpellHandler`: Implement live-like spell interrupt * Added two server properties: * * HARD_INTERRUPT_ON_ATTACKED: Replace the interrupt mechanic with the one we had until now. * * SPELL_SELF_INTERRUPT_DURATION: How long should the self interrupt initiated when attempting to cast during an interrupt timer last. This happens at the mid cast point. * Renamed CasterIsAttacked to TryInterruptCaster. Now private. * Added PerformDuringCastInterruptCheck and PerformOnAttackedInterruptCheck to support both mechanics.

GameServer/spells/SpellHandler.cs
@@ -338,7 +338,44 @@ namespace DOL.GS.Spells
 			}
 		}
 
-		public virtual bool CasterIsAttacked(GameLiving attacker)
+		private bool _halfwayCastChecked;
+
+		private bool HasPassedHalfCastTime => _castStartTick + _calculatedCastTime * 0.5 <= GameLoop.GameLoopTime;
+
+		public bool PerformOnAttackedInterruptCheck(GameLiving attacker)
+		{
+			if (!Properties.HARD_INTERRUPT_ON_ATTACKED)
+				return false;
+
+			if (CastState is not eCastState.Focusing)
+			{
+				if (!IsInCastingPhase || HasPassedHalfCastTime)
+					return false;
+			}
+
+			return TryInterruptCaster(attacker);
+		}
+
+		private bool PerformDuringCastInterruptCheck(GameLiving attacker)
+		{
+			// If we reach the half cast time while an interrupt timer is running, initiate a self interrupt.
+
+			if (Properties.HARD_INTERRUPT_ON_ATTACKED)
+				return false;
+
+			if (_halfwayCastChecked || !HasPassedHalfCastTime)
+				return false;
+
+			_halfwayCastChecked = true;
+
+			if (!Caster.IsInterrupted || !TryInterruptCaster(attacker))
+				return false;
+
+			Caster.StartInterruptTimer(Caster.SpellSelfInterruptDuration, AttackData.eAttackType.Spell, Caster);
+			return true;
+		}
+
+		protected virtual bool TryInterruptCaster(GameLiving attacker)
 		{
 			if (Spell.Uninterruptible)
 				return false;
@@ -352,16 +389,11 @@ namespace DOL.GS.Spells
 					return false;
 			}
 
-			if (Caster.effectListComponent.ContainsEffectForEffectType(eEffect.MasteryOfConcentration)
-				|| Caster.effectListComponent.ContainsEffectForEffectType(eEffect.FacilitatePainworking)
-				|| IsQuickCasting)
-				return false;
-
-			if (CastState is not eCastState.Focusing)
+			if (Caster.effectListComponent.ContainsEffectForEffectType(eEffect.MasteryOfConcentration) ||
+				Caster.effectListComponent.ContainsEffectForEffectType(eEffect.FacilitatePainworking) ||
+				IsQuickCasting)
 			{
-				// Only interrupt if we're under 50% of the way through the cast.
-				if (!IsInCastingPhase || GameLoop.GameLoopTime >= _castStartTick + _calculatedCastTime * 0.5)
-					return false;
+				return false;
 			}
 
 			if (Caster is GameSummonedPet petCaster && petCaster.Owner is GamePlayer casterOwner)
@@ -995,7 +1027,7 @@ namespace DOL.GS.Spells
 
 		public virtual bool CheckDuringCast(GameLiving target, bool quiet)
 		{
-			if (m_interrupted)
+			if (PerformDuringCastInterruptCheck(Caster.LastInterrupter))
 				return false;
 
 			bool checkLos = false;

GameServer/gameobjects/GameLiving.cs
@@ -715,10 +715,10 @@ namespace DOL.GS
 					return;
 			}
 
-			// Perform the actual interrupt.
-			if (castingComponent.SpellHandler?.CasterIsAttacked(attacker) == true)
+			if (castingComponent.SpellHandler?.PerformOnAttackedInterruptCheck(attacker) == true)
 				return;
-			else if (ActiveWeaponSlot is eActiveWeaponSlot.Distance)
+
+			if (ActiveWeaponSlot is eActiveWeaponSlot.Distance)
 			{
 				if (attackComponent.AttackState)
 					CheckRangedAttackInterrupt(attacker, attackType);
@@ -734,16 +734,23 @@ namespace DOL.GS
 		private long _selfInterruptTime; // Represents a hard interrupt timer inflicted by self.
 
 		public GameLiving LastInterrupter { get; private set; }
-		public long InterruptRemainingDuration => Math.Max(0, Math.Max(_interruptTime, _selfInterruptTime) - GameLoop.GameLoopTime);
 		public virtual bool SelfInterruptsOnMeleeAttack => true;
 		public virtual bool IsBeingInterrupted => IsInterrupted || IsSelfInterrupted;
 		public bool IsInterrupted => _interruptTime > GameLoop.GameLoopTime;
 		public bool IsSelfInterrupted => _selfInterruptTime > GameLoop.GameLoopTime;
 
-		/// <summary>
-		/// How long does an interrupt last?
-		/// </summary>
-		public virtual int SpellInterruptDuration => Properties.SPELL_INTERRUPT_DURATION;
+		public long InterruptRemainingDuration
+		{
+			get
+			{
+				// If HARD_INTERRUPT_ON_ATTACKED is true, there is no distinction between _selfInterruptTime and _interruptTime.
+				long interruptTime = Properties.HARD_INTERRUPT_ON_ATTACKED ? Math.Max(_selfInterruptTime, _interruptTime) : _selfInterruptTime;
+				return Math.Max(0, interruptTime - GameLoop.GameLoopTime);
+			}
+		}
+
+		public int SpellInterruptDuration => Properties.SPELL_INTERRUPT_DURATION;
+		public int SpellSelfInterruptDuration => Properties.SPELL_SELF_INTERRUPT_DURATION;
 
 		protected virtual bool CheckRangedAttackInterrupt(GameLiving attacker, eAttackType attackType)
 		{

GameServer/serverproperty/ServerProperties.cs
@@ -2212,11 +2212,14 @@ namespace DOL.GS.ServerProperties
 
 		#region SPELLS
 
-		/// <summary>
-		/// Spells-related properties
-		/// </summary>
-		[ServerProperty("spells", "spell_interrupt_duration", "", 3000)]
+		[ServerProperty("spells", "hard_interrupt_on_attacked", "Should the interrupt mechanic be replaced with immediate interrupts", false)]
+		public static bool HARD_INTERRUPT_ON_ATTACKED;
+
+		[ServerProperty("spells", "spell_interrupt_duration", "How long does an interrupt last", 3000)]
 		public static int SPELL_INTERRUPT_DURATION;
+
+		[ServerProperty("spells", "spell_self_interrupt_duration", "How long does a self interrupt lasts", 2200)]
+		public static int SPELL_SELF_INTERRUPT_DURATION;
 
 		[ServerProperty("spells", "spell_charm_named_check", "Prevents charm spell to work on Named Mobs, 0 = disable, 1 = enable", 1)]
 		public static int SPELL_CHARM_NAMED_CHECK;

GameServer/spells/ISpellHandler.cs
@@ -30,14 +30,6 @@ namespace DOL.GS.Spells
 		void CasterMoves();
 
 		/// <summary>
-		/// Has to be called when the caster is attacked by enemy
-		/// for interrupt checks
-		/// <param name="attacker">attacker that interrupts the cast sequence</param>
-		/// <returns>true if casting was interrupted</returns>
-		/// </summary>
-		bool CasterIsAttacked(GameLiving attacker);
-
-		/// <summary>
 		/// Returns true when spell is in casting phase
 		/// </summary>
 		bool IsInCastingPhase { get; }

GameServer/spells/RampingDamageFocus.cs
@@ -55,7 +55,7 @@ namespace DOL.GS.Spells
 			return new GameSpellEffect(this, CalculateEffectDuration(target), 0, effectiveness);
 		}
 
-		public override bool CasterIsAttacked(GameLiving attacker)
+		protected override bool TryInterruptCaster(GameLiving attacker)
 		{
 			if (Spell.Uninterruptible && Caster.GetDistanceTo(attacker) > 200)
 				return false;

GameServer/spells/SiegeDirectDamageSpellHandler.cs
@@ -22,7 +22,7 @@ namespace DOL.GS.Spells
             return 100;
         }
 
-        public override bool CasterIsAttacked(GameLiving attacker)
+        protected override bool TryInterruptCaster(GameLiving attacker)
         {
             return false;
         }

GameServer/spells/Warlock/UninterruptableSpellHandler.cs
@@ -47,10 +47,11 @@ namespace DOL.GS.Spells
             base.FinishSpellCast(target);
         }
 
-		public override bool CasterIsAttacked(GameLiving attacker)
-		{
-			return false;
-		}
+        protected override bool TryInterruptCaster(GameLiving attacker)
+        {
+            return false;
+        }
+
         #region Devle Info
         public override IList<string> DelveInfo
         {

commit 46d816439665eec6fc2fbcf34b356631eb1a7303

`SpellHandler`: Fix focus spell interrupts Regression caused by 21eda0f903a5e3d46d6d6a5a622e6ffd7ec6e3de

GameServer/spells/SpellHandler.cs
@@ -344,11 +344,11 @@ namespace DOL.GS.Spells
 
 		public bool PerformOnAttackedInterruptCheck(GameLiving attacker)
 		{
-			if (!Properties.HARD_INTERRUPT_ON_ATTACKED)
-				return false;
-
 			if (CastState is not eCastState.Focusing)
 			{
+				if (!Properties.HARD_INTERRUPT_ON_ATTACKED)
+					return false;
+
 				if (!IsInCastingPhase || HasPassedHalfCastTime)
 					return false;
 			}