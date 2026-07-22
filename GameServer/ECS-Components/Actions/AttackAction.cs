OpenDAoC/OpenDAoC-Core

commit 79bba259b4a9c95de63f6befc9c9aca247d89773

`AttackAction`: Implement live-like ranged attack interrupt Live mechanics (ignoring very minor differences): 1. Can always draw your bow, no hard interrupt. 2. Getting attacked starts an interrupt timer. If still active at the halfway draw time, interrupts the attack. 3. Getting attacked doesn't immediately interrupts unless the attack is held (past 100% the draw time).
* Replaed the whole GameLiving.CheckRangedAttackInterrupt and AttackAction.CheckInterruptTimer call chain. AttackAction now performs a halfway check in a similar fashion to SpellHandler. GameLiving.StartInterruptTimer now calls AttackAction.PerformOnAttackedInterruptCheck for immediate interrupt.
* Changed how SureShot is handled to prevent the interrupt timer from even starting. Unsure how this is supposed to work.
* Moved lord interrupt prevention to NpcAttackAction.

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -24,6 +24,7 @@ namespace DOL.GS
         private long _nextRangedTick;
         private bool _firstTick = true;
         private byte _styleChainStage; // Used to track the current stage of a style chain (0 = first style).
+        private bool _halfwayDrawChecked;
 
         public long RoundWithNoAttackTime { get; set; } // Used to prevent combat log spam and kept until reset by AttackComponent.SendAttackingCombatMessages.
 
@@ -98,7 +99,9 @@ namespace DOL.GS
             if (PrepareRangedAttack())
             {
                 PerformRangedAttack();
-                FinalizeRangedAttack();
+
+                if (FinalizeRangedAttack())
+                    PrepareRangedAttack(); // Immediately prepare the next attack if we can.
             }
 
             if (AttackComponent.AttackState)
@@ -129,6 +132,7 @@ namespace DOL.GS
 
             rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
             rangeAttackComponent.RangedAttackState = eRangedAttackState.None;
+            _halfwayDrawChecked = false;
         }
 
         public void OnHeadingUpdate()
@@ -144,20 +148,6 @@ namespace DOL.GS
             if (ad.Target != null && _owner.IsObjectInFront(ad.Target, 120) && _owner.IsWithinRadius(ad.Target, AttackComponent.AttackRange))
                 _firstTick = true;
         }
-
-        public virtual bool CheckInterruptTimer()
-        {
-            if (!_owner.IsInterrupted)
-                return false;
-
-            _owner.attackComponent.StopAttack();
-            OnAimInterrupt(_owner.LastInterrupter);
-            return true;
-        }
-
-        public virtual void OnAimInterrupt(GameLiving attacker) { }
-
-        public virtual void OnForcedWeaponSwitch() { }
 
         private bool ShouldTick()
         {
@@ -240,21 +230,45 @@ namespace DOL.GS
 
         protected virtual bool PrepareRangedAttack()
         {
-            int attackSpeed = _owner.attackComponent.AttackSpeed(_weapon);
+            int attackSpeed;
 
             if (_owner.rangeAttackComponent.RangedAttackState is eRangedAttackState.None)
             {
                 _owner.rangeAttackComponent.RangedAttackState = eRangedAttackState.Aim;
+                _owner.rangeAttackComponent.AttackStartTime = GameLoop.GameLoopTime;
+                _halfwayDrawChecked = false;
+
+                if (_owner.effectListComponent.ContainsEffectForEffectType(eEffect.SureShot))
+                    _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.SureShot;
+                else if (_owner.effectListComponent.ContainsEffectForEffectType(eEffect.RapidFire))
+                    _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.RapidFire;
+                else if (_owner.effectListComponent.ContainsEffectForEffectType(eEffect.TrueShot))
+                    _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.Long;
+                else
+                    _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
+
+                // Must be done after changing RangedAttackType to account for RapidFire.
+                attackSpeed = AttackComponent.AttackSpeed(_weapon);
 
                 if (_owner is not GamePlayer || !_owner.effectListComponent.ContainsEffectForEffectType(eEffect.Volley))
                 {
                     // The 'stance' parameter appears to be used to tell whether or not the animation should be held, and doesn't seem to be related to the weapon speed.
                     foreach (GamePlayer player in _owner.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
                         player.Out.SendCombatAnimation(_owner, null, (ushort) (_weapon != null ? _weapon.Model : 0), 0, player.Out.BowPrepare, 0x1A, 0x00, 0x00);
-
-                    _interval = attackSpeed;
                 }
 
+                _interval = attackSpeed / 2;
+                return false;
+            }
+
+            attackSpeed = _owner.attackComponent.AttackSpeed(_weapon);
+
+            if (!_halfwayDrawChecked)
+            {
+                if (PerformDuringDrawInterruptCheck())
+                    return false;
+
+                _interval = attackSpeed - attackSpeed / 2;
                 return false;
             }
 
@@ -434,28 +448,36 @@ namespace DOL.GS
 
         protected virtual bool FinalizeRangedAttack()
         {
-            if (CheckInterruptTimer())
+            _owner.rangeAttackComponent.RangedAttackState = eRangedAttackState.None;
+            _halfwayDrawChecked = false;
+            return true;
+        }
+
+        public virtual void OnForcedWeaponSwitch() { }
+
+        protected virtual void InterruptAim(GameLiving attacker) { }
+
+        public bool PerformOnAttackedInterruptCheck(GameLiving attacker)
+        {
+            // Classic mechanics interrupt immediately only if the target is holding (past 100% draw time).
+            if (_owner.rangeAttackComponent.RangedAttackState is not eRangedAttackState.ReadyToFire)
                 return false;
 
-            _owner.rangeAttackComponent.AttackStartTime = GameLoop.GameLoopTime;
-            _owner.rangeAttackComponent.RangedAttackState = eRangedAttackState.Aim;
+            InterruptAim(attacker);
+            return true;
+        }
 
-            if (_owner.effectListComponent.ContainsEffectForEffectType(eEffect.SureShot))
-                _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.SureShot;
-            else if (_owner.effectListComponent.ContainsEffectForEffectType(eEffect.RapidFire))
-                _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.RapidFire;
-            else if (_owner.effectListComponent.ContainsEffectForEffectType(eEffect.TrueShot))
-                _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.Long;
-            else
-                _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
+        private bool PerformDuringDrawInterruptCheck()
+        {
+            if (_halfwayDrawChecked)
+                return false;
 
-            // Must be done after changing `RangedAttackType`.
-            _interval = AttackComponent.AttackSpeed(_weapon);
+            _halfwayDrawChecked = true;
 
-            // The 'stance' parameter appears to be used to tell whether or not the animation should be held, and doesn't seem to be related to the weapon speed.
-            foreach (GamePlayer player in _owner.GetPlayersInRadius(WorldMgr.VISIBILITY_DISTANCE))
-                player.Out.SendCombatAnimation(_owner, null, (ushort) (_weapon != null ? _weapon.Model : 0), 0x00, player.Out.BowPrepare, 0x1A, 0x00, 0x00);
+            if (!_owner.IsInterrupted)
+                return false;
 
+            InterruptAim(_owner.LastInterrupter);
             return true;
         }
 
@@ -465,6 +487,7 @@ namespace DOL.GS
             _target = null;
             _firstTick = true;
             _styleChainStage = 0;
+            _halfwayDrawChecked = false;
         }
     }
 }

GameServer/ECS-Components/Actions/NpcAttackAction.cs
@@ -23,42 +23,6 @@ namespace DOL.GS
             _npcOwner = owner;
         }
 
-        public override void OnAimInterrupt(GameLiving attacker)
-        {
-            // Use the follow target or current target (maybe redundant) instead of the interrupter.
-            // We really don't want guards to move because a pet attacked them in melee.
-            GameObject target = _npcOwner.TargetObject ?? _npcOwner.FollowTarget;
-
-            if (target is not GameLiving livingFollowTarget)
-                return;
-
-            if (!_npcOwner.IsAllowedToFollow(livingFollowTarget))
-            {
-                _npcOwner.StopFollowing();
-                return;
-            }
-
-            SwitchToMeleeAndTick();
-        }
-
-        public override void OnForcedWeaponSwitch()
-        {
-            switch (_npcOwner.ActiveWeaponSlot)
-            {
-                case eActiveWeaponSlot.Standard:
-                case eActiveWeaponSlot.TwoHanded:
-                {
-                    _wasMeleeWeaponSwitchForced = true;
-                    break;
-                }
-                case eActiveWeaponSlot.Distance:
-                {
-                    _wasMeleeWeaponSwitchForced = false;
-                    break;
-                }
-            }
-        }
-
         protected override bool PrepareMeleeAttack()
         {
             // Check spells before attacking to allow spell casting opportunity.
@@ -187,6 +151,49 @@ namespace DOL.GS
             }
 
             return base.FinalizeRangedAttack();
+        }
+
+        public override void OnForcedWeaponSwitch()
+        {
+            switch (_npcOwner.ActiveWeaponSlot)
+            {
+                case eActiveWeaponSlot.Standard:
+                case eActiveWeaponSlot.TwoHanded:
+                {
+                    _wasMeleeWeaponSwitchForced = true;
+                    break;
+                }
+                case eActiveWeaponSlot.Distance:
+                {
+                    _wasMeleeWeaponSwitchForced = false;
+                    break;
+                }
+            }
+        }
+
+        protected override void InterruptAim(GameLiving attacker)
+        {
+            // Lords can only be interrupted by their own target, and in melee range.
+            if (_npcOwner is GuardLord &&
+                _npcOwner.MaxSpeedBase == 0 &&
+                (attacker != _npcOwner.TargetObject || !_npcOwner.IsWithinRadius(attacker, _npcOwner.MeleeAttackRange)))
+            {
+               return;
+            }
+
+            _npcOwner.StopAttack();
+            GameObject target = _npcOwner.TargetObject ?? _npcOwner.FollowTarget;
+
+            if (target is not GameLiving livingFollowTarget)
+                return;
+
+            if (!_npcOwner.IsAllowedToFollow(livingFollowTarget))
+            {
+                _npcOwner.StopFollowing();
+                return;
+            }
+
+            SwitchToMeleeAndTick();
         }
 
         public override void CleanUp()

GameServer/ECS-Components/Actions/PlayerAttackAction.cs
@@ -14,21 +14,6 @@ namespace DOL.GS
             _playerOwner = owner;
         }
 
-        public override void OnAimInterrupt(GameLiving attacker)
-        {
-            string attackTypeMsg;
-
-            if (_playerOwner.ActiveWeapon != null && _playerOwner.ActiveWeapon.Object_Type == (int)eObjectType.Thrown)
-                attackTypeMsg = LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.Attack.Type.Throw");
-            else
-                attackTypeMsg = LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.Attack.Type.Shot");
-
-            if (attacker is GameNPC npcAttacker)
-                _playerOwner.Out.SendMessage(LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.Attack.Interrupted", attacker.GetName(0, true, _playerOwner.Client.Account.Language, npcAttacker), attackTypeMsg), eChatType.CT_YouHit, eChatLoc.CL_SystemWindow);
-            else
-                _playerOwner.Out.SendMessage(LanguageMgr.GetTranslation(_playerOwner.Client.Account.Language, "GamePlayer.Attack.Interrupted", attacker.GetName(0, true), attackTypeMsg), eChatType.CT_YouHit, eChatLoc.CL_SystemWindow);
-        }
-
         protected override bool PrepareMeleeAttack()
         {
             _combatStyle = StyleComponent.GetStyleToUse();
@@ -112,6 +97,24 @@ namespace DOL.GS
             }
 
             return base.FinalizeRangedAttack();
+        }
+
+        protected override void InterruptAim(GameLiving attacker)
+        {
+            string language = _playerOwner.Client.Account.Language;
+            string attackTypeMsg;
+
+            if (_playerOwner.ActiveWeapon != null && (eObjectType) _playerOwner.ActiveWeapon.Object_Type is eObjectType.Thrown)
+                attackTypeMsg = LanguageMgr.GetTranslation(language, "GamePlayer.Attack.Type.Throw");
+            else
+                attackTypeMsg = LanguageMgr.GetTranslation(language, "GamePlayer.Attack.Type.Shot");
+
+            if (attacker is GameNPC npcAttacker)
+                _playerOwner.Out.SendMessage(LanguageMgr.GetTranslation(language, "GamePlayer.Attack.Interrupted", attacker.GetName(0, true, language, npcAttacker), attackTypeMsg), eChatType.CT_YouHit, eChatLoc.CL_SystemWindow);
+            else
+                _playerOwner.Out.SendMessage(LanguageMgr.GetTranslation(language, "GamePlayer.Attack.Interrupted", attacker.GetName(0, true), attackTypeMsg), eChatType.CT_YouHit, eChatLoc.CL_SystemWindow);
+
+            _playerOwner.attackComponent.StopAttack();
         }
     }
 }

GameServer/ECS-Components/AttackComponent.cs
@@ -703,12 +703,7 @@ namespace DOL.GS
                 owner.CancelEngageEffect();
 
             if (owner.ActiveWeaponSlot is eActiveWeaponSlot.Distance)
-            {
-                if (owner.rangeAttackComponent.RangedAttackState is not eRangedAttackState.Aim && attackAction.CheckInterruptTimer())
-                    return false;
-
                 owner.rangeAttackComponent.AttackStartTime = GameLoop.GameLoopTime;
-            }
 
             AttackState = true;
             return true;

GameServer/gameobjects/GameLiving.cs
@@ -693,6 +693,16 @@ namespace DOL.GS
 				return;
 			}
 
+			if (rangeAttackComponent.RangedAttackType is eRangedAttackType.SureShot)
+			{
+				if (attackType is not eAttackType.MeleeOneHand and
+					not eAttackType.MeleeTwoHand and
+					not eAttackType.MeleeDualWield)
+				{
+					return;
+				}
+			}
+
 			// 3% reduced interrupt chance per level difference.
 			if (!Util.Chance(100 + (attacker.EffectiveLevel - EffectiveLevel) * 3))
 				return;
@@ -721,7 +731,7 @@ namespace DOL.GS
 			if (ActiveWeaponSlot is eActiveWeaponSlot.Distance)
 			{
 				if (attackComponent.AttackState)
-					CheckRangedAttackInterrupt(attacker, attackType);
+					attackComponent.attackAction.PerformOnAttackedInterruptCheck(attacker);
 				else
 				{
 					AtlasOF_VolleyECSEffect volley = EffectListService.GetEffectOnTarget(this, eEffect.Volley) as AtlasOF_VolleyECSEffect;
@@ -751,31 +761,6 @@ namespace DOL.GS
 
 		public int SpellInterruptDuration => Properties.SPELL_INTERRUPT_DURATION;
 		public int SpellSelfInterruptDuration => Properties.SPELL_SELF_INTERRUPT_DURATION;
-
-		protected virtual bool CheckRangedAttackInterrupt(GameLiving attacker, eAttackType attackType)
-		{
-			if (rangeAttackComponent.RangedAttackType == eRangedAttackType.SureShot)
-			{
-				if (attackType is not eAttackType.MeleeOneHand
-					and not eAttackType.MeleeTwoHand
-					and not eAttackType.MeleeDualWield)
-					return false;
-			}
-
-			long rangeAttackHoldStart = rangeAttackComponent.AttackStartTime;
-
-			if (rangeAttackHoldStart > 0)
-			{
-				long elapsedTime = GameLoop.GameLoopTime - rangeAttackHoldStart;
-				long halfwayPoint = attackComponent.AttackSpeed(ActiveWeapon) / 2;
-				
-				if (rangeAttackComponent.RangedAttackState is not eRangedAttackState.ReadyToFire and not eRangedAttackState.None && elapsedTime > halfwayPoint)
-					return false;
-			}
-
-			attackComponent.StopAttack();
-			return true;
-		}
 
 		/// <summary>
 		/// Check if we can make a proc on a weapon go off.  Weapon Procs

GameServer/gameobjects/GameNPC.cs
@@ -2891,16 +2891,6 @@ namespace DOL.GS
 			base.StartInterruptTimer(duration, attackType, attacker);
 		}
 
-		protected override bool CheckRangedAttackInterrupt(GameLiving attacker, AttackData.eAttackType attackType)
-		{
-			bool interrupted = base.CheckRangedAttackInterrupt(attacker, attackType);
-
-			if (interrupted)
-				attackComponent.attackAction.OnAimInterrupt(attacker);
-
-			return interrupted;
-		}
-
 		/// <summary>
 		/// The time to wait before each mob respawn
 		/// </summary>

GameServer/gameobjects/GamePlayer.cs
@@ -5263,22 +5263,6 @@ namespace DOL.GS
                 Out.SendCloseTimerWindow();
             }
         }
-        /// <summary>
-        /// Does needed interrupt checks and interrupts if needed
-        /// </summary>
-        /// <param name="attacker">the attacker that is interrupting</param>
-        /// <param name="attackType">The attack type</param>
-        /// <returns>true if interrupted successfully</returns>
-        protected override bool CheckRangedAttackInterrupt(GameLiving attacker, AttackData.eAttackType attackType)
-        {
-            if (base.CheckRangedAttackInterrupt(attacker, attackType))
-            {
-                attackComponent.attackAction.OnAimInterrupt(attacker);
-                return true;
-            }
-
-            return false;
-        }
 
         public override void TakeDamage(GameObject source, eDamageType damageType, int damageAmount, int criticalAmount)
         {

GameServer/keeps/Gameobjects/Guards/Lord.cs
@@ -318,15 +318,6 @@ namespace DOL.GS.Keeps
             return true;
         }
 
-        protected override bool CheckRangedAttackInterrupt(GameLiving attacker, AttackData.eAttackType attackType)
-        {
-            // Lords can only be interrupted by their own target, and in melee range.
-            if (MaxSpeedBase == 0 && (attacker != TargetObject || !IsWithinRadius(attacker, MeleeAttackRange)))
-               return false;
-
-            return base.CheckRangedAttackInterrupt(attacker, attackType);
-        }
-
         protected override ICharacterClass GetClass()
         {
             if (ModelRealm == eRealm.Albion) return new ClassArmsman();

commit 441b8333d1132e07daefcb196187cc5611e6c58b

`AttackAction`: Fix Critical Shot, Longshot not always stopping Regression caused by 79bba259b4a9c95de63f6befc9c9aca247d89773

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -244,8 +244,6 @@ namespace DOL.GS
                     _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.RapidFire;
                 else if (_owner.effectListComponent.ContainsEffectForEffectType(eEffect.TrueShot))
                     _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.Long;
-                else
-                    _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
 
                 // Must be done after changing RangedAttackType to account for RapidFire.
                 attackSpeed = AttackComponent.AttackSpeed(_weapon);
@@ -400,10 +398,10 @@ namespace DOL.GS
                 _owner.rangeAttackComponent.RangedAttackType,
                 _owner.rangeAttackComponent.Ammo);
 
-            if (_owner.rangeAttackComponent.RangedAttackType is eRangedAttackType.Critical)
-                _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
-            else if (_owner.rangeAttackComponent.RangedAttackType is eRangedAttackType.Long)
+            if (_owner.rangeAttackComponent.RangedAttackType is eRangedAttackType.Long)
                 (EffectListService.GetEffectOnTarget(_owner, eEffect.TrueShot) as TrueShotECSGameEffect)?.Cancel(true);
+
+            _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
 
             // A positive ticksToTarget means the effects of our attack will be delayed. Typically used for ranged attacks.
             if (_ticksToTarget > 0)