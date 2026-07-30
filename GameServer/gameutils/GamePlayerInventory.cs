90bf244fe6397768904956692b3db8d66017d209
Baptiste Marie <bm01@users.noreply.github.com>
2026-07-30 14:22:52 +0200

    `GamePlayerInventory`: Remove misleading comment This change was mistakently included with 3b3daa3d7f7e54e196845c71c9f3dc145bfcbbfe but is no longer strictly needed. Still good to have to prevent a full element bindings iteration on the next save.

--- GameServer/gameutils/GamePlayerInventory.cs
+++ GameServer/gameutils/GamePlayerInventory.cs
@@ -91,7 +91,6 @@ namespace DOL.GS
 
                             // Since the `DbInventoryItem` has just been loaded and we had to recreate one because of poo poo design, we need to take a new snapshot.
                             // The snapshot is used to ensure we're not saving every column on the next save.
-                            // We also flip Dirty back to false, otherwise LastTimeRowUpdated will return a different time.
                             playerItem.Dirty = false;
                             playerItem.TakeSnapshot();
 
