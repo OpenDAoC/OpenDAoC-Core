OpenDAoC/OpenDAoC-Core

commit bf85d0c11ae252657bd8648ca056da85cf1c635a

`NpcAttackAction`: Make ranged NPCs finish their current bow draw before considering changing target / moving in range This makes guards and immobile NPCs no longer lose aggro immediately when losing LoS and delay their target acquisition a bit (they were a little too smart). Drastically simplified how NPC follow is initiated: * Replaced NpcCastingComponent.IsAllowedToFollow with GameNPC.IsAllowedToFollow. Similar logic, but can handle archer guards too. Replaced redundant logic in NpcAttackAction.OnAimInterrupt accordingly. * Moved distance check and call to OnOutOfRangeOrNoLosRangedAttack from AttackComponent.NpcStartAttack to NpcAttackAction.PrepareRangedAttack. * Made ranged NPCs no longer switch to melee when out of range / no LoS.

GameServer/ECS-Components/AttackComponent.cs
@@ -718,41 +718,19 @@ namespace DOL.GS
             GameNPC npc = owner as GameNPC;
             npc.FireAmbientSentence(GameNPC.eAmbientTrigger.fighting, _startAttackTarget);
 
-            // NPCs aren't allowed to prepare their ranged attack while moving or out of range.
-            // If we have a running `AttackAction`, let it decide what to do. Not every NPC should start following their target and this allows us to react faster.
-            if (npc.ActiveWeaponSlot is eActiveWeaponSlot.Distance)
+            if (!LivingStartAttack())
+                return;
+
+            npc.TargetObject = _startAttackTarget;
+            npc.TurnTo(_startAttackTarget);
+
+            if (_startAttackTarget != npc.FollowTarget)
             {
-                if (!npc.IsWithinRadius(_startAttackTarget, AttackRange - 30))
-                {
-                    if (attackAction == null || !attackAction.OnOutOfRangeOrNoLosRangedAttack())
-                    {
-                        // Default behavior. If `AttackAction` doesn't handle it, tell the NPC to get closer to its target.
-                        StopAttack();
-                        npc.Follow(_startAttackTarget, npc.StickMinimumRange, npc.StickMaximumRange);
-                    }
-
-                    return;
-                }
-
                 if (npc.IsMoving)
                     npc.StopMoving();
+
+                npc.Follow(_startAttackTarget, npc.StickMinimumRange, npc.StickMaximumRange);
             }
-
-            if (LivingStartAttack())
-            {
-                npc.TargetObject = _startAttackTarget;
-
-                if (_startAttackTarget != npc.FollowTarget)
-                {
-                    if (npc.IsMoving)
-                        npc.StopMoving();
-
-                    npc.TurnTo(_startAttackTarget);
-                    npc.Follow(_startAttackTarget, npc.StickMinimumRange, npc.StickMaximumRange);
-                }
-            }
-            else if (npc.ActiveWeaponSlot is eActiveWeaponSlot.Distance)
-                npc.TurnTo(_startAttackTarget);
         }
 
         public void StopAttack()

GameServer/ECS-Components/Actions/AttackAction.cs
@@ -157,11 +157,6 @@ namespace DOL.GS
 
         public virtual void OnForcedWeaponSwitch() { }
 
-        public virtual bool OnOutOfRangeOrNoLosRangedAttack()
-        {
-            return true;
-        }
-
         private bool ShouldTick()
         {
             if (!IsTickDue())
@@ -451,7 +446,6 @@ namespace DOL.GS
                 _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.Long;
             else
                 _owner.rangeAttackComponent.RangedAttackType = eRangedAttackType.Normal;
-
 
             // Must be done after changing `RangedAttackType`.
             _interval = AttackComponent.AttackSpeed(_weapon);

GameServer/ECS-Components/Actions/NpcAttackAction.cs
@@ -32,8 +32,7 @@ namespace DOL.GS
             if (target is not GameLiving livingFollowTarget)
                 return;
 
-            if (IsArcherGuardOrImmobile &&
-                (livingFollowTarget.ActiveWeaponSlot is eActiveWeaponSlot.Distance || !livingFollowTarget.IsWithinRadius(_npcOwner, livingFollowTarget.attackComponent.AttackRange)))
+            if (!_npcOwner.IsAllowedToFollow(livingFollowTarget))
             {
                 _npcOwner.StopFollowing();
                 return;
@@ -58,28 +57,6 @@ namespace DOL.GS
                     break;
                 }
             }
-        }
-
-        public override bool OnOutOfRangeOrNoLosRangedAttack()
-        {
-            // If we're a guard or an immobile NPC, let's forget about our target so that we can attack another one and not stare at the wall.
-            // Otherwise, switch to melee, but keep the timer alive.
-            if (IsArcherGuardOrImmobile)
-            {
-                StandardMobBrain brain = _npcOwner.Brain as StandardMobBrain;
-
-                if (_losCheckTarget is GameLiving livingLosCheckTarget)
-                    brain.RemoveFromAggroList(livingLosCheckTarget);
-
-                return true;
-            }
-            else if (AttackComponent.AttackState && !_hasLos)
-            {
-                SwitchToMeleeAndTick();
-                return true;
-            }
-
-            return false;
         }
 
         protected override bool PrepareMeleeAttack()
@@ -116,20 +93,16 @@ namespace DOL.GS
                 !_npcOwner.IsWithinRadius(_target, meleeAttackRange) &&
                 !_wasMeleeWeaponSwitchForced)
             {
-                // But only if there is no timer running or if it has LoS on its current target.
-                // If the timer is running, it'll check for LoS continuously.
-                if (_checkLosTimer == null || !_checkLosTimer.IsAlive)
-                {
-                    SwitchToRangedAndTick();
-                    return false;
-                }
+                bool timerInactive = _checkLosTimer == null || !_checkLosTimer.IsAlive;
+                bool targetChanged = _losCheckTarget != _target;
 
-                if (_losCheckTarget != _target)
+                if (!timerInactive && targetChanged)
                 {
                     _hasLos = false;
                     _checkLosTimer.ChangeTarget(_target);
                 }
-                else if (_hasLos)
+
+                if (timerInactive || targetChanged || _hasLos)
                 {
                     SwitchToRangedAndTick();
                     return false;
@@ -166,11 +139,23 @@ namespace DOL.GS
             {
                 _hasLos = false;
                 _checkLosTimer.ChangeTarget(_target);
+                _interval = TICK_INTERVAL_FOR_NON_ATTACK;
+                return false;
             }
 
             if (!_hasLos)
             {
+                // If this is a failed LoS check and we reached the end of our current bow draw time (or FinalizeRangedAttack pre-prepared).
+                if (_npcOwner.rangeAttackComponent.RangedAttackState is not eRangedAttackState.None && _losCheckTarget == _target)
+                    OnOutOfRangeOrNoLosRangedAttack();
+
                 _interval = TICK_INTERVAL_FOR_NON_ATTACK;
+                return false;
+            }
+
+            if (!_npcOwner.IsWithinRadius(_target, _npcOwner.attackComponent.AttackRange - 30))
+            {
+                OnOutOfRangeOrNoLosRangedAttack();
                 return false;
             }
 
@@ -198,12 +183,17 @@ namespace DOL.GS
         {
             _losCheckTarget = _npcOwner.CurrentRegion.GetObject(targetId);
 
+            // The target may have changed. Don't act on an obsolete check.
             if (_losCheckTarget == null || _losCheckTarget != _target)
+            {
                 _hasLos = false;
-            else
-                _hasLos = response is LosCheckResponse.True;
+                return;
+            }
 
-            if (!_hasLos)
+            _hasLos = response is LosCheckResponse.True;
+
+            // Only react immediately if we aren't currently waiting for a bow draw completion.
+            if (!_hasLos && _npcOwner.rangeAttackComponent.RangedAttackState is eRangedAttackState.None)
                 OnOutOfRangeOrNoLosRangedAttack();
         }
 
@@ -227,6 +217,26 @@ namespace DOL.GS
         {
             _hasLos = true;
             _npcOwner.TurnTo(_losCheckTarget);
+        }
+
+        private void OnOutOfRangeOrNoLosRangedAttack()
+        {
+            // If we're a guard or an immobile NPC, let's forget about our target so that we can attack another one and not stare at the wall.
+            // Otherwise, switch to melee, but keep the timer alive.
+
+            if (IsArcherGuardOrImmobile)
+            {
+                StandardMobBrain brain = _npcOwner.Brain as StandardMobBrain;
+
+                if (_losCheckTarget is GameLiving livingLosCheckTarget)
+                    brain.RemoveFromAggroList(livingLosCheckTarget);
+
+                // We could not return here. This would force the NPC to draw its bow again.
+                return;
+            }
+
+            if (AttackComponent.AttackState)
+                _npcOwner.StopAttack();
         }
 
         private class CheckLosTimer : ECSGameTimerWrapperBase

GameServer/ECS-Components/NpcCastingComponent.cs
@@ -100,17 +100,6 @@ namespace DOL.GS
                 _npcOwner.Follow(target, _npcOwner.StickMinimumRange, _npcOwner.StickMaximumRange);
         }
 
-        public bool IsAllowedToFollow(GameObject target)
-        {
-            if (!IsCasterGuardOrImmobile)
-                return true;
-
-            if (target is not GameLiving livingTarget)
-                return false;
-
-            return livingTarget.ActiveWeaponSlot is not eActiveWeaponSlot.Distance && livingTarget.IsWithinRadius(_npcOwner, livingTarget.attackComponent.AttackRange);
-        }
-
         public void HandleLosCheckResponse(GamePlayer losChecker, LosCheckResponse response, ushort targetId)
         {
             GameObject target = _npcOwner.CurrentRegion.GetObject(targetId);

GameServer/ECS-Components/NpcMovementComponent.cs
@@ -185,7 +185,7 @@ namespace DOL.GS
 
         public void Follow(GameLiving target, int minDistance, int maxDistance)
         {
-            if (target == null || target.ObjectState is not eObjectState.Active || !Owner.castingComponent.IsAllowedToFollow(target))
+            if (target == null || target.ObjectState is not eObjectState.Active || !Owner.IsAllowedToFollow(target))
                 return;
 
             if (target != FollowTarget)
@@ -613,7 +613,7 @@ namespace DOL.GS
         private int FollowTick()
         {
             // Stop moving if the NPC is casting or using ranged weapons.
-            if (Owner.IsCasting || (Owner.IsAttacking && Owner.ActiveWeaponSlot is eActiveWeaponSlot.Distance))
+            if (Owner.IsCasting || Owner.rangeAttackComponent.RangedAttackState is not eRangedAttackState.None)
             {
                 StopMoving();
                 return Properties.GAMENPC_FOLLOWCHECK_TIME;

GameServer/gameobjects/GameNPC.cs
@@ -12,6 +12,7 @@ using DOL.AI.Brain;
 using DOL.Database;
 using DOL.Events;
 using DOL.GS.Housing;
+using DOL.GS.Keeps;
 using DOL.GS.Movement;
 using DOL.GS.PacketHandler;
 using DOL.GS.Quests;
@@ -887,6 +888,19 @@ namespace DOL.GS
 		public virtual void TurnTo(GameObject target, int duration = 0)
 		{
 			movementComponent.TurnTo(target, duration);
+		}
+
+		public bool IsAllowedToFollow(GameObject target)
+		{
+			if (MaxSpeedBase <= 0)
+				return false;
+
+			if (this is not GuardArcher and not GuardCaster)
+				return true;
+
+			return target is GameLiving livingTarget &&
+				livingTarget.ActiveWeaponSlot is not eActiveWeaponSlot.Distance &&
+				livingTarget.IsWithinRadius(this, livingTarget.attackComponent.AttackRange);
 		}
 
 		#endregion
@@ -2658,8 +2672,6 @@ namespace DOL.GS
 		{
 			if (ActiveWeaponSlot is not eActiveWeaponSlot.Distance)
 			{
-				StopFollowing();
-
 				if (attackComponent.AttackState)
 					attackComponent.StopAttack();
 

commit 48d60bf140ef46b5516ada123493adc0826efcb8

`AttackComponent`: Fix NPCs attempting to face their target when pathing Regression caused by bf85d0c11ae252657bd8648ca056da85cf1c635a

GameServer/ECS-Components/AttackComponent.cs
@@ -718,13 +718,13 @@ namespace DOL.GS
                 return;
 
             npc.TargetObject = _startAttackTarget;
-            npc.TurnTo(_startAttackTarget);
 
             if (_startAttackTarget != npc.FollowTarget)
             {
                 if (npc.IsMoving)
                     npc.StopMoving();
 
+                npc.TurnTo(_startAttackTarget);
                 npc.Follow(_startAttackTarget, npc.StickMinimumRange, npc.StickMaximumRange);
             }
         }