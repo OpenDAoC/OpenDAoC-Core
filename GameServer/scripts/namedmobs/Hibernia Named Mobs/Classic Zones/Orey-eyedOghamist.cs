22c8b2d55ac96a60a7abed84f15730260381eebb
Fenyn <saterny@gmail.com>
2026-08-03 11:16:48 -0700

    path cleanup orey-eyed oghamist

--- GameServer/scripts/namedmobs/Hibernia Named Mobs/Classic Zones/Orey-eyedOghamist.cs
+++ GameServer/scripts/namedmobs/Hibernia Named Mobs/Classic Zones/Orey-eyedOghamist.cs
@@ -3,12 +3,30 @@ using DOL.AI.Brain;
 using DOL.Database;
 using DOL.Events;
 using DOL.GS;
+using DOL.GS.Movement;
 
 namespace DOL.GS
 {
 	public class OreyEyedOghamist : GameEpicBoss
 	{
 		public OreyEyedOghamist() : base() { }
+
+		private static readonly (int X, int Y, int Z)[] _patrolPoints =
+		[
+			(504754, 505351, 4939),
+			(506042, 507443, 4945),
+			(505194, 509109, 5052),
+			(505401, 510887, 5382),
+			(503682, 512545, 5423),
+			(502505, 513021, 5204),
+			(501470, 515019, 5120),
+			(500667, 516402, 4848),
+			(498310, 516387, 4923),
+			(495586, 513451, 5510),
+			(495006, 509166, 4991),
+			(498351, 507021, 5059),
+			(500833, 506164, 5074)
+		];
 
 		[ScriptLoadedEvent]
 		public static void ScriptLoaded(DOLEvent e, object sender, EventArgs args)
@@ -54,16 +72,11 @@ namespace DOL.GS
 			LoadTemplate(npcTemplate);
 
             RespawnInterval = ServerProperties.Properties.SET_EPIC_GAME_ENCOUNTER_RESPAWNINTERVAL * 60000;//1min is 60000 miliseconds
+			CurrentPathPoint = MovementMgr.CreatePath(EPathType.Loop, MaxSpeedBase, _patrolPoints);
 			OreyEyedOghamistBrain sbrain = new OreyEyedOghamistBrain();
 			SetOwnBrain(sbrain);
 			base.AddToWorld();
 			return true;
-		}
-		public override void ReturnToSpawnPoint(short speed)
-		{
-			if (IsAlive)
-				return;
-			base.ReturnToSpawnPoint(speed);
 		}
 	}
 }
@@ -78,144 +91,12 @@ namespace DOL.AI.Brain
 			AggroRange = 800;
 			ThinkInterval = 1500;
 		}
-        #region checks
-		private bool point1check = false;
-		private bool point2check = false;
-		private bool point3check = false;
-		private bool point4check = false;
-		private bool point5check = false;
-		private bool point6check = false;
-		private bool point7check = false;
-		private bool point8check = false;
-		private bool point9check = false;
-		private bool point10check = false;
-		private bool point11check = false;
-		private bool point12check = false;
-		private bool point13check = false;
-        #endregion
         public override void Think()
 		{
 			if (Body.IsAlive)
 			{
-				Point3D point1 = new Point3D(504754, 505351, 4939);
-				Point3D point2 = new Point3D(506042, 507443, 4945);
-				Point3D point3 = new Point3D(505194, 509109, 5052);
-				Point3D point4 = new Point3D(505401, 510887, 5382);
-				Point3D point5 = new Point3D(503682, 512545, 5423);
-				Point3D point6 = new Point3D(502505, 513021, 5204);
-				Point3D point7 = new Point3D(501470, 515019, 5120);
-				Point3D point8 = new Point3D(500667, 516402, 4848);
-				Point3D point9 = new Point3D(498310, 516387, 4923);
-				Point3D point10 = new Point3D(495586, 513451, 5510);
-				Point3D point11 = new Point3D(495006, 509166, 4991);
-				Point3D point12 = new Point3D(498351, 507021, 5059);
-				Point3D point13 = new Point3D(500833, 506164, 5074);
-
 				if (!Body.Spells.Contains(OreyBomb))
 					Body.Spells.Add(OreyBomb);
-
-				if (!Body.InCombat && !HasAggro)
-				{
-					short pathSpeed = Body.MaxSpeedBase;
-					#region Orey path
-					if (!Body.IsWithinRadius(point1, 30) && !point1check)
-						Body.WalkTo(point1, pathSpeed);
-					else
-					{
-						point1check = true;
-						if (!Body.IsWithinRadius(point2, 30) && point1check && !point2check)
-							Body.WalkTo(point2, pathSpeed);
-						else
-						{
-							point2check = true;
-							point13check = false;
-							if (!Body.IsWithinRadius(point3, 30) && point1check && point2check && !point3check)
-								Body.WalkTo(point3, pathSpeed);
-							else
-							{
-								point3check = true;
-								if (!Body.IsWithinRadius(point4, 30) && point1check && point2check && point3check && !point4check)
-									Body.WalkTo(point4, pathSpeed);
-								else
-								{
-									point4check = true;
-									if (!Body.IsWithinRadius(point5, 30) && point1check && point2check && point3check && point4check && !point5check)
-										Body.WalkTo(point5, pathSpeed);
-									else
-									{
-										point5check = true;
-										if (!Body.IsWithinRadius(point6, 30) && point1check && point2check && point3check && point4check && point5check && !point6check)
-											Body.WalkTo(point6, pathSpeed);
-										else
-										{
-											point6check = true;
-											if (!Body.IsWithinRadius(point7, 30) && point1check && point2check && point3check && point4check && point5check && point6check && !point7check)
-												Body.WalkTo(point7, pathSpeed);
-											else
-											{
-												point7check = true;
-												if (!Body.IsWithinRadius(point8, 30) && point1check && point2check && point3check && point4check && point5check && point6check && point7check && !point8check)
-													Body.WalkTo(point8, pathSpeed);
-												else
-												{
-													point8check = true;
-													if (!Body.IsWithinRadius(point9, 30) && point1check && point2check && point3check && point4check && point5check && point6check && point7check && point8check
-														&& !point9check)
-														Body.WalkTo(point9, pathSpeed);
-													else
-													{
-														point9check = true;
-														if (!Body.IsWithinRadius(point10, 30) && point1check && point2check && point3check && point4check && point5check && point6check && point7check && point8check
-															&& point9check && !point10check)
-															Body.WalkTo(point10, pathSpeed);
-														else
-														{
-															point10check = true;
-															if (!Body.IsWithinRadius(point11, 30) && point1check && point2check && point3check && point4check && point5check && point6check && point7check && point8check
-																&& point9check && point10check && !point11check)
-																Body.WalkTo(point11, pathSpeed);
-															else
-															{
-																point11check = true;
-																if (!Body.IsWithinRadius(point12, 30) && point1check && point2check && point3check && point4check && point5check && point6check && point7check && point8check
-																&& point9check && point10check && point11check && !point12check)
-																	Body.WalkTo(point12, pathSpeed);
-																else
-																{
-																	point12check = true;
-																	if (!Body.IsWithinRadius(point13, 30) && point1check && point2check && point3check && point4check && point5check && point6check && point7check && point8check
-																		&& point9check && point10check && point11check && point12check && !point13check)
-																		Body.WalkTo(point13, pathSpeed);
-																	else
-																	{
-																		point13check = true;
-																		point1check = false;
-																		point2check = false;
-																		point3check = false;
-																		point4check = false;
-																		point5check = false;
-																		point6check = false;
-																		point7check = false;
-																		point8check = false;
-																		point9check = false;
-																		point10check = false;
-																		point11check = false;
-																		point12check = false;
-																	}
-																}
-															}
-														}
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-					#endregion
-				}
 			}
 			if(Body.TargetObject != null && HasAggro)
             {
