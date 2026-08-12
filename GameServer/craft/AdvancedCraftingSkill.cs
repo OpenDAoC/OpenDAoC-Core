c4b6244c364d24b853762b98cda729851411f4f9
Baptiste Marie <bm01@users.noreply.github.com>
2026-08-12 22:59:41 +0200

    `AdvancedCraftingSkill.CheckForTools`: Minor clean up

--- GameServer/craft/AdvancedCraftingSkill.cs
+++ GameServer/craft/AdvancedCraftingSkill.cs
@@ -1,24 +1,7 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
 using DOL.Database;
 using DOL.Language;
 using DOL.GS.PacketHandler;
+using System;
 
 namespace DOL.GS
 {
@@ -26,24 +9,21 @@ namespace DOL.GS
 	/// AdvancedCraftingSkill is the skill for alchemy and spellcrafting whitch add all combine system
 	/// </summary>
 	public abstract class AdvancedCraftingSkill : AbstractProfession
-    {
+	{
 		#region Classic craft function
 		protected override bool CheckForTools(GamePlayer player, Recipe recipe)
 		{
+			if (player.Client.Account.PrivLevel > 1)
+				return true;
+
 			foreach (GameStaticItem item in player.GetItemsInRadius(CRAFT_DISTANCE))
 			{
-				if (item.Name.ToLower() == "alchemy table" || item.Model == 820) // Alchemy Table
-				{
+				if (item.Name.Equals("alchemy table", StringComparison.OrdinalIgnoreCase) || item.Model == 820) // Alchemy Table
 					return true;
-				}
 			}
 
 			player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language, "Crafting.CheckTool.NotHaveTools", recipe.Product.Name), eChatType.CT_System, eChatLoc.CL_SystemWindow);
 			player.Out.SendMessage(LanguageMgr.GetTranslation(player.Client.Account.Language, "Crafting.CheckTool.FindAlchemyTable"), eChatType.CT_System, eChatLoc.CL_SystemWindow);
-
-			if (player.Client.Account.PrivLevel > 1)
-				return true;
-
 			return false;
 		}
 
