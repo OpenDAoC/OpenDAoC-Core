OpenDAoC/OpenDAoC-Core

commit f1327630dd7fdb8f95a313a47da78f3355a7a48c

Simplify how pets resume their "walk state" It was done in AttackMostWanted when no target could be found, but this was causing issues on Blackthorn where pets are meant to forget about their target if they stealth, because AttackMostWanted wasn't even called.
ControlledMobBrain:
* Added ResumeWalkState, called from AggressionState.
* Changed AttackMostWanted() to no longer be responsible to resume the "walk state".
* Changed FollowOwner() to no longer call Disengage() to avoid infinite recursion.
* Cleaned up m_tempX, Y, Z fields to a single Vector3.

ControlledMobState_DEFENSIVE:
* Changed Think() to call ControlledMobBrain.ResumeWalkState instead of ControlledMobBrain.Follow. May solve a bug where the NPC doesn't go to its ordered position if it casts a defensive spell before reaching it.

ControlledMobState_AGGRO:
* Changed Think() to call ControlledMobBrain.ResumeWalkState if there’s no valid target.
* Now calls ControlledMobBrain.Disengage() before leaving to Idle state, since ControlledMobBrain.AttackMostWanted() no longer does it.
* Fixed 1.88 code about NPCs not following players entering stealth (note: will not work well, since this will only interrupt spells during think ticks).
* Moved defensive spell check further down so that offensive spells being cast don't prevent the switch to Idle state block (CheckSpells() returns true during cast).
* Removed Exit() overload (redundant).

ControlledMobState_PASSIVE:
* Changed Think() to call ControlledMobBrain.ResumeWalkState instead of ControlledMobBrain.Follow.
* Removed Enter() overload (redundant).

GameServer/ai/brain/ControlledMob/ControlledMobBrain.cs
@@ -28,9 +28,7 @@ namespace DOL.AI.Brain
 		public const short MIN_OWNER_FOLLOW_DIST = 80;
 		public const short MAX_OWNER_FOLLOW_DIST = 10000;
 
-		protected int m_tempX = 0;
-		protected int m_tempY = 0;
-		protected int m_tempZ = 0;
+		protected Vector3? _tempPosition = Vector3.Zero;
 
 		/// <summary>
 		/// Holds the controlling player of this brain
@@ -203,14 +201,7 @@ namespace DOL.AI.Brain
                 if (m_aggressionState is eAggressionState.Passive)
                 {
                     Disengage();
-
-                    if (WalkState == eWalkState.Follow)
-                        FollowOwner();
-                    else if (m_tempX > 0 && m_tempY > 0 && m_tempZ > 0)
-                    {
-                        Body.StopFollowing();
-                        Body.PathTo(new Vector3(m_tempX, m_tempY, m_tempZ), Body.MaxSpeed);
-                    }
+                    ResumeWalkState();
                 }
             }
         }
@@ -258,9 +249,7 @@ namespace DOL.AI.Brain
 		/// </summary>
 		public virtual void Stay()
 		{
-			m_tempX = Body.X;
-			m_tempY = Body.Y;
-			m_tempZ = Body.Z;
+			_tempPosition = new(Body.X, Body.Y, Body.Z);
 			WalkState = eWalkState.Stay;
 			Body.StopMoving();
 		}
@@ -270,9 +259,7 @@ namespace DOL.AI.Brain
 		/// </summary>
 		public virtual void ComeHere()
 		{
-			m_tempX = Owner.X;
-			m_tempY = Owner.Y;
-			m_tempZ = Owner.Z;
+			_tempPosition = new(Owner.X, Owner.Y, Owner.Z);
 			WalkState = eWalkState.ComeHere;
 			Body.StopFollowing();
 			Body.PathTo(Owner, Body.MaxSpeed);
@@ -284,9 +271,7 @@ namespace DOL.AI.Brain
 		/// <param name="target"></param>
 		public virtual void Goto(GameObject target)
 		{
-			m_tempX = target.X;
-			m_tempY = target.Y;
-			m_tempZ = target.Z;
+			_tempPosition = new(target.X, target.Y, target.Z);
 			WalkState = eWalkState.GoTarget;
 			Body.StopFollowing();
 			Body.PathTo(target, Body.MaxSpeed);
@@ -310,9 +295,6 @@ namespace DOL.AI.Brain
 		/// </summary>
 		public virtual void FollowOwner()
 		{
-			if (Body.IsAttacking)
-				Disengage();
-
 			if (Owner is GamePlayer
 			    && IsMainPet
 			    && ((GamePlayer)Owner).CharacterClass.ID != (int)eCharacterClass.Animist
@@ -825,60 +807,58 @@ namespace DOL.AI.Brain
 
 			GameLiving target = CalculateNextAttackTarget();
 
-			if (target != null)
+			if (target == null)
+				return;
+
+			if (!Body.IsAttacking || target != Body.TargetObject)
 			{
-				if (!Body.IsAttacking || target != Body.TargetObject)
+				Body.TargetObject = target;
+
+				List<GameSpellEffect> effects = new List<GameSpellEffect>();
+
+				lock (Body.EffectList.Lock)
 				{
-					Body.TargetObject = target;
-
-					List<GameSpellEffect> effects = new List<GameSpellEffect>();
-
-					lock (Body.EffectList.Lock)
+					foreach (IGameEffect effect in Body.EffectList)
 					{
-						foreach (IGameEffect effect in Body.EffectList)
-						{
-							if (effect is GameSpellEffect gameSpellEffect && gameSpellEffect.SpellHandler is SpeedEnhancementSpellHandler)
-								effects.Add(gameSpellEffect);
-						}
+						if (effect is GameSpellEffect gameSpellEffect && gameSpellEffect.SpellHandler is SpeedEnhancementSpellHandler)
+							effects.Add(gameSpellEffect);
 					}
-
-					lock (Owner.EffectList.Lock)
-					{
-						foreach (IGameEffect effect in Owner.EffectList)
-						{
-							if (effect is GameSpellEffect gameSpellEffect && gameSpellEffect.SpellHandler is SpeedEnhancementSpellHandler)
-								effects.Add(gameSpellEffect);
-						}
-					}
-
-					foreach (GameSpellEffect effect in effects)
-						effect.Cancel(false);
 				}
 
-				if (CheckSpells(eCheckSpellType.Offensive))
-					Body.StopAttack();
-				else
-					Body.StartAttack(target);
+				lock (Owner.EffectList.Lock)
+				{
+					foreach (IGameEffect effect in Owner.EffectList)
+					{
+						if (effect is GameSpellEffect gameSpellEffect && gameSpellEffect.SpellHandler is SpeedEnhancementSpellHandler)
+							effects.Add(gameSpellEffect);
+					}
+				}
+
+				foreach (GameSpellEffect effect in effects)
+					effect.Cancel(false);
 			}
+
+			if (CheckSpells(eCheckSpellType.Offensive))
+				Body.StopAttack();
 			else
-			{
-				if (Body.IsAttacking)
-					Disengage();
-
-				if (WalkState == eWalkState.Follow)
-					FollowOwner();
-				else if (m_tempX > 0 && m_tempY > 0 && m_tempZ > 0)
-				{
-					Body.StopFollowing();
-					Body.PathTo(new Vector3(m_tempX, m_tempY, m_tempZ), Body.MaxSpeed);
-				}
-			}
+				Body.StartAttack(target);
 		}
 
 		public override void Disengage()
 		{
 			m_orderAttackTarget = null;
 			base.Disengage();
+		}
+
+		public void ResumeWalkState()
+		{
+			if (WalkState is eWalkState.Follow)
+				FollowOwner();
+			else if (_tempPosition.HasValue)
+			{
+				Body.StopMoving();
+				Body.PathTo(_tempPosition.Value, Body.MaxSpeed);
+			}
 		}
 
 		public virtual void OnOwnerAttacked(AttackData ad)

GameServer/ai/brain/Bonedancer/BdPetBrain.cs
@@ -43,9 +43,6 @@ namespace DOL.AI.Brain
 
         public override void FollowOwner()
         {
-            if (Body.IsAttacking)
-                Disengage();
-
             Body.Follow(Owner, MIN_OWNER_FOLLOW_DIST, MAX_OWNER_FOLLOW_DIST);
         }
 

GameServer/ai/brain/ControlledMob/ControlledMobState.cs
@@ -58,29 +58,17 @@ namespace DOL.AI.Brain
             else if (brain.AggressionState is eAggressionState.Passive)
                 brain.FSM.SetCurrentState(eFSMStateType.PASSIVE);
 
-            // Handle pet movement.
-            if (brain.WalkState is eWalkState.Follow && brain.Owner != null)
-                brain.Follow(brain.Owner);
+            brain.CheckAbilities();
 
             // Cast defensive spells if applicable.
-            brain.CheckSpells(StandardMobBrain.eCheckSpellType.Defensive);
-            brain.CheckAbilities();
+            if (!brain.CheckSpells(StandardMobBrain.eCheckSpellType.Defensive))
+                brain.ResumeWalkState();
         }
     }
 
     public class ControlledMobState_AGGRO : StandardMobState_AGGRO
     {
         public ControlledMobState_AGGRO(ControlledMobBrain brain) : base(brain) { }
-
-        public override void Exit()
-        {
-            _brain.ClearAggroList();
-
-            if (_brain.Body.IsAttacking)
-                _brain.Body.StopAttack();
-
-            _brain.Body.TargetObject = null;
-        }
 
         public override void Think()
         {
@@ -104,37 +92,32 @@ namespace DOL.AI.Brain
             if (brain.AggressionState is eAggressionState.Aggressive)
                 brain.CheckProximityAggro();
 
-            /* this was added in 1.88 : https://camelotherald.fandom.com/wiki/Patch_Notes:_Version_1.88
-             * removing to conform to 1.65
-            // Stop hunting player entering in steath
-            if (brain.Body.TargetObject != null && brain.Body.TargetObject is GamePlayer)
+            // This was added in 1.88 : https://camelotherald.fandom.com/wiki/Patch_Notes:_Version_1.88
+            // Removing to conform to 1.65.
+            /*if (brain.Body.TargetObject is GamePlayer playerTarget && playerTarget.IsStealthed)
             {
-                GamePlayer player = brain.Body.TargetObject as GamePlayer;
-                if (brain.Body.IsAttacking && player.IsStealthed && !brain.previousIsStealthed)
-                {
-                    brain.FSM.SetCurrentState(eFSMStateType.IDLE);
-                }
-                brain.previousIsStealthed = player.IsStealthed;
+                brain.RemoveFromAggroList(playerTarget);
+                brain.OrderedAttackTarget = null;
             }*/
-
-            bool hasTarget = brain.HasAggro || brain.OrderedAttackTarget != null;
-
-            // Check for buffs, heals, etc, interrupting melee if not being interrupted.
-            if (!hasTarget)
-            {
-                if (brain.CheckSpells(StandardMobBrain.eCheckSpellType.Defensive))
-                    return;
-
-                // Return to defensive if there's no valid target, unless confused.
-                if (brain.AggressionState is not eAggressionState.Aggressive && !brain.Body.IsConfused)
-                {
-                    brain.FSM.SetCurrentState(eFSMStateType.IDLE);
-                    return;
-                }
-            }
 
             brain.AttackMostWanted();
             brain.CheckAbilities();
+
+            if (!brain.HasAggro && brain.OrderedAttackTarget == null)
+            {
+                // Return to defensive if there's no valid target, unless confused.
+
+                if (brain.AggressionState is not eAggressionState.Aggressive && !brain.Body.IsConfused)
+                {
+                    brain.Disengage();
+                    brain.FSM.SetCurrentState(eFSMStateType.IDLE);
+                    return;
+                }
+
+                // Only check defensive spells if there's no target.
+                if (!brain.CheckSpells(StandardMobBrain.eCheckSpellType.Defensive))
+                    brain.ResumeWalkState();
+            }
         }
     }
 
@@ -143,14 +126,6 @@ namespace DOL.AI.Brain
         public override eFSMStateType StateType => eFSMStateType.PASSIVE;
 
         public ControlledMobState_PASSIVE(ControlledMobBrain brain) : base(brain) { }
-
-        public override void Enter()
-        {
-            if (_brain.Body.IsCasting)
-                _brain.Body.StopCurrentSpellcast();
-
-            base.Enter();
-        }
 
         public override void Think()
         {
@@ -170,13 +145,11 @@ namespace DOL.AI.Brain
             else if (brain.AggressionState is eAggressionState.Defensive)
                 brain.FSM.SetCurrentState(eFSMStateType.IDLE);
 
-            // Handle pet movement.
-            if (brain.WalkState is eWalkState.Follow && brain.Owner != null)
-                brain.Follow(brain.Owner);
-
-            // Cast defensive spells if applicable.
-            brain.CheckSpells(StandardMobBrain.eCheckSpellType.Defensive);
             brain.CheckAbilities();
+
+            if (!brain.CheckSpells(StandardMobBrain.eCheckSpellType.Defensive))
+                brain.ResumeWalkState();
+
         }
     }
 }

GameServer/ai/brain/FollowOwnerBrain.cs
@@ -27,9 +27,6 @@ namespace DOL.AI.Brain
 		/// </summary>
 		public override void FollowOwner()
 		{
-			if (Body.IsAttacking)
-				Disengage();
-
 			Body.Follow(Owner, MIN_OWNER_FOLLOW_DIST, MAX_OWNER_FOLLOW_DIST);
 		}
 		

GameServer/ai/brain/IControlledBrain.cs
@@ -1,21 +1,3 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
 using DOL.GS;
 
 namespace DOL.AI.Brain
@@ -67,6 +49,7 @@ namespace DOL.AI.Brain
         GameLiving Owner { get; }
         void Attack(GameObject target);
         void Disengage();
+        void ResumeWalkState();
         void CheckAggressionStateOnPlayerOrder();
         void Follow(GameObject target);
         void FollowOwner();

GameServer/ai/brain/NoveltyPetBrain.cs
@@ -1,22 +1,3 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
-
 using DOL.GS;
 
 namespace DOL.AI.Brain
@@ -84,6 +65,7 @@ namespace DOL.AI.Brain
 		public GameLiving Owner { get { return m_owner; } }
 		public void Attack(GameObject target) { }
 		public void Disengage() { }
+		public void ResumeWalkState() { }
 		public void CheckAggressionStateOnPlayerOrder() { }
 		public void Follow(GameObject target) { }
 		public void FollowOwner() { }

GameServer/ai/brain/ProcPetBrain.cs
@@ -1,22 +1,3 @@
-/*
- * DAWN OF LIGHT - The first free open source DAoC server emulator
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
-
 using DOL.GS;
 
 namespace DOL.AI.Brain
@@ -79,7 +60,8 @@ namespace DOL.AI.Brain
 		public eAggressionState AggressionState { get { return eAggressionState.Aggressive; } set { } }
 		public GameLiving Owner { get { return m_owner; } }
 		public void Attack(GameObject target) { }
-		public void Disengage() { }
+		public override void Disengage() { }
+		public void ResumeWalkState() { }
 		public void CheckAggressionStateOnPlayerOrder() { }
 		public void Follow(GameObject target) { }
 		public void FollowOwner() { }

GameServer/spells/Masterlevel/Convoker.cs
@@ -745,9 +745,6 @@ namespace DOL.AI.Brain
 
 		public override void FollowOwner()
 		{
-			if (Body.IsAttacking)
-				Disengage();
-
 			Body.Follow(Owner, MIN_OWNER_FOLLOW_DIST, MAX_OWNER_FOLLOW_DIST);
 		}
 	}

commit b9a1095bd5ab8b2943487a009d85ae862565d3ff

`ControlledMobBrain.AttackMostWanted`: Call GameNPC.StopAttack when CalculateNextAttackTarget returns null Regression caused by f1327630dd7fdb8f95a313a47da78f3355a7a48c (used to call Disengage). Removed obsolete speed buff handling

GameServer/ai/brain/ControlledMob/ControlledMobBrain.cs
@@ -5,12 +5,10 @@ using System.Numerics;
 using System.Reflection;
 using System.Threading;
 using DOL.GS;
-using DOL.GS.Effects;
 using DOL.GS.PacketHandler;
 using DOL.GS.RealmAbilities;
 using DOL.GS.ServerProperties;
 using DOL.GS.SkillHandler;
-using DOL.GS.Spells;
 
 namespace DOL.AI.Brain
 {
@@ -580,35 +578,12 @@ namespace DOL.AI.Brain
 			GameLiving target = CalculateNextAttackTarget();
 
 			if (target == null)
-				return;
-
-			if (!Body.IsAttacking || target != Body.TargetObject)
 			{
-				Body.TargetObject = target;
-
-				List<GameSpellEffect> effects = new List<GameSpellEffect>();
-
-				lock (Body.EffectList.Lock)
-				{
-					foreach (IGameEffect effect in Body.EffectList)
-					{
-						if (effect is GameSpellEffect gameSpellEffect && gameSpellEffect.SpellHandler is SpeedEnhancementSpellHandler)
-							effects.Add(gameSpellEffect);
-					}
-				}
-
-				lock (Owner.EffectList.Lock)
-				{
-					foreach (IGameEffect effect in Owner.EffectList)
-					{
-						if (effect is GameSpellEffect gameSpellEffect && gameSpellEffect.SpellHandler is SpeedEnhancementSpellHandler)
-							effects.Add(gameSpellEffect);
-					}
-				}
-
-				foreach (GameSpellEffect effect in effects)
-					effect.Cancel(false);
+				Body.StopAttack();
+				return;
 			}
+
+			Body.TargetObject = target;
 
 			if (CheckSpells(eCheckSpellType.Offensive))
 				Body.StopAttack();