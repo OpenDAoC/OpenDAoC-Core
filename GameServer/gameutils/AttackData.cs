OpenDAoC/OpenDAoC-Core

commit 57a8584ff01f25181474a78d1b9f68ddd3a4a299

Fix damage adds / shield to use independent weapon speeds for dual wield

GameServer/gameutils/AttackData.cs
@@ -58,6 +58,13 @@ namespace DOL.GS
 
         public AttackData() { }
 
+        public int GetWeaponAttackSpeed()
+        {
+            // Used by damage adds and shields to calculate the damage based on the weapon's attack speed.
+            // Note: AttackSpeed returns capped attack speed, which may not be correct for the slowest weapon when dual wielding.
+            return Weapon != null ? Attacker.attackComponent.AttackSpeed(Weapon) : 0;
+        }
+
         public static eAttackType GetAttackType(DbInventoryItem weapon, WeaponAction action, GameLiving attacker)
         {
             if (action != null)

GameServer/ECS-Effects/TripleWieldECSEffect.cs
@@ -40,7 +40,8 @@ namespace DOL.GS
             if (attacker == null || attacker.ObjectState is not GameObject.eObjectState.Active || !attacker.IsAlive)
                 return;
 
-            double damage = attackData.Attacker.WeaponDamage(attackData.Weapon) * attackData.Interval * 0.0005; // 50% of weapon DPS damage add.
+            int weaponAttackSpeed = attackData.GetWeaponAttackSpeed();
+            double damage = attackData.Attacker.WeaponDamage(attackData.Weapon) * weaponAttackSpeed * 0.0005; // 50% of weapon DPS damage add.
             double damageResisted = damage * target.GetResist(eDamageType.Body) * -0.01;
 
             AttackData ad = new()
@@ -50,9 +51,8 @@ namespace DOL.GS
                 Damage = (int) (damage + damageResisted),
                 Modifier = (int) damageResisted,
                 DamageType = eDamageType.Body,
-                AttackType = AttackData.eAttackType.MeleeOneHand,
-                AttackResult = eAttackResult.HitUnstyled,
-                Interval = attackData.Interval
+                AttackType = AttackData.eAttackType.Spell,
+                AttackResult = eAttackResult.HitUnstyled
             };
 
             if (attacker is GamePlayer playerAttacker)

GameServer/effects/TripleWieldEffect.cs
@@ -58,9 +58,8 @@ namespace DOL.GS.Effects
 			if (atkArgs.AttackData.IsOffHand) return; // only react to main hand
 			if (atkArgs.AttackData.Weapon == null) return; // no weapon attack
 
-			//double dpsCap = (1.2 + 0.3 * attacker.Level) * 0.7;
-			//double dps = Math.Min(atkArgs.AttackData.Weapon.DPS_AF/10.0, dpsCap);
-			double damage = atkArgs.AttackData.Weapon.DPS_AF * atkArgs.AttackData.Interval * 0.001;
+			int weaponAttackSpeed = atkArgs.AttackData.GetWeaponAttackSpeed();
+			double damage = atkArgs.AttackData.Weapon.DPS_AF * weaponAttackSpeed * 0.001;
 			double damageResisted = damage * target.GetResist(eDamageType.Body) * -0.01;
 
 			AttackData ad = new AttackData();
@@ -69,9 +68,8 @@ namespace DOL.GS.Effects
 			ad.Damage = (int)(damage + damageResisted);
 			ad.Modifier = (int)damageResisted;
 			ad.DamageType = eDamageType.Body;
-			ad.AttackType = AttackData.eAttackType.MeleeOneHand;
+			ad.AttackType = AttackData.eAttackType.Spell;
 			ad.AttackResult = eAttackResult.HitUnstyled;
-			ad.Interval = atkArgs.AttackData.Interval;
 
 			GamePlayer owner = attacker as GamePlayer;
 			if (owner != null) {

GameServer/realmabilities/effects/rr5/MarkofPreyEffect.cs
@@ -102,7 +102,7 @@ namespace DOL.GS.Effects
 			dpsCap = (1.2 + 0.3 * attacker.Level) * 0.7;
 
 			double dps = Math.Min(RealmAbilities.MarkOfPreyAbility.VALUE, dpsCap);
-			double damage = dps * atkArgs.AttackData.Interval * 0.001;
+			double damage = dps * atkArgs.AttackData.GetWeaponAttackSpeed() * 0.001;
 			double damageResisted = damage * target.GetResist(eDamageType.Heat) * -0.01;
 
 			AttackData ad = new AttackData();

GameServer/spells/DamageAddAndShield.cs
@@ -111,7 +111,7 @@ namespace DOL.GS.Spells
             double damage;
 
             if (Spell.Damage > 0)
-                damage = Spell.Damage * attackData.Interval * 0.001;
+                damage = Spell.Damage * attackData.GetWeaponAttackSpeed() * 0.001;
             else
                 damage = attackData.BaseDamage * (1 * -Spell.Damage * 0.01);
 

commit 6e2d36b9e776443f9eb50f88888c87d18b0600cf

`AttackData`: Fix damage adds / shields on NPCs with no weapon Regression caused by 57a8584ff01f25181474a78d1b9f68ddd3a4a299

GameServer/gameutils/AttackData.cs
@@ -62,7 +62,7 @@ namespace DOL.GS
         {
             // Used by damage adds and shields to calculate the damage based on the weapon's attack speed.
             // Note: AttackSpeed returns capped attack speed, which may not be correct for the slowest weapon when dual wielding.
-            return Weapon != null ? Attacker.attackComponent.AttackSpeed(Weapon) : 0;
+            return Attacker.attackComponent.AttackSpeed(Weapon);
         }
 
         public static eAttackType GetAttackType(DbInventoryItem weapon, WeaponAction action, GameLiving attacker)